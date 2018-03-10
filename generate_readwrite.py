c6c0494e5badc40c1cbf1dceb5dc1d79c41783a1
Omar Sandoval <osandov@osandov.com>
2018-03-10 12:40:15 -0800

    readwrite: return int instead of object
    This shaves off about 8% of time in parsing relocations and DWARF since
    we don't have to refcount None.

---
+++ generate_readwrite.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+SIZES = [8, 16, 32, 64]
+
+
+def generate_read(name, read_type, ret_type=None):
+    if ret_type is None:
+        ret_type = read_type
+
+    print(f"""
+
+cdef inline int read_{name}(Py_buffer *buffer, Py_ssize_t *offset, {ret_type} *ret) except -1:
+    check_bounds(buffer, offset[0], sizeof({read_type}))
+    ret[0] = (<const {read_type} *>(<const char *>buffer.buf + offset[0]))[0]
+    offset[0] += sizeof({read_type})
+    return 0""")
+
+
+def generate_write(name, write_type):
+    print(f"""
+
+cdef inline int write_{name}(Py_buffer *buffer, Py_ssize_t offset, {write_type} value) except -1:
+    check_bounds(buffer, offset, sizeof({write_type}))
+    (<{write_type} *>(<char *>buffer.buf + offset))[0] = value
+    return 0""")
+
+
+if __name__ == '__main__':
+    for size in SIZES:
+        generate_read(f's{size}', f'int{size}_t')
+        generate_read(f'u{size}', f'uint{size}_t')
+        if size < 64:
+            generate_read(f'u{size}_into_u64', f'uint{size}_t', f'uint64_t')
+            generate_read(f'u{size}_into_ssize_t', f'uint{size}_t', f'Py_ssize_t')
+        generate_write(f'u{size}', f'uint{size}_t')
