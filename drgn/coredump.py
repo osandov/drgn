3f6130a9b380b5113783026e371eb50ab379c2d9
Omar Sandoval <osandov@osandov.com>
2018-04-05 00:22:56 -0700

    coredump: document Coredump and CoredumpObject

--- drgn/coredump.py
+++ drgn/coredump.py
@@ -2,6 +2,7 @@ from drgn.dwarf import DW_TAG
 from drgn.elf import ElfFile
 from drgn.type import (
     ArrayType,
+    CompoundType,
     PointerType,
     TypedefType,
     TypeFactory,
@@ -12,93 +13,169 @@ import os
 
 
 class CoredumpObject:
-    def __init__(self, coredump, address, type_):
-        self._coredump = coredump
-        self._address = address
-        self._type = type_
-        while isinstance(type_, TypedefType):
-            type_ = type_.type
-        self._real_type = type_
+    """
+    CoredumpObject(coredump, address, type) -> new object
+
+    A CoredumpObject represents an object in the memory of a program. It has
+    three members: coredump_, the program this object is from; address_, the
+    location in memory where this object resides in the program; and type_, the
+    type of this object in the program.
+
+    CoredumpObjects try to behave transparently like the object they represent
+    in C. E.g., structure members can be accessed with the dot (".") operator
+    and arrays can be subscripted with "[]". Note that because the structure
+    dereference operator ("->") is not valid syntax in Python, "." is also used
+    to access members of pointers to structures. Similarly, the indirection
+    operator ("*") is not valid syntax in Python, so pointers can be
+    dereferenced with "[0]" (e.g., write "p[0]" instead of "*p").
+
+    CoredumpObject members and methods are named with a trailing underscore to
+    avoid conflicting with structure or union members. The helper methods
+    always take precedence over structure members; use member_() if there is a
+    conflict.
+    """
+
+    def __init__(self, coredump, address, type):
+        self.coredump_ = coredump
+        self.address_ = address
+        self.type_ = type
+        while isinstance(type, TypedefType):
+            type = type.type
+        self._real_type = type
+
+    def __getattr__(self, name):
+        """Implement self.name. Shortcurt for self.member_(name)"""
+        return self.member_(name)
+
+    def __getitem__(self, idx):
+        """
+        Implement self[idx]. Return a CoredumpObject representing an array
+        element at the given index.
+
+        This is only valid for pointers and arrays.
+        """
+        if isinstance(self._real_type, PointerType):
+            buffer = self.coredump_.read(self.address_, self._real_type.sizeof())
+            address = self._real_type.read(buffer)
+            offset = idx.__index__() * self._real_type.type.sizeof()
+        elif isinstance(self._real_type, ArrayType):
+            address = self.address_
+            offset = idx.__index__() * self._real_type.type.sizeof()
+        else:
+            raise ValueError('not an array or pointer')
+        return CoredumpObject(self.coredump_, address + offset,
+                              self._real_type.type)
 
     def __repr__(self):
-        return f'CoredumpObject(address=0x{self._address:x}, type=<{self._type.type_name()}>)'
+        return f'CoredumpObject(address=0x{self.address_:x}, type=<{self.type_.type_name()}>)'
 
     def __str__(self):
-        buffer = self._coredump.read(self._address, self._real_type.sizeof())
+        """
+        Implement str(self). Return a string representation of the value of
+        this object in C syntax.
+        """
+        buffer = self.coredump_.read(self.address_, self._real_type.sizeof())
         return self._real_type.format(buffer)
 
-    def _value(self):
-        buffer = self._coredump.read(self._address, self._real_type.sizeof())
+    def value_(self):
+        """
+        Return the value of this object as a Python object.
+
+        For basic types (int, bool, etc.), this returns an object of the
+        directly corresponding Python type. For pointers, this returns the
+        address value of the pointer. For enums, this returns an enum.IntEnum
+        object. For structures and unions, this returns an OrderedDict of
+        members. For arrays, this returns a list of values.
+        """
+        buffer = self.coredump_.read(self.address_, self._real_type.sizeof())
         return self._real_type.read(buffer)
 
-    def _string(self):
+    def string_(self):
+        """
+        Return the null-terminated string pointed to by this object as bytes.
+
+        This is only valid for pointers and arrays.
+        """
+
         if isinstance(self._real_type, PointerType):
-            addresses = itertools.count(self._value())
+            addresses = itertools.count(self.value_())
         elif isinstance(self._real_type, ArrayType):
             if self._real_type.size is None:
-                addresses = itertools.count(self._address)
+                addresses = itertools.count(self.address_)
             else:
-                addresses = range(self._address, self._address + self._real_type.size)
+                addresses = range(self.address_, self.address_ + self._real_type.size)
         else:
             raise ValueError('not an array or pointer')
         b = bytearray()
         for address in addresses:
-            byte = self._coredump.read(address, 1)[0]
+            byte = self.coredump_.read(address, 1)[0]
             if not byte:
                 break
             b.append(byte)
         return bytes(b)
 
-    def _member(self, name):
+    def member_(self, name):
+        """
+        Return a CoredumpObject representing the given structure or union
+        member.
+
+        This is only valid for structs, unions, and pointers to either.
+        Normally the dot operator (".") can be used to accomplish the same
+        thing, but this method can be used if there is a name conflict with a
+        CoredumpObject member or method.
+        """
+
         if isinstance(self._real_type, PointerType):
-            address = self._value()
+            address = self.value_()
             type_ = self._real_type.type
         else:
-            address = self._address
+            address = self.address_
             type_ = self._real_type
+        if not isinstance(type_, CompoundType):
+            raise AttributeError()
         member_type = type_.typeof(name)
         offset = type_.offsetof(name)
-        return CoredumpObject(self._coredump, address + offset, member_type)
+        return CoredumpObject(self.coredump_, address + offset, member_type)
 
-    def _cast(self, type):
+    def cast_(self, type):
+        """
+        Return a copy of this object casted to another type. The given type is
+        usually a string, but it can also be a Type or TypeName object.
+        """
         if isinstance(type, TypeName):
-            type = self._coredump._type_factory.from_type_name(type)
+            type = self.coredump_._type_factory.from_type_name(type)
         elif isinstance(type, str):
-            type = self._coredump._type_factory.from_type_string(type)
+            type = self.coredump_._type_factory.from_type_string(type)
         elif not isinstance(type, Type):
             raise ValueError('type must be Type, TypeName, or string')
-        return CoredumpObject(self._coredump, self._address, type)
+        return CoredumpObject(self.coredump_, self.address_, type)
 
-    def _containerof(self, type, member):
+    def container_of_(self, type, member):
+        """
+        Return the containing object of the object pointed to by this object.
+        The given type is the type of the containing object, and the given
+        member is the name of this object in that type. This corresponds to the
+        container_of() macro in C.
+
+        This is only valid for pointers.
+        """
         if isinstance(type, TypeName):
-            type = self._coredump._type_factory.from_type_name(type)
+            type = self.coredump_._type_factory.from_type_name(type)
         elif isinstance(type, str):
-            type = self._coredump._type_factory.from_type_string(type)
+            type = self.coredump_._type_factory.from_type_string(type)
         elif not isinstance(type, Type):
             raise ValueError('type must be Type, TypeName, or string')
         if not isinstance(self._real_type, PointerType):
             raise ValueError('containerof is only valid on pointers')
-        address = self._value() - type.offsetof(member)
-        return CoredumpObject(self._coredump, address, type)
-
-    def __getitem__(self, item):
-        if isinstance(self._real_type, PointerType):
-            buffer = self._coredump.read(self._address, self._real_type.sizeof())
-            address = self._real_type.read(buffer)
-            offset = item.__index__() * self._real_type.type.sizeof()
-        elif isinstance(self._real_type, ArrayType):
-            address = self._address
-            offset = item.__index__() * self._real_type.type.sizeof()
-        else:
-            raise ValueError('not an array or pointer')
-        return CoredumpObject(self._coredump, address + offset,
-                              self._real_type.type)
-
-    def __getattr__(self, name):
-        return self._member(name)
+        address = self.value_() - type.offsetof(member)
+        return CoredumpObject(self.coredump_, address, type)
 
 
 class Coredump:
+    """
+    A Coredump object represents a crashed or running program to be debugged.
+    """
+
     def __init__(self, core_file, dwarf_index, symbols):
         self._core_file = core_file
         self._core_elf_file = ElfFile(core_file)
@@ -107,6 +184,12 @@ class Coredump:
         self._symbols = symbols
 
     def read(self, address, size):
+        """
+        Return size bytes of memory starting at address in the coredump.
+
+        >>> core.read(0xffffffffbe012b40, 16)
+        b'swapper/0\\x00\\x00\\x00\\x00\\x00\\x00\\x00'
+        """
         for phdr in self._core_elf_file.phdrs():
             if phdr.p_vaddr <= address <= phdr.p_vaddr + phdr.p_memsz:
                 break
@@ -115,8 +198,15 @@ class Coredump:
         return os.pread(self._core_file.fileno(), size,
                         phdr.p_offset + address - phdr.p_vaddr)
 
-    def __getitem__(self, key):
-        address = self._symbols[key][-1]
-        dwarf_type = self._dwarf_index.find(key, DW_TAG.variable).type()
+    def __getitem__(self, name):
+        """
+        Implement self[name]. Return a CoredumpObject representing the variable
+        with the given name.
+
+        >>> core['init_task']
+        CoredumpObject(address=0xffffffffbe012480, type=<struct task_struct>)
+        """
+        address = self._symbols[name][-1]
+        dwarf_type = self._dwarf_index.find(name, DW_TAG.variable).type()
         type_ = self._type_factory.from_dwarf_type(dwarf_type)
         return CoredumpObject(self, address, type_)
