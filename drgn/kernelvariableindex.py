631f50a2f66a0233cacf801795bfcdaa9b16fd28
Omar Sandoval <osandov@osandov.com>
2018-07-09 22:03:49 -0700

    variableindex: factor DwarfVariableIndex out of KernelVariableIndex
    The DWARF functionality can be shared with userspace programs.

--- drgn/kernelvariableindex.py
+++ drgn/kernelvariableindex.py
@@ -2,25 +2,23 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import os.path
-from typing import cast, Any, Optional, Tuple
+from typing import Tuple
 
 from drgn.elf import ET_EXEC
-from drgn.dwarf import Die, DW_TAG, DwarfAttribNotFoundError
+from drgn.dwarf import Die, DwarfAttribNotFoundError
 from drgn.helpers.kernel import list_for_each_entry
-from drgn.type import Type, EnumType
+from drgn.type import Type
 from drgn.typeindex import DwarfTypeIndex
 from drgn.program import Program
-from drgn.variableindex import VariableIndex
+from drgn.variableindex import DwarfVariableIndex
 
 
 # This would ideally just go in variableindex.py, but then we end up with
 # circular imports between program.py and variableindex.py.
 
-class KernelVariableIndex(VariableIndex):
+class KernelVariableIndex(DwarfVariableIndex):
     def __init__(self, type_index: DwarfTypeIndex, kaslr_offset: int = 0) -> None:
         super().__init__(type_index)
-        self._type_index: DwarfTypeIndex
-        self._dwarf_index = type_index._dwarf_index
         self._kaslr_offset = kaslr_offset
 
     # This isn't pretty, but we need the program in order to apply relocations
@@ -28,64 +26,35 @@ class KernelVariableIndex(VariableIndex):
     def set_program(self, program: Program) -> None:
         self._prog = program
 
-    def _find_die(self, name: str,
-                  filename: Optional[str] = None) -> Die:
-        for tag in [DW_TAG.variable, DW_TAG.enumerator]:
-            try:
-                dies = self._dwarf_index.find(name, tag)
-            except ValueError:
-                continue
-            for die in dies:
-                try:
-                    if filename is None or die.decl_file() == filename:
-                        return die
-                except DwarfAttribNotFoundError:
-                    continue
-        if filename is None:
-            raise ValueError(f'could not find {name!r}')
-        else:
-            raise ValueError(f'could not find {name!r} in {filename!r}')
+    def _find_variable_address(self, name: str, die: Die) -> int:
+        address = die.location()
+        dwarf_file = die.cu.dwarf_file
+        elf_file = dwarf_file.elf_file
 
-    def find(self, name: str,
-             filename: Optional[str] = None) -> Tuple[Type, Any, Optional[int]]:
-        die = self._find_die(name, filename)
-        if die.tag == DW_TAG.variable:
-            address = die.location()
-            dwarf_file = die.cu.dwarf_file
-            elf_file = dwarf_file.elf_file
-            # vmlinux is an executable file, kernel modules are relocatable
-            # files.
-            if elf_file.ehdr.e_type == ET_EXEC:
-                address += self._kaslr_offset
-            else:
-                file_name = os.path.basename(dwarf_file.path).split('.', 1)[0]
-                module_name = file_name.replace('-', '_').encode('ascii')
-                for mod in list_for_each_entry('struct module',
-                                               self._prog['modules'].address_of_(),
-                                               'list'):
-                    if mod.name.string_() == module_name:
-                        break
-                else:
-                    raise ValueError(f'{module_name.decode()} is not loaded')
-                for sym in elf_file.symbols[name]:
-                    if sym.st_value == address:
-                        break
-                else:
-                    raise ValueError(f'Could not find {name} symbol')
-                section_name = elf_file.shdrs[sym.st_shndx].name.encode()
-                mod_sects = mod.sect_attrs.attrs
-                for i in range(mod.sect_attrs.nsections):
-                    attr = mod.sect_attrs.attrs[i]
-                    if attr.name.string_() == section_name:
-                        address += attr.address.value_()
-                        break
-                else:
-                    raise ValueError(f'Could not find module section {section_name.decode()}')
-            try:
-                dwarf_type = die.type()
-            except DwarfAttribNotFoundError:
-                dwarf_type = die.specification().type()
-            return self._type_index.find_dwarf_type(dwarf_type), None, address
-        else:  # die.tag == DW_TAG.enumeration_type
-            type_ = cast(EnumType, self._type_index.find_dwarf_type(die))
-            return type_, getattr(type_.enum, name), None
+        # vmlinux is an executable file, kernel modules are relocatable
+        # files.
+        if elf_file.ehdr.e_type == ET_EXEC:
+            return address + self._kaslr_offset
+
+        file_name = os.path.basename(dwarf_file.path).split('.', 1)[0]
+        module_name = file_name.replace('-', '_').encode('ascii')
+        for mod in list_for_each_entry('struct module',
+                                       self._prog['modules'].address_of_(),
+                                       'list'):
+            if mod.name.string_() == module_name:
+                break
+        else:
+            raise ValueError(f'{module_name.decode()} is not loaded')
+        for sym in elf_file.symbols[name]:
+            if sym.st_value == address:
+                break
+        else:
+            raise ValueError(f'Could not find {name} symbol')
+        section_name = elf_file.shdrs[sym.st_shndx].name.encode()
+        mod_sects = mod.sect_attrs.attrs
+        for i in range(mod.sect_attrs.nsections):
+            attr = mod.sect_attrs.attrs[i]
+            if attr.name.string_() == section_name:
+                return address + attr.address.value_()
+        else:
+            raise ValueError(f'Could not find module section {section_name.decode()}')
--- drgn/variableindex.py
+++ drgn/variableindex.py
@@ -1,9 +1,10 @@
 # Copyright 2018 - Omar Sandoval
 # SPDX-License-Identifier: GPL-3.0+
 
-from typing import Any, Optional, Tuple
+from typing import cast, Any, Optional, Tuple
 
-from drgn.type import Type
+from drgn.dwarf import Die, DW_TAG, DwarfAttribNotFoundError
+from drgn.type import Type, EnumType
 from drgn.typeindex import DwarfTypeIndex, TypeIndex
 
 
@@ -14,3 +15,45 @@ class VariableIndex:
     def find(self, name: str,
              filename: Optional[str] = None) -> Tuple[Type, Any, Optional[int]]:
         raise NotImplementedError()
+
+
+class DwarfVariableIndex(VariableIndex):
+    def __init__(self, type_index: DwarfTypeIndex) -> None:
+        super().__init__(type_index)
+        self._type_index: DwarfTypeIndex
+        self._dwarf_index = type_index._dwarf_index
+
+    def _find_die(self, name: str,
+                  filename: Optional[str] = None) -> Die:
+        for tag in [DW_TAG.variable, DW_TAG.enumerator]:
+            try:
+                dies = self._dwarf_index.find(name, tag)
+            except ValueError:
+                continue
+            for die in dies:
+                try:
+                    if filename is None or die.decl_file() == filename:
+                        return die
+                except DwarfAttribNotFoundError:
+                    continue
+        if filename is None:
+            raise ValueError(f'could not find {name!r}')
+        else:
+            raise ValueError(f'could not find {name!r} in {filename!r}')
+
+    def _find_variable_address(self, name: str, die: Die) -> int:
+        raise NotImplementedError()
+
+    def find(self, name: str,
+             filename: Optional[str] = None) -> Tuple[Type, Any, Optional[int]]:
+        die = self._find_die(name, filename)
+        if die.tag == DW_TAG.variable:
+            address = self._find_variable_address(name, die)
+            try:
+                dwarf_type = die.type()
+            except DwarfAttribNotFoundError:
+                dwarf_type = die.specification().type()
+            return self._type_index.find_dwarf_type(dwarf_type), None, address
+        else:  # die.tag == DW_TAG.enumeration_type
+            type_ = cast(EnumType, self._type_index.find_dwarf_type(die))
+            return type_, getattr(type_.enum, name), None
