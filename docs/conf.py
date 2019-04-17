fbbc7c55a27369630d858d702bed63c6e90062a4
Omar Sandoval <osandov@osandov.com>
2019-04-17 01:08:06 -0700

    Add logo and favicon

--- docs/conf.py
+++ docs/conf.py
@@ -27,12 +27,19 @@ intersphinx_mapping = {
 
 manpages_url = 'http://man7.org/linux/man-pages/man{section}/{page}.{section}.html'
 
+html_static_path = ['_static']
+
 html_theme = 'alabaster'
 
 html_theme_options = {
     'description': 'Debugger-as-a-library',
+    'logo': 'logo.png',
+    'logo_name': True,
+    'logo_text_align': 'center',
     'github_user': 'osandov',
     'github_repo': 'drgn',
     'github_button': True,
     'github_type': 'star',
 }
+
+html_favicon = 'favicon.ico'
