1ace9de2e37586ad1679911275788a2b956364bc
Andrey Ignatov <rdna@fb.com>
2019-12-23 17:22:25 -0800

    helpers: add list_nulls helpers
    These list helpers are useful to iterate over nulls hash lists of
    sockets, specifically `ehash` member of `struct tcp_hashinfo`.
    
    Corresponding functions and macros are defined in
    `include/linux/list_nulls.h` kernel header.
    
    Signed-off-by: Andrey Ignatov <rdna@fb.com>

---
+++ drgn/helpers/linux/list_nulls.py
@@ -0,0 +1,66 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Nulls Lists
+-----------
+
+The ``drgn.helpers.linux.list_nulls`` module provides helpers for working with
+the special version of lists (``struct hlist_nulls_head`` and ``struct
+hlist_nulls_node``) in :linux:`include/linux/list_nulls.h` where the end of
+list is not a ``NULL`` pointer, but a "nulls" marker.
+"""
+
+from drgn import container_of
+
+
+__all__ = [
+    'is_a_nulls',
+    'hlist_nulls_empty',
+    'hlist_nulls_entry',
+    'hlist_nulls_for_each_entry',
+]
+
+
+def is_a_nulls(pos):
+    """
+    .. c:function:: bool is_a_nulls(struct hlist_nulls_node *pos)
+
+    Return whether a a pointer is a nulls marker.
+    """
+    return bool(pos.value_() & 1)
+
+
+def hlist_nulls_empty(head):
+    """
+    .. c:function:: bool hlist_nulls_empty(struct hlist_nulls_head *head)
+
+    Return whether a nulls hash list is empty.
+    """
+    return is_a_nulls(head.first)
+
+
+def hlist_nulls_entry(pos, type, member):
+    """
+    .. c:function:: type *hlist_nulls_entry(struct hlist_nulls_node *pos, type, member)
+
+    Return an entry in a nulls hash list.
+
+    The nulls hash list is assumed to be non-empty.
+    """
+    return container_of(pos, type, member)
+
+
+def hlist_nulls_for_each_entry(type, head, member):
+    """
+    .. c:function:: hlist_nulls_for_each_entry(type, struct hlist_nulls_head *head, member)
+
+    Iterate over all the entries in a nulls hash list specified by ``struct
+    hlist_nulls_head`` head, given the type of the entry and the ``struct
+    hlist_nulls_node`` member in that type.
+
+    :return: Iterator of ``type *`` objects.
+    """
+    pos = head.first
+    while not is_a_nulls(pos):
+        yield hlist_nulls_entry(pos, type, member)
+        pos = pos.next
