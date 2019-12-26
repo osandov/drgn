67725768bab34445b6dd27e14c5a599196992ba5
Andrey Ignatov <rdna@fb.com>
2019-12-26 10:13:52 -0800

    helpers: add network sockets helpers
    Add helpers for working with network sockets defined in
    include/net/sock.h:
    - sk_fullsock()
    - sk_nulls_for_each()
    
    sk_fullsock() is useful to filter out timewait and request sockets since
    not all operations can be applied to such sockets.
    
    sk_nulls_for_each() is useful to e.g. iterate over established sockets.
    
    Signed-off-by: Andrey Ignatov <rdna@fb.com>

---
+++ drgn/helpers/linux/net.py
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Networking
+----------
+
+The ``drgn.helpers.linux.net`` module provides helpers for working with the
+Linux kernel networking subsystem.
+"""
+
+from drgn.helpers.linux.list_nulls import hlist_nulls_for_each_entry
+from drgn.helpers.linux.tcp import get_tcp_states
+
+
+__all__ = [
+    'sk_fullsock',
+    'sk_nulls_for_each',
+]
+
+
+def sk_fullsock(sk):
+    """
+    .. c:function:: bool sk_fullsock(struct sock *sk)
+
+    Check whether a socket is a full socket, i.e., not a time-wait or request
+    socket.
+    """
+    TcpStates = get_tcp_states(sk.prog_)
+    return TcpStates(sk.__sk_common.skc_state) not in (TcpStates.SYN_RECV,
+                                                       TcpStates.TIME_WAIT)
+
+
+def sk_nulls_for_each(head):
+    """
+    .. c:function:: sk_nulls_for_each(struct hlist_nulls_head *head)
+
+    Iterate over all the entries in a nulls hash list of sockets specified by
+    ``struct hlist_nulls_head`` head.
+
+    :return: Iterator of ``struct sock`` objects.
+    """
+    for sk in hlist_nulls_for_each_entry( 'struct sock', head,
+                                         '__sk_common.skc_nulls_node'):
+        yield sk
