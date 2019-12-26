b33e97604bd0fdea52c0fdb4c9429d732323952e
Andrey Ignatov <rdna@fb.com>
2019-12-26 10:19:08 -0800

    helpers: add TCP states helper
    Add get_tcp_states() helper to get all TCP states enumerators and
    construct enum.IntEnum from them.
    
    The helper is useful in debugging TCP sockets.
    
    Signed-off-by: Andrey Ignatov <rdna@fb.com>

---
+++ drgn/helpers/linux/tcp.py
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+TCP
+---
+
+The ``drgn.helpers.linux.tcp`` module provides helpers for working with the TCP
+protocol in the Linux kernel.
+"""
+
+import enum
+
+
+__all__ = [
+    'get_tcp_states',
+]
+
+
+def _get_tcp_states(prog, enum_name):
+    # The enum type is anonymous, but it can be looked up from one of the
+    # enumerators.
+    raw_enumerators = prog['TCP_ESTABLISHED'].type_.enumerators
+    tcp_ = 'TCP_'
+    enumerators = [
+        (name[len(tcp_):], value) if name.startswith(tcp_) else (name, value)
+        for (name, value) in raw_enumerators
+        if name != 'TCP_MAX_STATES'
+    ]
+    return enum.IntEnum(enum_name, enumerators)
+
+
+def get_tcp_states(prog):
+    """
+    Get all definitions for the TCP protocol states
+    (:linux:`include/net/tcp_states.h`) as an :class:`enum.IntEnum` class.
+
+    :rtype: enum.IntEnum
+    """
+    enum_name = 'TcpStates'
+    try:
+        return prog.cache[enum_name]
+    except KeyError:
+        prog.cache[enum_name] = _get_tcp_states(prog, enum_name)
+        return prog.cache[enum_name]
