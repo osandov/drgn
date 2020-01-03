2681f6723fb1d5c29a64f9687b034efb563dd4c0
Omar Sandoval <osandov@osandov.com>
2020-01-02 16:22:38 -0800

    tests: add tests for new net and tcp helpers
    Because the vmtest kernels aren't currently built with networking
    support, we need to skip them if TCP isn't supported.

---
+++ tests/helpers/linux/test_net.py
@@ -0,0 +1,15 @@
+import os
+
+from drgn import cast
+from drgn.helpers.linux.fs import fget
+from drgn.helpers.linux.net import sk_fullsock
+from drgn.helpers.linux.pid import find_task
+from tests.helpers.linux import LinuxHelperTestCase, create_socket
+
+
+class TestNet(LinuxHelperTestCase):
+    def test_sk_fullsock(self):
+        with create_socket() as sock:
+            file = fget(find_task(self.prog, os.getpid()), sock.fileno())
+            sk = cast('struct socket *', file.private_data).sk.read_()
+            self.assertTrue(sk_fullsock(sk))
