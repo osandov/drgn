f03700b0fbf233d6bf61472dd8f25e067cc90967
Andrey Ignatov <rdna@fb.com>
2020-01-07 16:19:49 -0800

    examples: Print attached cgroup-bpf programs
    Support additional argument `bpf` in `cgroup.py` that, if specified,
    prints only cgroups with attached BPF programs in a way similar to
    `bpftool cgroup tree`. W/o the `bpf` argument the tool works as before.
    
    For example:
    
      # python36 -m drgn examples/linux/cgroup.py /sys/fs/cgroup/workload.slice/ bpf
      /workload.slice
          98761    BPF_CGROUP_SOCK_OPS            2               ned_cgrp_dctcp
      /workload.slice/workload-tw.slice/nc_0.58_dev082.prn2_598fe2e810bc4_2.task
          93488    BPF_CGROUP_INET_INGRESS        0               tw_ingress
          93487    BPF_CGROUP_INET_EGRESS         0               tw_egress
          93482    BPF_CGROUP_SOCK_OPS            2               tw_ipt_listen
          93480    BPF_CGROUP_INET6_BIND          0               tw_ipt_bind
          93481    BPF_CGROUP_INET6_CONNECT       0               tw_ipt_connect
          93483    BPF_CGROUP_UDP6_SENDMSG        0               tw_ipt_sendmsg
    
    Signed-off-by: Andrey Ignatov <rdna@fb.com>

--- examples/linux/cgroup.py
+++ examples/linux/cgroup.py
@@ -6,11 +6,20 @@ import sys
 from contextlib import contextmanager
 
 from drgn import cast
+from drgn.helpers import enum_type_to_class
 from drgn.helpers.linux import (
+    cgroup_bpf_prog_for_each,
     cgroup_path,
     css_for_each_descendant_pre,
     fget,
     find_task,
+)
+
+
+BpfAttachType = enum_type_to_class(
+    prog.type("enum bpf_attach_type"),
+    "BpfAttachType",
+    exclude=("__MAX_BPF_ATTACH_TYPE",),
 )
 
 
@@ -34,7 +43,27 @@ def get_cgroup():
         return cast("struct cgroup *", kn.priv)
 
 
+def print_cgroup_bpf_progs(cgrp):
+    cgroup_printed = False
+    for attach_type in BpfAttachType:
+        attach_flags = cgrp.bpf.flags[attach_type.value].value_()
+        for prog in cgroup_bpf_prog_for_each(cgrp, attach_type.value):
+            prog_id = prog.aux.id.value_()
+            prog_name = prog.aux.name.string_().decode()
+            if not cgroup_printed:
+                print(cgroup_path(cgrp).decode())
+                cgroup_printed = True
+            print(
+                "    {:<8} {:<30} {:<15} {:<15}".format(
+                    prog_id, attach_type.name, attach_flags, prog_name
+                )
+            )
+
+
 css = get_cgroup().self.address_of_()
 
 for pos in css_for_each_descendant_pre(css):
-    print(cgroup_path(pos.cgroup).decode())
+    if sys.argv[-1] == "bpf":
+        print_cgroup_bpf_progs(pos.cgroup)
+    else:
+        print(cgroup_path(pos.cgroup).decode())
