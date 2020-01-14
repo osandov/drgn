f5dcea2ec4b204be6dd163817cecef92f2ddf077
Omar Sandoval <osandov@osandov.com>
2020-01-14 15:04:38 -0800

    tests: add tests for kernfs helpers

---
+++ tests/helpers/linux/test_kernfs.py
@@ -0,0 +1,21 @@
+import os
+
+from drgn import cast
+from drgn.helpers.linux.fs import fget
+from drgn.helpers.linux.kernfs import kernfs_name, kernfs_path
+from drgn.helpers.linux.pid import find_task
+from tests.helpers.linux import LinuxHelperTestCase
+
+
+class TestKernfs(LinuxHelperTestCase):
+    def test_kernfs_name(self):
+        with open("/sys/kernel/vmcoreinfo", "r") as f:
+            file = fget(find_task(self.prog, os.getpid()), f.fileno())
+            kn = cast("struct kernfs_node *", file.f_inode.i_private)
+            self.assertEqual(kernfs_name(kn), b"vmcoreinfo")
+
+    def test_kernfs_path(self):
+        with open("/sys/kernel/vmcoreinfo", "r") as f:
+            file = fget(find_task(self.prog, os.getpid()), f.fileno())
+            kn = cast("struct kernfs_node *", file.f_inode.i_private)
+            self.assertEqual(kernfs_path(kn), b"/kernel/vmcoreinfo")
