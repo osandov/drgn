80fef04c704d9fac072d064eb08bd879323082c0
Serapheim Dimitropoulos <serapheim@delphix.com>
2020-02-03 10:27:49 -0800

    Add address attribute to FaultError exception

--- libdrgn/python/error.c
+++ libdrgn/python/error.c
@@ -3,6 +3,63 @@
 
 #include "drgnpy.h"
 
+int FaultError_init(PyObject *self, PyObject *args, PyObject *kwds)
+{
+	static char *keywords[] = {"message", "address", NULL};
+	PyObject *address, *message;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO:FaultError", keywords,
+					 &message, &address))
+		return -1;
+
+	if (PyObject_SetAttrString(self, "message", message) < 0 ||
+	    PyObject_SetAttrString(self, "address", address) < 0)
+		return -1;
+	return 0;
+}
+
+static PyObject *FaultError_str(PyObject *self)
+{
+	PyObject *message, *address, *args, *fmt, *ret = NULL;
+
+	message = PyObject_GetAttrString(self, "message");
+	if (!message)
+		return NULL;
+
+	address = PyObject_GetAttrString(self, "address");
+	if (!address)
+		goto out_message;
+
+	args = Py_BuildValue("OO", message, address);
+	if (!args)
+		goto out_address;
+
+	fmt = PyUnicode_FromString("%s: %#x");
+	if (!fmt)
+		goto out_args;
+
+	ret = PyUnicode_Format(fmt, args);
+
+	Py_DECREF(fmt);
+out_args:
+	Py_DECREF(args);
+out_address:
+	Py_DECREF(address);
+out_message:
+	Py_DECREF(message);
+	return ret;
+}
+
+PyTypeObject FaultError_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.FaultError",
+	.tp_basicsize = sizeof(PyBaseExceptionObject),
+	.tp_str = (reprfunc)FaultError_str,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+	.tp_doc = drgn_FaultError_DOC,
+	.tp_init = (initproc)FaultError_init,
+};
+
 static struct drgn_error drgn_error_python = {
 	.code = DRGN_ERROR_OTHER,
 	.message = "error in Python callback",
@@ -122,9 +179,15 @@ DRGNPY_PUBLIC void *set_drgn_error(struct drgn_error *err)
 	case DRGN_ERROR_LOOKUP:
 		PyErr_SetString(PyExc_LookupError, err->message);
 		break;
-	case DRGN_ERROR_FAULT:
-		PyErr_SetString(FaultError, err->message);
+	case DRGN_ERROR_FAULT: {
+		PyObject *exc;
+
+		exc = PyObject_CallFunction((PyObject *)&FaultError_type, "sK",
+					    err->message, err->address);
+		if (exc)
+			PyErr_SetObject((PyObject *)&FaultError_type, exc);
 		break;
+	}
 	case DRGN_ERROR_TYPE:
 		PyErr_SetString(PyExc_TypeError, err->message);
 		break;
