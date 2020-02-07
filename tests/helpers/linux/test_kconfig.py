23c7d340998f91044f7eef3b32c931c04141c039
Jay Kamat <jaygkamat@gmail.com>
2020-02-07 08:43:25 -0800

    helpers: Add get_config helper for getting kconfig map

---
+++ tests/helpers/linux/test_kconfig.py
@@ -0,0 +1,13 @@
+import os.path
+
+from drgn.helpers.linux.kconfig import get_kconfig
+
+from tests.helpers.linux import LinuxHelperTestCase
+
+
+class TestKconfig(LinuxHelperTestCase):
+    def test_get_kconfig(self):
+        if not os.path.isfile("/proc/config.gz"):
+            self.skipTest("kernel not built with CONFIG_IKCONFIG_PROC")
+        m = get_kconfig(self.prog)
+        self.assertIn(m["CONFIG_IKCONFIG"], {"y", "m"})
