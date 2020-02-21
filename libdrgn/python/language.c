3d3c32f84995de4764be1ae2e8d0a82dc9df2a54
Omar Sandoval <osandov@osandov.com>
2020-02-21 15:08:37 -0800

    libdrgn/python: add Language to Python bindings

---
+++ libdrgn/python/language.c
@@ -0,0 +1,64 @@
+// Copyright 2020 - Omar Sandoval
+// SPDX-License-Identifier: GPL-3.0+
+
+#include "drgnpy.h"
+#include "../language.h"
+
+static PyObject *Language_repr(Language *self)
+{
+	return PyUnicode_FromFormat("Language.%s", self->attr_name);
+}
+
+static PyObject *Language_get_name(Language *self, void *arg)
+{
+	return PyUnicode_FromString(self->language->name);
+}
+
+static PyGetSetDef Language_getset[] = {
+	{"name", (getter)Language_get_name, NULL, drgn_Language_name_DOC, NULL},
+	{},
+};
+
+PyTypeObject Language_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.Language",
+	.tp_basicsize = sizeof(Language),
+	.tp_repr = (reprfunc)Language_repr,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = drgn_Language_DOC,
+	.tp_getset = Language_getset,
+};
+
+static PyObject *languages_py[DRGN_NUM_LANGUAGES];
+
+PyObject *Language_wrap(const struct drgn_language *language)
+{
+	PyObject *obj = languages_py[language - drgn_languages];
+	Py_INCREF(obj);
+	return obj;
+}
+
+int add_languages(void)
+{
+	static const char *attr_names[] = {
+		[DRGN_LANGUAGE_C] = "C",
+	};
+	size_t i;
+
+	for (i = 0; i < DRGN_NUM_LANGUAGES; i++) {
+		Language *language_obj;
+		int ret;
+
+		language_obj = (Language *)Language_type.tp_alloc(&Language_type, 0);
+		if (!language_obj)
+			return -1;
+		language_obj->attr_name = attr_names[i];
+		language_obj->language = &drgn_languages[i];
+		languages_py[i] = (PyObject *)language_obj;
+		ret = PyDict_SetItemString(Language_type.tp_dict, attr_names[i],
+					   (PyObject *)language_obj);
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
--- libdrgn/python/module.c
+++ libdrgn/python/module.c
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 - Omar Sandoval
+// Copyright 2018-2020 - Omar Sandoval
 // SPDX-License-Identifier: GPL-3.0+
 
 #include "drgnpy.h"
@@ -171,6 +171,13 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	if (!OutOfBoundsError)
 		goto err;
 	PyModule_AddObject(m, "OutOfBoundsError", OutOfBoundsError);
+
+	if (PyType_Ready(&Language_type) < 0)
+		goto err;
+	Py_INCREF(&Language_type);
+	PyModule_AddObject(m, "Language", (PyObject *)&Language_type);
+	if (add_languages() == -1)
+		goto err;
 
 	if (PyStructSequence_InitType2(&Register_type, &Register_desc) == -1)
 		goto err;
