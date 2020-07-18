209eaee4858b0abad9d97241226fdddefc4caafb
Omar Sandoval <osandov@osandov.com>
2020-07-18 01:36:50 -0700

    setup.py: import setuptools before distutils
    setuptools recently started warning if distutils is imported before it.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # SPDX-License-Identifier: GPL-3.0+
 
+# setuptools must be imported before distutils (see pypa/setuptools#2230).
+from setuptools import setup, find_packages, Command
+
 import contextlib
 from distutils import log
 from distutils.command.build import build as _build
@@ -12,7 +15,6 @@ import os
 import os.path
 import re
 import pkg_resources
-from setuptools import setup, find_packages, Command
 from setuptools.command.build_ext import build_ext as _build_ext
 from setuptools.command.egg_info import egg_info as _egg_info
 from setuptools.extension import Extension
