d40526d85de2d9990126b0f6b8c52f11dea48c15
Omar Sandoval <osandov@osandov.com>
2020-08-25 18:07:31 -0700

    scripts: add Python include header path to cscope
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- scripts/cscope.sh
+++ scripts/cscope.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-find libdrgn -name '*.[ch]' -o -name '*.[ch].in' | cscope -bq -i-
+: ${PYTHON:=python3}
+cscope_args=(-bq -i-)
+
+python_include="$("$PYTHON" -c 'import sysconfig; print(sysconfig.get_path("include"))' 2>/dev/null)"
+if [[ -n $python_include ]] ; then
+	cscope_args+=("-I$python_include")
+fi
+python_platinclude="$("$PYTHON" -c 'import sysconfig; print(sysconfig.get_path("platinclude"))' 2>/dev/null)"
+if [[ -n $python_platinclude && $python_platinclude != $python_include ]] ; then
+	cscope_args+=("-I$python_platinclude")
+fi
+
+find libdrgn -name '*.[ch]' -o -name '*.[ch].in' | cscope "${cscope_args[@]}"
