81a203c48f0e9763f0b7701dc3c067bc0e4afe4f
Omar Sandoval <osandov@osandov.com>
2021-01-21 10:07:32 -0800

    helpers: fix for_each_{possible,online,present}_cpu() on v4.4
    Also reorder the definitions to alphabetical order and add tests.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/cpumask.py
+++ drgn/helpers/linux/cpumask.py
@@ -36,16 +36,28 @@ def for_each_cpu(mask: Object) -> Iterator[int]:
                 yield (word_bits * i) + j
 
 
-def for_each_possible_cpu(prog: Program) -> Iterator[int]:
-    """Iterate over all possible CPUs."""
-    return for_each_cpu(prog["__cpu_possible_mask"])
+def _for_each_cpu_mask(prog: Program, name: str) -> Iterator[int]:
+    try:
+        mask = prog[name]
+    except KeyError:
+        # Before Linux kernel commit c4c54dd1caf1 ("kernel/cpu.c: change type
+        # of cpu_possible_bits and friends") (in v4.5), the CPU masks are
+        # struct cpumask *cpu_foo_mask instead of
+        # struct cpumask __cpu_foo_mask.
+        mask = prog[name[2:]][0]
+    return for_each_cpu(mask)
 
 
 def for_each_online_cpu(prog: Program) -> Iterator[int]:
     """Iterate over all online CPUs."""
-    return for_each_cpu(prog["__cpu_online_mask"])
+    return _for_each_cpu_mask(prog, "__cpu_online_mask")
+
+
+def for_each_possible_cpu(prog: Program) -> Iterator[int]:
+    """Iterate over all possible CPUs."""
+    return _for_each_cpu_mask(prog, "__cpu_possible_mask")
 
 
 def for_each_present_cpu(prog: Program) -> Iterator[int]:
     """Iterate over all present CPUs."""
-    return for_each_cpu(prog["__cpu_present_mask"])
+    return _for_each_cpu_mask(prog, "__cpu_present_mask")
---
+++ tests/helpers/linux/test_cpumask.py
@@ -0,0 +1,41 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# SPDX-License-Identifier: GPL-3.0+
+
+from pathlib import Path
+
+from drgn.helpers.linux.cpumask import (
+    for_each_online_cpu,
+    for_each_possible_cpu,
+    for_each_present_cpu,
+)
+from tests.helpers.linux import LinuxHelperTestCase
+
+CPU_PATH = Path("/sys/devices/system/cpu")
+
+
+def parse_cpulist(cpulist):
+    cpus = set()
+    for cpu_range in cpulist.split(","):
+        first, sep, last = cpu_range.partition("-")
+        if sep:
+            cpus.update(range(int(first), int(last) + 1))
+        else:
+            cpus.add(int(first))
+    return cpus
+
+
+class TestCpuMask(LinuxHelperTestCase):
+    def _test_for_each_cpu(self, func, name):
+        self.assertEqual(
+            list(func(self.prog)),
+            sorted(parse_cpulist((CPU_PATH / name).read_text())),
+        )
+
+    def test_for_each_online_cpu(self):
+        self._test_for_each_cpu(for_each_online_cpu, "online")
+
+    def test_for_each_possible_cpu(self):
+        self._test_for_each_cpu(for_each_possible_cpu, "possible")
+
+    def test_for_each_present_cpu(self):
+        self._test_for_each_cpu(for_each_present_cpu, "present")
