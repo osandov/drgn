5f170ea3f39efae65cd082e17c902f65237f94ef
Omar Sandoval <osandov@osandov.com>
2021-01-21 10:45:03 -0800

    helpers: add per_cpu()
    The correct way to access global per-CPU variables
    (per_cpu_ptr(prog[name].address_of_(), cpu)) has been a common source of
    confusion (see #77). Add an analogue to the per_cpu() macro in the
    kernel as a shortcut and document it as the easiest method for getting a
    global per-CPU variable: per_cpu(prog[name], cpu).
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/percpu.py
+++ drgn/helpers/linux/percpu.py
@@ -14,18 +14,42 @@ from drgn import IntegerLike, Object
 from drgn.helpers.linux.cpumask import for_each_online_cpu
 
 __all__ = (
+    "per_cpu",
     "per_cpu_ptr",
     "percpu_counter_sum",
 )
+
+
+def per_cpu(var: Object, cpu: IntegerLike) -> Object:
+    """
+    Return the per-CPU variable for a given CPU.
+
+    >>> print(repr(prog["runqueues"]))
+    Object(prog, 'struct rq', address=0x278c0)
+    >>> per_cpu(prog["runqueues"], 6).curr.comm
+    (char [16])"python3"
+
+    :param var: Per-CPU variable, i.e., ``type __percpu`` (not a pointer; use
+        :func:`per_cpu_ptr()` for that).
+    :param cpu: CPU number.
+    :return: ``type`` object.
+    """
+    return per_cpu_ptr(var.address_of_(), cpu)[0]
 
 
 def per_cpu_ptr(ptr: Object, cpu: IntegerLike) -> Object:
     """
     Return the per-CPU pointer for a given CPU.
 
-    :param ptr: ``type __percpu *``
+    >>> prog["init_net"].loopback_dev.pcpu_refcnt
+    (int *)0x2c980
+    >>> per_cpu_ptr(prog["init_net"].loopback_dev.pcpu_refcnt, 7)
+    *(int *)0xffff925e3ddec980 = 4
+
+    :param ptr: Per-CPU pointer, i.e., ``type __percpu *``. For global
+        variables, it's usually easier to use :func:`per_cpu()`.
     :param cpu: CPU number.
-    :return: ``type *``
+    :return: ``type *`` object.
     """
     offset = ptr.prog_["__per_cpu_offset"][cpu].value_()
     return Object(ptr.prog_, ptr.type_, value=ptr.value_() + offset)
---
+++ tests/helpers/linux/test_percpu.py
@@ -0,0 +1,12 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# SPDX-License-Identifier: GPL-3.0+
+
+from drgn.helpers.linux.cpumask import for_each_possible_cpu
+from drgn.helpers.linux.percpu import per_cpu
+from tests.helpers.linux import LinuxHelperTestCase
+
+
+class TestPerCpu(LinuxHelperTestCase):
+    def test_per_cpu(self):
+        for cpu in for_each_possible_cpu(self.prog):
+            self.assertEqual(per_cpu(self.prog["runqueues"], cpu).cpu, cpu)
