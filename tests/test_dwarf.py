osandov/drgn

commit 352c31e1acf29841eac091a9621d4a53cc1b6743

Add support for C++ template parameters
Add struct drgn_type_template_parameter to libdrgn, the corresponding
TypeTemplateParameter to the Python bindings, and support for parsing
them from DWARF.

With this, support for templates is almost, but not quite, complete. The
main wart is that DW_TAG_name of compound types includes the template
parameters, so the type tag includes it as well. We should remove that
from the tag and instead have the type formatting code add it only when
getting the full type name.

Based on a patch from Jay Kamat.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -1710,6 +1710,96 @@ err:
 	return err;
 }
 
+struct drgn_dwarf_die_thunk_arg {
+	Dwarf_Die die;
+	uint64_t bias;
+};
+
+static struct drgn_error *
+drgn_dwarf_template_type_parameter_thunk_fn(struct drgn_object *res, void *arg_)
+{
+	struct drgn_error *err;
+	struct drgn_dwarf_die_thunk_arg *arg = arg_;
+	if (res) {
+		struct drgn_qualified_type qualified_type;
+		err = drgn_type_from_dwarf_attr(drgn_object_program(res)->_dbinfo,
+						&arg->die, arg->bias, NULL,
+						true, true, NULL,
+						&qualified_type);
+		if (err)
+			return err;
+
+		err = drgn_object_set_absent(res, qualified_type, 0);
+		if (err)
+			return err;
+	}
+	free(arg);
+	return NULL;
+}
+
+static struct drgn_error *
+drgn_dwarf_template_value_parameter_thunk_fn(struct drgn_object *res,
+					     void *arg_)
+{
+	struct drgn_error *err;
+	struct drgn_dwarf_die_thunk_arg *arg = arg_;
+	if (res) {
+		err = drgn_object_from_dwarf_variable(drgn_object_program(res)->_dbinfo,
+						      &arg->die, arg->bias,
+						      res);
+		if (err)
+			return err;
+	}
+	free(arg);
+	return NULL;
+}
+
+static struct drgn_error *
+parse_template_parameter(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
+			 uint64_t bias, drgn_object_thunk_fn *thunk_fn,
+			 struct drgn_template_parameters_builder *builder)
+{
+	char tag_buf[DW_TAG_BUF_LEN];
+
+	Dwarf_Attribute attr_mem, *attr;
+	const char *name;
+	if ((attr = dwarf_attr_integrate(die, DW_AT_name, &attr_mem))) {
+		name = dwarf_formstring(attr);
+		if (!name) {
+			return drgn_error_format(DRGN_ERROR_OTHER,
+						 "%s has invalid DW_AT_name",
+						 dwarf_tag_str(die, tag_buf));
+		}
+	} else {
+		name = NULL;
+	}
+
+	bool defaulted;
+	if (dwarf_flag(die, DW_AT_default_value, &defaulted)) {
+		return drgn_error_format(DRGN_ERROR_OTHER,
+					 "%s has invalid DW_AT_default_value",
+					 dwarf_tag_str(die, tag_buf));
+	}
+
+	struct drgn_dwarf_die_thunk_arg *thunk_arg =
+		malloc(sizeof(*thunk_arg));
+	if (!thunk_arg)
+		return &drgn_enomem;
+	thunk_arg->die = *die;
+	thunk_arg->bias = bias;
+
+	union drgn_lazy_object argument;
+	drgn_lazy_object_init_thunk(&argument, dbinfo->prog, thunk_fn,
+				    thunk_arg);
+
+	struct drgn_error *err =
+		drgn_template_parameters_builder_add(builder, &argument, name,
+						     defaulted);
+	if (err)
+		drgn_lazy_object_deinit(&argument);
+	return err;
+}
+
 static struct drgn_error *
 drgn_compound_type_from_dwarf(struct drgn_debug_info *dbinfo,
 			      Dwarf_Die *die, uint64_t bias,
@@ -1747,34 +1837,55 @@ drgn_compound_type_from_dwarf(struct drgn_debug_info *dbinfo,
 			return err;
 	}
 
-	if (declaration) {
-		return drgn_incomplete_compound_type_create(dbinfo->prog, kind,
-							    tag, lang, ret);
-	}
-
-	int size = dwarf_bytesize(die);
-	if (size == -1) {
-		return drgn_error_format(DRGN_ERROR_OTHER,
-					 "%s has missing or invalid DW_AT_byte_size",
-					 dwarf_tag_str(die, tag_buf));
-	}
-
 	struct drgn_compound_type_builder builder;
 	drgn_compound_type_builder_init(&builder, dbinfo->prog, kind);
+
+	int size;
 	bool little_endian;
-	dwarf_die_is_little_endian(die, false, &little_endian);
+	if (declaration) {
+		size = 0;
+	} else {
+		size = dwarf_bytesize(die);
+		if (size == -1) {
+			return drgn_error_format(DRGN_ERROR_OTHER,
+						 "%s has missing or invalid DW_AT_byte_size",
+						 dwarf_tag_str(die, tag_buf));
+		}
+		dwarf_die_is_little_endian(die, false, &little_endian);
+	}
+
 	Dwarf_Die member = {}, child;
 	int r = dwarf_child(die, &child);
 	while (r == 0) {
-		if (dwarf_tag(&child) == DW_TAG_member) {
-			if (member.addr) {
-				err = parse_member(dbinfo, &member, bias,
-						   little_endian, false,
-						   &builder);
-				if (err)
-					goto err;
+		switch (dwarf_tag(&child)) {
+		case DW_TAG_member:
+			if (!declaration) {
+				if (member.addr) {
+					err = parse_member(dbinfo, &member,
+							   bias, little_endian,
+							   false, &builder);
+					if (err)
+						goto err;
+				}
+				member = child;
 			}
-			member = child;
+			break;
+		case DW_TAG_template_type_parameter:
+			err = parse_template_parameter(dbinfo, &child, bias,
+						       drgn_dwarf_template_type_parameter_thunk_fn,
+						       &builder.template_builder);
+			if (err)
+				goto err;
+			break;
+		case DW_TAG_template_value_parameter:
+			err = parse_template_parameter(dbinfo, &child, bias,
+						       drgn_dwarf_template_value_parameter_thunk_fn,
+						       &builder.template_builder);
+			if (err)
+				goto err;
+			break;
+		default:
+			break;
 		}
 		r = dwarf_siblingof(&child, &child);
 	}
@@ -1796,7 +1907,8 @@ drgn_compound_type_from_dwarf(struct drgn_debug_info *dbinfo,
 			goto err;
 	}
 
-	err = drgn_compound_type_create(&builder, tag, size, lang, ret);
+	err = drgn_compound_type_create(&builder, tag, size, !declaration, lang,
+					ret);
 	if (err)
 		goto err;
 	return NULL;
@@ -2144,16 +2256,11 @@ out:
 	return err;
 }
 
-struct drgn_dwarf_formal_parameter_thunk_arg {
-	Dwarf_Die die;
-	uint64_t bias;
-};
-
 static struct drgn_error *
 drgn_dwarf_formal_parameter_thunk_fn(struct drgn_object *res, void *arg_)
 {
 	struct drgn_error *err;
-	struct drgn_dwarf_formal_parameter_thunk_arg *arg = arg_;
+	struct drgn_dwarf_die_thunk_arg *arg = arg_;
 	if (res) {
 		struct drgn_qualified_type qualified_type;
 		err = drgn_type_from_dwarf_attr(drgn_object_program(res)->_dbinfo,
@@ -2188,7 +2295,7 @@ parse_formal_parameter(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
 		name = NULL;
 	}
 
-	struct drgn_dwarf_formal_parameter_thunk_arg *thunk_arg =
+	struct drgn_dwarf_die_thunk_arg *thunk_arg =
 		malloc(sizeof(*thunk_arg));
 	if (!thunk_arg)
 		return &drgn_enomem;
@@ -2246,6 +2353,20 @@ drgn_function_type_from_dwarf(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
 				goto err;
 			}
 			is_variadic = true;
+			break;
+		case DW_TAG_template_type_parameter:
+			err = parse_template_parameter(dbinfo, &child, bias,
+						       drgn_dwarf_template_type_parameter_thunk_fn,
+						       &builder.template_builder);
+			if (err)
+				goto err;
+			break;
+		case DW_TAG_template_value_parameter:
+			err = parse_template_parameter(dbinfo, &child, bias,
+						       drgn_dwarf_template_value_parameter_thunk_fn,
+						       &builder.template_builder);
+			if (err)
+				goto err;
 			break;
 		default:
 			break;

tests/test_dwarf.py
@@ -16,6 +16,7 @@ from drgn import (
     TypeEnumerator,
     TypeMember,
     TypeParameter,
+    TypeTemplateParameter,
 )
 from tests import DEFAULT_LANGUAGE, TestCase, identical
 from tests.dwarf import DW_AT, DW_ATE, DW_FORM, DW_LANG, DW_TAG
@@ -1391,6 +1392,52 @@ class TestTypes(TestCase):
                     TypeMember(prog.int_type("int", 4, True), "x", 0),
                     TypeMember(prog.int_type("int", 4, True), "y", 32),
                     TypeMember(prog.int_type("int", 4, True), "z", 64),
+                ),
+            ),
+        )
+
+    def test_class_template(self):
+        prog = dwarf_program(
+            test_type_dies(
+                (
+                    DwarfDie(
+                        DW_TAG.class_type,
+                        (
+                            DwarfAttrib(DW_AT.name, DW_FORM.string, "Array"),
+                            DwarfAttrib(DW_AT.declaration, DW_FORM.flag_present, True),
+                        ),
+                        (
+                            DwarfDie(
+                                DW_TAG.template_type_parameter,
+                                (
+                                    DwarfAttrib(DW_AT.type, DW_FORM.ref4, 1),
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "T"),
+                                ),
+                            ),
+                            DwarfDie(
+                                DW_TAG.template_value_parameter,
+                                (
+                                    DwarfAttrib(DW_AT.type, DW_FORM.ref4, 2),
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "N"),
+                                    DwarfAttrib(DW_AT.const_value, DW_FORM.data1, 2),
+                                ),
+                            ),
+                        ),
+                    ),
+                    int_die,
+                    unsigned_int_die,
+                )
+            )
+        )
+        self.assertIdentical(
+            prog.type("TEST").type,
+            prog.class_type(
+                "Array",
+                template_parameters=(
+                    TypeTemplateParameter(prog.int_type("int", 4, True), "T"),
+                    TypeTemplateParameter(
+                        Object(prog, prog.int_type("unsigned int", 4, False), 2), "N"
+                    ),
                 ),
             ),
         )
@@ -3204,6 +3251,49 @@ class TestTypes(TestCase):
                 prog.void_type(),
                 (TypeParameter(prog.array_type(prog.int_type("int", 4, True))),),
                 False,
+            ),
+        )
+
+    def test_function_template(self):
+        prog = dwarf_program(
+            test_type_dies(
+                (
+                    DwarfDie(
+                        DW_TAG.subroutine_type,
+                        (DwarfAttrib(DW_AT.type, DW_FORM.ref4, 1),),
+                        (
+                            DwarfDie(DW_TAG.unspecified_parameters, ()),
+                            DwarfDie(
+                                DW_TAG.template_type_parameter,
+                                (DwarfAttrib(DW_AT.name, DW_FORM.string, "T"),),
+                            ),
+                            DwarfDie(
+                                DW_TAG.template_value_parameter,
+                                (
+                                    DwarfAttrib(DW_AT.type, DW_FORM.ref4, 2),
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "N"),
+                                    DwarfAttrib(DW_AT.const_value, DW_FORM.data1, 2),
+                                ),
+                            ),
+                        ),
+                    ),
+                    int_die,
+                    unsigned_int_die,
+                )
+            )
+        )
+        self.assertIdentical(
+            prog.type("TEST").type,
+            prog.function_type(
+                prog.int_type("int", 4, True),
+                (),
+                is_variadic=True,
+                template_parameters=(
+                    TypeTemplateParameter(prog.void_type(), "T"),
+                    TypeTemplateParameter(
+                        Object(prog, prog.int_type("unsigned int", 4, False), 2), "N"
+                    ),
+                ),
             ),
         )
 

_drgn.pyi
@@ -531,6 +531,7 @@ class Program:
         size: IntegerLike,
         members: Sequence[TypeMember],
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -540,6 +541,7 @@ class Program:
         :param tag: :attr:`Type.tag`
         :param size: :attr:`Type.size`
         :param members: :attr:`Type.members`
+        :param template_parameters: :attr:`Type.template_parameters`
         :param qualifiers: :attr:`Type.qualifiers`
         :param lang: :attr:`Type.language`
         """
@@ -551,6 +553,7 @@ class Program:
         size: None = None,
         members: None = None,
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -563,6 +566,7 @@ class Program:
         size: IntegerLike,
         members: Sequence[TypeMember],
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -578,6 +582,7 @@ class Program:
         size: None = None,
         members: None = None,
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -590,6 +595,7 @@ class Program:
         size: IntegerLike,
         members: Sequence[TypeMember],
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -605,6 +611,7 @@ class Program:
         size: None = None,
         members: None = None,
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -700,6 +707,7 @@ class Program:
         parameters: Sequence[TypeParameter],
         is_variadic: bool = False,
         *,
+        template_parameters: Sequence[TypeTemplateParameter] = (),
         qualifiers: Qualifiers = Qualifiers.NONE,
         language: Optional[Language] = None,
     ) -> Type:
@@ -709,6 +717,7 @@ class Program:
         :param type: The return type (:attr:`Type.type`)
         :param parameters: :attr:`Type.parameters`
         :param is_variadic: :attr:`Type.is_variadic`
+        :param template_parameters: :attr:`Type.template_parameters`
         :param qualifiers: :attr:`Type.qualifiers`
         :param lang: :attr:`Type.language`
         """
@@ -1559,6 +1568,12 @@ class Type:
     Whether this type takes a variable number of arguments. This is only
     present for function types.
     """
+
+    template_parameters: Sequence[TypeTemplateParameter]
+    """
+    List of template parameters of this type. This is present for structure,
+    union, class, and function types.
+    """
     def type_name(self) -> str:
         """Get a descriptive full name of this type."""
         ...
@@ -1751,6 +1766,66 @@ class TypeParameter:
 
     name: Optional[str]
     """Parameter name, or ``None`` if the parameter is unnamed."""
+
+class TypeTemplateParameter:
+    """
+    A ``TypeTemplateParameter`` represents a template parameter of a structure,
+    union, class, or function type.
+    """
+
+    def __init__(
+        self,
+        argument: Union[Type, Object, Callable[[], Union[Type, Object]]],
+        name: Optional[str] = None,
+        is_default: bool = False,
+    ) -> None:
+        """
+        Create a ``TypeTemplateParameter``.
+
+        :param argument: One of:
+
+            1. :attr:`TypeTemplateParameter.argument` as a :class:`Type` if the
+               parameter is a type template parameter.
+            2. :attr:`TypeTemplateParameter.argument` as a non-absent
+               :class:`Object` if the parameter is a non-type template
+               parameter.
+            3. A callable that takes no arguments and returns one of the above.
+               It is called when ``argument`` is first accessed, and the result
+               is cached.
+        :param name: :attr:`TypeTemplateParameter.name`
+        :param is_default: :attr:`TypeTemplateParameter.is_default`
+        """
+        ...
+    argument: Union[Type, Object]
+    """
+    Template argument.
+
+    If this is a type template parameter, then this is a :class:`Type`. If this
+    is a non-type template parameter, then this is an :class:`Object`.
+    """
+
+    name: Optional[str]
+    """Template parameter name, or ``None`` if the parameter is unnamed."""
+
+    is_default: bool
+    """
+    Whether :attr:`argument` is the default for the template parameter.
+
+    .. note::
+
+        There are two ways to interpret this:
+
+            1. The argument was omitted entirely and thus defaulted to the
+               default argument.
+            2. The (specified or defaulted) argument is the same as the default
+               argument.
+
+        Compilers are inconsistent about which interpretation they use.
+
+        GCC added this information in version 4.9. Clang added it in version 11
+        (and only when emitting DWARF version 5). If the program was compiled
+        by an older version, this is always false.
+    """
 
 class TypeKind(enum.Enum):
     """A ``TypeKind`` represents a kind of type."""

drgn/__init__.py
@@ -72,6 +72,7 @@ from _drgn import (
     TypeKind,
     TypeMember,
     TypeParameter,
+    TypeTemplateParameter,
     _with_libkdumpfile as _with_libkdumpfile,
     cast,
     container_of,
@@ -113,6 +114,7 @@ __all__ = (
     "TypeKind",
     "TypeMember",
     "TypeParameter",
+    "TypeTemplateParameter",
     "cast",
     "container_of",
     "execscript",

libdrgn/drgn.h.in
@@ -1921,6 +1921,21 @@ struct drgn_type_parameter {
 	const char *name;
 };
 
+/** Template parameter of a structure, union, class, or function type. */
+struct drgn_type_template_parameter {
+	/**
+	 * Template parameter type or value.
+	 *
+	 * Access this with @ref drgn_template_parameter_type() and @ref
+	 * drgn_template_parameter_object().
+	 */
+	union drgn_lazy_object argument;
+	/** Template parameter name or @c NULL if it is unnamed. */
+	const char *name;
+	/** Whether the argument is the default. */
+	bool is_default;
+};
+
 /**
  * Language-agnostic type descriptor.
  *
@@ -1961,6 +1976,8 @@ struct drgn_type {
 			struct drgn_type_enumerator *enumerators;
 			struct drgn_type_parameter *parameters;
 		};
+		struct drgn_type_template_parameter *template_parameters;
+		size_t num_template_parameters;
 	} _private;
 };
 
@@ -2289,6 +2306,43 @@ static inline bool drgn_type_is_variadic(struct drgn_type *type)
 	return type->_private.is_variadic;
 }
 
+/** Get whether a kind of type can have template parameters. */
+static inline bool
+drgn_type_kind_has_template_parameters(enum drgn_type_kind kind)
+{
+	return (kind == DRGN_TYPE_STRUCT ||
+		kind == DRGN_TYPE_UNION ||
+		kind == DRGN_TYPE_CLASS ||
+		kind == DRGN_TYPE_FUNCTION);
+}
+
+/** Get whether a type can have template parameters. */
+static inline bool drgn_type_has_template_parameters(struct drgn_type *type)
+{
+	return drgn_type_kind_has_template_parameters(drgn_type_kind(type));
+}
+
+/**
+ * Get the template parameters of a type. @ref
+ * drgn_type_has_template_parameters() must be true for this type.
+ */
+static inline struct drgn_type_template_parameter *
+drgn_type_template_parameters(struct drgn_type *type)
+{
+	assert(drgn_type_has_template_parameters(type));
+	return type->_private.template_parameters;
+}
+
+/**
+ * Get the number of template parameters of a type. @ref
+ * drgn_type_has_template_parameters() must be true for this type.
+ */
+static inline size_t drgn_type_num_template_parameters(struct drgn_type *type)
+{
+	assert(drgn_type_has_template_parameters(type));
+	return type->_private.num_template_parameters;
+}
+
 /**
  * Get the object corresponding to a @ref drgn_type_member.
  *
@@ -2335,6 +2389,34 @@ drgn_parameter_default_argument(struct drgn_type_parameter *parameter,
  */
 struct drgn_error *drgn_parameter_type(struct drgn_type_parameter *parameter,
 				       struct drgn_qualified_type *ret)
+	__attribute__((__nonnull__(1, 2)));
+
+/**
+ * Get the type of a @ref drgn_type_template_parameter.
+ *
+ * If the template parameter is a non-type template parameter, this is the type
+ * of its value.
+ *
+ * @param[in] parameter Template parameter.
+ * @param[out] ret Returned type.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_template_parameter_type(struct drgn_type_template_parameter *parameter,
+			     struct drgn_qualified_type *ret)
+	__attribute__((__nonnull__(1, 2)));
+
+/**
+ * Get the value of a @ref drgn_type_template_parameter.
+ *
+ * @param[in] parameter Template parameter.
+ * @param[out] ret Returned object. If @p parameter is a type template
+ * parameter, this is returned as @c NULL.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_template_parameter_object(struct drgn_type_template_parameter *parameter,
+			       const struct drgn_object **ret)
 	__attribute__((__nonnull__(1, 2)));
 
 /**

libdrgn/program.h
@@ -101,10 +101,10 @@ struct drgn_program {
 	struct drgn_dedupe_type_set dedupe_types;
 	/**
 	 * List of created types that are not deduplicated: types with non-empty
-	 * lists of members, parameters, or enumerators.
+	 * lists of members, parameters, template parameters, or enumerators.
 	 *
-	 * Members and parameters contain lazily-evaluated objects, so they
-	 * cannot be easily deduplicated.
+	 * Members, parameters, and template parameters contain lazily-evaluated
+	 * objects, so they cannot be easily deduplicated.
 	 *
 	 * Enumerators could be deduplicated, but it's probably not worth the
 	 * effort to hash and compare them.

libdrgn/python/drgnpy.h
@@ -138,6 +138,12 @@ typedef struct {
 	PyObject *name;
 } TypeParameter;
 
+typedef struct {
+	LazyObject lazy_obj;
+	PyObject *name;
+	PyObject *is_default;
+} TypeTemplateParameter;
+
 extern PyObject *Architecture_class;
 extern PyObject *FindObjectFlags_class;
 extern PyObject *PlatformFlags_class;
@@ -160,6 +166,7 @@ extern PyTypeObject Symbol_type;
 extern PyTypeObject TypeEnumerator_type;
 extern PyTypeObject TypeMember_type;
 extern PyTypeObject TypeParameter_type;
+extern PyTypeObject TypeTemplateParameter_type;
 extern PyObject *MissingDebugInfoError;
 extern PyObject *ObjectAbsentError;
 extern PyObject *OutOfBoundsError;

libdrgn/python/module.c
@@ -305,6 +305,12 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	Py_INCREF(&TypeParameter_type);
 	PyModule_AddObject(m, "TypeParameter", (PyObject *)&TypeParameter_type);
 
+	if (PyType_Ready(&TypeTemplateParameter_type) < 0)
+		goto err;
+	Py_INCREF(&TypeTemplateParameter_type);
+	PyModule_AddObject(m, "TypeTemplateParameter",
+			   (PyObject *)&TypeTemplateParameter_type);
+
 	host_platform_obj = Platform_wrap(&drgn_host_platform);
 	if (!host_platform_obj)
 		goto err;

commit bbefc573d8ea7250328a8ba59073b34dee0fa39d

libdrgn: debug_info: make sure DW_TAG_template_value_parameter has value
Otherwise, an invalid DW_TAG_template_value_parameter can be confused
for a type parameter.

Fixes: 352c31e1acf2 ("Add support for C++ template parameters")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -1411,6 +1411,10 @@ drgn_object_from_dwarf_variable(struct drgn_debug_info *dbinfo,
 						       qualified_type, attr,
 						       ret);
 	} else {
+		if (dwarf_tag(die) == DW_TAG_template_value_parameter) {
+			return drgn_error_create(DRGN_ERROR_OTHER,
+						 "DW_AT_template_value_parameter is missing value");
+		}
 		return drgn_object_set_absent(ret, qualified_type, 0);
 	}
 }

tests/test_dwarf.py
@@ -1442,6 +1442,35 @@ class TestTypes(TestCase):
             ),
         )
 
+    def test_template_value_parameter_missing_value(self):
+        with self.assertRaisesRegex(
+            Exception, "DW_AT_template_value_parameter is missing value"
+        ):
+            dwarf_program(
+                test_type_dies(
+                    (
+                        DwarfDie(
+                            DW_TAG.class_type,
+                            (
+                                DwarfAttrib(
+                                    DW_AT.declaration, DW_FORM.flag_present, True
+                                ),
+                            ),
+                            (
+                                DwarfDie(
+                                    DW_TAG.template_value_parameter,
+                                    (
+                                        DwarfAttrib(DW_AT.type, DW_FORM.ref4, 1),
+                                        DwarfAttrib(DW_AT.name, DW_FORM.string, "N"),
+                                    ),
+                                ),
+                            ),
+                        ),
+                        unsigned_int_die,
+                    )
+                )
+            ).type("TEST").type.template_parameters[0].argument
+
     def test_lazy_cycle(self):
         prog = dwarf_program(
             test_type_dies(