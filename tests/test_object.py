osandov/drgn

commit e72ecd0e2ccc087e6b9f28852c9f9604585e0711

libdrgn: replace drgn_program_member_info() with drgn_type_find_member()
Now that types are associated with their program, we don't need to pass
the program separately to drgn_program_member_info() and can replace it
with a more natural drgn_type_find_member() API that takes only the type
and member name. While we're at it, get rid of drgn_member_info and
return the drgn_type_member and bit_offset directly. This also fixes a
bug that drgn_error_member_not_found() ignores the member name length.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -62,8 +62,6 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 				    bool little_endian)
 {
 	struct drgn_error *err;
-	PyObject *tmp, *items;
-	Py_ssize_t num_items, i;
 	int ret = -1;
 
 	if (!PyMapping_Check(value_obj)) {
@@ -72,7 +70,7 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 		return -1;
 	}
 
-	tmp = PyMapping_Items(value_obj);
+	PyObject *tmp = PyMapping_Items(value_obj);
 	if (!tmp)
 		return -1;
 
@@ -80,48 +78,52 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 	 * Since Python 3.7, PyMapping_Items() always returns a list. However,
 	 * before that, it could also return a tuple.
 	 */
-	items = PySequence_Fast(tmp, "items must be sequence");
+	PyObject *items = PySequence_Fast(tmp, "items must be sequence");
 	Py_DECREF(tmp);
 	if (!items)
 		return -1;
 
-	num_items = PySequence_Fast_GET_SIZE(items);
-	for (i = 0; i < num_items; i++) {
-		PyObject *item, *key;
-		const char *member_name;
-		struct drgn_member_info member;
-		struct drgn_object_type member_type;
-
-		item = PySequence_Fast_GET_ITEM(items, i);
+	Py_ssize_t num_items = PySequence_Fast_GET_SIZE(items);
+	for (Py_ssize_t i = 0; i < num_items; i++) {
+		PyObject *item = PySequence_Fast_GET_ITEM(items, i);
 		if (!PyTuple_Check(item) || PyTuple_GET_SIZE(item) != 2) {
 			PyErr_SetString(PyExc_TypeError, "invalid item");
 			goto out;
 		}
-		key = PyTuple_GET_ITEM(item, 0);
+		PyObject *key = PyTuple_GET_ITEM(item, 0);
 		if (!PyUnicode_Check(key)) {
 			PyErr_SetString(PyExc_TypeError,
 					"member key must be string");
 			goto out;
 		}
-		member_name = PyUnicode_AsUTF8(key);
+		const char *member_name = PyUnicode_AsUTF8(key);
 		if (!member_name)
 			goto out;
 
-		err = drgn_program_member_info(prog, type->underlying_type,
-					       member_name, &member);
+		struct drgn_type_member *member;
+		uint64_t member_bit_offset;
+		err = drgn_type_find_member(type->underlying_type, member_name,
+					    &member, &member_bit_offset);
+		if (err) {
+			set_drgn_error(err);
+			goto out;
+		}
+		struct drgn_qualified_type member_qualified_type;
+		err = drgn_member_type(member, &member_qualified_type);
 		if (err) {
 			set_drgn_error(err);
 			goto out;
 		}
 
-		member_type.type = member.qualified_type.type;
-		member_type.qualifiers = member.qualified_type.qualifiers;
-		member_type.underlying_type =
-			drgn_underlying_type(member_type.type);
-		member_type.bit_field_size = member.bit_field_size;
-
+		struct drgn_object_type member_type = {
+			.type = member_qualified_type.type,
+			.qualifiers = member_qualified_type.qualifiers,
+			.underlying_type =
+				drgn_underlying_type(member_qualified_type.type),
+			.bit_field_size = member->bit_field_size,
+		};
 		if (serialize_py_object(prog, buf, buf_bit_size,
-					bit_offset + member.bit_offset,
+					bit_offset + member->bit_offset,
 					PyTuple_GET_ITEM(item, 1),
 					&member_type, little_endian) == -1)
 			goto out;

tests/test_object.py
@@ -1701,6 +1701,15 @@ class TestCOperators(MockProgramTestCase):
             ),
         )
 
+        self.assertRaisesRegex(
+            LookupError,
+            "'struct line_segment' has no member 'c'",
+            container_of,
+            obj,
+            self.line_segment_type,
+            "c.x",
+        )
+
         polygon_type = self.prog.struct_type(
             "polygon", 0, (TypeMember(self.prog.array_type(self.point_type), "points"),)
         )

libdrgn/object.c
@@ -1451,19 +1451,24 @@ drgn_object_member(struct drgn_object *res, const struct drgn_object *obj,
 		   const char *member_name)
 {
 	struct drgn_error *err;
-	struct drgn_member_info member;
 
 	if (drgn_object_program(res) != drgn_object_program(obj)) {
 		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
 					 "objects are from different programs");
 	}
 
-	err = drgn_program_member_info(drgn_object_program(obj), obj->type,
-				       member_name, &member);
+	struct drgn_type_member *member;
+	uint64_t member_bit_offset;
+	err = drgn_type_find_member(obj->type, member_name, &member,
+				    &member_bit_offset);
 	if (err)
 		return err;
-	return drgn_object_slice(res, obj, member.qualified_type,
-				 member.bit_offset, member.bit_field_size);
+	struct drgn_qualified_type member_type;
+	err = drgn_member_type(member, &member_type);
+	if (err)
+		return err;
+	return drgn_object_slice(res, obj, member_type, member_bit_offset,
+				 member->bit_field_size);
 }
 
 struct drgn_error *drgn_object_member_dereference(struct drgn_object *res,
@@ -1471,34 +1476,30 @@ struct drgn_error *drgn_object_member_dereference(struct drgn_object *res,
 						  const char *member_name)
 {
 	struct drgn_error *err;
-	struct drgn_type *underlying_type;
-	struct drgn_member_value *member;
-	struct drgn_qualified_type qualified_type;
 
 	if (drgn_object_program(res) != drgn_object_program(obj)) {
 		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
 					 "objects are from different programs");
 	}
 
-	underlying_type = drgn_underlying_type(obj->type);
+	struct drgn_type *underlying_type = drgn_underlying_type(obj->type);
 	if (drgn_type_kind(underlying_type) != DRGN_TYPE_POINTER) {
 		return drgn_type_error("'%s' is not a pointer to a structure, union, or class",
 				       obj->type);
 	}
 
-	err = drgn_program_find_member(drgn_object_program(obj),
-				       drgn_type_type(underlying_type).type,
-				       member_name, strlen(member_name),
-				       &member);
+	struct drgn_type_member *member;
+	uint64_t member_bit_offset;
+	err = drgn_type_find_member(drgn_type_type(underlying_type).type,
+				    member_name, &member, &member_bit_offset);
 	if (err)
 		return err;
-
-	err = drgn_lazy_type_evaluate(member->type, &qualified_type);
+	struct drgn_qualified_type member_type;
+	err = drgn_member_type(member, &member_type);
 	if (err)
 		return err;
-
-	return drgn_object_dereference_offset(res, obj, qualified_type,
-					      member->bit_offset,
+	return drgn_object_dereference_offset(res, obj, member_type,
+					      member_bit_offset,
 					      member->bit_field_size);
 }
 

libdrgn/drgn.h.in
@@ -19,6 +19,7 @@
 #include <assert.h>
 #include <limits.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 
 /**
@@ -844,6 +845,38 @@ struct drgn_error *drgn_type_sizeof(struct drgn_type *type,
 				    uint64_t *ret);
 
 /**
+ * Like @ref drgn_type_find_member(), but takes the length of @p member_name.
+ */
+struct drgn_error *
+drgn_type_find_member_len(struct drgn_type *type, const char *member_name,
+			  size_t member_name_len,
+			  struct drgn_type_member **member_ret,
+			  uint64_t *bit_offset_ret);
+
+/**
+ * Find a member in a @ref drgn_type by name.
+ *
+ * If the type has any unnamed members, this also matches members of those
+ * unnamed members, recursively.
+ *
+ * @param[in] type Structure, union, or class type.
+ * @param[in] member_name Name of member.
+ * @param[out] member_ret Returned member.
+ * @param[out] bit_offset Offset in bits from the beginning of @p type to the
+ * beginning of the member. This can be different from @ref
+ * drgn_type_member::bit_offset if the returned member was found in an unnamed
+ * member of @p type.
+ */
+static inline struct drgn_error *
+drgn_type_find_member(struct drgn_type *type, const char *member_name,
+		      struct drgn_type_member **member_ret,
+		      uint64_t *bit_offset_ret)
+{
+	return drgn_type_find_member_len(type, member_name, strlen(member_name),
+					 member_ret, bit_offset_ret);
+}
+
+/**
  * Compare two @ref drgn_type%s for equality.
  *
  * Two types are equal if all of their fields are equal, recursively.
@@ -1404,48 +1437,6 @@ struct drgn_element_info {
 struct drgn_error *drgn_program_element_info(struct drgn_program *prog,
 					     struct drgn_type *type,
 					     struct drgn_element_info *ret);
-
-/**
- * Type, offset, and bit field size of an object member.
- *
- * @sa drgn_type_member
- */
-struct drgn_member_info {
-	/** Type of the member. */
-	struct drgn_qualified_type qualified_type;
-	/**
-	 * Offset in bits from the beginning of the type to the beginning of the
-	 * member.
-	 *
-	 * If the member was found inside an unnamed member of the enclosing
-	 * type, this is the offset from the beginning of the type passed to
-	 * @ref drgn_program_member_info().
-	 *
-	 * See @ref drgn_type_member::bit_offset.
-	 */
-	uint64_t bit_offset;
-	/** See @ref drgn_type_member::bit_field_size. */
-	uint64_t bit_field_size;
-};
-
-/**
- * Get the type, offset, and bit field size of a member of a @ref drgn_type by
- * name.
- *
- * If the type has any unnamed members, this also matches members of those
- * unnamed members, recursively.
- *
- * @param[in] prog Program.
- * @param[in] type Structure or union type. After this function is called, this
- * type must remain valid until the program is destroyed.
- * @param[in] member_name Name of member.
- * @param[out] ret Returned member information.
- * @return @c NULL on success, non-@c NULL on error.
- */
-struct drgn_error *drgn_program_member_info(struct drgn_program *prog,
-					    struct drgn_type *type,
-					    const char *member_name,
-					    struct drgn_member_info *ret);
 
 /** @} */
 

libdrgn/error.h
@@ -96,15 +96,6 @@ drgn_qualified_type_error(const char *format,
 struct drgn_error *drgn_error_incomplete_type(const char *format,
 					      struct drgn_type *type);
 
-/**
- * Create a @ref drgn_error for a type which does not have a given member.
- *
- * The error code will be @ref DRGN_ERROR_LOOKUP.
- */
-struct drgn_error *drgn_error_member_not_found(struct drgn_type *type,
-					       const char *member_name)
-	__attribute__((returns_nonnull));
-
 /** Create a @ref drgn_error for invalid types to a binary operator. */
 struct drgn_error *drgn_error_binary_op(const char *op_name,
 					struct drgn_object_type *type1,

libdrgn/language_c.c
@@ -2595,24 +2595,24 @@ struct drgn_error *c_bit_offset(struct drgn_program *prog,
 		case INT_MIN:
 		case C_TOKEN_DOT:
 			if (token.kind == C_TOKEN_IDENTIFIER) {
-				struct drgn_member_value *member;
-				struct drgn_qualified_type member_type;
-
-				err = drgn_program_find_member(prog, type,
-							       token.value,
-							       token.len,
-							       &member);
+				struct drgn_type_member *member;
+				uint64_t member_bit_offset;
+				err = drgn_type_find_member_len(type,
+								token.value,
+								token.len,
+								&member,
+								&member_bit_offset);
 				if (err)
 					goto out;
 				if (__builtin_add_overflow(bit_offset,
-							   member->bit_offset,
+							   member_bit_offset,
 							   &bit_offset)) {
 					err = drgn_error_create(DRGN_ERROR_OVERFLOW,
 								"offset is too large");
 					goto out;
 				}
-				err = drgn_lazy_type_evaluate(member->type,
-							      &member_type);
+				struct drgn_qualified_type member_type;
+				err = drgn_member_type(member, &member_type);
 				if (err)
 					goto out;
 				type = member_type.type;

libdrgn/linux_kernel.c
@@ -1321,12 +1321,14 @@ report_kernel_modules(struct drgn_debug_info_load_state *load,
 	size_t module_name_offset = 0;
 	if (need_module_definition) {
 		struct drgn_qualified_type module_type;
-		struct drgn_member_info name_member;
+		struct drgn_type_member *name_member;
+		uint64_t name_bit_offset;
 		err = drgn_program_find_type(prog, "struct module", NULL,
 					     &module_type);
 		if (!err) {
-			err = drgn_program_member_info(prog, module_type.type,
-						       "name", &name_member);
+			err = drgn_type_find_member(module_type.type, "name",
+						    &name_member,
+						    &name_bit_offset);
 		}
 		if (err) {
 			return drgn_debug_info_report_error(load,
@@ -1334,7 +1336,7 @@ report_kernel_modules(struct drgn_debug_info_load_state *load,
 							    "could not get kernel module names",
 							    err);
 		}
-		module_name_offset = name_member.bit_offset / 8;
+		module_name_offset = name_bit_offset / 8;
 	}
 
 	struct kernel_module_table kmod_table = HASH_TABLE_INIT;

libdrgn/linux_kernel_helpers.c
@@ -108,7 +108,6 @@ linux_helper_radix_tree_lookup(struct drgn_object *res,
 	uint64_t RADIX_TREE_INTERNAL_NODE;
 	uint64_t RADIX_TREE_MAP_MASK;
 	struct drgn_object node, tmp;
-	struct drgn_member_info member;
 	struct drgn_qualified_type node_type;
 
 	drgn_object_init(&node, drgn_object_program(res));
@@ -146,17 +145,22 @@ linux_helper_radix_tree_lookup(struct drgn_object *res,
 		goto out;
 	}
 
-	err = drgn_program_member_info(drgn_object_program(res),
-				       drgn_type_type(node_type.type).type,
-				       "slots", &member);
+	struct drgn_type_member *member;
+	uint64_t member_bit_offset;
+	err = drgn_type_find_member(drgn_type_type(node_type.type).type,
+				    "slots", &member, &member_bit_offset);
 	if (err)
 		goto out;
-	if (drgn_type_kind(member.qualified_type.type) != DRGN_TYPE_ARRAY) {
+	struct drgn_qualified_type member_type;
+	err = drgn_member_type(member, &member_type);
+	if (err)
+		goto out;
+	if (drgn_type_kind(member_type.type) != DRGN_TYPE_ARRAY) {
 		err = drgn_error_create(DRGN_ERROR_TYPE,
 					"struct radix_tree_node slots member is not an array");
 		goto out;
 	}
-	RADIX_TREE_MAP_MASK = drgn_type_length(member.qualified_type.type) - 1;
+	RADIX_TREE_MAP_MASK = drgn_type_length(member_type.type) - 1;
 
 	for (;;) {
 		uint64_t value;
@@ -252,43 +256,60 @@ find_pid_in_pid_hash(struct drgn_object *res, const struct drgn_object *ns,
 		     const struct drgn_object *pid_hash, uint64_t pid)
 {
 	struct drgn_error *err;
-	struct drgn_qualified_type pidp_type, upid_type;
-	struct drgn_member_info pid_chain_member, nr_member, ns_member;
-	struct drgn_object node, tmp;
-	uint64_t ns_addr;
-	union drgn_value ns_level, pidhash_shift;
-	uint64_t i;
 
+	struct drgn_qualified_type pidp_type;
 	err = drgn_program_find_type(drgn_object_program(res), "struct pid *",
 				     NULL, &pidp_type);
 	if (err)
 		return err;
+
+	struct drgn_qualified_type upid_type;
 	err = drgn_program_find_type(drgn_object_program(res), "struct upid",
 				     NULL, &upid_type);
 	if (err)
 		return err;
-	err = drgn_program_member_info(drgn_object_program(res), upid_type.type,
-				       "pid_chain", &pid_chain_member);
-	if (err)
-		return err;
-	err = drgn_program_member_info(drgn_object_program(res), upid_type.type,
-				       "nr", &nr_member);
-	if (err)
-		return err;
-	err = drgn_program_member_info(drgn_object_program(res), upid_type.type,
-				       "ns", &ns_member);
+
+	struct drgn_type_member *pid_chain_member;
+	uint64_t pid_chain_bit_offset;
+	err = drgn_type_find_member(upid_type.type, "pid_chain",
+				    &pid_chain_member, &pid_chain_bit_offset);
 	if (err)
 		return err;
 
+	struct drgn_type_member *nr_member;
+	uint64_t nr_bit_offset;
+	err = drgn_type_find_member(upid_type.type, "nr", &nr_member,
+				    &nr_bit_offset);
+	if (err)
+		return err;
+	struct drgn_qualified_type nr_type;
+	err = drgn_member_type(nr_member, &nr_type);
+	if (err)
+		return err;
+
+	struct drgn_type_member *ns_member;
+	uint64_t ns_bit_offset;
+	err = drgn_type_find_member(upid_type.type, "ns", &ns_member,
+				    &ns_bit_offset);
+	if (err)
+		return err;
+	struct drgn_qualified_type ns_type;
+	err = drgn_member_type(ns_member, &ns_type);
+	if (err)
+		return err;
+
+	struct drgn_object node, tmp;
 	drgn_object_init(&node, drgn_object_program(res));
 	drgn_object_init(&tmp, drgn_object_program(res));
 
 	err = drgn_object_read(&tmp, ns);
 	if (err)
 		goto out;
+	uint64_t ns_addr;
 	err = drgn_object_read_unsigned(&tmp, &ns_addr);
 	if (err)
 		goto out;
+	union drgn_value ns_level;
 	err = drgn_object_member_dereference(&tmp, &tmp, "level");
 	if (err)
 		goto out;
@@ -302,9 +323,11 @@ find_pid_in_pid_hash(struct drgn_object *res, const struct drgn_object *ns,
 				       DRGN_FIND_OBJECT_ANY, &tmp);
 	if (err)
 		goto out;
+	union drgn_value pidhash_shift;
 	err = drgn_object_read_integer(&tmp, &pidhash_shift);
 	if (err)
 		goto out;
+	uint64_t i;
 	if (pidhash_shift.uvalue >= 64)
 		i = 0;
 	else
@@ -331,12 +354,12 @@ find_pid_in_pid_hash(struct drgn_object *res, const struct drgn_object *ns,
 				goto out;
 			if (!addr)
 				break;
-			addr -= pid_chain_member.bit_offset / 8;
+			addr -= pid_chain_bit_offset / 8;
 
 			/* tmp = container_of(node, struct upid, pid_chain)->nr */
-			tmp_addr = addr + nr_member.bit_offset / 8;
-			err = drgn_object_set_reference(&tmp, nr_member.qualified_type,
-							tmp_addr, 0, 0,
+			tmp_addr = addr + nr_bit_offset / 8;
+			err = drgn_object_set_reference(&tmp, nr_type, tmp_addr,
+							0, 0,
 							DRGN_PROGRAM_ENDIAN);
 			if (err)
 				goto out;
@@ -347,9 +370,9 @@ find_pid_in_pid_hash(struct drgn_object *res, const struct drgn_object *ns,
 				goto next;
 
 			/* tmp = container_of(node, struct upid, pid_chain)->ns */
-			tmp_addr = addr + ns_member.bit_offset / 8;
-			err = drgn_object_set_reference(&tmp, ns_member.qualified_type,
-							tmp_addr, 0, 0,
+			tmp_addr = addr + ns_bit_offset / 8;
+			err = drgn_object_set_reference(&tmp, ns_type, tmp_addr,
+							0, 0,
 							DRGN_PROGRAM_ENDIAN);
 			if (err)
 				goto out;

commit 55e3a58e062ab503fae115256aba7b5fc5d724c4

libdrgn: python: use correct member offset when creating object from value
We need to use the offset of the member in the outermost object type,
not the offset in the immediate containing type in the case of nested
anonymous structs.

Fixes: e72ecd0e2ccc ("libdrgn: replace drgn_program_member_info() with drgn_type_find_member()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -121,7 +121,7 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 		if (err)
 			goto out;
 		if (serialize_py_object(prog, buf, buf_bit_size,
-					bit_offset + member->bit_offset,
+					bit_offset + member_bit_offset,
 					PyTuple_GET_ITEM(item, 1),
 					&member_type) == -1)
 			goto out;

tests/test_object.py
@@ -710,6 +710,24 @@ class TestValue(MockProgramTestCase):
             value={"z": 999},
         )
 
+    def test_compound_offset(self):
+        value = {"n": 23, "x": 100, "y": -5}
+        obj = Object(
+            self.prog,
+            self.prog.struct_type(
+                None,
+                12,
+                (
+                    TypeMember(self.prog.int_type("int", 4, True), "n"),
+                    TypeMember(self.point_type, None, 32),
+                ),
+            ),
+            value,
+        )
+        self.assertEqual(obj.value_(), value)
+        self.assertIdentical(obj.x, Object(self.prog, "int", value=100))
+        self.assertIdentical(obj.y, Object(self.prog, "int", value=-5))
+
     def test_pointer(self):
         obj = Object(self.prog, "int *", value=0xFFFF0000)
         self.assertFalse(obj.absent_)