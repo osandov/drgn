osandov/drgn

commit cf3a07bdfbbc87695831acfa2a31e946115377c4

libdrgn: python: replace Object.__format__ with Object.format_
We'd like to have more control over how objects are formatted. I
considered defining a custom string format specification syntax, but
that's not easily discoverable. Instead, let's get rid of the current
format specification support and replace it with a normal method.

libdrgn/python/object.c
@@ -943,42 +943,28 @@ static PyObject *DrgnObject_str(DrgnObject *self)
 	return ret;
 }
 
-static PyObject *DrgnObject_format(DrgnObject *self, PyObject *arg)
+static PyObject *DrgnObject_format(DrgnObject *self, PyObject *args,
+				   PyObject *kwds)
 {
+	static char *keywords[] = {"columns", NULL};
 	struct drgn_error *err;
-	const char *format_spec;
+	PyObject *columns_obj = Py_None;
 	size_t columns = SIZE_MAX;
-	char *end, *str;
+	char *str;
 	PyObject *ret;
 
-	format_spec = PyUnicode_AsUTF8(arg);
-	if (!format_spec)
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|$O", keywords,
+					 &columns_obj))
 		return NULL;
 
-	if (format_spec[0] == '.') {
-		unsigned long tmp;
-
-		if (format_spec[1] == '-') {
-			PyErr_SetString(PyExc_ValueError,
-					"Format specifier precision must be non-negative");
+	if (columns_obj != Py_None) {
+		columns_obj = PyNumber_Index(columns_obj);
+		if (!columns_obj)
 			return NULL;
-		}
-
-		errno = 0;
-		tmp = strtoul(format_spec + 1, &end, 10);
-		if (tmp == ULONG_MAX && errno == ERANGE) {
-			PyErr_SetString(PyExc_OverflowError,
-					"Format specifier precision is too large");
+		columns = PyLong_AsSize_t(columns_obj);
+		Py_DECREF(columns_obj);
+		if (columns == (size_t)-1 && PyErr_Occurred())
 			return NULL;
-		}
-		columns = tmp;
-	} else {
-		end = (char *)format_spec;
-	}
-	if (*end) {
-		PyErr_SetString(PyExc_ValueError,
-				"Format specifier can only include precision");
-		return NULL;
 	}
 
 	err = drgn_format_object(&self->obj, columns, &str);
@@ -1622,6 +1608,8 @@ static PyMethodDef DrgnObject_methods[] = {
 	 drgn_Object_address_of__DOC},
 	{"read_", (PyCFunction)DrgnObject_read, METH_NOARGS,
 	 drgn_Object_read__DOC},
+	{"format_", (PyCFunction)DrgnObject_format,
+	 METH_VARARGS | METH_KEYWORDS, drgn_Object_format__DOC},
 	{"__round__", (PyCFunction)DrgnObject_round,
 	 METH_VARARGS | METH_KEYWORDS},
 	{"__trunc__", (PyCFunction)DrgnObject_trunc, METH_NOARGS},
@@ -1629,8 +1617,6 @@ static PyMethodDef DrgnObject_methods[] = {
 	{"__ceil__", (PyCFunction)DrgnObject_ceil, METH_NOARGS},
 	{"__dir__", (PyCFunction)DrgnObject_dir, METH_NOARGS,
 "dir() implementation which includes structure, union, and class members."},
-	{"__format__", (PyCFunction)DrgnObject_format, METH_O,
-"Object formatter."},
 	{},
 };
 

drgn/internal/cli.py
@@ -22,8 +22,7 @@ def displayhook(value: Any) -> None:
         return
     setattr(builtins, '_', None)
     if isinstance(value, drgn.Object):
-        columns = shutil.get_terminal_size((0, 0)).columns
-        text = f'{value:.{columns}}'
+        text = value.format_(columns=shutil.get_terminal_size((0, 0)).columns)
     elif isinstance(value, (drgn.StackTrace, drgn.Type)):
         text = str(value)
     else:

tests/test_object.py
@@ -1419,33 +1419,33 @@ class TestCPretty(ObjectTestCase):
         obj = Object(prog, 'int [5]', address=0xffff0000)
 
         self.assertEqual(str(obj), "(int [5]){ 0, 1, 2, 3, 4, }")
-        self.assertEqual(f'{obj:.27}', str(obj))
+        self.assertEqual(obj.format_(columns=27), str(obj))
         for columns in range(22, 27):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [5]){
 	0, 1, 2, 3, 4,
 }""")
         for columns in range(19, 22):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [5]){
 	0, 1, 2, 3,
 	4,
 }""")
         for columns in range(16, 19):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [5]){
 	0, 1, 2,
 	3, 4,
 }""")
         for columns in range(13, 16):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [5]){
 	0, 1,
 	2, 3,
 	4,
 }""")
         for columns in range(13):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [5]){
 	0,
 	1,
@@ -1465,20 +1465,20 @@ class TestCPretty(ObjectTestCase):
 
         self.assertEqual(str(obj),
                          "(int [2][5]){ { 0, 1, 2, 3, 4, }, { 5, 6, 7, 8, 9, }, }")
-        self.assertEqual(f'{obj:.55}', str(obj))
+        self.assertEqual(obj.format_(columns=55), str(obj))
         for columns in range(47, 55):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [2][5]){
 	{ 0, 1, 2, 3, 4, }, { 5, 6, 7, 8, 9, },
 }""")
         for columns in range(27, 47):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [2][5]){
 	{ 0, 1, 2, 3, 4, },
 	{ 5, 6, 7, 8, 9, },
 }""")
         for columns in range(24, 27):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [2][5]){
 	{
 		0, 1, 2,
@@ -1490,7 +1490,7 @@ class TestCPretty(ObjectTestCase):
 	},
 }""")
         for columns in range(21, 24):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [2][5]){
 	{
 		0, 1,
@@ -1504,7 +1504,7 @@ class TestCPretty(ObjectTestCase):
 	},
 }""")
         for columns in range(21):
-            self.assertEqual(f'{obj:.{columns}}', """\
+            self.assertEqual(obj.format_(columns=columns), """\
 (int [2][5]){
 	{
 		0,
@@ -1539,16 +1539,16 @@ class TestCPretty(ObjectTestCase):
 (struct <anonymous>){
 	.arr = (int [5]){ 0, 1, 2, 3, 4, },
 }""")
-        self.assertEqual(f'{obj:.43}', str(obj))
+        self.assertEqual(obj.format_(columns=43), str(obj))
 
-        self.assertEqual(f'{obj:.42}', """\
+        self.assertEqual(obj.format_(columns=42), """\
 (struct <anonymous>){
 	.arr = (int [5]){
 		0, 1, 2, 3, 4,
 	},
 }""")
 
-        self.assertEqual(f'{obj:.18}', """\
+        self.assertEqual(obj.format_(columns=18), """\
 (struct <anonymous>){
 	.arr = (int [5]){
 		0,
@@ -1855,22 +1855,6 @@ class TestSpecialMethods(ObjectTestCase):
                          Object(self.prog, 'int', value=1))
         self.assertEqual(round(Object(self.prog, 'double', value=0.123), 2),
                          Object(self.prog, 'double', value=0.12))
-
-    def test_format(self):
-        obj = Object(self.prog, 'int', value=0)
-        obj.__format__('.10')
-        self.assertRaisesRegex(ValueError,
-                               'Format specifier can only include precision',
-                               obj.__format__, '10')
-        self.assertRaisesRegex(ValueError,
-                               'Format specifier can only include precision',
-                               obj.__format__, '.10 ')
-        self.assertRaisesRegex(ValueError,
-                               'Format specifier precision must be non-negative',
-                               obj.__format__, '.-10')
-        self.assertRaisesRegex(OverflowError,
-                               'Format specifier precision is too large',
-                               obj.__format__, f'.{2**128}')
 
     def test_iter(self):
         obj = Object(self.prog, 'int [4]', value=[0, 1, 2, 3])

commit ca1a2598fd716f8567f9db5b90f5ad1bc3433469

libdrgn: python: add missing function name to Object.format_() exceptions
The ":function name" is missing from the PyArg_ParseTupleAndKeywords()
call in DrgnObject_format(), so errors say, for example, "'foo' is an
invalid keyword argument for this function" instead of "for format_()".

Fixes: cf3a07bdfbbc ("libdrgn: python: replace Object.__format__ with Object.format_")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -906,7 +906,7 @@ static PyObject *DrgnObject_format(DrgnObject *self, PyObject *args,
 #define X(name, value) "O&"
 					 FLAGS
 #undef X
-					 "O", keywords,
+					 "O:format_", keywords,
 #define X(name, value) format_object_flag_converter, &name##_arg,
 					 FLAGS
 #undef X