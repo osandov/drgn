osandov/drgn

commit bc85c2da08b2215eea1975f0d2d1168eeab51d71

libdrgn: kdump: fix kdump_vmcoreinfo_raw() memory leak
Commit dd503c975ab3 ("Fix kdump_vmcoreinfo_raw()") in libkdumpfile
changed the buffer returned by kdump_vmcoreinfo_raw() to be dynamically
allocated. We need to free it on versions containing that change.

Closes #76.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/kdump.c
@@ -72,7 +72,6 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 	struct drgn_error *err;
 	kdump_ctx_t *ctx;
 	kdump_status ks;
-	const char *vmcoreinfo;
 	bool had_platform;
 
 	ctx = kdump_new();
@@ -96,6 +95,11 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 		goto err;
 	}
 
+#if KDUMPFILE_VERSION >= KDUMPFILE_MKVER(0, 4, 1)
+	char *vmcoreinfo;
+#else
+	const char *vmcoreinfo;
+#endif
 	ks = kdump_vmcoreinfo_raw(ctx, &vmcoreinfo);
 	if (ks != KDUMP_OK) {
 		err = drgn_error_format(DRGN_ERROR_OTHER,
@@ -108,6 +112,14 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 			       &prog->vmcoreinfo);
 	if (err)
 		goto err;
+
+	/*
+	 * As of libkdumpfile 0.4.1, the string returned by
+	 * kdump_vmcoreinfo_raw() needs to be freed.
+	 */
+#if KDUMPFILE_VERSION >= KDUMPFILE_MKVER(0, 4, 1)
+	free(vmcoreinfo);
+#endif
 
 	had_platform = prog->has_platform;
 	if (!had_platform) {

commit 794ffc22e8ad84d85b902cb60736530ad6c013fc

libdrgn: kdump: fix leak in leak fix
The previous fix still leaks the vmcoreinfo buffer if parse_vmcoreinfo()
fails.

Fixes: bc85c2da08b2 ("libdrgn: kdump: fix kdump_vmcoreinfo_raw() memory leak")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/kdump.c
@@ -110,9 +110,6 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 
 	err = parse_vmcoreinfo(vmcoreinfo, strlen(vmcoreinfo) + 1,
 			       &prog->vmcoreinfo);
-	if (err)
-		goto err;
-
 	/*
 	 * As of libkdumpfile 0.4.1, the string returned by
 	 * kdump_vmcoreinfo_raw() needs to be freed.
@@ -120,6 +117,8 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 #if KDUMPFILE_VERSION >= KDUMPFILE_MKVER(0, 4, 1)
 	free(vmcoreinfo);
 #endif
+	if (err)
+		goto err;
 
 	had_platform = prog->has_platform;
 	if (!had_platform) {