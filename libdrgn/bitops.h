osandov/drgn

commit 5541fad063aa2298a5d349ab98320c80fe25acf8

Fix some flake8 errors
Mainly unused imports, unused variables, unnecessary f-strings, and
regex literals missing the r prefix. I'm not adding it to the CI linter
because it's too noisy, though.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

scripts/iwyu.py
@@ -122,7 +122,7 @@ def gen_python_mapping_file(mapping_path):
         # For some reason, include-what-you-mean wants struct _typeobject, but
         # find-all-symbols only reports PyTypeObject. Add it manually.
         imp.write(
-            f'  {{"symbol": ["_typeobject", "private", "<Python.h>", "public"]}},  # From cpython/object.h\n'
+            '  {{"symbol": ["_typeobject", "private", "<Python.h>", "public"]}},  # From cpython/object.h\n'
         )
 
         imp.write("]\n")

docs/exts/drgndoc/docstrings.py
@@ -5,7 +5,7 @@
 import argparse
 import functools
 import sys
-from typing import Union, cast
+from typing import cast
 
 from drgndoc.format import Formatter
 from drgndoc.namespace import Namespace, ResolvedNode
@@ -74,7 +74,7 @@ if __name__ == "__main__":
 
     if args.header:
         output_file.write(
-            f"""\
+            """\
 /*
  * Generated by drgndoc.docstrings -H.
  *
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 """
         )
     else:
-        output_file.write(f"/* Generated by drgndoc.docstrings. */\n\n")
+        output_file.write("/* Generated by drgndoc.docstrings. */\n\n")
 
     def aux(resolved: ResolvedNode[Node], name: str) -> None:
         node = resolved.node

docs/exts/drgndoc/ext.py
@@ -42,7 +42,7 @@ file for the C extension itself (drgndoc.docstrings).
 
 import os.path
 import re
-from typing import Any, Dict, List, cast
+from typing import Any, Dict, cast
 
 import docutils.nodes
 import docutils.parsers.rst.directives

docs/exts/drgndoc/namespace.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import itertools
-from typing import Generic, Iterator, List, Mapping, Optional, Sequence, TypeVar, Union
+from typing import Generic, Iterator, List, Mapping, Sequence, TypeVar, Union
 
 from drgndoc.parse import (
     Class,
@@ -14,7 +14,6 @@ from drgndoc.parse import (
     Node,
     Variable,
 )
-from drgndoc.util import dot_join
 
 NodeT_co = TypeVar("NodeT_co", bound=Node, covariant=True)
 

docs/exts/drgndoc/visitor.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import ast
-import sys
 from typing import Any, Optional
 
 

drgn/helpers/__init__.py
@@ -23,7 +23,7 @@ useful for scripts or for implementing other helpers.
 
 import enum
 import typing
-from typing import Container, Iterable, List, Tuple
+from typing import Container, Iterable
 
 from drgn import Type
 

drgn/helpers/linux/idr.py
@@ -15,7 +15,7 @@ from typing import Iterator, Tuple
 
 from _drgn import _linux_helper_idr_find as idr_find
 from drgn import Object
-from drgn.helpers.linux.radixtree import radix_tree_for_each, radix_tree_lookup
+from drgn.helpers.linux.radixtree import radix_tree_for_each
 
 __all__ = (
     "idr_find",

drgn/helpers/linux/mm.py
@@ -11,7 +11,7 @@ implemented.
 """
 
 import operator
-from typing import Any, Iterator, List, Optional, Union, overload
+from typing import Iterator, List, Optional, Union, overload
 
 from _drgn import _linux_helper_read_vm
 from drgn import IntegerLike, Object, Program, cast

commit d1745755f116cf4b082074b0d720ca36ae45dff4

Fix some include-what-you-use warnings
Also:

* Rename struct string to struct nstring and move it to its own header.
* Fix scripts/iwyu.py, which was broken by commit 5541fad063aa ("Fix
  some flake8 errors").
* Add workarounds for a few outstanding include-what-you-use issues.

There is still a false positive for
include-what-you-use/include-what-you-use#970, but hopefully that is
fixed soon.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/Makefile.am
@@ -55,6 +55,7 @@ libdrgnimpl_la_SOURCES = $(ARCH_DEFS:.defs=.c) \
 			 memory_reader.c \
 			 memory_reader.h \
 			 minmax.h \
+			 nstring.h \
 			 object.c \
 			 object.h \
 			 object_index.c \

libdrgn/bitops.h
@@ -38,7 +38,7 @@
  *
  * @param[in] x Integer.
  */
-#define ctz(x) generic_bitop(x, PP_UNIQUE(_x), __builtin_ctz)
+#define ctz(x) generic_bitop(x, PP_UNIQUE(_x), builtin_bitop_impl, ctz)
 
 /**
  * Find Last Set bit.
@@ -60,7 +60,7 @@
  *
  * @param[in] x Integer.
  */
-#define fls(x) generic_bitop(x, PP_UNIQUE(_x), fls_)
+#define fls(x) generic_bitop(x, PP_UNIQUE(_x), fls_impl,)
 /** @cond */
 /*
  * The straightfoward implementation is bits - clz. However, as noted by the
@@ -71,21 +71,19 @@
  * This doesn't do the normal macro argument safety stuff because it should only
  * be used via generic_bitop() which already does it.
  */
-#define fls_impl(x, type, suffix)	\
-	(x ? 1 + ((8 * sizeof(type) - 1) ^ __builtin_clz##suffix(x)) : 0)
-#define fls_(x) fls_impl(x, unsigned int,)
-#define fls_l(x) fls_impl(x, unsigned long, l)
-#define fls_ll(x) fls_impl(x, unsigned long long, ll)
+#define fls_impl(arg, suffix, x)	\
+	(x ? 1 + ((8 * sizeof(0u##suffix) - 1) ^ __builtin_clz##suffix(x)) : 0)
 
-#define generic_bitop(x, unique_x, op) ({				\
+#define builtin_bitop_impl(arg, suffix, x) __builtin_##arg##suffix(x)
+#define generic_bitop(x, unique_x, impl, impl_arg) ({			\
 	__auto_type unique_x = (x);					\
 	_Static_assert(sizeof(unique_x) <= sizeof(unsigned long long),	\
 		       "type is too large");				\
 	(unsigned int)(sizeof(unique_x) <= sizeof(unsigned int) ?	\
-		       op(unique_x) :					\
+		       impl(impl_arg, , unique_x) :			\
 		       sizeof(unique_x) <= sizeof(unsigned long) ?	\
-		       op##l(unique_x) :				\
-		       op##ll(unique_x));				\
+		       impl(impl_arg, l, unique_x) :			\
+		       impl(impl_arg, ll, unique_x));			\
 })
 /** @endcond */
 

libdrgn/build-aux/gen_arch.awk
@@ -133,7 +133,7 @@ END {
 
 	print "/* Generated by libdrgn/build-aux/gen_arch.awk. */"
 	print ""
-	print "#include \"arch_register_layout.h\""
+	print "#include \"arch_register_layout.h\" // IWYU pragma: export"
 
 	print ""
 	print "static const struct drgn_register registers[] = {"

libdrgn/dwarf_index.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <assert.h>
+#include <byteswap.h>
 #include <dwarf.h>
 #include <elfutils/libdwfl.h>
 #include <inttypes.h>
@@ -15,7 +16,6 @@
 #include "drgn.h"
 #include "dwarf_index.h"
 #include "error.h"
-#include "path.h"
 #include "platform.h"
 #include "util.h"
 
@@ -188,7 +188,8 @@ struct drgn_dwarf_index_pending_die {
 
 DEFINE_VECTOR_FUNCTIONS(drgn_dwarf_index_pending_die_vector)
 
-DEFINE_HASH_MAP_FUNCTIONS(drgn_dwarf_index_die_map, string_hash_pair, string_eq)
+DEFINE_HASH_MAP_FUNCTIONS(drgn_dwarf_index_die_map, nstring_hash_pair,
+			  nstring_eq)
 DEFINE_VECTOR_FUNCTIONS(drgn_dwarf_index_die_vector)
 
 static inline uintptr_t
@@ -2717,7 +2718,7 @@ drgn_dwarf_index_iterator_init(struct drgn_dwarf_index_iterator *it,
 	if (err)
 		return err;
 	it->ns = ns;
-	struct string key = {
+	struct nstring key = {
 		.str = name,
 		.len = name_len,
 	};

libdrgn/dwarf_index.h
@@ -38,9 +38,6 @@ static inline int omp_get_max_threads(void)
 #include "hash_table.h"
 #include "vector.h"
 
-struct drgn_debug_info_module;
-struct drgn_error;
-
 /**
  * @ingroup Internals
  *
@@ -91,7 +88,7 @@ struct drgn_dwarf_index_die {
 	uintptr_t addr;
 };
 
-DEFINE_HASH_MAP_TYPE(drgn_dwarf_index_die_map, struct string, uint32_t)
+DEFINE_HASH_MAP_TYPE(drgn_dwarf_index_die_map, struct nstring, uint32_t)
 DEFINE_VECTOR_TYPE(drgn_dwarf_index_die_vector, struct drgn_dwarf_index_die)
 
 struct drgn_dwarf_index_shard {

libdrgn/hash_table.h
@@ -27,6 +27,7 @@
 #include "bitops.h"
 #include "cityhash.h"
 #include "minmax.h"
+#include "nstring.h" // IWYU pragma: export
 #include "util.h"
 
 /**
@@ -1750,8 +1751,8 @@ bool c_string_key_eq(const char * const *a, const char * const *b);
 #define c_string_key_eq(a, b) ((bool)(strcmp(*(a), *(b)) == 0))
 #endif
 
-/** Double hash a @ref string. */
-static inline struct hash_pair string_hash_pair(const struct string *key)
+/** Double hash a @ref nstring. */
+static inline struct hash_pair nstring_hash_pair(const struct nstring *key)
 {
 	return hash_pair_from_avalanching_hash(hash_bytes(key->str, key->len));
 }

libdrgn/kdump.c
@@ -3,6 +3,7 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "linux_kernel.h"

libdrgn/language_c.c
@@ -1686,7 +1686,8 @@ static const char *token_spelling[] = {
 	[C_TOKEN_ENUM] = "enum",
 };
 
-DEFINE_HASH_MAP(c_keyword_map, struct string, int, string_hash_pair, string_eq)
+DEFINE_HASH_MAP(c_keyword_map, struct nstring, int, nstring_hash_pair,
+		nstring_eq)
 
 static struct c_keyword_map c_keywords = HASH_TABLE_INIT;
 
@@ -1750,7 +1751,7 @@ struct drgn_error *drgn_lexer_c(struct drgn_lexer *lexer,
 		break;
 	default:
 		if (isalpha(*p) || *p == '_') {
-			struct string key;
+			struct nstring key;
 			struct c_keyword_map_iterator it;
 
 			do {

libdrgn/nstring.h
@@ -1,0 +1,37 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/**
+ * @file
+ *
+ * String with length.
+ */
+
+#ifndef DRGN_NSTRING_H
+#define DRGN_NSTRING_H
+
+#include <string.h>
+
+/** A string with a stored length. */
+struct nstring {
+	/**
+	 * The string, which is not necessarily null-terminated and may have
+	 * embedded null bytes.
+	 */
+	const char *str;
+	/** The length in bytes of the string. */
+	size_t len;
+};
+
+/** Compare two @ref nstring keys for equality. */
+static inline bool nstring_eq(const struct nstring *a, const struct nstring *b)
+{
+	/*
+	 * len == 0 is a special case because memcmp(NULL, NULL, 0) is
+	 * technically undefined.
+	 */
+	return (a->len == b->len &&
+		(a->len == 0 || memcmp(a->str, b->str, a->len) == 0));
+}
+
+#endif /* DRGN_NSTRING_H */

libdrgn/path.c
@@ -6,12 +6,13 @@
 #include <string.h>
 
 #include "path.h"
+#include "util.h"
 
 bool path_iterator_next(struct path_iterator *it, const char **component_ret,
 			size_t *component_len_ret)
 {
 	while (it->num_components) {
-		struct string *cur = &it->components[it->num_components - 1];
+		struct nstring *cur = &it->components[it->num_components - 1];
 		while (cur->len > 0) {
 			if (cur->str[cur->len - 1] == '/') {
 				if (cur->len == 1) {
@@ -95,7 +96,7 @@ bool die_matches_filename(Dwarf_Die *die, const char *filename)
 	if (!filename || !filename[0])
 		return true;
 
-	struct string die_components[2];
+	struct nstring die_components[2];
 	struct path_iterator die_path = {
 		.components = die_components,
 	};
@@ -123,7 +124,7 @@ bool die_matches_filename(Dwarf_Die *die, const char *filename)
 	die_path.num_components++;
 
 	struct path_iterator needle = {
-		.components = (struct string []){
+		.components = (struct nstring []){
 			{ filename, strlen(filename) }
 		},
 		.num_components = 1,
@@ -136,13 +137,13 @@ LIBDRGN_PUBLIC bool drgn_filename_matches(const char *haystack,
 					  const char *needle)
 {
 	struct path_iterator haystack_path = {
-		.components = (struct string []){
+		.components = (struct nstring []){
 			{ haystack, strlen(haystack) }
 		},
 		.num_components = 1,
 	};
 	struct path_iterator needle_path = {
-		.components = (struct string []){
+		.components = (struct nstring []){
 			{ needle, strlen(needle) }
 		},
 		.num_components = 1,

libdrgn/path.h
@@ -12,12 +12,11 @@
 #ifndef DRGN_PATH_H
 #define DRGN_PATH_H
 
+#include <elfutils/libdw.h>
 #include <stdbool.h>
 #include <stddef.h>
 
-#include <elfutils/libdw.h>
-
-#include "util.h"
+#include "nstring.h" // IWYU pragma: export
 
 /**
  * @ingroup Internals
@@ -61,10 +60,10 @@ struct path_iterator {
 	 * Array of input components.
 	 *
 	 * The input components are treated as if they were joined with a "/".
-	 * @ref string::str and @ref string::len should be initialized for each
-	 * component. The latter will be modified as the path is iterated.
+	 * @ref nstring::str and @ref nstring::len should be initialized for
+	 * each component. The latter will be modified as the path is iterated.
 	 */
-	struct string *components;
+	struct nstring *components;
 	/** Number of components in @ref path_iterator::components. */
 	size_t num_components;
 	/**

libdrgn/program.c
@@ -679,7 +679,7 @@ struct drgn_error *drgn_program_cache_prstatus_entry(struct drgn_program *prog,
 						     size_t size)
 {
 	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
-		struct string *entry =
+		struct nstring *entry =
 			drgn_prstatus_vector_append_entry(&prog->prstatus_vector);
 		if (!entry)
 			return &drgn_enomem;
@@ -784,7 +784,7 @@ out:
 
 struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
 						     uint32_t cpu,
-						     struct string *ret,
+						     struct nstring *ret,
 						     uint32_t *tid_ret)
 {
 	assert(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL);
@@ -804,7 +804,7 @@ struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
 
 struct drgn_error *drgn_program_find_prstatus_by_tid(struct drgn_program *prog,
 						     uint32_t tid,
-						     struct string *ret)
+						     struct nstring *ret)
 {
 	struct drgn_error *err;
 	struct drgn_prstatus_map_iterator it;

libdrgn/program.h
@@ -28,7 +28,6 @@
 #include "type.h"
 #include "vector.h"
 
-struct drgn_debug_info;
 struct drgn_symbol;
 
 /**
@@ -66,8 +65,8 @@ struct vmcoreinfo {
 };
 
 DEFINE_VECTOR_TYPE(drgn_typep_vector, struct drgn_type *)
-DEFINE_VECTOR_TYPE(drgn_prstatus_vector, struct string)
-DEFINE_HASH_MAP_TYPE(drgn_prstatus_map, uint32_t, struct string)
+DEFINE_VECTOR_TYPE(drgn_prstatus_vector, struct nstring)
+DEFINE_HASH_MAP_TYPE(drgn_prstatus_map, uint32_t, struct nstring)
 
 struct drgn_program {
 	/** @privatesection */
@@ -268,7 +267,7 @@ drgn_program_address_mask(const struct drgn_program *prog, uint64_t *ret)
  */
 struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
 						     uint32_t cpu,
-						     struct string *ret,
+						     struct nstring *ret,
 						     uint32_t *tid_ret);
 
 /**
@@ -281,7 +280,7 @@ struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
  */
 struct drgn_error *drgn_program_find_prstatus_by_tid(struct drgn_program *prog,
 						     uint32_t tid,
-						     struct string *ret);
+						     struct nstring *ret);
 
 /**
  * Cache the @c NT_PRSTATUS note provided by @p data in @p prog.

libdrgn/python/module.c
@@ -43,7 +43,7 @@ static PyObject *filename_matches(PyObject *self, PyObject *args,
 		return NULL;
 
 	struct path_iterator haystack = {
-		.components = (struct string [1]){},
+		.components = (struct nstring [1]){},
 		.num_components = 0,
 	};
 	if (haystack_arg.path) {
@@ -52,7 +52,7 @@ static PyObject *filename_matches(PyObject *self, PyObject *args,
 		haystack.num_components = 1;
 	}
 	struct path_iterator needle = {
-		.components = (struct string [1]){},
+		.components = (struct nstring [1]){},
 		.num_components = 0,
 	};
 	if (needle_arg.path) {

libdrgn/stack_trace.c
@@ -14,9 +14,9 @@
 #include "debug_info.h"
 #include "drgn.h"
 #include "error.h"
-#include "hash_table.h"
 #include "helpers.h"
 #include "minmax.h"
+#include "nstring.h"
 #include "platform.h"
 #include "program.h"
 #include "register_state.h"
@@ -525,7 +525,7 @@ drgn_get_initial_registers(struct drgn_program *prog, uint32_t tid,
 	struct drgn_error *err;
 	struct drgn_object obj;
 	struct drgn_object tmp;
-	struct string prstatus;
+	struct nstring prstatus;
 
 	drgn_object_init(&obj, prog);
 	drgn_object_init(&tmp, prog);

libdrgn/stack_trace.h
@@ -12,6 +12,7 @@
 #ifndef DRGN_STACK_TRACE_H
 #define DRGN_STACK_TRACE_H
 
+#include <elfutils/libdw.h>
 #include <stddef.h>
 
 /**

libdrgn/symbol.c
@@ -1,7 +1,7 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <stdbool.h>
+#include <elf.h>
 #include <stdlib.h>
 #include <string.h>
 

libdrgn/util.h
@@ -145,26 +145,4 @@ static inline uint64_t uint_max(int n)
 #define add_to_possibly_null_pointer(ptr, i)	\
 	((typeof(ptr))((uintptr_t)(ptr) + (i) * sizeof(*(ptr))))
 
-/** A string with a stored length. */
-struct string {
-	/**
-	 * The string, which is not necessarily null-terminated and may have
-	 * embedded null bytes.
-	 */
-	const char *str;
-	/** The length in bytes of the string. */
-	size_t len;
-};
-
-/** Compare two @ref string keys for equality. */
-static inline bool string_eq(const struct string *a, const struct string *b)
-{
-	/*
-	 * len == 0 is a special case because memcmp(NULL, NULL, 0) is
-	 * technically undefined.
-	 */
-	return (a->len == b->len &&
-		(a->len == 0 || memcmp(a->str, b->str, a->len) == 0));
-}
-
 #endif /* DRGN_UTIL_H */

scripts/iwyu.imp
@@ -1,0 +1,6 @@
+[
+  # include-what-you-use/include-what-you-use#967
+  { include: [ "<stdint.h>", public, "<inttypes.h>", public ] },
+  # include-what-you-use/include-what-you-use#968
+  { include: [ "<bits/struct_stat.h>", private, "<sys/stat.h>", public ] },
+]

scripts/iwyu.py
@@ -122,12 +122,50 @@ def gen_python_mapping_file(mapping_path):
         # For some reason, include-what-you-mean wants struct _typeobject, but
         # find-all-symbols only reports PyTypeObject. Add it manually.
         imp.write(
-            '  {{"symbol": ["_typeobject", "private", "<Python.h>", "public"]}},  # From cpython/object.h\n'
+            '  {"symbol": ["_typeobject", "private", "<Python.h>", "public"]},  # From cpython/object.h\n'
         )
 
         imp.write("]\n")
 
     os.rename(mapping_path + ".tmp", mapping_path)
+
+
+def iwyu_associated_header(path):
+    with open(path, "r") as f:
+        match = re.search(
+            r'^\s*#\s*include\s+"([^"]+)"\s+//\s+IWYU\s+pragma:\s+associated',
+            f.read(),
+            re.M,
+        )
+        if match:
+            return os.path.join(os.path.dirname(path), match.group(1))
+    if path.endswith(".c"):
+        return path[:-2] + ".h"
+    return None
+
+
+def ignore_line(path, state, line):
+    # include-what-you-use/include-what-you-use#969: iwyu recommends bogus
+    # forward declarations for the anonymous unions generated by
+    # BINARY_OP_SIGNED_2C.
+    if line.endswith("::;"):
+        return True
+
+    # include-what-you-use/include-what-you-use#971: drgn.h "exports" a forward
+    # declaration of several opaque types, but iwyu doesn't have such a notion.
+    if re.fullmatch(
+        r"struct drgn_(language|platform|program|register|stack_trace|symbol);", line
+    ):
+        paths = [path]
+        associated_header = iwyu_associated_header(path)
+        if associated_header is not None:
+            paths.append(associated_header)
+        for path in paths:
+            with open(path, "r") as f:
+                if re.search(r'^#include "(drgn.h|drgnpy.h)"', f.read(), re.M):
+                    return True
+
+    return False
 
 
 def main():
@@ -181,6 +219,8 @@ def main():
             + [
                 "-Xiwyu",
                 "--mapping_file=" + os.path.abspath(python_mapping_file),
+                "-Xiwyu",
+                "--mapping_file=" + os.path.abspath("scripts/iwyu.imp"),
                 "-w",  # We don't want warnings from Clang.
             ],
             cwd=command["directory"],
@@ -209,14 +249,15 @@ def main():
                     else:
                         header = None
                     lines.clear()
-                elif state != "include_list" and line:
+                elif (
+                    line
+                    and state != "include_list"
+                    and not ignore_line(path, state, line)
+                ):
                     if header is not None:
                         print("\n" + header)
                         header = None
                     print(line)
-    print(
-        "Please ignore suggestions to declare opaque types if the appropriate header has already been included."
-    )
 
 
 if __name__ == "__main__":