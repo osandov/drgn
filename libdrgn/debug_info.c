26ff3667cb67ffd48949583fab879859041d27ff
Omar Sandoval <osandov@osandov.com>
2022-01-27 11:37:46 -0800

    libdrgn: debug_info: use elf_rawdata() instead of elf_getdata()
    Most of the places where we call elf_getdata() (via read_elf_section())
    deal with SHT_PROGBITS sections. elf_getdata() always returns the
    literal contents of the file for SHT_PROGBITS sections (usually straight
    out of the mmap'd file).
    
    The exceptions are the SHT_RELA and SHT_SYMTAB sections in
    relocate_elf_section(). For these, if the byte order or alignment of the
    file do not match the host, elf_getdata() allocates a new buffer and
    converts the contents. relocate_elf_section() also handles unaligned
    buffers and swaps the byte order, so it mistakenly ends up with the
    original byte order of the file.
    
    Rather than removing that from relocate_elf_section(), let's avoid the
    extra allocation and use elf_rawdata(), which always returns the literal
    contents of the file.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- libdrgn/debug_info.c
+++ libdrgn/debug_info.c
@@ -2200,14 +2200,12 @@ struct drgn_error *find_elf_file(char **path_ret, int *fd_ret, Elf **elf_ret,
 struct drgn_error *read_elf_section(Elf_Scn *scn, Elf_Data **ret)
 {
 	GElf_Shdr shdr_mem, *shdr;
-	Elf_Data *data;
-
 	shdr = gelf_getshdr(scn, &shdr_mem);
 	if (!shdr)
 		return drgn_error_libelf();
 	if ((shdr->sh_flags & SHF_COMPRESSED) && elf_compress(scn, 0, 0) < 0)
 		return drgn_error_libelf();
-	data = elf_getdata(scn, NULL);
+	Elf_Data *data = elf_rawdata(scn, NULL);
 	if (!data)
 		return drgn_error_libelf();
 	*ret = data;
