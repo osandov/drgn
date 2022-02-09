osandov/drgn

commit 301cc767bab778cb72b591c48cd228dfb06afcf9

Implement a new API for representing threads
Previously, drgn had no way to represent a thread – retrieving a stack
trace (the only extant thread-specific operation) was achieved by
requiring the user to directly provide a tid.

This commit introduces the scaffolding for the design outlined in
issue #92, and implements the corresponding methods for userspace core
dumps, the live Linux kernel, and Linux kernel core dumps. Future work
will build on top of this commit to support live userspace processes.

Signed-off-by: Kevin Svetlitski <svetlitski@fb.com>

libdrgn/python/thread.c
@@ -1,0 +1,116 @@
+#include "drgnpy.h"
+
+static Program *Thread_prog(Thread *self)
+{
+	return container_of(self->thread.prog, Program, prog);
+}
+
+PyObject *Thread_wrap(struct drgn_thread *thread)
+{
+	Thread *ret = (Thread *)Thread_type.tp_alloc(&Thread_type, 0);
+	if (!ret)
+		return NULL;
+	struct drgn_error *err =
+		drgn_thread_dup_internal(thread, &ret->thread);
+	if (err) {
+		ret->thread.prog = NULL;
+		Py_DECREF(ret);
+		return set_drgn_error(err);
+	}
+	Py_INCREF(container_of(thread->prog, Program, prog));
+	return (PyObject *)ret;
+}
+
+static void Thread_dealloc(Thread *self)
+{
+	if (self->thread.prog) {
+		Program *prog = Thread_prog(self);
+		drgn_thread_deinit(&self->thread);
+		Py_DECREF(prog);
+	}
+	Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *Thread_get_tid(Thread *self)
+{
+	return PyLong_FromUnsignedLong(self->thread.tid);
+}
+
+static DrgnObject *Thread_get_object(Thread *self)
+{
+	const struct drgn_object *object;
+	struct drgn_error *err = drgn_thread_object(&self->thread, &object);
+	if (err)
+		return set_drgn_error(err);
+	DrgnObject *ret = DrgnObject_alloc(Thread_prog(self));
+	if (!ret)
+		return NULL;
+	err = drgn_object_copy(&ret->obj, object);
+	if (err) {
+		Py_DECREF(ret);
+		return set_drgn_error(err);
+	}
+	return ret;
+}
+
+static PyObject *Thread_stack_trace(Thread *self)
+{
+	struct drgn_error *err;
+	struct drgn_stack_trace *trace;
+	err = drgn_thread_stack_trace(&self->thread, &trace);
+	if (err)
+		return set_drgn_error(err);
+	PyObject *ret = StackTrace_wrap(trace);
+	if (!ret)
+		drgn_stack_trace_destroy(trace);
+	return ret;
+}
+
+static PyGetSetDef Thread_getset[] = {
+	{"tid", (getter)Thread_get_tid, NULL, drgn_Thread_tid_DOC},
+	{"object", (getter)Thread_get_object, NULL, drgn_Thread_object_DOC},
+	{},
+};
+
+static PyMethodDef Thread_methods[] = {
+	{"stack_trace", (PyCFunction)Thread_stack_trace, METH_NOARGS,
+	 drgn_Thread_stack_trace_DOC},
+	{},
+};
+
+PyTypeObject Thread_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.Thread",
+	.tp_basicsize = sizeof(Thread),
+	.tp_dealloc = (destructor)Thread_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = drgn_Thread_DOC,
+	.tp_getset = Thread_getset,
+	.tp_methods = Thread_methods,
+};
+
+static void ThreadIterator_dealloc(ThreadIterator *self)
+{
+	Py_XDECREF(self->prog);
+	Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *ThreadIterator_next(ThreadIterator *self)
+{
+	struct drgn_error *err;
+	struct drgn_thread *thread;
+	err = drgn_thread_iterator_next(self->iterator, &thread);
+	if (err)
+		return set_drgn_error(err);
+	return thread ? Thread_wrap(thread) : NULL;
+}
+
+PyTypeObject ThreadIterator_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn._ThreadIterator",
+	.tp_basicsize = sizeof(ThreadIterator),
+	.tp_dealloc = (destructor)ThreadIterator_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_iter = PyObject_SelfIter,
+	.tp_iternext = (iternextfunc)ThreadIterator_next,
+};

_drgn.pyi
@@ -253,6 +253,31 @@ class Program:
             the given file
         """
         ...
+    def threads(self) -> Iterator[Thread]:
+        """Get an iterator over all of the threads in the program."""
+        ...
+    def thread(self, tid: IntegerLike) -> Thread:
+        """
+        Get the thread with the given thread ID.
+
+        :param tid: Thread ID (as defined by `gettid(2)
+            <http://man7.org/linux/man-pages/man2/gettid.2.html>`_).
+        :raises LookupError: if no thread has the given thread ID
+        """
+        ...
+    def crashed_thread(self) -> Thread:
+        """
+        Get the thread that caused the program to crash.
+
+        For userspace programs, this is the thread that received the fatal
+        signal (e.g., ``SIGSEGV`` or ``SIGQUIT``).
+
+        For the kernel, this is the thread that panicked (either directly or as
+        a result of an oops, ``BUG_ON()``, etc.).
+
+        :raises ValueError: if the program is live (i.e., not a core dump)
+        """
+        ...
     def read(
         self, address: IntegerLike, size: IntegerLike, physical: bool = False
     ) -> bytes:
@@ -763,6 +788,28 @@ class FindObjectFlags(enum.Flag):
     ""
     ANY = ...
     ""
+
+class Thread:
+    """A thread in a program."""
+
+    tid: int
+    """
+    Thread ID (as defined by `gettid(2)
+    <http://man7.org/linux/man-pages/man2/gettid.2.html>`_).
+    """
+    object: Object
+    """
+    If the program is the Linux kernel, the ``struct task_struct *`` object for
+    this thread. Otherwise, not defined.
+    """
+    def stack_trace(self) -> StackTrace:
+        """
+        Get the stack trace for this thread.
+
+        This is equivalent to ``prog.stack_trace(thread.tid)``. See
+        :meth:`Program.stack_trace()`.
+        """
+        ...
 
 def filename_matches(haystack: Optional[str], needle: Optional[str]) -> bool:
     """

drgn/__init__.py
@@ -69,6 +69,7 @@ from _drgn import (
     Symbol,
     SymbolBinding,
     SymbolKind,
+    Thread,
     Type,
     TypeEnumerator,
     TypeKind,
@@ -114,6 +115,7 @@ __all__ = (
     "Symbol",
     "SymbolBinding",
     "SymbolKind",
+    "Thread",
     "Type",
     "TypeEnumerator",
     "TypeKind",

libdrgn/Makefile.am
@@ -132,6 +132,7 @@ _drgn_la_SOURCES = python/constants.c \
 		   python/stack_trace.c \
 		   python/symbol.c \
 		   python/test.c \
+		   python/thread.c \
 		   python/type.c \
 		   python/util.c
 

libdrgn/drgn.h.in
@@ -420,6 +420,7 @@ const char * const *drgn_register_names(const struct drgn_register *reg,
 /** @} */
 
 struct drgn_object;
+struct drgn_thread;
 
 /**
  * @defgroup Programs Programs
@@ -2750,6 +2751,114 @@ struct drgn_error *drgn_program_stack_trace(struct drgn_program *prog,
  * @sa drgn_program_stack_trace().
  */
 struct drgn_error *drgn_object_stack_trace(const struct drgn_object *obj,
+					   struct drgn_stack_trace **ret);
+
+/** @} */
+
+/**
+ * @defgroup Threads Threads
+ *
+ * Threads in a program.
+ *
+ * @{
+ */
+
+/**
+ * @struct drgn_thread
+ *
+ * A thread in a program.
+ */
+struct drgn_thread;
+
+/**
+ * Create a copy of a @ref drgn_thread.
+ *
+ * @param[in] thread Thread to copy.
+ * @param[out] ret Returned copy. On success, must be destroyed with @ref
+ * drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_dup(const struct drgn_thread *thread,
+				   struct drgn_thread **ret);
+
+/** Free a @ref drgn_thread. */
+void drgn_thread_destroy(struct drgn_thread *thread);
+
+/**
+ * @struct drgn_thread_iterator
+ *
+ * An iterator over all the threads in a program.
+ */
+struct drgn_thread_iterator;
+
+/**
+ * Get an iterator over all of the threads in the program.
+ *
+ * @param[out] ret Returned iterator, which can be advanced with @ref
+ * drgn_thread_iterator_next, and must be destroyed with @ref
+ * drgn_thread_iterator_destroy.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_thread_iterator_create(struct drgn_program *prog,
+			    struct drgn_thread_iterator **ret);
+
+/** Free a @ref drgn_thread_iterator. */
+void drgn_thread_iterator_destroy(struct drgn_thread_iterator *it);
+
+/**
+ * Get the next thread from a @ref drgn_thread_iterator.
+ *
+ * @param[out] ret Borrowed thread handle, or @c NULL if there are no more
+ * threads. This is valid until until the next call to @ref
+ * drgn_thread_iterator_next() with the same @p it, or until @p it is destroyed.
+ * It may be copied with @ref drgn_thread_dup() if it is needed for longer. This
+ * must NOT be destroyed with @ref drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_iterator_next(struct drgn_thread_iterator *it,
+					     struct drgn_thread **ret);
+
+/**
+ * Get the thread with the given thread ID.
+ *
+ * @param[in] tid Thread ID.
+ * @param[out] ret New thread handle. On success, must be destroyed with @ref
+ * drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_find_thread(struct drgn_program *prog,
+					    uint32_t tid,
+					    struct drgn_thread **ret);
+
+/**
+ * Get the thread that caused the program to crash.
+ *
+ * @param[out] ret Borrowed thread handle. This is valid for the lifetime of @p
+ * prog. This must NOT be destroyed with @ref drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_crashed_thread(struct drgn_program *prog,
+					       struct drgn_thread **ret);
+
+/**
+ * Get the object for the given thread. This is currently only defined for the
+ * Linux kernel.
+ *
+ * @param[out] ret Returned object. This must not be modified and is valid for
+ * the lifetime of @p thread. It can be copied with @ref drgn_object_copy() if
+ * it is needed for longer.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_object(struct drgn_thread *thread,
+				      const struct drgn_object **ret);
+
+/**
+ * Get a stack trace for the thread represented by @p thread.
+ *
+ * @sa drgn_program_stack_trace().
+ */
+struct drgn_error *drgn_thread_stack_trace(struct drgn_thread *thread,
 					   struct drgn_stack_trace **ret);
 
 /** @} */

libdrgn/helpers.h
@@ -69,12 +69,11 @@ void linux_helper_task_iterator_deinit(struct linux_helper_task_iterator *it);
 /**
  * Get the next task from a @ref linux_helper_task_iterator.
  *
- * @param[out] ret Returned `struct task_struct *` object. This points to an
- * object stored in @p it. The caller may modify this, but it will be
- * overwritten the next time this function is called on the same @p it.
+ * @param[out] ret Returned `struct task_struct *` object. This is valid until
+ * the next call to this function on the same @p it or until @p it is destroyed.
  */
 struct drgn_error *
 linux_helper_task_iterator_next(struct linux_helper_task_iterator *it,
-				struct drgn_object **ret);
+				const struct drgn_object **ret);
 
 #endif /* DRGN_HELPERS_H */

libdrgn/kdump.c
@@ -159,7 +159,7 @@ err:
 	return err;
 }
 
-struct drgn_error *drgn_program_cache_prstatus_kdump(struct drgn_program *prog)
+struct drgn_error *drgn_program_cache_kdump_notes(struct drgn_program *prog)
 {
 	struct drgn_error *err;
 	kdump_num_t ncpus, i;
@@ -208,9 +208,9 @@ struct drgn_error *drgn_program_cache_prstatus_kdump(struct drgn_program *prog)
 
 		prstatus_data = kdump_blob_pin(prstatus_attr.val.blob);
 		prstatus_size = kdump_blob_size(prstatus_attr.val.blob);
-		err = drgn_program_cache_prstatus_entry(prog,
-							prstatus_data,
-							prstatus_size);
+		uint32_t _;
+		err = drgn_program_cache_prstatus_entry(prog, prstatus_data,
+							prstatus_size, &_);
 		if (err)
 			return err;
 	}

libdrgn/linux_kernel.h
@@ -33,7 +33,7 @@ linux_kernel_report_debug_info(struct drgn_debug_info_load_state *load);
 #define KDUMP_SIG_LEN (sizeof(KDUMP_SIGNATURE) - 1)
 
 #ifdef WITH_LIBKDUMPFILE
-struct drgn_error *drgn_program_cache_prstatus_kdump(struct drgn_program *prog);
+struct drgn_error *drgn_program_cache_kdump_notes(struct drgn_program *prog);
 struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog);
 #else
 static inline struct drgn_error *

commit 50ba745c24fd3f7cf4ca260788b451ca789087af

libdrgn: python: fix drgn_thread_iterator leak
LeakSanitizer reported a leak of a drgn_thread_iterator when running the
unit tests. The root cause is that ThreadIterator_dealloc() isn't
freeing the underlying drgn_thread_iterator().

Fixes: 301cc767bab7 ("Implement a new API for representing threads")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/thread.c
@@ -91,6 +91,7 @@ PyTypeObject Thread_type = {
 
 static void ThreadIterator_dealloc(ThreadIterator *self)
 {
+	drgn_thread_iterator_destroy(self->iterator);
 	Py_XDECREF(self->prog);
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }