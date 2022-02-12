osandov/drgn

commit 301cc767bab778cb72b591c48cd228dfb06afcf9

Implement a new API for representing threads
Previously, drgn had no way to represent a thread – retrieving a stack
trace (the only extant thread-specific operation) was achieved by
requiring the user to directly provide a tid.

This commit introduces the scaffolding for the design outlined in
issue #92, and implements the corresponding methods for userspace core
dumps, the live Linux kernel, and Linux kernel core dumps. Future work
will build on top of this commit to support live userspace processes.

Signed-off-by: Kevin Svetlitski <svetlitski@fb.com>

libdrgn/drgn.h.in
@@ -420,6 +420,7 @@ const char * const *drgn_register_names(const struct drgn_register *reg,
 /** @} */
 
 struct drgn_object;
+struct drgn_thread;
 
 /**
  * @defgroup Programs Programs
@@ -2750,6 +2751,114 @@ struct drgn_error *drgn_program_stack_trace(struct drgn_program *prog,
  * @sa drgn_program_stack_trace().
  */
 struct drgn_error *drgn_object_stack_trace(const struct drgn_object *obj,
+					   struct drgn_stack_trace **ret);
+
+/** @} */
+
+/**
+ * @defgroup Threads Threads
+ *
+ * Threads in a program.
+ *
+ * @{
+ */
+
+/**
+ * @struct drgn_thread
+ *
+ * A thread in a program.
+ */
+struct drgn_thread;
+
+/**
+ * Create a copy of a @ref drgn_thread.
+ *
+ * @param[in] thread Thread to copy.
+ * @param[out] ret Returned copy. On success, must be destroyed with @ref
+ * drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_dup(const struct drgn_thread *thread,
+				   struct drgn_thread **ret);
+
+/** Free a @ref drgn_thread. */
+void drgn_thread_destroy(struct drgn_thread *thread);
+
+/**
+ * @struct drgn_thread_iterator
+ *
+ * An iterator over all the threads in a program.
+ */
+struct drgn_thread_iterator;
+
+/**
+ * Get an iterator over all of the threads in the program.
+ *
+ * @param[out] ret Returned iterator, which can be advanced with @ref
+ * drgn_thread_iterator_next, and must be destroyed with @ref
+ * drgn_thread_iterator_destroy.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_thread_iterator_create(struct drgn_program *prog,
+			    struct drgn_thread_iterator **ret);
+
+/** Free a @ref drgn_thread_iterator. */
+void drgn_thread_iterator_destroy(struct drgn_thread_iterator *it);
+
+/**
+ * Get the next thread from a @ref drgn_thread_iterator.
+ *
+ * @param[out] ret Borrowed thread handle, or @c NULL if there are no more
+ * threads. This is valid until until the next call to @ref
+ * drgn_thread_iterator_next() with the same @p it, or until @p it is destroyed.
+ * It may be copied with @ref drgn_thread_dup() if it is needed for longer. This
+ * must NOT be destroyed with @ref drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_iterator_next(struct drgn_thread_iterator *it,
+					     struct drgn_thread **ret);
+
+/**
+ * Get the thread with the given thread ID.
+ *
+ * @param[in] tid Thread ID.
+ * @param[out] ret New thread handle. On success, must be destroyed with @ref
+ * drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_find_thread(struct drgn_program *prog,
+					    uint32_t tid,
+					    struct drgn_thread **ret);
+
+/**
+ * Get the thread that caused the program to crash.
+ *
+ * @param[out] ret Borrowed thread handle. This is valid for the lifetime of @p
+ * prog. This must NOT be destroyed with @ref drgn_thread_destroy().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_crashed_thread(struct drgn_program *prog,
+					       struct drgn_thread **ret);
+
+/**
+ * Get the object for the given thread. This is currently only defined for the
+ * Linux kernel.
+ *
+ * @param[out] ret Returned object. This must not be modified and is valid for
+ * the lifetime of @p thread. It can be copied with @ref drgn_object_copy() if
+ * it is needed for longer.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_thread_object(struct drgn_thread *thread,
+				      const struct drgn_object **ret);
+
+/**
+ * Get a stack trace for the thread represented by @p thread.
+ *
+ * @sa drgn_program_stack_trace().
+ */
+struct drgn_error *drgn_thread_stack_trace(struct drgn_thread *thread,
 					   struct drgn_stack_trace **ret);
 
 /** @} */

libdrgn/program.c
@@ -17,6 +17,7 @@
 
 #include "debug_info.h"
 #include "error.h"
+#include "helpers.h"
 #include "language.h"
 #include "linux_kernel.h"
 #include "memory_reader.h"
@@ -27,8 +28,25 @@
 #include "vector.h"
 #include "util.h"
 
+static inline uint32_t drgn_thread_to_key(const struct drgn_thread *entry)
+{
+	return entry->tid;
+}
+
 DEFINE_VECTOR_FUNCTIONS(drgn_prstatus_vector)
-DEFINE_HASH_MAP_FUNCTIONS(drgn_prstatus_map, int_key_hash_pair, scalar_key_eq)
+DEFINE_HASH_TABLE_FUNCTIONS(drgn_thread_set, drgn_thread_to_key,
+			    int_key_hash_pair, scalar_key_eq)
+
+struct drgn_thread_iterator {
+	struct drgn_program *prog;
+	union {
+		struct drgn_thread_set_iterator iterator;
+		struct {
+			struct linux_helper_task_iterator task_iter;
+			struct drgn_thread entry;
+		};
+	};
+};
 
 LIBDRGN_PUBLIC enum drgn_program_flags
 drgn_program_flags(struct drgn_program *prog)
@@ -75,12 +93,13 @@ void drgn_program_init(struct drgn_program *prog,
 
 void drgn_program_deinit(struct drgn_program *prog)
 {
-	if (prog->prstatus_cached) {
+	if (prog->core_dump_notes_cached) {
 		if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
 			drgn_prstatus_vector_deinit(&prog->prstatus_vector);
 		else
-			drgn_prstatus_map_deinit(&prog->prstatus_map);
+			drgn_thread_set_deinit(&prog->thread_set);
 	}
+	drgn_thread_destroy(prog->crashed_thread);
 	free(prog->pgtable_it);
 
 	drgn_object_deinit(&prog->vmemmap);
@@ -655,9 +674,62 @@ static struct drgn_error *get_prstatus_pid(struct drgn_program *prog, const char
 	return NULL;
 }
 
+struct drgn_error *drgn_thread_dup_internal(const struct drgn_thread *thread,
+					    struct drgn_thread *ret)
+{
+	struct drgn_error *err = NULL;
+	ret->prog = thread->prog;
+	ret->tid = thread->tid;
+	/* Don't need a deep copy here since the PRSTATUS notes are cached. */
+	ret->prstatus = thread->prstatus;
+	if (thread->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		drgn_object_init(&ret->object, thread->prog);
+		err = drgn_object_copy(&ret->object, &thread->object);
+		if (err)
+			drgn_object_deinit(&ret->object);
+	}
+	return err;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_thread_dup(const struct drgn_thread *thread, struct drgn_thread **ret)
+{
+	if (!(thread->prog->flags &
+	      (DRGN_PROGRAM_IS_LINUX_KERNEL | DRGN_PROGRAM_IS_LIVE))) {
+		/*
+		 * For userspace core dumps, all threads are cached and
+		 * immutable, so we can return the same handle.
+		 */
+		*ret = (struct drgn_thread *)thread;
+		return NULL;
+	}
+
+	*ret = malloc(sizeof(**ret));
+	if (!*ret)
+		return &drgn_enomem;
+	struct drgn_error *err = drgn_thread_dup_internal(thread, *ret);
+	if (err)
+		free(*ret);
+	return err;
+}
+
+void drgn_thread_deinit(struct drgn_thread *thread) {
+	if (thread->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
+		drgn_object_deinit(&thread->object);
+}
+
+LIBDRGN_PUBLIC void drgn_thread_destroy(struct drgn_thread *thread)
+{
+	if (thread) {
+		drgn_thread_deinit(thread);
+		if (thread->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
+			free(thread);
+	}
+}
+
 struct drgn_error *drgn_program_cache_prstatus_entry(struct drgn_program *prog,
 						     const char *data,
-						     size_t size)
+						     size_t size, uint32_t *ret)
 {
 	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
 		struct nstring *entry =
@@ -667,36 +739,45 @@ struct drgn_error *drgn_program_cache_prstatus_entry(struct drgn_program *prog,
 		entry->str = data;
 		entry->len = size;
 	} else {
-		struct drgn_prstatus_map_entry entry = {
-			.value = { data, size },
-		};
+		struct drgn_thread thread = {
+			.prog = prog,
+			.prstatus = { data, size },
+	       };
 		struct drgn_error *err = get_prstatus_pid(prog, data, size,
-							  &entry.key);
+							  &thread.tid);
 		if (err)
 			return err;
-		if (drgn_prstatus_map_insert(&prog->prstatus_map, &entry,
-					     NULL) == -1)
+		*ret = thread.tid;
+		if (drgn_thread_set_insert(&prog->thread_set, &thread,
+					   NULL) == -1)
 			return &drgn_enomem;
 	}
 	return NULL;
 }
 
-static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
+static struct drgn_error *
+drgn_program_cache_core_dump_notes(struct drgn_program *prog)
 {
 	struct drgn_error *err;
 	size_t phnum, i;
+	bool found_prstatus = false;
+	uint32_t first_prstatus_tid;
 
-	if (prog->prstatus_cached)
+	if (prog->core_dump_notes_cached)
 		return NULL;
+
+	assert(!(prog->flags & DRGN_PROGRAM_IS_LIVE));
 
 	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
 		drgn_prstatus_vector_init(&prog->prstatus_vector);
 	else
-		drgn_prstatus_map_init(&prog->prstatus_map);
+		drgn_thread_set_init(&prog->thread_set);
 
 #ifdef WITH_LIBKDUMPFILE
 	if (prog->kdump_ctx) {
-		err = drgn_program_cache_prstatus_kdump(prog);
+		err = drgn_program_cache_kdump_notes(prog);
+		if (err)
+			goto err;
 		goto out;
 	}
 #endif
@@ -706,7 +787,7 @@ static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
 	}
 	if (elf_getphdrnum(prog->core, &phnum) != 0) {
 		err = drgn_error_libelf();
-		goto out;
+		goto err;
 	}
 	for (i = 0; i < phnum; i++) {
 		GElf_Phdr phdr_mem, *phdr;
@@ -718,7 +799,7 @@ static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
 		phdr = gelf_getphdr(prog->core, i, &phdr_mem);
 		if (!phdr) {
 			err = drgn_error_libelf();
-			goto out;
+			goto err;
 		}
 		if (phdr->p_type != PT_NOTE)
 			continue;
@@ -728,7 +809,7 @@ static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
 					    note_header_type(phdr->p_align));
 		if (!data) {
 			err = drgn_error_libelf();
-			goto out;
+			goto err;
 		}
 
 		offset = 0;
@@ -742,25 +823,270 @@ static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
 			    nhdr.n_type != NT_PRSTATUS)
 				continue;
 
+			uint32_t tid;
 			err = drgn_program_cache_prstatus_entry(prog,
 								(char *)data->d_buf + desc_offset,
-								nhdr.n_descsz);
+								nhdr.n_descsz,
+								&tid);
 			if (err)
-				goto out;
+				goto err;
+			/*
+			 * The first PRSTATUS note is the crashed thread. See
+			 * fs/binfmt_elf.c:fill_note_info in the Linux kernel
+			 * and bfd/elf.c:elfcore_grok_prstatus in BFD.
+			 */
+			if (!found_prstatus) {
+				found_prstatus = true;
+				first_prstatus_tid = tid;
+			}
 		}
 	}
 
-	err = NULL;
 out:
-	if (err) {
-		if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
-			drgn_prstatus_vector_deinit(&prog->prstatus_vector);
-		else
-			drgn_prstatus_map_deinit(&prog->prstatus_map);
-	} else {
-		prog->prstatus_cached = true;
+	prog->core_dump_notes_cached = true;
+	if (found_prstatus &&
+	    !(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)) {
+		/*
+		 * Now that thread_set won't be modified, look up the crashed
+		 * thread entry.
+		 */
+		struct drgn_thread_set_iterator it =
+			drgn_thread_set_search(&prog->thread_set,
+					       &first_prstatus_tid);
+		assert(it.entry);
+		prog->crashed_thread = it.entry;
 	}
+	return NULL;
+
+err:
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
+		drgn_prstatus_vector_deinit(&prog->prstatus_vector);
+	else
+		drgn_thread_set_deinit(&prog->thread_set);
 	return err;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_thread_iterator_create(struct drgn_program *prog,
+			    struct drgn_thread_iterator **ret)
+{
+	struct drgn_error *err;
+
+	if ((prog->flags &
+	     (DRGN_PROGRAM_IS_LINUX_KERNEL | DRGN_PROGRAM_IS_LIVE)) ==
+	    DRGN_PROGRAM_IS_LIVE) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "finding threads is not yet supported for live processes");
+	}
+
+	*ret = malloc(sizeof(**ret));
+	if (!*ret)
+		return &drgn_enomem;
+	(*ret)->prog = prog;
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		err = linux_helper_task_iterator_init(&(*ret)->task_iter, prog);
+		if (err)
+			goto err;
+		drgn_object_init(&(*ret)->entry.object, prog);
+		(*ret)->entry.prstatus = (struct nstring){};
+	} else {
+		err = drgn_program_cache_core_dump_notes(prog);
+		if (err)
+			goto err;
+		(*ret)->iterator = drgn_thread_set_first(&prog->thread_set);
+	}
+	return NULL;
+
+err:
+	free(*ret);
+	return err;
+}
+
+LIBDRGN_PUBLIC void
+drgn_thread_iterator_destroy(struct drgn_thread_iterator *it)
+{
+	if (it) {
+		if (it->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+			drgn_object_deinit(&it->entry.object);
+			linux_helper_task_iterator_deinit(&it->task_iter);
+		}
+		free(it);
+	}
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_thread_iterator_next(struct drgn_thread_iterator *it,
+			  struct drgn_thread **ret)
+{
+	if (it->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		const struct drgn_object *task;
+		struct drgn_error *err =
+			linux_helper_task_iterator_next(&it->task_iter, &task);
+		if (err)
+			return err;
+		if (!task) {
+			*ret = NULL;
+			return NULL;
+		}
+		it->entry.prog = drgn_object_program(task);
+		err = drgn_object_copy(&it->entry.object, task);
+		if (err)
+			return err;
+		struct drgn_object tid;
+		union drgn_value tid_value;
+		drgn_object_init(&tid, drgn_object_program(task));
+		err = drgn_object_member_dereference(&tid, task, "pid");
+		if (!err)
+			err = drgn_object_read_integer(&tid, &tid_value);
+		drgn_object_deinit(&tid);
+		if (err)
+			return err;
+		it->entry.tid = tid_value.uvalue;
+		*ret = &it->entry;
+	} else {
+		*ret = it->iterator.entry;
+		if (it->iterator.entry)
+			it->iterator = drgn_thread_set_next(it->iterator);
+	}
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_find_thread(struct drgn_program *prog, uint32_t tid,
+			 struct drgn_thread **ret)
+{
+	struct drgn_error *err;
+
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		*ret = malloc(sizeof(**ret));
+		if (!*ret)
+			return &drgn_enomem;
+		(*ret)->prog = prog;
+		(*ret)->tid = tid;
+		(*ret)->prstatus = (struct nstring){};
+
+		struct drgn_object *object = &(*ret)->object;
+		drgn_object_init(object, prog);
+		err = drgn_program_find_object(prog, "init_pid_ns", NULL,
+					       DRGN_FIND_OBJECT_VARIABLE,
+					       object);
+		if (err)
+			goto kernel_err;
+		err = drgn_object_address_of(object, object);
+		if (err)
+			goto kernel_err;
+		err = linux_helper_find_task(object, object, tid);
+		if (err)
+			goto kernel_err;
+		return NULL;
+
+kernel_err:
+		drgn_thread_destroy(*ret);
+		return err;
+	} else if (prog->flags & DRGN_PROGRAM_IS_LIVE) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "finding threads is not yet supported for live processes");
+	} else {
+		err = drgn_program_cache_core_dump_notes(prog);
+		if (err)
+			return err;
+		*ret = drgn_thread_set_search(&prog->thread_set, &tid).entry;
+		return NULL;
+	}
+}
+
+static struct drgn_error *
+drgn_program_kernel_core_dump_cache_crashed_thread(struct drgn_program *prog)
+{
+	struct drgn_error *err;
+
+	assert((prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) &&
+	       !(prog->flags & DRGN_PROGRAM_IS_LIVE));
+	assert(prog->core_dump_notes_cached);
+	if (prog->crashed_thread)
+		return NULL;
+
+	struct drgn_object crashing_cpu;
+	union drgn_value crashing_cpu_value;
+	drgn_object_init(&crashing_cpu, prog);
+	err = drgn_program_find_object(prog, "crashing_cpu", NULL,
+				       DRGN_FIND_OBJECT_VARIABLE,
+				       &crashing_cpu);
+	if (!err) {
+		err = drgn_object_read_integer(&crashing_cpu,
+					       &crashing_cpu_value);
+	}
+	drgn_object_deinit(&crashing_cpu);
+	if (err || crashing_cpu_value.uvalue >= prog->prstatus_vector.size)
+		return err;
+
+	struct nstring *prstatus =
+		&prog->prstatus_vector.data[crashing_cpu_value.uvalue];
+	uint32_t crashed_thread_tid;
+	err = get_prstatus_pid(prog, prstatus->str, prstatus->len,
+			       &crashed_thread_tid);
+	if (err)
+		return err;
+
+	if (crashed_thread_tid == 0) {
+		prog->crashed_thread = malloc(sizeof(*prog->crashed_thread));
+		if (!prog->crashed_thread)
+			return &drgn_enomem;
+		prog->crashed_thread->prog = prog;
+		prog->crashed_thread->tid = crashed_thread_tid;
+		drgn_object_init(&prog->crashed_thread->object, prog);
+		err = linux_helper_idle_task(&prog->crashed_thread->object,
+					     crashing_cpu_value.uvalue);
+		if (err) {
+			drgn_object_deinit(&prog->crashed_thread->object);
+			free(prog->crashed_thread);
+		}
+	} else {
+		err = drgn_program_find_thread(prog, crashed_thread_tid,
+					       &prog->crashed_thread);
+	}
+	if (err) {
+		prog->crashed_thread = NULL;
+		return err;
+	}
+	prog->crashed_thread->prstatus = *prstatus;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_crashed_thread(struct drgn_program *prog, struct drgn_thread **ret)
+{
+	struct drgn_error *err;
+
+	if (prog->flags & DRGN_PROGRAM_IS_LIVE) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "crashed thread is only defined for core dumps");
+	}
+	err = drgn_program_cache_core_dump_notes(prog);
+	if (err)
+		return err;
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		err = drgn_program_kernel_core_dump_cache_crashed_thread(prog);
+		if (err)
+			return err;
+	}
+	if (!prog->crashed_thread) {
+		return drgn_error_create(DRGN_ERROR_OTHER,
+					 "crashed thread not found");
+	}
+	*ret = prog->crashed_thread;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_thread_object(struct drgn_thread *thread, const struct drgn_object **ret)
+{
+	if (!(thread->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "thread object is currently only defined for the Linux kernel");
+	}
+	*ret = &thread->object;
+	return NULL;
 }
 
 struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
@@ -769,7 +1095,7 @@ struct drgn_error *drgn_program_find_prstatus_by_cpu(struct drgn_program *prog,
 						     uint32_t *tid_ret)
 {
 	assert(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL);
-	struct drgn_error *err = drgn_program_cache_prstatus(prog);
+	struct drgn_error *err = drgn_program_cache_core_dump_notes(prog);
 	if (err)
 		return err;
 
@@ -788,20 +1114,19 @@ struct drgn_error *drgn_program_find_prstatus_by_tid(struct drgn_program *prog,
 						     struct nstring *ret)
 {
 	struct drgn_error *err;
-	struct drgn_prstatus_map_iterator it;
 
 	assert(!(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL));
-	err = drgn_program_cache_prstatus(prog);
+	struct drgn_thread *thread;
+	err = drgn_program_find_thread(prog, tid, &thread);
 	if (err)
 		return err;
 
-	it = drgn_prstatus_map_search(&prog->prstatus_map, &tid);
-	if (!it.entry) {
+	if (!thread) {
 		ret->str = NULL;
 		ret->len = 0;
 		return NULL;
 	}
-	*ret = it.entry->value;
+	*ret = thread->prstatus;
 	return NULL;
 }
 

tests/linux_kernel/vmcore/test_vmcore.py
@@ -1,16 +1,29 @@
-from pathlib import Path
-import unittest
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-from drgn import Program, ProgramFlags
-from tests import TestCase
+from drgn import ProgramFlags
+from drgn.helpers.linux.pid import find_task
+from tests.linux_kernel.vmcore import LinuxVMCoreTestCase
 
-VMCORE_PATH = Path("/proc/vmcore")
 
+class TestVMCore(LinuxVMCoreTestCase):
+    def test_program_flags(self):
+        self.assertFalse(self.prog.flags & ProgramFlags.IS_LIVE)
+        self.assertTrue(self.prog.flags & ProgramFlags.IS_LINUX_KERNEL)
 
-@unittest.skipUnless(VMCORE_PATH.exists(), "not running in kdump")
-class TestAttachToVMCore(TestCase):
-    def test_attach_to_vmcore(self):
-        prog = Program()
-        prog.set_core_dump("/proc/vmcore")
-        self.assertFalse(prog.flags & ProgramFlags.IS_LIVE)
-        self.assertTrue(prog.flags & ProgramFlags.IS_LINUX_KERNEL)
+    def test_threads(self):
+        tids = [thread.tid for thread in self.prog.threads()]
+        self.assertIn(1, tids)
+        self.assertIn(self.prog.crashed_thread().tid, tids)
+
+    def test_thread(self):
+        thread = self.prog.thread(1)
+        self.assertEqual(thread.tid, 1)
+        self.assertEqual(thread.object, find_task(self.prog, 1))
+
+        crashed_thread_tid = self.prog.crashed_thread().tid
+        self.assertEqual(self.prog.thread(crashed_thread_tid).tid, crashed_thread_tid)
+
+    def test_crashed_thread(self):
+        crashed_thread = self.prog.crashed_thread()
+        self.assertGreater(crashed_thread.tid, 0)

tests/test_thread.py
@@ -1,0 +1,65 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import os.path
+import subprocess
+import tempfile
+import unittest
+
+from drgn import Program
+from tests import TestCase
+
+
+class TestCoreDump(TestCase):
+    TIDS = (
+        2265413,
+        2265414,
+        2265415,
+        2265416,
+        2265417,
+        2265418,
+        2265419,
+        2265420,
+        2265421,
+        2265422,
+        2265423,
+        2265424,
+        2265425,
+    )
+
+    CRASHED_TID = 2265419
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        with tempfile.NamedTemporaryFile() as core_dump_file:
+            try:
+                subprocess.check_call(
+                    [
+                        "zstd",
+                        "--quiet",
+                        "--decompress",
+                        "--stdout",
+                        "tests/sample.coredump.zst",
+                    ],
+                    stdout=core_dump_file,
+                )
+            except FileNotFoundError:
+                raise unittest.SkipTest("zstd not found")
+            cls.prog = Program()
+            cls.prog.set_core_dump(core_dump_file.name)
+
+    def test_threads(self):
+        self.assertSequenceEqual(
+            sorted(thread.tid for thread in self.prog.threads()),
+            self.TIDS,
+        )
+
+    def test_thread(self):
+        for tid in self.TIDS:
+            self.assertEqual(self.prog.thread(tid).tid, tid)
+        self.assertRaises(LookupError, self.prog.thread, 99)
+
+    def test_crashed_thread(self):
+        self.assertEqual(self.prog.crashed_thread().tid, self.CRASHED_TID)

libdrgn/python/program.c
@@ -698,14 +698,13 @@ static DrgnObject *Program_variable(Program *self, PyObject *args,
 				   DRGN_FIND_OBJECT_VARIABLE);
 }
 
-static StackTrace *Program_stack_trace(Program *self, PyObject *args,
-				       PyObject *kwds)
+static PyObject *Program_stack_trace(Program *self, PyObject *args,
+				     PyObject *kwds)
 {
 	static char *keywords[] = {"thread", NULL};
 	struct drgn_error *err;
 	PyObject *thread;
 	struct drgn_stack_trace *trace;
-	StackTrace *ret;
 
 	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O:stack_trace", keywords,
 					 &thread))
@@ -724,13 +723,9 @@ static StackTrace *Program_stack_trace(Program *self, PyObject *args,
 	if (err)
 		return set_drgn_error(err);
 
-	ret = (StackTrace *)StackTrace_type.tp_alloc(&StackTrace_type, 0);
-	if (!ret) {
+	PyObject *ret = StackTrace_wrap(trace);
+	if (!ret)
 		drgn_stack_trace_destroy(trace);
-		return NULL;
-	}
-	ret->trace = trace;
-	Py_INCREF(self);
 	return ret;
 }
 
@@ -763,6 +758,59 @@ static PyObject *Program_symbol(Program *self, PyObject *arg)
 		return NULL;
 	}
 	return ret;
+}
+
+static ThreadIterator *Program_threads(Program *self)
+{
+	struct drgn_thread_iterator *it;
+	struct drgn_error *err = drgn_thread_iterator_create(&self->prog, &it);
+	if (err)
+		return set_drgn_error(err);
+	ThreadIterator *ret =
+		(ThreadIterator *)ThreadIterator_type.tp_alloc(&ThreadIterator_type,
+							       0);
+	if (!ret) {
+		drgn_thread_iterator_destroy(it);
+		return NULL;
+	}
+	ret->prog = self;
+	ret->iterator = it;
+	Py_INCREF(self);
+	return ret;
+}
+
+static PyObject *Program_thread(Program *self, PyObject *args, PyObject *kwds)
+{
+	static char *keywords[] = {"tid", NULL};
+	struct drgn_error *err;
+	struct index_arg tid = {};
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O&:thread", keywords,
+					 index_converter, &tid))
+		return NULL;
+
+	struct drgn_thread *thread;
+	err = drgn_program_find_thread(&self->prog, tid.uvalue, &thread);
+	if (err)
+		return set_drgn_error(err);
+	if (!thread) {
+		return PyErr_Format(PyExc_LookupError,
+				    "thread with ID %llu not found",
+				    tid.uvalue);
+	}
+	PyObject *ret = Thread_wrap(thread);
+	drgn_thread_destroy(thread);
+	return ret;
+}
+
+static PyObject *Program_crashed_thread(Program *self)
+{
+	struct drgn_error *err;
+	struct drgn_thread *thread;
+	err = drgn_program_crashed_thread(&self->prog, &thread);
+	if (err)
+		return set_drgn_error(err);
+	return Thread_wrap(thread);
 }
 
 static DrgnObject *Program_subscript(Program *self, PyObject *key)
@@ -907,6 +955,12 @@ static PyMethodDef Program_methods[] = {
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_stack_trace_DOC},
 	{"symbol", (PyCFunction)Program_symbol, METH_O,
 	 drgn_Program_symbol_DOC},
+	{"threads", (PyCFunction)Program_threads, METH_NOARGS,
+	 drgn_Program_threads_DOC},
+	{"thread", (PyCFunction)Program_thread,
+	 METH_VARARGS | METH_KEYWORDS, drgn_Program_thread_DOC},
+	{"crashed_thread", (PyCFunction)Program_crashed_thread, METH_NOARGS,
+	 drgn_Program_crashed_thread_DOC},
 	{"void_type", (PyCFunction)Program_void_type,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_void_type_DOC},
 	{"int_type", (PyCFunction)Program_int_type,

_drgn.pyi
@@ -253,6 +253,31 @@ class Program:
             the given file
         """
         ...
+    def threads(self) -> Iterator[Thread]:
+        """Get an iterator over all of the threads in the program."""
+        ...
+    def thread(self, tid: IntegerLike) -> Thread:
+        """
+        Get the thread with the given thread ID.
+
+        :param tid: Thread ID (as defined by `gettid(2)
+            <http://man7.org/linux/man-pages/man2/gettid.2.html>`_).
+        :raises LookupError: if no thread has the given thread ID
+        """
+        ...
+    def crashed_thread(self) -> Thread:
+        """
+        Get the thread that caused the program to crash.
+
+        For userspace programs, this is the thread that received the fatal
+        signal (e.g., ``SIGSEGV`` or ``SIGQUIT``).
+
+        For the kernel, this is the thread that panicked (either directly or as
+        a result of an oops, ``BUG_ON()``, etc.).
+
+        :raises ValueError: if the program is live (i.e., not a core dump)
+        """
+        ...
     def read(
         self, address: IntegerLike, size: IntegerLike, physical: bool = False
     ) -> bytes:
@@ -763,6 +788,28 @@ class FindObjectFlags(enum.Flag):
     ""
     ANY = ...
     ""
+
+class Thread:
+    """A thread in a program."""
+
+    tid: int
+    """
+    Thread ID (as defined by `gettid(2)
+    <http://man7.org/linux/man-pages/man2/gettid.2.html>`_).
+    """
+    object: Object
+    """
+    If the program is the Linux kernel, the ``struct task_struct *`` object for
+    this thread. Otherwise, not defined.
+    """
+    def stack_trace(self) -> StackTrace:
+        """
+        Get the stack trace for this thread.
+
+        This is equivalent to ``prog.stack_trace(thread.tid)``. See
+        :meth:`Program.stack_trace()`.
+        """
+        ...
 
 def filename_matches(haystack: Optional[str], needle: Optional[str]) -> bool:
     """

commit 98577e5e23515d553a4d09038280a46909318c2d

libdrgn: fix drgn_program_find_thread() for Linux kernel when thread isn't found
If a TID does not exist, then linux_helper_find_task() succeeds but
returns a null pointer object. Check for that instead of returning a
bogus thread.

Fixes: 301cc767bab7 ("Implement a new API for representing threads")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/drgn.h.in
@@ -2863,8 +2863,8 @@ struct drgn_error *drgn_thread_iterator_next(struct drgn_thread_iterator *it,
  * Get the thread with the given thread ID.
  *
  * @param[in] tid Thread ID.
- * @param[out] ret New thread handle. On success, must be destroyed with @ref
- * drgn_thread_destroy().
+ * @param[out] ret New thread handle, or @c NULL if not found. On success, must
+ * be destroyed with @ref drgn_thread_destroy().
  * @return @c NULL on success, non-@c NULL on error.
  */
 struct drgn_error *drgn_program_find_thread(struct drgn_program *prog,

libdrgn/program.c
@@ -1026,6 +1026,14 @@ drgn_program_find_thread(struct drgn_program *prog, uint32_t tid,
 		err = linux_helper_find_task(object, object, tid);
 		if (err)
 			goto kernel_err;
+		bool truthy;
+		err = drgn_object_bool(object, &truthy);
+		if (err)
+			goto kernel_err;
+		if (!truthy) {
+			drgn_thread_destroy(*ret);
+			*ret = NULL;
+		}
 		return NULL;
 
 kernel_err:

tests/linux_kernel/vmcore/test_vmcore.py
@@ -24,6 +24,13 @@ class TestVMCore(LinuxVMCoreTestCase):
         crashed_thread_tid = self.prog.crashed_thread().tid
         self.assertEqual(self.prog.thread(crashed_thread_tid).tid, crashed_thread_tid)
 
+    def test_thread_not_found(self):
+        tids = {thread.tid for thread in self.prog.threads()}
+        tid = 1
+        while tid in tids:
+            tid += 1
+        self.assertRaises(LookupError, self.prog.thread, tid)
+
     def test_main_thread(self):
         self.assertRaisesRegex(
             ValueError,

tests/test_thread.py
@@ -60,6 +60,8 @@ class TestCoreDump(TestCase):
     def test_thread(self):
         for tid in self.TIDS:
             self.assertEqual(self.prog.thread(tid).tid, tid)
+
+    def test_thread_not_found(self):
         self.assertRaises(LookupError, self.prog.thread, 99)
 
     def test_main_thread(self):