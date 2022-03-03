b300021ba3c12294533a34c2d38a924729941806
Omar Sandoval <osandov@osandov.com>
2022-03-03 01:23:01 -0800

    scripts/build_dists.sh: use docker run --pull always instead of docker pull
    This way we only need to invoke docker (and sudo, if running with
    DOCKER="sudo docker") once.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- scripts/build_dists.sh
+++ scripts/build_dists.sh
@@ -6,9 +6,7 @@ set -eux
 "$PYTHON" setup.py sdist
 SDIST=dist/drgn-"$("$PYTHON" setup.py --version)".tar.gz
 
-: "${DOCKER=docker}"
-$DOCKER pull quay.io/pypa/manylinux2010_x86_64
-$DOCKER run -it \
+${DOCKER=docker} run -it \
 	--env PLAT=manylinux2010_x86_64 \
 	--env SDIST="$SDIST" \
 	--env OWNER="$(id -u):$(id -g)" \
@@ -17,5 +15,6 @@ $DOCKER run -it \
 	--workdir /io \
 	--hostname drgn \
 	--rm \
+	--pull always \
 	quay.io/pypa/manylinux2010_x86_64 \
 	./scripts/build_manylinux_in_docker.sh
