eb75ecadd6e726179bf736905a7a8d89ea67ee01
Omar Sandoval <osandov@osandov.com>
2022-04-01 16:06:41 -0700

    tests: fix slab tests for SLOB allocator
    SLOB doesn't have /proc/slabinfo (and it can be disabled for SLUB and
    SLAB on some kernel versions, too). Fall back to some known slab caches
    if /proc/slabinfo doesn't exist.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/helpers/linux/test_slab.py
+++ tests/helpers/linux/test_slab.py
@@ -13,14 +13,41 @@ def get_proc_slabinfo_names():
         return [line.split()[0] for line in f]
 
 
+def fallback_slab_cache_names(prog):
+    # SLOB does not provide /proc/slabinfo. It is also disabled for SLUB if
+    # CONFIG_SLUB_DEBUG=n. Before Linux kernel commit 5b36577109be ("mm:
+    # slabinfo: remove CONFIG_SLABINFO") (in v4.15), it could also be disabled
+    # for SLAB. So, pick a few slab caches which we know exist to test against.
+    # In case they were merged into other caches, get their names from the
+    # structs rather than just returning the names.
+    return {
+        prog["dentry_cache"].name.string_(),
+        prog["mm_cachep"].name.string_(),
+        prog["uid_cachep"].name.string_(),
+    }
+
+
 class TestSlab(LinuxKernelTestCase):
     def test_for_each_slab_cache(self):
-        self.assertCountEqual(
-            get_proc_slabinfo_names(),
-            [s.name.string_() for s in for_each_slab_cache(self.prog)],
-        )
+        try:
+            slab_cache_names = get_proc_slabinfo_names()
+        except FileNotFoundError:
+            # The found names should be a superset of the fallback names.
+            self.assertGreaterEqual(
+                {s.name.string_() for s in for_each_slab_cache(self.prog)},
+                fallback_slab_cache_names(self.prog),
+            )
+        else:
+            self.assertCountEqual(
+                [s.name.string_() for s in for_each_slab_cache(self.prog)],
+                slab_cache_names,
+            )
 
     def test_find_slab_cache(self):
-        for name in get_proc_slabinfo_names():
+        try:
+            slab_cache_names = get_proc_slabinfo_names()
+        except FileNotFoundError:
+            slab_cache_names = fallback_slab_cache_names(self.prog)
+        for name in slab_cache_names:
             slab = find_slab_cache(self.prog, name)
             self.assertEqual(name, slab.name.string_())
