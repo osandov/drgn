042c98be4aefb1ae03da4424d9a22db1d8d030d0
Omar Sandoval <osandov@osandov.com>
2022-05-16 13:24:24 -0700

    tests: use test kernel module for Linux list helper tests
    Rather than picking a couple of lists which we hope won't change, use
    the newly added test kernel module to define a few lists and test
    against those. This also gives us proper tests for list_is_singular().
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/helpers/linux/test_list.py
+++ tests/helpers/linux/test_list.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 import unittest
 
-from drgn import Object, container_of
+from drgn import NULL
 from drgn.helpers.linux.list import (
     list_empty,
     list_first_entry,
@@ -18,153 +18,141 @@ from drgn.helpers.linux.list import (
     list_next_entry,
     list_prev_entry,
 )
-from tests.linux_kernel import LinuxKernelTestCase
+from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
 
 
-class ListTestCaseMixin:
-    @classmethod
-    def _node_to_key(cls, node):
-        entry = container_of(node, cls.ENTRY_TYPE, cls.NODE_MEMBER)
-        return cls._entry_to_key(entry)
-
-    def test_list_for_each(self):
-        self.assertEqual(
-            [self._node_to_key(node) for node in list_for_each(self.list)], self.keys
-        )
-
-    def test_list_for_each_reverse(self):
-        self.assertEqual(
-            [self._node_to_key(node) for node in list_for_each_reverse(self.list)],
-            list(reversed(self.keys)),
-        )
-
-    def test_list_for_each_entry(self):
-        self.assertEqual(
-            [
-                self._entry_to_key(entry)
-                for entry in list_for_each_entry(
-                    self.ENTRY_TYPE, self.list, self.NODE_MEMBER
-                )
-            ],
-            self.keys,
-        )
-
-    def test_list_for_each_reverse_entry(self):
-        self.assertEqual(
-            [
-                self._entry_to_key(entry)
-                for entry in list_for_each_entry_reverse(
-                    self.ENTRY_TYPE, self.list, self.NODE_MEMBER
-                )
-            ],
-            list(reversed(self.keys)),
-        )
-
-
-class TestListNotEmpty(LinuxKernelTestCase, ListTestCaseMixin):
+@skip_unless_have_test_kmod
+class TestList(LinuxKernelTestCase):
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
-        cls.keys = []
-        try:
-            with open("/proc/modules", "rb") as f:
-                for line in f:
-                    cls.keys.append(line.partition(b" ")[0])
-        except FileNotFoundError:
-            pass
-        if not cls.keys:
-            raise unittest.SkipTest("no modules loaded")
-        cls.list = cls.prog["modules"].address_of_()
-        cls.ENTRY_TYPE = cls.prog.type("struct module")
-        cls.NODE_MEMBER = "list"
+        cls.empty = cls.prog["drgn_test_empty_list"].address_of_()
+        cls.full = cls.prog["drgn_test_full_list"].address_of_()
+        cls.entries = cls.prog["drgn_test_list_entries"]
+        cls.num_entries = 3
+        cls.singular = cls.prog["drgn_test_singular_list"].address_of_()
+        cls.singular_entry = cls.prog["drgn_test_singular_list_entry"].address_of_()
+        cls.singular_node = cls.singular_entry.node.address_of_()
 
-    @classmethod
-    def _entry_to_key(cls, entry):
-        return entry.name.string_()
+    def node(self, n):
+        return self.entries[n].node.address_of_()
+
+    def entry(self, n):
+        return self.entries[n].address_of_()
 
     def test_list_empty(self):
-        self.assertFalse(list_empty(self.list))
+        self.assertTrue(list_empty(self.empty))
+        self.assertFalse(list_empty(self.full))
+        self.assertFalse(list_empty(self.singular))
 
     def test_list_is_singular(self):
-        self.assertEqual(list_is_singular(self.list), len(self.keys) == 1)
+        self.assertFalse(list_is_singular(self.empty))
+        self.assertFalse(list_is_singular(self.full))
+        self.assertTrue(list_is_singular(self.singular))
 
     def test_list_first_entry(self):
         self.assertEqual(
-            self._entry_to_key(
-                list_first_entry(self.list, self.ENTRY_TYPE, self.NODE_MEMBER)
-            ),
-            self.keys[0],
+            list_first_entry(self.full, "struct drgn_test_list_entry", "node"),
+            self.entry(0),
+        )
+        self.assertEqual(
+            list_first_entry(self.singular, "struct drgn_test_list_entry", "node"),
+            self.singular_entry,
         )
 
     def test_list_first_entry_or_null(self):
         self.assertEqual(
-            self._entry_to_key(
-                list_first_entry_or_null(self.list, self.ENTRY_TYPE, self.NODE_MEMBER)
+            list_first_entry_or_null(self.empty, "struct drgn_test_list_entry", "node"),
+            NULL(self.prog, "struct drgn_test_list_entry *"),
+        )
+        self.assertEqual(
+            list_first_entry_or_null(self.full, "struct drgn_test_list_entry", "node"),
+            self.entry(0),
+        )
+        self.assertEqual(
+            list_first_entry_or_null(
+                self.singular, "struct drgn_test_list_entry", "node"
             ),
-            self.keys[0],
+            self.singular_entry,
         )
 
     def test_list_last_entry(self):
         self.assertEqual(
-            self._entry_to_key(
-                list_last_entry(self.list, self.ENTRY_TYPE, self.NODE_MEMBER)
-            ),
-            self.keys[-1],
+            list_last_entry(self.full, "struct drgn_test_list_entry", "node"),
+            self.entry(self.num_entries - 1),
+        )
+        self.assertEqual(
+            list_last_entry(self.singular, "struct drgn_test_list_entry", "node"),
+            self.singular_entry,
         )
 
     def test_list_next_entry(self):
-        entry = list_first_entry(self.list, self.ENTRY_TYPE, self.NODE_MEMBER)
-        keys = []
-        while True:
-            keys.append(self._entry_to_key(entry))
-            if getattr(entry, self.NODE_MEMBER).next == self.list:
-                break
-            entry = list_next_entry(entry, self.NODE_MEMBER)
-        self.assertEqual(keys, self.keys)
+        for i in range(self.num_entries - 1):
+            self.assertEqual(list_next_entry(self.entry(i), "node"), self.entry(i + 1))
 
     def test_list_prev_entry(self):
-        entry = list_last_entry(self.list, self.ENTRY_TYPE, self.NODE_MEMBER)
-        keys = []
-        while True:
-            keys.append(self._entry_to_key(entry))
-            if getattr(entry, self.NODE_MEMBER).prev == self.list:
-                break
-            entry = list_prev_entry(entry, self.NODE_MEMBER)
-        self.assertEqual(keys, list(reversed(self.keys)))
+        for i in range(1, self.num_entries):
+            self.assertEqual(list_prev_entry(self.entry(i), "node"), self.entry(i - 1))
 
+    def test_list_for_each(self):
+        self.assertEqual(list(list_for_each(self.empty)), [])
+        self.assertEqual(
+            list(list_for_each(self.full)),
+            [self.node(i) for i in range(self.num_entries)],
+        )
+        self.assertEqual(list(list_for_each(self.singular)), [self.singular_node])
 
-@unittest.skipIf(Path("/proc/vmcore").exists(), "list is not empty in kdump kernel")
-class TestListEmpty(LinuxKernelTestCase, ListTestCaseMixin):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.keys = []
-        try:
-            # In the kdump kernel, this list is populated with chunks of memory
-            # in the vmcore file. We can assume that it's empty otherwise.
-            cls.list = cls.prog["vmcore_list"].address_of_()
-        except LookupError:
-            # If other definitions from the same file exist, then maybe
-            # vmcore_list was removed or renamed. Fail hard so we update the
-            # test.
-            if "proc_vmcore" in cls.prog or "vmcore_init" in cls.prog:
-                raise
-            raise unittest.SkipTest("kernel not built with CONFIG_PROC_VMCORE")
-        cls.ENTRY_TYPE = cls.prog.type("struct vmcore")
-        cls.NODE_MEMBER = "list"
+    def test_list_for_each_reverse(self):
+        self.assertEqual(list(list_for_each_reverse(self.empty)), [])
+        self.assertEqual(
+            list(list_for_each_reverse(self.full)),
+            [self.node(i) for i in range(self.num_entries - 1, -1, -1)],
+        )
+        self.assertEqual(
+            list(list_for_each_reverse(self.singular)), [self.singular_node]
+        )
 
-    @classmethod
-    def _entry_to_key(cls, entry):
-        return entry.paddr.value_()
+    def test_list_for_each_entry(self):
+        self.assertEqual(
+            list(
+                list_for_each_entry("struct drgn_test_list_entry", self.empty, "node")
+            ),
+            [],
+        )
+        self.assertEqual(
+            list(list_for_each_entry("struct drgn_test_list_entry", self.full, "node")),
+            [self.entry(i) for i in range(self.num_entries)],
+        )
+        self.assertEqual(
+            list(
+                list_for_each_entry(
+                    "struct drgn_test_list_entry", self.singular, "node"
+                )
+            ),
+            [self.singular_entry],
+        )
 
-    def test_list_empty(self):
-        self.assertTrue(list_empty(self.list))
-
-    def test_list_is_singular(self):
-        self.assertFalse(list_is_singular(self.list))
-
-    def test_list_first_entry_or_null(self):
-        self.assertIdentical(
-            list_first_entry_or_null(self.list, self.ENTRY_TYPE, self.NODE_MEMBER),
-            Object(self.prog, self.prog.pointer_type(self.ENTRY_TYPE), 0),
+    def test_list_for_each_entry_reverse(self):
+        self.assertEqual(
+            list(
+                list_for_each_entry_reverse(
+                    "struct drgn_test_list_entry", self.empty, "node"
+                )
+            ),
+            [],
+        )
+        self.assertEqual(
+            list(
+                list_for_each_entry_reverse(
+                    "struct drgn_test_list_entry", self.full, "node"
+                )
+            ),
+            [self.entry(i) for i in range(self.num_entries - 1, -1, -1)],
+        )
+        self.assertEqual(
+            list(
+                list_for_each_entry_reverse(
+                    "struct drgn_test_list_entry", self.singular, "node"
+                )
+            ),
+            [self.singular_entry],
         )
--- tests/linux_kernel/kmod/drgn_test.c
+++ tests/linux_kernel/kmod/drgn_test.c
@@ -8,10 +8,36 @@
 // This is intended to be used with drgn's vmtest framework, but in theory it
 // can be used with any kernel that has debug info enabled (at your own risk).
 
+#include <linux/kernel.h>
+#include <linux/list.h>
 #include <linux/module.h>
+
+LIST_HEAD(drgn_test_empty_list);
+LIST_HEAD(drgn_test_full_list);
+LIST_HEAD(drgn_test_singular_list);
+
+struct drgn_test_list_entry {
+	struct list_head node;
+	int value;
+};
+
+struct drgn_test_list_entry drgn_test_list_entries[3];
+struct drgn_test_list_entry drgn_test_singular_list_entry;
+
+static void drgn_test_list_init(void)
+{
+	size_t i;
+
+	for (i = 0; i < ARRAY_SIZE(drgn_test_list_entries); i++) {
+		list_add_tail(&drgn_test_list_entries[i].node,
+			      &drgn_test_full_list);
+	}
+	list_add(&drgn_test_singular_list_entry.node, &drgn_test_singular_list);
+}
 
 static int __init drgn_test_init(void)
 {
+	drgn_test_list_init();
 	return 0;
 }
 
