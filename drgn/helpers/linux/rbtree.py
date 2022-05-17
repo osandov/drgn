osandov/drgn

commit 755f79012e6c3ef9fbe4294de396a5784540014f

helpers: add RB_EMPTY_ROOT
We couldn't test this before, but now that we can we might as well add
it.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/rbtree.py
@@ -26,6 +26,15 @@ __all__ = (
 )
 
 
+def RB_EMPTY_ROOT(root: Object) -> bool:
+    """
+    Return whether a red-black tree is empty.
+
+    :param node: ``struct rb_root *``
+    """
+    return not root.rb_node
+
+
 def RB_EMPTY_NODE(node: Object) -> bool:
     """
     Return whether a red-black tree node is empty, i.e., not inserted in a

tests/helpers/linux/test_rbtree.py
@@ -8,6 +8,7 @@ import signal
 from drgn import NULL
 from drgn.helpers.linux.rbtree import (
     RB_EMPTY_NODE,
+    RB_EMPTY_ROOT,
     rb_find,
     rb_first,
     rb_last,
@@ -33,6 +34,12 @@ class TestRbtree(LinuxKernelTestCase):
 
     def entry(self, n):
         return self.entries[n].address_of_()
+
+    def test_RB_EMPTY_ROOT(self):
+        self.assertTrue(
+            RB_EMPTY_ROOT(self.prog["drgn_test_empty_rb_root"].address_of_())
+        )
+        self.assertFalse(RB_EMPTY_ROOT(self.root))
 
     def test_RB_EMPTY_NODE(self):
         self.assertTrue(

tests/linux_kernel/kmod/drgn_test.c
@@ -55,6 +55,7 @@ static void drgn_test_list_init(void)
 
 // rbtree
 
+struct rb_root drgn_test_empty_rb_root = RB_ROOT;
 struct rb_root drgn_test_rb_root = RB_ROOT;
 
 struct drgn_test_rb_entry {

commit 4a6b499811e6e500cfd07932cdd4cda98a5b451d

helpers: add RB_EMPTY_ROOT to __all__
Fixes: 755f79012e6c ("helpers: add RB_EMPTY_ROOT")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/rbtree.py
@@ -14,6 +14,7 @@ from typing import Callable, Iterator, TypeVar, Union
 from drgn import NULL, Object, Type, container_of
 
 __all__ = (
+    "RB_EMPTY_ROOT",
     "RB_EMPTY_NODE",
     "rb_find",
     "rb_first",