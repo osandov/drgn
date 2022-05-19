osandov/drgn

commit cb3bb6cc2bad3405adc57984c97a9e177661f201

helpers: add slab cache merging helper and documentation
Slab cache merging can cause major confusion when debugging using slab
cache helpers. Add a helper to detect whether a slab cache is merged and
an explanation of the implications.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/slab.py
@@ -7,6 +7,12 @@ Slab Allocator
 
 The ``drgn.helpers.linux.slab`` module provides helpers for working with the
 Linux slab allocator.
+
+.. warning::
+
+    Beware of slab merging when using these helpers. See
+    :func:`slab_cache_is_merged()
+    <drgn.helpers.linux.slab.slab_cache_is_merged>`.
 """
 
 from typing import Iterator, Optional, Set, Union
@@ -22,6 +28,45 @@ __all__ = (
     "print_slab_caches",
     "slab_cache_for_each_allocated_object",
 )
+
+
+def slab_cache_is_merged(slab_cache: Object) -> bool:
+    """
+    Return whether a slab cache has been merged with any other slab caches.
+
+    Unless configured otherwise, the kernel may merge slab caches of similar
+    sizes together. See the `SLUB users guide
+    <https://docs.kernel.org/vm/slub.html#slab-merging>`_ and
+    ``slab_merge``/``slab_nomerge`` in the `kernel parameters documentation
+    <https://www.kernel.org/doc/Documentation/admin-guide/kernel-parameters.txt>`_.
+
+    This can cause confusion, as only the name of the first cache will be
+    found, and objects of different types will be mixed in the same slab cache.
+
+    For example, suppose that we have two types, ``struct foo`` and ``struct
+    bar``, which have the same size but are otherwise unrelated. If the kernel
+    creates a slab cache named ``foo`` for ``struct foo``, then another slab
+    cache named ``bar`` for ``struct bar``, then slab cache ``foo`` will be
+    reused instead of creating another cache for ``bar``. So the following will
+    fail::
+
+        find_slab_cache(prog, "bar")
+
+    And the following will also return ``struct bar *`` objects errantly casted to
+    ``struct foo *``::
+
+        slab_cache_for_each_allocated_object(
+            find_slab_cache(prog, "foo"), "struct foo"
+        )
+
+    Unfortunately, these issues are difficult to work around generally, so one
+    must be prepared to handle them on a case-by-case basis (e.g., by looking
+    up the slab cache by its variable name and by checking that members of the
+    structure make sense for the expected type).
+
+    :param slab_cache: ``struct kmem_cache *``
+    """
+    return slab_cache.refcount > 1
 
 
 def for_each_slab_cache(prog: Program) -> Iterator[Object]:

tests/helpers/linux/test_slab.py
@@ -1,11 +1,15 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from collections import defaultdict
+from pathlib import Path
+
 from drgn.helpers.linux.kconfig import get_kconfig
 from drgn.helpers.linux.slab import (
     find_slab_cache,
     for_each_slab_cache,
     slab_cache_for_each_allocated_object,
+    slab_cache_is_merged,
 )
 from tests.linux_kernel import LinuxKernelTestCase
 
@@ -47,6 +51,38 @@ class TestSlab(LinuxKernelTestCase):
             else:
                 raise Exception("couldn't find slab allocator config option")
             cls.allocator = allocator
+
+    def _slab_cache_aliases(self):
+        slab_path = Path("/sys/kernel/slab")
+        if not slab_path.exists():
+            self.skipTest(f"{slab_path} does not exist")
+        aliases = defaultdict(list)
+        for child in slab_path.iterdir():
+            if not child.name.startswith(":"):
+                aliases[child.stat().st_ino].append(child.name)
+        return aliases
+
+    def test_slab_cache_is_merged_false(self):
+        for aliases in self._slab_cache_aliases().values():
+            if len(aliases) == 1:
+                break
+        else:
+            self.skipTest("no unmerged slab caches")
+        self.assertFalse(slab_cache_is_merged(find_slab_cache(self.prog, aliases[0])))
+
+    def test_slab_cache_is_merged_true(self):
+        for aliases in self._slab_cache_aliases().values():
+            if len(aliases) > 1:
+                break
+        else:
+            self.skipTest("no merged slab caches")
+        for alias in aliases:
+            slab_cache = find_slab_cache(self.prog, alias)
+            if slab_cache is not None:
+                break
+        else:
+            self.fail("couldn't find slab cache")
+        self.assertTrue(slab_cache_is_merged(slab_cache))
 
     def test_for_each_slab_cache(self):
         try:

commit 34dfaabb2d4ae90c21c51c510d752587f46aa884

helpers: add slab_cache_is_merged() to __all__
Fixes: cb3bb6cc2bad ("helpers: add slab cache merging helper and documentation")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/slab.py
@@ -29,6 +29,7 @@ __all__ = (
     "for_each_slab_cache",
     "print_slab_caches",
     "slab_cache_for_each_allocated_object",
+    "slab_cache_is_merged",
 )
 
 