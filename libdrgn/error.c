osandov/drgn

commit 80fef04c704d9fac072d064eb08bd879323082c0

Add address attribute to FaultError exception

libdrgn/drgn.h.in
@@ -108,6 +108,11 @@ struct drgn_error {
 	 * encountered the error if applicable. Otherwise, @c NULL.
 	 */
 	char *path;
+	/*
+	 * If @c code is @c DRGN_ERROR_FAULT, then the address of the read
+	 * which encountered the error.
+	 */
+	uint64_t address;
 	/** Human-readable error message. */
 	char *message;
 };
@@ -182,6 +187,28 @@ struct drgn_error *drgn_error_create_os(const char *message, int errnum,
 struct drgn_error *drgn_error_format_os(const char *message, int errnum,
 					const char *path_format, ...)
 	__attribute__((returns_nonnull,format(printf, 3, 4)));
+
+/**
+ * Create a @ref DRGN_ERROR_FAULT @ref drgn_error.
+ *
+ * @param[in] message Human-readable error message. This string is copied.
+ * @param[in] address Address where the fault happened.
+ */
+struct drgn_error *drgn_error_create_fault(const char *message, uint64_t address)
+	__attribute__((returns_nonnull));
+
+/**
+ *
+ * Create a @ref DRGN_ERROR_FAULT @ref drgn_error with a printf-style formatted
+ * message.
+ *
+ * @param[in] address Address where the fault happened.
+ * @param[in] format printf-style format string for message.
+ * @param[in] ... Arguments for the format string.
+ */
+struct drgn_error *drgn_error_format_fault(uint64_t address,
+					   const char *format, ...)
+	__attribute__((returns_nonnull,format(printf, 2, 3)));
 
 /**
  * Write a @ref drgn_error to a @c stdio stream.

libdrgn/error.c
@@ -3,6 +3,7 @@
 
 #include <errno.h>
 #include <elfutils/libdwfl.h>
+#include <inttypes.h>
 #include <libelf.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -42,6 +43,7 @@ static struct drgn_error *drgn_error_create_nodup(enum drgn_error_code code,
 	err->needs_destroy = true;
 	err->errnum = 0;
 	err->path = NULL;
+	err->address = 0;
 	err->message = message;
 	return err;
 }
@@ -83,6 +85,7 @@ drgn_error_format_os(const char *message, int errnum, const char *path_format,
 	} else {
 		err->path = NULL;
 	}
+	err->address = 0;
 	err->message = strdup(message);
 	if (!err->message) {
 		free(err->path);
@@ -117,6 +120,36 @@ LIBDRGN_PUBLIC struct drgn_error *drgn_error_format(enum drgn_error_code code,
 	return drgn_error_create_nodup(code, message);
 }
 
+LIBDRGN_PUBLIC struct drgn_error *drgn_error_create_fault(const char *message,
+							  uint64_t address)
+{
+	struct drgn_error *err;
+
+	err = drgn_error_create(DRGN_ERROR_FAULT, message);
+	if (err != &drgn_enomem)
+		err->address = address;
+	return err;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_error_format_fault(uint64_t address, const char *format, ...)
+{
+	struct drgn_error *err;
+	va_list ap;
+	char *message;
+	int ret;
+
+	va_start(ap, format);
+	ret = vasprintf(&message, format, ap);
+	va_end(ap);
+	if (ret == -1)
+                return &drgn_enomem;
+	err = drgn_error_create_nodup(DRGN_ERROR_FAULT, message);
+	if (err != &drgn_enomem)
+		err->address = address;
+	return err;
+}
+
 struct drgn_error *drgn_error_from_string_builder(enum drgn_error_code code,
 						  struct string_builder *sb)
 {
@@ -144,6 +177,10 @@ bool string_builder_append_error(struct string_builder *sb,
 			ret = string_builder_appendf(sb, "%s: %m",
 						     err->message);
 		}
+	} else if (err->code == DRGN_ERROR_FAULT) {
+		ret = string_builder_appendf(sb, "%s: 0x%" PRIx64,
+					     err->message,
+					     err->address);
 	} else {
 		ret = string_builder_append(sb, err->message);
 	}

libdrgn/python/error.c
@@ -3,6 +3,63 @@
 
 #include "drgnpy.h"
 
+int FaultError_init(PyObject *self, PyObject *args, PyObject *kwds)
+{
+	static char *keywords[] = {"message", "address", NULL};
+	PyObject *address, *message;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "OO:FaultError", keywords,
+					 &message, &address))
+		return -1;
+
+	if (PyObject_SetAttrString(self, "message", message) < 0 ||
+	    PyObject_SetAttrString(self, "address", address) < 0)
+		return -1;
+	return 0;
+}
+
+static PyObject *FaultError_str(PyObject *self)
+{
+	PyObject *message, *address, *args, *fmt, *ret = NULL;
+
+	message = PyObject_GetAttrString(self, "message");
+	if (!message)
+		return NULL;
+
+	address = PyObject_GetAttrString(self, "address");
+	if (!address)
+		goto out_message;
+
+	args = Py_BuildValue("OO", message, address);
+	if (!args)
+		goto out_address;
+
+	fmt = PyUnicode_FromString("%s: %#x");
+	if (!fmt)
+		goto out_args;
+
+	ret = PyUnicode_Format(fmt, args);
+
+	Py_DECREF(fmt);
+out_args:
+	Py_DECREF(args);
+out_address:
+	Py_DECREF(address);
+out_message:
+	Py_DECREF(message);
+	return ret;
+}
+
+PyTypeObject FaultError_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.FaultError",
+	.tp_basicsize = sizeof(PyBaseExceptionObject),
+	.tp_str = (reprfunc)FaultError_str,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+	.tp_doc = drgn_FaultError_DOC,
+	.tp_init = (initproc)FaultError_init,
+};
+
 static struct drgn_error drgn_error_python = {
 	.code = DRGN_ERROR_OTHER,
 	.message = "error in Python callback",
@@ -122,9 +179,15 @@ DRGNPY_PUBLIC void *set_drgn_error(struct drgn_error *err)
 	case DRGN_ERROR_LOOKUP:
 		PyErr_SetString(PyExc_LookupError, err->message);
 		break;
-	case DRGN_ERROR_FAULT:
-		PyErr_SetString(FaultError, err->message);
+	case DRGN_ERROR_FAULT: {
+		PyObject *exc;
+
+		exc = PyObject_CallFunction((PyObject *)&FaultError_type, "sK",
+					    err->message, err->address);
+		if (exc)
+			PyErr_SetObject((PyObject *)&FaultError_type, exc);
 		break;
+	}
 	case DRGN_ERROR_TYPE:
 		PyErr_SetString(PyExc_TypeError, err->message);
 		break;

libdrgn/kdump.c
@@ -59,8 +59,9 @@ static struct drgn_error *drgn_read_kdump(void *buf, uint64_t address,
 	ks = kdump_read(ctx, physical ? KDUMP_KPHYSADDR : KDUMP_KVADDR, address,
 			buf, &count);
 	if (ks != KDUMP_OK) {
-		return drgn_error_format(DRGN_ERROR_FAULT, "kdump_read: %s",
-					 kdump_get_err(ctx));
+		return drgn_error_format_fault(address,
+					       "could not read memory from kdump: %s",
+					       kdump_get_err(ctx));
 	}
 	return NULL;
 }

libdrgn/memory_reader.c
@@ -240,9 +240,8 @@ struct drgn_error *drgn_memory_reader_read(struct drgn_memory_reader *reader,
 		segment = drgn_memory_segment_tree_search_le(tree,
 							     &address).entry;
 		if (!segment || segment->address + segment->size <= address) {
-			return drgn_error_format(DRGN_ERROR_FAULT,
-						 "could not find memory segment containing 0x%" PRIx64,
-						 address);
+			return drgn_error_create_fault("could not find memory segment",
+						       address);
 		}
 
 		n = min(segment->address + segment->size - address,
@@ -283,15 +282,14 @@ struct drgn_error *drgn_read_memory_file(void *buf, uint64_t address,
 			if (errno == EINTR) {
 				continue;
 			} else if (errno == EIO && file_segment->eio_is_fault) {
-				return drgn_error_format(DRGN_ERROR_FAULT,
-							 "could not read memory at 0x%" PRIx64,
-							 address);
+				return drgn_error_create_fault("could not read memory",
+							       address);
 			} else {
 				return drgn_error_create_os("pread", errno, NULL);
 			}
 		} else if (ret == 0) {
-			return drgn_error_format(DRGN_ERROR_FAULT,
-						 "short read from memory file");
+			return drgn_error_create_fault("short read from memory file",
+						       address);
 		}
 		p += ret;
 		file_count -= ret;

libdrgn/python/drgnpy.h
@@ -111,6 +111,7 @@ extern PyObject *TypeKind_class;
 extern PyStructSequence_Desc Register_desc;
 extern PyTypeObject DrgnObject_type;
 extern PyTypeObject DrgnType_type;
+extern PyTypeObject FaultError_type;
 extern PyTypeObject ObjectIterator_type;
 extern PyTypeObject Platform_type;
 extern PyTypeObject Program_type;
@@ -118,7 +119,6 @@ extern PyTypeObject Register_type;
 extern PyTypeObject StackFrame_type;
 extern PyTypeObject StackTrace_type;
 extern PyTypeObject Symbol_type;
-extern PyObject *FaultError;
 extern PyObject *MissingDebugInfoError;
 extern PyObject *OutOfBoundsError;
 

libdrgn/python/module.c
@@ -6,7 +6,6 @@
 #include <libkdumpfile/kdumpfile.h>
 #endif
 
-PyObject *FaultError;
 PyObject *MissingDebugInfoError;
 PyObject *OutOfBoundsError;
 
@@ -155,11 +154,11 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	if (add_module_constants(m) == -1)
 		goto err;
 
-	FaultError = PyErr_NewExceptionWithDoc("_drgn.FaultError",
-					       drgn_FaultError_DOC, NULL, NULL);
-	if (!FaultError)
+	FaultError_type.tp_base = (PyTypeObject *)PyExc_Exception;
+	if (PyType_Ready(&FaultError_type) < 0)
 		goto err;
-	PyModule_AddObject(m, "FaultError", FaultError);
+	Py_INCREF(&FaultError_type);
+	PyModule_AddObject(m, "FaultError", (PyObject *)&FaultError_type);
 
 	MissingDebugInfoError = PyErr_NewExceptionWithDoc("_drgn.MissingDebugInfoError",
 							  drgn_MissingDebugInfoError_DOC,

commit 09ee4daed334b9807c0896b47c201d3a6f297508

libdrgn: fix drgn_error_fwrite() for DRGN_ERROR_FAULT
drgn_error_fwrite() only calls string_builder_append_error() to get
special formatting for DRGN_ERROR_OS, but DRGN_ERROR_FAULT also needs
special formatting. Rather than needing to keep drgn_error_fwrite() and
string_builder_append_error() in sync, define them both in terms of a
common macro.

Fixes: 80fef04c704d ("Add address attribute to FaultError exception")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/drgn.h.in
@@ -230,15 +230,11 @@ struct drgn_error *drgn_error_copy(struct drgn_error *src)
 	__attribute__((__returns_nonnull__));
 
 /**
- * Write a @ref drgn_error to a @c stdio stream.
- *
- * For @ref DRGN_ERROR_OS errors, this concatenates @ref drgn_error::message,
- * @ref drgn_error::path, and @c strerror() of @ref drgn_error::errnum.
- * Otherwise, this just writes @c message.
+ * Write a @ref drgn_error followed by a newline to a @c stdio stream.
  *
  * @param[in] file File to write to (usually @c stderr).
  * @param[in] err Error to write.
- * @return Non-negative on success, @c EOF on failure.
+ * @return Non-negative on success, negative on failure.
  */
 int drgn_error_fwrite(FILE *file, struct drgn_error *err);
 

libdrgn/error.c
@@ -203,54 +203,39 @@ LIBDRGN_PUBLIC struct drgn_error *drgn_error_copy(struct drgn_error *src)
 	return dst;
 }
 
+#define emit_error(err) (							\
+	err->code == DRGN_ERROR_OS ?						\
+		/* This is easier than dealing with strerror_r(). */		\
+		(errno = err->errnum,						\
+		 err->path ?							\
+		 emit_error_format("%s: %s: %m", err->message, err->path) :	\
+		 emit_error_format("%s: %m", err->message))			\
+	: err->code == DRGN_ERROR_FAULT ?					\
+		emit_error_format("%s: 0x%" PRIx64, err->message, err->address)	\
+	:									\
+		emit_error_string(err->message)					\
+)
+
 bool string_builder_append_error(struct string_builder *sb,
 				 struct drgn_error *err)
 {
-	bool ret;
-
-	if (err->code == DRGN_ERROR_OS) {
-		/* This is easier than dealing with strerror_r(). */
-		errno = err->errnum;
-		if (err->path) {
-			ret = string_builder_appendf(sb, "%s: %s: %m",
-						     err->message, err->path);
-		} else {
-			ret = string_builder_appendf(sb, "%s: %m",
-						     err->message);
-		}
-	} else if (err->code == DRGN_ERROR_FAULT) {
-		ret = string_builder_appendf(sb, "%s: 0x%" PRIx64,
-					     err->message,
-					     err->address);
-	} else {
-		ret = string_builder_append(sb, err->message);
-	}
-	return ret;
+#define emit_error_format(...) string_builder_appendf(sb, __VA_ARGS__)
+#define emit_error_string(s) string_builder_append(sb, s)
+	return emit_error(err);
+#undef emit_error_string
+#undef emit_error_format
 }
 
 LIBDRGN_PUBLIC int drgn_error_fwrite(FILE *file, struct drgn_error *err)
 {
-	struct string_builder sb = {};
-	char *message;
-	int ret;
-
-	if (err->code == DRGN_ERROR_OS) {
-		if (!string_builder_append_error(&sb, err) ||
-		    !string_builder_finalize(&sb, &message)) {
-			free(sb.str);
-			errno = ENOMEM;
-			return EOF;
-		}
-		ret = fputs(message, file);
-		free(message);
-		if (ret == EOF)
-			return EOF;
-	} else {
-		if (fputs(err->message, file) == EOF)
-			return EOF;
-	}
-	return fputc('\n', file);
+#define emit_error_format(format, ...) fprintf(file, format "\n", __VA_ARGS__)
+#define emit_error_string(s) fprintf(file, "%s\n", s)
+	return emit_error(err);
+#undef emit_error_string
+#undef emit_error_format
 }
+
+#undef emit_error
 
 LIBDRGN_PUBLIC void drgn_error_destroy(struct drgn_error *err)
 {