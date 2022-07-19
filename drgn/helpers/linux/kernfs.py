dd58f3b1ad9c22d0de8ff53cd177471eb13a2e5c
Omar Sandoval <osandov@osandov.com>
2022-07-18 17:09:33 -0700

    drgn.helpers.linux.kernfs: add kernfs_walk()
    This looks up a kernfs node from a path. It will be used to look up
    cgroups by path. This is based on kernfs_walk_ns() from the Linux
    kernel, but it doesn't handle namespaced kernfs nodes yet.
    kernfs_walk_ns() in the kernel is actually built on another function,
    kernfs_find_ns(), but I don't think the latter is very useful as a
    helper.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/kernfs.py
+++ drgn/helpers/linux/kernfs.py
@@ -9,11 +9,15 @@ The ``drgn.helpers.linux.kernfs`` module provides helpers for working with the
 kernfs pseudo filesystem interface in :linux:`include/linux/kernfs.h`.
 """
 
-from drgn import Object
+import os
+
+from drgn import NULL, Object, Path
+from drgn.helpers.linux.rbtree import rbtree_inorder_for_each_entry
 
 __all__ = (
     "kernfs_name",
     "kernfs_path",
+    "kernfs_walk",
 )
 
 
@@ -55,3 +59,30 @@ def kernfs_path(kn: Object) -> bytes:
     names.reverse()
 
     return b"/".join(names)
+
+
+def kernfs_walk(parent: Object, path: Path) -> Object:
+    """
+    Find the kernfs node with the given path from the given parent kernfs node.
+
+    :param parent: ``struct kernfs_node *``
+    :param path: Path name.
+    :return: ``struct kernfs_node *`` (``NULL`` if not found)
+    """
+    kernfs_nodep_type = parent.type_
+    kernfs_node_type = kernfs_nodep_type.type
+    for name in os.fsencode(path).split(b"/"):
+        if not name:
+            continue
+
+        for parent in rbtree_inorder_for_each_entry(
+            kernfs_node_type, parent.dir.children.address_of_(), "rb"
+        ):
+            if (
+                parent.name.string_() == name
+                and not parent.ns  # For now, we don't bother with namespaced kernfs nodes.
+            ):
+                break
+        else:
+            return NULL(parent.prog_, kernfs_nodep_type)
+    return parent
--- tests/linux_kernel/helpers/test_kernfs.py
+++ tests/linux_kernel/helpers/test_kernfs.py
@@ -3,22 +3,47 @@
 
 import os
 
-from drgn import cast
+from drgn import NULL, cast
 from drgn.helpers.linux.fs import fget
-from drgn.helpers.linux.kernfs import kernfs_name, kernfs_path
+from drgn.helpers.linux.kernfs import kernfs_name, kernfs_path, kernfs_walk
 from drgn.helpers.linux.pid import find_task
 from tests.linux_kernel import LinuxKernelTestCase
 
 
 class TestKernfs(LinuxKernelTestCase):
+    @classmethod
+    def kernfs_node_from_fd(cls, fd):
+        file = fget(find_task(cls.prog, os.getpid()), fd)
+        return cast("struct kernfs_node *", file.f_inode.i_private)
+
     def test_kernfs_name(self):
         with open("/sys/kernel/vmcoreinfo", "r") as f:
-            file = fget(find_task(self.prog, os.getpid()), f.fileno())
-            kn = cast("struct kernfs_node *", file.f_inode.i_private)
+            kn = self.kernfs_node_from_fd(f.fileno())
             self.assertEqual(kernfs_name(kn), b"vmcoreinfo")
 
     def test_kernfs_path(self):
         with open("/sys/kernel/vmcoreinfo", "r") as f:
-            file = fget(find_task(self.prog, os.getpid()), f.fileno())
-            kn = cast("struct kernfs_node *", file.f_inode.i_private)
+            kn = self.kernfs_node_from_fd(f.fileno())
             self.assertEqual(kernfs_path(kn), b"/kernel/vmcoreinfo")
+
+    def test_kernfs_walk(self):
+        fds = []
+        try:
+            for path in ("/sys", "/sys/kernel", "/sys/kernel/vmcoreinfo"):
+                fds.append(os.open(path, os.O_RDONLY))
+            kns = [self.kernfs_node_from_fd(fd) for fd in fds]
+            self.assertEqual(kernfs_walk(kns[0], ""), kns[0])
+            self.assertEqual(kernfs_walk(kns[0], "/"), kns[0])
+            self.assertEqual(kernfs_walk(kns[0], "kernel"), kns[1])
+            self.assertEqual(kernfs_walk(kns[0], "kernel/vmcoreinfo"), kns[2])
+            self.assertEqual(
+                kernfs_walk(kns[0], "kernel/foobar"),
+                NULL(self.prog, "struct kernfs_node *"),
+            )
+            self.assertEqual(
+                kernfs_walk(kns[0], "kernel/foo/bar"),
+                NULL(self.prog, "struct kernfs_node *"),
+            )
+        finally:
+            for fd in fds:
+                os.close(fd)
