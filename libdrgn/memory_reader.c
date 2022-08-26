osandov/drgn

commit 9684771d61e4e0d7153af9c4b22a734d85b2c077

libdrgn: Zero fill excluded pages in kernel core dumps rather than FaultError
makedumpfile will exclude zero pages. We found a core file where a
structure straddled a page boundary and the end of the structure
was all zeros so the page was excluded and we were generating a
FaultError trying to access the structure.

This change reverts a portion of that behaviour such that when we are
debugging a kernel core we go back to the zero fill behaviour. To do this
we go back to creating segments based on memsz instead of filesz and
handling the filesz->memsz gap in drgn_read_memory_file.

Fixes: 02912ca7d073 ("libdrgn: fix handling of p_filesz < p_memsz in core dumps")
Signed-off-by: Glen McCready <gkm@mysteryinc.ca>

libdrgn/memory_reader.c
@@ -273,19 +273,20 @@ struct drgn_error *drgn_read_memory_file(void *buf, uint64_t address,
 					 void *arg, bool physical)
 {
 	struct drgn_memory_file_segment *file_segment = arg;
-
-	if (offset > file_segment->file_size ||
-	    count > file_segment->file_size - offset) {
-		if (offset <= file_segment->file_size)
-			address += file_segment->file_size - offset;
-		return drgn_error_create_fault("memory not saved in core dump",
-					       address);
-	}
-
 	uint64_t file_offset = file_segment->file_offset + offset;
+	uint64_t file_end = file_segment->file_offset + file_segment->file_size;
 	char *p = buf;
+
+	if (!file_segment->zerofill && file_offset + count > file_end)
+		return drgn_error_create_fault("memory not saved in core dump",
+					       address + file_segment->file_size);
 	while (count) {
-		ssize_t ret = pread(file_segment->fd, p, count, file_offset);
+		size_t readlen = min(file_end - file_offset, count);
+		ssize_t ret;
+
+		if (readlen == 0)
+			break;
+		ret = pread(file_segment->fd, p, readlen, file_offset);
 		if (ret == -1) {
 			if (errno == EINTR) {
 				continue;
@@ -304,5 +305,7 @@ struct drgn_error *drgn_read_memory_file(void *buf, uint64_t address,
 		count -= ret;
 		file_offset += ret;
 	}
+	/* p_filesz < p_memsz case; end of file reached, zero fill the buffer */
+	memset(p, '\0', count);
 	return NULL;
 }

libdrgn/memory_reader.h
@@ -100,7 +100,8 @@ struct drgn_memory_file_segment {
 	 * Size of the segment in the file. This may be less than the size of
 	 * the segment in memory, which means that the remaining bytes were in
 	 * the program's memory but were not saved in the core dump. Attempting
-	 * to read these bytes is treated as a fault.
+	 * to read these bytes will be zero filled or a fault depending on
+	 * zerofill below.
 	 */
 	uint64_t file_size;
 	/** File descriptor. */
@@ -110,6 +111,11 @@ struct drgn_memory_file_segment {
 	 * OS error.
 	 */
 	bool eio_is_fault;
+	/**
+	 * If @c true, attempts to read between file_size and memory_size
+	 * will be zero filled, otherwise it is treated as a memory fault.
+	 */
+	bool zerofill;
 };
 
 /** @ref drgn_memory_read_fn which reads from a file. */

libdrgn/program.c
@@ -433,14 +433,13 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 		prog->file_segments[j].file_size = phdr->p_filesz;
 		prog->file_segments[j].fd = prog->core_fd;
 		prog->file_segments[j].eio_is_fault = false;
+		/*
+		 * Only zerofill between p_filesz and p_memsz for saved
+		 * kernel cores. makedumpfile excludes zero pages; userland
+		 * and /proc/kcore are not affected.
+		 */
+		prog->file_segments[j].zerofill = vmcoreinfo_note && !is_proc_kcore;
 		err = drgn_program_add_memory_segment(prog, phdr->p_vaddr,
-						      /*
-						       * Don't override the page
-						       * table reader for
-						       * unsaved regions.
-						       */
-						      pgtable_reader ?
-						      phdr->p_filesz :
 						      phdr->p_memsz,
 						      drgn_read_memory_file,
 						      &prog->file_segments[j],
@@ -451,8 +450,6 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 		    phdr->p_paddr != (is_64_bit ? UINT64_MAX : UINT32_MAX)) {
 			err = drgn_program_add_memory_segment(prog,
 							      phdr->p_paddr,
-							      pgtable_reader ?
-							      phdr->p_filesz :
 							      phdr->p_memsz,
 							      drgn_read_memory_file,
 							      &prog->file_segments[j],
@@ -606,6 +603,7 @@ drgn_program_set_pid(struct drgn_program *prog, pid_t pid)
 	prog->file_segments[0].file_size = UINT64_MAX;
 	prog->file_segments[0].fd = prog->core_fd;
 	prog->file_segments[0].eio_is_fault = true;
+	prog->file_segments[0].zerofill = false;
 	err = drgn_program_add_memory_segment(prog, 0, UINT64_MAX,
 					      drgn_read_memory_file,
 					      prog->file_segments, false);

commit 56fda2a0cfd88bd5ff10e257b3b275840fe40524

libdrgn: fix min() warning on 32-bit architectures
The call to min() in drgn_read_memory_file() results in the following
warning on 32-bit architectures that I missed on review:

In file included from ../../libdrgn/memory_reader.c:10:
../../libdrgn/memory_reader.c: In function 'drgn_read_memory_file':
../../libdrgn/minmax.h:36:26: warning: comparison of distinct pointer types lacks a cast
   36 |         (void)(&unique_x == &unique_y);                                         \
      |                          ^~
../../libdrgn/minmax.h:28:19: note: in expansion of macro 'cmp_once_impl'
   28 | #define min(x, y) cmp_once_impl(x, y, PP_UNIQUE(_x), PP_UNIQUE(_y), <)
      |                   ^~~~~~~~~~~~~
../../libdrgn/memory_reader.c:284:34: note: in expansion of macro 'min'
  284 |                 size_t readlen = min(file_end - file_offset, count);
      |                                  ^~~

We can fix it with a cast, and additionally do the call to min() earlier
and rework the logic a bit.

Fixes: 9684771d61e4 ("libdrgn: Zero fill excluded pages in kernel core dumps rather than FaultError")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/memory_reader.c
@@ -273,20 +273,23 @@ struct drgn_error *drgn_read_memory_file(void *buf, uint64_t address,
 					 void *arg, bool physical)
 {
 	struct drgn_memory_file_segment *file_segment = arg;
-	uint64_t file_offset = file_segment->file_offset + offset;
-	uint64_t file_end = file_segment->file_offset + file_segment->file_size;
-	char *p = buf;
-
-	if (!file_segment->zerofill && file_offset + count > file_end)
+	size_t file_count;
+	if (offset < file_segment->file_size) {
+		file_count = min((uint64_t)count,
+				 file_segment->file_size - offset);
+	} else {
+		file_count = 0;
+	}
+	size_t zero_count = count - file_count;
+	if (!file_segment->zerofill && zero_count > 0) {
 		return drgn_error_create_fault("memory not saved in core dump",
-					       address + file_segment->file_size);
-	while (count) {
-		size_t readlen = min(file_end - file_offset, count);
-		ssize_t ret;
+					       address + file_count);
+	}
 
-		if (readlen == 0)
-			break;
-		ret = pread(file_segment->fd, p, readlen, file_offset);
+	uint64_t file_offset = file_segment->file_offset + offset;
+	char *p = buf;
+	while (file_count) {
+		ssize_t ret = pread(file_segment->fd, p, file_count, file_offset);
 		if (ret == -1) {
 			if (errno == EINTR) {
 				continue;
@@ -302,10 +305,9 @@ struct drgn_error *drgn_read_memory_file(void *buf, uint64_t address,
 		}
 		p += ret;
 		address += ret;
-		count -= ret;
+		file_count -= ret;
 		file_offset += ret;
 	}
-	/* p_filesz < p_memsz case; end of file reached, zero fill the buffer */
-	memset(p, '\0', count);
+	memset(p, '\0', zero_count);
 	return NULL;
 }