81dcd81c1a1fd2cf3f9cd4baabb693764992b92f
Omar Sandoval <osandov@osandov.com>
2022-09-13 10:15:09 -0700

    Create drgn.helpers.common package
    This will contain the new modules that Nhat is adding and be the new
    home for some of the stuff currently in the top-level drgn.helpers
    module.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

---
+++ drgn/helpers/common/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+Common
+------
+
+The ``drgn.helpers.common`` package provides helpers that can be used with any
+program. The helpers are available from the individual modules in which they
+are defined and from this top-level package. E.g., the following are both
+valid:
+
+>>> from drgn.helpers.common.memory import identify_address
+>>> from drgn.helpers.common import identify_address
+
+Some of these helpers may have additional program-specific behavior but are
+otherwise generic.
+"""
+
+import importlib
+import pkgutil
+from typing import List
+
+__all__: List[str] = []
+for _module_info in pkgutil.iter_modules(__path__, prefix=__name__ + "."):
+    _submodule = importlib.import_module(_module_info.name)
+    _submodule_all = getattr(_submodule, "__all__", ())
+    __all__.extend(_submodule_all)
+    for _name in _submodule_all:
+        globals()[_name] = getattr(_submodule, _name)
