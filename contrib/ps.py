be38bc9da706ee7820fd54e70230f18df26741a3
Martin Liska <mliska@suse.cz>
2023-01-26 14:56:47 +0100

    enhance ps
    New output:
    
    PID     PPID    CPU  ST COMM
    1       0       4    S  systemd
    2       0       13   S  [kthreadd]
    3       2       0    I  [rcu_gp]
    4       2       0    I  [rcu_par_gp]
    5       2       0    I  [slub_flushwq]
    6       2       0    I  [netns]
    8       2       0    I  [kworker/0:0H]
    10      2       0    I  [mm_percpu_wq]
    11      2       0    I  [rcu_tasks_kthre]
    ...
    
    Signed-off-by: Martin Liska <mliska@suse.cz>
    Co-authored-by: Omar Sandoval <osandov@osandov.com>

--- contrib/ps.py
+++ contrib/ps.py
@@ -5,9 +5,28 @@
 """A simplified implementation of ps(1) using drgn"""
 
 from drgn.helpers.linux.pid import for_each_task
+from drgn.helpers.linux.sched import task_cpu, task_state_to_char
 
-print("PID        COMM")
+
+def is_kthread(task):
+    """
+    Make a guess if task_struct is a kernel thread.
+    """
+
+    return not task.mm
+
+
+print("PID     PPID    CPU  ST COMM")
 for task in for_each_task(prog):
     pid = task.pid.value_()
+    ppid = task.parent.pid.value_() if task.parent else 0
+
     comm = task.comm.string_().decode()
-    print(f"{pid:<10} {comm}")
+    # Distinguish kernel and user-space threads
+    if is_kthread(task):
+        comm = f"[{comm}]"
+
+    cpu = task_cpu(task)
+    state = task_state_to_char(task)
+
+    print(f"{pid:<7} {ppid:<7} {cpu:<4} {state}  {comm}")
