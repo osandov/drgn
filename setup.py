osandov/drgn

commit 2e9e66cedcb74eaff05e5992b0130d10efdbb21c

setup.py: use stricter check for git repository
To test builds on other architectures, I'm setting up a Debian chroot
with debootstrap and then building with something like:

  mount --bind . "$chroot/mnt"
  chroot "$chroot" sh -c 'cd /mnt && python3 setup.py build_ext -i'

However, if I do this from a Git worktree, this fails with an error from
the git call to determine the local version:

  fatal: not a git repository: /home/osandov/repos/drgn/.git/worktrees/drgn2

Instead of only checking whether .git exists, use `git --git-dir=.git
rev-parse` to verify that .git is valid. (This will of course rely on
drgn/internal/version.py already having been generated.)

Signed-off-by: Omar Sandoval <osandov@osandov.com>

setup.py
@@ -339,7 +339,12 @@ def get_version():
 
     # If this is a git repository, use a git-describe(1)-esque local version.
     # Otherwise, get the local version saved in the sdist.
-    if os.path.exists(".git"):
+    if (
+        subprocess.call(
+            ["git", "--git-dir=.git", "rev-parse"], stderr=subprocess.DEVNULL
+        )
+        == 0
+    ):
         # Read the Docs modifies the working tree (namely, docs/conf.py). We
         # don't want the documentation to display a dirty version, so ignore
         # modifications for RTD builds.

commit 38b090ea9d4427c75cde168522714772faae0e03

setup.py: handle git not being installed
My previous change to call git unconditionally caused Packit failures,
presumably because git isn't installed for the build:

Traceback (most recent call last):
  File "/builddir/build/BUILD/drgn-0.0.22+71.g2f97cc0/setup.py", line 411, in <module>
    version=get_version(),
            ^^^^^^^^^^^^^
  File "/builddir/build/BUILD/drgn-0.0.22+71.g2f97cc0/setup.py", line 343, in get_version
    subprocess.call(
  File "/usr/lib64/python3.11/subprocess.py", line 389, in call
    with Popen(*popenargs, **kwargs) as p:
         ^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/lib64/python3.11/subprocess.py", line 1024, in __init__
    self._execute_child(args, executable, preexec_fn, close_fds,
  File "/usr/lib64/python3.11/subprocess.py", line 1901, in _execute_child
    raise child_exception_type(errno_num, err_msg, err_filename)
FileNotFoundError: [Errno 2] No such file or directory: 'git'

We shouldn't bother calling git if .git doesn't exist.

Fixes: 2e9e66cedcb7 ("setup.py: use stricter check for git repository")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

setup.py
@@ -339,7 +339,7 @@ def get_version():
 
     # If this is a git repository, use a git-describe(1)-esque local version.
     # Otherwise, get the local version saved in the sdist.
-    if (
+    if os.path.exists(".git") and (
         subprocess.call(
             ["git", "--git-dir=.git", "rev-parse"], stderr=subprocess.DEVNULL
         )