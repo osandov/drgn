d5c53099d2e59479ae7fd283d64d2543554894d7
Omar Sandoval <osandov@osandov.com>
2023-06-29 15:21:50 -0700

    vmtest.enter_kdump: use kexec(8) on non-x86-64 architectures
    We can trivially call the kexec_file_load() syscall on x86-64, but it's
    more complicated for other architectures. We can call the kexec utility
    for those.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- vmtest/enter_kdump.py
+++ vmtest/enter_kdump.py
@@ -4,7 +4,7 @@
 import ctypes
 import os
 import re
-import sys
+import subprocess
 
 from util import NORMALIZED_MACHINE_NAME, SYS
 
@@ -13,11 +13,6 @@ KEXEC_FILE_NO_INITRAMFS = 4
 
 
 def main():
-    # Some architectures don't implement kexec_file_load. Note that even if the
-    # number is defined, it may not be implemented.
-    if "kexec_file_load" not in SYS:
-        sys.exit(f"kexec_file_load is not defined on {NORMALIZED_MACHINE_NAME}")
-
     with open("/proc/cmdline", "rb") as f:
         cmdline = f.read().rstrip(b"\n")
         cmdline = re.sub(rb"(^|\s)crashkernel=\S+", b"", cmdline)
@@ -31,20 +26,34 @@ def main():
 
     vmlinuz = f"/lib/modules/{os.uname().release}/vmlinuz"
 
-    syscall = ctypes.CDLL(None, use_errno=True).syscall
-    syscall.restype = ctypes.c_long
+    # On x86-64, kexec_file_load() is supported on all kernel versions we care
+    # about, and it's simple enough to call ourselves. On other architectures,
+    # we just use kexec(8).
+    if NORMALIZED_MACHINE_NAME == "x86_64":
+        syscall = ctypes.CDLL(None, use_errno=True).syscall
+        syscall.restype = ctypes.c_long
 
-    with open(vmlinuz, "rb") as kernel:
-        if syscall(
-            ctypes.c_long(SYS["kexec_file_load"]),
-            ctypes.c_int(kernel.fileno()),
-            ctypes.c_int(-1),
-            ctypes.c_ulong(len(cmdline) + 1),
-            ctypes.c_char_p(cmdline + b"\0"),
-            ctypes.c_ulong(KEXEC_FILE_ON_CRASH | KEXEC_FILE_NO_INITRAMFS),
-        ):
-            errno = ctypes.get_errno()
-            raise OSError(errno, os.strerror(errno))
+        with open(vmlinuz, "rb") as kernel:
+            if syscall(
+                ctypes.c_long(SYS["kexec_file_load"]),
+                ctypes.c_int(kernel.fileno()),
+                ctypes.c_int(-1),
+                ctypes.c_ulong(len(cmdline) + 1),
+                ctypes.c_char_p(cmdline + b"\0"),
+                ctypes.c_ulong(KEXEC_FILE_ON_CRASH | KEXEC_FILE_NO_INITRAMFS),
+            ):
+                errno = ctypes.get_errno()
+                raise OSError(errno, os.strerror(errno))
+    else:
+        subprocess.check_call(
+            [
+                "kexec",
+                "--load-panic",
+                "--kexec-syscall-auto",
+                "--command-line=" + cmdline.decode(),
+                vmlinuz,
+            ]
+        )
 
     with open("/proc/self/comm", "w") as f:
         f.write("selfdestruct")
