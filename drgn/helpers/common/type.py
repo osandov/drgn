58013a9125bf87ae1492048ac8022d1a7c2325ee
Stephen Brennan <stephen.s.brennan@oracle.com>
2023-08-07 16:15:01 -0700

    Add example type checking annotation
    This is a demo that adds a type checker. For now it only supports types
    "Object" and "Optional[Object]", though it could be extended to some
    simple types like Tuples, Lists, Dicts, etc which contain Objects.
    
    The idea is that you can set DRGN_TYPE_CHECK=1, and any function with
    the given annotation is type checked at runtime. There's no runtime cost
    when the annotation is disabled, so it's safe to leave in your code.
    
    The key trade-off made here is that we expect the C type annotation to
    be in the docstring. This is simply because my code convention is that
    every docstring for an Object needs a C type description, inside the
    double-backtick for readability on Sphinx docs. It's quite simply my
    preference. There is another option, typing.Annotated, which allows you
    to include additional metadata for type checks. This would allow the C
    type information to live with the Python types, but the downside to that
    is that it probably wouldn't look nice in Sphinx (if it gets displayed
    at all). It doesn't seem hard to implement support for that instead.
    
    The actual C type checks are just string comparison for type_name(). I
    suppose I should at least tokenize it so that it's not whitespace
    sensitive, but that hasn't been an issue in my testing.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- drgn/helpers/common/type.py
+++ drgn/helpers/common/type.py
@@ -10,12 +10,18 @@ with types in ways that aren't provided by the core drgn library.
 """
 
 import enum
+import inspect
+import os
+import re
 import typing
-from typing import Container
+from typing import Any, Callable, Container, Dict, Optional, TypeVar, Union
 
-from drgn import Type
+from drgn import Object, Type
 
 __all__ = ("enum_type_to_class",)
+
+
+F = TypeVar("F", bound=Callable[..., Any])
 
 
 def enum_type_to_class(
@@ -38,3 +44,170 @@ def enum_type_to_class(
         if name not in exclude
     ]
     return enum.IntEnum(name, enumerators)  # type: ignore  # python/mypy#4865
+
+
+def _parse_docstring_c_types(doc: str) -> Dict[str, str]:
+    current_param = ""
+    params = {}
+    expr = re.compile(r":(param (\w+)|returns?|retval):.*``([^`]+)``.*")
+    # expr = re.compile(r":param (\w+): (.*)")
+
+    def add_param() -> None:
+        m = expr.fullmatch(current_param)
+        if m:
+            if m.group(1).startswith("param"):
+                params[m.group(2)] = m.group(3)
+            else:
+                params["return"] = m.group(3)
+
+    for line in doc.splitlines():
+        line = line.strip()
+        # End the previous param with a new one, or blank line
+        if (not line or line.startswith(":")) and current_param:
+            add_param()
+            current_param = ""
+        # Continue loading param if we already started one, or at declaration
+        if current_param:
+            current_param = "{current_param} {line}"
+        elif line.startswith(":"):
+            current_param = line
+    if current_param:
+        add_param()
+    return params
+
+
+def _check_simple_type(
+    expected: Optional[str],
+    strict: bool,
+    name: str,
+    fname: str,
+    required: bool,
+) -> Optional[Callable[[Any], None]]:
+    if not expected and required:
+        raise ValueError(
+            f"{fname}: Drgn Type Checking: parameter {name} has a "
+            "Python type annotation, but is missing the corresponding "
+            "C type annotation in the docstring."
+        )
+    elif not expected:
+        return None
+
+    if os.getenv("DRGN_TYPE_CHECK") == "verbose":
+        print(f"{fname}: {name}: simple: {expected}")
+
+    def _check(value: Any) -> None:
+        if os.getenv("DRGN_TYPE_CHECK") == "verbose":
+            print(f"{fname}: {name}: checking")
+        if not isinstance(value, Object):
+            if strict:
+                raise TypeError(
+                    f"{fname}: Drgn Type Error: {name}:\n"
+                    "expected an Object of type "
+                    f" {expected}, but instead got Python value of "
+                    f" type {type(value)}"
+                )
+            else:
+                return
+
+        # At this point, we have a drgn.Object, so do the "type checking" by
+        # string comparison.
+        drgn_type = value.type_.type_name()
+        if drgn_type != expected:
+            raise TypeError(
+                f"{fname}: Drgn Type Error: {name}:\n"
+                f"expected an Object of type '{expected}',"
+                f" but the argument has type '{drgn_type}'"
+            )
+
+    return _check
+
+
+def _create_checkers(
+    sig: inspect.Signature, doc: str, fname: str, strict: bool
+) -> Dict[str, Callable[[Any], None]]:
+    c_types = _parse_docstring_c_types(doc)
+    if not c_types:
+        raise ValueError(
+            f"Requested type checking on {fname}, but there are no"
+            " C types in the docstring"
+        )
+    checkers = {}
+    for name, param in sig.parameters.items():
+        c_type = c_types[name]
+        annot = param.annotation
+        origin = typing.get_origin(annot)
+        args = typing.get_args(annot)
+        checker = None
+        if annot is Object:
+            checker = _check_simple_type(c_type, True, name, fname, strict)
+        elif origin is Union and Object in args:
+            checker = _check_simple_type(c_type, False, name, fname, strict)
+        elif origin is list and args[0] is Object:
+            pass
+        elif origin is dict:
+            pass
+        elif origin is tuple:
+            pass
+        if checker:
+            checkers[name] = checker
+    return checkers
+
+
+def drgn_type_check(func: F) -> F:
+    """
+    Annotation which performs runtime type checking on Drgn Objects
+
+    Use this function annotation, along with a well-formatted docstring
+    containing C types, to check the types of Object arguments and return
+    values. The docstring should contain lines something like this:
+
+    .. code::
+
+        :param task: object of type ``struct task_struct *``
+        :return: current CPU number as an ``int``
+
+    This is the reStructuredText standard docstring format. It is important for
+    parameter names to be surrounded by colons, as shown, and for the C type
+    information to be enclosed in double-backticks. Parameter documentation may
+    span multiple lines, and it is terminated by an empty line, a new line which
+    starts with a colon, or the end of the docstring.
+
+    The type checker will be activated when the environment variable
+    ``DRGN_TYPE_CHECK`` is set to any value. When the type checker is inactive,
+    it has no overhead. The type checker should not be enabled in production,
+    but would be useful during interactive debugging, development, and testing.
+
+    When activated, the type checker will examine three sources of information:
+
+    1. The argument passed to the function
+    2. The type annotation for the argument
+    3. The C type string contained in the documentation
+
+    Currently, the type checker can only handle simple parameters of type
+    "Object", or Unions containing the type "Object", such as
+    ``Optional[Object]``. The actual type check is done by comparing C type name
+    against the string type name included in the docstring. This, of course,
+    leaves room for ambiguity: for example, the type name may be
+    """
+    if not os.getenv("DRGN_TYPE_CHECK"):
+        return func
+    doc = func.__doc__
+    if not doc:
+        raise ValueError(
+            f"Cannot type check function {func.__name__} without docstring"
+        )
+    sig = inspect.signature(func)
+    checkers = _create_checkers(sig, doc, func.__name__, False)
+    ret_checker = checkers.pop("return", None)
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        bound = sig.bind(*args, **kwargs)
+        bound.apply_defaults()
+        for name, checker in checkers.items():
+            checker(bound.arguments[name])
+        retval = func(*args, **kwargs)
+        if ret_checker:
+            ret_checker(retval)
+        return retval
+
+    return wrapper  # type: ignore
