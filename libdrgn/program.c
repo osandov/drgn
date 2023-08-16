osandov/drgn

commit 5591d199b189ddc9838db9c751dea40e677dfc4a

libdrgn: debug_info: split DWARF support into its own file
Continuing the refactoring from the previous commit, move the DWARF code
from debug_info.c to its own file, leaving only the generic ELF file
management in debug_info.c

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -3,7 +3,6 @@
 
 #include <assert.h>
 #include <byteswap.h>
-#include <dwarf.h>
 #include <elf.h>
 #include <elfutils/libdw.h>
 #include <elfutils/version.h>
@@ -17,9 +16,7 @@
 #include <sys/statfs.h>
 #include <unistd.h>
 
-#include "array.h"
 #include "debug_info.h"
-#include "dwarf_index.h"
 #include "error.h"
 #include "language.h"
 #include "linux_kernel.h"
@@ -551,38 +548,18 @@ out_fd:
 }
 
 /* Set the default language from the language of "main". */
-static void drgn_program_set_language_from_main(struct drgn_debug_info *dbinfo)
+static void drgn_program_set_language_from_main(struct drgn_program *prog)
 {
 	struct drgn_error *err;
-	struct drgn_dwarf_index_iterator it;
-	static const uint64_t tags[] = { DW_TAG_subprogram };
-	err = drgn_dwarf_index_iterator_init(&it, &dbinfo->dindex.global,
-					     "main", strlen("main"), tags,
-					     array_size(tags));
-	if (err) {
-		drgn_error_destroy(err);
-		return;
-	}
-	struct drgn_dwarf_index_die *index_die;
-	while ((index_die = drgn_dwarf_index_iterator_next(&it))) {
-		Dwarf_Die die;
-		err = drgn_dwarf_index_get_die(index_die, &die);
-		if (err) {
-			drgn_error_destroy(err);
-			continue;
-		}
 
-		const struct drgn_language *lang;
-		err = drgn_language_from_die(&die, false, &lang);
-		if (err) {
-			drgn_error_destroy(err);
-			continue;
-		}
-		if (lang) {
-			dbinfo->prog->lang = lang;
-			break;
-		}
-	}
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
+		return;
+	const struct drgn_language *lang;
+	err = drgn_debug_info_main_language(prog->dbinfo, &lang);
+	if (err)
+		drgn_error_destroy(err);
+	if (lang)
+		prog->lang = lang;
 }
 
 static int drgn_set_platform_from_dwarf(Dwfl_Module *module, void **userdatap,
@@ -639,9 +616,8 @@ drgn_program_load_debug_info(struct drgn_program *prog, const char **paths,
 
 	err = drgn_debug_info_load(dbinfo, paths, n, load_default, load_main);
 	if ((!err || err->code == DRGN_ERROR_MISSING_DEBUG_INFO)) {
-		if (!prog->lang &&
-		    !(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL))
-			drgn_program_set_language_from_main(dbinfo);
+		if (!prog->lang)
+			drgn_program_set_language_from_main(prog);
 		if (!prog->has_platform) {
 			dwfl_getdwarf(dbinfo->dwfl,
 				      drgn_set_platform_from_dwarf, prog, 0);

libdrgn/Makefile.am
@@ -37,6 +37,8 @@ libdrgnimpl_la_SOURCES = $(ARCH_DEFS:.defs=.c) \
 			 debug_info.h \
 			 dwarf_index.c \
 			 dwarf_index.h \
+			 dwarf_info.c \
+			 dwarf_info.h \
 			 error.c \
 			 error.h \
 			 hash_table.c \

libdrgn/debug_info.h
@@ -12,21 +12,18 @@
 #ifndef DRGN_DEBUG_INFO_H
 #define DRGN_DEBUG_INFO_H
 
-#include <elfutils/libdw.h>
 #include <elfutils/libdwfl.h>
 #include <libelf.h>
 
 #include "binary_buffer.h"
 #include "cfi.h"
 #include "drgn.h"
-#include "dwarf_index.h"
+#include "dwarf_info.h"
 #include "hash_table.h"
 #include "orc_info.h"
 #include "platform.h"
 #include "string_builder.h"
 #include "vector.h"
-
-struct drgn_register_state;
 
 /**
  * @ingroup Internals
@@ -81,15 +78,6 @@ enum drgn_debug_info_scn {
 	DRGN_NUM_DEBUG_SCNS,
 };
 
-struct drgn_dwarf_fde {
-	uint64_t initial_location;
-	uint64_t address_range;
-	/* CIE for this FDE as an index into drgn_debug_info_module::cies. */
-	size_t cie;
-	const char *instructions;
-	size_t instructions_size;
-};
-
 /**
  * A module reported to a @ref drgn_debug_info.
  *
@@ -114,21 +102,8 @@ struct drgn_debug_info_module {
 	Elf_Scn *scns[DRGN_NUM_DEBUG_SCNS];
 	Elf_Data *scn_data[DRGN_NUM_DEBUG_SCN_DATA];
 
-	/** Base for `DW_EH_PE_pcrel`. */
-	uint64_t pcrel_base;
-	/** Base for `DW_EH_PE_textrel`. */
-	uint64_t textrel_base;
-	/** Base for `DW_EH_PE_datarel`. */
-	uint64_t datarel_base;
-	/** Array of DWARF Common Information Entries. */
-	struct drgn_dwarf_cie *cies;
-	/**
-	 * Array of DWARF Frame Description Entries sorted by initial_location.
-	 */
-	struct drgn_dwarf_fde *fdes;
-	/** Number of elements in @ref drgn_debug_info_module::fdes. */
-	size_t num_fdes;
-
+	/** DWARF debugging information. */
+	struct drgn_dwarf_module_info dwarf;
 	/** ORC unwinder information. */
 	struct drgn_orc_module_info orc;
 
@@ -191,62 +166,10 @@ drgn_debug_info_buffer_init(struct drgn_debug_info_buffer *buffer,
 	buffer->scn = scn;
 }
 
-/**
- * Find the DWARF DIEs in a @ref drgn_debug_info_module for the scope containing
- * a given program counter.
- *
- * @param[in] module Module containing @p pc.
- * @param[in] pc Program counter.
- * @param[out] bias_ret Returned difference between addresses in the loaded
- * module and addresses in the returned DIEs.
- * @param[out] dies_ret Returned DIEs. `(*dies_ret)[*length_ret - 1]` is the
- * innermost DIE containing @p pc, `(*dies_ret)[*length_ret - 2]` is its parent
- * (which may not contain @p pc itself), `(*dies_ret)[*length_ret - 3]` is its
- * grandparent, etc. Must be freed with @c free().
- * @param[out] length_ret Returned length of @p dies_ret.
- */
-struct drgn_error *
-drgn_debug_info_module_find_dwarf_scopes(struct drgn_debug_info_module *module,
-					 uint64_t pc, uint64_t *bias_ret,
-					 Dwarf_Die **dies_ret,
-					 size_t *length_ret)
-	__attribute__((__nonnull__(1, 3, 4, 5)));
-
-/**
- * Find the ancestors of a DWARF DIE.
- *
- * This finds the parent, grandparent, etc., of a DWARF DIE in the tree of DIEs.
- *
- * @param[in] module Module containing @p die.
- * @param[in] die DIE to find.
- * @param[out] dies_ret Returned DIEs. `(*dies_ret)[*length_ret]` is the DIE,
- * `(*dies_ret)[*length_ret - 1]` is its parent, `(*dies_ret)[*length_ret - 2]`
- * is its grandparent, etc., and `(*dies_ret)[0]` is the top-level unit DIE.
- * @param[out] length_ret Returned number of ancestors in @p dies_ret.
- */
-struct drgn_error *drgn_find_die_ancestors(Dwarf_Die *die, Dwarf_Die **dies_ret,
-					  size_t *length_ret)
-	__attribute__((__nonnull__(2, 3)));
-
 DEFINE_HASH_TABLE_TYPE(drgn_debug_info_module_table,
 		       struct drgn_debug_info_module *)
 
 DEFINE_HASH_SET_TYPE(c_string_set, const char *)
-
-/** Cached type in a @ref drgn_debug_info. */
-struct drgn_dwarf_type {
-	struct drgn_type *type;
-	enum drgn_qualifiers qualifiers;
-	/**
-	 * Whether this is an incomplete array type or a typedef of one.
-	 *
-	 * This is used to work around a GCC bug; see @ref
-	 * drgn_type_from_dwarf_internal().
-	 */
-	bool is_incomplete_array;
-};
-
-DEFINE_HASH_MAP_TYPE(drgn_dwarf_type_map, const void *, struct drgn_dwarf_type);
 
 /** Cache of debugging information. */
 struct drgn_debug_info {
@@ -264,25 +187,8 @@ struct drgn_debug_info {
 	 * they should not be freed.
 	 */
 	struct c_string_set module_names;
-	/** Index of DWARF debugging information. */
-	struct drgn_dwarf_index dindex;
-
-	/**
-	 * Cache of parsed types.
-	 *
-	 * The key is the address of the DIE (@c Dwarf_Die::addr). The value is
-	 * a @ref drgn_dwarf_type.
-	 */
-	struct drgn_dwarf_type_map types;
-	/**
-	 * Cache of parsed types which appear to be incomplete array types but
-	 * can't be.
-	 *
-	 * See @ref drgn_type_from_dwarf_internal().
-	 */
-	struct drgn_dwarf_type_map cant_be_incomplete_array_types;
-	/** Current parsing recursion depth. */
-	int depth;
+	/** DWARF debugging information. */
+	struct drgn_dwarf_info dwarf;
 };
 
 /** Create a @ref drgn_debug_info. */
@@ -376,6 +282,14 @@ struct drgn_error *drgn_debug_info_load(struct drgn_debug_info *dbinfo,
 bool drgn_debug_info_is_indexed(struct drgn_debug_info *dbinfo,
 				const char *name);
 
+/**
+ * Get the language of the program's `main` function or `NULL` if it could not
+ * be found.
+ */
+struct drgn_error *
+drgn_debug_info_main_language(struct drgn_debug_info *dbinfo,
+			      const struct drgn_language **ret);
+
 /** @ref drgn_type_find_fn() that uses debugging information. */
 struct drgn_error *drgn_debug_info_find_type(enum drgn_type_kind kind,
 					     const char *name, size_t name_len,
@@ -388,44 +302,6 @@ drgn_debug_info_find_object(const char *name, size_t name_len,
 			    const char *filename,
 			    enum drgn_find_object_flags flags, void *arg,
 			    struct drgn_object *ret);
-
-/**
- * Find an object DIE in an array of DWARF scopes.
- *
- * @param[in] scopes Array of scopes, from outermost to innermost.
- * @param[in] num_scopes Number of scopes in @p scopes.
- * @param[out] die_ret Returned object DIE.
- * @param[out] type_ret If @p die_ret is a `DW_TAG_enumerator` DIE, its parent.
- * Otherwise, undefined.
- */
-struct drgn_error *drgn_find_in_dwarf_scopes(Dwarf_Die *scopes,
-					     size_t num_scopes,
-					     const char *name,
-					     Dwarf_Die *die_ret,
-					     Dwarf_Die *type_ret);
-
-/**
- * Create a @ref drgn_object from a `Dwarf_Die`.
- *
- * @param[in] die Object DIE (e.g., `DW_TAG_subprogram`, `DW_TAG_variable`,
- * `DW_TAG_formal_parameter`, `DW_TAG_enumerator`,
- * `DW_TAG_template_value_parameter`).
- * @param[in] type_die DIE of object's type. If @c NULL, use the `DW_AT_type`
- * attribute of @p die. If @p die is a `DW_TAG_enumerator` DIE, this should be
- * its parent.
- * @param[in] function_die DIE of current function. @c NULL if not in function
- * context.
- * @param[in] regs Registers of current stack frame. @c NULL if not in stack
- * frame context.
- * @param[out] ret Returned object.
- */
-struct drgn_error *
-drgn_object_from_dwarf(struct drgn_debug_info *dbinfo,
-		       struct drgn_debug_info_module *module,
-		       Dwarf_Die *die, Dwarf_Die *type_die,
-		       Dwarf_Die *function_die,
-		       const struct drgn_register_state *regs,
-		       struct drgn_object *ret);
 
 /**
  * Get the Call Frame Information in a @ref drgn_debug_info_module at a given
@@ -445,12 +321,6 @@ drgn_debug_info_module_find_cfi(struct drgn_program *prog,
 				uint64_t pc, struct drgn_cfi_row **row_ret,
 				bool *interrupted_ret,
 				drgn_register_number *ret_addr_regno_ret);
-
-struct drgn_error *
-drgn_eval_cfi_dwarf_expression(struct drgn_program *prog,
-			       const struct drgn_cfi_rule *rule,
-			       const struct drgn_register_state *regs,
-			       void *buf, size_t size);
 
 struct drgn_error *open_elf_file(const char *path, int *fd_ret, Elf **elf_ret);
 

libdrgn/dwarf_info.h
@@ -1,0 +1,194 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/**
+ * @file
+ *
+ * DWARF and .eh_frame support.
+ *
+ * See @ref DebugInfo.
+ */
+
+#ifndef DRGN_DEBUG_INFO_DWARF_H
+#define DRGN_DEBUG_INFO_DWARF_H
+
+/**
+ * @ingroup DebugInfo
+ *
+ * @{
+ */
+
+#include <elfutils/libdw.h>
+
+#include "cfi.h"
+#include "drgn.h"
+#include "dwarf_index.h"
+#include "hash_table.h"
+
+struct drgn_debug_info;
+struct drgn_debug_info_module;
+struct drgn_register_state;
+
+/** DWARF Frame Description Entry. */
+struct drgn_dwarf_fde {
+	uint64_t initial_location;
+	uint64_t address_range;
+	/* CIE for this FDE as an index into drgn_debug_info_module::cies. */
+	size_t cie;
+	const char *instructions;
+	size_t instructions_size;
+};
+
+/** DWARF debugging information for a @ref drgn_debug_info_module. */
+struct drgn_dwarf_module_info {
+	/** Base for `DW_EH_PE_pcrel`. */
+	uint64_t pcrel_base;
+	/** Base for `DW_EH_PE_textrel`. */
+	uint64_t textrel_base;
+	/** Base for `DW_EH_PE_datarel`. */
+	uint64_t datarel_base;
+	/** Array of DWARF Common Information Entries. */
+	struct drgn_dwarf_cie *cies;
+	/**
+	 * Array of DWARF Frame Description Entries sorted by initial_location.
+	 */
+	struct drgn_dwarf_fde *fdes;
+	/** Number of elements in @ref drgn_debug_info_module::fdes. */
+	size_t num_fdes;
+};
+
+void drgn_dwarf_module_info_deinit(struct drgn_debug_info_module *module);
+
+/** Cached type in a @ref drgn_debug_info. */
+struct drgn_dwarf_type {
+	struct drgn_type *type;
+	enum drgn_qualifiers qualifiers;
+	/**
+	 * Whether this is an incomplete array type or a typedef of one.
+	 *
+	 * This is used to work around a GCC bug; see @ref
+	 * drgn_type_from_dwarf_internal().
+	 */
+	bool is_incomplete_array;
+};
+
+DEFINE_HASH_MAP_TYPE(drgn_dwarf_type_map, const void *, struct drgn_dwarf_type)
+
+/** DWARF debugging information for a program/@ref drgn_debug_info. */
+struct drgn_dwarf_info {
+	/** Index of DWARF debugging information. */
+	struct drgn_dwarf_index index;
+
+	/**
+	 * Cache of parsed types.
+	 *
+	 * The key is the address of the DIE (@c Dwarf_Die::addr). The value is
+	 * a @ref drgn_dwarf_type.
+	 */
+	struct drgn_dwarf_type_map types;
+	/**
+	 * Cache of parsed types which appear to be incomplete array types but
+	 * can't be.
+	 *
+	 * See @ref drgn_type_from_dwarf_internal().
+	 */
+	struct drgn_dwarf_type_map cant_be_incomplete_array_types;
+
+	/** Current parsing recursion depth. */
+	int depth;
+};
+
+void drgn_dwarf_info_init(struct drgn_debug_info *dbinfo);
+void drgn_dwarf_info_deinit(struct drgn_debug_info *dbinfo);
+
+/**
+ * Find the DWARF DIEs in a @ref drgn_debug_info_module for the scope containing
+ * a given program counter.
+ *
+ * @param[in] module Module containing @p pc.
+ * @param[in] pc Program counter.
+ * @param[out] bias_ret Returned difference between addresses in the loaded
+ * module and addresses in the returned DIEs.
+ * @param[out] dies_ret Returned DIEs. `(*dies_ret)[*length_ret - 1]` is the
+ * innermost DIE containing @p pc, `(*dies_ret)[*length_ret - 2]` is its parent
+ * (which may not contain @p pc itself), `(*dies_ret)[*length_ret - 3]` is its
+ * grandparent, etc. Must be freed with @c free().
+ * @param[out] length_ret Returned length of @p dies_ret.
+ */
+struct drgn_error *
+drgn_debug_info_module_find_dwarf_scopes(struct drgn_debug_info_module *module,
+					 uint64_t pc, uint64_t *bias_ret,
+					 Dwarf_Die **dies_ret,
+					 size_t *length_ret)
+	__attribute__((__nonnull__(1, 3, 4, 5)));
+
+/**
+ * Find the ancestors of a DWARF DIE.
+ *
+ * This finds the parent, grandparent, etc., of a DWARF DIE in the tree of DIEs.
+ *
+ * @param[in] module Module containing @p die.
+ * @param[in] die DIE to find.
+ * @param[out] dies_ret Returned DIEs. `(*dies_ret)[*length_ret]` is the DIE,
+ * `(*dies_ret)[*length_ret - 1]` is its parent, `(*dies_ret)[*length_ret - 2]`
+ * is its grandparent, etc., and `(*dies_ret)[0]` is the top-level unit DIE.
+ * @param[out] length_ret Returned number of ancestors in @p dies_ret.
+ */
+struct drgn_error *drgn_find_die_ancestors(Dwarf_Die *die, Dwarf_Die **dies_ret,
+					  size_t *length_ret)
+	__attribute__((__nonnull__(2, 3)));
+
+/**
+ * Find an object DIE in an array of DWARF scopes.
+ *
+ * @param[in] scopes Array of scopes, from outermost to innermost.
+ * @param[in] num_scopes Number of scopes in @p scopes.
+ * @param[out] die_ret Returned object DIE.
+ * @param[out] type_ret If @p die_ret is a `DW_TAG_enumerator` DIE, its parent.
+ * Otherwise, undefined.
+ */
+struct drgn_error *drgn_find_in_dwarf_scopes(Dwarf_Die *scopes,
+					     size_t num_scopes,
+					     const char *name,
+					     Dwarf_Die *die_ret,
+					     Dwarf_Die *type_ret);
+
+/**
+ * Create a @ref drgn_object from a `Dwarf_Die`.
+ *
+ * @param[in] die Object DIE (e.g., `DW_TAG_subprogram`, `DW_TAG_variable`,
+ * `DW_TAG_formal_parameter`, `DW_TAG_enumerator`,
+ * `DW_TAG_template_value_parameter`).
+ * @param[in] type_die DIE of object's type. If @c NULL, use the `DW_AT_type`
+ * attribute of @p die. If @p die is a `DW_TAG_enumerator` DIE, this should be
+ * its parent.
+ * @param[in] function_die DIE of current function. @c NULL if not in function
+ * context.
+ * @param[in] regs Registers of current stack frame. @c NULL if not in stack
+ * frame context.
+ * @param[out] ret Returned object.
+ */
+struct drgn_error *
+drgn_object_from_dwarf(struct drgn_debug_info *dbinfo,
+		       struct drgn_debug_info_module *module,
+		       Dwarf_Die *die, Dwarf_Die *type_die,
+		       Dwarf_Die *function_die,
+		       const struct drgn_register_state *regs,
+		       struct drgn_object *ret);
+
+struct drgn_error *
+drgn_debug_info_find_dwarf_cfi(struct drgn_debug_info_module *module,
+			       uint64_t unbiased_pc,
+			       struct drgn_cfi_row **row_ret,
+			       bool *interrupted_ret,
+			       drgn_register_number *ret_addr_regno_ret);
+
+struct drgn_error *
+drgn_eval_cfi_dwarf_expression(struct drgn_program *prog,
+			       const struct drgn_cfi_rule *rule,
+			       const struct drgn_register_state *regs,
+			       void *buf, size_t size);
+
+/** @} */
+
+#endif /* DRGN_DEBUG_INFO_DWARF_H */

libdrgn/language.c
@@ -1,9 +1,6 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <dwarf.h>
-
-#include "error.h"
 #include "language.h"
 
 const struct drgn_language drgn_languages[] = {
@@ -62,29 +59,3 @@ const struct drgn_language drgn_languages[] = {
 		.op_not = c_op_not,
 	},
 };
-
-struct drgn_error *drgn_language_from_die(Dwarf_Die *die, bool fall_back,
-					  const struct drgn_language **ret)
-{
-	Dwarf_Die cudie;
-	if (!dwarf_cu_die(die->cu, &cudie, NULL, NULL, NULL, NULL, NULL, NULL))
-		return drgn_error_libdw();
-	switch (dwarf_srclang(&cudie)) {
-	case DW_LANG_C:
-	case DW_LANG_C89:
-	case DW_LANG_C99:
-	case DW_LANG_C11:
-		*ret = &drgn_language_c;
-		break;
-	case DW_LANG_C_plus_plus:
-	case DW_LANG_C_plus_plus_03:
-	case DW_LANG_C_plus_plus_11:
-	case DW_LANG_C_plus_plus_14:
-		*ret = &drgn_language_cpp;
-		break;
-	default:
-		*ret = fall_back ? &drgn_default_language : NULL;
-		break;
-	}
-	return NULL;
-}

libdrgn/language.h
@@ -12,8 +12,6 @@
 #ifndef DRGN_LANGUAGE_H
 #define DRGN_LANGUAGE_H
 
-#include <elfutils/libdw.h>
-
 #include "drgn.h"
 
 /**
@@ -202,18 +200,6 @@ drgn_element_format_object_flags(enum drgn_format_object_flags flags)
 	return (drgn_passthrough_format_object_flags(flags) |
 		(flags & DRGN_FORMAT_OBJECT_ELEMENT_TYPE_NAMES) >> 2);
 }
-
-/**
- * Return the @ref drgn_language of the CU of the given DIE.
- *
- * @param[in] fall_back Whether to fall back if the language is not found or
- * unknown. If @c true, @ref drgn_default_language is returned in this case. If
- * @c false, @c NULL is returned.
- * @param[out] ret Returned language.
- * @return @c NULL on success, non-@c NULL on error.
- */
-struct drgn_error *drgn_language_from_die(Dwarf_Die *die, bool fall_back,
-					  const struct drgn_language **ret);
 
 /** @} */
 

commit 245a3835011d190c545963342d1281b2d94323e2

libdrgn: fix segfault if looking up main language fails
If the call to drgn_debug_info_main_language() from
drgn_program_set_language_from_main() fails, then the latter needs to
bail, not write garbage from the stack into prog->lang, which will crash
later.

Fixes: 5591d199b189 ("libdrgn: debug_info: split DWARF support into its own file")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -659,8 +659,10 @@ static void drgn_program_set_language_from_main(struct drgn_program *prog)
 		return;
 	const struct drgn_language *lang;
 	err = drgn_debug_info_main_language(prog->dbinfo, &lang);
-	if (err)
+	if (err) {
 		drgn_error_destroy(err);
+		return;
+	}
 	if (lang)
 		prog->lang = lang;
 }