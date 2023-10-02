osandov/drgn

commit 6b5ed1d43e05eb8b74a13aa90e796301d2a8a276

Allow Program.set_core_dump() and program_from_core_dump() to accept FD
This allows an open file descriptor to be passed into Drgn and treated
as a core dump.  There are many use cases, but one interesting one is
that the FD could be sent by a helper process running as root, allowing
a non-root Drgn process to debug the running kernel.

Signed-off-by: Stephen Brennan <stephen@brennan.io>

libdrgn/program.c
@@ -226,8 +226,8 @@ static struct drgn_error *has_kdump_signature(const char *path, int fd,
 	return NULL;
 }
 
-LIBDRGN_PUBLIC struct drgn_error *
-drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
+struct drgn_error *
+drgn_program_set_core_dump_fd_internal(struct drgn_program *prog, int fd, const char *path)
 {
 	struct drgn_error *err;
 	GElf_Ehdr ehdr_mem, *ehdr;
@@ -241,14 +241,7 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 	size_t vmcoreinfo_size = 0;
 	bool have_nt_taskstruct = false, is_proc_kcore;
 
-	err = drgn_program_check_initialized(prog);
-	if (err)
-		return err;
-
-	prog->core_fd = open(path, O_RDONLY);
-	if (prog->core_fd == -1)
-		return drgn_error_create_os("open", errno, path);
-
+	prog->core_fd = fd;
 	err = has_kdump_signature(path, prog->core_fd, &is_kdump);
 	if (err)
 		goto out_fd;
@@ -593,6 +586,39 @@ out_fd:
 	close(prog->core_fd);
 	prog->core_fd = -1;
 	return err;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_set_core_dump_fd(struct drgn_program *prog, int fd)
+{
+	struct drgn_error *err;
+
+	err = drgn_program_check_initialized(prog);
+	if (err)
+		return err;
+
+	#define FORMAT "/proc/self/fd/%d"
+	char path[sizeof(FORMAT) - sizeof("%d") + max_decimal_length(int) + 1];
+	snprintf(path, sizeof(path), FORMAT, fd);
+	#undef FORMAT
+
+	return drgn_program_set_core_dump_fd_internal(prog, fd, path);
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
+{
+	struct drgn_error *err;
+
+	err = drgn_program_check_initialized(prog);
+	if (err)
+		return err;
+
+	int fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return drgn_error_create_os("open", errno, path);
+
+	return drgn_program_set_core_dump_fd_internal(prog, fd, path);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
@@ -1497,6 +1523,21 @@ struct drgn_error *drgn_program_init_core_dump(struct drgn_program *prog,
 	return err;
 }
 
+struct drgn_error *drgn_program_init_core_dump_fd(struct drgn_program *prog, int fd)
+{
+	struct drgn_error *err;
+
+	err = drgn_program_set_core_dump_fd(prog, fd);
+	if (err)
+		return err;
+	err = drgn_program_load_debug_info(prog, NULL, 0, true, true);
+	if (err && err->code == DRGN_ERROR_MISSING_DEBUG_INFO) {
+		drgn_error_destroy(err);
+		err = NULL;
+	}
+	return err;
+}
+
 struct drgn_error *drgn_program_init_kernel(struct drgn_program *prog)
 {
 	struct drgn_error *err;
@@ -1539,6 +1580,28 @@ drgn_program_from_core_dump(const char *path, struct drgn_program **ret)
 
 	drgn_program_init(prog, NULL);
 	err = drgn_program_init_core_dump(prog, path);
+	if (err) {
+		drgn_program_deinit(prog);
+		free(prog);
+		return err;
+	}
+
+	*ret = prog;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_from_core_dump_fd(int fd, struct drgn_program **ret)
+{
+	struct drgn_error *err;
+	struct drgn_program *prog;
+
+	prog = malloc(sizeof(*prog));
+	if (!prog)
+		return &drgn_enomem;
+
+	drgn_program_init(prog, NULL);
+	err = drgn_program_init_core_dump_fd(prog, fd);
 	if (err) {
 		drgn_program_deinit(prog);
 		free(prog);

libdrgn/python/program.c
@@ -515,13 +515,16 @@ static PyObject *Program_set_core_dump(Program *self, PyObject *args,
 {
 	static char *keywords[] = {"path", NULL};
 	struct drgn_error *err;
-	struct path_arg path = {};
+	struct path_arg path = { .allow_fd = true };
 
 	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O&:set_core_dump",
 					 keywords, path_converter, &path))
 		return NULL;
 
-	err = drgn_program_set_core_dump(&self->prog, path.path);
+	if (path.fd >= 0)
+		err = drgn_program_set_core_dump_fd(&self->prog, path.fd);
+	else
+		err = drgn_program_set_core_dump(&self->prog, path.path);
 	path_cleanup(&path);
 	if (err)
 		return set_drgn_error(err);
@@ -1247,7 +1250,7 @@ Program *program_from_core_dump(PyObject *self, PyObject *args, PyObject *kwds)
 {
 	static char *keywords[] = {"path", NULL};
 	struct drgn_error *err;
-	struct path_arg path = {};
+	struct path_arg path = { .allow_fd = true };
 	if (!PyArg_ParseTupleAndKeywords(args, kwds,
 					 "O&:program_from_core_dump", keywords,
 					 path_converter, &path))
@@ -1260,7 +1263,10 @@ Program *program_from_core_dump(PyObject *self, PyObject *args, PyObject *kwds)
 		return NULL;
 	}
 
-	err = drgn_program_init_core_dump(&prog->prog, path.path);
+	if (path.fd >= 0)
+		err = drgn_program_init_core_dump_fd(&prog->prog, path.fd);
+	else
+		err = drgn_program_init_core_dump(&prog->prog, path.path);
 	path_cleanup(&path);
 	if (err)
 		return set_drgn_error(err);

_drgn.pyi
@@ -451,7 +451,7 @@ class Program:
             return an :class:`Object`.
         """
         ...
-    def set_core_dump(self, path: Path) -> None:
+    def set_core_dump(self, path: Union[Path, int]) -> None:
         """
         Set the program to a core dump.
 
@@ -459,7 +459,7 @@ class Program:
         mapped executable and libraries. It does not load any debugging
         symbols; see :meth:`load_default_debug_info()`.
 
-        :param path: Core dump file path.
+        :param path: Core dump file path or open file descriptor.
         """
         ...
     def set_kernel(self) -> None:
@@ -888,12 +888,12 @@ def filename_matches(haystack: Optional[str], needle: Optional[str]) -> bool:
     """
     ...
 
-def program_from_core_dump(path: Path) -> Program:
+def program_from_core_dump(path: Union[Path, int]) -> Program:
     """
     Create a :class:`Program` from a core dump file. The type of program (e.g.,
     userspace or kernel) is determined automatically.
 
-    :param path: Core dump file path.
+    :param path: Core dump file path or open file descriptor.
     """
     ...
 

libdrgn/drgn.h.in
@@ -671,6 +671,14 @@ struct drgn_error *drgn_program_set_core_dump(struct drgn_program *prog,
 					      const char *path);
 
 /**
+ * Set a @ref drgn_program to a core dump from a file descriptor.
+ *
+ * @param[in] path Core dump file descriptor.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_set_core_dump_fd(struct drgn_program *prog, int fd);
+
+/**
  * Set a @ref drgn_program to the running operating system kernel.
  *
  * @return @c NULL on success, non-@c NULL on error.
@@ -711,6 +719,19 @@ struct drgn_error *drgn_program_load_debug_info(struct drgn_program *prog,
  */
 struct drgn_error *drgn_program_from_core_dump(const char *path,
 					       struct drgn_program **ret);
+
+/**
+ * Create a @ref drgn_program from a core dump file descriptor.
+ *
+ * Same as @ref drgn_program_from_core_dump but with an already-opened file
+ * descriptor.
+ *
+ * @param[in] fd Core dump file path descriptor.
+ * @param[out] ret Returned program.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_program_from_core_dump_fd(int fd,
+						  struct drgn_program **ret);
 
 /**
  * Create a @ref drgn_program from the running operating system kernel.

libdrgn/program.h
@@ -227,6 +227,13 @@ struct drgn_error *drgn_program_init_core_dump(struct drgn_program *prog,
 					       const char *path);
 
 /**
+ * Implement @ref drgn_program_from_core_dump_fd() on an initialized @ref
+ * drgn_program.
+ */
+struct drgn_error *drgn_program_init_core_dump_fd(struct drgn_program *prog,
+						  int fd);
+
+/**
  * Implement @ref drgn_program_from_kernel() on an initialized @ref
  * drgn_program.
  */

libdrgn/python/drgnpy.h
@@ -330,7 +330,9 @@ struct index_arg {
 int index_converter(PyObject *o, void *p);
 
 struct path_arg {
+	bool allow_fd;
 	bool allow_none;
+	int fd;
 	char *path;
 	Py_ssize_t length;
 	PyObject *object;

libdrgn/python/util.c
@@ -86,7 +86,29 @@ int path_converter(PyObject *o, void *p)
 	}
 
 	struct path_arg *path = p;
-	if (path->allow_none && o == Py_None) {
+	path->fd = -1;
+	path->path = NULL;
+	path->length = 0;
+	path->bytes = NULL;
+	if (path->allow_fd && PyIndex_Check(o)) {
+		_cleanup_pydecref_ PyObject *fd_obj = PyNumber_Index(o);
+		if (!fd_obj)
+			return 0;
+		int overflow;
+		long fd = PyLong_AsLongAndOverflow(fd_obj, &overflow);
+		if (fd == -1 && PyErr_Occurred())
+			return 0;
+		if (overflow > 0 || fd > INT_MAX) {
+			PyErr_SetString(PyExc_OverflowError,
+					"fd is greater than maximum");
+			return 0;
+		}
+		if (fd < 0) {
+			PyErr_SetString(PyExc_ValueError, "fd is negative");
+			return 0;
+		}
+		path->fd = fd;
+	} else if (path->allow_none && o == Py_None) {
 		path->path = NULL;
 		path->length = 0;
 		path->bytes = NULL;

commit 154b77aa3ad80992e9bebd55807e3899e56b6abc

libdrgn: make drgn_program_set_core_dump_fd_internal static
This is only used in libdrgn/program.c.

Fixes: 6b5ed1d43e05 ("Allow Program.set_core_dump() and program_from_core_dump() to accept FD")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -235,8 +235,9 @@ static struct drgn_error *has_kdump_signature(const char *path, int fd,
 	return NULL;
 }
 
-struct drgn_error *
-drgn_program_set_core_dump_fd_internal(struct drgn_program *prog, int fd, const char *path)
+static struct drgn_error *
+drgn_program_set_core_dump_fd_internal(struct drgn_program *prog, int fd,
+				       const char *path)
 {
 	struct drgn_error *err;
 	GElf_Ehdr ehdr_mem, *ehdr;