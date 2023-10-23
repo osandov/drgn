osandov/drgn

commit c85dd74f3e98860ddade9fd7f908032d6192eca6

libdrgn: embed drgn_debug_info in drgn_program
This will simplify the implementation of the module API (#332).

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -2086,12 +2086,9 @@ bool drgn_debug_info_is_indexed(struct drgn_debug_info *dbinfo,
 	return c_string_set_search(&dbinfo->module_names, &name).entry != NULL;
 }
 
-struct drgn_error *drgn_debug_info_create(struct drgn_program *prog,
-					  struct drgn_debug_info **ret)
+void drgn_debug_info_init(struct drgn_debug_info *dbinfo,
+			  struct drgn_program *prog)
 {
-	struct drgn_debug_info *dbinfo = malloc(sizeof(*dbinfo));
-	if (!dbinfo)
-		return &drgn_enomem;
 	dbinfo->prog = prog;
 	const Dwfl_Callbacks *dwfl_callbacks;
 	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
@@ -2101,10 +2098,10 @@ struct drgn_error *drgn_debug_info_create(struct drgn_program *prog,
 	else
 		dwfl_callbacks = &drgn_userspace_core_dump_dwfl_callbacks;
 	dbinfo->dwfl = dwfl_begin(dwfl_callbacks);
-	if (!dbinfo->dwfl) {
-		free(dbinfo);
-		return drgn_error_libdwfl();
-	}
+	// This is temporary until we stop using libdwfl, and is extremely
+	// unlikely to fail anwyays, so don't bother propagating an error up.
+	if (!dbinfo->dwfl)
+		abort();
 	drgn_program_add_type_finder_impl(prog, &dbinfo->type_finder,
 					  drgn_debug_info_find_type, dbinfo);
 	drgn_program_add_object_finder_impl(prog, &dbinfo->object_finder,
@@ -2113,21 +2110,16 @@ struct drgn_error *drgn_debug_info_create(struct drgn_program *prog,
 	drgn_module_table_init(&dbinfo->modules);
 	c_string_set_init(&dbinfo->module_names);
 	drgn_dwarf_info_init(dbinfo);
-	*ret = dbinfo;
-	return NULL;
 }
 
-void drgn_debug_info_destroy(struct drgn_debug_info *dbinfo)
+void drgn_debug_info_deinit(struct drgn_debug_info *dbinfo)
 {
-	if (!dbinfo)
-		return;
 	drgn_dwarf_info_deinit(dbinfo);
 	c_string_set_deinit(&dbinfo->module_names);
 	drgn_debug_info_free_modules(dbinfo, false, true);
 	assert(drgn_module_table_empty(&dbinfo->modules));
 	drgn_module_table_deinit(&dbinfo->modules);
 	dwfl_end(dbinfo->dwfl);
-	free(dbinfo);
 }
 
 struct drgn_elf_file *drgn_module_find_dwarf_file(struct drgn_module *module,

libdrgn/debug_info.h
@@ -153,12 +153,12 @@ struct drgn_debug_info {
 	struct drgn_dwarf_info dwarf;
 };
 
-/** Create a @ref drgn_debug_info. */
-struct drgn_error *drgn_debug_info_create(struct drgn_program *prog,
-					  struct drgn_debug_info **ret);
+/** Initialize a @ref drgn_debug_info. */
+void drgn_debug_info_init(struct drgn_debug_info *dbinfo,
+			  struct drgn_program *prog);
 
-/** Destroy a @ref drgn_debug_info. */
-void drgn_debug_info_destroy(struct drgn_debug_info *dbinfo);
+/** Deinitialize a @ref drgn_debug_info. */
+void drgn_debug_info_deinit(struct drgn_debug_info *dbinfo);
 
 DEFINE_VECTOR_TYPE(drgn_module_vector, struct drgn_module *);
 

libdrgn/dwarf_info.c
@@ -4917,7 +4917,7 @@ drgn_dwarf_member_thunk_fn(struct drgn_object *res, void *arg_)
 	struct drgn_dwarf_member_thunk_arg *arg = arg_;
 	if (res) {
 		struct drgn_qualified_type qualified_type;
-		err = drgn_type_from_dwarf_attr(drgn_object_program(res)->dbinfo,
+		err = drgn_type_from_dwarf_attr(&drgn_object_program(res)->dbinfo,
 						arg->file, &arg->die, NULL,
 						false,
 						arg->can_be_incomplete_array,
@@ -5200,7 +5200,7 @@ drgn_dwarf_template_type_parameter_thunk_fn(struct drgn_object *res, void *arg_)
 	struct drgn_dwarf_die_thunk_arg *arg = arg_;
 	if (res) {
 		struct drgn_qualified_type qualified_type;
-		err = drgn_type_from_dwarf_attr(drgn_object_program(res)->dbinfo,
+		err = drgn_type_from_dwarf_attr(&drgn_object_program(res)->dbinfo,
 						arg->file, &arg->die, NULL,
 						true, true, NULL,
 						&qualified_type);
@@ -5222,7 +5222,7 @@ drgn_dwarf_template_value_parameter_thunk_fn(struct drgn_object *res,
 	struct drgn_error *err;
 	struct drgn_dwarf_die_thunk_arg *arg = arg_;
 	if (res) {
-		err = drgn_object_from_dwarf(drgn_object_program(res)->dbinfo,
+		err = drgn_object_from_dwarf(&drgn_object_program(res)->dbinfo,
 					     arg->file, &arg->die, NULL, NULL,
 					     NULL, res);
 		if (err)
@@ -5790,7 +5790,7 @@ drgn_dwarf_formal_parameter_thunk_fn(struct drgn_object *res, void *arg_)
 	struct drgn_dwarf_die_thunk_arg *arg = arg_;
 	if (res) {
 		struct drgn_qualified_type qualified_type;
-		err = drgn_type_from_dwarf_attr(drgn_object_program(res)->dbinfo,
+		err = drgn_type_from_dwarf_attr(&drgn_object_program(res)->dbinfo,
 						arg->file, &arg->die, NULL,
 						false, true, NULL,
 						&qualified_type);

libdrgn/program.c
@@ -100,6 +100,7 @@ void drgn_program_init(struct drgn_program *prog,
 	drgn_memory_reader_init(&prog->reader);
 	drgn_program_init_types(prog);
 	drgn_object_index_init(&prog->oindex);
+	drgn_debug_info_init(&prog->dbinfo, prog);
 	prog->core_fd = -1;
 	if (platform)
 		drgn_program_set_platform(prog, platform);
@@ -147,7 +148,7 @@ void drgn_program_deinit(struct drgn_program *prog)
 	if (prog->core_fd != -1)
 		close(prog->core_fd);
 
-	drgn_debug_info_destroy(prog->dbinfo);
+	drgn_debug_info_deinit(&prog->dbinfo);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
@@ -695,7 +696,7 @@ static void drgn_program_set_language_from_main(struct drgn_program *prog)
 	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
 		return;
 	const struct drgn_language *lang;
-	err = drgn_debug_info_main_language(prog->dbinfo, &lang);
+	err = drgn_debug_info_main_language(&prog->dbinfo, &lang);
 	if (err) {
 		drgn_error_destroy(err);
 		return;
@@ -734,20 +735,12 @@ drgn_program_load_debug_info(struct drgn_program *prog, const char **paths,
 		return NULL;
 
 	drgn_blocking_guard(prog);
-	struct drgn_debug_info *dbinfo = prog->dbinfo;
-	if (!dbinfo) {
-		err = drgn_debug_info_create(prog, &dbinfo);
-		if (err)
-			return err;
-		prog->dbinfo = dbinfo;
-	}
-
-	err = drgn_debug_info_load(dbinfo, paths, n, load_default, load_main);
+	err = drgn_debug_info_load(&prog->dbinfo, paths, n, load_default, load_main);
 	if ((!err || err->code == DRGN_ERROR_MISSING_DEBUG_INFO)) {
 		if (!prog->lang)
 			drgn_program_set_language_from_main(prog);
 		if (!prog->has_platform) {
-			dwfl_getdwarf(dbinfo->dwfl,
+			dwfl_getdwarf(prog->dbinfo.dwfl,
 				      drgn_set_platform_from_dwarf, prog, 0);
 		}
 	}
@@ -1784,13 +1777,9 @@ bool drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
 						  struct drgn_symbol *ret)
 {
 	if (!module) {
-		if (prog->dbinfo) {
-			module = dwfl_addrmodule(prog->dbinfo->dwfl, address);
-			if (!module)
-				return false;
-		} else {
+		module = dwfl_addrmodule(prog->dbinfo.dwfl, address);
+		if (!module)
 			return false;
-		}
 	}
 
 	GElf_Off offset;
@@ -1894,11 +1883,6 @@ symbols_search(struct drgn_program *prog, struct symbols_search_arg *arg,
 {
 	struct drgn_error *err;
 
-	if (!prog->dbinfo) {
-		return drgn_error_create(DRGN_ERROR_MISSING_DEBUG_INFO,
-					 "could not find matching symbols");
-	}
-
 	symbolp_vector_init(&arg->results);
 
 	/*
@@ -1907,12 +1891,12 @@ symbols_search(struct drgn_program *prog, struct symbols_search_arg *arg,
 	 */
 	err = NULL;
 	if (arg->flags & SYMBOLS_SEARCH_ADDRESS) {
-		Dwfl_Module *module = dwfl_addrmodule(prog->dbinfo->dwfl,
+		Dwfl_Module *module = dwfl_addrmodule(prog->dbinfo.dwfl,
 						      arg->address);
 		if (module && symbols_search_cb(module, NULL, NULL, 0, arg))
 			err = &drgn_enomem;
 	} else {
-		if (dwfl_getmodules(prog->dbinfo->dwfl, symbols_search_cb, arg,
+		if (dwfl_getmodules(prog->dbinfo.dwfl, symbols_search_cb, arg,
 				    0))
 			err = &drgn_enomem;
 	}
@@ -2015,17 +1999,14 @@ drgn_program_find_symbol_by_name(struct drgn_program *prog,
 	struct find_symbol_by_name_arg arg = {
 		.name = name,
 	};
-	if (prog->dbinfo) {
-		dwfl_getmodules(prog->dbinfo->dwfl, find_symbol_by_name_cb,
-				&arg, 0);
-		if (arg.found) {
-			struct drgn_symbol *sym = malloc(sizeof(*sym));
-			if (!sym)
-				return &drgn_enomem;
-			drgn_symbol_from_elf(name, arg.addr, &arg.sym, sym);
-			*ret = sym;
-			return NULL;
-		}
+	dwfl_getmodules(prog->dbinfo.dwfl, find_symbol_by_name_cb, &arg, 0);
+	if (arg.found) {
+		struct drgn_symbol *sym = malloc(sizeof(*sym));
+		if (!sym)
+			return &drgn_enomem;
+		drgn_symbol_from_elf(name, arg.addr, &arg.sym, sym);
+		*ret = sym;
+		return NULL;
 	}
 	return drgn_error_format(DRGN_ERROR_LOOKUP,
 				 "could not find symbol with name '%s'%s", name,

libdrgn/program.h
@@ -19,6 +19,7 @@
 #include <libkdumpfile/kdumpfile.h>
 #endif
 
+#include "debug_info.h"
 #include "drgn.h"
 #include "hash_table.h"
 #include "language.h"
@@ -107,7 +108,7 @@ struct drgn_program {
 	 * Debugging information.
 	 */
 	struct drgn_object_index oindex;
-	struct drgn_debug_info *dbinfo;
+	struct drgn_debug_info dbinfo;
 
 	/*
 	 * Program information.

libdrgn/register_state.c
@@ -105,15 +105,13 @@ void drgn_register_state_set_pc(struct drgn_program *prog,
 	pc &= drgn_platform_address_mask(&prog->platform);
 	regs->_pc = pc;
 	drgn_register_state_set_known(regs, 0);
-	if (prog->dbinfo) {
-		Dwfl_Module *dwfl_module = dwfl_addrmodule(prog->dbinfo->dwfl,
-							   pc - !regs->interrupted);
-		if (dwfl_module) {
-			void **userdatap;
-			dwfl_module_info(dwfl_module, &userdatap, NULL, NULL,
-					 NULL, NULL, NULL, NULL);
-			regs->module = *userdatap;
-		}
+	Dwfl_Module *dwfl_module = dwfl_addrmodule(prog->dbinfo.dwfl,
+						   pc - !regs->interrupted);
+	if (dwfl_module) {
+		void **userdatap;
+		dwfl_module_info(dwfl_module, &userdatap, NULL, NULL,
+				 NULL, NULL, NULL, NULL);
+		regs->module = *userdatap;
 	}
 }
 

libdrgn/stack_trace.c
@@ -495,7 +495,7 @@ not_found:;
 	if (!drgn_platforms_equal(&file->platform, &trace->prog->platform))
 		regs = NULL;
 	Dwarf_Die function_die = frame->scopes[frame->function_scope];
-	return drgn_object_from_dwarf(trace->prog->dbinfo, file, &die,
+	return drgn_object_from_dwarf(&trace->prog->dbinfo, file, &die,
 				      dwarf_tag(&die) == DW_TAG_enumerator ?
 				      &type_die : NULL,
 				      &function_die, regs, ret);

commit a34be20882ccb72665d52ae3fe610bfa262641c0

libdrgn: fix getting debug info for live processes
We're determining which callbacks to use for the Dwfl handle too early,
before the program flags are set. Instead of creating it later, shift
the flag checks to the callback itself.

Fixes: c85dd74f3e98 ("libdrgn: embed drgn_debug_info in drgn_program")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -76,6 +76,9 @@ DEFINE_HASH_SET_FUNCTIONS(c_string_set, c_string_key_hash_pair,
 /**
  * @c Dwfl_Callbacks::find_elf() implementation.
  *
+ * If the ELF file was reported directly, this returns it. Otherwise, it falls
+ * back to an appropriate callback.
+ *
  * Ideally we'd use @c dwfl_report_elf() instead, but that doesn't take an @c
  * Elf handle, which we need for a couple of reasons:
  *
@@ -96,53 +99,27 @@ static int drgn_dwfl_find_elf(Dwfl_Module *dwfl_module, void **userdatap,
 			      char **file_name, Elf **elfp)
 {
 	struct drgn_module *module = *userdatap;
-	/*
-	 * libdwfl consumes the returned path, file descriptor, and ELF handle,
-	 * so clear the fields.
-	 */
-	*file_name = module->path;
-	int fd = module->fd;
-	*elfp = module->elf;
-	module->path = NULL;
-	module->fd = -1;
-	module->elf = NULL;
-	return fd;
-}
-
-/*
- * Uses drgn_dwfl_find_elf() if the ELF file was reported directly and falls
- * back to dwfl_linux_proc_find_elf() otherwise.
- */
-static int drgn_dwfl_linux_proc_find_elf(Dwfl_Module *dwfl_module,
-					 void **userdatap, const char *name,
-					 Dwarf_Addr base, char **file_name,
-					 Elf **elfp)
-{
-	struct drgn_module *module = *userdatap;
 	if (module->elf) {
-		return drgn_dwfl_find_elf(dwfl_module, userdatap, name, base,
-					  file_name, elfp);
+		*file_name = module->path;
+		int fd = module->fd;
+		*elfp = module->elf;
+		// libdwfl consumes the returned path, file descriptor, and ELF
+		// handle, so clear the fields.
+		module->path = NULL;
+		module->fd = -1;
+		module->elf = NULL;
+		return fd;
 	}
-	return dwfl_linux_proc_find_elf(dwfl_module, userdatap, name, base,
-					file_name, elfp);
-}
-
-/*
- * Uses drgn_dwfl_find_elf() if the ELF file was reported directly and falls
- * back to dwfl_build_id_find_elf() otherwise.
- */
-static int drgn_dwfl_build_id_find_elf(Dwfl_Module *dwfl_module,
-				       void **userdatap, const char *name,
-				       Dwarf_Addr base, char **file_name,
-				       Elf **elfp)
-{
-	struct drgn_module *module = *userdatap;
-	if (module->elf) {
-		return drgn_dwfl_find_elf(dwfl_module, userdatap, name, base,
-					  file_name, elfp);
+	if (module->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		*elfp = NULL;
+		return -1;
+	} else if (module->prog->flags & DRGN_PROGRAM_IS_LIVE) {
+		return dwfl_linux_proc_find_elf(dwfl_module, userdatap, name,
+						base, file_name, elfp);
+	} else {
+		return dwfl_build_id_find_elf(dwfl_module, userdatap, name,
+					      base, file_name, elfp);
 	}
-	return dwfl_build_id_find_elf(dwfl_module, userdatap, name, base,
-				      file_name, elfp);
 }
 
 /**
@@ -164,18 +141,6 @@ static int drgn_dwfl_section_address(Dwfl_Module *module, void **userdatap,
 
 static const Dwfl_Callbacks drgn_dwfl_callbacks = {
 	.find_elf = drgn_dwfl_find_elf,
-	.find_debuginfo = dwfl_standard_find_debuginfo,
-	.section_address = drgn_dwfl_section_address,
-};
-
-static const Dwfl_Callbacks drgn_linux_proc_dwfl_callbacks = {
-	.find_elf = drgn_dwfl_linux_proc_find_elf,
-	.find_debuginfo = dwfl_standard_find_debuginfo,
-	.section_address = drgn_dwfl_section_address,
-};
-
-static const Dwfl_Callbacks drgn_userspace_core_dump_dwfl_callbacks = {
-	.find_elf = drgn_dwfl_build_id_find_elf,
 	.find_debuginfo = dwfl_standard_find_debuginfo,
 	.section_address = drgn_dwfl_section_address,
 };
@@ -2069,14 +2034,7 @@ void drgn_debug_info_init(struct drgn_debug_info *dbinfo,
 			  struct drgn_program *prog)
 {
 	dbinfo->prog = prog;
-	const Dwfl_Callbacks *dwfl_callbacks;
-	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)
-		dwfl_callbacks = &drgn_dwfl_callbacks;
-	else if (prog->flags & DRGN_PROGRAM_IS_LIVE)
-		dwfl_callbacks = &drgn_linux_proc_dwfl_callbacks;
-	else
-		dwfl_callbacks = &drgn_userspace_core_dump_dwfl_callbacks;
-	dbinfo->dwfl = dwfl_begin(dwfl_callbacks);
+	dbinfo->dwfl = dwfl_begin(&drgn_dwfl_callbacks);
 	// This is temporary until we stop using libdwfl, and is extremely
 	// unlikely to fail anwyays, so don't bother propagating an error up.
 	if (!dbinfo->dwfl)