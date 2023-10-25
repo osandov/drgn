e3e3295a57009133f50477b1956d4fdcd98fa5fd
Omar Sandoval <osandov@osandov.com>
2023-10-25 09:40:32 -0700

    tests: add test for cpumask_to_cpulist()
    And refactor the for_each_cpu() tests.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/linux_kernel/helpers/test_cpumask.py
+++ tests/linux_kernel/helpers/test_cpumask.py
@@ -3,28 +3,36 @@
 
 from pathlib import Path
 
-from drgn.helpers.linux.cpumask import (
-    for_each_online_cpu,
-    for_each_possible_cpu,
-    for_each_present_cpu,
-)
+import drgn.helpers.linux.cpumask
+from drgn.helpers.linux.cpumask import cpumask_to_cpulist
 from tests.linux_kernel import LinuxKernelTestCase, parse_range_list
 
 CPU_PATH = Path("/sys/devices/system/cpu")
 
 
 class TestCpuMask(LinuxKernelTestCase):
-    def _test_for_each_cpu(self, func, name):
-        self.assertEqual(
-            list(func(self.prog)),
-            sorted(parse_range_list((CPU_PATH / name).read_text())),
-        )
+    _MASKS = ("online", "possible", "present")
 
-    def test_for_each_online_cpu(self):
-        self._test_for_each_cpu(for_each_online_cpu, "online")
+    def test_for_each_cpu(self):
+        for name in self._MASKS:
+            with self.subTest(name=name):
+                self.assertEqual(
+                    list(
+                        getattr(drgn.helpers.linux.cpumask, f"for_each_{name}_cpu")(
+                            self.prog
+                        )
+                    ),
+                    sorted(parse_range_list((CPU_PATH / name).read_text())),
+                )
 
-    def test_for_each_possible_cpu(self):
-        self._test_for_each_cpu(for_each_possible_cpu, "possible")
-
-    def test_for_each_present_cpu(self):
-        self._test_for_each_cpu(for_each_present_cpu, "present")
+    def test_cpumask_to_cpulist(self):
+        for name in self._MASKS:
+            with self.subTest(name=name):
+                self.assertEqual(
+                    cpumask_to_cpulist(
+                        getattr(drgn.helpers.linux.cpumask, f"cpu_{name}_mask")(
+                            self.prog
+                        )
+                    ),
+                    (CPU_PATH / name).read_text().strip(),
+                )
