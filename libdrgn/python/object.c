osandov/drgn

commit 085b0bc9dbfaea1c60ca557694b8243f6b3771a4

libdrgn: add DRGN_OBJECT scope guard
Temporary local drgn_objects are fairly common. They require defining a
variable, initializing it with drgn_object_init, and cleaning it up with
drgn_object_deinit. Add a scope guard, DRGN_OBJECT, to do this all
automatically, and convert existing uses to it.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -544,10 +544,6 @@ static PyObject *DrgnObject_compound_value(struct drgn_object *obj,
 					   struct drgn_type *underlying_type)
 {
 	struct drgn_error *err;
-	PyObject *dict;
-	struct drgn_object member;
-	struct drgn_type_member *members;
-	size_t num_members, i;
 
 	if (!drgn_type_is_complete(underlying_type)) {
 		PyErr_Format(PyExc_TypeError,
@@ -556,106 +552,79 @@ static PyObject *DrgnObject_compound_value(struct drgn_object *obj,
 		return NULL;
 	}
 
-	dict = PyDict_New();
+	_cleanup_pydecref_ PyObject *dict = PyDict_New();
 	if (!dict)
 		return NULL;
 
-	drgn_object_init(&member, drgn_object_program(obj));
-	members = drgn_type_members(underlying_type);
-	num_members = drgn_type_num_members(underlying_type);
-	for (i = 0; i < num_members; i++) {
+	DRGN_OBJECT(member, drgn_object_program(obj));
+	struct drgn_type_member *members = drgn_type_members(underlying_type);
+	size_t num_members = drgn_type_num_members(underlying_type);
+	for (size_t i = 0; i < num_members; i++) {
 		struct drgn_qualified_type member_type;
 		uint64_t member_bit_field_size;
 		err = drgn_member_type(&members[i], &member_type,
 				       &member_bit_field_size);
-		if (err) {
-			set_drgn_error(err);
-			Py_CLEAR(dict);
-			goto out;
-		}
+		if (err)
+			return set_drgn_error(err);
 
 		err = drgn_object_slice(&member, obj, member_type,
 					members[i].bit_offset,
 					member_bit_field_size);
-		if (err) {
-			set_drgn_error(err);
-			Py_CLEAR(dict);
-			goto out;
-		}
+		if (err)
+			return set_drgn_error(err);
 
 		_cleanup_pydecref_ PyObject *member_value =
 			DrgnObject_value_impl(&member);
-		if (!member_value) {
-			Py_CLEAR(dict);
-			goto out;
-		}
+		if (!member_value)
+			return NULL;
 
-		int ret;
 		if (members[i].name) {
-			ret = PyDict_SetItemString(dict, members[i].name,
-						   member_value);
+			if (PyDict_SetItemString(dict, members[i].name,
+						 member_value))
+				return NULL;
 		} else {
-			ret = PyDict_Update(dict, member_value);
-		}
-		if (ret) {
-			Py_CLEAR(dict);
-			goto out;
+			if (PyDict_Update(dict, member_value))
+				return NULL;
 		}
 	}
-
-out:
-	drgn_object_deinit(&member);
-	return dict;
+	return no_cleanup_ptr(dict);
 }
 
 static PyObject *DrgnObject_array_value(struct drgn_object *obj,
 					struct drgn_type *underlying_type)
 {
 	struct drgn_error *err;
-	struct drgn_qualified_type element_type;
-	uint64_t element_bit_size, length, i;
-	PyObject *list;
-	struct drgn_object element;
 
-	element_type = drgn_type_type(underlying_type);
+	struct drgn_qualified_type element_type =
+		drgn_type_type(underlying_type);
+	uint64_t element_bit_size;
 	err = drgn_type_bit_size(element_type.type, &element_bit_size);
 	if (err)
 		return set_drgn_error(err);
 
-	length = drgn_type_length(underlying_type);
+	uint64_t length = drgn_type_length(underlying_type);
 	if (length > PY_SSIZE_T_MAX) {
 		PyErr_NoMemory();
 		return NULL;
 	}
 
-	list = PyList_New(length);
+	_cleanup_pydecref_ PyObject *list = PyList_New(length);
 	if (!list)
 		return NULL;
 
-	drgn_object_init(&element, drgn_object_program(obj));
-	for (i = 0; i < length; i++) {
-		PyObject *element_value;
-
+	DRGN_OBJECT(element, drgn_object_program(obj));
+	for (uint64_t i = 0; i < length; i++) {
 		err = drgn_object_slice(&element, obj, element_type,
 					i * element_bit_size, 0);
-		if (err) {
-			set_drgn_error(err);
-			Py_CLEAR(list);
-			goto out;
-		}
+		if (err)
+			return set_drgn_error(err);
 
-		element_value = DrgnObject_value_impl(&element);
-		if (!element_value) {
-			Py_CLEAR(list);
-			goto out;
-		}
-
+		PyObject *element_value = DrgnObject_value_impl(&element);
+		if (!element_value)
+			return NULL;
 		PyList_SET_ITEM(list, i, element_value);
 	}
-
-out:
-	drgn_object_deinit(&element);
-	return list;
+	return no_cleanup_ptr(list);
 }
 
 static PyObject *DrgnObject_value_impl(struct drgn_object *obj)

libdrgn/object.c
@@ -18,16 +18,23 @@
 #include "type.h"
 #include "util.h"
 
+#define DRGN_OBJECT_INITIALIZER(prog)			\
+	(struct drgn_object){				\
+		.type = drgn_void_type(prog, NULL),	\
+		.encoding = DRGN_OBJECT_ENCODING_NONE,	\
+		.kind = DRGN_OBJECT_ABSENT,		\
+	}
+
+LIBDRGN_PUBLIC
+struct drgn_object drgn_object_initializer(struct drgn_program *prog)
+{
+	return DRGN_OBJECT_INITIALIZER(prog);
+}
+
 LIBDRGN_PUBLIC void drgn_object_init(struct drgn_object *obj,
 				     struct drgn_program *prog)
 {
-	obj->type = drgn_void_type(prog, NULL);
-	obj->bit_size = 0;
-	obj->qualifiers = 0;
-	obj->encoding = DRGN_OBJECT_ENCODING_NONE;
-	obj->kind = DRGN_OBJECT_ABSENT;
-	obj->is_bit_field = false;
-	obj->little_endian = false;
+	*obj = DRGN_OBJECT_INITIALIZER(prog);
 }
 
 static void drgn_value_deinit(const struct drgn_object *obj,
@@ -1176,11 +1183,10 @@ drgn_compound_object_is_zero(const struct drgn_object *obj,
 			     struct drgn_type *underlying_type, bool *ret)
 {
 	struct drgn_error *err;
-	struct drgn_object member;
 	struct drgn_type_member *members;
 	size_t num_members, i;
 
-	drgn_object_init(&member, drgn_object_program(obj));
+	DRGN_OBJECT(member, drgn_object_program(obj));
 	members = drgn_type_members(underlying_type);
 	num_members = drgn_type_num_members(underlying_type);
 	for (i = 0; i < num_members; i++) {
@@ -1189,23 +1195,19 @@ drgn_compound_object_is_zero(const struct drgn_object *obj,
 		err = drgn_member_type(&members[i], &member_type,
 				       &member_bit_field_size);
 		if (err)
-			goto out;
+			return err;
 
 		err = drgn_object_slice(&member, obj, member_type,
 					members[i].bit_offset,
 					member_bit_field_size);
 		if (err)
-			goto out;
+			return err;
 
 		err = drgn_object_is_zero_impl(&member, ret);
 		if (err || !*ret)
-			goto out;
+			return err;
 	}
-
-	err = NULL;
-out:
-	drgn_object_deinit(&member);
-	return err;
+	return NULL;
 }
 
 static struct drgn_error *
@@ -1215,30 +1217,25 @@ drgn_array_object_is_zero(const struct drgn_object *obj,
 	struct drgn_error *err;
 	struct drgn_qualified_type element_type;
 	uint64_t element_bit_size, length, i;
-	struct drgn_object element;
 
 	element_type = drgn_type_type(underlying_type);
 	err = drgn_type_bit_size(element_type.type, &element_bit_size);
 	if (err)
 		return err;
 
-	drgn_object_init(&element, drgn_object_program(obj));
+	DRGN_OBJECT(element, drgn_object_program(obj));
 	length = drgn_type_length(underlying_type);
 	for (i = 0; i < length; i++) {
 		err = drgn_object_slice(&element, obj, element_type,
 					i * element_bit_size, 0);
 		if (err)
-			goto out;
+			return err;
 
 		err = drgn_object_is_zero_impl(&element, ret);
 		if (err || !*ret)
-			goto out;
+			return err;
 	}
-
-	err = NULL;
-out:
-	drgn_object_deinit(&element);
-	return err;
+	return NULL;
 }
 
 static struct drgn_error *

libdrgn/arch_aarch64.c
@@ -195,43 +195,37 @@ linux_kernel_get_initial_registers_aarch64(const struct drgn_object *task_obj,
 	struct drgn_error *err;
 	struct drgn_program *prog = drgn_object_program(task_obj);
 
-	struct drgn_object cpu_context_obj;
-	drgn_object_init(&cpu_context_obj, prog);
+	DRGN_OBJECT(cpu_context_obj, prog);
 
 	err = drgn_object_member_dereference(&cpu_context_obj, task_obj,
 					     "thread");
 	if (err)
-		goto out;
+		return err;
 	err = drgn_object_member(&cpu_context_obj, &cpu_context_obj,
 				 "cpu_context");
 	if (err)
-		goto out;
+		return err;
 	if (cpu_context_obj.encoding != DRGN_OBJECT_ENCODING_BUFFER ||
 	    drgn_object_size(&cpu_context_obj) < 104) {
-		err = drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
-					"cpu_context is truncated");
-		goto out;
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "cpu_context is truncated");
 	}
 	err = drgn_object_read(&cpu_context_obj, &cpu_context_obj);
 	if (err)
-		goto out;
+		return err;
 
 	const void *buf = drgn_object_buffer(&cpu_context_obj);
 	struct drgn_register_state *regs =
 		drgn_register_state_create(x30, false);
-	if (!regs) {
-		err = &drgn_enomem;
-		goto out;
-	}
+	if (!regs)
+		return &drgn_enomem;
 
 	drgn_register_state_set_from_buffer(regs, x30, (uint64_t *)buf + 12);
 	drgn_register_state_set_from_buffer(regs, sp, (uint64_t *)buf + 11);
 	drgn_register_state_set_range_from_buffer(regs, x19, x29, buf);
 	drgn_register_state_set_pc_from_register(prog, regs, x30);
 	*ret = regs;
-out:
-	drgn_object_deinit(&cpu_context_obj);
-	return err;
+	return NULL;
 }
 
 static struct drgn_error *

libdrgn/arch_ppc64.c
@@ -6,6 +6,7 @@
 #include <endian.h>
 #include <string.h>
 
+#include "cleanup.h"
 #include "drgn.h"
 #include "error.h"
 #include "platform.h" // IWYU pragma: associated
@@ -191,54 +192,49 @@ linux_kernel_get_initial_registers_ppc64(const struct drgn_object *task_obj,
 	struct drgn_error *err;
 	struct drgn_program *prog = drgn_object_program(task_obj);
 
-	struct drgn_object sp_obj;
-	drgn_object_init(&sp_obj, prog);
+	DRGN_OBJECT(sp_obj, prog);
 
 	// The top of the stack is saved in task->thread.ksp.
 	err = drgn_object_member_dereference(&sp_obj, task_obj, "thread");
 	if (err)
-		goto out;
+		return err;
 	err = drgn_object_member(&sp_obj, &sp_obj, "ksp");
 	if (err)
-		goto out;
+		return err;
 	uint64_t ksp;
 	err = drgn_object_read_unsigned(&sp_obj, &ksp);
 	if (err)
-		goto out;
+		return err;
 	// The previous stack pointer is stored at the top of the stack.
 	uint64_t r1;
 	err = drgn_program_read_u64(prog, ksp, false, &r1);
 	if (err)
-		goto out;
+		return err;
 
 	// The struct pt_regs is stored above the previous stack pointer.
 	struct drgn_qualified_type pt_regs_type;
 	err = drgn_program_find_type(prog, "struct pt_regs", NULL,
 				     &pt_regs_type);
 	if (err)
-		goto out;
+		return err;
 	uint64_t sizeof_pt_regs;
 	err = drgn_type_sizeof(pt_regs_type.type, &sizeof_pt_regs);
 	if (err)
-		goto out;
+		return err;
 
 	char buf[312];
 	err = drgn_program_read_memory(prog, buf, r1 - sizeof_pt_regs,
 				       sizeof(buf), false);
 	if (err)
-		goto out;
+		return err;
 
 	err = get_initial_registers_from_struct_ppc64(prog, buf, sizeof(buf),
 						      false, true, ret);
 	if (err)
-		goto out;
+		return err;
 
 	drgn_register_state_set_from_u64(prog, *ret, r1, r1);
-
-	err = NULL;
-out:
-	drgn_object_deinit(&sp_obj);
-	return err;
+	return NULL;
 }
 
 static struct drgn_error *
@@ -311,71 +307,53 @@ static struct drgn_error *
 linux_kernel_pgtable_iterator_create_ppc64(struct drgn_program * prog,
 					   struct pgtable_iterator **ret)
 {
-	uint64_t mmu_features;
 	struct drgn_error *err = NULL;
 	const uint64_t page_shift = prog->vmcoreinfo.page_shift;
-	struct drgn_object book3s, cur_cpu_spec, mmu_features_obj;
 
-	struct pgtable_iterator_ppc64 *it = malloc(sizeof(*it));
+	_cleanup_free_ struct pgtable_iterator_ppc64 *it = malloc(sizeof(*it));
 	if (!it)
 		return &drgn_enomem;
 
-	drgn_object_init(&book3s, prog);
-	drgn_object_init(&cur_cpu_spec, prog);
-	drgn_object_init(&mmu_features_obj, prog);
-
-	if (page_shift == 16) {
+	if (page_shift == 16)
 		it->pt_levels = pt_levels_radix_64k;
-	} else if (page_shift == 12) {
+	else if (page_shift == 12)
 		it->pt_levels = pt_levels_radix_4k;
-	} else {
-		err = drgn_error_create(DRGN_ERROR_OTHER,
-					"Unknown page size.");
-		goto out;
-	}
+	else
+		return drgn_error_create(DRGN_ERROR_OTHER, "unknown page size");
+
+	DRGN_OBJECT(tmp, prog);
 
 	// Only BOOK3S CPU family is supported, not BOOK3E.
 	err = drgn_program_find_object(prog, "interrupt_base_book3e", NULL,
-				       DRGN_FIND_OBJECT_ANY, &book3s);
+				       DRGN_FIND_OBJECT_ANY, &tmp);
 	if (!err) {
-		err = drgn_error_create(DRGN_ERROR_OTHER,
-					"virtual address translation is not available for BOOK3E CPU family");
-		goto out;
+		return drgn_error_create(DRGN_ERROR_OTHER,
+					 "virtual address translation is not available for BOOK3E CPU family");
 	}
 	if (err->code != DRGN_ERROR_LOOKUP)
-		goto out;
+		return err;
 	drgn_error_destroy(err);
 
 	// Identify the MMU type.
 	err = drgn_program_find_object(prog, "cur_cpu_spec", NULL,
-				       DRGN_FIND_OBJECT_ANY, &cur_cpu_spec);
+				       DRGN_FIND_OBJECT_ANY, &tmp);
 	if (err)
-		goto out;
-	err = drgn_object_member_dereference(&mmu_features_obj, &cur_cpu_spec,
-					     "mmu_features");
+		return err;
+	err = drgn_object_member_dereference(&tmp, &tmp, "mmu_features");
 	if (err)
-		goto out;
-
-	err = drgn_object_read_unsigned(&mmu_features_obj, &mmu_features);
+		return err;
+	uint64_t mmu_features;
+	err = drgn_object_read_unsigned(&tmp, &mmu_features);
 
 	if (err)
-		goto out;
-
+		return err;
 	if (!(mmu_features & 0x40)) {
-		err = drgn_error_create(DRGN_ERROR_OTHER,
-					"virtual address translation is only supported for Radix MMU");
-		goto out;
+		return drgn_error_create(DRGN_ERROR_OTHER,
+					 "virtual address translation is only supported for Radix MMU");
 	}
 
-	*ret = &it->it;
-
-out:
-	drgn_object_deinit(&book3s);
-	drgn_object_deinit(&cur_cpu_spec);
-	drgn_object_deinit(&mmu_features_obj);
-	if (err)
-		free(it);
-	return err;
+	*ret = &no_cleanup_ptr(it)->it;
+	return NULL;
 }
 
 static void

libdrgn/arch_s390x.c
@@ -205,19 +205,18 @@ linux_kernel_get_initial_registers_s390x(const struct drgn_object *task_obj,
 	struct drgn_program *prog = drgn_object_program(task_obj);
 	struct drgn_error *err;
 
-	struct drgn_object ctx;
-	drgn_object_init(&ctx, prog);
+	DRGN_OBJECT(ctx, prog);
 
 	err = drgn_object_member_dereference(&ctx, task_obj, "thread");
 	if (err)
-		goto out;
+		return err;
 	err = drgn_object_member(&ctx, &ctx, "ksp");
 	if (err)
-		goto out;
+		return err;
 	uint64_t ksp;
 	err = drgn_object_read_unsigned(&ctx, &ksp);
 	if (err)
-		goto out;
+		return err;
 
 	// r6-r15
 	uint64_t buf[10];
@@ -225,21 +224,16 @@ linux_kernel_get_initial_registers_s390x(const struct drgn_object *task_obj,
 	err = drgn_program_read_memory(prog, buf, ksp + stack_frame_gprs_offset,
 				       sizeof(buf), false);
 	if (err)
-		goto out;
+		return err;
 
 	struct drgn_register_state *regs =
 		drgn_register_state_create(r15, false);
-	if (!regs) {
-		err = &drgn_enomem;
-		goto out;
-	}
+	if (!regs)
+		return &drgn_enomem;
 	drgn_register_state_set_range_from_buffer(regs, r6, r15, buf);
 	drgn_register_state_set_pc_from_register(prog, regs, r14);
 	*ret = regs;
-	err = NULL;
-out:
-	drgn_object_deinit(&ctx);
-	return err;
+	return NULL;
 }
 
 struct pgtable_data {

libdrgn/arch_x86_64.c
@@ -443,15 +443,14 @@ linux_kernel_get_initial_registers_x86_64(const struct drgn_object *task_obj,
 	struct drgn_error *err;
 	struct drgn_program *prog = drgn_object_program(task_obj);
 
-	struct drgn_object sp_obj;
-	drgn_object_init(&sp_obj, prog);
+	DRGN_OBJECT(sp_obj, prog);
 
 	err = drgn_object_member_dereference(&sp_obj, task_obj, "thread");
 	if (err)
-		goto out;
+		return err;
 	err = drgn_object_member(&sp_obj, &sp_obj, "sp");
 	if (err)
-		goto out;
+		return err;
 
 	/*
 	 * Since Linux kernel commit 0100301bfdf5 ("sched/x86: Rewrite the
@@ -467,23 +466,23 @@ linux_kernel_get_initial_registers_x86_64(const struct drgn_object *task_obj,
 	if (!err) {
 		err = drgn_object_cast(&sp_obj, frame_type, &sp_obj);
 		if (err)
-			goto out;
+			return err;
 		err = drgn_object_dereference(&sp_obj, &sp_obj);
 		if (err)
-			goto out;
+			return err;
 		err = get_initial_registers_inactive_task_frame(&sp_obj, ret);
 	} else if (err->code == DRGN_ERROR_LOOKUP) {
 		drgn_error_destroy(err);
 		err = drgn_program_find_type(prog, "void **", NULL,
 					     &frame_type);
 		if (err)
-			goto out;
+			return err;
 		err = drgn_object_cast(&sp_obj, frame_type, &sp_obj);
 		if (err)
-			goto out;
+			return err;
 		err = drgn_object_dereference(&sp_obj, &sp_obj);
 		if (err)
-			goto out;
+			return err;
 		uint64_t frame_pointer;
 		err = drgn_object_read_unsigned(&sp_obj, &frame_pointer);
 		if (err)
@@ -495,8 +494,6 @@ linux_kernel_get_initial_registers_x86_64(const struct drgn_object *task_obj,
 						"invalid frame pointer");
 		}
 	}
-out:
-	drgn_object_deinit(&sp_obj);
 	return err;
 }
 

libdrgn/drgn.h.in
@@ -1375,8 +1375,8 @@ drgn_object_qualified_type(const struct drgn_object *obj)
 /**
  * Initialize a @ref drgn_object.
  *
- * The object is initialized to a @c NULL reference with a void type. This must
- * be paired with a call to @ref drgn_object_deinit().
+ * The object is initialized to an absent object with a void type. This must be
+ * paired with a call to @ref drgn_object_deinit().
  *
  * @param[in] obj Object to initialize.
  * @param[in] prog Program containing the object.
@@ -1392,6 +1392,28 @@ void drgn_object_init(struct drgn_object *obj, struct drgn_program *prog);
  * @param[in] obj Object to deinitialize.
  */
 void drgn_object_deinit(struct drgn_object *obj);
+
+/**
+ * Define and initialize a @ref drgn_object named @p obj that is automatically
+ * deinitialized when it goes out of scope.
+ *
+ * This is equivalent to
+ *
+ * ```
+ * struct drgn_object obj;
+ * drgn_object_init(&obj, prog);
+ * ...
+ * drgn_object_deinit(&obj);
+ * ```
+ *
+ * @param[in] obj Name of object.
+ * @param[in] prog Program containing the object.
+ */
+#define DRGN_OBJECT(obj, prog)					\
+	struct drgn_object obj					\
+	__attribute__((__cleanup__(drgn_object_deinit))) =	\
+	drgn_object_initializer(prog)
+struct drgn_object drgn_object_initializer(struct drgn_program *prog);
 
 /** Get the program that a @ref drgn_object is from. */
 static inline struct drgn_program *

libdrgn/language_c.c
@@ -799,18 +799,15 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 					       struct string_builder *sb)
 {
 	struct drgn_error *err;
-	struct drgn_object obj;
 	enum drgn_format_object_flags initializer_flags;
 	size_t brace, remaining_columns, start_columns;
 
-	drgn_object_init(&obj, prog);
+	DRGN_OBJECT(obj, prog);
 
 	/* First, try to fit everything on one line. */
 	brace = sb->len;
-	if (!string_builder_appendc(sb, '{')) {
-		err = &drgn_enomem;
-		goto out;
-	}
+	if (!string_builder_appendc(sb, '{'))
+		return &drgn_enomem;
 	if (__builtin_sub_overflow(one_line_columns, 1, &remaining_columns))
 		remaining_columns = 0;
 	for (;;) {
@@ -820,7 +817,7 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 		if (err == &drgn_stop)
 			break;
 		else if (err)
-			goto out;
+			return err;
 
 		if (!same_line) {
 			err = &drgn_line_wrap;
@@ -834,10 +831,8 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 				err = &drgn_line_wrap;
 				break;
 			}
-			if (!string_builder_appendc(sb, ' ')) {
-				err = &drgn_enomem;
-				goto out;
-			}
+			if (!string_builder_appendc(sb, ' '))
+				return &drgn_enomem;
 			remaining_columns--;
 		} else {
 			if (remaining_columns < 4) {
@@ -848,10 +843,8 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 				err = &drgn_line_wrap;
 				break;
 			}
-			if (!string_builder_append(sb, ", ")) {
-				err = &drgn_enomem;
-				goto out;
-			}
+			if (!string_builder_append(sb, ", "))
+				return &drgn_enomem;
 			remaining_columns -= 2;
 		}
 
@@ -860,7 +853,7 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 
 			err = iter->append_designation(iter, sb);
 			if (err)
-				goto out;
+				return err;
 			if (__builtin_sub_overflow(remaining_columns,
 						   sb->len - designation_start,
 						   &remaining_columns)) {
@@ -876,7 +869,7 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 		if (err == &drgn_line_wrap)
 			break;
 		else if (err)
-			goto out;
+			return err;
 
 		if (__builtin_sub_overflow(remaining_columns,
 					   sb->len - initializer_start,
@@ -890,16 +883,14 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 		if (sb->len == brace + 1) {
 			/* There were no initializers. */
 			if (string_builder_appendc(sb, '}'))
-				err = NULL;
+				return NULL;
 			else
-				err = &drgn_enomem;
-			goto out;
+				return &drgn_enomem;
 		} else if (remaining_columns >= 2) {
 			if (string_builder_append(sb, " }"))
-				err = NULL;
+				return NULL;
 			else
-				err = &drgn_enomem;
-			goto out;
+				return &drgn_enomem;
 		}
 		/* The final space and closing brace didn't fit. */
 	}
@@ -908,8 +899,7 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 
 	if (multi_line_columns == 0) {
 		/* We were asked to stay on one line. */
-		err = &drgn_line_wrap;
-		goto out;
+		return &drgn_line_wrap;
 	}
 
 	sb->len = brace + 1;
@@ -925,21 +915,19 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 		if (err == &drgn_stop)
 			break;
 		else if (err)
-			goto out;
+			return err;
 
 		newline = sb->len;
 		if (!string_builder_appendc(sb, '\n') ||
-		    !append_tabs(indent + 1, sb)) {
-			err = &drgn_enomem;
-			goto out;
-		}
+		    !append_tabs(indent + 1, sb))
+			return &drgn_enomem;
 
 		designation_start = sb->len;
 		line_columns = start_columns;
 		if (iter->append_designation) {
 			err = iter->append_designation(iter, sb);
 			if (err)
-				goto out;
+				return err;
 			if (__builtin_sub_overflow(line_columns,
 						   sb->len - designation_start,
 						   &line_columns))
@@ -964,25 +952,21 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 						&sb->str[designation_start],
 						len);
 					sb->len = newline + 1 + len;
-					if (!string_builder_appendc(sb, ',')) {
-						err = &drgn_enomem;
-						goto out;
-					}
+					if (!string_builder_appendc(sb, ','))
+						return &drgn_enomem;
 					remaining_columns -= len + 2;
 					continue;
 				}
 				if (len < start_columns) {
 					/* It fit on the new line. */
-					if (!string_builder_appendc(sb, ',')) {
-						err = &drgn_enomem;
-						goto out;
-					}
+					if (!string_builder_appendc(sb, ','))
+						return &drgn_enomem;
 					remaining_columns =
 						start_columns - len - 1;
 					continue;
 				}
 			} else if (err != &drgn_line_wrap) {
-				goto out;
+				return err;
 			}
 			/* It didn't fit. */
 			sb->len = initializer_start;
@@ -992,23 +976,16 @@ static struct drgn_error *c_format_initializer(struct drgn_program *prog,
 					   multi_line_columns,
 					   initializer_flags, sb);
 		if (err)
-			goto out;
-		if (!string_builder_appendc(sb, ',')) {
-			err = &drgn_enomem;
-			goto out;
-		}
+			return err;
+		if (!string_builder_appendc(sb, ','))
+			return &drgn_enomem;
 		remaining_columns = 0;
 	}
 
 	if (!string_builder_appendc(sb, '\n') || !append_tabs(indent, sb) ||
-	    !string_builder_appendc(sb, '}')) {
-		err = &drgn_enomem;
-		goto out;
-	}
-	err = NULL;
-out:
-	drgn_object_deinit(&obj);
-	return err;
+	    !string_builder_appendc(sb, '}'))
+		return &drgn_enomem;
+	return NULL;
 }
 
 struct compound_initializer_state {
@@ -1183,34 +1160,30 @@ c_format_compound_object(const struct drgn_object *obj,
 	if (!(flags & (DRGN_FORMAT_OBJECT_MEMBER_NAMES |
 		       DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS)) &&
 	    new->member < new->end) {
-		struct drgn_object member;
-		drgn_object_init(&member, drgn_object_program(obj));
+		DRGN_OBJECT(member, drgn_object_program(obj));
 		do {
 			struct drgn_qualified_type member_type;
 			uint64_t member_bit_field_size;
 			err = drgn_member_type(&new->end[-1], &member_type,
 					       &member_bit_field_size);
 			if (err)
-				break;
+				goto out;
 
 			err = drgn_object_slice(&member, obj, member_type,
 						new->end[-1].bit_offset,
 						member_bit_field_size);
 			if (err)
-				break;
+				goto out;
 
 			bool zero;
 			err = drgn_object_is_zero(&member, &zero);
 			if (err)
-				break;
+				goto out;
 			if (zero)
 				new->end--;
 			else
 				break;
 		} while (new->member < new->end);
-		drgn_object_deinit(&member);
-		if (err)
-			goto out;
 	}
 
 	err = c_format_initializer(drgn_object_program(obj), &iter.iter, indent,
@@ -1345,12 +1318,9 @@ c_format_pointer_object(const struct drgn_object *obj,
 		err = c_format_string(drgn_object_program(obj), uvalue,
 				      UINT64_MAX, sb);
 	} else {
-		struct drgn_object dereferenced;
-
-		drgn_object_init(&dereferenced, drgn_object_program(obj));
+		DRGN_OBJECT(dereferenced, drgn_object_program(obj));
 		err = drgn_object_dereference(&dereferenced, obj);
 		if (err) {
-			drgn_object_deinit(&dereferenced);
 			if (err->code == DRGN_ERROR_TYPE)
 				goto no_dereference;
 			return err;
@@ -1361,7 +1331,6 @@ c_format_pointer_object(const struct drgn_object *obj,
 		err = c_format_object_impl(&dereferenced, indent,
 					   one_line_columns, multi_line_columns,
 					   passthrough_flags, sb);
-		drgn_object_deinit(&dereferenced);
 	}
 	if (!err || (err->code != DRGN_ERROR_FAULT && err->code != DRGN_ERROR_OUT_OF_BOUNDS)) {
 		/* We either succeeded or hit a fatal error. */
@@ -1517,9 +1486,7 @@ c_format_array_object(const struct drgn_object *obj,
 	if (!(flags & (DRGN_FORMAT_OBJECT_ELEMENT_INDICES |
 		       DRGN_FORMAT_OBJECT_IMPLICIT_ELEMENTS)) &&
 	    iter.length) {
-		struct drgn_object element;
-
-		drgn_object_init(&element, drgn_object_program(obj));
+		DRGN_OBJECT(element, drgn_object_program(obj));
 		do {
 			bool zero;
 
@@ -1529,19 +1496,16 @@ c_format_array_object(const struct drgn_object *obj,
 						iter.element_bit_size,
 						0);
 			if (err)
-				break;
+				return err;
 
 			err = drgn_object_is_zero(&element, &zero);
 			if (err)
-				break;
+				return err;
 			if (zero)
 				iter.length--;
 			else
 				break;
 		} while (iter.length);
-		drgn_object_deinit(&element);
-		if (err)
-			return err;
 	}
 	return c_format_initializer(drgn_object_program(obj), &iter.iter,
 				    indent, one_line_columns,

commit f9105049de114b6ac754bef5c80d784af78d7fed

libdrgn: python: replace return no_cleanup_ptr() with return_ptr()
They're equivalent, but return_ptr() is preferred.

Fixes: 085b0bc9dbfa ("libdrgn: add DRGN_OBJECT scope guard")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -587,7 +587,7 @@ static PyObject *DrgnObject_compound_value(struct drgn_object *obj,
 				return NULL;
 		}
 	}
-	return no_cleanup_ptr(dict);
+	return_ptr(dict);
 }
 
 static PyObject *DrgnObject_array_value(struct drgn_object *obj,
@@ -624,7 +624,7 @@ static PyObject *DrgnObject_array_value(struct drgn_object *obj,
 			return NULL;
 		PyList_SET_ITEM(list, i, element_value);
 	}
-	return no_cleanup_ptr(list);
+	return_ptr(list);
 }
 
 static PyObject *DrgnObject_value_impl(struct drgn_object *obj)