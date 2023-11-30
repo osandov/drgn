osandov/drgn

commit 38939cce5e198a699ec8843cc656076ed2e4252b

helpers: add decorators to use the default program
To simplify converting helpers to allow omitting the prog argument, add
a couple of decorators that take care all of the messy details.

@takes_program_or_default is based on an example from Stephen Brennan,
and the rest handle progressively more complex calling conventions.

Also update drgndoc to be aware of these decorators and add some
boilerplate to the generated documentation.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/common/prog.py
@@ -1,0 +1,255 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+Program Decorators
+------------------
+
+The ``drgn.helpers.common.prog`` module provides decorators to transparently
+use the :ref:`default program argument <default-program>`.
+"""
+
+import functools
+import inspect
+import typing
+
+from drgn import Object, Program, get_default_prog
+
+__all__ = (
+    "takes_object_or_program_or_default",
+    "takes_program_or_default",
+)
+
+# This would require Python 3.10, but we don't need any of it at runtime.
+if typing.TYPE_CHECKING:
+    from typing import (  # novermin
+        Any,
+        Callable,
+        Concatenate,
+        Optional,
+        ParamSpec,
+        Protocol,
+        TypeVar,
+        overload,
+    )
+
+    P = ParamSpec("P")
+    R = TypeVar("R")
+    R_co = TypeVar("R_co", covariant=True)
+
+    class TakesProgram(Protocol[P, R_co]):
+        def __call__(self, prog: Program, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+    class TakesProgramOrDefault(Protocol[P, R_co]):
+        @overload
+        def __call__(self, prog: Program, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+        @overload
+        def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+    class TakesObjectOrProgramOrDefault(Protocol[P, R_co]):
+        @overload
+        def __call__(self, prog: Program, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+        @overload
+        def __call__(self, __obj: Object, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+        @overload
+        def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R_co:
+            ...
+
+
+def takes_program_or_default(f: "TakesProgram[P, R]") -> "TakesProgramOrDefault[P, R]":
+    """
+    Wrap a function taking a :class:`~drgn.Program` so that it uses the
+    :ref:`default program argument <default-program>` if omitted.
+
+    .. code-block:: python3
+
+        @takes_program_or_default
+        def my_helper(prog: Program, n: IntegerLike) -> Foo:
+            ...
+
+        my_helper(1)
+        # is equivalent to
+        my_helper(get_default_prog(), 1)
+
+        obj = Object(...)
+        my_helper(obj)
+        # is equivalent to
+        my_helper(obj.prog_, obj)
+    """
+    parameters_iter = iter(inspect.signature(f).parameters.values())
+    if next(parameters_iter).name != "prog":
+        raise TypeError("first parameter must be prog: Program")
+    param1 = None
+    for parameter in parameters_iter:
+        if parameter.kind not in (
+            inspect.Parameter.KEYWORD_ONLY,
+            inspect.Parameter.VAR_KEYWORD,
+        ):
+            param1 = parameter.name
+        break
+
+    if param1 is None:
+
+        @functools.wraps(f)
+        def wrapper(*args: "Any", **kwds: "Any") -> "R":
+            if (args and isinstance(args[0], Program)) or ("prog" in kwds):
+                return f(*args, **kwds)
+            else:
+                return f(get_default_prog(), *args, **kwds)
+
+    else:
+
+        @functools.wraps(f)
+        def wrapper(*args: "Any", **kwds: "Any") -> "R":
+            if args:
+                if isinstance(args[0], Program):
+                    return f(*args, **kwds)
+                elif isinstance(args[0], Object):
+                    return f(args[0].prog_, *args, **kwds)
+            elif "prog" in kwds:
+                return f(**kwds)
+            elif param1 in kwds:
+                arg1 = kwds[param1]
+                if isinstance(arg1, Object):
+                    return f(arg1.prog_, **kwds)
+            return f(get_default_prog(), *args, **kwds)
+
+    # Update the docstring for pydoc.
+    if wrapper.__doc__ is not None:
+        wrapper.__doc__ += ":param prog: Program, which may be omitted to use the default program argument.\n"
+    return wrapper
+
+
+def takes_object_or_program_or_default(
+    # This should be a Protocol instead of Callable, but there's currently no
+    # way for a Protocol to express that the second parameter can have any name
+    # and then use that name in the return type. See python/typing#1505.
+    f: "Callable[Concatenate[Program, Optional[Object], P], R]",
+) -> "TakesObjectOrProgramOrDefault[P, R]":
+    """
+    Wrap a function taking a :class:`~drgn.Program` and an optional
+    :class:`~drgn.Object` so that it accepts a ``Program`` *or* an ``Object``
+    *or neither*, in which case the :ref:`default program argument
+    <default-program>` is used.
+
+    .. code-block:: python3
+
+        @takes_object_or_program_or_default
+        def my_helper(prog: Program, obj: Optional[Object], n: IntegerLike) -> Foo:
+            ...
+
+        my_helper(prog, 1)
+        # is equivalent to
+        my_helper.__wrapped__(prog, None, 1)
+
+        obj = Object(...)
+        my_helper(obj, 1)
+        # is equivalent to
+        my_helper.__wrapped__(obj.prog_, obj, 1)
+
+        my_helper(1)
+        # is equivalent to
+        my_helper.__wrapped__(get_default_prog(), None, 1)
+
+        one_obj = Object(..., 1)
+        my_helper(one_obj)
+        # is equivalent to
+        my_helper.__wrapped__(one_obj.prog_, None, one_obj)
+
+    .. warning::
+
+        This cannot be used with positional parameters with a default value, as
+        that would create ambiguity. Keyword-only parameters with a default
+        value are OK.
+
+        .. code-block:: python3
+
+            # NOT ALLOWED
+            @takes_object_or_program_or_default
+            def my_helper(prog: Program, obj: Optional[Object], foo: str = ""): ...
+
+            # OK
+            @takes_object_or_program_or_default
+            def my_helper(prog: Program, obj: Optional[Object], *, foo: str = ""): ...
+
+    .. note::
+
+        The object parameter can be passed as a keyword, but because of
+        `limitations of the Python type system
+        <https://github.com/python/typing/issues/1505>`_, type checkers do
+        not recognize this.
+    """
+    signature = inspect.signature(f)
+    parameters = list(signature.parameters.values())
+    if parameters[0].name != "prog":
+        raise TypeError("first parameter must be prog: Program")
+    object_param = parameters[1].name
+    extra_params = []
+    for parameter in parameters[2:]:
+        if parameter.kind in (
+            inspect.Parameter.KEYWORD_ONLY,
+            inspect.Parameter.VAR_KEYWORD,
+        ):
+            break
+        if parameter.default != inspect.Parameter.empty:
+            raise ValueError(
+                f"{getattr(f, '__name__', str(f))} using @takes_object_or_program_or_default can't have positional parameters with defaults due to ambiguity"
+            )
+        extra_params.append(parameter.name)
+
+    if extra_params:
+        param2 = extra_params[0]
+
+        @functools.wraps(f)
+        def wrapper(*args: "Any", **kwds: "Any") -> "R":
+            if args:
+                if len(args) > len(extra_params) or extra_params[len(args) - 1] in kwds:
+                    if isinstance(args[0], Program):
+                        return f(args[0], None, *args[1:], **kwds)
+                    else:
+                        return f(args[0].prog_, *args, **kwds)
+                elif isinstance(args[0], Object):
+                    return f(args[0].prog_, None, *args, **kwds)
+            elif object_param in kwds:
+                return f(kwds[object_param].prog_, *args, **kwds)
+            elif "prog" in kwds:
+                kwds[object_param] = None
+                return f(*args, **kwds)
+            elif param2 in kwds:
+                arg2 = kwds[param2]
+                if isinstance(arg2, Object):
+                    return f(arg2.prog_, None, *args, **kwds)
+            return f(get_default_prog(), None, *args, **kwds)
+
+    else:
+
+        @functools.wraps(f)
+        def wrapper(*args: "Any", **kwds: "Any") -> "R":
+            if args:
+                if isinstance(args[0], Program):
+                    return f(args[0], None, *args[1:], **kwds)
+                else:
+                    return f(args[0].prog_, *args, **kwds)
+            elif object_param in kwds:
+                return f(kwds[object_param].prog_, *args, **kwds)
+            elif "prog" in kwds:
+                kwds[object_param] = None
+                return f(*args, **kwds)
+            return f(get_default_prog(), None, *args, **kwds)
+
+    # Update the signature for pydoc.
+    wrapper.__signature__ = signature.replace(  # type: ignore[attr-defined]
+        parameters=[
+            parameters[1].replace(annotation=typing.Union[Object, Program]),
+            *parameters[2:],
+        ]
+    )
+    return wrapper

docs/exts/drgndoc/parse.py
@@ -5,6 +5,7 @@ import ast
 import inspect
 import operator
 import os.path
+import re
 import stat
 from typing import (
     Any,
@@ -193,6 +194,48 @@ def _docstring_from_node(node: Optional[ast.AST]) -> Optional[str]:
     return inspect.cleandoc(text)
 
 
+def _transform_function(node: Function) -> Function:
+    signature = node.signatures[-1]
+
+    if (
+        signature.has_decorator("takes_program_or_default")
+        and signature.docstring is not None
+    ):
+        match = re.search(
+            r"^(\s*):(?:param|return|raises)", signature.docstring, flags=re.M
+        )
+        if match:
+            prefix = match.group(1)
+            pos = match.start()
+        else:
+            prefix = "\n\n"
+            pos = len(signature.docstring)
+        signature.docstring = "".join(
+            (
+                signature.docstring[:pos],
+                prefix,
+                ":param prog: Program, which :ref:`may be omitted to use the default program argument <default-program>`.",
+                signature.docstring[pos:],
+            )
+        )
+
+    if signature.has_decorator("takes_object_or_program_or_default"):
+        del signature.args.args[0]
+        signature.args.args[0].annotation = ast.Subscript(
+            value=ast.Name(id="Union", ctx=ast.Load()),
+            slice=ast.Tuple(
+                elts=[
+                    ast.Name(id="Object", ctx=ast.Load()),
+                    ast.Name(id="Program", ctx=ast.Load()),
+                ],
+                ctx=ast.Load(),
+            ),
+            ctx=ast.Load(),
+        )
+
+    return node
+
+
 class _ModuleVisitor(NodeVisitor):
     def visit(self, node: ast.AST) -> Tuple[Optional[str], Dict[str, NonModuleNode]]:
         self._attrs: Dict[str, NonModuleNode] = {}
@@ -240,7 +283,7 @@ class _ModuleVisitor(NodeVisitor):
             signatures.append(signature)
         else:
             signatures = [signature]
-        self._attrs[node.name] = Function(async_, signatures)
+        self._attrs[node.name] = _transform_function(Function(async_, signatures))
         # NB: we intentionally don't visit the function body.
 
     visit_FunctionDef = _visit_function

tests/helpers/common/test_prog.py
@@ -1,0 +1,363 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import contextlib
+import inspect
+import unittest.mock
+
+from drgn import NoDefaultProgramError, Object, get_default_prog, set_default_prog
+from drgn.helpers.common.prog import (
+    takes_object_or_program_or_default,
+    takes_program_or_default,
+)
+from tests import IdenticalMatcher, TestCase, mock_program
+
+
+def my_create_autospec(f):
+    mock = unittest.mock.create_autospec(f)
+    # unittest.mock.create_autospec() does this automatically since CPython
+    # commit f7fa62ef4422 ("bpo-17185: Add __signature__ to mock that can be
+    # used by inspect for signature (GH11048)") (in v3.8).
+    mock.__signature__ = inspect.signature(f)
+    return mock
+
+
+@contextlib.contextmanager
+def mock_default_prog():
+    try:
+        old_default_prog = get_default_prog()
+    except NoDefaultProgramError:
+        old_default_prog = None
+    prog = mock_program()
+    try:
+        set_default_prog(prog)
+        yield prog
+    finally:
+        set_default_prog(old_default_prog)
+
+
+class TestTakesProgramOrDefault(TestCase):
+    def setUp(self):
+        def f1(prog):
+            pass
+
+        self.mock1 = my_create_autospec(f1)
+
+        def f2(prog, x):
+            pass
+
+        self.mock2 = my_create_autospec(f2)
+
+        def f3(prog, x, s):
+            pass
+
+        self.mock3 = my_create_autospec(f3)
+
+    def test_explicit_prog_no_args(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock1)(prog)
+        self.mock1.assert_called_once_with(prog)
+
+    def test_explicit_prog_kwarg_no_args(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock1)(prog=prog)
+        self.mock1.assert_called_once_with(prog=prog)
+
+    def test_default_prog_no_args(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock1)()
+            self.mock1.assert_called_once_with(prog)
+
+    def test_explicit_prog_one_arg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock2)(prog, 1)
+        self.mock2.assert_called_once_with(prog, 1)
+
+    def test_explicit_prog_one_kwarg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock2)(prog, x=1)
+        self.mock2.assert_called_once_with(prog, x=1)
+
+    def test_explicit_prog_kwarg_one_kwarg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock2)(prog=prog, x=1)
+        self.mock2.assert_called_once_with(prog=prog, x=1)
+
+    def test_object_prog_one_arg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock2)(Object(prog, "int", 1))
+        self.mock2.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1))
+        )
+
+    def test_object_prog_one_kwarg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock2)(x=Object(prog, "int", 1))
+        self.mock2.assert_called_once_with(
+            prog, x=IdenticalMatcher(Object(prog, "int", 1))
+        )
+
+    def test_default_prog_one_arg(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock2)(1)
+            self.mock2.assert_called_once_with(prog, 1)
+
+    def test_default_prog_one_kwarg(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock2)(x=1)
+            self.mock2.assert_called_once_with(prog, x=1)
+
+    def test_explicit_prog_two_args(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(prog, 1, "foo")
+        self.mock3.assert_called_once_with(prog, 1, "foo")
+
+    def test_explicit_prog_one_arg_one_kwarg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(prog, 1, s="foo")
+        self.mock3.assert_called_once_with(prog, 1, s="foo")
+
+    def test_explicit_prog_two_kwargs(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(prog, x=1, s="foo")
+        self.mock3.assert_called_once_with(prog, x=1, s="foo")
+
+    def test_explicit_prog_kwarg_two_kwargs(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(prog=prog, x=1, s="foo")
+        self.mock3.assert_called_once_with(prog=prog, x=1, s="foo")
+
+    def test_object_prog_two_args(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(Object(prog, "int", 2), "foo")
+        self.mock3.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 2)), "foo"
+        )
+
+    def test_object_prog_one_arg_one_kwarg(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(Object(prog, "int", 2), s="foo")
+        self.mock3.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 2)), s="foo"
+        )
+
+    def test_object_prog_two_kwargs(self):
+        prog = mock_program()
+        takes_program_or_default(self.mock3)(x=Object(prog, "int", 2), s="foo")
+        self.mock3.assert_called_once_with(
+            prog, x=IdenticalMatcher(Object(prog, "int", 2)), s="foo"
+        )
+
+    def test_default_prog_two_args(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock3)(2, "foo")
+            self.mock3.assert_called_once_with(prog, 2, "foo")
+
+    def test_default_prog_one_arg_one_kwarg(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock3)(2, s="foo")
+            self.mock3.assert_called_once_with(prog, 2, s="foo")
+
+    def test_default_prog_two_kwargs(self):
+        with mock_default_prog() as prog:
+            takes_program_or_default(self.mock3)(x=2, s="foo")
+            self.mock3.assert_called_once_with(prog, x=2, s="foo")
+
+
+class TestTakesObjectOrProgramOrDefault(TestCase):
+    def setUp(self):
+        def f1(prog, obj):
+            pass
+
+        self.mock1 = my_create_autospec(f1)
+
+        def f2(prog, obj, x):
+            pass
+
+        self.mock2 = my_create_autospec(f2)
+
+        def f3(prog, obj, x, s):
+            pass
+
+        self.mock3 = my_create_autospec(f3)
+
+    def test_explicit_prog_no_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock1)(prog)
+        self.mock1.assert_called_once_with(prog, None)
+
+    def test_explicit_prog_kwarg_no_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock1)(prog=prog)
+        self.mock1.assert_called_once_with(prog=prog, obj=None)
+
+    def test_obj_no_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock1)(Object(prog, "int", 1))
+        self.mock1.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1))
+        )
+
+    def test_obj_kwarg_no_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock1)(obj=Object(prog, "int", 1))
+        self.mock1.assert_called_once_with(
+            prog, obj=IdenticalMatcher(Object(prog, "int", 1))
+        )
+
+    def test_default_prog_no_args(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock1)()
+            self.mock1.assert_called_once_with(prog, None)
+
+    def test_explicit_prog_one_arg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(prog, 1)
+        self.mock2.assert_called_once_with(prog, None, 1)
+
+    def test_explicit_prog_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(prog, x=1)
+        self.mock2.assert_called_once_with(prog, None, x=1)
+
+    def test_explicit_prog_kwarg_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(prog=prog, x=1)
+        self.mock2.assert_called_once_with(prog=prog, obj=None, x=1)
+
+    def test_obj_one_arg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(Object(prog, "int", 1), 2)
+        self.mock2.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1)), 2
+        )
+
+    def test_obj_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(Object(prog, "int", 1), x=2)
+        self.mock2.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1)), x=2
+        )
+
+    def test_obj_kwarg_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(obj=Object(prog, "int", 1), x=2)
+        self.mock2.assert_called_once_with(
+            prog, obj=IdenticalMatcher(Object(prog, "int", 1)), x=2
+        )
+
+    def test_object_prog_one_arg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(Object(prog, "int", 2))
+        self.mock2.assert_called_once_with(
+            prog, None, IdenticalMatcher(Object(prog, "int", 2))
+        )
+
+    def test_object_prog_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock2)(x=Object(prog, "int", 2))
+        self.mock2.assert_called_once_with(
+            prog, None, x=IdenticalMatcher(Object(prog, "int", 2))
+        )
+
+    def test_default_prog_one_arg(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock2)(2)
+            self.mock2.assert_called_once_with(prog, None, 2)
+
+    def test_default_prog_one_kwarg(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock2)(x=2)
+            self.mock2.assert_called_once_with(prog, None, x=2)
+
+    def test_explicit_prog_two_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(prog, 1, "foo")
+        self.mock3.assert_called_once_with(prog, None, 1, "foo")
+
+    def test_explicit_prog_one_arg_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(prog, 1, s="foo")
+        self.mock3.assert_called_once_with(prog, None, 1, s="foo")
+
+    def test_explicit_prog_two_kwargs(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(prog, x=1, s="foo")
+        self.mock3.assert_called_once_with(prog, None, x=1, s="foo")
+
+    def test_explicit_prog_kwarg_two_kwargs(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(prog=prog, x=1, s="foo")
+        self.mock3.assert_called_once_with(prog=prog, obj=None, x=1, s="foo")
+
+    def test_obj_two_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(Object(prog, "int", 1), 2, "foo")
+        self.mock3.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1)), 2, "foo"
+        )
+
+    def test_obj_one_arg_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(
+            Object(prog, "int", 1), 2, s="foo"
+        )
+        self.mock3.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1)), 2, s="foo"
+        )
+
+    def test_obj_two_kwargs(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(
+            Object(prog, "int", 1), x=2, s="foo"
+        )
+        self.mock3.assert_called_once_with(
+            prog, IdenticalMatcher(Object(prog, "int", 1)), x=2, s="foo"
+        )
+
+    def test_obj_kwarg_two_kwargs(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(
+            obj=Object(prog, "int", 1), x=2, s="foo"
+        )
+        self.mock3.assert_called_once_with(
+            prog, obj=IdenticalMatcher(Object(prog, "int", 1)), x=2, s="foo"
+        )
+
+    def test_object_prog_two_args(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(Object(prog, "int", 2), "foo")
+        self.mock3.assert_called_once_with(
+            prog, None, IdenticalMatcher(Object(prog, "int", 2)), "foo"
+        )
+
+    def test_object_prog_one_arg_one_kwarg(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(Object(prog, "int", 2), s="foo")
+        self.mock3.assert_called_once_with(
+            prog, None, IdenticalMatcher(Object(prog, "int", 2)), s="foo"
+        )
+
+    def test_object_prog_two_kwargs(self):
+        prog = mock_program()
+        takes_object_or_program_or_default(self.mock3)(
+            x=Object(prog, "int", 2), s="foo"
+        )
+        self.mock3.assert_called_once_with(
+            prog, None, x=IdenticalMatcher(Object(prog, "int", 2)), s="foo"
+        )
+
+    def test_default_prog_two_args(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock3)(2, "foo")
+            self.mock3.assert_called_once_with(prog, None, 2, "foo")
+
+    def test_default_prog_one_arg_one_kwarg(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock3)(2, s="foo")
+            self.mock3.assert_called_once_with(prog, None, 2, s="foo")
+
+    def test_default_prog_two_kwargs(self):
+        with mock_default_prog() as prog:
+            takes_object_or_program_or_default(self.mock3)(x=2, s="foo")
+            self.mock3.assert_called_once_with(prog, None, x=2, s="foo")

commit 7c881f105c15f64991c05ef46fc964ab63014584

drgn.helpers.common.prog: fix mypy on Python < 3.10
Fixes: 38939cce5e19 ("helpers: add decorators to use the default program")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/common/prog.py
@@ -20,18 +20,15 @@ __all__ = (
     "takes_program_or_default",
 )
 
-# This would require Python 3.10, but we don't need any of it at runtime.
+# We don't need any of this at runtime.
 if typing.TYPE_CHECKING:
-    from typing import (  # novermin
-        Any,
-        Callable,
-        Concatenate,
-        Optional,
-        ParamSpec,
-        Protocol,
-        TypeVar,
-        overload,
-    )
+    import sys
+    from typing import Any, Optional, Protocol, TypeVar, overload  # novermin
+
+    if sys.version_info < (3, 10):
+        from typing_extensions import Callable, Concatenate, ParamSpec
+    else:
+        from typing import Callable, Concatenate, ParamSpec  # novermin
 
     P = ParamSpec("P")
     R = TypeVar("R")