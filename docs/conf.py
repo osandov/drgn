osandov/drgn

commit 98cf8d51ebc2fbd81dbea524fd12b1845205a304

docs: make page body wider
The Alabaster theme's body is awkwardly narrow, especially for code
blocks. Override the theme options with something wider that doesn't
look too overwhelming on large screens.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

docs/conf.py
@@ -44,6 +44,9 @@ html_theme_options = {
     "logo": "logo.png",
     "logo_name": True,
     "logo_text_align": "center",
+    # Make the page body wider.
+    "page_width": "50%",
+    "body_max_width": "auto",
     "github_user": "osandov",
     "github_repo": "drgn",
     "github_button": True,

commit 54740b75fa75af5c574506ff35906b83249e06e2

docs: Prevent jarring 50% width for medium screens
For screens that aren't terribly wide, there's a break point where the
current documentation design will snap to 50% width, and the text is
difficult to read due to the narrow layout. Rather than using 50% as the
rule, let's setup a couple good breakpoints that are wider than
Alabaster's default.

In particular, at the 875px breakpoint the sidebar appears, removing
220px from the content. So add a breakpoint at 875 + 220 px, allowing
the content width to grow up until that point, at which point we can cap
the width.

Fixes: 98cf8d51 ("docs: make page body wider")

Signed-off-by: Stephen Brennan <stephen@brennan.io>

docs/_static/custom.css
@@ -6,3 +6,14 @@ div.admonition p.admonition-title {
     font-size: 17px;
     font-weight: bold;
 }
+
+@media screen and (min-width: 875px) {
+    div.document {
+        width: 100%;
+    }
+}
+@media screen and (min-width: 1095px) {
+    div.document {
+        width: 1095px;
+    }
+}

docs/conf.py
@@ -41,9 +41,6 @@ html_theme_options = {
     "logo": "logo.png",
     "logo_name": True,
     "logo_text_align": "center",
-    # Make the page body wider.
-    "page_width": "50%",
-    "body_max_width": "auto",
     "github_user": "osandov",
     "github_repo": "drgn",
     "github_button": True,