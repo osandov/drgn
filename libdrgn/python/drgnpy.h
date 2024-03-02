a25f13598d831f5faad59dbefe204c9f716724c3
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-03-01 16:46:53 -0800

    python: Add helper for returning list of Symbols
    This will be reused in an upcoming commit.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- libdrgn/python/drgnpy.h
+++ libdrgn/python/drgnpy.h
@@ -304,6 +304,8 @@ Program *program_from_kernel(PyObject *self);
 Program *program_from_pid(PyObject *self, PyObject *args, PyObject *kwds);
 
 PyObject *Symbol_wrap(struct drgn_symbol *sym, PyObject *name_obj);
+PyObject *Symbol_list_wrap(struct drgn_symbol **symbols, size_t count,
+			   PyObject *name_obj);
 
 PyObject *Thread_wrap(struct drgn_thread *drgn_thread);
 
