235fab31d79be3a61411a961b30a0cca1886fe93
Omar Sandoval <osandov@osandov.com>
2024-04-01 16:45:06 -0700

    helpers.linux.stackdepot: update stack_depot_fetch() for Linux 6.9 and 6.8.2
    pool_index in depot_stack_handle_t was changed to be offset by 1, which
    broke stack_depot_fetch(). We can hack around it with trial and error.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/stackdepot.py
+++ drgn/helpers/linux/stackdepot.py
@@ -10,7 +10,7 @@ the stack trace storage from :linux:`include/linux/stackdepot.h` used by KASAN
 and other kernel debugging tools.
 """
 
-from typing import Optional
+from typing import Optional, Tuple
 
 from drgn import Object, StackTrace, cast, reinterpret
 
@@ -25,24 +25,57 @@ def stack_depot_fetch(handle: Object) -> Optional[StackTrace]:
     :return: The stack trace, or ``None`` if not available.
     """
     prog = handle.prog_
+    handle = handle.read_()
     handle_parts = reinterpret("union handle_parts", handle)
 
     # Renamed in Linux kernel commit 961c949b012f ("lib/stackdepot: rename slab
     # to pool") (in v6.3).
     try:
-        pool = prog["stack_pools"][handle_parts.pool_index]
+        pools = prog["stack_pools"]
     except KeyError:
-        pool = prog["stack_slabs"][handle_parts.slabindex]
-
-    if not pool:
-        return None
+        pools = prog["stack_slabs"]
+        pool_index = handle_parts.slabindex.value_()
+        try_offsets: Tuple[int, ...] = (0,)
+        offset_cached = True
+    else:
+        pool_index = handle_parts.pool_index.value_()
+        # Linux kernel commit 3ee34eabac2a ("lib/stackdepot: fix first entry
+        # having a 0-handle") (in v6.9), which was also backported to some
+        # stable branches, changed pool_index to mean the actual index + 1. The
+        # only way to tell is to try both pool_index - 1 and pool_index (as
+        # long as they're in bounds). Once we find a match, we can cache the
+        # correct offset.
+        try:
+            try_offsets = (prog.cache["stack_depot_pool_index_offset"],)
+            offset_cached = True
+        except KeyError:
+            if pool_index == 0:
+                try_offsets = (0,)
+            elif pool_index == prog["pools_num"].value_():
+                try_offsets = (-1,)
+            else:
+                try_offsets = (-1, 0)
+            offset_cached = False
 
     # This has remained the same since the stack depot was introduced in Linux
     # kernel commit cd11016e5f52 ("mm, kasan: stackdepot implementation. Enable
     # stackdepot for SLAB") (in v4.6), when it was known as STACK_ALLOC_ALIGN.
     DEPOT_STACK_ALIGN = 4
 
-    record = cast(
-        "struct stack_record *", pool + (handle_parts.offset << DEPOT_STACK_ALIGN)
-    )
-    return prog.stack_trace_from_pcs([record.entries[x] for x in range(record.size)])
+    for offset in try_offsets:
+        pool = pools[pool_index + offset].read_()
+        if pool:
+            record = cast(
+                "struct stack_record *",
+                pool + (handle_parts.offset << DEPOT_STACK_ALIGN),
+            )
+            # If there are other offsets to try, make sure that this is the
+            # correct record.
+            if offset != try_offsets[-1] and record.handle.handle != handle:
+                continue
+            if not offset_cached:
+                prog.cache["stack_depot_pool_index_offset"] = offset
+            return prog.stack_trace_from_pcs(
+                [record.entries[x] for x in range(record.size)]
+            )
+    return None
