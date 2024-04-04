a8551604ae01fd24f5c2dec8267244876b1959c0
Omar Sandoval <osandov@osandov.com>
2024-04-04 15:17:04 -0700

    helpers.common.type: add member_at_offset() helper
    This is basically the opposite of offsetof(). I've wanted it to figure
    out what member an offset returned by slab_object_info() or
    identify_address() corresponds to.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/common/type.py
+++ drgn/helpers/common/type.py
@@ -10,12 +10,16 @@ with types in ways that aren't provided by the core drgn library.
 """
 
 import enum
+import operator
 import typing
-from typing import Container
+from typing import Container, List, Tuple
 
-from drgn import Type
+from drgn import IntegerLike, Type, TypeKind, TypeMember, sizeof
 
-__all__ = ("enum_type_to_class",)
+__all__ = (
+    "enum_type_to_class",
+    "member_at_offset",
+)
 
 
 def enum_type_to_class(
@@ -38,3 +42,251 @@ def enum_type_to_class(
         if name not in exclude
     ]
     return enum.IntEnum(name, enumerators)  # type: ignore  # python/mypy#4865
+
+
+def member_at_offset(type: Type, offset: IntegerLike) -> str:
+    """
+    Return the name of the member at an offset in a type.
+
+    This is effectively the opposite of :func:`~drgn.offsetof()`.
+
+    >>> prog.type('struct list_head')
+    struct list_head {
+            struct list_head *next;
+            struct list_head *prev;
+    }
+    >>> member_at_offset(prog.type('struct list_head'), 0)
+    'next'
+    >>> member_at_offset(prog.type('struct list_head'), 8)
+    'prev'
+
+    This includes nested structures and array elements:
+
+    >>> prog.type('struct sigpending')
+    struct sigpending {
+            struct list_head list;
+            sigset_t signal;
+    }
+    >>> prog.type('sigset_t')
+    typedef struct {
+            unsigned long sig[1];
+    } sigset_t
+    >>> member_at_offset(prog.type('struct sigpending'), 0)
+    'list.next'
+    >>> member_at_offset(prog.type('struct sigpending'), 8)
+    'list.prev'
+    >>> member_at_offset(prog.type('struct sigpending'), 16)
+    'signal.sig[0]'
+
+    This also includes all possible matches for a union:
+
+    >>> prog.type('union mc_target')
+    union mc_target {
+            struct folio *folio;
+            swp_entry_t ent;
+    }
+    >>> prog.type('swp_entry_t')
+    typedef struct {
+            unsigned long val;
+    } swp_entry_t
+    >>> member_at_offset(prog.type('union mc_target'), 0)
+    'folio or ent.val'
+
+    Offsets in the middle of a member are represented:
+
+    >>> member_at_offset(prog.type('struct list_head'), 4)
+    'next+0x4'
+
+    Offsets in padding or past the end of the type are also represented:
+
+    >>> prog.type('struct autogroup')
+    struct autogroup {
+            struct kref kref;
+            struct task_group *tg;
+            struct rw_semaphore lock;
+            unsigned long id;
+            int nice;
+    }
+    >>> member_at_offset(prog.type('struct autogroup'), 4)
+    '<padding between kref and tg>'
+    >>> member_at_offset(prog.type('struct autogroup'), 70)
+    '<padding at end>'
+    >>> member_at_offset(prog.type('struct autogroup'), 72)
+    '<end>'
+    >>> member_at_offset(prog.type('struct autogroup'), 80)
+    '<past end>'
+
+    :param type: Type to check.
+    :param offset: Offset in bytes.
+    :raises TypeError: if *type* is not a structure, union, class, or array
+        type (or a typedef of one of those)
+    """
+    bit_offset = operator.index(offset) * 8
+
+    while type.kind == TypeKind.TYPEDEF:
+        type = type.type
+    if type.kind not in (
+        TypeKind.STRUCT,
+        TypeKind.UNION,
+        TypeKind.CLASS,
+        TypeKind.ARRAY,
+    ):
+        raise TypeError("must be compound type or array")
+
+    # Chain of member accesses and array subscripts that we've followed.
+    chain = []
+    # We traverse all union members in a depth-first search. This stack stores
+    # the members that still need to be explored, along with the remaining
+    # bit_offset from that member and the length of the chain leading to that
+    # member.
+    stack: List[Tuple[TypeMember, int, int]] = []
+    results = []
+
+    # When we've reached the end of a chain, add it to the results and go to
+    # the next member in the stack (if any).
+    def emit_and_pop_member() -> bool:
+        nonlocal type, bit_offset
+
+        if bit_offset:
+            if (bit_offset & 7) == 0:
+                chain.append("+")
+                chain.append(hex(bit_offset // 8))
+            else:
+                chain.append("+")
+                chain.append(str(bit_offset))
+                chain.append(" bits")
+        results.append("".join(chain))
+
+        if not stack:
+            return False
+
+        member, parent_bit_offset, chain_len = stack.pop()
+
+        type = member.type
+        bit_offset = parent_bit_offset - member.bit_offset
+        del chain[chain_len:]
+        if member.name is not None:
+            if chain:
+                chain.append(".")
+            chain.append(member.name)
+
+        return True
+
+    while True:
+        if type.kind == TypeKind.TYPEDEF:  # type: ignore[comparison-overlap]  # python/mypy#17096
+            type = type.type
+        elif type.kind == TypeKind.ARRAY:
+            element_bit_size = sizeof(type.type) * 8
+            # Treat incomplete arrays as if they have infinite size.
+            if type.length is None or bit_offset < type.length * element_bit_size:
+                i = bit_offset // element_bit_size
+                bit_offset -= i * element_bit_size
+                chain.append(f"[{i}]")
+                type = type.type
+            else:
+                if bit_offset == type.length * element_bit_size:
+                    chain.append("<end>")
+                else:
+                    chain.append("<past end>")
+                bit_offset = 0
+                if not emit_and_pop_member():
+                    break
+        else:
+            members = getattr(type, "members", None)
+            if members is None:
+                if not emit_and_pop_member():
+                    break
+                continue
+
+            orig_bit_offset = bit_offset
+            orig_chain_len = len(chain)
+
+            # At first, we go forwards through the members. If this is a union,
+            # then we go backwards through the rest once we've found a match.
+            # This allows us to (1) avoid the stack as an optimization for the
+            # common case of structures and (2) return results sorted by
+            # declaration order in the source code.
+            i = 0
+            end = len(members)
+            step = 1
+            while i != end:
+                member = members[i]
+                bit_size = member.bit_field_size
+                if bit_size is None:
+                    try:
+                        bit_size = sizeof(member.type) * 8
+                    except TypeError:
+                        # Ignore incomplete members other than arrays.
+                        if member.type.kind != TypeKind.ARRAY:
+                            i += step
+                            continue
+                if (
+                    member.bit_offset <= bit_offset
+                    # Treat incomplete arrays as if they have infinite size.
+                    and (bit_size is None or bit_offset < member.bit_offset + bit_size)
+                ):
+                    if step == 1:
+                        step = -1
+                        if type.kind == TypeKind.UNION:
+                            i, end = end, i
+                        else:
+                            # Set i so that we break on the next iteration.
+                            i = end + 1
+
+                        type = member.type
+                        bit_offset -= member.bit_offset
+                        if member.name is not None:
+                            if chain:
+                                chain.append(".")
+                            chain.append(member.name)
+                    else:
+                        stack.append((member, orig_bit_offset, orig_chain_len))
+                i += step
+            if step == 1:
+                # No matching members.
+                bit_size = sizeof(type) * 8
+                if bit_offset == bit_size:
+                    chain.append("<end>")
+                elif bit_offset > bit_size:
+                    chain.append("<past end>")
+                else:
+                    prev_member = None
+                    next_member = None
+                    for member in members:
+                        if member.bit_offset < bit_offset:
+                            if (
+                                prev_member is None
+                                or member.bit_offset > prev_member.bit_offset
+                            ):
+                                prev_member = member
+                        else:
+                            if (
+                                next_member is None
+                                or member.bit_offset < next_member.bit_offset
+                            ):
+                                next_member = member
+                    if chain:
+                        chain.append(".")
+                    if prev_member and next_member:
+                        chain.append("<padding between ")
+                        chain.append(
+                            "<unnamed>"
+                            if prev_member.name is None
+                            else prev_member.name
+                        )
+                        chain.append(" and ")
+                        chain.append(
+                            "<unnamed>"
+                            if next_member.name is None
+                            else next_member.name
+                        )
+                        chain.append(">")
+                    elif next_member:
+                        chain.append("<padding at beginning>")
+                    else:
+                        chain.append("<padding at end>")
+                bit_offset = 0
+                if not emit_and_pop_member():
+                    break
+
+    return " or ".join(results)
---
+++ tests/helpers/common/test_type.py
@@ -0,0 +1,415 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+
+from drgn import TypeMember
+from drgn.helpers.common.type import member_at_offset
+from tests import TestCase, mock_program
+
+
+class TestMemberAtOffset(TestCase):
+    def test_simple(self):
+        prog = mock_program()
+        point_type = prog.struct_type(
+            "point",
+            8,
+            (
+                TypeMember(prog.int_type("int", 4, True), "x", 0),
+                TypeMember(prog.int_type("int", 4, True), "y", 32),
+            ),
+        )
+        self.assertEqual(member_at_offset(point_type, 0), "x")
+        self.assertEqual(member_at_offset(point_type, 4), "y")
+
+    def test_unnamed_member(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            8,
+            (
+                TypeMember(
+                    prog.struct_type(
+                        None,
+                        8,
+                        (
+                            TypeMember(prog.int_type("int", 4, True), "x", 0),
+                            TypeMember(prog.int_type("int", 4, True), "y", 32),
+                        ),
+                    ),
+                    None,
+                ),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 0), "x")
+        self.assertEqual(member_at_offset(type, 4), "y")
+
+    def test_end(self):
+        prog = mock_program()
+        point_type = prog.struct_type(
+            "point",
+            8,
+            (
+                TypeMember(prog.int_type("int", 4, True), "x", 0),
+                TypeMember(prog.int_type("int", 4, True), "y", 32),
+            ),
+        )
+        self.assertEqual(member_at_offset(point_type, 8), "<end>")
+        self.assertEqual(member_at_offset(point_type, 9), "<past end>")
+
+    def test_padding_between_members(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.int_type("int", 4, True), "i", 0),
+                TypeMember(prog.int_type("long", 8, True), "l", 64),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 4), "<padding between i and l>")
+
+    def test_padding_between_unnamed_members(self):
+        prog = mock_program()
+
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.int_type("int", 4, True), "i"),
+                TypeMember(
+                    prog.struct_type(
+                        None, 8, (TypeMember(prog.int_type("long", 8, True), "l"),)
+                    ),
+                    None,
+                    64,
+                ),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 4), "<padding between i and <unnamed>>")
+
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(
+                    prog.struct_type(
+                        None, 4, (TypeMember(prog.int_type("int", 4, True), "i"),)
+                    ),
+                    None,
+                ),
+                TypeMember(prog.int_type("long", 8, True), "l", 64),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 4), "<padding between <unnamed> and l>")
+
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(
+                    prog.struct_type(
+                        None, 4, (TypeMember(prog.int_type("int", 4, True), "i"),)
+                    ),
+                    None,
+                ),
+                TypeMember(
+                    prog.struct_type(
+                        None, 8, (TypeMember(prog.int_type("long", 8, True), "l"),)
+                    ),
+                    None,
+                    64,
+                ),
+            ),
+        )
+        self.assertEqual(
+            member_at_offset(type, 4), "<padding between <unnamed> and <unnamed>>"
+        )
+
+    def test_padding_at_end(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.int_type("long", 8, True), "l", 0),
+                TypeMember(prog.int_type("int", 4, True), "i", 64),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 12), "<padding at end>")
+
+    def test_padding_at_beginning(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (TypeMember(prog.int_type("int", 4, True), "i", 32),),
+        )
+        self.assertEqual(member_at_offset(type, 0), "<padding at beginning>")
+
+    def test_offset(self):
+        prog = mock_program()
+        point_type = prog.struct_type(
+            "point",
+            8,
+            (
+                TypeMember(prog.int_type("int", 4, True), "x", 0),
+                TypeMember(prog.int_type("int", 4, True), "y", 32),
+            ),
+        )
+        self.assertEqual(member_at_offset(point_type, 2), "x+0x2")
+        self.assertEqual(member_at_offset(point_type, 6), "y+0x2")
+
+    def test_nested(self):
+        prog = mock_program()
+        point_type = prog.struct_type(
+            "point",
+            8,
+            (
+                TypeMember(prog.int_type("int", 4, True), "x", 0),
+                TypeMember(prog.int_type("int", 4, True), "y", 32),
+            ),
+        )
+        line_segment_type = prog.struct_type(
+            "line_segment",
+            16,
+            (TypeMember(point_type, "a"), TypeMember(point_type, "b", 64)),
+        )
+        self.assertEqual(member_at_offset(line_segment_type, 0), "a.x")
+        self.assertEqual(member_at_offset(line_segment_type, 4), "a.y")
+        self.assertEqual(member_at_offset(line_segment_type, 8), "b.x")
+        self.assertEqual(member_at_offset(line_segment_type, 12), "b.y")
+
+    def test_array(self):
+        prog = mock_program()
+        type = prog.array_type(prog.int_type("int", 4, True), 2)
+        self.assertEqual(member_at_offset(type, 0), "[0]")
+        self.assertEqual(member_at_offset(type, 4), "[1]")
+
+    def test_array_end(self):
+        prog = mock_program()
+        type = prog.array_type(prog.int_type("int", 4, True), 2)
+        self.assertEqual(member_at_offset(type, 8), "<end>")
+        self.assertEqual(member_at_offset(type, 9), "<past end>")
+
+    def test_array_padding_at_end_of_element(self):
+        prog = mock_program()
+        type = prog.array_type(
+            prog.struct_type(
+                None,
+                16,
+                (
+                    TypeMember(prog.int_type("long", 8, True), "l", 0),
+                    TypeMember(prog.int_type("int", 4, True), "i", 64),
+                ),
+            ),
+            2,
+        )
+        self.assertEqual(member_at_offset(type, 12), "[0].<padding at end>")
+
+    def test_array_offset(self):
+        prog = mock_program()
+        type = prog.array_type(prog.int_type("int", 4, True), 2)
+        self.assertEqual(member_at_offset(type, 1), "[0]+0x1")
+        self.assertEqual(member_at_offset(type, 7), "[1]+0x3")
+
+    def test_array_of_structs(self):
+        prog = mock_program()
+        type = prog.array_type(
+            prog.struct_type(
+                "point",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.int_type("int", 4, True), "y", 32),
+                ),
+            ),
+            2,
+        )
+        self.assertEqual(member_at_offset(type, 0), "[0].x")
+        self.assertEqual(member_at_offset(type, 4), "[0].y")
+        self.assertEqual(member_at_offset(type, 8), "[1].x")
+        self.assertEqual(member_at_offset(type, 12), "[1].y")
+
+    def test_array_of_structs_end(self):
+        prog = mock_program()
+        type = prog.array_type(
+            prog.struct_type(
+                "point",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.int_type("int", 4, True), "y", 32),
+                ),
+            ),
+            2,
+        )
+        self.assertEqual(member_at_offset(type, 16), "<end>")
+        self.assertEqual(member_at_offset(type, 17), "<past end>")
+
+    def test_array_member(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.array_type(prog.int_type("int", 4, True), 3), "a", 0),
+                TypeMember(prog.array_type(prog.int_type("int", 4, True), 1), "b", 96),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 0), "a[0]")
+        self.assertEqual(member_at_offset(type, 4), "a[1]")
+        self.assertEqual(member_at_offset(type, 8), "a[2]")
+        self.assertEqual(member_at_offset(type, 12), "b[0]")
+
+    def test_array_member_end(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.array_type(prog.int_type("int", 4, True), 3), "a", 0),
+                TypeMember(prog.array_type(prog.int_type("int", 4, True), 1), "b", 96),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 16), "<end>")
+        self.assertEqual(member_at_offset(type, 17), "<past end>")
+
+    def test_incomplete_array(self):
+        prog = mock_program()
+        type = prog.array_type(prog.int_type("int", 4, True), None)
+        self.assertEqual(member_at_offset(type, 0), "[0]")
+        self.assertEqual(member_at_offset(type, 8), "[2]")
+
+    def test_incomplete_array_offset(self):
+        prog = mock_program()
+        type = prog.array_type(prog.int_type("int", 4, True), None)
+        self.assertEqual(member_at_offset(type, 3), "[0]+0x3")
+        self.assertEqual(member_at_offset(type, 10), "[2]+0x2")
+
+    def test_incomplete_array_member(self):
+        prog = mock_program()
+        type = prog.struct_type(
+            None,
+            16,
+            (
+                TypeMember(prog.int_type("int", 4, True), "n", 0),
+                TypeMember(
+                    prog.array_type(prog.int_type("int", 4, True), None), "a", 32
+                ),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 0), "n")
+        self.assertEqual(member_at_offset(type, 4), "a[0]")
+        self.assertEqual(member_at_offset(type, 8), "a[1]")
+
+    def test_union(self):
+        prog = mock_program()
+        option_type = prog.union_type(
+            "option",
+            4,
+            (
+                TypeMember(prog.int_type("int", 4, True), "i"),
+                TypeMember(prog.float_type("float", 4), "f"),
+            ),
+        )
+        self.assertEqual(member_at_offset(option_type, 0), "i or f")
+
+    def test_union_end(self):
+        prog = mock_program()
+        option_type = prog.union_type(
+            "option",
+            4,
+            (
+                TypeMember(prog.int_type("int", 4, True), "i"),
+                TypeMember(prog.float_type("float", 4), "f"),
+            ),
+        )
+        self.assertEqual(member_at_offset(option_type, 4), "<end>")
+
+    def test_union_of_structs(self):
+        prog = mock_program()
+        type = prog.union_type(
+            None,
+            8,
+            (
+                TypeMember(
+                    prog.struct_type(
+                        None,
+                        8,
+                        (
+                            TypeMember(prog.int_type("int", 4, True), "x", 0),
+                            TypeMember(prog.int_type("int", 4, True), "y", 32),
+                        ),
+                    ),
+                    "point",
+                ),
+                TypeMember(prog.float_type("int", 4), "i"),
+            ),
+        )
+        self.assertEqual(member_at_offset(type, 0), "point.x or i")
+        self.assertEqual(member_at_offset(type, 4), "point.y")
+
+    def test_unions_nested(self):
+        prog = mock_program()
+        type = prog.union_type(
+            None,
+            8,
+            (
+                TypeMember(
+                    prog.union_type(
+                        None,
+                        8,
+                        (
+                            TypeMember(prog.int_type("long", 8, True), "l"),
+                            TypeMember(prog.float_type("double", 8), "d"),
+                        ),
+                    ),
+                    "big",
+                ),
+                TypeMember(
+                    prog.union_type(
+                        None,
+                        4,
+                        (
+                            TypeMember(prog.int_type("int", 4, True), "i"),
+                            TypeMember(prog.float_type("float", 4), "f"),
+                        ),
+                    ),
+                    "small",
+                ),
+            ),
+        )
+        self.assertEqual(
+            member_at_offset(type, 0), "big.l or big.d or small.i or small.f"
+        )
+        self.assertEqual(member_at_offset(type, 4), "big.l+0x4 or big.d+0x4")
+
+    def test_type_error(self):
+        prog = mock_program()
+        self.assertRaises(TypeError, member_at_offset, prog.int_type("int", 4, True), 0)
+
+    def test_typedefs(self):
+        prog = mock_program()
+        point_type = prog.typedef_type(
+            "Point",
+            prog.struct_type(
+                None,
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.int_type("int", 4, True), "y", 32),
+                ),
+            ),
+        )
+        line_segment_type = prog.typedef_type(
+            "LineSegment",
+            prog.struct_type(
+                None,
+                16,
+                (TypeMember(point_type, "a"), TypeMember(point_type, "b", 64)),
+            ),
+        )
+        self.assertEqual(member_at_offset(line_segment_type, 0), "a.x")
+        self.assertEqual(member_at_offset(line_segment_type, 4), "a.y")
+        self.assertEqual(member_at_offset(line_segment_type, 8), "b.x")
+        self.assertEqual(member_at_offset(line_segment_type, 12), "b.y")
