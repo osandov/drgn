1112ff17219ded2f5e3f17b4b504477b3958688b
Omar Sandoval <osandov@osandov.com>
2024-04-18 16:35:36 -0700

    libdrgn: pp: make PP_NARGS() expand to 0
    The C standard treats an empty variable argument list as a single, empty
    argument, so PP_NARGS() currently expands to 1. But this is surprising,
    especially for PP_OVERLOAD(). Use the , ##__VA_ARGS__ GNU C extension to
    make PP_NARGS() expand to 0 instead. (We could also use __VA_OPT__(,) to
    achieve the same thing. It has the advantage of being standardized for
    C23, but the huge disadvantage that it's only available on relatively
    recent versions of GCC and Clang.) Also check that the extension is
    supported in configure.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- libdrgn/pp.h
+++ libdrgn/pp.h
@@ -33,18 +33,25 @@
  * PP_NARGS(a, b, c) // Expands to 3
  * #define ARGS x, y
  * PP_NARGS(ARGS) // Expands to 2
- * PP_NARGS() // Expands to 1 (one empty argument, not no arguments)
  * ```
  *
- * Note that standard C requires at least one argument for the "..." in a
- * variadic macro, so an empty argument list is actually a single (empty)
- * argument.
+ * An empty argument list is considered to have 0 arguments.
+ *
+ * ```
+ * PP_NARGS() // Expands to 0.
+ * ```
+ *
+ * @remark This depends on the `, ##__VA_ARGS__` GNU C extension. `__VA_OPT__`
+ * could be used instead, but it is only supported since GCC 8 (released in
+ * 2018) and Clang 12 (released in 2021).
  *
  * @hideinitializer
  */
-#define PP_NARGS(...) PP_NARGS_I(__VA_ARGS__, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define PP_NARGS(...) PP_NARGS_I(__VA_ARGS__)
 /** @cond */
-#define PP_NARGS_I(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, N, ...) N
+// The extra layer of indirection ensures that arguments are expanded.
+#define PP_NARGS_I(...) PP_NARGS_II(, ##__VA_ARGS__, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define PP_NARGS_II(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, N, ...) N
 /** @endcond */
 
 /**
