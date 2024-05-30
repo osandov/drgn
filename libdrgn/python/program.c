osandov/drgn

commit 4d970a98c1a307371b92444ae989ece5423e2765

libdrgn: python: set Python error indicator if Program_hold_reserve() fails
All of its callers are assuming it does.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/program.c
@@ -180,8 +180,12 @@ int Program_hold_object(Program *prog, PyObject *obj)
 
 bool Program_hold_reserve(Program *prog, size_t n)
 {
-	return pyobjectp_set_reserve(&prog->objects,
-				     pyobjectp_set_size(&prog->objects) + n);
+	if (!pyobjectp_set_reserve(&prog->objects,
+				   pyobjectp_set_size(&prog->objects) + n)) {
+		PyErr_NoMemory();
+		return false;
+	}
+	return true;
 }
 
 int Program_type_arg(Program *prog, PyObject *type_obj, bool can_be_none,

commit 551851b03dc91be9fe7d692cfb6c6cc2a59bf69c

libdrgn: python: set Python error indicator if Program_hold_object() fails
Just like in commit 4d970a98c1a3 ("libdrgn: python: set Python error
indicator if Program_hold_reserve() fails"), callers of
Program_hold_object() assume it sets the error indicator if it fails.

Fixes: a8d632b4c15d ("libdrgn/python: use F14 instead of PyDict for Program::objects")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/program.c
@@ -175,6 +175,8 @@ int Program_hold_object(Program *prog, PyObject *obj)
 	if (ret > 0) {
 		Py_INCREF(obj);
 		ret = 0;
+	} else if (ret < 0) {
+		PyErr_NoMemory();
 	}
 	return ret;
 }