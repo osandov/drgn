osandov/drgn

commit 69bd07ddb2d2e40970373d0b39199ab054fd396c

drgn.helpers.common.memory: add optional cache to identify_address()
print_annotated_memory() and print_annotated_stack() are pretty slow
when called on large memory regions, largely because identify_address()
walks every task for every call on a non-direct map address in order to
resolve vmap stacks. We can cache the mapping from a vmap stack area to
a task, but we need callers to be able to indicate when it's okay to
cache in order to avoid stale results. To do so, add a cache parameter
to identify_address(), which is just an opaque dictionary that should be
passed between successive calls.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/common/memory.py
@@ -11,10 +11,18 @@ The ``drgn.helpers.common.memory`` module provides helpers for working with memo
 import operator
 import os
 import typing
-from typing import Optional
+from typing import Any, Dict, Optional
 
 import drgn
-from drgn import FaultError, IntegerLike, PlatformFlags, Program, SymbolKind, cast
+from drgn import (
+    FaultError,
+    IntegerLike,
+    Object,
+    PlatformFlags,
+    Program,
+    SymbolKind,
+    cast,
+)
 from drgn.helpers.common.format import escape_ascii_string
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.mm import (
@@ -39,7 +47,69 @@ _SYMBOL_KIND_STR = {
 }
 
 
-def _identify_kernel_address(prog: Program, addr: int) -> Optional[str]:
+def _identify_kernel_vmap(
+    prog: Program, addr: int, cache: Optional[Dict[Any, Any]] = None
+) -> Optional[str]:
+    va = find_vmap_area(prog, addr)
+    if not va:
+        return None
+
+    vm = va.vm.read_()
+    if not vm:
+        return None
+
+    task: Optional[Object]
+    # The cached and uncached cases are separate so that we can avoid creating
+    # a large cache and stop early in the uncached case.
+    if cache is None:
+        for task in for_each_task(prog):
+            try:
+                if task.stack_vm_area == vm:
+                    break
+            except AttributeError:
+                # CONFIG_VMAP_STACK must be disabled.
+                break
+            except FaultError:
+                continue
+        else:
+            task = None
+    else:
+        try:
+            stack_vm_area_to_task = cache["stack_vm_area_to_task"]
+        except KeyError:
+            stack_vm_area_to_task = {}
+            for task in for_each_task(prog):
+                try:
+                    stack_vm_area_to_task[task.stack_vm_area.value_()] = task
+                except AttributeError:
+                    # CONFIG_VMAP_STACK must be disabled.
+                    break
+                except FaultError:
+                    continue
+            cache["stack_vm_area_to_task"] = stack_vm_area_to_task
+        task = stack_vm_area_to_task.get(vm.value_())
+
+    if task is not None:
+        return (
+            f"vmap stack: {task.pid.value_()}"
+            f" ({os.fsdecode(task.comm.string_())})"
+            f" +{hex(addr - task.stack.value_())}"
+        )
+
+    caller = ""
+    caller_value = vm.caller.value_()
+    try:
+        caller_sym = prog.symbol(caller_value)
+    except LookupError:
+        pass
+    else:
+        caller = f" caller {caller_sym.name}+{hex(caller_value - caller_sym.address)}"
+    return f"vmap: {hex(va.va_start)}-{hex(va.va_end)}{caller}"
+
+
+def _identify_kernel_address(
+    prog: Program, addr: int, cache: Optional[Dict[Any, Any]] = None
+) -> Optional[str]:
     try:
         direct_map_start = pfn_to_virt(prog["min_low_pfn"]).value_()
         direct_map_end = (pfn_to_virt(prog["max_low_pfn"]) + prog["PAGE_SIZE"]).value_()
@@ -69,40 +139,14 @@ def _identify_kernel_address(prog: Program, addr: int) -> Optional[str]:
                 maybe_free = "" if slab_info.allocated else "free "
                 return f"{maybe_free}slab object: {cache_name}+{hex(addr - slab_info.address)}"
     else:
-        va = find_vmap_area(prog, addr)
-        if va:
-            caller = ""
-            vm = va.vm.read_()
-            if vm:
-                for task in for_each_task(prog):
-                    try:
-                        try:
-                            stack_vm_area = task.stack_vm_area
-                        except AttributeError:
-                            # CONFIG_VMAP_STACK must be disabled.
-                            break
-                        if stack_vm_area == vm:
-                            return (
-                                f"vmap stack: {task.pid.value_()}"
-                                f" ({os.fsdecode(task.comm.string_())})"
-                                f" +{hex(addr - task.stack.value_())}"
-                            )
-                    except FaultError:
-                        pass
-
-                caller_value = vm.caller.value_()
-                try:
-                    caller_sym = prog.symbol(caller_value)
-                except LookupError:
-                    pass
-                else:
-                    caller = f" caller {caller_sym.name}+{hex(caller_value - caller_sym.address)}"
-            return f"vmap: {hex(va.va_start)}-{hex(va.va_end)}{caller}"
+        return _identify_kernel_vmap(prog, addr, cache)
     return None
 
 
 @takes_program_or_default
-def identify_address(prog: Program, addr: IntegerLike) -> Optional[str]:
+def identify_address(
+    prog: Program, addr: IntegerLike, *, cache: Optional[Dict[Any, Any]] = None
+) -> Optional[str]:
     """
     Try to identify what an address refers to.
 
@@ -132,6 +176,10 @@ def identify_address(prog: Program, addr: IntegerLike) -> Optional[str]:
     This may recognize other types of addresses in the future.
 
     :param addr: ``void *``
+    :param cache: Opaque cache used to amortize expensive lookups. If you're
+        going to call this function many times in a short period, create an
+        empty dictionary and pass the same dictionary as *cache* to each call.
+        Don't reuse it indefinitely or you may get stale results.
     :return: Identity as string, or ``None`` if the address is unrecognized.
     """
     addr = operator.index(addr)
@@ -147,7 +195,7 @@ def identify_address(prog: Program, addr: IntegerLike) -> Optional[str]:
 
     if prog.flags & drgn.ProgramFlags.IS_LINUX_KERNEL:
         # Linux kernel-specific identification:
-        return _identify_kernel_address(prog, addr)
+        return _identify_kernel_address(prog, addr, cache)
     return None
 
 
@@ -201,9 +249,10 @@ def print_annotated_memory(
         line_format = "{:08x}: {:08x}{}"
         print("ADDRESS   VALUE")
 
+    cache: Dict[Any, Any] = {}
     for offset in range(0, len(mem), word_size):
         value = int.from_bytes(mem[offset : offset + word_size], byteorder)
-        identified = identify_address(prog, value)
+        identified = identify_address(prog, value, cache=cache)
         if identified is None:
             identified = ""
         else:

drgn/helpers/common/stack.py
@@ -9,6 +9,8 @@ The ``drgn.helpers.common.stack`` module provides helpers for working with
 stack traces.
 """
 
+from typing import Any, Dict
+
 from drgn import FaultError, PlatformFlags, StackTrace
 from drgn.helpers.common.memory import identify_address
 
@@ -65,6 +67,7 @@ def print_annotated_stack(trace: StackTrace) -> None:
         line_format = "{:08x}: {:08x}{}"
         print("STACK     VALUE\nPOINTER")
 
+    cache: Dict[Any, Any] = {}
     start = 0
     while start < len(trace):
         # Find the bounds of this stack. Our heuristics for the end of the
@@ -126,7 +129,7 @@ def print_annotated_stack(trace: StackTrace) -> None:
                 print(f"[stack frame {frame}]")
                 frame_ind += 1
 
-            identified = identify_address(prog, word_val)
+            identified = identify_address(prog, word_val, cache=cache)
             if identified is None:
                 identified = ""
             else:

commit 9ad29fd86499eb32847473e928b6540872d3d59a

drgn.helpers.common.memory: fix identify_address vmap identification with CONFIG_VMAP_STACK=n
In the uncached case, the task variable is supposed to be the task that
matched the vm area. But, if we detect that CONFIG_VMAP_STACK is
disabled, we break without clearing the task variable. Fix that, and
also make sure to test both the cached and uncached cases.

Closes #414.

Fixes: 69bd07ddb2d2 ("drgn.helpers.common.memory: add optional cache to identify_address()")
Reported-by: Sourabh Jain <sourabhjain@linux.ibm.com>
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/common/memory.py
@@ -54,6 +54,7 @@ def _identify_kernel_vmap(
                     break
             except AttributeError:
                 # CONFIG_VMAP_STACK must be disabled.
+                task = None
                 break
             except FaultError:
                 continue

tests/linux_kernel/helpers/test_common.py
@@ -52,20 +52,28 @@ class TestIdentifyAddress(LinuxKernelTestCase):
 
     @skip_unless_have_test_kmod
     def test_identify_vmap(self):
-        self.assertTrue(
-            identify_address(self.prog["drgn_test_vmalloc_va"]).startswith("vmap: 0x")
-        )
+        for cache in (None, {}):
+            with self.subTest("uncached" if cache is None else "cached"):
+                self.assertTrue(
+                    identify_address(
+                        self.prog["drgn_test_vmalloc_va"], cache=cache
+                    ).startswith("vmap: 0x")
+                )
 
     @skip_unless_have_test_kmod
     def test_identify_vmap_stack(self):
         if not self.prog["drgn_test_vmap_stack_enabled"]:
             self.skipTest("kernel does not use vmap stacks (CONFIG_VMAP_STACK)")
-        self.assertEqual(
-            identify_address(
-                self.prog, self.prog["drgn_test_kthread"].stack.value_() + 1234
-            ),
-            f"vmap stack: {self.prog['drgn_test_kthread'].pid.value_()} (drgn_test_kthre) +0x4d2",
-        )
+        for cache in (None, {}):
+            with self.subTest("uncached" if cache is None else "cached"):
+                self.assertEqual(
+                    identify_address(
+                        self.prog,
+                        self.prog["drgn_test_kthread"].stack.value_() + 1234,
+                        cache=cache,
+                    ),
+                    f"vmap stack: {self.prog['drgn_test_kthread'].pid.value_()} (drgn_test_kthre) +0x4d2",
+                )
 
     @skip_unless_have_full_mm_support
     @skip_unless_have_test_kmod