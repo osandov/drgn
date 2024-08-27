osandov/drgn

commit ca810eec6654bdd02870be58585c7eea68315d2e

libdrgn: define auto to __auto_type via autoconf
Let's pretend we live in the C23 future.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/m4/my_c_auto.m4
@@ -1,0 +1,21 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Check whether C23 auto is supported by the current compiler and compilation
+# flags. If not, but the GNU __auto_type extension is supported, define auto to
+# __auto_type. Otherwise, fail.
+AC_DEFUN([MY_C_AUTO],
+[AC_CACHE_CHECK([for auto], [my_cv_c_auto],
+		[AC_COMPILE_IFELSE([AC_LANG_SOURCE([[auto x = 1;]])],
+				   [my_cv_c_auto=yes], [my_cv_c_auto=no])])
+if test "x$my_cv_c_auto" != xyes; then
+	AC_CACHE_CHECK([for __auto_type], [my_cv_c___auto_type],
+		       [AC_COMPILE_IFELSE([AC_LANG_SOURCE([[__auto_type x = 1;]])],
+					  [my_cv_c___auto_type=yes], [my_cv_c___auto_type=no])])
+	if test "x$my_cv_c___auto_type" == xyes; then
+		AC_DEFINE([auto], [__auto_type])
+	else
+		AC_MSG_ERROR([no auto or __auto_type])
+	fi
+fi
+])

libdrgn/configure.ac
@@ -17,6 +17,8 @@ LT_INIT
 
 AC_SYS_LARGEFILE
 
+MY_C_AUTO
+
 AC_ARG_ENABLE([openmp],
 	      [AS_HELP_STRING([--enable-openmp@<:@=ARG@:>@],
 			      [use OpenMP. ARG may be yes, no, or the name of

libdrgn/m4/.gitignore
@@ -4,3 +4,4 @@
 !/ax_append_flag.m4
 !/ax_check_compile_flag.m4
 !/ax_require_defined.m4
+!/my_c_auto.m4

commit 617a98f65e5d05b7be2f2bd8e240b85f4db44a82

libdrgn: fix bashism in M4 macro
configure scripts need to be runnable with a POSIX-compliant /bin/sh.

On many (but not all!) systems, /bin/sh is provided by Bash, so errors
like this aren't spotted. Notably Debian defaults to /bin/sh provided
by dash which doesn't tolerate such bashisms as '=='.

This retains compatibility with bash.

Fixes: ca810eec6654 ("libdrgn: define auto to __auto_type via autoconf")
Signed-off-by: Sam James <sam@gentoo.org>

libdrgn/m4/my_c_auto.m4
@@ -12,7 +12,7 @@ if test "x$my_cv_c_auto" != xyes; then
 	AC_CACHE_CHECK([for __auto_type], [my_cv_c___auto_type],
 		       [AC_COMPILE_IFELSE([AC_LANG_SOURCE([[__auto_type x = 1;]])],
 					  [my_cv_c___auto_type=yes], [my_cv_c___auto_type=no])])
-	if test "x$my_cv_c___auto_type" == xyes; then
+	if test "x$my_cv_c___auto_type" = xyes; then
 		AC_DEFINE([auto], [__auto_type])
 	else
 		AC_MSG_ERROR([no auto or __auto_type])