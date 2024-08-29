osandov/drgn

commit 9c00552007dac335e543fcad9e5e3117bf47fdc9

libdrgn: python: add Object.from_bytes_()
Add a way to create an object from raw bytes. One example where I've
wanted this is creating a struct pt_regs from a PRSTATUS note or other
source.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -746,6 +746,55 @@ static DrgnObject *DrgnObject_read(DrgnObject *self)
 	)
 }
 
+static DrgnObject *DrgnObject_from_bytes(PyTypeObject *type, PyObject *args,
+					 PyObject *kwds)
+{
+	static char *keywords[] = {
+		"prog", "type", "bytes", "bit_offset", "bit_field_size", NULL
+	};
+	struct drgn_error *err;
+	Program *prog;
+	PyObject *type_obj = Py_None;
+	Py_buffer bytes;
+	struct index_arg bit_offset = {};
+	struct index_arg bit_field_size = { .allow_none = true, .is_none = true };
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!Oy*|O&O&:from_bytes_",
+					 keywords, &Program_type, &prog,
+					 &type_obj, &bytes, index_converter,
+					 &bit_offset, index_converter,
+					 &bit_field_size))
+		return NULL;
+
+	DrgnObject *res = NULL;
+	struct drgn_qualified_type qualified_type;
+	if (Program_type_arg(prog, type_obj, false, &qualified_type) == -1)
+		goto out;
+
+	if (!bit_field_size.is_none && bit_field_size.uvalue == 0) {
+		PyErr_SetString(PyExc_ValueError,
+				"bit field size cannot be zero");
+		goto out;
+	}
+
+	res = DrgnObject_alloc(prog);
+	if (!res)
+		goto out;
+
+	err = drgn_object_set_from_buffer(&res->obj, qualified_type, bytes.buf,
+					  bytes.len, bit_offset.uvalue,
+					  bit_field_size.uvalue);
+	if (err) {
+		set_drgn_error(err);
+		Py_DECREF(res);
+		res = NULL;
+		goto out;
+	}
+
+out:
+	PyBuffer_Release(&bytes);
+	return res;
+}
+
 static int append_bit_offset(PyObject *parts, uint8_t bit_offset)
 {
 	if (bit_offset == 0)
@@ -1582,6 +1631,9 @@ static PyMethodDef DrgnObject_methods[] = {
 	 drgn_Object_address_of__DOC},
 	{"read_", (PyCFunction)DrgnObject_read, METH_NOARGS,
 	 drgn_Object_read__DOC},
+	{"from_bytes_", (PyCFunction)DrgnObject_from_bytes,
+	 METH_CLASS | METH_VARARGS | METH_KEYWORDS,
+	 drgn_Object_from_bytes__DOC},
 	{"format_", (PyCFunction)DrgnObject_format,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Object_format__DOC},
 	{"__round__", (PyCFunction)DrgnObject_round,

_drgn.pyi
@@ -1177,6 +1177,28 @@ class Object:
             ``void``)
         """
         ...
+    @classmethod
+    def from_bytes_(
+        cls,
+        prog: Program,
+        type: Union[str, Type],
+        bytes: bytes,
+        *,
+        bit_offset: IntegerLike = 0,
+        bit_field_size: Optional[IntegerLike] = None,
+    ) -> Object:
+        """
+        Return a value object from its binary representation.
+
+        :param prog: Program to create the object in.
+        :param type: Type of the object.
+        :param bytes: Buffer containing value of the object.
+        :param bit_offset: Offset in bits from the beginning of *bytes* to the
+            beginning of the object.
+        :param bit_field_size: Size in bits of the object if it is a bit field.
+            The default is ``None``, which means the object is not a bit field.
+        """
+        ...
     def format_(
         self,
         *,

tests/test_object.py
@@ -930,6 +930,134 @@ class TestConversions(MockProgramTestCase):
             Object(self.prog, "int []", address=0),
         )
 
+    def test_int_from_bytes(self):
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                type_ = self.prog.int_type("int", 4, True, byteorder)
+                self.assertIdentical(
+                    Object.from_bytes_(
+                        self.prog, type_, (0x12345678).to_bytes(4, byteorder)
+                    ),
+                    Object(self.prog, type_, 0x12345678),
+                )
+
+    def test_int_from_bytes_bit_offset(self):
+        self.assertIdentical(
+            Object.from_bytes_(self.prog, "int", b"\xe0Y\xd1H\x00", bit_offset=2),
+            Object(self.prog, "int", 0x12345678),
+        )
+
+    def test_int_from_bytes_big_endian_bit_offset(self):
+        self.assertIdentical(
+            Object.from_bytes_(
+                self.prog,
+                self.prog.int_type("int", 4, True, "big"),
+                b"\x04\x8d\x15\x9e\x00",
+                bit_offset=2,
+            ),
+            Object(self.prog, self.prog.int_type("int", 4, True, "big"), 0x12345678),
+        )
+
+    def test_int_from_bytes_bit_field(self):
+        self.assertIdentical(
+            Object.from_bytes_(self.prog, "int", b"\xcc", bit_field_size=8),
+            Object(self.prog, "int", 0xCC, bit_field_size=8),
+        )
+
+    def test_float64_from_bytes(self):
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                type_ = self.prog.float_type("double", 8, byteorder)
+                self.assertIdentical(
+                    Object.from_bytes_(
+                        self.prog,
+                        type_,
+                        struct.pack(
+                            ("<" if byteorder == "little" else ">") + "d", math.e
+                        ),
+                    ),
+                    Object(self.prog, type_, math.e),
+                )
+
+    def test_float32_from_bytes(self):
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                type_ = self.prog.float_type("float", 4, byteorder)
+                self.assertIdentical(
+                    Object.from_bytes_(
+                        self.prog,
+                        type_,
+                        struct.pack(
+                            ("<" if byteorder == "little" else ">") + "f", math.e
+                        ),
+                    ),
+                    Object(self.prog, type_, math.e),
+                )
+
+    def test_struct_from_bytes(self):
+        self.assertIdentical(
+            Object.from_bytes_(
+                self.prog, self.point_type, b"\x01\x00\x00\x00\x02\x00\x00\x00"
+            ),
+            Object(self.prog, self.point_type, {"x": 1, "y": 2}),
+        )
+
+    def test_struct_from_bytes_bit_offset(self):
+        self.assertIdentical(
+            Object.from_bytes_(
+                self.prog,
+                self.point_type,
+                b"\xff\x01\x00\x00\x00\x02\x00\x00\x00",
+                bit_offset=8,
+            ),
+            Object(self.prog, self.point_type, {"x": 1, "y": 2}),
+        )
+
+    def test_struct_from_bytes_invalid_bit_offset(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "non-scalar must be byte-aligned",
+            Object.from_bytes_,
+            self.prog,
+            self.point_type,
+            b"\xff\x01\x00\x00\x00\x02\x00\x00\x00",
+            bit_offset=2,
+        )
+
+    def test_from_bytes_invalid_bit_field_size(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "bit field size cannot be zero",
+            Object.from_bytes_,
+            self.prog,
+            "int",
+            b"",
+            bit_field_size=0,
+        )
+
+    def test_from_bytes_buffer_too_small(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "buffer is too small",
+            Object.from_bytes_,
+            self.prog,
+            "int",
+            bytes(3),
+        )
+
+    def test_from_bytes_incomplete_type(self):
+        self.assertRaisesRegex(
+            TypeError,
+            "cannot create object with void type",
+            Object.from_bytes_,
+            self.prog,
+            "void",
+            b"",
+        )
+
+    def test_from_bytes_bad_type(self):
+        self.assertRaises(TypeError, Object.from_bytes_, self.prog, None, b"")
+
 
 class TestInvalidBitField(MockProgramTestCase):
     def test_integer(self):

commit 269b7bad75b5b746d9d9698bd01a027ca5d8fd9f

python: make Object.from_bytes_ keyword-only arguments actually keyword-only
bit_offset and bit_field_size are annotated as keyword-only, but the
PyArg_ParseTupleAndKeywords() call is missing "$" in the format string.
Fix it.

Fixes: 9c00552007da ("libdrgn: python: add Object.from_bytes_()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -778,7 +778,7 @@ static DrgnObject *DrgnObject_from_bytes(PyTypeObject *type, PyObject *args,
 	Py_buffer bytes;
 	struct index_arg bit_offset = {};
 	struct index_arg bit_field_size = { .allow_none = true, .is_none = true };
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!Oy*|O&O&:from_bytes_",
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!Oy*|$O&O&:from_bytes_",
 					 keywords, &Program_type, &prog,
 					 &type_obj, &bytes, index_converter,
 					 &bit_offset, index_converter,