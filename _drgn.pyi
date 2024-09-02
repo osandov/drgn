osandov/drgn

commit 78316a28fbb1005c2610aeddb4ccc9778f38f08b

libdrgn: remove half-baked support for complex types
We've nominally supported complex types since commit 75c3679147a5
("Rewrite drgn core in C"), but parsing them from DWARF has been
incorrect from the start (they don't have a DW_AT_type attribute like we
assume), and we never implemented proper support for complex objects.
Drop the partial implementation; we can bring it back (properly) if
someone requests it.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -505,25 +505,6 @@ class Program:
         :param lang: :attr:`Type.language`
         """
         ...
-    def complex_type(
-        self,
-        name: str,
-        size: IntegerLike,
-        type: Type,
-        *,
-        qualifiers: Qualifiers = Qualifiers.NONE,
-        language: Optional[Language] = None,
-    ) -> Type:
-        """
-        Create a new complex type. It has kind :attr:`TypeKind.COMPLEX`.
-
-        :param name: :attr:`Type.name`
-        :param size: :attr:`Type.size`
-        :param type: The corresponding real type (:attr:`Type.type`)
-        :param qualifiers: :attr:`Type.qualifiers`
-        :param lang: :attr:`Type.language`
-        """
-        ...
     @overload
     def struct_type(
         self,
@@ -1504,7 +1485,7 @@ class Type:
     name: str
     """
     Name of this type. This is present for integer, boolean, floating-point,
-    complex, and typedef types.
+    and typedef types.
     """
 
     tag: Optional[str]
@@ -1516,8 +1497,8 @@ class Type:
     size: Optional[int]
     """
     Size of this type in bytes, or ``None`` if this is an incomplete type. This
-    is present for integer, boolean, floating-point, complex, structure, union,
-    class, and pointer types.
+    is present for integer, boolean, floating-point, structure, union, class,
+    and pointer types.
     """
 
     length: Optional[int]
@@ -1533,7 +1514,6 @@ class Type:
     """
     Type underlying this type, defined as follows:
 
-    * For complex types, the corresponding the real type.
     * For typedef types, the aliased type.
     * For enumerated types, the compatible integer type, which is ``None`` if
       this is an incomplete type.

libdrgn/debug_info.c
@@ -1461,32 +1461,8 @@ drgn_base_type_from_dwarf(struct drgn_debug_info *dbinfo,
 	case DW_ATE_unsigned_char:
 		return drgn_int_type_create(dbinfo->prog, name, size, false,
 					    lang, ret);
-	/*
-	 * GCC also supports complex integer types, but DWARF 4 doesn't have an
-	 * encoding for that. GCC as of 8.2 emits DW_ATE_lo_user, but that's
-	 * ambiguous because it also emits that in other cases. For now, we
-	 * don't support it.
-	 */
-	case DW_ATE_complex_float: {
-		Dwarf_Die child;
-		if (dwarf_type(die, &child)) {
-			return drgn_error_create(DRGN_ERROR_OTHER,
-						 "DW_TAG_base_type has missing or invalid DW_AT_type");
-		}
-		struct drgn_qualified_type real_type;
-		struct drgn_error *err = drgn_type_from_dwarf(dbinfo, module,
-							      &child,
-							      &real_type);
-		if (err)
-			return err;
-		if (drgn_type_kind(real_type.type) != DRGN_TYPE_FLOAT &&
-		    drgn_type_kind(real_type.type) != DRGN_TYPE_INT) {
-			return drgn_error_create(DRGN_ERROR_OTHER,
-						 "DW_AT_type of DW_ATE_complex_float is not a floating-point or integer type");
-		}
-		return drgn_complex_type_create(dbinfo->prog, name, size,
-						real_type.type, lang, ret);
-	}
+	/* We don't support complex types yet. */
+	case DW_ATE_complex_float:
 	default:
 		return drgn_error_format(DRGN_ERROR_OTHER,
 					 "DW_TAG_base_type has unknown DWARF encoding 0x%llx",

libdrgn/drgn.h.in
@@ -271,8 +271,6 @@ enum drgn_type_kind {
 	DRGN_TYPE_BOOL,
 	/** Floating-point type. */
 	DRGN_TYPE_FLOAT,
-	/** Complex type. */
-	DRGN_TYPE_COMPLEX,
 	/** Structure type. */
 	DRGN_TYPE_STRUCT,
 	/** Union type. */
@@ -903,9 +901,9 @@ enum drgn_object_encoding {
 	/**
 	 * Memory buffer.
 	 *
-	 * This is used for objects with a complex, structure, union, class, or
-	 * array type. The value is a buffer of the contents of that object's
-	 * memory in the program.
+	 * This is used for objects with a structure, union, class, or array
+	 * type. The value is a buffer of the contents of that object's memory
+	 * in the program.
 	 */
 	DRGN_OBJECT_ENCODING_BUFFER,
 	/**
@@ -2028,14 +2026,13 @@ drgn_type_language(struct drgn_type *type)
 
 /**
  * Get whether a kind of type has a name. This is true for integer, boolean,
- * floating-point, complex, and typedef types.
+ * floating-point, and typedef types.
  */
 static inline bool drgn_type_kind_has_name(enum drgn_type_kind kind)
 {
 	return (kind == DRGN_TYPE_INT ||
 		kind == DRGN_TYPE_BOOL ||
 		kind == DRGN_TYPE_FLOAT ||
-		kind == DRGN_TYPE_COMPLEX ||
 		kind == DRGN_TYPE_TYPEDEF);
 }
 /** Get whether a type has a name. @sa drgn_type_kind_has_name() */
@@ -2054,14 +2051,13 @@ static inline const char *drgn_type_name(struct drgn_type *type)
 
 /**
  * Get whether a kind of type has a size. This is true for integer, boolean,
- * floating-point, complex, structure, union, class, and pointer types.
+ * floating-point, structure, union, class, and pointer types.
  */
 static inline bool drgn_type_kind_has_size(enum drgn_type_kind kind)
 {
 	return (kind == DRGN_TYPE_INT ||
 		kind == DRGN_TYPE_BOOL ||
 		kind == DRGN_TYPE_FLOAT ||
-		kind == DRGN_TYPE_COMPLEX ||
 		kind == DRGN_TYPE_STRUCT ||
 		kind == DRGN_TYPE_UNION ||
 		kind == DRGN_TYPE_CLASS ||
@@ -2164,13 +2160,12 @@ static inline size_t drgn_type_num_members(struct drgn_type *type)
 }
 
 /**
- * Get whether a kind of type has a wrapped type. This is true for complex,
- * enumerated, typedef, pointer, array, and function types.
+ * Get whether a kind of type has a wrapped type. This is true for enumerated,
+ * typedef, pointer, array, and function types.
  */
 static inline bool drgn_type_kind_has_type(enum drgn_type_kind kind)
 {
-	return (kind == DRGN_TYPE_COMPLEX ||
-		kind == DRGN_TYPE_ENUM ||
+	return (kind == DRGN_TYPE_ENUM ||
 		kind == DRGN_TYPE_TYPEDEF ||
 		kind == DRGN_TYPE_POINTER ||
 		kind == DRGN_TYPE_ARRAY ||
@@ -2183,8 +2178,6 @@ static inline bool drgn_type_has_type(struct drgn_type *type)
 }
 /**
  * Get the type wrapped by this type.
- *
- * For a complex type, this is the corresponding real type.
  *
  * For an enumerated type, this is the compatible integer type. It is @c NULL if
  * the enumerated type is incomplete.

libdrgn/language_c.c
@@ -342,7 +342,6 @@ c_declare_variable(struct drgn_qualified_type qualified_type,
 	case DRGN_TYPE_INT:
 	case DRGN_TYPE_BOOL:
 	case DRGN_TYPE_FLOAT:
-	case DRGN_TYPE_COMPLEX:
 	case DRGN_TYPE_TYPEDEF:
 		return c_declare_basic(qualified_type, name, indent, sb);
 	case DRGN_TYPE_STRUCT:
@@ -493,7 +492,6 @@ c_define_type(struct drgn_qualified_type qualified_type, size_t indent,
 	case DRGN_TYPE_INT:
 	case DRGN_TYPE_BOOL:
 	case DRGN_TYPE_FLOAT:
-	case DRGN_TYPE_COMPLEX:
 		return c_declare_basic(qualified_type, NULL, indent, sb);
 	case DRGN_TYPE_STRUCT:
 	case DRGN_TYPE_UNION:
@@ -1591,9 +1589,6 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 		return c_format_int_object(obj, flags, sb);
 	case DRGN_TYPE_FLOAT:
 		return c_format_float_object(obj, sb);
-	case DRGN_TYPE_COMPLEX:
-		return drgn_error_format(DRGN_ERROR_TYPE,
-					 "complex object formatting is not implemented");
 	case DRGN_TYPE_STRUCT:
 	case DRGN_TYPE_UNION:
 	case DRGN_TYPE_CLASS:

libdrgn/python/drgnpy.h
@@ -236,7 +236,6 @@ DrgnType *Program_void_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_int_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_bool_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_float_type(Program *self, PyObject *args, PyObject *kwds);
-DrgnType *Program_complex_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_struct_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_union_type(Program *self, PyObject *args, PyObject *kwds);
 DrgnType *Program_class_type(Program *self, PyObject *args, PyObject *kwds);

libdrgn/python/object.c
@@ -283,10 +283,6 @@ static int serialize_py_object(struct drgn_program *prog, char *buf,
 	}
 	case DRGN_OBJECT_ENCODING_BUFFER:
 		switch (drgn_type_kind(type->underlying_type)) {
-		case DRGN_TYPE_COMPLEX:
-			PyErr_SetString(PyExc_NotImplementedError,
-					"creating complex object is not implemented");
-			return -1;
 		case DRGN_TYPE_STRUCT:
 		case DRGN_TYPE_UNION:
 		case DRGN_TYPE_CLASS:
@@ -739,10 +735,6 @@ static PyObject *DrgnObject_value_impl(struct drgn_object *obj)
 	}
 	case DRGN_OBJECT_ENCODING_BUFFER:
 		switch (drgn_type_kind(underlying_type)) {
-		case DRGN_TYPE_COMPLEX:
-			PyErr_SetString(PyExc_NotImplementedError,
-					"reading complex type is not implemented");
-			return NULL;
 		case DRGN_TYPE_STRUCT:
 		case DRGN_TYPE_UNION:
 		case DRGN_TYPE_CLASS:

libdrgn/python/program.c
@@ -912,8 +912,6 @@ static PyMethodDef Program_methods[] = {
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_bool_type_DOC},
 	{"float_type", (PyCFunction)Program_float_type,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_float_type_DOC},
-	{"complex_type", (PyCFunction)Program_complex_type,
-	 METH_VARARGS | METH_KEYWORDS, drgn_Program_complex_type_DOC},
 	{"struct_type", (PyCFunction)Program_struct_type,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_struct_type_DOC},
 	{"union_type", (PyCFunction)Program_union_type,

libdrgn/python/type.c
@@ -1500,72 +1500,6 @@ DrgnType *Program_float_type(Program *self, PyObject *args, PyObject *kwds)
 	return type_obj;
 }
 
-DrgnType *Program_complex_type(Program *self, PyObject *args, PyObject *kwds)
-{
-	static char *keywords[] = {
-		"name", "size", "type", "qualifiers", "language", NULL
-	};
-	PyObject *name_obj;
-	struct index_arg size = {};
-	DrgnType *real_type_obj;
-	enum drgn_qualifiers qualifiers = 0;
-	const struct drgn_language *language = NULL;
-	if (!PyArg_ParseTupleAndKeywords(args, kwds,
-					 "O!O&O!|$O&O&:complex_type", keywords,
-					 &PyUnicode_Type, &name_obj,
-					 index_converter, &size, &DrgnType_type,
-					 &real_type_obj, qualifiers_converter,
-					 &qualifiers, language_converter,
-					 &language))
-		return NULL;
-
-	const char *name = PyUnicode_AsUTF8(name_obj);
-	if (!name)
-		return NULL;
-
-	struct drgn_type *real_type = real_type_obj->type;
-	if (drgn_type_kind(real_type) != DRGN_TYPE_FLOAT &&
-	    drgn_type_kind(real_type) != DRGN_TYPE_INT) {
-		PyErr_SetString(PyExc_ValueError,
-				"complex_type() real type must be floating-point or integer type");
-		return NULL;
-	}
-	if (real_type_obj->qualifiers) {
-		PyErr_SetString(PyExc_ValueError,
-				"complex_type() real type must be unqualified");
-		return NULL;
-	}
-
-	if (!Program_hold_reserve(self, 1))
-		return NULL;
-
-	struct drgn_qualified_type qualified_type;
-	struct drgn_error *err = drgn_complex_type_create(&self->prog, name,
-							  size.uvalue,
-							  real_type, language,
-							  &qualified_type.type);
-	if (err)
-		return set_drgn_error(err);
-
-	if (drgn_type_name(qualified_type.type) == name)
-		Program_hold_object(self, name_obj);
-
-	qualified_type.qualifiers = qualifiers;
-	DrgnType *type_obj = (DrgnType *)DrgnType_wrap(qualified_type);
-	if (!type_obj)
-		return NULL;
-
-	if (_PyDict_SetItemId(type_obj->attr_cache, &DrgnType_attr_type.id,
-			      (PyObject *)real_type_obj) == -1 ||
-	    _PyDict_SetItemId(type_obj->attr_cache, &DrgnType_attr_name.id,
-			      name_obj) == -1) {
-		Py_DECREF(type_obj);
-		return NULL;
-	}
-
-	return type_obj;
-}
-
 static struct drgn_error *py_lazy_object_thunk_fn(struct drgn_object *res,
 						  void *arg)
 {

commit df7116c14d9ab23b6860aba0e48469ba86605961

Remove some complex type leftovers
I missed these when I removed our (broken) complex type support.

Fixes: 78316a28fbb1 ("libdrgn: remove half-baked support for complex types")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -2401,9 +2401,6 @@ class TypeKind(enum.Enum):
     FLOAT = ...
     """Floating-point type."""
 
-    COMPLEX = ...
-    """Complex type."""
-
     STRUCT = ...
     """Structure type."""
 

libdrgn/build-aux/gen_c_keywords_inc_strswitch.py
@@ -5,7 +5,6 @@
 C_KEYWORDS = (
     "_Atomic",
     "_Bool",
-    "_Complex",
     "char",
     "class",
     "const",

libdrgn/c_lexer.h
@@ -25,8 +25,7 @@ enum {
 	C_TOKEN_BOOL,
 	C_TOKEN_FLOAT,
 	C_TOKEN_DOUBLE,
-	C_TOKEN_COMPLEX,
-	MAX_SPECIFIER_TOKEN = C_TOKEN_COMPLEX,
+	MAX_SPECIFIER_TOKEN = C_TOKEN_DOUBLE,
 	MIN_QUALIFIER_TOKEN,
 	C_TOKEN_CONST = MIN_QUALIFIER_TOKEN,
 	C_TOKEN_RESTRICT,

libdrgn/tests/language_c.c.in
@@ -41,7 +41,7 @@ static void assert_lexes(const char *s, const struct test_token *tokens,
 #define assert_lexes_cpp(s, tokens) assert_lexes(s, tokens, true)
 
 static const char c_keywords[] =
-	"void char short int long signed unsigned _Bool float double _Complex "
+	"void char short int long signed unsigned _Bool float double "
 	"const restrict volatile _Atomic struct union class enum";
 
 #test lexer_empty
@@ -86,7 +86,6 @@ static const char c_keywords[] =
 		{ C_TOKEN_BOOL, "_Bool" },
 		{ C_TOKEN_FLOAT, "float" },
 		{ C_TOKEN_DOUBLE, "double" },
-		{ C_TOKEN_COMPLEX, "_Complex" },
 		{ C_TOKEN_CONST, "const" },
 		{ C_TOKEN_RESTRICT, "restrict" },
 		{ C_TOKEN_VOLATILE, "volatile" },
@@ -116,7 +115,6 @@ static const char c_keywords[] =
 		{ C_TOKEN_BOOL, "_Bool" },
 		{ C_TOKEN_FLOAT, "float" },
 		{ C_TOKEN_DOUBLE, "double" },
-		{ C_TOKEN_COMPLEX, "_Complex" },
 		{ C_TOKEN_CONST, "const" },
 		{ C_TOKEN_RESTRICT, "restrict" },
 		{ C_TOKEN_VOLATILE, "volatile" },