osandov/drgn

commit fa4805d5c46114cba2dd23da979d49a1abb3d5f0

drgn.helpers.linux.sched: add task_thread_info() helper
Arm stack unwinding support needs the thread info, so extract a new
task_thread_info() helper from task_cpu().

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/helpers.c
@@ -127,6 +127,26 @@ DrgnObject *drgnpy_linux_helper_idle_task(PyObject *self, PyObject *args)
 	return_ptr(res);
 }
 
+DrgnObject *drgnpy_linux_helper_task_thread_info(PyObject *self, PyObject *args,
+						 PyObject *kwds)
+{
+	static char *keywords[] = {"task", NULL};
+	struct drgn_error *err;
+	DrgnObject *task;
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!:task_cpu", keywords,
+					 &DrgnObject_type, &task))
+		return NULL;
+
+	_cleanup_pydecref_ DrgnObject *res =
+		DrgnObject_alloc(DrgnObject_prog(task));
+	if (!res)
+		return NULL;
+	err = linux_helper_task_thread_info(&res->obj, &task->obj);
+	if (err)
+		return set_drgn_error(err);
+	return_ptr(res);
+}
+
 PyObject *drgnpy_linux_helper_task_cpu(PyObject *self, PyObject *args,
 				       PyObject *kwds)
 {

_drgn.pyi
@@ -2730,6 +2730,15 @@ def _linux_helper_per_cpu_ptr(ptr: Object, cpu: IntegerLike) -> Object:
 
 def _linux_helper_cpu_curr(__prog: Program, __cpu: IntegerLike) -> Object: ...
 def _linux_helper_idle_task(__prog: Program, __cpu: IntegerLike) -> Object: ...
+def _linux_helper_task_thread_info(task: Object) -> Object:
+    """
+    Return the thread information structure for a task.
+
+    :param task: ``struct task_struct *``
+    :return: ``struct thread_info *``
+    """
+    ...
+
 def _linux_helper_task_cpu(task: Object) -> int:
     """
     Return the CPU number that the given task last ran on.

drgn/helpers/linux/sched.py
@@ -15,6 +15,7 @@ from _drgn import (
     _linux_helper_cpu_curr,
     _linux_helper_idle_task,
     _linux_helper_task_cpu as task_cpu,
+    _linux_helper_task_thread_info as task_thread_info,
 )
 from drgn import IntegerLike, Object, Program
 from drgn.helpers.common.prog import takes_program_or_default
@@ -25,6 +26,7 @@ __all__ = (
     "loadavg",
     "task_cpu",
     "task_state_to_char",
+    "task_thread_info",
 )
 
 _TASK_NOLOAD = 0x400

libdrgn/helpers.h
@@ -41,6 +41,10 @@ struct drgn_error *linux_helper_cpu_curr(struct drgn_object *res, uint64_t cpu);
 struct drgn_error *linux_helper_idle_task(struct drgn_object *res,
 					  uint64_t cpu);
 
+struct drgn_error *
+linux_helper_task_thread_info(struct drgn_object *res,
+			      const struct drgn_object *task);
+
 struct drgn_error *linux_helper_task_cpu(const struct drgn_object *task,
 					 uint64_t *ret);
 

libdrgn/linux_kernel_helpers.c
@@ -289,6 +289,34 @@ struct drgn_error *linux_helper_idle_task(struct drgn_object *res, uint64_t cpu)
 	return cpu_rq_member(res, cpu, "idle");
 }
 
+struct drgn_error *linux_helper_task_thread_info(struct drgn_object *res,
+						 const struct drgn_object *task)
+{
+	struct drgn_error *err;
+	DRGN_OBJECT(tmp, drgn_object_program(task));
+
+	err = drgn_object_member_dereference(&tmp, task, "thread_info");
+	if (!err) {
+		// CONFIG_THREAD_INFO_IN_TASK=y
+		return drgn_object_address_of(res, &tmp);
+	} else if (err->code == DRGN_ERROR_LOOKUP) {
+		// CONFIG_THREAD_INFO_IN_TASK=n
+		drgn_error_destroy(err);
+		err = drgn_object_member_dereference(&tmp, task, "stack");
+		if (err)
+			return err;
+		struct drgn_qualified_type thread_info_type;
+		err = drgn_program_find_type(drgn_object_program(task),
+					     "struct thread_info *", NULL,
+					     &thread_info_type);
+		if (err)
+			return err;
+		return drgn_object_cast(res, thread_info_type, &tmp);
+	} else {
+		return err;
+	}
+}
+
 struct drgn_error *linux_helper_task_cpu(const struct drgn_object *task,
 					 uint64_t *ret)
 {
@@ -308,29 +336,13 @@ struct drgn_error *linux_helper_task_cpu(const struct drgn_object *task,
 	// ((struct thread_info *)task->stack)->cpu.
 	//
 	// If none of those exist, then the kernel must be !SMP.
-	err = drgn_object_member_dereference(&tmp, task, "thread_info");
-	if (!err) {
-		err = drgn_object_member(&tmp, &tmp, "cpu");
-		if (err && err->code == DRGN_ERROR_LOOKUP) {
-			drgn_error_destroy(err);
-			err = drgn_object_member_dereference(&tmp, task, "cpu");
-		}
-	} else if (err->code == DRGN_ERROR_LOOKUP) {
-		// CONFIG_THREAD_INFO_IN_TASK=n
+	err = linux_helper_task_thread_info(&tmp, task);
+	if (err)
+		return err;
+	err = drgn_object_member_dereference(&tmp, &tmp, "cpu");
+	if (err && err->code == DRGN_ERROR_LOOKUP) {
 		drgn_error_destroy(err);
-		err = drgn_object_member_dereference(&tmp, task, "stack");
-		if (err)
-			return err;
-		struct drgn_qualified_type thread_info_type;
-		err = drgn_program_find_type(drgn_object_program(task),
-					     "struct thread_info *", NULL,
-					     &thread_info_type);
-		if (err)
-			return err;
-		err = drgn_object_cast(&tmp, thread_info_type, &tmp);
-		if (err)
-			return err;
-		err = drgn_object_member_dereference(&tmp, &tmp, "cpu");
+		err = drgn_object_member_dereference(&tmp, task, "cpu");
 	}
 	if (!err) {
 		union drgn_value value;

libdrgn/python/drgnpy.h
@@ -373,6 +373,8 @@ DrgnObject *drgnpy_linux_helper_per_cpu_ptr(PyObject *self, PyObject *args,
 					    PyObject *kwds);
 DrgnObject *drgnpy_linux_helper_cpu_curr(PyObject *self, PyObject *args);
 DrgnObject *drgnpy_linux_helper_idle_task(PyObject *self, PyObject *args);
+DrgnObject *drgnpy_linux_helper_task_thread_info(PyObject *self, PyObject *args,
+						 PyObject *kwds);
 PyObject *drgnpy_linux_helper_task_cpu(PyObject *self, PyObject *args,
 				       PyObject *kwds);
 DrgnObject *drgnpy_linux_helper_xa_load(PyObject *self, PyObject *args,

libdrgn/python/main.c
@@ -188,6 +188,9 @@ static PyMethodDef drgn_methods[] = {
 	 METH_VARARGS},
 	{"_linux_helper_idle_task", (PyCFunction)drgnpy_linux_helper_idle_task,
 	 METH_VARARGS},
+	{"_linux_helper_task_thread_info",
+	 (PyCFunction)drgnpy_linux_helper_task_thread_info,
+	 METH_VARARGS | METH_KEYWORDS, drgn__linux_helper_task_thread_info_DOC},
 	{"_linux_helper_task_cpu", (PyCFunction)drgnpy_linux_helper_task_cpu,
 	 METH_VARARGS | METH_KEYWORDS},
 	{"_linux_helper_xa_load",

tests/linux_kernel/helpers/test_sched.py
@@ -12,17 +12,26 @@ from drgn.helpers.linux.sched import (
     loadavg,
     task_cpu,
     task_state_to_char,
+    task_thread_info,
 )
 from tests.linux_kernel import (
     LinuxKernelTestCase,
     fork_and_stop,
     proc_state,
+    skip_unless_have_test_kmod,
     smp_enabled,
     wait_until,
 )
 
 
 class TestSched(LinuxKernelTestCase):
+    @skip_unless_have_test_kmod
+    def test_task_thread_info(self):
+        self.assertEqual(
+            task_thread_info(self.prog["drgn_test_kthread"]),
+            self.prog["drgn_test_kthread_info"],
+        )
+
     def test_task_cpu(self):
         cpu = os.cpu_count() - 1
         with fork_and_stop(os.sched_setaffinity, 0, (cpu,)) as (pid, _):

commit 1177f64c61cca6db76c6d9e829ead7ca341bcd10

python: fix task_thread_info() function name in PyArg_ParseTupleAndKeywords()
Fixes: fa4805d5c461 ("drgn.helpers.linux.sched: add task_thread_info() helper")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/helpers.c
@@ -133,8 +133,8 @@ DrgnObject *drgnpy_linux_helper_task_thread_info(PyObject *self, PyObject *args,
 	static char *keywords[] = {"task", NULL};
 	struct drgn_error *err;
 	DrgnObject *task;
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!:task_cpu", keywords,
-					 &DrgnObject_type, &task))
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!:task_thread_info",
+					 keywords, &DrgnObject_type, &task))
 		return NULL;
 
 	_cleanup_pydecref_ DrgnObject *res =