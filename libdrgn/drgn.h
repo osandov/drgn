86b29d041c965030c24e9ab0a6907ff22c2a75fa
Omar Sandoval <osandov@osandov.com>
2024-10-08 12:55:40 -0700

    drgn 0.0.29
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- libdrgn/drgn.h
+++ libdrgn/drgn.h
@@ -44,7 +44,7 @@
 /** Minor version of drgn. */
 #define DRGN_VERSION_MINOR 0
 /** Patch level of drgn. */
-#define DRGN_VERSION_PATCH 28
+#define DRGN_VERSION_PATCH 29
 
 /**
  * @defgroup ErrorHandling Error handling
