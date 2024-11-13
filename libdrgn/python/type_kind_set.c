osandov/drgn

commit 9b73b449089ec862886d4176f22cbaf4123854a7

python: add TypeKindSet
C type finders are passed a bitset of type kinds, but Python type
finders currently get called for each type kind. An upcoming update to
the type finder interface will fix this, but we need a set of TypeKinds,
and we'd rather not construct a real Python set for it. Instead, add a
TypeKindSet bitset that satisfies the collections.abc.Set interface.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/type_kind_set.c
@@ -1,0 +1,346 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "drgnpy.h"
+#include "../bitops.h"
+
+static PyObject *collections_abc_Set;
+
+PyObject *TypeKindSet_wrap(uint64_t mask)
+{
+	TypeKindSet *res = call_tp_alloc(TypeKindSet);
+	if (res)
+		res->kinds = mask;
+	return (PyObject *)res;
+}
+
+int init_type_kind_set(void)
+{
+	_cleanup_pydecref_ PyObject *collections_abc =
+		PyImport_ImportModule("collections.abc");
+	if (!collections_abc)
+		return -1;
+	collections_abc_Set = PyObject_GetAttrString(collections_abc, "Set");
+	if (!collections_abc_Set)
+		return -1;
+	_cleanup_pydecref_ PyObject *res =
+		PyObject_CallMethod(collections_abc_Set, "register", "O",
+				    &TypeKindSet_type);
+	if (!res)
+		return -1;
+	Py_DECREF(res);
+	return 0;
+}
+
+static inline const char *type_kind_to_str(enum drgn_type_kind kind)
+{
+	SWITCH_ENUM(kind,
+	case DRGN_TYPE_VOID:
+		return "TypeKind.VOID";
+	case DRGN_TYPE_INT:
+		return "TypeKind.INT";
+	case DRGN_TYPE_BOOL:
+		return "TypeKind.BOOL";
+	case DRGN_TYPE_FLOAT:
+		return "TypeKind.FLOAT";
+	case DRGN_TYPE_STRUCT:
+		return "TypeKind.STRUCT";
+	case DRGN_TYPE_UNION:
+		return "TypeKind.UNION";
+	case DRGN_TYPE_CLASS:
+		return "TypeKind.CLASS";
+	case DRGN_TYPE_ENUM:
+		return "TypeKind.ENUM";
+	case DRGN_TYPE_TYPEDEF:
+		return "TypeKind.TYPEDEF";
+	case DRGN_TYPE_POINTER:
+		return "TypeKind.POINTER";
+	case DRGN_TYPE_ARRAY:
+		return "TypeKind.ARRAY";
+	case DRGN_TYPE_FUNCTION:
+		return "TypeKind.FUNCTION";
+	)
+}
+
+static PyObject *TypeKindSet_repr(TypeKindSet *self)
+{
+	_cleanup_pydecref_ PyObject *parts = PyList_New(0);
+	if (!parts)
+		return NULL;
+	if (append_string(parts, "TypeKindSet("))
+		return NULL;
+	bool first = true;
+	unsigned int kind;
+	uint64_t kinds = self->kinds;
+	for_each_bit(kind, kinds) {
+		if (append_format(parts, "%s%s", first ? "{" : ", ",
+				  type_kind_to_str(kind)))
+			return NULL;
+		first = false;
+	}
+	if (append_string(parts, first ? ")" : "})"))
+		return NULL;
+	return join_strings(parts);
+}
+
+static int TypeKind_value(PyObject *obj)
+{
+	_cleanup_pydecref_ PyObject *value_obj =
+		PyObject_GetAttrString(obj, "value");
+	if (!value_obj)
+		return -1;
+	long value = PyLong_AsLong(value_obj);
+	if ((value < 0 && !PyErr_Occurred()) || value >= 64) {
+		PyErr_BadArgument();
+		return -1;
+	}
+	return value;
+}
+
+static int TypeKindSet_mask_from_iterable(PyObject *iterable, uint64_t *ret)
+{
+	if (PyObject_TypeCheck(iterable, &TypeKindSet_type)) {
+		*ret = ((TypeKindSet *)iterable)->kinds;
+		return 0;
+	}
+
+	int non_typekind = 0;
+	uint64_t mask = 0;
+	if (iterable) {
+		_cleanup_pydecref_ PyObject *it = PyObject_GetIter(iterable);
+		if (!it)
+			return -1;
+
+		for (;;) {
+			_cleanup_pydecref_ PyObject *item = PyIter_Next(it);
+			if (!item)
+				break;
+			if (PyObject_TypeCheck(item,
+					       (PyTypeObject *)TypeKind_class)) {
+				int value = TypeKind_value(item);
+				if (value < 0)
+					return -1;
+				mask |= 1 << value;
+			} else {
+				non_typekind = 1;
+			}
+		}
+		if (PyErr_Occurred())
+			return -1;
+	}
+	*ret = mask;
+	return non_typekind;
+}
+
+static TypeKindSet *TypeKindSet_new(PyTypeObject *subtype, PyObject *args,
+				    PyObject *kwds)
+{
+	static char *keywords[] = { "", NULL };
+	PyObject *iterable = NULL;
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|O:TypeKindSet", keywords,
+					 &iterable))
+		return NULL;
+	uint64_t kinds = 0;
+	if (iterable) {
+		int r = TypeKindSet_mask_from_iterable(iterable, &kinds);
+		if (r < 0)
+			return NULL;
+		if (r > 0) {
+			PyErr_SetString(PyExc_TypeError,
+					"TypeKindSet elements must be TypeKind");
+			return NULL;
+		}
+	}
+	TypeKindSet *res = (TypeKindSet *)subtype->tp_alloc(subtype, 0);
+	res->kinds = kinds;
+	return res;
+}
+
+static Py_ssize_t TypeKindSet_length(TypeKindSet *self)
+{
+	return popcount(self->kinds);
+}
+
+static int TypeKindSet_contains(TypeKindSet *self, PyObject *other)
+{
+	if (!PyObject_TypeCheck(other, (PyTypeObject *)TypeKind_class))
+		return 0;
+	int value = TypeKind_value(other);
+	if (value < 0)
+		return value;
+	return !!(self->kinds & (1 << value));
+}
+
+static Py_ssize_t TypeKindSet_hash(TypeKindSet *self)
+{
+	return self->kinds;
+}
+
+static PyObject *TypeKindSet_richcompare(TypeKindSet *self, PyObject *other,
+					 int op)
+{
+	if (!PyObject_IsInstance(other, collections_abc_Set))
+		Py_RETURN_NOTIMPLEMENTED;
+
+	uint64_t other_kinds;
+	int other_non_typekind =
+		TypeKindSet_mask_from_iterable(other, &other_kinds);
+	if (other_non_typekind < 0)
+		return NULL;
+
+	switch (op) {
+	case Py_EQ:
+		Py_RETURN_BOOL(self->kinds == other_kinds && !other_non_typekind);
+	case Py_NE:
+		Py_RETURN_BOOL(self->kinds != other_kinds || other_non_typekind);
+	case Py_LT:
+		Py_RETURN_BOOL((self->kinds != other_kinds || other_non_typekind)
+			       && (other_kinds | self->kinds) == other_kinds);
+	case Py_GT:
+		Py_RETURN_BOOL(self->kinds != other_kinds
+			       && (self->kinds | other_kinds) == self->kinds
+			       && !other_non_typekind);
+	case Py_LE:
+		Py_RETURN_BOOL((other_kinds | self->kinds) == other_kinds);
+	case Py_GE:
+		Py_RETURN_BOOL((self->kinds | other_kinds) == self->kinds
+			       && !other_non_typekind);
+	default:
+		Py_UNREACHABLE();
+	}
+}
+
+static TypeKindSetIterator *TypeKindSet_iter(TypeKindSet *self)
+{
+	TypeKindSetIterator *it = call_tp_alloc(TypeKindSetIterator);
+	if (!it)
+		return NULL;
+	it->mask = self->kinds;
+	return it;
+}
+
+static PyObject *TypeKindSet_isdisjoint(TypeKindSet *self, PyObject *other)
+{
+	uint64_t other_kinds;
+	if (TypeKindSet_mask_from_iterable(other, &other_kinds) < 0)
+		return NULL;
+	// Non-TypeKind elements in other cannot be in self, so they don't
+	// affect the answer and can be ignored.
+	Py_RETURN_BOOL((self->kinds ^ other_kinds)
+		       == (self->kinds | other_kinds));
+}
+
+static PyObject *TypeKindSet_sub(PyObject *left, PyObject *right)
+{
+	uint64_t left_kinds;
+	int left_r = TypeKindSet_mask_from_iterable(left, &left_kinds);
+	if (left_r < 0)
+		return NULL;
+	if (left_r > 0)
+		Py_RETURN_NOTIMPLEMENTED;
+
+	uint64_t right_kinds;
+	if (TypeKindSet_mask_from_iterable(right, &right_kinds) < 0)
+		return NULL;
+	// If right has non-TypeKind elements, then left is a TypeKindSet and
+	// removing non-TypeKind elements has no effect, so they can be ignored.
+	return TypeKindSet_wrap(left_kinds & ~right_kinds);
+}
+
+static PyObject *TypeKindSet_and(PyObject *left, PyObject *right)
+{
+	uint64_t left_kinds, right_kinds;
+	if (TypeKindSet_mask_from_iterable(left, &left_kinds) < 0
+	    || TypeKindSet_mask_from_iterable(right, &right_kinds) < 0)
+		return NULL;
+	// At least one of the operands is a TypeKindSet, so non-TypeKind
+	// elements cannot be in both and can be ignored.
+	return TypeKindSet_wrap(left_kinds & right_kinds);
+}
+
+#define TypeKindSet_OR_OP(name, op)						\
+static PyObject *TypeKindSet_##name(PyObject *left, PyObject *right)		\
+{										\
+	/* Both operands must only contain TypeKind elements. */		\
+	uint64_t left_kinds;							\
+	int left_r = TypeKindSet_mask_from_iterable(left, &left_kinds);		\
+	if (left_r < 0)								\
+		return NULL;							\
+	if (left_r > 0)								\
+		Py_RETURN_NOTIMPLEMENTED;					\
+										\
+	uint64_t right_kinds;							\
+	int right_r = TypeKindSet_mask_from_iterable(right, &right_kinds);	\
+	if (right_r < 0)							\
+		return NULL;							\
+	if (right_r > 0)							\
+		Py_RETURN_NOTIMPLEMENTED;					\
+										\
+	return TypeKindSet_wrap(left_kinds op right_kinds);			\
+}
+TypeKindSet_OR_OP(xor, ^)
+TypeKindSet_OR_OP(or, |)
+#undef TypeKindSet_OR_OP
+
+static PyNumberMethods TypeKindSet_as_number = {
+    .nb_subtract = TypeKindSet_sub,
+    .nb_and = TypeKindSet_and,
+    .nb_xor = TypeKindSet_xor,
+    .nb_or = TypeKindSet_or,
+};
+
+static PySequenceMethods TypeKindSet_as_sequence = {
+	.sq_length = (lenfunc)TypeKindSet_length,
+	.sq_contains = (objobjproc)TypeKindSet_contains,
+};
+
+static PyMethodDef TypeKindSet_methods[] = {
+	{"isdisjoint", (PyCFunction)TypeKindSet_isdisjoint, METH_O},
+	{},
+};
+
+PyTypeObject TypeKindSet_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.TypeKindSet",
+	.tp_basicsize = sizeof(TypeKindSet),
+	.tp_repr = (reprfunc)TypeKindSet_repr,
+	.tp_as_number = &TypeKindSet_as_number,
+	.tp_as_sequence = &TypeKindSet_as_sequence,
+	.tp_hash = (hashfunc)TypeKindSet_hash,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = drgn_TypeKindSet_DOC,
+	.tp_richcompare = (richcmpfunc)TypeKindSet_richcompare,
+	.tp_iter = (getiterfunc)TypeKindSet_iter,
+	.tp_methods = TypeKindSet_methods,
+	.tp_new = (newfunc)TypeKindSet_new,
+};
+
+static PyObject *TypeKindSetIterator_next(TypeKindSetIterator *self)
+{
+	if (self->mask == 0)
+		return NULL;
+	unsigned int i = ctz(self->mask);
+	self->mask &= self->mask - 1;
+	return PyObject_CallFunction(TypeKind_class, "I", i);
+}
+
+static PyObject *TypeKindSetIterator_length_hint(TypeKindSetIterator *self)
+{
+	return PyLong_FromUnsignedLong(popcount(self->mask));
+}
+
+static PyMethodDef TypeKindSetIterator_methods[] = {
+	{"__length_hint__", (PyCFunction)TypeKindSetIterator_length_hint,
+	 METH_NOARGS},
+	{},
+};
+
+PyTypeObject TypeKindSetIterator_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn._TypeKindSetIterator",
+	.tp_basicsize = sizeof(TypeKindSetIterator),
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_iter = PyObject_SelfIter,
+	.tp_iternext = (iternextfunc)TypeKindSetIterator_next,
+	.tp_methods = TypeKindSetIterator_methods,
+};

_drgn.pyi
@@ -7,6 +7,7 @@ libdrgn bindings
 Don't use this module directly. Instead, use the drgn package.
 """
 
+import collections.abc
 import enum
 import os
 import sys
@@ -2328,6 +2329,26 @@ class TypeKind(enum.Enum):
 
     FUNCTION = ...
     """Function type."""
+
+class TypeKindSet(collections.abc.Set[TypeKind]):
+    """
+    Immutable set of :class:`TypeKind`\\ s.
+
+    >>> kinds = TypeKindSet({TypeKind.STRUCT, TypeKind.CLASS})
+    >>> TypeKind.STRUCT in kinds
+    True
+    >>> TypeKind.INT in kinds
+    False
+    >>> for kind in kinds:
+    ...     print(kind)
+    ...
+    TypeKind.STRUCT
+    TypeKind.CLASS
+    """
+
+    def __contains__(self, __x: object) -> bool: ...
+    def __iter__(self) -> Iterator[TypeKind]: ...
+    def __len__(self) -> int: ...
 
 class PrimitiveType(enum.Enum):
     """A ``PrimitiveType`` represents a primitive type known to drgn."""

drgn/__init__.py
@@ -75,6 +75,7 @@ from _drgn import (
     Type,
     TypeEnumerator,
     TypeKind,
+    TypeKindSet,
     TypeMember,
     TypeParameter,
     TypeTemplateParameter,
@@ -129,6 +130,7 @@ __all__ = (
     "Type",
     "TypeEnumerator",
     "TypeKind",
+    "TypeKindSet",
     "TypeMember",
     "TypeParameter",
     "TypeTemplateParameter",

libdrgn/Makefile.am
@@ -172,6 +172,7 @@ _drgn_la_SOURCES = python/constants.c \
 		   python/test.c \
 		   python/thread.c \
 		   python/type.c \
+		   python/type_kind_set.c \
 		   python/util.c
 
 _drgn_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden

libdrgn/bitops.h
@@ -87,6 +87,18 @@
 #define ilog2_impl(arg, suffix, x)	\
 	((8 * sizeof(0u##suffix) - 1) ^ __builtin_clz##suffix(x))
 
+/**
+ * Bit population count.
+ *
+ * Return the number of 1-bits in @p x.
+ *
+ * ```
+ * popcount(8) == 1
+ * popcount(3) == 2
+ * ```
+ */
+#define popcount(x) generic_bitop(x, PP_UNIQUE(_x), builtin_bitop_impl, popcount)
+
 #define builtin_bitop_impl(arg, suffix, x) __builtin_##arg##suffix(x)
 #define generic_bitop(x, unique_x, impl, impl_arg) ({			\
 	__auto_type unique_x = (x);					\

libdrgn/python/drgnpy.h
@@ -197,6 +197,16 @@ typedef struct {
 } LazyObject;
 
 typedef struct {
+	PyObject_HEAD
+	uint64_t kinds;
+} TypeKindSet;
+
+typedef struct {
+	PyObject_HEAD
+	uint64_t mask;
+} TypeKindSetIterator;
+
+typedef struct {
 	LazyObject lazy_obj;
 	PyObject *name;
 	PyObject *bit_offset;
@@ -236,6 +246,8 @@ extern PyTypeObject Symbol_type;
 extern PyTypeObject Thread_type;
 extern PyTypeObject ThreadIterator_type;
 extern PyTypeObject TypeEnumerator_type;
+extern PyTypeObject TypeKindSet_type;
+extern PyTypeObject TypeKindSetIterator_type;
 extern PyTypeObject TypeMember_type;
 extern PyTypeObject TypeParameter_type;
 extern PyTypeObject TypeTemplateParameter_type;
@@ -258,6 +270,9 @@ void *set_error_type_name(const char *format,
 PyObject *Language_wrap(const struct drgn_language *language);
 int language_converter(PyObject *o, void *p);
 int add_languages(void);
+
+PyObject *TypeKindSet_wrap(uint64_t mask);
+int init_type_kind_set(void);
 
 static inline DrgnObject *DrgnObject_alloc(Program *prog)
 {

libdrgn/python/main.c
@@ -277,6 +277,9 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	    add_type(m, &Thread_type) ||
 	    add_type(m, &ThreadIterator_type) ||
 	    add_type(m, &TypeEnumerator_type) ||
+	    add_type(m, &TypeKindSet_type) ||
+	    PyType_Ready(&TypeKindSetIterator_type) ||
+	    init_type_kind_set() ||
 	    add_type(m, &TypeMember_type) ||
 	    add_type(m, &TypeParameter_type) ||
 	    add_type(m, &TypeTemplateParameter_type) ||

tests/test_type_kind_set.py
@@ -1,0 +1,308 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from drgn import TypeKind, TypeKindSet
+from tests import TestCase
+
+
+class TestTypeKindSet(TestCase):
+    def test_repr(self):
+        self.assertEqual(
+            repr(TypeKindSet()),
+            "TypeKindSet()",
+        )
+        self.assertEqual(
+            repr(TypeKindSet({TypeKind.STRUCT})),
+            "TypeKindSet({TypeKind.STRUCT})",
+        )
+        self.assertEqual(
+            repr(TypeKindSet({TypeKind.STRUCT, TypeKind.CLASS})),
+            "TypeKindSet({TypeKind.STRUCT, TypeKind.CLASS})",
+        )
+
+    def test_len(self):
+        self.assertEqual(len(TypeKindSet()), 0)
+        self.assertEqual(len(TypeKindSet({TypeKind.STRUCT})), 1)
+        self.assertEqual(len(TypeKindSet({TypeKind.INT, TypeKind.STRUCT})), 2)
+
+    def test_in(self):
+        self.assertNotIn(TypeKind.INT, TypeKindSet())
+        self.assertIn(TypeKind.INT, TypeKindSet({TypeKind.INT}))
+        self.assertIn(TypeKind.INT, TypeKindSet({TypeKind.INT, TypeKind.STRUCT}))
+        self.assertIn(TypeKind.STRUCT, TypeKindSet({TypeKind.INT, TypeKind.STRUCT}))
+        self.assertNotIn(TypeKind.UNION, TypeKindSet({TypeKind.INT, TypeKind.STRUCT}))
+        self.assertNotIn(0, TypeKindSet({TypeKind.INT}))
+        self.assertNotIn(TypeKind.INT.value, TypeKindSet({TypeKind.INT}))
+
+    def test_iter(self):
+        for s in (
+            set(),
+            {TypeKind.VOID},
+            {TypeKind.ARRAY, TypeKind.POINTER},
+        ):
+            self.assertEqual(set(TypeKindSet(s)), s)
+
+    def test_eq(self):
+        self.assertTrue(TypeKindSet() == TypeKindSet())
+        self.assertFalse(TypeKindSet() == TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(TypeKindSet({TypeKind.FLOAT}) == TypeKindSet({TypeKind.FLOAT}))
+        self.assertFalse(
+            TypeKindSet({TypeKind.FUNCTION}) == TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            == TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FUNCTION})
+            == TypeKindSet({TypeKind.BOOL, TypeKind.ARRAY}),
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            == {TypeKind.FLOAT, TypeKind.TYPEDEF}
+        )
+        self.assertFalse(TypeKindSet() == {"asdf"})
+        self.assertFalse(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            == {TypeKind.FLOAT, TypeKind.TYPEDEF, "foo"}
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            == [TypeKind.FLOAT, TypeKind.TYPEDEF]
+        )
+
+    def test_ne(self):
+        self.assertFalse(TypeKindSet() != TypeKindSet())
+        self.assertTrue(TypeKindSet() != TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(TypeKindSet({TypeKind.FLOAT}) != TypeKindSet({TypeKind.FLOAT}))
+        self.assertTrue(
+            TypeKindSet({TypeKind.FUNCTION}) != TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            != TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FUNCTION})
+            != TypeKindSet({TypeKind.BOOL, TypeKind.ARRAY}),
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            != {TypeKind.FLOAT, TypeKind.TYPEDEF}
+        )
+        self.assertTrue(TypeKindSet() != {"asdf"})
+        self.assertTrue(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            != {TypeKind.FLOAT, TypeKind.TYPEDEF, "foo"}
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.FLOAT, TypeKind.TYPEDEF})
+            != [TypeKind.FLOAT, TypeKind.TYPEDEF]
+        )
+
+    def test_lt(self):
+        self.assertFalse(TypeKindSet() < TypeKindSet())
+        self.assertTrue(TypeKindSet() < TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(TypeKindSet({TypeKind.BOOL}) < TypeKindSet())
+        self.assertFalse(TypeKindSet({TypeKind.BOOL}) < TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL}) < TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT})
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT}) < TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertFalse(TypeKindSet({TypeKind.INT}) < TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL}) < {TypeKind.BOOL, TypeKind.FLOAT, "foo"}
+        )
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.BOOL}) < [TypeKind.BOOL, TypeKind.FLOAT]
+
+    def test_gt(self):
+        self.assertFalse(TypeKindSet() > TypeKindSet())
+        self.assertFalse(TypeKindSet() > TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(TypeKindSet({TypeKind.BOOL}) > TypeKindSet())
+        self.assertFalse(TypeKindSet({TypeKind.BOOL}) > TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL}) > TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT})
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT}) > TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertFalse(TypeKindSet({TypeKind.INT}) > TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT}) > {TypeKind.BOOL, "foo"}
+        )
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.BOOL}) > []
+
+    def test_le(self):
+        self.assertTrue(TypeKindSet() <= TypeKindSet())
+        self.assertTrue(TypeKindSet() <= TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(TypeKindSet({TypeKind.BOOL}) <= TypeKindSet())
+        self.assertTrue(TypeKindSet({TypeKind.BOOL}) <= TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL}) <= TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT})
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT}) <= TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertFalse(TypeKindSet({TypeKind.INT}) <= TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(TypeKindSet({TypeKind.BOOL}) <= {TypeKind.BOOL, "foo"})
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.BOOL}) <= [TypeKind.BOOL, TypeKind.FLOAT]
+
+    def test_ge(self):
+        self.assertTrue(TypeKindSet() >= TypeKindSet())
+        self.assertFalse(TypeKindSet() >= TypeKindSet({TypeKind.BOOL}))
+        self.assertTrue(TypeKindSet({TypeKind.BOOL}) >= TypeKindSet())
+        self.assertTrue(TypeKindSet({TypeKind.BOOL}) >= TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(
+            TypeKindSet({TypeKind.BOOL}) >= TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT})
+        )
+        self.assertTrue(
+            TypeKindSet({TypeKind.BOOL, TypeKind.FLOAT}) >= TypeKindSet({TypeKind.BOOL})
+        )
+        self.assertFalse(TypeKindSet({TypeKind.INT}) >= TypeKindSet({TypeKind.BOOL}))
+        self.assertFalse(TypeKindSet({TypeKind.BOOL}) >= {TypeKind.BOOL, "foo"})
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.BOOL}) >= [TypeKind.BOOL]
+
+    def test_isdisjoint(self):
+        self.assertTrue(TypeKindSet().isdisjoint(TypeKindSet()))
+        self.assertTrue(
+            TypeKindSet({TypeKind.POINTER}).isdisjoint(TypeKindSet({TypeKind.ARRAY}))
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.POINTER}).isdisjoint(TypeKindSet({TypeKind.POINTER}))
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.ARRAY}).isdisjoint(
+                TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            )
+        )
+        self.assertFalse(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}).isdisjoint(
+                TypeKindSet({TypeKind.ARRAY})
+            )
+        )
+        self.assertTrue(TypeKindSet({TypeKind.POINTER}).isdisjoint({TypeKind.ARRAY}))
+        self.assertTrue(TypeKindSet({TypeKind.POINTER}).isdisjoint([TypeKind.ARRAY]))
+        self.assertFalse(TypeKindSet({TypeKind.POINTER}).isdisjoint({TypeKind.POINTER}))
+        self.assertFalse(TypeKindSet({TypeKind.POINTER}).isdisjoint([TypeKind.POINTER]))
+
+    def test_sub(self):
+        self.assertEqual(TypeKindSet() - TypeKindSet(), TypeKindSet())
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) - TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) - TypeKindSet({TypeKind.POINTER}),
+            TypeKindSet(),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.ARRAY})
+            - TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+            TypeKindSet(),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            - TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}) - {TypeKind.ARRAY},
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}) - {TypeKind.ARRAY, "asdf"},
+            TypeKindSet({TypeKind.POINTER}),
+        )
+
+    def test_and(self):
+        self.assertEqual(TypeKindSet() & TypeKindSet(), TypeKindSet())
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) & TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet(),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) & TypeKindSet({TypeKind.POINTER}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.ARRAY})
+            & TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            & TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}) & {TypeKind.ARRAY},
+            TypeKindSet({TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            & {TypeKind.POINTER, "asdf"},
+            TypeKindSet({TypeKind.POINTER}),
+        )
+
+    def test_xor(self):
+        self.assertEqual(TypeKindSet() ^ TypeKindSet(), TypeKindSet())
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) ^ TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) ^ TypeKindSet({TypeKind.POINTER}),
+            TypeKindSet(),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.ARRAY})
+            ^ TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            ^ TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}) ^ {TypeKind.ARRAY},
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}) ^ {TypeKind.ARRAY, "asdf"}
+
+    def test_or(self):
+        self.assertEqual(TypeKindSet() | TypeKindSet(), TypeKindSet())
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) | TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) | TypeKindSet({TypeKind.POINTER}),
+            TypeKindSet({TypeKind.POINTER}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.ARRAY})
+            | TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY})
+            | TypeKindSet({TypeKind.ARRAY}),
+            TypeKindSet({TypeKind.POINTER, TypeKind.ARRAY}),
+        )
+        self.assertEqual(
+            TypeKindSet({TypeKind.POINTER}) | {TypeKind.ARRAY},
+            {TypeKind.POINTER, TypeKind.ARRAY},
+        )
+        with self.assertRaises(TypeError):
+            TypeKindSet({TypeKind.POINTER}) | {TypeKind.ARRAY, "asdf"}
+
+    def test_all_kinds(self):
+        self.assertEqual(set(TypeKindSet(TypeKind)), set(TypeKind))

commit 57e4e3327d4aad698d5ee0b475cd2067cd687956

python: fix double decref in init_type_kind_set()
We're explicitly calling Py_DECREF() on a variable that we defined with
_cleanup_pydecref_. Luckily, the variable should be set to Py_None, so
it's unlikely to be a problem in practice.

Fixes: 9b73b449089e ("python: add TypeKindSet")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/type_kind_set.c
@@ -29,7 +29,6 @@ int init_type_kind_set(void)
 				    &TypeKindSet_type);
 	if (!res)
 		return -1;
-	Py_DECREF(res);
 	return 0;
 }
 