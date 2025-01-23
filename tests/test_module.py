osandov/drgn

commit 50e4ac82452acd86c75e28a301a6eb36ce7e3d09

libdrgn: allow overriding program default language
Our cheap heuristic for the default language will not always be correct,
and although we can improve it as cases arise, we should also just have
a way for the user to explicitly set the default language. Add
drgn_program_set_language() to libdrgn and allow setting
drgn.Program.language in the Python bindings. This will also make unit
testing different languages easier.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/program.c
@@ -985,6 +985,16 @@ static PyObject *Program_get_language(Program *self, void *arg)
 	return Language_wrap(drgn_program_language(&self->prog));
 }
 
+static int Program_set_language(Program *self, PyObject *value, void *arg)
+{
+	if (!PyObject_TypeCheck(value, &Language_type)) {
+		PyErr_SetString(PyExc_TypeError, "language must be Language");
+		return -1;
+	}
+	drgn_program_set_language(&self->prog, ((Language *)value)->language);
+	return 0;
+}
+
 static PyMethodDef Program_methods[] = {
 	{"add_memory_segment", (PyCFunction)Program_add_memory_segment,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_add_memory_segment_DOC},
@@ -1079,7 +1089,7 @@ static PyGetSetDef Program_getset[] = {
 	{"flags", (getter)Program_get_flags, NULL, drgn_Program_flags_DOC},
 	{"platform", (getter)Program_get_platform, NULL,
 	 drgn_Program_platform_DOC},
-	{"language", (getter)Program_get_language, NULL,
+	{"language", (getter)Program_get_language, (setter)Program_set_language,
 	 drgn_Program_language_DOC},
 	{},
 };

tests/test_program.py
@@ -10,6 +10,7 @@ from drgn import (
     Architecture,
     FaultError,
     FindObjectFlags,
+    Language,
     Object,
     Platform,
     PlatformFlags,
@@ -126,7 +127,15 @@ class TestProgram(TestCase):
         Program().load_debug_info([])
 
     def test_language(self):
-        self.assertEqual(Program().language, DEFAULT_LANGUAGE)
+        prog = Program()
+        self.assertEqual(prog.language, DEFAULT_LANGUAGE)
+        prog.language = Language.CPP
+        self.assertEqual(prog.language, Language.CPP)
+        prog.language = Language.C
+        self.assertEqual(prog.language, Language.C)
+        self.assertRaisesRegex(
+            TypeError, "language must be Language", setattr, prog, "language", "CPP"
+        )
 
 
 class TestMemory(TestCase):

libdrgn/program.c
@@ -76,6 +76,12 @@ drgn_program_language(struct drgn_program *prog)
 	return prog->lang ? prog->lang : &drgn_default_language;
 }
 
+LIBDRGN_PUBLIC void drgn_program_set_language(struct drgn_program *prog,
+					      const struct drgn_language *lang)
+{
+	prog->lang = lang;
+}
+
 void drgn_program_set_platform(struct drgn_program *prog,
 			       const struct drgn_platform *platform)
 {

_drgn.pyi
@@ -87,9 +87,12 @@ class Program:
     This is used for interpreting the type name given to :meth:`type()` and
     when creating an :class:`Object` without an explicit type.
 
-    For the Linux kernel, this is :attr:`Language.C`. For userspace programs,
-    this is determined from the language of ``main`` in the program, falling
+    For the Linux kernel, this defaults to :attr:`Language.C`. For userspace
+    programs, this defaults to the language of ``main`` in the program, falling
     back to :attr:`Language.C`. This heuristic may change in the future.
+
+    This can be explicitly set to a different language (e.g., if the heuristic
+    was incorrect).
     """
     def __getitem__(self, name: str) -> Object:
         """

libdrgn/drgn.h.in
@@ -713,6 +713,10 @@ const struct drgn_platform *drgn_program_platform(struct drgn_program *prog);
 /** Get the default language of a @ref drgn_program. */
 const struct drgn_language *drgn_program_language(struct drgn_program *prog);
 
+/** Set the default language of a @ref drgn_program. */
+void drgn_program_set_language(struct drgn_program *prog,
+			       const struct drgn_language *lang);
+
 /**
  * Read from a program's memory.
  *

commit 4738ddfa5c01dda14e42f6696a8294804faa32e4

libdrgn/python: fix segfault on del prog.language and more
None of our setter functions handle deletions, which pass the value as
NULL. This results in a segfault when attempting to access the value.
Fix them all with a new convenience macro.

Fixes: 50e4ac82452a ("libdrgn: allow overriding program default language")
Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/drgnpy.h
@@ -91,6 +91,18 @@ static inline PyObject *PyObject_CallOneArg(PyObject *callable, PyObject *arg)
 		Py_RETURN_FALSE;	\
 } while (0)
 
+/**
+ * Return from a PyGetSetDef setter with an error if attempting to delete the
+ * attribute.
+ */
+#define SETTER_NO_DELETE(name, value) do {				\
+	if (!(value)) {							\
+		PyErr_Format(PyExc_AttributeError,			\
+			     "can't delete '%s' attribute", (name));	\
+		return -1;						\
+	}								\
+} while (0)
+
 static inline void pydecrefp(void *p)
 {
 	Py_XDECREF(*(PyObject **)p);

libdrgn/python/module.c
@@ -214,6 +214,7 @@ static PyObject *Module_get_address_range(Module *self, void *arg)
 
 static int Module_set_address_range(Module *self, PyObject *value, void *arg)
 {
+	SETTER_NO_DELETE("address_range", value);
 	struct drgn_error *err;
 	if (value == Py_None) {
 		err = drgn_module_set_address_range(self->module, -1, -1);
@@ -258,6 +259,7 @@ static PyObject *Module_get_build_id(Module *self, void *arg)
 
 static int Module_set_build_id(Module *self, PyObject *value, void *arg)
 {
+	SETTER_NO_DELETE("build_id", value);
 	struct drgn_error *err;
 	if (value == Py_None) {
 		err = drgn_module_set_build_id(self->module, NULL, 0);
@@ -300,6 +302,7 @@ static PyObject *Module_get_##which##_file_status(Module *self, void *arg)	\
 static int Module_set_##which##_file_status(Module *self, PyObject *value,	\
 					    void *arg)				\
 {										\
+	SETTER_NO_DELETE(#which, value);					\
 	if (!PyObject_TypeCheck(value,						\
 				(PyTypeObject *)ModuleFileStatus_class)) {	\
 		PyErr_SetString(PyExc_TypeError,				\

libdrgn/python/program.c
@@ -1286,6 +1286,7 @@ static PyObject *Program_get_debug_info_path(Program *self, void *arg)
 
 static int Program_set_debug_info_path(Program *self, PyObject *value, void *arg)
 {
+	SETTER_NO_DELETE("debug_info_path", value);
 	const char *path;
 	if (value == Py_None) {
 		path = NULL;
@@ -1909,6 +1910,7 @@ static PyObject *Program_get_language(Program *self, void *arg)
 
 static int Program_set_language(Program *self, PyObject *value, void *arg)
 {
+	SETTER_NO_DELETE("language", value);
 	if (!PyObject_TypeCheck(value, &Language_type)) {
 		PyErr_SetString(PyExc_TypeError, "language must be Language");
 		return -1;

tests/test_module.py
@@ -351,6 +351,11 @@ class TestModule(TestCase):
         with self.assertRaisesRegex(ValueError, "invalid module address range"):
             module.address_range = (2**64 - 1, 2**64 - 1)
 
+    def test_address_range_del(self):
+        module = Program().extra_module("/foo/bar", create=True)[0]
+        with self.assertRaises(AttributeError):
+            del module.address_range
+
     def test_build_id(self):
         module = Program().extra_module("/foo/bar", create=True)[0]
 
@@ -375,6 +380,11 @@ class TestModule(TestCase):
         module = Program().extra_module("/foo/bar", create=True)[0]
         with self.assertRaisesRegex(ValueError, "build ID cannot be empty"):
             module.build_id = b""
+
+    def test_build_id_del(self):
+        module = Program().extra_module("/foo/bar", create=True)[0]
+        with self.assertRaises(AttributeError):
+            del module.build_id
 
     def test_find_by_address(self):
         prog = Program()
@@ -455,6 +465,8 @@ class TestModule(TestCase):
 
         setattr(module, status_attr, ModuleFileStatus.WANT)
         self.assertEqual(getattr(module, status_attr), ModuleFileStatus.WANT)
+
+        self.assertRaises(AttributeError, delattr, module, status_attr)
 
     def test_loaded_file_status(self):
         self._test_file_status("loaded")

tests/test_program.py
@@ -162,6 +162,10 @@ class TestProgram(TestCase):
             TypeError, "language must be Language", setattr, prog, "language", "CPP"
         )
 
+    def test_language_del(self):
+        with self.assertRaises(AttributeError):
+            del Program().language
+
 
 class TestMemory(TestCase):
     def test_simple_read(self):