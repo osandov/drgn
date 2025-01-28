osandov/drgn

commit 4e83130008e9c28c3d775a0ab40075f2f9123720

Introduce module and debug info finder APIs
drgn currently provides limited control over how debugging information
is found. drgn has hardcoded logic for where to search for debugging
information. The most the user can do is provide a list of files for
drgn to try in addition to the default locations (with the -s CLI option
or the drgn.Program.load_debug_info() method).

The implementation is also a mess. We use libdwfl, but its data model is
slightly different from what we want, so we have to work around it or
reimplement its functionality in several places: see commits
e5874ad18a53 ("libdrgn: use libdwfl"), e6abfeac0329 ("libdrgn:
debug_info: report userspace core dump debug info ourselves"), and
1d4854a5bce7 ("libdrgn: implement optimized x86-64 ELF relocations") for
some examples. The mismatched combination of libdwfl and our own code is
difficult to maintain, and the lack of control over the whole debug info
pipeline has made it difficult to fix several longstanding issues.

The solution is a major rework removing our libdwfl dependency and
replacing it with our own model. This (huge) commit is that rework
comprising the following components:

- drgn.Module/struct drgn_module, a representation of a binary used by a
  program.
- Automatic discovery of the modules loaded in a program.
- Interfaces for manually creating and overriding modules.
- Automatic discovery of debugging information from the standard
  locations and debuginfod.
- Interfaces for custom debug info finders and for manually overriding
  debugging information.
- Tons of test cases.

A lot of care was taken to make these interfaces extremely flexible yet
cohesive. The existing interfaces are also reimplemented on top of the
new functionality to maintain backwards compatibility, with one
exception: drgn.Program.load_debug_info()/-s would previously accept
files that it didn't find loaded in the program. This turned out to be a
big footgun for users, so now this must be done explicitly (with

libdrgn/elf_file.h
@@ -43,6 +43,14 @@ struct drgn_module;
  */
 struct drgn_error *read_elf_section(Elf_Scn *scn, Elf_Data **ret);
 
+/**
+ * Truncate any bytes beyond the last null character in an ELF string table.
+ *
+ * This sets `data->d_size` so that any string table index less than
+ * `data->d_size` is guaranteed to be valid.
+ */
+void truncate_elf_string_data(Elf_Data *data);
+
 static inline bool elf_data_contains_ptr(Elf_Data *data, const void *ptr)
 {
 	uintptr_t bufi = (uintptr_t)data->d_buf;
@@ -55,11 +63,38 @@ struct drgn_elf_file {
 	/** Module using this file. */
 	struct drgn_module *module;
 	/** Filesystem path to this file. */
-	const char *path;
+	char *path;
+	/**
+	 * Memory image backing @ref elf.
+	 *
+	 * @c NULL if not backed by a memory image.
+	 */
+	char *image;
+	/**
+	 * File descriptor backing @ref elf.
+	 *
+	 * -1 if not backed by a file.
+	 */
+	int fd;
+	/** Whether the file is loadable. */
+	bool is_loadable;
+	/** Whether the file is relocatable. */
+	bool is_relocatable;
+	/** Whether the file still need to have relocations applied. */
+	bool needs_relocation;
+	/** Whether the file is a Linux kernel image (`vmlinux`). */
+	bool is_vmlinux;
 	/** libelf handle. */
 	Elf *elf;
-	/** libdw handle if we're using DWARF information from this file. */
-	Dwarf *dwarf;
+	/**
+	 * libdw handle.
+	 *
+	 * @c NULL if not yet created.
+	 *
+	 * Don't access this directly. Get it with @ref
+	 * drgn_elf_file_get_dwarf() instead.
+	 */
+	Dwarf *_dwarf;
 	/**
 	 * Platform of this file.
 	 *
@@ -86,16 +121,33 @@ struct drgn_elf_file {
 	Elf_Data *alt_debug_str_data;
 };
 
+/**
+ * Create a @ref drgn_elf_file.
+ *
+ * On success, this takes ownership of @p fd, @p image, and @p elf. @p path is
+ * copied.
+ */
 struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
-					const char *path, Elf *elf,
-					struct drgn_elf_file **ret);
+					const char *path, int fd, char *image,
+					Elf *elf, struct drgn_elf_file **ret);
 
 void drgn_elf_file_destroy(struct drgn_elf_file *file);
 
-struct drgn_error *drgn_elf_file_precache_sections(struct drgn_elf_file *file);
-
+/** Apply ELF relocations to the file if needed. */
 struct drgn_error *
-drgn_elf_file_cache_section(struct drgn_elf_file *file, enum drgn_section_index scn);
+drgn_elf_file_apply_relocations(struct drgn_elf_file *file);
+
+/**
+ * Read an indexed ELF section.
+ *
+ * This applies ELF relocations to the file first if needed.
+ */
+struct drgn_error *drgn_elf_file_read_section(struct drgn_elf_file *file,
+					      enum drgn_section_index scn,
+					      Elf_Data **ret);
+
+struct drgn_error *drgn_elf_file_get_dwarf(struct drgn_elf_file *file,
+					   Dwarf **ret);
 
 static inline bool
 drgn_elf_file_is_little_endian(const struct drgn_elf_file *file)
@@ -108,6 +160,12 @@ static inline bool drgn_elf_file_bswap(const struct drgn_elf_file *file)
 	return drgn_platform_bswap(&file->platform);
 }
 
+static inline bool
+drgn_elf_file_is_64_bit(const struct drgn_elf_file *file)
+{
+	return drgn_platform_is_64_bit(&file->platform);
+}
+
 static inline uint8_t
 drgn_elf_file_address_size(const struct drgn_elf_file *file)
 {
@@ -118,6 +176,12 @@ static inline uint64_t
 drgn_elf_file_address_mask(const struct drgn_elf_file *file)
 {
 	return drgn_platform_address_mask(&file->platform);
+}
+
+static inline bool drgn_elf_file_has_dwarf(const struct drgn_elf_file *file)
+{
+	return (file->scns[DRGN_SCN_DEBUG_INFO]
+		&& file->scns[DRGN_SCN_DEBUG_ABBREV]);
 }
 
 struct drgn_error *
@@ -156,6 +220,10 @@ drgn_elf_file_section_buffer_init(struct drgn_elf_file_section_buffer *buffer,
 	buffer->data = data;
 }
 
+/**
+ * Initialize a @ref binary_buffer for an indexed ELF section that has already
+ * been read.
+ */
 static inline void
 drgn_elf_file_section_buffer_init_index(struct drgn_elf_file_section_buffer *buffer,
 					struct drgn_elf_file *file,
@@ -164,6 +232,32 @@ drgn_elf_file_section_buffer_init_index(struct drgn_elf_file_section_buffer *buf
 	drgn_elf_file_section_buffer_init(buffer, file, file->scns[scn],
 					  file->scn_data[scn]);
 }
+
+/**
+ * Read an indexed ELF section (applying ELF relocations if needed) and
+ * initialize a @ref binary_buffer for it.
+ */
+static inline struct drgn_error *
+drgn_elf_file_section_buffer_read(struct drgn_elf_file_section_buffer *buffer,
+				  struct drgn_elf_file *file,
+				  enum drgn_section_index scn)
+{
+	Elf_Data *data;
+	struct drgn_error *err = drgn_elf_file_read_section(file, scn, &data);
+	if (err)
+		return err;
+	drgn_elf_file_section_buffer_init(buffer, file, file->scns[scn], data);
+	return NULL;
+}
+
+/**
+ * Return the virtual address range of an ELF file.
+ *
+ * @param[out] start_ret Minimum virtual address (inclusive).
+ * @param[out] end_ret Maximum virtual address (exclusive).
+ */
+bool drgn_elf_file_address_range(struct drgn_elf_file *file,
+				 uint64_t *start_ret, uint64_t *end_ret);
 
 /** @} */
 

tests/linux_kernel/test_debug_info.py
@@ -2,49 +2,89 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import os
-from pathlib import Path
-import unittest
 
-from drgn import Program
+from drgn import Program, RelocatableModule
+from drgn.helpers.linux.module import find_module
 from tests import modifyenv
 from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
 
-KALLSYMS_PATH = Path("/proc/kallsyms")
+
+def iter_proc_modules():
+    try:
+        f = open("/proc/modules", "r")
+    except FileNotFoundError:
+        return
+    with f:
+        for line in f:
+            tokens = line.split()
+            yield tokens[0], int(tokens[5], 16)
 
 
-@unittest.skipUnless(
-    KALLSYMS_PATH.exists(), "kernel does not have kallsyms (CONFIG_KALLSYMS)"
-)
-@skip_unless_have_test_kmod
-class TestModuleDebugInfo(LinuxKernelTestCase):
-    # Arbitrary symbol that we can use to check that the module debug info was
-    # loaded.
-    SYMBOL = "drgn_test_function"
+class TestModule(LinuxKernelTestCase):
+    def test_loaded_modules(self):
+        expected = [("kernel", None), *iter_proc_modules()]
 
-    def setUp(self):
-        super().setUp()
-        with KALLSYMS_PATH.open() as f:
-            for line in f:
-                tokens = line.split()
-                if tokens[2] == self.SYMBOL:
-                    self.symbol_address = int(tokens[0], 16)
-                    break
+        loaded_modules = []
+        for module, _ in self.prog.loaded_modules():
+            if isinstance(module, RelocatableModule):
+                loaded_modules.append((module.name, module.address))
             else:
-                self.fail(f"{self.SYMBOL!r} symbol not found")
+                loaded_modules.append((module.name, None))
 
-    def _test_module_debug_info(self, use_sys_module):
-        old_use_sys_module = int(os.environ.get("DRGN_USE_SYS_MODULE", "1")) != 0
-        with modifyenv({"DRGN_USE_SYS_MODULE": "1" if use_sys_module else "0"}):
-            if old_use_sys_module == use_sys_module:
-                prog = self.prog
+        self.assertCountEqual(loaded_modules, expected)
+
+    @skip_unless_have_test_kmod
+    def test_find(self):
+        self.assertEqual(self.prog.main_module().name, "kernel")
+        for name, address in iter_proc_modules():
+            if name == "drgn_test":
+                self.assertEqual(
+                    self.prog.relocatable_module(name, address).name, "drgn_test"
+                )
+                break
+        else:
+            self.fail("test module not found")
+
+    @skip_unless_have_test_kmod
+    def test_find_by_obj(self):
+        for module in self.prog.modules():
+            if module.name == "drgn_test":
+                break
+        else:
+            self.fail("test module not found")
+
+        module_obj = find_module(self.prog, "drgn_test")
+        self.assertEqual(self.prog.linux_kernel_loadable_module(module_obj), module)
+        self.assertEqual(
+            self.prog.linux_kernel_loadable_module(module_obj, create=True),
+            (module, False),
+        )
+
+    def test_no_sys_module(self):
+        # Test that we get the same modules with and without using /sys/module.
+
+        def module_dict(prog):
+            return {
+                (module.name, module.address): (
+                    module.address_range,
+                    module.build_id,
+                    dict(module.section_addresses),
+                )
+                for module, _ in prog.loaded_modules()
+                if isinstance(module, RelocatableModule)
+            }
+
+        use_sys_module = int(os.environ.get("DRGN_USE_SYS_MODULE", "1")) != 0
+
+        with modifyenv({"DRGN_USE_SYS_MODULE": str(int(not use_sys_module))}):
+            prog = Program()
+            prog.set_kernel()
+
+            if use_sys_module:
+                with_sys_module = module_dict(self.prog)
+                without_sys_module = module_dict(prog)
             else:
-                prog = Program()
-                prog.set_kernel()
-                self._load_debug_info(prog)
-            self.assertEqual(prog.symbol(self.SYMBOL).address, self.symbol_address)
+                with_sys_module = module_dict(prog)
+                without_sys_module = module_dict(self.prog)
 
-    def test_module_debug_info_use_proc_and_sys(self):
-        self._test_module_debug_info(True)
-
-    def test_module_debug_info_use_core_dump(self):
-        self._test_module_debug_info(False)
+            self.assertEqual(with_sys_module, without_sys_module)

commit 45c4a228ac3e3c58603f16f46939b4e4e727d903

libdrgn: debug_info: match vmlinux by version if build ID is not available
Since the module API was introduced, Program.load_debug_info() and the
drgn CLI's -s option match strictly based on build IDs. This fails when
the build ID is not available, specifically in the case of Linux kernel
core dumps without a usable build ID in VMCOREINFO (old versions and a
few buggy stable versions).

Before the module API, Program.load_debug_info() and -s used any vmlinux
file given to them. This caused confusion when the wrong file was given,
so we don't want to bring that behavior back. Instead, let's look for a
vmlinux file matching the Linux version from VMCOREINFO.

Fixes #464.

Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -4969,6 +4969,80 @@ load_debug_info_try_provided_supplementary_files(struct drgn_module *module,
 }
 
 static struct drgn_error *
+load_debug_info_try_provided_vmlinux(struct drgn_module *module,
+				     struct load_debug_info_state *state)
+{
+	struct drgn_error *err;
+	struct drgn_program *prog = module->prog;
+	bool logged_trying = false;
+	for (auto it = load_debug_info_provided_table_first(&state->provided);
+	     it.entry;
+	     it = load_debug_info_provided_table_next(it)) {
+		vector_for_each(load_debug_info_file_vector, file,
+				&it.entry->files) {
+			int r = elf_is_vmlinux(file->elf);
+			if (r < 0) {
+				drgn_log_debug(prog, "%s: %s", file->path,
+					       elf_errmsg(-1));
+			}
+			if (r <= 0)
+				continue;
+
+			if (!logged_trying) {
+				drgn_module_try_files_log(module,
+							  "(Linux version %s): trying provided files for",
+							  prog->vmcoreinfo.osrelease);
+				logged_trying = true;
+			}
+
+			const char *release;
+			ssize_t release_len =
+				elf_vmlinux_release(file->elf, &release);
+			if (release_len < 0) {
+				drgn_log_debug(prog, "%s: %s", file->path,
+					       elf_errmsg(-1));
+				continue;
+			} else if (release_len == 0) {
+				drgn_log_debug(prog, "%s: %s Linux version not found",
+					       module->name, file->path);
+				continue;
+			}
+
+			if (strlen(prog->vmcoreinfo.osrelease) == release_len
+			    && memcmp(release, prog->vmcoreinfo.osrelease,
+				      release_len) == 0) {
+				drgn_log_debug(prog, "%s: %s Linux version matches",
+					       module->name, file->path);
+			} else {
+				drgn_log_debug(prog,
+					       "%s: %s Linux version (%.*s) does not match",
+					       module->name, file->path,
+					       release_len > INT_MAX
+					       ? INT_MAX : (int)release_len,
+					       release);
+				continue;
+			}
+
+			if (!it.entry->matched) {
+				state->unmatched_provided--;
+				it.entry->matched = true;
+			}
+
+			err = drgn_module_try_file_internal(module, file->path,
+							    file->fd, true,
+							    NULL);
+			file->fd = -1;
+			if (err)
+				return err;
+			if (module->loaded_file_status != DRGN_MODULE_FILE_WANT
+			    && module->debug_file_status != DRGN_MODULE_FILE_WANT)
+				break;
+		}
+	}
+	return NULL;
+}
+
+static struct drgn_error *
 load_debug_info_try_provided_files(struct drgn_module *module,
 				   struct load_debug_info_state *state)
 {
@@ -4981,7 +5055,7 @@ load_debug_info_try_provided_files(struct drgn_module *module,
 	const void *build_id;
 	size_t build_id_len;
 	drgn_module_build_id(module, &build_id, &build_id_len);
-	if (build_id_len != 0) {
+	if (build_id_len > 0) {
 		// Look up the provided file even if we don't need it so that it
 		// counts as matched.
 		struct load_debug_info_provided *provided =
@@ -5006,6 +5080,18 @@ load_debug_info_try_provided_files(struct drgn_module *module,
 					return err;
 			}
 		}
+	} else if (module->prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL
+		   && drgn_module_kind(module) == DRGN_MODULE_MAIN) {
+		// Before Linux kernel commit 0935288c6e00 ("kdump: append
+		// kernel build-id string to VMCOREINFO") (in v5.9) and in a few
+		// broken stable versions (see
+		// ignore_broken_vmcoreinfo_build_id()), we can't get the
+		// vmlinux build ID from a kernel core dump. Fall back to
+		// checking every provided file for a vmlinux file with a
+		// matching version.
+		err = load_debug_info_try_provided_vmlinux(module, state);
+		if (err)
+			return err;
 	}
 	return NULL;
 }

libdrgn/elf_file.c
@@ -164,6 +164,7 @@ struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
 				// We consider a file to be vmlinux if it has an
 				// .init.text section and is not relocatable
 				// (which excludes kernel modules).
+				// Keep this in sync with elf_is_vmlinux().
 				file->is_vmlinux = ehdr->e_type != ET_REL;
 				index = DRGN_SECTION_INDEX_NUM;
 			} else {
@@ -760,4 +761,103 @@ bool drgn_elf_file_address_range(struct drgn_elf_file *file,
 								  start_ret,
 								  end_ret);
 	}
+}
+
+// Keep this in sync with drgn_elf_file_create().
+int elf_is_vmlinux(Elf *elf)
+{
+	GElf_Ehdr ehdr_mem, *ehdr = gelf_getehdr(elf, &ehdr_mem);
+	if (!ehdr)
+		return -1;
+
+	if (ehdr->e_type == ET_REL)
+		return 0;
+
+	size_t shstrndx;
+	if (elf_getshdrstrndx(elf, &shstrndx))
+		return -1;
+	Elf_Scn *scn = NULL;
+	while ((scn = elf_nextscn(elf, scn))) {
+		GElf_Shdr shdr_mem, *shdr = gelf_getshdr(scn, &shdr_mem);
+		if (!shdr)
+			return -1;
+
+		if (shdr->sh_type != SHT_PROGBITS)
+			continue;
+
+		const char *scnname = elf_strptr(elf, shstrndx, shdr->sh_name);
+		if (!scnname)
+			return -1;
+
+		if (strcmp(scnname, ".init.text") == 0)
+			return 1;
+	}
+	return 0;
+}
+
+ssize_t elf_vmlinux_release(Elf *elf, const char **ret)
+{
+	Elf_Scn *scn = NULL;
+	while ((scn = elf_nextscn(elf, scn))) {
+		GElf_Shdr shdr_mem, *shdr = gelf_getshdr(scn, &shdr_mem);
+		if (!shdr)
+			return -1;
+
+		if (shdr->sh_type != SHT_SYMTAB || shdr->sh_entsize == 0)
+			continue;
+
+		Elf_Data *data = elf_getdata(scn, NULL);
+		if (!data)
+			return -1;
+
+		size_t num_syms = shdr->sh_size / shdr->sh_entsize;
+		for (size_t i = 0; i < num_syms; i++) {
+			GElf_Sym sym_mem, *sym = gelf_getsym(data, i, &sym_mem);
+			if (!sym)
+				return -1;
+
+			static const char prefix[] = "Linux version ";
+
+			if (GELF_ST_TYPE(sym->st_info) != STT_OBJECT
+			    || GELF_ST_BIND(sym->st_info) != STB_GLOBAL
+			    || sym->st_size < sizeof(prefix) - 1)
+				continue;
+
+			const char *name = elf_strptr(elf, shdr->sh_link,
+						      sym->st_name);
+			if (!name)
+				return -1;
+			if (strcmp(name, "linux_banner") != 0)
+				continue;
+
+			GElf_Shdr sym_shdr_mem, *sym_shdr =
+				gelf_getshdr(elf_getscn(elf, sym->st_shndx),
+					     &sym_shdr_mem);
+			if (!sym_shdr)
+				return -1;
+
+			int64_t offset = sym_shdr->sh_offset
+					 + sym->st_value - sym_shdr->sh_addr;
+			Elf_Data *banner_data =
+				elf_getdata_rawchunk(elf, offset, sym->st_size,
+						     ELF_T_BYTE);
+			if (!banner_data)
+				return -1;
+
+			if (memcmp(banner_data->d_buf, prefix,
+				   sizeof(prefix) - 1) != 0)
+				return 0;
+
+			const char *release = (const char *)banner_data->d_buf
+					      + (sizeof(prefix) - 1);
+			const char *space =
+				memchr(release, ' ',
+				       banner_data->d_size - (sizeof(prefix) - 1));
+			if (!space)
+				return 0;
+			*ret = release;
+			return space - release;
+		}
+	}
+	return 0;
 }

libdrgn/elf_file.h
@@ -259,6 +259,21 @@ drgn_elf_file_section_buffer_read(struct drgn_elf_file_section_buffer *buffer,
 bool drgn_elf_file_address_range(struct drgn_elf_file *file,
 				 uint64_t *start_ret, uint64_t *end_ret);
 
+/**
+ * Return whether an ELF file is a vmlinux file.
+ *
+ * @return > 0 if the file is vmlinux, 0 if it is not, < 0 on libelf error.
+ */
+int elf_is_vmlinux(Elf *elf);
+
+/**
+ * Get the Linux release from a vmlinux file.
+ *
+ * @param[out] ret Returned release.
+ * @return Length of @p ret on success, 0 if not found, < 0 on libelf error.
+ */
+ssize_t elf_vmlinux_release(Elf *elf, const char **ret);
+
 /** @} */
 
 #endif /* DRGN_ELF_FILE_H */

tests/linux_kernel/test_debug_info.py
@@ -3,7 +3,7 @@
 
 import os
 
-from drgn import Program, RelocatableModule
+from drgn import MainModule, Program, RelocatableModule
 from drgn.helpers.linux.module import find_module
 from tests import modifyenv
 from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
@@ -18,6 +18,23 @@ def iter_proc_modules():
         for line in f:
             tokens = line.split()
             yield tokens[0], int(tokens[5], 16)
+
+
+class TestLoadDebugInfo(LinuxKernelTestCase):
+    def test_no_build_id(self):
+        prog = Program()
+        prog.set_kernel()
+        prog.set_enabled_debug_info_finders([])
+        for module, _ in prog.loaded_modules():
+            if isinstance(module, MainModule):
+                module.build_id = None
+                break
+        else:
+            self.fail("main module not found")
+        prog.load_debug_info([self.prog.main_module().debug_file_path])
+        self.assertEqual(
+            prog.main_module().debug_file_path, self.prog.main_module().debug_file_path
+        )
 
 
 class TestModule(LinuxKernelTestCase):