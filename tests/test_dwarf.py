osandov/drgn

commit a8e2e1719760e4367a86e7eb74e19eb1513e54f9

libdrgn: dwarf_info: resolve address of inline functions with out-of-line instances
If a function is inlined at some call sites but is also compiled to a
normal function, then there will be two DW_TAG_subprogram DIEs for it:
the abstract instance root, with the DW_AT_name but no DW_AT_low_pc, and
the concrete out-of-line instance, with the DW_AT_low_pc and a
DW_AT_abstract_origin referring to the abstract instance root but no
DW_AT_name. The DWARF index finds the former but not the latter, so the
DWARF object finder returns these functions as absent objects.

To fix this, stuff DW_AT_abstract_origin attributes into the
specifications map just like we do DW_AT_declaration attributes, and
then use that to look for a concrete out-of-line instance when we index
an abstract instance root.

There was no measurable performance difference with this change.

Fixes #147.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/dwarf_info.c
@@ -317,7 +317,14 @@ enum drgn_dwarf_index_abbrev_insn {
 	 * The byte after INSN_END contains the DIE flags, which are a bitmask
 	 * of flags combined with the drgn_dwarf_index_tag.
 	 */
-	INSN_DIE_FLAG_TAG_MASK = 0x3f,
+	INSN_DIE_FLAG_TAG_MASK = 0x1f,
+	/*
+	 * DIE has a DW_AT_inline attribute (which may be DW_INL_not_inlined or
+	 * DW_INL_declared_not_inlined). We use this to decide whether to look
+	 * for a concrete out-of-line instance of an abstract instance root, so
+	 * false positives are okay.
+	 */
+	INSN_DIE_FLAG_MAYBE_INLINED = 0x20,
 	/* DIE is a declaration. */
 	INSN_DIE_FLAG_DECLARATION = 0x40,
 	/* DIE has children. */
@@ -899,7 +906,7 @@ dw_at_specification_to_insn(struct drgn_dwarf_index_cu *cu,
 		return NULL;
 	default:
 		return binary_buffer_error(bb,
-					   "unknown attribute form %#" PRIx64 " for DW_AT_specification",
+					   "unknown attribute form %#" PRIx64 " for DW_AT_specification or DW_AT_abstract_origin",
 					   form);
 	}
 }
@@ -964,10 +971,15 @@ read_abbrev_decl(struct drgn_elf_file_section_buffer *buffer,
 		} else if (name == DW_AT_declaration && should_index) {
 			err = dw_at_declaration_to_insn(&buffer->bb, form,
 							&insn, &die_flags);
-		} else if (name == DW_AT_specification && should_index) {
+		} else if (should_index
+			   && (name == DW_AT_specification
+			       || (tag == DW_TAG_subprogram
+				   && name == DW_AT_abstract_origin))) {
 			err = dw_at_specification_to_insn(cu, &buffer->bb, form,
 							  &insn);
 		} else {
+			if (tag == DW_TAG_subprogram && name == DW_AT_inline)
+				die_flags |= INSN_DIE_FLAG_MAYBE_INLINED;
 			err = dw_form_to_insn(cu, &buffer->bb, form, &insn);
 		}
 		if (err)
@@ -1096,8 +1108,8 @@ static struct drgn_error *read_indirect_insn(struct drgn_dwarf_index_cu *cu,
 }
 
 /*
- * First pass: index DIEs with DW_AT_specification. This recurses into
- * namespaces.
+ * First pass: index DIEs with DW_AT_specification and DW_AT_abstract_origin.
+ * This recurses into namespaces.
  */
 static struct drgn_error *
 index_cu_first_pass(struct drgn_dwarf_specification_map *specifications,
@@ -1333,12 +1345,13 @@ skip:
 }
 
 /**
- * Find a definition corresponding to a declaration DIE.
+ * Find the address of a top-level DIE with a @c DW_AT_specification or @c
+ * DW_AT_abstract_origin attribute that refers to the given DIE address.
  *
- * This finds the address of a DIE with a @c DW_AT_specification attribute that
- * refers to the given address.
+ * This can be used to find the definition of a declaration or the concrete
+ * out-of-line instance of an abstract instance root.
  *
- * @param[in] die_addr The address of the declaration DIE.
+ * @param[in] die_addr Address of a DIE.
  * @param[out] ret Returned address of the definition DIE.
  * @return @c true if a definition DIE was found, @c false if not (in which case
  * `*ret` is not modified).
@@ -1649,6 +1662,11 @@ skip:
 								die_addr,
 								&die_addr))
 					goto next;
+			}
+
+			if (insn & INSN_DIE_FLAG_MAYBE_INLINED) {
+				drgn_dwarf_find_definition(dbinfo, die_addr,
+							   &die_addr);
 			}
 
 			if (!index_die(map, base_types, name, tag, die_addr))

tests/test_dwarf.py
@@ -31,7 +31,17 @@ from tests import (
     identical,
 )
 import tests.assembler as assembler
-from tests.dwarf import DW_AT, DW_ATE, DW_END, DW_FORM, DW_LANG, DW_OP, DW_TAG, DW_UT
+from tests.dwarf import (
+    DW_AT,
+    DW_ATE,
+    DW_END,
+    DW_FORM,
+    DW_INL,
+    DW_LANG,
+    DW_OP,
+    DW_TAG,
+    DW_UT,
+)
 from tests.dwarfwriter import (
     DwarfAttrib,
     DwarfDie,
@@ -4455,7 +4465,10 @@ class TestObjects(TestCase):
                     (
                         DwarfDie(
                             DW_TAG.formal_parameter,
-                            (DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),),
+                            (
+                                DwarfAttrib(DW_AT.name, DW_FORM.string, "x"),
+                                DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                            ),
                         ),
                     ),
                 ),
@@ -4467,7 +4480,7 @@ class TestObjects(TestCase):
                 prog,
                 prog.function_type(
                     prog.int_type("int", 4, True),
-                    (TypeParameter(prog.int_type("int", 4, True)),),
+                    (TypeParameter(prog.int_type("int", 4, True), "x"),),
                     False,
                 ),
                 address=0x7FC3EB9B1C30,
@@ -4493,6 +4506,67 @@ class TestObjects(TestCase):
         )
         self.assertIdentical(
             prog.object("abort"), Object(prog, prog.function_type(prog.void_type(), ()))
+        )
+
+    def test_function_concrete_out_of_line_instance(self):
+        prog = dwarf_program(
+            wrap_test_type_dies(
+                *labeled_int_die,
+                DwarfLabel("abstract_instance_root"),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (
+                        DwarfAttrib(DW_AT.name, DW_FORM.string, "abs"),
+                        DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                        DwarfAttrib(DW_AT.inline, DW_FORM.data1, DW_INL.inlined),
+                    ),
+                    (
+                        DwarfLabel("abstract_instance_parameter"),
+                        DwarfDie(
+                            DW_TAG.formal_parameter,
+                            (
+                                DwarfAttrib(DW_AT.name, DW_FORM.string, "x"),
+                                DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                            ),
+                        ),
+                    ),
+                ),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (
+                        DwarfAttrib(
+                            DW_AT.abstract_origin,
+                            DW_FORM.ref4,
+                            "abstract_instance_root",
+                        ),
+                        DwarfAttrib(DW_AT.low_pc, DW_FORM.addr, 0x7FC3EB9B1C30),
+                    ),
+                    (
+                        DwarfDie(
+                            DW_TAG.formal_parameter,
+                            (
+                                DwarfAttrib(
+                                    DW_AT.abstract_origin,
+                                    DW_FORM.ref4,
+                                    "abstract_instance_parameter",
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        )
+        self.assertIdentical(
+            prog["abs"],
+            Object(
+                prog,
+                prog.function_type(
+                    prog.int_type("int", 4, True),
+                    (TypeParameter(prog.int_type("int", 4, True), "x"),),
+                    False,
+                ),
+                address=0x7FC3EB9B1C30,
+            ),
         )
 
     def test_variable(self):

libdrgn/dwarf_info.h
@@ -189,10 +189,9 @@ struct drgn_dwarf_info {
 	 */
 	struct drgn_dwarf_base_type_map base_types;
 	/**
-	 * Map from the address of a (usually non-defining) DIE to the address
-	 * of a DIE with a DW_AT_specification attribute that references it.
-	 * This is used to resolve DIEs with DW_AT_declaration to their
-	 * definition.
+	 * Map from the address of a DIE to the address of a top-level DIE with
+	 * a `DW_AT_specification` or `DW_AT_abstract_origin` attribute that
+	 * refers to it.
 	 */
 	struct drgn_dwarf_specification_map specifications;
 	/** Indexed compilation units. */

commit 72b4ecf001670b1458039993e3df9000f37ef54e

libdrgn: dwarf_info: resolve address of inline member functions
GCC and Clang both represent inline member functions that have an
out-of-line instance with a declaration in the class scope, an abstract
instance root at the top level that refers to the declaration with
DW_AT_specification, and a concrete instance at the top level that
refers to the abstract instance root with DW_AT_abstract_origin. To
index this correctly, we therefore need to resolve two references.

DWARF indexing can resolve both references: one if a DIE has
DW_AT_declaration and another if it has DW_AT_inline. The problem for
inline member functions is that the check for DW_AT_inline checks the
original (declaration) DIE, which doesn't have DW_AT_inline. So, it only
resolves the first reference but not the second.

We could store an extra flag in the specification map to indicate
whether a definition has DW_AT_inline and check that. However, that
would require additional memory.

Instead, this replaces the check for DW_AT_inline with a more general
check for the absence of DW_AT_low_pc/DW_AT_ranges. This triggers the
necessary second lookup for declaration DIEs while still working for the
global inline function case. It will also work for functions in
supplementary debug files in the future.

The second lookup may be unnecessary in some cases, but this had no
measurable performance difference.

Fixes: a8e2e1719760 ("libdrgn: dwarf_info: resolve address of inline functions with out-of-line instances")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/dwarf_info.c
@@ -318,13 +318,8 @@ enum drgn_dwarf_index_abbrev_insn {
 	 * of flags combined with the drgn_dwarf_index_tag.
 	 */
 	INSN_DIE_FLAG_TAG_MASK = 0x1f,
-	/*
-	 * DIE has a DW_AT_inline attribute (which may be DW_INL_not_inlined or
-	 * DW_INL_declared_not_inlined). We use this to decide whether to look
-	 * for a concrete out-of-line instance of an abstract instance root, so
-	 * false positives are okay.
-	 */
-	INSN_DIE_FLAG_MAYBE_INLINED = 0x20,
+	/* DIE is DW_TAG_subprogram with no DW_AT_low_pc or DW_AT_ranges. */
+	INSN_DIE_FLAG_SUBPROGRAM_NO_PC = 0x20,
 	/* DIE is a declaration. */
 	INSN_DIE_FLAG_DECLARATION = 0x40,
 	/* DIE has children. */
@@ -958,6 +953,8 @@ read_abbrev_decl(struct drgn_elf_file_section_buffer *buffer,
 		should_index = false;
 		break;
 	}
+	if (tag == DW_TAG_subprogram)
+		die_flags |= INSN_DIE_FLAG_SUBPROGRAM_NO_PC;
 
 	uint8_t children;
 	if ((err = binary_buffer_next_u8(&buffer->bb, &children)))
@@ -989,8 +986,8 @@ read_abbrev_decl(struct drgn_elf_file_section_buffer *buffer,
 			err = dw_at_specification_to_insn(cu, &buffer->bb, form,
 							  &insn);
 		} else {
-			if (tag == DW_TAG_subprogram && name == DW_AT_inline)
-				die_flags |= INSN_DIE_FLAG_MAYBE_INLINED;
+			if (name == DW_AT_low_pc || name == DW_AT_ranges)
+				die_flags &= ~INSN_DIE_FLAG_SUBPROGRAM_NO_PC;
 			err = dw_form_to_insn(cu, &buffer->bb, form, &insn);
 		}
 		if (err)
@@ -1675,7 +1672,24 @@ skip:
 					goto next;
 			}
 
-			if (insn & INSN_DIE_FLAG_MAYBE_INLINED) {
+			// A subprogram DIE without an address may be the
+			// abstract instance root for an inlined function. Check
+			// for a concrete instance.
+			//
+			// Note that if the original DIE was a declaration, then
+			// this is technically checking whether the declaration
+			// itself has an address, not the definition. Since
+			// declarations don't have an address, this always does
+			// an extra lookup for definitions of declarations.
+			//
+			// The extra lookup is redundant for normal definitions,
+			// but we actually need it in the case that the
+			// definition is an abstract instance root (so we need
+			// to go from declaration -> abstract instance root ->
+			// concrete instance). Avoiding redundant lookups would
+			// require storing an extra flag in the specification
+			// map, which empirically isn't worth it.
+			if (insn & INSN_DIE_FLAG_SUBPROGRAM_NO_PC) {
 				drgn_dwarf_find_definition(dbinfo, die_addr,
 							   &die_addr);
 			}

tests/test_dwarf.py
@@ -4578,7 +4578,7 @@ class TestObjects(TestCase):
 
     def test_function_concrete_out_of_line_instance(self):
         prog = dwarf_program(
-            wrap_test_type_dies(
+            (
                 *labeled_int_die,
                 DwarfLabel("abstract_instance_root"),
                 DwarfDie(
@@ -4636,6 +4636,118 @@ class TestObjects(TestCase):
                 address=0x7FC3EB9B1C30,
             ),
         )
+
+    def test_member_function_specification(self):
+        prog = dwarf_program(
+            (
+                DwarfDie(
+                    DW_TAG.class_type,
+                    (
+                        DwarfAttrib(DW_AT.name, DW_FORM.string, "Foo"),
+                        DwarfAttrib(DW_AT.byte_size, DW_FORM.data1, 0),
+                    ),
+                    (
+                        DwarfLabel("declaration"),
+                        DwarfDie(
+                            DW_TAG.subprogram,
+                            (
+                                DwarfAttrib(DW_AT.name, DW_FORM.string, "bar"),
+                                DwarfAttrib(
+                                    DW_AT.declaration, DW_FORM.flag_present, True
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                # This is how GCC and Clang do it: the declaration is in the
+                # correct scope, and the definition is at the top level.
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (
+                        DwarfAttrib(DW_AT.specification, DW_FORM.ref4, "declaration"),
+                        DwarfAttrib(DW_AT.low_pc, DW_FORM.addr, 0x7FC3EB9B1C30),
+                    ),
+                ),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (DwarfAttrib(DW_AT.name, DW_FORM.string, "main"),),
+                ),
+            ),
+            lang=DW_LANG.C_plus_plus,
+        )
+        self.assertIdentical(
+            prog["Foo::bar"],
+            Object(
+                prog,
+                prog.function_type(
+                    prog.void_type(),
+                    (),
+                ),
+                address=0x7FC3EB9B1C30,
+            ),
+        )
+        self.assertNotIn("bar", prog)
+
+    def test_member_function_concrete_out_of_line_instance(self):
+        prog = dwarf_program(
+            (
+                DwarfDie(
+                    DW_TAG.class_type,
+                    (
+                        DwarfAttrib(DW_AT.name, DW_FORM.string, "Foo"),
+                        DwarfAttrib(DW_AT.byte_size, DW_FORM.data1, 0),
+                    ),
+                    (
+                        DwarfLabel("declaration"),
+                        DwarfDie(
+                            DW_TAG.subprogram,
+                            (
+                                DwarfAttrib(DW_AT.name, DW_FORM.string, "bar"),
+                                DwarfAttrib(
+                                    DW_AT.declaration, DW_FORM.flag_present, True
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                DwarfLabel("abstract_instance_root"),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (
+                        DwarfAttrib(DW_AT.specification, DW_FORM.ref4, "declaration"),
+                        DwarfAttrib(
+                            DW_AT.inline, DW_FORM.data1, DW_INL.declared_inlined
+                        ),
+                    ),
+                ),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (
+                        DwarfAttrib(
+                            DW_AT.specification, DW_FORM.ref4, "abstract_instance_root"
+                        ),
+                        DwarfAttrib(DW_AT.low_pc, DW_FORM.addr, 0x7FC3EB9B1C30),
+                    ),
+                ),
+                DwarfDie(
+                    DW_TAG.subprogram,
+                    (DwarfAttrib(DW_AT.name, DW_FORM.string, "main"),),
+                ),
+            ),
+            lang=DW_LANG.C_plus_plus,
+        )
+        self.assertIdentical(
+            prog["Foo::bar"],
+            Object(
+                prog,
+                prog.function_type(
+                    prog.void_type(),
+                    (),
+                ),
+                address=0x7FC3EB9B1C30,
+            ),
+        )
+        self.assertNotIn("bar", prog)
 
     def test_variable(self):
         prog = dwarf_program(