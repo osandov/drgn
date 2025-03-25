osandov/drgn

commit 150ee760dcd302ea6212fab8d57f6d9da49133f8

cli: add -e option to exec() code directly
The Python interpreter has "-c" which allows directly running code
provided as a command line argument. This is useful for quick tests,
without needing to write a script and execute it, and without needing to
run the interactive interpreter. It can also be used in scripts or
one-liners.

Unfortunately, "-c" is already used in the drgn CLI. However, this
functionality would be quite useful. Let's add it to the CLI using the
option "-e", which is short for "execute" or "exec", the underlying
Python function.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn/cli.py
@@ -492,6 +492,12 @@ def _main() -> None:
         help="don't print any logs or download progress",
     )
     parser.add_argument(
+        "-e",
+        dest="exec",
+        metavar="CODE",
+        help="an expression or statement to evaluate, instead of running in interactive mode",
+    )
+    parser.add_argument(
         "script",
         metavar="ARG",
         type=str,
@@ -502,7 +508,7 @@ def _main() -> None:
 
     args = parser.parse_args()
 
-    if args.script:
+    if args.script and not args.exec:
         # A common mistake users make is running drgn $core_dump, which tries
         # to run $core_dump as a Python script. Rather than failing later with
         # some inscrutable syntax or encoding error, try to catch this early
@@ -518,7 +524,7 @@ def _main() -> None:
             )
         elif script_type == "elf":
             sys.exit(f"error: {args.script[0]} is a binary, not a drgn script")
-    else:
+    elif not args.exec:
         print(version, file=sys.stderr, flush=True)
 
     if args.log_level == "none":
@@ -564,7 +570,11 @@ def _main() -> None:
 
     _load_debugging_symbols(prog, args, color)
 
-    if args.script:
+    if args.exec:
+        sys.path.insert(0, "")
+        sys.argv = ["-e"] + args.script
+        exec(args.exec, default_globals(prog))
+    elif args.script:
         sys.argv = args.script
         script = args.script[0]
         if pkgutil.get_importer(script) is None:

commit 1527cca92ca12e24629927a855af2fef69b20a68

cli: set default program for -e
Do this for consistency with the other modes.

Fixes: 150ee760dcd3 ("cli: add -e option to exec() code directly")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/cli.py
@@ -573,6 +573,7 @@ def _main() -> None:
     if args.exec:
         sys.path.insert(0, "")
         sys.argv = ["-e"] + args.script
+        drgn.set_default_prog(prog)
         exec(args.exec, default_globals(prog))
     elif args.script:
         sys.argv = args.script