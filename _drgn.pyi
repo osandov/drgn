osandov/drgn

commit 64dd5c883d4196e6f3287ae47c868db3dd477c65

Add Program.create_loaded_modules()
This is a shortcut for `for _ in prog.loaded_modules: pass` or
`list(prog.loaded_modules())`, which is used in several test cases and
is occasionally useful elsewhere.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -702,8 +702,10 @@ class Program:
 
     def loaded_modules(self) -> Iterator[Tuple[Module, bool]]:
         """
-        Determine what executables, libraries, etc. are loaded in the program
-        and create modules to represent them.
+        Get an iterator over executables, libraries, etc. that are loaded in
+        the program, creating modules to represent them.
+
+        Modules are created lazily as items are consumed.
 
         This may automatically load some debugging information necessary to
         enumerate the modules. Other than that, it does not load debugging
@@ -716,6 +718,20 @@ class Program:
             or ``False`` if it was previously found.
         """
         ...
+
+    def create_loaded_modules(self) -> None:
+        """
+        Determine what executables, libraries, etc. are loaded in the program
+        and create modules to represent them.
+
+        This is a shortcut for exhausting a :meth:`loaded_modules()` iterator.
+        It is equivalent to:
+
+        .. code-block:: python3
+
+        for _ in prog.loaded_modules():
+            pass
+        """
 
     @overload
     def main_module(

libdrgn/debug_info.c
@@ -4832,6 +4832,24 @@ drgn_loaded_module_iterator_create(struct drgn_program *prog,
 		return null_module_iterator_create(prog, ret);
 }
 
+static inline void drgn_module_iterator_destroyp(struct drgn_module_iterator **itp)
+{
+	drgn_module_iterator_destroy(*itp);
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_create_loaded_modules(struct drgn_program *prog)
+{
+	_cleanup_(drgn_module_iterator_destroyp)
+		struct drgn_module_iterator *it = NULL;
+	struct drgn_error *err = drgn_loaded_module_iterator_create(prog, &it);
+	if (err)
+		return err;
+	struct drgn_module *module;
+	while (!(err = drgn_module_iterator_next(it, &module, NULL)) && module);
+	return err;
+}
+
 struct load_debug_info_file {
 	const char *path;
 	// We only keep this to keep load_debug_info_provided::build_id alive
@@ -5183,11 +5201,6 @@ static void load_debug_info_log_missing(struct drgn_module *module,
 	drgn_log_warning(module->prog, "missing %s%s%s for %s", missing_loaded,
 			 missing_loaded[0] && missing_debug[0] ? " and ": "",
 			 missing_debug, module->name);
-}
-
-static inline void drgn_module_iterator_destroyp(struct drgn_module_iterator **itp)
-{
-	drgn_module_iterator_destroy(*itp);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *

libdrgn/drgn.h
@@ -1860,7 +1860,7 @@ drgn_module_iterator_program(const struct drgn_module_iterator *it);
 /**
  * Get the next module in a module iterator.
  *
- * @param[out] ret Returned module.
+ * @param[out] ret Returned module, or @c NULL if there are no more modules.
  * @param[out] new_ret Whether the module was newly created. May be @c NULL.
  */
 struct drgn_error *drgn_module_iterator_next(struct drgn_module_iterator *it,
@@ -1879,6 +1879,17 @@ drgn_created_module_iterator_create(struct drgn_program *prog,
 struct drgn_error *
 drgn_loaded_module_iterator_create(struct drgn_program *prog,
 				   struct drgn_module_iterator **ret);
+
+/**
+ * Determine what executables, libraries, etc. are loaded in the program and
+ * create modules to represent them.
+ *
+ * This is a shortcut for creating an iterator with @ref
+ * drgn_loaded_module_iterator_create() and calling @ref
+ * drgn_module_iterator_next() until it is exhausted.
+ */
+struct drgn_error *
+drgn_create_loaded_modules(struct drgn_program *prog);
 
 /**
  * Load debugging information for the given set of files and/or modules.

libdrgn/python/program.c
@@ -1046,6 +1046,14 @@ static ModuleIterator *Program_loaded_modules(Program *self)
 	return it;
 }
 
+static PyObject *Program_create_loaded_modules(Program *self)
+{
+	struct drgn_error *err = drgn_create_loaded_modules(&self->prog);
+	if (err)
+		return set_drgn_error(err);
+	Py_RETURN_NONE;
+}
+
 static PyObject *Program_main_module(Program *self, PyObject *args,
 				     PyObject *kwds)
 {
@@ -1985,6 +1993,8 @@ static PyMethodDef Program_methods[] = {
 	 drgn_Program_modules_DOC},
 	{"loaded_modules", (PyCFunction)Program_loaded_modules, METH_NOARGS,
 	 drgn_Program_loaded_modules_DOC},
+	{"create_loaded_modules", (PyCFunction)Program_create_loaded_modules,
+	 METH_NOARGS, drgn_Program_create_loaded_modules_DOC},
 	{"main_module", (PyCFunction)Program_main_module,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_main_module_DOC},
 	{"shared_library_module", (PyCFunction)Program_shared_library_module,

tests/linux_kernel/test_stack_trace.py
@@ -100,7 +100,7 @@ class TestStackTrace(LinuxKernelStackTraceTestCase):
             prog.load_debug_info(main=True)
             # Now that vmlinux is loaded, enumerate all the kernel modules so
             # that a drgn_module is created to hold the ORC data
-            list(prog.loaded_modules())
+            prog.create_loaded_modules()
             kallsyms = load_module_kallsyms(prog)
             prog.register_symbol_finder("module_kallsyms", kallsyms, enable_index=1)
             for thread in prog.threads():

tests/test_debug_info.py
@@ -636,9 +636,7 @@ class TestLinuxUserspaceCoreDump(TestCase):
 
     def test_bias(self):
         self.prog.set_core_dump(get_resource("crashme.core"))
-
-        for _ in self.prog.loaded_modules():
-            pass
+        self.prog.create_loaded_modules()
 
         with self.subTest(module="main"):
             module = self.prog.main_module()
@@ -724,9 +722,7 @@ class TestLinuxUserspaceCoreDump(TestCase):
 
     def test_bias_pie(self):
         self.prog.set_core_dump(get_resource("crashme_pie.core"))
-
-        for _ in self.prog.loaded_modules():
-            pass
+        self.prog.create_loaded_modules()
 
         with self.subTest(module="main"):
             module = self.prog.main_module()
@@ -784,9 +780,7 @@ class TestLinuxUserspaceCoreDump(TestCase):
 
     def test_bias_static(self):
         self.prog.set_core_dump(get_resource("crashme_static.core"))
-
-        for _ in self.prog.loaded_modules():
-            pass
+        self.prog.create_loaded_modules()
 
         with self.subTest(module="main"):
             module = self.prog.main_module()
@@ -836,9 +830,7 @@ class TestLinuxUserspaceCoreDump(TestCase):
 
     def test_bias_static_pie(self):
         self.prog.set_core_dump(get_resource("crashme_static_pie.core"))
-
-        for _ in self.prog.loaded_modules():
-            pass
+        self.prog.create_loaded_modules()
 
         with self.subTest(module="main"):
             module = self.prog.main_module()

commit b72fc704b9dbee1a1f5e31fad6c62d1191da9b20

docs: fix formatting of create_loaded_modules() docstring
Fixes: 64dd5c883d41 ("Add Program.create_loaded_modules()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -729,8 +729,8 @@ class Program:
 
         .. code-block:: python3
 
-        for _ in prog.loaded_modules():
-            pass
+            for _ in prog.loaded_modules():
+                pass
         """
 
     @overload