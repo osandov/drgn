osandov/drgn

commit 3d493604e62daf3c078246401c656762223c63aa

libdrgn: combine libdrgn and _drgn Python extension into one .so
For the upcoming plugin system, libdrgn needs to call Python extension
code and vice versa. Doing this from two separate libraries would be
messy, so let's combine them into one library. liblldb seems to do the
same thing, so I don't think this is too crazy.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/Makefile.am
@@ -5,7 +5,8 @@ ACLOCAL_AMFLAGS = -I m4
 
 .DELETE_ON_ERROR:
 
-AM_CPPFLAGS = -I $(top_srcdir)/include -D_GNU_SOURCE
+AM_CPPFLAGS = -I $(top_srcdir)/include -iquote $(dir $@) \
+	      -iquote $(srcdir)/$(dir $@) -D_GNU_SOURCE
 AM_CFLAGS = $(WARN_CFLAGS) $(SANITIZER_CFLAGS)
 AM_LDFLAGS= $(SANITIZER_LDFLAGS)
 
@@ -134,7 +135,7 @@ libdrgnimpl_la_SOURCES = $(ARCH_DEFS_PYS:_defs.py=.c) \
 
 libdrgnimpl_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden $(OPENMP_CFLAGS) \
 			$(elfutils_CFLAGS)
-libdrgnimpl_la_CPPFLAGS = $(AM_CPPFLAGS) -iquote .
+libdrgnimpl_la_CPPFLAGS = $(AM_CPPFLAGS)
 libdrgnimpl_la_LIBADD = $(OPENMP_LIBS) $(elfutils_LIBS) -lm
 
 if WITH_DEBUGINFOD
@@ -152,6 +153,36 @@ libdrgnimpl_la_CFLAGS += $(libkdumpfile_CFLAGS)
 libdrgnimpl_la_LIBADD += $(libkdumpfile_LIBS)
 endif
 
+if ENABLE_PYTHON
+BUILT_SOURCES += python/docstrings.h
+
+libdrgnimpl_la_SOURCES += python/constants.c \
+			  python/debug_info_options.c \
+			  python/docstrings.c \
+			  python/docstrings.h \
+			  python/drgnpy.h \
+			  python/error.c \
+			  python/helpers.c \
+			  python/language.c \
+			  python/main.c \
+			  python/module.c \
+			  python/module_section_addresses.c \
+			  python/object.c \
+			  python/platform.c \
+			  python/program.c \
+			  python/stack_trace.c \
+			  python/symbol.c \
+			  python/symbol_index.c \
+			  python/test.c \
+			  python/thread.c \
+			  python/type.c \
+			  python/type_kind_set.c \
+			  python/util.c
+
+libdrgnimpl_la_CPPFLAGS += $(PYTHON_CPPFLAGS)
+libdrgnimpl_la_LIBADD += $(PYTHON_LIBS)
+endif
+
 %: %.strswitch build-aux/gen_strswitch.py build-aux/codegen_utils.py
 	$(AM_V_GEN)$(PYTHON) $(word 2, $^) -o $@ $<
 
@@ -167,52 +198,6 @@ drgn_section_name_to_index.inc: build-aux/gen_elf_sections.py build-aux/gen_strs
 elf_sections.h: build-aux/gen_elf_sections.py build-aux/codegen_utils.py
 	$(AM_V_GEN)$(PYTHON) $< -H > $@
 
-lib_LTLIBRARIES = libdrgn.la
-
-libdrgn_la_SOURCES =
-libdrgn_la_LDFLAGS = $(AM_LDFLAGS) -version-info 0:0:0
-libdrgn_la_LIBADD = libdrgnimpl.la
-
-if ENABLE_PYTHON
-BUILT_SOURCES += python/docstrings.h
-
-noinst_LTLIBRARIES += _drgn.la
-endif
-
-_drgn_la_SOURCES = python/constants.c \
-		   python/debug_info_options.c \
-		   python/docstrings.c \
-		   python/docstrings.h \
-		   python/drgnpy.h \
-		   python/error.c \
-		   python/helpers.c \
-		   python/language.c \
-		   python/main.c \
-		   python/module.c \
-		   python/module_section_addresses.c \
-		   python/object.c \
-		   python/platform.c \
-		   python/program.c \
-		   python/stack_trace.c \
-		   python/symbol.c \
-		   python/symbol_index.c \
-		   python/test.c \
-		   python/thread.c \
-		   python/type.c \
-		   python/type_kind_set.c \
-		   python/util.c
-
-_drgn_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden
-_drgn_la_CPPFLAGS = $(AM_CPPFLAGS) $(PYTHON_CPPFLAGS) -iquote $(srcdir)/python \
-		    -iquote python
-_drgn_la_LDFLAGS = $(AM_LDFLAGS) -Wl,--exclude-libs,ALL -avoid-version -module \
-		   -shared -rpath $(pkgpyexecdir)
-_drgn_la_LIBADD = libdrgnimpl.la
-
-if WITH_LIBKDUMPFILE
-_drgn_la_CFLAGS += $(libkdumpfile_CFLAGS)
-endif
-
 python/constants.c: drgn.h build-aux/gen_constants.py
 	$(AM_V_GEN)$(PYTHON) $(word 2, $^) < $< > $@
 
@@ -224,6 +209,12 @@ python/docstrings.c: ../_drgn.pyi $(drgndoc_docstrings_deps)
 
 python/docstrings.h: ../_drgn.pyi $(drgndoc_docstrings_deps)
 	$(AM_V_GEN)$(drgndoc_docstrings) -H -m _drgn:drgn $< > $@
+
+lib_LTLIBRARIES = libdrgn.la
+
+libdrgn_la_SOURCES =
+libdrgn_la_LDFLAGS = $(AM_LDFLAGS) -version-info 0:0:0
+libdrgn_la_LIBADD = libdrgnimpl.la
 
 EXTRA_DIST = $(ARCH_DEFS_PYS) \
 	     $(STRSWITCH_INCS:.inc=.inc.strswitch) \

libdrgn/configure.ac
@@ -52,30 +52,7 @@ AC_ARG_ENABLE([python],
 	      [], [enable_python=no])
 
 AM_CONDITIONAL([ENABLE_PYTHON], [test "x$enable_python" != xno])
-AM_COND_IF([ENABLE_PYTHON],
-	   [AS_IF([test -z "$PYTHON_CPPFLAGS"],
-		  [prog="import sysconfig
-include = sysconfig.get_path('include')
-platinclude = sysconfig.get_path('platinclude')
-include_paths = [[include]]
-if platinclude != include:
-    include_paths.append(plat_include)
-print(' '.join('-I' + path for path in include_paths))"
-		   PYTHON_CPPFLAGS=`"$PYTHON" -c "$prog"`])
-	    AC_SUBST(PYTHON_CPPFLAGS)
-	    AC_MSG_CHECKING([for $PYTHON development headers])
-	    save_CPPFLAGS="$CPPFLAGS"
-	    CPPFLAGS="$CPPFLAGS $PYTHON_CPPFLAGS"
-	    AC_COMPILE_IFELSE([AC_LANG_SOURCE([[#include <Python.h>]])],
-			      [AC_MSG_RESULT([yes])],
-			      [AC_MSG_RESULT([no])
-			       AC_MSG_ERROR(
-[Could not compile test program with Python headers.
-
-You may need to install your distribution's Python development package (e.g.,
-python3-devel or python3-dev) or specify the location of the Python development
-headers by setting the PYTHON_CPPFLAGS environment variable.])])
-	    CPPFLAGS="$save_CPPFLAGS"])
+AM_COND_IF([ENABLE_PYTHON], [MY_PYTHON_DEVEL])
 
 PKG_PROG_PKG_CONFIG
 

libdrgn/m4/my_python_devel.m4
@@ -1,0 +1,47 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+# Check for Python development files and define PYTHON_CPPFLAGS and PYTHON_LIBS
+# accordingly.
+AC_DEFUN([MY_PYTHON_DEVEL],
+[
+AS_IF([test -z "$PYTHON_CPPFLAGS"],
+      [prog="import sysconfig
+include = sysconfig.get_path('include')
+platinclude = sysconfig.get_path('platinclude')
+include_paths = [[include]]
+if platinclude != include:
+    include_paths.append(plat_include)
+print(' '.join('-I' + path for path in include_paths))"
+       PYTHON_CPPFLAGS=`"$PYTHON" -c "$prog"`])
+AC_SUBST(PYTHON_CPPFLAGS)
+AS_IF([test -z "$PYTHON_LIBS"],
+      [prog="import sysconfig
+print('-L' + sysconfig.get_config_var('LIBDIR') +
+      ' -lpython' + sysconfig.get_config_var('LDVERSION'))"
+       PYTHON_LIBS=`"$PYTHON" -c "$prog"`])
+AC_SUBST(PYTHON_LIBS)
+AC_MSG_CHECKING([for $PYTHON development files])
+save_CPPFLAGS="$CPPFLAGS"
+save_LIBS="$LIBS"
+CPPFLAGS="$CPPFLAGS $PYTHON_CPPFLAGS"
+LIBS="$LIBS $PYTHON_LIBS"
+AC_LINK_IFELSE([AC_LANG_SOURCE([[
+#include <Python.h>
+
+int main(void)
+{
+	Py_Initialize();
+}
+]])],
+	       [AC_MSG_RESULT([yes])],
+	       [AC_MSG_RESULT([no])
+		AC_MSG_ERROR(
+[Could not compile Python development test program.
+
+You may need to install your distribution's Python development package (e.g.,
+python3-devel or python3-dev) or set the PYTHON_CPPFLAGS and PYTHON_LIBS
+environment variables.])])
+CPPFLAGS="$save_CPPFLAGS"
+LIBS="$save_LIBS"
+])

setup.py
@@ -123,7 +123,7 @@ class build_ext(_build_ext):
 
     def run(self):
         self.make()
-        so = os.path.join(self.build_temp, ".libs/_drgn.so")
+        so = os.path.join(self.build_temp, ".libs/libdrgn.so")
         if self.inplace:
             self.copy_file(so, self.get_ext_fullpath("_drgn"))
         old_inplace, self.inplace = self.inplace, 0

libdrgn/m4/.gitignore
@@ -7,3 +7,4 @@
 !/my_c_auto.m4
 !/my_c_switch_enum.m4
 !/my_check_va_args_comma_deletion.m4
+!/my_python_devel.m4

commit 4c90e748c03dd4f2d286a6cab8bd005daad64f32

libdrgn: optionally split up libdrgn and _drgn Python extension .so files again
Since the fixes commit, the _drgn Python extension module links directly
to libpython. However, manylinux extensions are not supposed to link
against libpython [1], and the manylinux container images intentionally
omit libpython, so building drgn for manylinux fails.

Add a few modes of building libdrgn to address this:

* --disable-libdrgn --enable-python-extension: build _drgn.so (the
  Python extension module), which doesn't link against libpython, and
  don't build libdrgn.so. This is what setup.py uses since Python-only
  installations don't need libdrgn. It works in manylinux.
* --enable-python: build libdrgn.so with Python support, which links
  against libpython, and don't build _drgn.so. In this configuration,
  libdrgn.so also works as the Python extension module, so _drgn.so can
  be installed as a symlink to libdrgn.so.
* (No options, equivalent to --enable-libdrgn --disable-python
  --disable-python-extension): build libdrgn.so with no Python support,
  don't build _drgn.so. For backwards-compatibility, this is the default
  when building libdrgn directly, but I doubt anyone does that these
  days.
* --enable-libdrgn --enable-python-extension: build libdrgn.so with
  Python support, which links against libpython, and build _drgn.so,
  which doesn't link against libpython. I guess this works but I don't
  see a reason to prefer it over --enable-python + a symlink.

This also requires refactoring our libtool convenience libraries.
Hopefully this sucks less in Meson.

[1]: https://peps.python.org/pep-0513/#libpythonx-y-so-1

Fixes: 3d493604e62d ("libdrgn: combine libdrgn and _drgn Python extension into one .so")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/Makefile.am
@@ -33,157 +33,167 @@ BUILT_SOURCES = $(ARCH_DEFS_INCS) \
 		drgn_section_name_to_index.inc \
 		elf_sections.h
 
-noinst_LTLIBRARIES = libdrgnimpl.la
+if ENABLE_PYTHON
+BUILT_SOURCES += python/docstrings.h
+endif
 
-libdrgnimpl_la_SOURCES = $(ARCH_DEFS_PYS:_defs.py=.c) \
-			 $(ARCH_DEFS_INCS) \
-			 $(STRSWITCH_INCS) \
-			 accessors.c \
-			 arch_i386.c \
-			 arch_riscv.c \
-			 array.h \
-			 binary_buffer.c \
-			 binary_buffer.h \
-			 binary_search.h \
-			 binary_search_tree.h \
-			 bitops.h \
-			 c_keywords.inc \
-			 c_lexer.h \
-			 cfi.c \
-			 cfi.h \
-			 cityhash.h \
-			 cleanup.h \
-			 crc32.c \
-			 crc32.h \
-			 debug_info.c \
-			 debug_info.h \
-			 debug_info_options.c \
-			 debug_info_options.h \
-			 drgn_internal.h \
-			 drgn_section_name_to_index.inc \
-			 dwarf_constants.c \
-			 dwarf_constants.h \
-			 dwarf_info.c \
-			 dwarf_info.h \
-			 elf_file.c \
-			 elf_file.h \
-			 elf_notes.c \
-			 elf_notes.h \
-			 elf_symtab.c \
-			 elf_symtab.h \
-			 elf_sections.h \
-			 error.c \
-			 error.h \
-			 generics.h \
-			 handler.c \
-			 handler.h \
-			 hash_table.c \
-			 hash_table.h \
-			 helpers.h \
-			 hexlify.c \
-			 hexlify.h \
-			 io.c \
-			 io.h \
-			 kallsyms.c \
-			 kallsyms.h \
-			 language.c \
-			 language.h \
-			 language_c.c \
-			 lazy_object.c \
-			 lazy_object.h \
-			 lexer.c \
-			 lexer.h \
-			 linux_kernel.c \
-			 linux_kernel.h \
-			 linux_kernel_helpers.c \
-			 log.c \
-			 log.h \
-			 memory_reader.c \
-			 memory_reader.h \
-			 minmax.h \
-			 nstring.h \
-			 object.c \
-			 object.h \
-			 openmp.c \
-			 openmp.h \
-			 orc.h \
-			 orc_info.c \
-			 orc_info.h \
-			 path.c \
-			 path.h \
-			 platform.c \
-			 platform.h \
-			 plugins.h \
-			 pp.h \
-			 program.c \
-			 program.h \
-			 register_state.c \
-			 register_state.h \
-			 serialize.c \
-			 serialize.h \
-			 splay_tree.c \
-			 stack_trace.c \
-			 stack_trace.h \
-			 string_builder.c \
-			 string_builder.h \
-			 symbol.c \
-			 symbol.h \
-			 type.c \
-			 type.h \
-			 util.c \
-			 util.h \
-			 vector.h
+noinst_LTLIBRARIES = libdrgn_common.la
 
-libdrgnimpl_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden $(OPENMP_CFLAGS) \
-			$(elfutils_CFLAGS)
-libdrgnimpl_la_CPPFLAGS = $(AM_CPPFLAGS)
-libdrgnimpl_la_LIBADD = $(OPENMP_LIBS) $(elfutils_LIBS) -lm
+libdrgn_common_la_SOURCES = $(ARCH_DEFS_PYS:_defs.py=.c) \
+			    $(ARCH_DEFS_INCS) \
+			    $(STRSWITCH_INCS) \
+			    accessors.c \
+			    arch_i386.c \
+			    arch_riscv.c \
+			    array.h \
+			    binary_buffer.c \
+			    binary_buffer.h \
+			    binary_search.h \
+			    binary_search_tree.h \
+			    bitops.h \
+			    c_keywords.inc \
+			    c_lexer.h \
+			    cfi.c \
+			    cfi.h \
+			    cityhash.h \
+			    cleanup.h \
+			    crc32.c \
+			    crc32.h \
+			    debug_info.c \
+			    debug_info.h \
+			    debug_info_options.c \
+			    debug_info_options.h \
+			    drgn_internal.h \
+			    drgn_section_name_to_index.inc \
+			    dwarf_constants.c \
+			    dwarf_constants.h \
+			    dwarf_info.c \
+			    dwarf_info.h \
+			    elf_file.c \
+			    elf_file.h \
+			    elf_notes.c \
+			    elf_notes.h \
+			    elf_symtab.c \
+			    elf_symtab.h \
+			    elf_sections.h \
+			    error.c \
+			    error.h \
+			    generics.h \
+			    handler.c \
+			    handler.h \
+			    hash_table.c \
+			    hash_table.h \
+			    helpers.h \
+			    hexlify.c \
+			    hexlify.h \
+			    io.c \
+			    io.h \
+			    kallsyms.c \
+			    kallsyms.h \
+			    language.c \
+			    language.h \
+			    language_c.c \
+			    lazy_object.c \
+			    lazy_object.h \
+			    lexer.c \
+			    lexer.h \
+			    linux_kernel.c \
+			    linux_kernel.h \
+			    linux_kernel_helpers.c \
+			    log.c \
+			    log.h \
+			    memory_reader.c \
+			    memory_reader.h \
+			    minmax.h \
+			    nstring.h \
+			    object.c \
+			    object.h \
+			    openmp.c \
+			    openmp.h \
+			    orc.h \
+			    orc_info.c \
+			    orc_info.h \
+			    path.c \
+			    path.h \
+			    platform.c \
+			    platform.h \
+			    plugins.h \
+			    pp.h \
+			    program.c \
+			    program.h \
+			    register_state.c \
+			    register_state.h \
+			    serialize.c \
+			    serialize.h \
+			    splay_tree.c \
+			    stack_trace.c \
+			    stack_trace.h \
+			    string_builder.c \
+			    string_builder.h \
+			    symbol.c \
+			    symbol.h \
+			    type.c \
+			    type.h \
+			    util.c \
+			    util.h \
+			    vector.h
+
+libdrgn_common_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden $(OPENMP_CFLAGS) \
+			   $(elfutils_CFLAGS)
+libdrgn_common_la_LIBADD = $(OPENMP_LIBS) $(elfutils_LIBS) -lm
 
 if WITH_DEBUGINFOD
 if ENABLE_DLOPEN_DEBUGINFOD
-libdrgnimpl_la_LIBADD += -ldl
+libdrgn_common_la_LIBADD += -ldl
 else
-libdrgnimpl_la_CFLAGS += $(libdebuginfod_CFLAGS)
-libdrgnimpl_la_LIBADD += $(libdebuginfod_LIBS)
+libdrgn_common_la_CFLAGS += $(libdebuginfod_CFLAGS)
+libdrgn_common_la_LIBADD += $(libdebuginfod_LIBS)
 endif
 endif
 
 if WITH_LIBKDUMPFILE
-libdrgnimpl_la_SOURCES += kdump.c
-libdrgnimpl_la_CFLAGS += $(libkdumpfile_CFLAGS)
-libdrgnimpl_la_LIBADD += $(libkdumpfile_LIBS)
+libdrgn_common_la_SOURCES += kdump.c
+libdrgn_common_la_CFLAGS += $(libkdumpfile_CFLAGS)
+libdrgn_common_la_LIBADD += $(libkdumpfile_LIBS)
 endif
 
 if ENABLE_PYTHON
-BUILT_SOURCES += python/docstrings.h
+noinst_LTLIBRARIES += libdrgn_common_python.la
 
-libdrgnimpl_la_SOURCES += python/constants.c \
-			  python/debug_info_options.c \
-			  python/docstrings.c \
-			  python/docstrings.h \
-			  python/drgnpy.h \
-			  python/error.c \
-			  python/helpers.c \
-			  python/language.c \
-			  python/main.c \
-			  python/module.c \
-			  python/module_section_addresses.c \
-			  python/object.c \
-			  python/platform.c \
-			  python/plugins.c \
-			  python/program.c \
-			  python/stack_trace.c \
-			  python/symbol.c \
-			  python/symbol_index.c \
-			  python/test.c \
-			  python/thread.c \
-			  python/type.c \
-			  python/type_kind_set.c \
-			  python/util.c
+libdrgn_common_python_la_SOURCES = python/constants.c \
+				   python/debug_info_options.c \
+				   python/docstrings.c \
+				   python/docstrings.h \
+				   python/drgnpy.h \
+				   python/error.c \
+				   python/helpers.c \
+				   python/language.c \
+				   python/main.c \
+				   python/module.c \
+				   python/module_section_addresses.c \
+				   python/object.c \
+				   python/platform.c \
+				   python/plugins.c \
+				   python/program.c \
+				   python/stack_trace.c \
+				   python/symbol.c \
+				   python/symbol_index.c \
+				   python/test.c \
+				   python/thread.c \
+				   python/type.c \
+				   python/type_kind_set.c \
+				   python/util.c
 
-libdrgnimpl_la_CPPFLAGS += $(PYTHON_CPPFLAGS)
-libdrgnimpl_la_LIBADD += $(PYTHON_LIBS)
+libdrgn_common_python_la_CFLAGS = $(libdrgn_common_la_CFLAGS)
+libdrgn_common_python_la_CPPFLAGS = $(AM_CPPFLAGS) $(PYTHON_CPPFLAGS)
+libdrgn_common_python_la_LIBADD = libdrgn_common.la
 endif
+
+EXTRA_LTLIBRARIES = libdrgn_common_no_python.la
+
+libdrgn_common_no_python_la_SOURCES = no_python.c
+libdrgn_common_no_python_la_CFLAGS = $(libdrgn_common_la_CFLAGS)
+libdrgn_common_no_python_la_LIBADD = libdrgn_common.la
 
 %: %.strswitch build-aux/gen_strswitch.py build-aux/codegen_utils.py
 	$(AM_V_GEN)$(PYTHON) $(word 2, $^) -o $@ $<
@@ -212,11 +222,31 @@ python/docstrings.c: ../_drgn.pyi $(drgndoc_docstrings_deps)
 python/docstrings.h: ../_drgn.pyi $(drgndoc_docstrings_deps)
 	$(AM_V_GEN)$(drgndoc_docstrings) -H -m _drgn:drgn $< > $@
 
+if ENABLE_LIBDRGN
+if ENABLE_PYTHON
+libdrgn_libs = $(PYTHON_LIBS) libdrgn_common_python.la
+else
+libdrgn_libs = libdrgn_common_no_python.la
+endif
+else
+libdrgn_libs = libdrgn_common_no_python.la
+endif
+
+if ENABLE_LIBDRGN
 lib_LTLIBRARIES = libdrgn.la
 
 libdrgn_la_SOURCES =
 libdrgn_la_LDFLAGS = $(AM_LDFLAGS) -version-info 0:0:0
-libdrgn_la_LIBADD = libdrgnimpl.la
+libdrgn_la_LIBADD = $(libdrgn_libs)
+endif
+
+if ENABLE_PYTHON_EXTENSION
+noinst_LTLIBRARIES += _drgn.la
+
+_drgn_la_SOURCES =
+_drgn_la_LDFLAGS = $(AM_LDFLAGS) -avoid-version -module -shared -rpath $(pkgpyexecdir)
+_drgn_la_LIBADD = libdrgn_common_python.la
+endif
 
 EXTRA_DIST = $(ARCH_DEFS_PYS) \
 	     $(STRSWITCH_INCS:.inc=.inc.strswitch) \
@@ -232,7 +262,7 @@ EXTRA_DIST = $(ARCH_DEFS_PYS) \
 EXTRA_PROGRAMS = examples/load_debug_info
 
 examples_load_debug_info_SOURCES = examples/load_debug_info.c
-examples_load_debug_info_LDADD = libdrgnimpl.la
+examples_load_debug_info_LDADD = $(libdrgn_libs)
 
 # Only test internals here. Anything exposed via Python should be tested in
 # Python unit tests instead.
@@ -260,7 +290,7 @@ tests/%.c: build-aux/checkmk tests/%.c.in
 
 test_cflags = $(AM_CFLAGS) $(check_CFLAGS)
 test_cppflags = $(AM_CPPFLAGS) -iquote $(srcdir)/tests
-test_ldadd = $(check_LIBS) libdrgnimpl.la
+test_ldadd = $(check_LIBS) $(libdrgn_libs)
 
 tests_binary_search_CFLAGS = $(test_cflags)
 tests_binary_search_CPPFLAGS = $(test_cppflags)

libdrgn/configure.ac
@@ -46,15 +46,35 @@ AC_SUBST(OPENMP_LIBS)
 dnl We need Python for code generation even if we're not building the bindings.
 AM_PATH_PYTHON([3.6])
 
+AC_ARG_ENABLE([libdrgn],
+	      [AS_HELP_STRING([--disable-libdrgn],
+			      [don't build the libdrgn C library. Note that
+			       libdrgn's API and ABI are unstable])],
+	      [], [enable_libdrgn=yes])
+
 AC_ARG_ENABLE([python],
 	      [AS_HELP_STRING([--enable-python],
-			      [build Python bindings @<:@default=no@:>@])],
+			      [enable Python support in libdrgn, which allows
+			       it to use Python plugins and double as the
+			       Python extension module])],
 	      [], [enable_python=no])
 
-AM_CONDITIONAL([ENABLE_PYTHON], [test "x$enable_python" != xno])
+AC_ARG_ENABLE([python-extension],
+	      [AS_HELP_STRING([--enable-python-extension],
+			      [build the drgn Python extension module separate
+			       from libdrgn])],
+	      [], [enable_python_extension=no])
+
+AM_CONDITIONAL([ENABLE_LIBDRGN], [test "x$enable_libdrgn" != xno])
+AM_CONDITIONAL([ENABLE_PYTHON],
+	       [test "x$enable_python" != xno || test "x$enable_python_extension" != xno])
+AM_CONDITIONAL([ENABLE_PYTHON_EXTENSION],
+	       [test "x$enable_python_extension" != xno])
+
 AM_COND_IF([ENABLE_PYTHON],
-	   [MY_PYTHON_DEVEL
-	    AC_DEFINE(ENABLE_PYTHON)])
+	   [AS_IF([test "x$enable_libdrgn" != xno],
+		  [find_libpython=yes], [find_libpython=no])
+	    MY_PYTHON_DEVEL([$find_libpython])])
 
 PKG_PROG_PKG_CONFIG
 

libdrgn/m4/my_python_devel.m4
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+# MY_PYTHON_DEVEL([find-libpython=no])
 # Check for Python development files and define PYTHON_CPPFLAGS and PYTHON_LIBS
 # accordingly.
 AC_DEFUN([MY_PYTHON_DEVEL],
@@ -14,19 +15,22 @@ if platinclude != include:
     include_paths.append(plat_include)
 print(' '.join('-I' + path for path in include_paths))"
        PYTHON_CPPFLAGS=`"$PYTHON" -c "$prog"`])
-AC_SUBST(PYTHON_CPPFLAGS)
-AS_IF([test -z "$PYTHON_LIBS"],
-      [prog="import sysconfig
+
+save_CPPFLAGS="$CPPFLAGS"
+CPPFLAGS="$CPPFLAGS $PYTHON_CPPFLAGS"
+
+AS_IF([test "x$1" = xyes],
+      [AS_IF([test -z "$PYTHON_LIBS"]
+	     [prog="import sysconfig
 print('-L' + sysconfig.get_config_var('LIBDIR') +
       ' -lpython' + sysconfig.get_config_var('LDVERSION'))"
        PYTHON_LIBS=`"$PYTHON" -c "$prog"`])
-AC_SUBST(PYTHON_LIBS)
-AC_MSG_CHECKING([for $PYTHON development files])
-save_CPPFLAGS="$CPPFLAGS"
-save_LIBS="$LIBS"
-CPPFLAGS="$CPPFLAGS $PYTHON_CPPFLAGS"
-LIBS="$LIBS $PYTHON_LIBS"
-AC_LINK_IFELSE([AC_LANG_SOURCE([[
+
+       save_LIBS="$LIBS"
+       LIBS="$LIBS $PYTHON_LIBS"
+
+       AC_MSG_CHECKING([for $PYTHON development headers and library])
+       AC_LINK_IFELSE([AC_LANG_SOURCE([[
 #include <Python.h>
 
 int main(void)
@@ -34,14 +38,30 @@ int main(void)
 	Py_Initialize();
 }
 ]])],
-	       [AC_MSG_RESULT([yes])],
-	       [AC_MSG_RESULT([no])
-		AC_MSG_ERROR(
-[Could not compile Python development test program.
+		      [AC_MSG_RESULT([yes])],
+		      [AC_MSG_RESULT([no])
+		       AC_MSG_ERROR(
+[Could not compile and link test program with Python headers and library.
 
 You may need to install your distribution's Python development package (e.g.,
-python3-devel or python3-dev) or set the PYTHON_CPPFLAGS and PYTHON_LIBS
+python3-devel or python3-dev) or specify the location of the Python development
+headers and/or library by setting the PYTHON_CPPFLAGS and PYTHON_LIBS
 environment variables.])])
+
+       LIBS="$save_LIBS"],
+      [AC_MSG_CHECKING([for $PYTHON development headers])
+       AC_COMPILE_IFELSE([AC_LANG_SOURCE([[#include <Python.h>]])],
+			 [AC_MSG_RESULT([yes])],
+			 [AC_MSG_RESULT([no])
+			  AC_MSG_ERROR(
+[Could not compile test program with Python headers.
+
+You may need to install your distribution's Python development package (e.g.,
+python3-devel or python3-dev) or specify the location of the Python development
+headers by setting the PYTHON_CPPFLAGS environment variable.])])])
+
 CPPFLAGS="$save_CPPFLAGS"
-LIBS="$save_LIBS"
+
+AC_SUBST(PYTHON_CPPFLAGS)
+AC_SUBST(PYTHON_LIBS)
 ])

libdrgn/no_python.c
@@ -1,0 +1,40 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// Fallback implementations for builds without Python support.
+
+#include "plugins.h"
+#include "program.h"
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_create(const struct drgn_platform *platform,
+		    struct drgn_program **ret)
+{
+	struct drgn_program *prog = malloc(sizeof(*prog));
+	if (!prog)
+		return &drgn_enomem;
+	drgn_program_init(prog, platform);
+	*ret = prog;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC void drgn_program_destroy(struct drgn_program *prog)
+{
+	if (prog) {
+		drgn_program_deinit(prog);
+		free(prog);
+	}
+}
+
+void drgn_call_plugins_prog(const char *name, struct drgn_program *prog)
+{
+}
+
+void *drgn_begin_blocking(void)
+{
+	return NULL;
+}
+
+void drgn_end_blocking(void *state)
+{
+}

libdrgn/plugins.h
@@ -4,14 +4,8 @@
 #ifndef DRGN_PLUGINS_H
 #define DRGN_PLUGINS_H
 
-#include <stdbool.h>
-
 struct drgn_program;
 
-#if ENABLE_PYTHON
 void drgn_call_plugins_prog(const char *name, struct drgn_program *prog);
-#else
-static inline void drgn_call_plugins_prog(const char *name, struct drgn_program *prog) {}
-#endif
 
 #endif /* DRGN_PLUGINS_H */

libdrgn/program.c
@@ -178,30 +178,6 @@ void drgn_program_deinit(struct drgn_program *prog)
 	drgn_debug_info_deinit(&prog->dbinfo);
 }
 
-#if !ENABLE_PYTHON
-LIBDRGN_PUBLIC struct drgn_error *
-drgn_program_create(const struct drgn_platform *platform,
-		    struct drgn_program **ret)
-{
-	struct drgn_program *prog;
-
-	prog = malloc(sizeof(*prog));
-	if (!prog)
-		return &drgn_enomem;
-	drgn_program_init(prog, platform);
-	*ret = prog;
-	return NULL;
-}
-
-LIBDRGN_PUBLIC void drgn_program_destroy(struct drgn_program *prog)
-{
-	if (prog) {
-		drgn_program_deinit(prog);
-		free(prog);
-	}
-}
-#endif
-
 LIBDRGN_PUBLIC struct drgn_error *
 drgn_program_add_memory_segment(struct drgn_program *prog, uint64_t address,
 				uint64_t size, drgn_memory_read_fn read_fn,

libdrgn/program.h
@@ -462,7 +462,6 @@ drgn_program_register_symbol_finder_impl(struct drgn_program *prog,
 					 const struct drgn_symbol_finder_ops *ops,
 					 void *arg, size_t enable_index);
 
-#if ENABLE_PYTHON
 /**
  * Call before a blocking (I/O or long-running) operation.
  *
@@ -478,16 +477,6 @@ void *drgn_begin_blocking(void);
  * @param[in] state Return value of @ref drgn_begin_blocking().
  */
 void drgn_end_blocking(void *state);
-#else
-static inline void *drgn_begin_blocking(void)
-{
-	return NULL;
-}
-
-static inline void drgn_end_blocking(void *state)
-{
-}
-#endif
 
 static inline void drgn_blocking_guard_cleanup(void **statep)
 {

setup.py
@@ -92,7 +92,8 @@ class build_ext(_build_ext):
             args = [
                 os.path.relpath("libdrgn/configure", self.build_temp),
                 "--disable-static",
-                "--enable-python",
+                "--disable-libdrgn",
+                "--enable-python-extension",
             ]
             try:
                 args.extend(shlex.split(os.environ["CONFIGURE_FLAGS"]))
@@ -123,7 +124,7 @@ class build_ext(_build_ext):
 
     def run(self):
         self.make()
-        so = os.path.join(self.build_temp, ".libs/libdrgn.so")
+        so = os.path.join(self.build_temp, ".libs/_drgn.so")
         if self.inplace:
             self.copy_file(so, self.get_ext_fullpath("_drgn"))
         old_inplace, self.inplace = self.inplace, 0