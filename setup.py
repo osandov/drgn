8449bd7044b80cf16326eddcff4d008f550276ec
Omar Sandoval <osandov@osandov.com>
2025-04-16 11:25:03 -0700

    setup.py: depend on setuptools (for pkg_resources) on Python < 3.8
    Fixes: adf64729095b ("Add plugin system")
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- setup.py
+++ setup.py
@@ -467,6 +467,8 @@ setup(
     },
     entry_points={"console_scripts": ["drgn=drgn.cli:_main"]},
     python_requires=">=3.6",
+    # We use pkg_resources on Python < 3.8.
+    install_requires=['setuptools;python_version<"3.8"'],
     author="Omar Sandoval",
     author_email="osandov@osandov.com",
     description="Programmable debugger",
