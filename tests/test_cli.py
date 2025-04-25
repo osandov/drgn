osandov/drgn

commit 150ee760dcd302ea6212fab8d57f6d9da49133f8

cli: add -e option to exec() code directly
The Python interpreter has "-c" which allows directly running code
provided as a command line argument. This is useful for quick tests,
without needing to write a script and execute it, and without needing to
run the interactive interpreter. It can also be used in scripts or
one-liners.

Unfortunately, "-c" is already used in the drgn CLI. However, this
functionality would be quite useful. Let's add it to the CLI using the
option "-e", which is short for "execute" or "exec", the underlying
Python function.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn/cli.py
@@ -492,6 +492,12 @@ def _main() -> None:
         help="don't print any logs or download progress",
     )
     parser.add_argument(
+        "-e",
+        dest="exec",
+        metavar="CODE",
+        help="an expression or statement to evaluate, instead of running in interactive mode",
+    )
+    parser.add_argument(
         "script",
         metavar="ARG",
         type=str,
@@ -502,7 +508,7 @@ def _main() -> None:
 
     args = parser.parse_args()
 
-    if args.script:
+    if args.script and not args.exec:
         # A common mistake users make is running drgn $core_dump, which tries
         # to run $core_dump as a Python script. Rather than failing later with
         # some inscrutable syntax or encoding error, try to catch this early
@@ -518,7 +524,7 @@ def _main() -> None:
             )
         elif script_type == "elf":
             sys.exit(f"error: {args.script[0]} is a binary, not a drgn script")
-    else:
+    elif not args.exec:
         print(version, file=sys.stderr, flush=True)
 
     if args.log_level == "none":
@@ -564,7 +570,11 @@ def _main() -> None:
 
     _load_debugging_symbols(prog, args, color)
 
-    if args.script:
+    if args.exec:
+        sys.path.insert(0, "")
+        sys.argv = ["-e"] + args.script
+        exec(args.exec, default_globals(prog))
+    elif args.script:
         sys.argv = args.script
         script = args.script[0]
         if pkgutil.get_importer(script) is None:

commit 7237106c5e1b70598626e0288d4d3de4e73b105f

cli: don't enter interactive mode for empty -e argument
drgn -e '' should start drgn, execute the empty statement, and exit, not
enter interactive mode. Fix it by checking whether args.exec is None
instead of bool(args.exec).

Fixes: 150ee760dcd3 ("cli: add -e option to exec() code directly")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/cli.py
@@ -530,8 +530,8 @@ def _main() -> None:
 
     args = parser.parse_args()
 
-    script = bool(not args.exec and args.args)
-    interactive = bool(not args.exec and not args.args and _is_tty(sys.stdin))
+    script = bool(args.exec is None and args.args)
+    interactive = bool(args.exec is None and not args.args and _is_tty(sys.stdin))
     if script:
         # A common mistake users make is running drgn $core_dump, which tries
         # to run $core_dump as a Python script. Rather than failing later with
@@ -609,13 +609,13 @@ def _main() -> None:
         else:
             sys.path.insert(0, "")
             exec_globals = default_globals(prog)
-            if args.exec:
-                sys.argv = ["-e"] + args.args
-                exec(args.exec, exec_globals)
-            else:
+            if args.exec is None:
                 sys.argv = [""]
                 exec_globals["__file__"] = "<stdin>"
                 exec(compile(sys.stdin.read(), "<stdin>", "exec"), exec_globals)
+            else:
+                sys.argv = ["-e"] + args.args
+                exec(args.exec, exec_globals)
 
 
 def run_interactive(

tests/test_cli.py
@@ -90,6 +90,13 @@ print(sys.argv)
         )
         self.assertEqual(proc.stdout, "['-e', 'pass']\n")
 
+    def test_e_empty(self):
+        self.run_cli(
+            ["--quiet", "--pid", "0", "--no-default-symbols", "-e", ""],
+            # This shouldn't be executed.
+            input="raise Exception('-e was ignored')",
+        )
+
     def test_script(self):
         with tempfile.NamedTemporaryFile() as f:
             f.write(