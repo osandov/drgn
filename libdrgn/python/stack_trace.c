osandov/drgn

commit 46343ae08d509a6e0f4273e92976e2ce2fca731b

libdrgn: get rid of struct drgn_stack_frame
In preparation for adding a "real", internal-only struct
drgn_stack_frame, replace the existing struct drgn_stack_frame with
explicit trace/frame arguments.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/stack_trace.c
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: GPL-3.0+
 
 #include "drgnpy.h"
+#include "../stack_trace.h"
 
 static void StackTrace_dealloc(StackTrace *self)
 {
+	struct drgn_program *prog = self->trace->prog;
 	drgn_stack_trace_destroy(self->trace);
-	Py_XDECREF(self->prog);
+	Py_XDECREF(container_of(prog, Program, prog));
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
@@ -32,18 +34,16 @@ static Py_ssize_t StackTrace_length(StackTrace *self)
 
 static StackFrame *StackTrace_item(StackTrace *self, Py_ssize_t i)
 {
-	StackFrame *ret;
-
 	if (i < 0 || i >= drgn_stack_trace_num_frames(self->trace)) {
 		PyErr_SetString(PyExc_IndexError,
 				"stack frame index out of range");
 		return NULL;
 	}
-	ret = (StackFrame *)StackFrame_type.tp_alloc(&StackFrame_type, 0);
+	StackFrame *ret =
+		(StackFrame *)StackFrame_type.tp_alloc(&StackFrame_type, 0);
 	if (!ret)
 		return NULL;
-	ret->frame.trace = self->trace;
-	ret->frame.i = i;
+	ret->i = i;
 	ret->trace = self;
 	Py_INCREF(self);
 	return ret;
@@ -74,13 +74,12 @@ static void StackFrame_dealloc(StackFrame *self)
 static PyObject *StackFrame_symbol(StackFrame *self)
 {
 	struct drgn_error *err;
+	Program *prog = container_of(self->trace->trace->prog, Program, prog);
 	struct drgn_symbol *sym;
-	PyObject *ret;
-
-	err = drgn_stack_frame_symbol(self->frame, &sym);
+	err = drgn_stack_frame_symbol(self->trace->trace, self->i, &sym);
 	if (err)
 		return set_drgn_error(err);
-	ret = Symbol_wrap(sym, self->trace->prog);
+	PyObject *ret = Symbol_wrap(sym, prog);
 	if (!ret) {
 		drgn_symbol_destroy(sym);
 		return NULL;
@@ -94,7 +93,8 @@ static PyObject *StackFrame_register(StackFrame *self, PyObject *arg)
 	uint64_t value;
 
 	if (PyUnicode_Check(arg)) {
-		err = drgn_stack_frame_register_by_name(self->frame,
+		err = drgn_stack_frame_register_by_name(self->trace->trace,
+							self->i,
 							PyUnicode_AsUTF8(arg),
 							&value);
 	} else {
@@ -104,8 +104,8 @@ static PyObject *StackFrame_register(StackFrame *self, PyObject *arg)
 			arg = PyStructSequence_GET_ITEM(arg, 1);
 		if (!index_converter(arg, &number))
 			return NULL;
-		err = drgn_stack_frame_register(self->frame, number.uvalue,
-						&value);
+		err = drgn_stack_frame_register(self->trace->trace, self->i,
+						number.uvalue, &value);
 	}
 	if (err)
 		return set_drgn_error(err);
@@ -122,7 +122,7 @@ static PyObject *StackFrame_registers(StackFrame *self)
 	dict = PyDict_New();
 	if (!dict)
 		return NULL;
-	platform = drgn_program_platform(&self->trace->prog->prog);
+	platform = drgn_program_platform(self->trace->trace->prog);
 	num_registers = drgn_platform_num_registers(platform);
 	for (i = 0; i < num_registers; i++) {
 		const struct drgn_register *reg;
@@ -131,7 +131,7 @@ static PyObject *StackFrame_registers(StackFrame *self)
 		int ret;
 
 		reg = drgn_platform_register(platform, i);
-		err = drgn_stack_frame_register(self->frame,
+		err = drgn_stack_frame_register(self->trace->trace, self->i,
 						drgn_register_number(reg),
 						&value);
 		if (err) {
@@ -156,7 +156,8 @@ static PyObject *StackFrame_registers(StackFrame *self)
 
 static PyObject *StackFrame_get_pc(StackFrame *self, void *arg)
 {
-	return PyLong_FromUnsignedLongLong(drgn_stack_frame_pc(self->frame));
+	uint64_t pc = drgn_stack_frame_pc(self->trace->trace, self->i);
+	return PyLong_FromUnsignedLongLong(pc);
 }
 
 static PyMethodDef StackFrame_methods[] = {

libdrgn/stack_trace.c
@@ -16,20 +16,11 @@
 #include "helpers.h"
 #include "platform.h"
 #include "program.h"
+#include "stack_trace.h"
 #include "string_builder.h"
 #include "symbol.h"
 #include "type.h"
 #include "util.h"
-
-struct drgn_stack_trace {
-	struct drgn_program *prog;
-	union {
-		size_t capacity;
-		Dwfl_Thread *thread;
-	};
-	size_t num_frames;
-	Dwfl_Frame *frames[];
-};
 
 LIBDRGN_PUBLIC void drgn_stack_trace_destroy(struct drgn_stack_trace *trace)
 {
@@ -37,8 +28,8 @@ LIBDRGN_PUBLIC void drgn_stack_trace_destroy(struct drgn_stack_trace *trace)
 	free(trace);
 }
 
-LIBDRGN_PUBLIC
-size_t drgn_stack_trace_num_frames(struct drgn_stack_trace *trace)
+LIBDRGN_PUBLIC size_t
+drgn_stack_trace_num_frames(struct drgn_stack_trace *trace)
 {
 	return trace->num_frames;
 }
@@ -48,21 +39,17 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 {
 	struct drgn_error *err;
 	struct string_builder str = {};
-	struct drgn_stack_frame frame = { .trace = trace, };
-
-	for (; frame.i < trace->num_frames; frame.i++) {
-		Dwarf_Addr pc;
-		bool isactivation;
-		Dwfl_Module *module;
-		struct drgn_symbol sym;
-
-		if (!string_builder_appendf(&str, "#%-2zu ", frame.i)) {
+	for (size_t frame = 0; frame < trace->num_frames; frame++) {
+		if (!string_builder_appendf(&str, "#%-2zu ", frame)) {
 			err = &drgn_enomem;
 			goto err;
 		}
 
-		dwfl_frame_pc(trace->frames[frame.i], &pc, &isactivation);
-		module = dwfl_frame_module(trace->frames[frame.i]);
+		Dwarf_Addr pc;
+		bool isactivation;
+		dwfl_frame_pc(trace->frames[frame], &pc, &isactivation);
+		Dwfl_Module *module = dwfl_frame_module(trace->frames[frame]);
+		struct drgn_symbol sym;
 		if (module &&
 		    drgn_program_find_symbol_by_address_internal(trace->prog,
 								 pc - !isactivation,
@@ -82,7 +69,7 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 			}
 		}
 
-		if (frame.i != trace->num_frames - 1 &&
+		if (frame != trace->num_frames - 1 &&
 		    !string_builder_appendc(&str, '\n')) {
 			err = &drgn_enomem;
 			goto err;
@@ -99,32 +86,30 @@ err:
 	return err;
 }
 
-LIBDRGN_PUBLIC uint64_t drgn_stack_frame_pc(struct drgn_stack_frame frame)
+LIBDRGN_PUBLIC uint64_t drgn_stack_frame_pc(struct drgn_stack_trace *trace,
+					    size_t frame)
 {
 	Dwarf_Addr pc;
-
-	dwfl_frame_pc(frame.trace->frames[frame.i], &pc, NULL);
+	dwfl_frame_pc(trace->frames[frame], &pc, NULL);
 	return pc;
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
-drgn_stack_frame_symbol(struct drgn_stack_frame frame, struct drgn_symbol **ret)
+drgn_stack_frame_symbol(struct drgn_stack_trace *trace, size_t frame,
+			struct drgn_symbol **ret)
 {
 	Dwarf_Addr pc;
 	bool isactivation;
-	Dwfl_Module *module;
-	struct drgn_symbol *sym;
-
-	dwfl_frame_pc(frame.trace->frames[frame.i], &pc, &isactivation);
+	dwfl_frame_pc(trace->frames[frame], &pc, &isactivation);
 	if (!isactivation)
 		pc--;
-	module = dwfl_frame_module(frame.trace->frames[frame.i]);
+	Dwfl_Module *module = dwfl_frame_module(trace->frames[frame]);
 	if (!module)
 		return drgn_error_symbol_not_found(pc);
-	sym = malloc(sizeof(*sym));
+	struct drgn_symbol *sym = malloc(sizeof(*sym));
 	if (!sym)
 		return &drgn_enomem;
-	if (!drgn_program_find_symbol_by_address_internal(frame.trace->prog, pc,
+	if (!drgn_program_find_symbol_by_address_internal(trace->prog, pc,
 							  module, sym)) {
 		free(sym);
 		return drgn_error_symbol_not_found(pc);
@@ -134,12 +119,11 @@ drgn_stack_frame_symbol(struct drgn_stack_frame frame, struct drgn_symbol **ret)
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
-drgn_stack_frame_register(struct drgn_stack_frame frame,
+drgn_stack_frame_register(struct drgn_stack_trace *trace, size_t frame,
 			  enum drgn_register_number regno, uint64_t *ret)
 {
 	Dwarf_Addr value;
-
-	if (!dwfl_frame_register(frame.trace->frames[frame.i], regno, &value)) {
+	if (!dwfl_frame_register(trace->frames[frame], regno, &value)) {
 		return drgn_error_create(DRGN_ERROR_LOOKUP,
 					 "register value is not known");
 	}
@@ -148,18 +132,17 @@ drgn_stack_frame_register(struct drgn_stack_frame frame,
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
-drgn_stack_frame_register_by_name(struct drgn_stack_frame frame,
+drgn_stack_frame_register_by_name(struct drgn_stack_trace *trace, size_t frame,
 				  const char *name, uint64_t *ret)
 {
-	const struct drgn_register *reg;
-
-	reg = drgn_architecture_register_by_name(frame.trace->prog->platform.arch,
-						 name);
+	const struct drgn_register *reg =
+		drgn_architecture_register_by_name(trace->prog->platform.arch,
+						   name);
 	if (!reg) {
 		return drgn_error_format(DRGN_ERROR_LOOKUP,
 					 "unknown register '%s'", name);
 	}
-	return drgn_stack_frame_register(frame, reg->number, ret);
+	return drgn_stack_frame_register(trace, frame, reg->number, ret);
 }
 
 static bool drgn_thread_memory_read(Dwfl *dwfl, Dwarf_Addr addr,

libdrgn/Makefile.am
@@ -60,6 +60,7 @@ libdrgnimpl_la_SOURCES = $(ARCH_INS:.c.in=.c) \
 			 siphash.h \
 			 splay_tree.c \
 			 stack_trace.c \
+			 stack_trace.h \
 			 string_builder.c \
 			 string_builder.h \
 			 symbol.c \

libdrgn/drgn.h.in
@@ -2597,11 +2597,6 @@ bool drgn_symbol_eq(struct drgn_symbol *a, struct drgn_symbol *b);
 
 struct drgn_stack_trace;
 
-struct drgn_stack_frame {
-	struct drgn_stack_trace *trace;
-	size_t i;
-};
-
 /** Destroy a @ref drgn_stack_trace. */
 void drgn_stack_trace_destroy(struct drgn_stack_trace *trace);
 
@@ -2619,7 +2614,7 @@ struct drgn_error *drgn_format_stack_trace(struct drgn_stack_trace *trace,
 					   char **ret);
 
 /** Get the return address at a stack frame. */
-uint64_t drgn_stack_frame_pc(struct drgn_stack_frame frame);
+uint64_t drgn_stack_frame_pc(struct drgn_stack_trace *trace, size_t frame);
 
 /**
  * Get the function symbol at a stack frame.
@@ -2628,17 +2623,19 @@ uint64_t drgn_stack_frame_pc(struct drgn_stack_frame frame);
  * drgn_symbol_destroy(). On error, its contents are undefined.
  * @return @c NULL on success, non-@c NULL on error.
  */
-struct drgn_error *drgn_stack_frame_symbol(struct drgn_stack_frame frame,
+struct drgn_error *drgn_stack_frame_symbol(struct drgn_stack_trace *trace,
+					   size_t frame,
 					   struct drgn_symbol **ret);
 
 /** Get the value of a register (by number) in a stack frame. */
-struct drgn_error *drgn_stack_frame_register(struct drgn_stack_frame frame,
+struct drgn_error *drgn_stack_frame_register(struct drgn_stack_trace *trace,
+					     size_t frame,
 					     enum drgn_register_number regno,
 					     uint64_t *ret);
 
 /** Get the value of a register (by name) in a stack frame. */
 struct drgn_error *
-drgn_stack_frame_register_by_name(struct drgn_stack_frame frame,
+drgn_stack_frame_register_by_name(struct drgn_stack_trace *trace, size_t frame,
 				  const char *name, uint64_t *ret);
 
 /**

libdrgn/python/drgnpy.h
@@ -92,14 +92,13 @@ typedef struct {
 
 typedef struct {
 	PyObject_HEAD
-	Program *prog;
 	struct drgn_stack_trace *trace;
 } StackTrace;
 
 typedef struct {
 	PyObject_HEAD
 	StackTrace *trace;
-	struct drgn_stack_frame frame;
+	size_t i;
 } StackFrame;
 
 typedef struct {

libdrgn/python/program.c
@@ -729,7 +729,6 @@ static StackTrace *Program_stack_trace(Program *self, PyObject *args,
 		return NULL;
 	}
 	ret->trace = trace;
-	ret->prog = self;
 	Py_INCREF(self);
 	return ret;
 }

libdrgn/stack_trace.h
@@ -1,0 +1,40 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// SPDX-License-Identifier: GPL-3.0+
+
+/**
+ * @file
+ *
+ * Stack trace internals
+ *
+ * See @ref StackTraceInternals.
+ */
+
+#ifndef DRGN_STACK_TRACE_H
+#define DRGN_STACK_TRACE_H
+
+/**
+ * @ingroup Internals
+ *
+ * @defgroup StackTraceInternals Stack traces
+ *
+ * Stack trace internals.
+ *
+ * This provides the internal data structures used for stack traces.
+ *
+ * @{
+ */
+
+struct drgn_stack_trace {
+	struct drgn_program *prog;
+	union {
+		Dwfl_Thread *thread;
+		/* Used during creation. */
+		size_t capacity;
+	};
+	size_t num_frames;
+	Dwfl_Frame *frames[];
+};
+
+/** @} */
+
+#endif /* DRGN_STACK_TRACE_H */

commit 3ccbf9faac0f5a0620146b359bfeca0389275ba4

libdrgn: python: check correct pointer in StackTrace_dealloc()
We should be checking if self->trace is NULL, not if
container_of(self->trace->prog, Program, prog) is NULL. We don't create
StackTrace objects with a NULL self->trace anyways, so this is mostly
defensive.

Fixes: 46343ae08d50 ("libdrgn: get rid of struct drgn_stack_frame")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/stack_trace.c
@@ -16,9 +16,11 @@ PyObject *StackTrace_wrap(struct drgn_stack_trace *trace) {
 
 static void StackTrace_dealloc(StackTrace *self)
 {
-	struct drgn_program *prog = self->trace->prog;
-	drgn_stack_trace_destroy(self->trace);
-	Py_XDECREF(container_of(prog, Program, prog));
+	if (self->trace) {
+		struct drgn_program *prog = self->trace->prog;
+		drgn_stack_trace_destroy(self->trace);
+		Py_DECREF(container_of(prog, Program, prog));
+	}
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }
 