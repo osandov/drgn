osandov/drgn

commit 3f3a9575628d6d8e635b94e9c8dc17b77316682a

libdrgn: linux_kernel: Fix module detection on kernel v6.4
Kernel commit ac3b43283923 ("module: replace module_layout with
module_memory") in v6.4 changed the layout of `struct module`, resulting
in the following drgn error [1].

Fix this by first trying to determine the base address and size of each
kernel module via the `struct module_memory mem[MOD_TEXT]` member,
before falling back to previous methods that work on older kernels.

Tested on v6.4-rc2 and v6.3 which does not include the above mentioned
commit.

Note that kernel commit b4aff7513df3 ("scripts/gdb: use mem instead of
core_layout to get the module address") performs a similar fix in Python
GDB scripts.

Closes #296.

[1]
```
# drgn
drgn 0.0.22 (using Python 3.11.3, elfutils 0.189, with libkdumpfile)
For help, type help(drgn).
>>> import drgn
>>> from drgn import NULL, Object, cast, container_of, execscript, offsetof, reinterpret, sizeof
>>> from drgn.helpers.common import *
>>> from drgn.helpers.linux import *
warning: could not get debugging information for:
kernel modules (could not find loaded kernel modules: 'struct module' has no member 'core_size')
```

Signed-off-by: Ido Schimmel <idosch@nvidia.com>

libdrgn/linux_kernel.c
@@ -496,28 +496,65 @@ kernel_module_iterator_next(struct kernel_module_iterator *it)
 		return err;
 
 	// Set tmp1 to the module base address and tmp2 to the size.
-	err = drgn_object_member(&it->tmp1, &it->mod, "core_layout");
+	err = drgn_object_member(&it->tmp1, &it->mod, "mem");
 	if (!err) {
+		union drgn_value mod_mem_type;
+
+		// Since Linux kernel commit ac3b43283923 ("module: replace
+		// module_layout with module_memory") (in v6.4), the base and
+		// size are in the `struct module_memory mem[MOD_TEXT]` member
+		// of `struct module`.
+		err = drgn_program_find_object(drgn_object_program(&it->mod),
+					       "MOD_TEXT", NULL,
+					       DRGN_FIND_OBJECT_CONSTANT,
+					       &it->tmp3);
+		if (err)
+			return err;
+		err = drgn_object_read_integer(&it->tmp3, &mod_mem_type);
+		if (err)
+			return err;
+
+		err = drgn_object_subscript(&it->tmp3, &it->tmp1,
+					    mod_mem_type.uvalue);
+		if (err)
+			return err;
+		err = drgn_object_member(&it->tmp2, &it->tmp3, "size");
+		if (err)
+			return err;
+		err = drgn_object_member(&it->tmp1, &it->tmp3, "base");
+		if (err)
+			return err;
+	} else if (err->code == DRGN_ERROR_LOOKUP) {
 		// Since Linux kernel commit 7523e4dc5057 ("module: use a
 		// structure to encapsulate layout.") (in v4.5), the base and
 		// size are in the `struct module_layout core_layout` member of
 		// `struct module`.
-		err = drgn_object_member(&it->tmp2, &it->tmp1, "size");
-		if (err)
-			return err;
-		err = drgn_object_member(&it->tmp1, &it->tmp1, "base");
-		if (err)
-			return err;
-	} else if (err->code == DRGN_ERROR_LOOKUP) {
-		// Before that, they are directly in the `struct module`.
 		drgn_error_destroy(err);
 
-		err = drgn_object_member(&it->tmp2, &it->mod, "core_size");
-		if (err)
+		err = drgn_object_member(&it->tmp1, &it->mod, "core_layout");
+		if (!err) {
+			err = drgn_object_member(&it->tmp2, &it->tmp1, "size");
+			if (err)
+				return err;
+			err = drgn_object_member(&it->tmp1, &it->tmp1, "base");
+			if (err)
+				return err;
+		} else if (err->code == DRGN_ERROR_LOOKUP) {
+			// Before that, they are directly in the `struct
+			// module`.
+			drgn_error_destroy(err);
+
+			err = drgn_object_member(&it->tmp2, &it->mod,
+						 "core_size");
+			if (err)
+				return err;
+			err = drgn_object_member(&it->tmp1, &it->mod,
+						 "module_core");
+			if (err)
+				return err;
+		} else {
 			return err;
-		err = drgn_object_member(&it->tmp1, &it->mod, "module_core");
-		if (err)
-			return err;
+		}
 	} else {
 		return err;
 	}

commit 8c4abe92f17f72328e9ecf425cb60163170a9da3

libdrgn: linux_kernel: add all address ranges for kernel modules
Since Linux kernel commit ac3b43283923 ("module: replace module_layout
with module_memory") (in v6.4), the memory for Linux kernel loadable
modules is allocated in multiple different "types". These allocations
can be discontiguous and can even be interleaved with other modules.

The fixes commit missed this when updating module detection for v6.4
(and even if we had noticed it, we wouldn't have been able to deal with
it easily before the module API). We currently only add the address
range for the module text. As a result, module lookups by address (and
thus symbol lookups by address) fail for other (e.g., data) addresses.

Now that a drgn_module can have multiple address ranges, add one for
each memory type.

Closes #501.

Fixes: 3f3a9575628d ("libdrgn: linux_kernel: Fix module detection on kernel v6.4")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/linux_kernel.c
@@ -1401,6 +1401,149 @@ yield_vmlinux(struct linux_kernel_loaded_module_iterator *it,
 	return NULL;
 }
 
+enum kernel_module_address_ranges_version {
+	// Since Linux kernel commit ac3b43283923 ("module: replace
+	// module_layout with module_memory") (in v6.4), `struct module`
+	// contains an array, `struct module_memory mem[]`, of discontiguous
+	// allocations per memory type (`module->mem[type].base` and
+	// `module->mem[type].size`). The module address is
+	// `module->mem[MOD_TEXT].base`.
+	MODULE_MEMORY,
+	// Between that and Linux kernel commit 7523e4dc5057 ("module: use a
+	// structure to encapsulate layout.") (in v4.5), `struct module`
+	// contains a `struct module_layout core_layout` member with the base
+	// address (`module->core_layout.base`) and contiguous size
+	// (`module->core_layout.size`).
+	MODULE_LAYOUT,
+	// Before that, `struct module` contains the base address
+	// (`module->module_core`) and contiguous size (`module->core_size`)
+	// directly.
+	IN_MODULE,
+};
+
+static struct drgn_error *
+kernel_module_address(const struct drgn_object *module_obj,
+		      struct drgn_object *mem,
+		      enum kernel_module_address_ranges_version *version_ret,
+		      uint64_t *address_ret)
+{
+	struct drgn_program *prog = drgn_object_program(module_obj);
+	struct drgn_error *err;
+
+	DRGN_OBJECT(tmp, prog);
+	err = drgn_object_member(mem, module_obj, "mem");
+	if (!err) {
+		*version_ret = MODULE_MEMORY;
+		if (!prog->mod_text_cached) {
+			err = drgn_program_find_object(prog, "MOD_TEXT", NULL,
+						       DRGN_FIND_OBJECT_CONSTANT,
+						       &tmp);
+			if (err)
+				return err;
+			union drgn_value mod_text_value;
+			err = drgn_object_read_integer(&tmp, &mod_text_value);
+			if (err)
+				return err;
+			prog->mod_text = mod_text_value.uvalue;
+			prog->mod_text_cached = true;
+		}
+		err = drgn_object_subscript(&tmp, mem, prog->mod_text);
+		if (err)
+			return err;
+		err = drgn_object_member(&tmp, &tmp, "base");
+	} else if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP)) {
+		err = drgn_object_member(mem, module_obj, "core_layout");
+		if (!err) {
+			*version_ret = MODULE_LAYOUT;
+			err = drgn_object_member(&tmp, mem, "base");
+		} else if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP)) {
+			*version_ret = IN_MODULE;
+			err = drgn_object_member(&tmp, module_obj,
+						 "module_core");
+		}
+	}
+	if (err)
+		return err;
+	return drgn_object_read_unsigned(&tmp, address_ret);
+}
+
+// If version is MODULE_MEMORY, mem is struct module::mem. If version is
+// MODULE_LAYOUT, mem is struct module::core_layout.
+static struct drgn_error *
+kernel_module_set_address_ranges(struct drgn_module *module,
+				 enum kernel_module_address_ranges_version version,
+				 const struct drgn_object *module_obj,
+				 const struct drgn_object *mem,
+				 uint64_t address)
+{
+	struct drgn_program *prog = module->prog;
+	struct drgn_error *err;
+
+	DRGN_OBJECT(tmp, prog);
+	if (version != MODULE_MEMORY) {
+		if (version == IN_MODULE)
+			err = drgn_object_member(&tmp, module_obj, "core_size");
+		else
+			err = drgn_object_member(&tmp, mem, "size");
+		if (err)
+			return err;
+		uint64_t size;
+		err = drgn_object_read_unsigned(&tmp, &size);
+		if (err)
+			return err;
+		drgn_log_debug(prog, "module size is %" PRIu64, size);
+		return drgn_module_set_address_range(module, address,
+						     address + size);
+	}
+
+	struct drgn_type *mem_array_type = drgn_underlying_type(mem->type);
+	if (drgn_type_kind(mem_array_type) != DRGN_TYPE_ARRAY) {
+		return drgn_error_create(DRGN_ERROR_TYPE,
+					 "struct module::mem is not an array");
+	}
+	uint64_t length = drgn_type_length(mem_array_type);
+
+	if (length > SIZE_MAX)
+		return &drgn_enomem;
+	_cleanup_free_ uint64_t (*ranges)[2] =
+		malloc_array(length, sizeof(*ranges));
+	if (!ranges)
+		return &drgn_enomem;
+
+	DRGN_OBJECT(element, prog);
+	size_t num_ranges = 0;
+	for (size_t i = 0; i < length; i++) {
+		err = drgn_object_subscript(&element, mem, i);
+		if (err)
+			return err;
+
+		err = drgn_object_member(&tmp, &element, "size");
+		if (err)
+			return err;
+		uint64_t size;
+		err = drgn_object_read_unsigned(&tmp, &size);
+		if (err)
+			return err;
+		if (!size)
+			continue;
+
+		err = drgn_object_member(&tmp, &element, "base");
+		if (err)
+			return err;
+		uint64_t base;
+		err = drgn_object_read_unsigned(&tmp, &base);
+		if (err)
+			return err;
+
+		drgn_log_debug(prog, "module has address range %" PRIu64 "-%" PRIu64,
+			       base, base + size);
+		ranges[num_ranges][0] = base;
+		ranges[num_ranges][1] = base + size;
+		num_ranges++;
+	}
+	return drgn_module_set_address_ranges(module, ranges, num_ranges);
+}
+
 static struct drgn_error *
 kernel_module_set_build_id_live(struct drgn_module *module)
 {
@@ -1852,56 +1995,9 @@ kernel_module_find_or_create_internal(const struct drgn_object *module_ptr,
 	const char *name = drgn_object_buffer(module_obj) + name_offset;
 
 	DRGN_OBJECT(mem, prog);
-	DRGN_OBJECT(val, prog);
-	bool layout_in_module = false;
-	err = drgn_object_member(&mem, module_obj, "mem");
-	if (!err) {
-		// Since Linux kernel commit ac3b43283923 ("module: replace
-		// module_layout with module_memory") (in v6.4), the base and
-		// size are in the `struct module_memory mem[MOD_TEXT]` member
-		// of `struct module`.
-		if (!prog->mod_text_cached) {
-			err = drgn_program_find_object(prog, "MOD_TEXT", NULL,
-						       DRGN_FIND_OBJECT_CONSTANT,
-						       &val);
-			if (err)
-				return err;
-			union drgn_value mod_text_value;
-			err = drgn_object_read_integer(&val, &mod_text_value);
-			if (err)
-				return err;
-			prog->mod_text = mod_text_value.uvalue;
-			prog->mod_text_cached = true;
-		}
-		err = drgn_object_subscript(&mem, &mem, prog->mod_text);
-		if (err)
-			return err;
-	} else {
-		if (err->code != DRGN_ERROR_LOOKUP)
-			return err;
-		drgn_error_destroy(err);
-		// Between that and Linux kernel commit 7523e4dc5057 ("module:
-		// use a structure to encapsulate layout.") (in v4.5), the base
-		// and size are in the `struct module_layout core_layout` member
-		// of `struct module`.
-		err = drgn_object_member(&mem, module_obj, "core_layout");
-		if (err) {
-			if (err->code != DRGN_ERROR_LOOKUP)
-				return err;
-			drgn_error_destroy(err);
-			// Before that, they are directly in the `struct
-			// module`.
-			layout_in_module = true;
-		}
-	}
-	if (layout_in_module)
-		err = drgn_object_member(&val, module_obj, "module_core");
-	else
-		err = drgn_object_member(&val, &mem, "base");
-	if (err)
-		return err;
+	enum kernel_module_address_ranges_version version;
 	uint64_t address;
-	err = drgn_object_read_unsigned(&val, &address);
+	err = kernel_module_address(module_obj, &mem, &version, &address);
 	if (err)
 		return err;
 
@@ -1934,19 +2030,8 @@ kernel_module_find_or_create_internal(const struct drgn_object *module_ptr,
 	if (err)
 		return err;
 
-	if (layout_in_module)
-		err = drgn_object_member(&val, module_obj, "core_size");
-	else
-		err = drgn_object_member(&val, &mem, "size");
-	if (err)
-		return err;
-	uint64_t size;
-	err = drgn_object_read_unsigned(&val, &size);
-	if (err)
-		return err;
-
-	drgn_log_debug(prog, "module size is %" PRIu64, size);
-	err = drgn_module_set_address_range(module, address, address + size);
+	err = kernel_module_set_address_ranges(module, version, module_obj,
+					       &mem, address);
 	if (err)
 		return err;
 

tests/linux_kernel/kmod/drgn_test.c
@@ -1277,6 +1277,9 @@ int drgn_test_function(int x)
 	return x + 1;
 }
 
+char drgn_test_data[] = "abc";
+const char drgn_test_rodata[] = "def";
+
 // kmodify
 
 #ifdef __x86_64__

tests/linux_kernel/test_debug_info.py
@@ -120,7 +120,11 @@ class TestModule(LinuxKernelTestCase):
         def module_dict(prog):
             return {
                 (module.name, module.address): (
-                    module.address_range,
+                    (
+                        None
+                        if module.address_ranges is None
+                        else sorted(module.address_ranges)
+                    ),
                     module.build_id,
                     dict(module.section_addresses),
                 )

tests/linux_kernel/test_symbol.py
@@ -2,12 +2,48 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 from drgn import SymbolBinding, SymbolKind
-from tests.linux_kernel import LinuxKernelTestCase
+from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
 
 
 class TestSymbol(LinuxKernelTestCase):
     def test_global_symbol(self):
         symbol = self.prog.symbol("jiffies")
         self.assertEqual(symbol.name, "jiffies")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.OBJECT)
+
+    @skip_unless_have_test_kmod
+    def test_module_function_symbol(self):
+        symbol = self.prog.symbol("drgn_test_function")
+        self.assertEqual(symbol.name, "drgn_test_function")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.FUNC)
+
+        symbol = self.prog.symbol(symbol.address)
+        self.assertEqual(symbol.name, "drgn_test_function")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.FUNC)
+
+    @skip_unless_have_test_kmod
+    def test_module_data_symbol(self):
+        symbol = self.prog.symbol("drgn_test_data")
+        self.assertEqual(symbol.name, "drgn_test_data")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.OBJECT)
+
+        symbol = self.prog.symbol(symbol.address)
+        self.assertEqual(symbol.name, "drgn_test_data")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.OBJECT)
+
+    @skip_unless_have_test_kmod
+    def test_module_rodata_symbol(self):
+        symbol = self.prog.symbol("drgn_test_rodata")
+        self.assertEqual(symbol.name, "drgn_test_rodata")
+        self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
+        self.assertEqual(symbol.kind, SymbolKind.OBJECT)
+
+        symbol = self.prog.symbol(symbol.address)
+        self.assertEqual(symbol.name, "drgn_test_rodata")
         self.assertEqual(symbol.binding, SymbolBinding.GLOBAL)
         self.assertEqual(symbol.kind, SymbolKind.OBJECT)