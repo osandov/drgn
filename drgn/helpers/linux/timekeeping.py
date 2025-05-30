50efc0279d493c1e32a17cbbd687b8ca42d410fb
Omar Sandoval <osandov@osandov.com>
2025-05-30 16:12:46 -0700

    drgn.helpers.linux.timekeeping: make ktime helpers less racy
    I've caught a few spurious failures in the timekeeping helper tests
    where the value returned by the helpers goes backwards. This happens
    when we race between updates of tk_core.timekeeper.tkr_mono.base and
    tk_core.timekeeper.coarse_nsec/tk_core.timekeeper.tkr_mono.xtime_nsec.
    
    It's very difficult to avoid this race entirely, but we can make it much
    narrower by doing a single memory read and manually extracting members
    we need. Checking whether the seqlock is write-locked makes it even
    narrower.
    
    This version made it through millions of iterations. If we still see
    flakiness, we can add a tolerance of 1 second in the tests.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/timekeeping.py
+++ drgn/helpers/linux/timekeeping.py
@@ -12,11 +12,18 @@ The ``drgn.helpers.linux.timekeeping`` module provides helpers for timestamps.
     In core dumps, it is only possible to recover coarse timestamps, which are
     only updated once per tick (~1-10 ms). Therefore, only the
     ``ktime_get_seconds()`` and ``ktime_get_coarse_ns()`` families of functions
-    are provided.
+    are provided. These functions can also race with timekeeping updates and
+    return a value with an error of up to 1 second.
 """
 
-from drgn import Object, Program, cast
+import functools
+import logging
+from typing import Optional
+
+from drgn import Object, Program, ProgramFlags, cast, sizeof
 from drgn.helpers.common.prog import takes_program_or_default
+
+logger = logging.getLogger("drgn")
 
 __all__ = (
     "ktime_get_boottime_seconds",
@@ -43,6 +50,108 @@ def ktime_get_seconds(prog: Program) -> Object:
     return cast("time64_t", prog["tk_core"].timekeeper.ktime_sec)
 
 
+def _ktime_get_coarse_ns(prog: Program, offs_name: Optional[str]) -> Object:
+    # The timekeeper is updated every tick, so we're very likely to catch it in
+    # the middle of an update. To make that less likely, we take a "snapshot"
+    # of the relevant timekeeper memory, check whether the seqcount was
+    # write-locked during our snapshot, and retry (if live) or warn if so. The
+    # snapshot isn't atomic, so it's still possible to race, but the window is
+    # narrow.
+    #
+    # An alternative that fully avoids the race would be simulating a
+    # read_seqlock operation, but that requires multiple reads and would likely
+    # never succeed on, for example, slow remote connections.
+    #
+    # We cache the address and size to read and functions for extracting
+    # members.
+    try:
+        address, size, from_snapshot = prog.cache["timekeeper_snapshot"]
+    except KeyError:
+        tk_core = prog["tk_core"]
+        tk = tk_core.timekeeper
+        members = {
+            "base": tk.tkr_mono.base,
+            "offs_real": tk.offs_real,
+            "offs_boot": tk.offs_boot,
+            "offs_tai": tk.offs_tai,
+        }
+
+        # Since Linux kernel commit 025e82bcbc34 ("timekeeping: Use sequence
+        # counter with associated raw spinlock") (in v5.9), struct tk_data::seq
+        # is a seqcount_raw_spinlock_t. Before that, it is a seqcount_t.
+        try:
+            members["seq"] = tk_core.seq.seqcount.sequence
+        except AttributeError:
+            members["seq"] = tk_core.seq.sequence
+
+        # Since Linux kernel commit b71f9804f66c ("timekeeping: Prevent coarse
+        # clocks going backwards") (in v6.15), the nanoseconds part is directly
+        # in struct timekeeper::coarse_nsec. Before that, it is computed from
+        # struct timekeeper::tkr_mono.
+        try:
+            members["coarse_nsec"] = tk.coarse_nsec
+        except AttributeError:
+            members["xtime_nsec"] = tk.tkr_mono.xtime_nsec
+            members["shift"] = tk.tkr_mono.shift
+
+        address = min(  # type: ignore[type-var]  # member.address_ can't be None
+            member.address_ for member in members.values()
+        )
+        size = (
+            max(
+                member.address_ + sizeof(member)  # type: ignore[operator]  # member.address_ can't be None.
+                for member in members.values()
+            )
+            - address
+        )
+        assert size <= 1024
+
+        from_snapshot = {}
+        for name, member in members.items():
+            member_type = member.type_
+            # Since Linux kernel commit 2456e8553544 ("ktime: Get rid of the
+            # union") (in v4.10), ktime_t is a typedef of s64. Before that, it
+            # was a dummy union wrapping an s64. In both cases, we actually
+            # want a u64.
+            if member_type.type_name() == "ktime_t":
+                member_type = prog.type("u64")
+            from_snapshot[name] = functools.partial(
+                Object.from_bytes_,
+                prog,
+                member_type,
+                bit_offset=(member.address_ - address) * 8,
+            )
+
+        prog.cache["timekeeper_snapshot"] = address, size, from_snapshot
+
+    # On live kernels, we retry a limited number of times, then warn and move
+    # on.
+    for _ in range(1000):
+        snapshot = prog.read(address, size)
+        seq = from_snapshot["seq"](snapshot)
+        if seq & 1:
+            if prog.flags & ProgramFlags.IS_LIVE:
+                continue
+            else:
+                # For core dumps, the best we can do is warn.
+                logger.warning("timekeeper was write-locked; ktime may be inconsistent")
+                break
+        break
+    else:
+        logger.warning(
+            "couldn't get unlocked snapshot of timekeeper; ktime may be inconsistent"
+        )
+
+    ns = from_snapshot["base"](snapshot)
+    try:
+        ns += from_snapshot["coarse_nsec"](snapshot)
+    except KeyError:
+        ns += from_snapshot["xtime_nsec"](snapshot) >> from_snapshot["shift"](snapshot)
+    if offs_name is not None:
+        ns += from_snapshot[offs_name](snapshot)
+    return ns
+
+
 @takes_program_or_default
 def ktime_get_coarse_ns(prog: Program) -> Object:
     """
@@ -50,33 +159,7 @@ def ktime_get_coarse_ns(prog: Program) -> Object:
 
     :return: ``u64``
     """
-    tk = prog["tk_core"].timekeeper
-    # Since Linux kernel commit 2456e8553544 ("ktime: Get rid of the union")
-    # (in v4.10), ktime_t is a typedef of s64. Before that, it was a dummy
-    # union wrapping an s64.
-    try:
-        base = cast("u64", tk.tkr_mono.base)
-    except TypeError:
-        base = cast("u64", tk.tkr_mono.base.tv64)
-    # Since Linux kernel commit b71f9804f66c ("timekeeping: Prevent coarse
-    # clocks going backwards") (in v6.15), the nanoseconds part is directly in
-    # struct timekeeper::coarse_nsec. Before that, it is computed from
-    # struct timekeeper::tkr_mono.
-    try:
-        nsecs = tk.coarse_nsec
-    except AttributeError:
-        nsecs = tk.tkr_mono.xtime_nsec >> tk.tkr_mono.shift
-    return base + nsecs
-
-
-def _ktime_get_coarse_ns_with_offset(prog: Program, offs_name: str) -> Object:
-    ns = ktime_get_coarse_ns(prog)
-    offset = prog.variable("offsets", "timekeeping.c")[prog[offs_name]]
-    # Handle old union ktime. See the comment in ktime_get_coarse_ns().
-    try:
-        return ns + offset[0]
-    except TypeError:
-        return ns + offset.tv64
+    return _ktime_get_coarse_ns(prog, None)
 
 
 @takes_program_or_default
@@ -97,7 +180,7 @@ def ktime_get_coarse_real_ns(prog: Program) -> Object:
 
     :return: ``u64``
     """
-    return _ktime_get_coarse_ns_with_offset(prog, "TK_OFFS_REAL")
+    return _ktime_get_coarse_ns(prog, "offs_real")
 
 
 @takes_program_or_default
@@ -119,7 +202,7 @@ def ktime_get_coarse_boottime_ns(prog: Program) -> Object:
 
     :return: ``u64``
     """
-    return _ktime_get_coarse_ns_with_offset(prog, "TK_OFFS_BOOT")
+    return _ktime_get_coarse_ns(prog, "offs_boot")
 
 
 @takes_program_or_default
@@ -140,7 +223,7 @@ def ktime_get_coarse_clocktai_ns(prog: Program) -> Object:
 
     :return: ``u64``
     """
-    return _ktime_get_coarse_ns_with_offset(prog, "TK_OFFS_TAI")
+    return _ktime_get_coarse_ns(prog, "offs_tai")
 
 
 @takes_program_or_default
