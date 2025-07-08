osandov/drgn

commit 9e62d688cda0c872267abee157c98da7dff023e7

drgndoc: use NamedTuple for {Bound,Resolved}Node to fix mypy error
Since we converted to dataclasses, we get the following error from mypy:

  error: Cannot use a covariant type variable as a parameter

The easiest fix is apparently making the type read-only.

Fixes: e404b6330c17 ("drgndoc: use dataclasses")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

docs/exts/drgndoc/namespace.py
@@ -1,9 +1,17 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-import dataclasses
 import itertools
-from typing import Generic, Iterator, List, Mapping, Sequence, TypeVar, Union
+from typing import (
+    Generic,
+    Iterator,
+    List,
+    Mapping,
+    NamedTuple,
+    Sequence,
+    TypeVar,
+    Union,
+)
 
 from drgndoc.parse import (
     Class,
@@ -19,14 +27,12 @@ from drgndoc.parse import (
 NodeT_co = TypeVar("NodeT_co", bound=Node, covariant=True)
 
 
-@dataclasses.dataclass
-class BoundNode(Generic[NodeT_co]):
+class BoundNode(NamedTuple, Generic[NodeT_co]):
     name: str
     node: NodeT_co
 
 
-@dataclasses.dataclass
-class ResolvedNode(Generic[NodeT_co]):
+class ResolvedNode(NamedTuple, Generic[NodeT_co]):
     modules: Sequence[BoundNode[Module]]
     classes: Sequence[BoundNode[Class]]
     name: str

commit 9928e266729796d3060cd2510bec9a90d0ceeb5d

drgndoc: go back to defining BoundNode and ResolvedNode manually
dataclasses give us mypy errors, and NamedTuples give us "TypeError:
Multiple inheritance with NamedTuple is not supported" on Python < 3.11
(I mistakenly thought this was supported since Python 3.8). Go back to
defining these types manually.

Fixes: 9e62d688cda0 ("drgndoc: use NamedTuple for {Bound,Resolved}Node to fix mypy error")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

docs/exts/drgndoc/namespace.py
@@ -2,16 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import itertools
-from typing import (
-    Generic,
-    Iterator,
-    List,
-    Mapping,
-    NamedTuple,
-    Sequence,
-    TypeVar,
-    Union,
-)
+from typing import Generic, Iterator, List, Mapping, Sequence, TypeVar, Union
 
 from drgndoc.parse import (
     Class,
@@ -27,16 +18,24 @@ from drgndoc.parse import (
 NodeT_co = TypeVar("NodeT_co", bound=Node, covariant=True)
 
 
-class BoundNode(NamedTuple, Generic[NodeT_co]):
-    name: str
-    node: NodeT_co
+class BoundNode(Generic[NodeT_co]):
+    def __init__(self, name: str, node: NodeT_co) -> None:
+        self.name = name
+        self.node = node
 
 
-class ResolvedNode(NamedTuple, Generic[NodeT_co]):
-    modules: Sequence[BoundNode[Module]]
-    classes: Sequence[BoundNode[Class]]
-    name: str
-    node: NodeT_co
+class ResolvedNode(Generic[NodeT_co]):
+    def __init__(
+        self,
+        modules: Sequence[BoundNode[Module]],
+        classes: Sequence[BoundNode[Class]],
+        name: str,
+        node: NodeT_co,
+    ) -> None:
+        self.modules = modules
+        self.classes = classes
+        self.name = name
+        self.node = node
 
     def qualified_name(self) -> str:
         return ".".join(