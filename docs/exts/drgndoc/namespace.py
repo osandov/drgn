osandov/drgn

commit e404b6330c171fddf659bab85d37d9e7bae3eb4c

drgndoc: use dataclasses
Now that we don't support Python 3.6, we can finally get replace the
boilerplate for the various node types with dataclasses.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

docs/exts/drgndoc/namespace.py
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+import dataclasses
 import itertools
 from typing import Generic, Iterator, List, Mapping, Sequence, TypeVar, Union
 
@@ -18,24 +19,18 @@ from drgndoc.parse import (
 NodeT_co = TypeVar("NodeT_co", bound=Node, covariant=True)
 
 
+@dataclasses.dataclass
 class BoundNode(Generic[NodeT_co]):
-    def __init__(self, name: str, node: NodeT_co) -> None:
-        self.name = name
-        self.node = node
+    name: str
+    node: NodeT_co
 
 
+@dataclasses.dataclass
 class ResolvedNode(Generic[NodeT_co]):
-    def __init__(
-        self,
-        modules: Sequence[BoundNode[Module]],
-        classes: Sequence[BoundNode[Class]],
-        name: str,
-        node: NodeT_co,
-    ) -> None:
-        self.modules = modules
-        self.classes = classes
-        self.name = name
-        self.node = node
+    modules: Sequence[BoundNode[Module]]
+    classes: Sequence[BoundNode[Class]]
+    name: str
+    node: NodeT_co
 
     def qualified_name(self) -> str:
         return ".".join(

docs/exts/drgndoc/parse.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import ast
+import dataclasses
 import inspect
 import operator
 import os.path
@@ -64,32 +65,21 @@ class _PreTransformer(ast.NodeTransformer):
         return self.visit(node.value)
 
 
-# Once we don't care about Python 3.6, we can replace all of this boilerplate
-# with dataclasses.
-
-
+@dataclasses.dataclass
 class Module:
-    def __init__(
-        self, path: Optional[str], docstring: Optional[str], attrs: Mapping[str, "Node"]
-    ) -> None:
-        self.path = path
-        self.docstring = docstring
-        self.attrs = attrs
+    path: Optional[str]
+    docstring: Optional[str]
+    attrs: Mapping[str, "Node"]
 
     def has_docstring(self) -> bool:
         return self.docstring is not None
 
 
+@dataclasses.dataclass
 class Class:
-    def __init__(
-        self,
-        bases: Sequence[ast.expr],
-        docstring: Optional[str],
-        attrs: Mapping[str, "NonModuleNode"],
-    ) -> None:
-        self.bases = bases
-        self.docstring = docstring
-        self.attrs = attrs
+    bases: Sequence[ast.expr]
+    docstring: Optional[str]
+    attrs: Mapping[str, "NonModuleNode"]
 
     def has_docstring(self) -> bool:
         if self.docstring is not None:
@@ -98,18 +88,12 @@ class Class:
         return isinstance(init, Function) and init.has_docstring()
 
 
+@dataclasses.dataclass
 class FunctionSignature:
-    def __init__(
-        self,
-        args: ast.arguments,
-        returns: Optional[ast.expr],
-        decorator_list: Sequence[ast.expr],
-        docstring: Optional[str],
-    ) -> None:
-        self.args = args
-        self.returns = returns
-        self.decorator_list = decorator_list
-        self.docstring = docstring
+    args: ast.arguments
+    returns: Optional[ast.expr]
+    decorator_list: Sequence[ast.expr]
+    docstring: Optional[str]
 
     def has_decorator(self, name: str) -> bool:
         return any(
@@ -118,43 +102,39 @@ class FunctionSignature:
         )
 
 
+@dataclasses.dataclass
 class Function:
-    def __init__(self, async_: bool, signatures: Sequence[FunctionSignature]) -> None:
-        self.async_ = async_
-        self.signatures = signatures
+    async_: bool
+    signatures: Sequence[FunctionSignature]
 
     def has_docstring(self) -> bool:
         return any(signature.docstring is not None for signature in self.signatures)
 
 
+@dataclasses.dataclass
 class Variable:
-    def __init__(
-        self, annotation: Optional[ast.expr], docstring: Optional[str]
-    ) -> None:
-        self.annotation = annotation
-        self.docstring = docstring
+    annotation: Optional[ast.expr]
+    docstring: Optional[str]
 
     def has_docstring(self) -> bool:
         return self.docstring is not None
 
 
+@dataclasses.dataclass
 class Import:
-    def __init__(self, module: str, aliased: bool) -> None:
-        self.module = module
-        self.aliased = aliased
+    module: str
+    aliased: bool
 
     def has_docstring(self) -> bool:
         return False
 
 
+@dataclasses.dataclass
 class ImportFrom:
-    def __init__(
-        self, name: str, module: Optional[str], level: int, aliased: bool
-    ) -> None:
-        self.name = name
-        self.module = module
-        self.level = level
-        self.aliased = aliased
+    name: str
+    module: Optional[str]
+    level: int
+    aliased: bool
 
     def has_docstring(self) -> bool:
         return False

commit 9e62d688cda0c872267abee157c98da7dff023e7

drgndoc: use NamedTuple for {Bound,Resolved}Node to fix mypy error
Since we converted to dataclasses, we get the following error from mypy:

  error: Cannot use a covariant type variable as a parameter

The easiest fix is apparently making the type read-only.

Fixes: e404b6330c17 ("drgndoc: use dataclasses")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

docs/exts/drgndoc/namespace.py
@@ -1,9 +1,17 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-import dataclasses
 import itertools
-from typing import Generic, Iterator, List, Mapping, Sequence, TypeVar, Union
+from typing import (
+    Generic,
+    Iterator,
+    List,
+    Mapping,
+    NamedTuple,
+    Sequence,
+    TypeVar,
+    Union,
+)
 
 from drgndoc.parse import (
     Class,
@@ -19,14 +27,12 @@ from drgndoc.parse import (
 NodeT_co = TypeVar("NodeT_co", bound=Node, covariant=True)
 
 
-@dataclasses.dataclass
-class BoundNode(Generic[NodeT_co]):
+class BoundNode(NamedTuple, Generic[NodeT_co]):
     name: str
     node: NodeT_co
 
 
-@dataclasses.dataclass
-class ResolvedNode(Generic[NodeT_co]):
+class ResolvedNode(NamedTuple, Generic[NodeT_co]):
     modules: Sequence[BoundNode[Module]]
     classes: Sequence[BoundNode[Class]]
     name: str