osandov/drgn

commit 75c3679147a513dc2114324ae80c945766a3d4de

Rewrite drgn core in C
The current mixed Python/C implementation works well, but it has a
couple of important limitations:

- It's too slow for some common use cases, like iterating over large
  data structures.
- It can't be reused in utilities written in other languages.

This replaces the internals with a new library written in C, libdrgn. It
includes Python bindings with mostly the same public interface as
before, with some important improvements:

- Types are now represented by a single Type class rather than the messy
  polymorphism in the Python implementation.
- Qualifiers are a bitmask instead of a set of strings.
- Bit fields are not considered a separate type.
- The lvalue/rvalue terminology is replaced with reference/value.
- Structure, union, and array values are better supported.
- Function objects are supported.
- Program distinguishes between lookups of variables, constants, and
  functions.

The C rewrite is about 6x as fast as the original Python when using the
Python bindings, and about 8x when using the C API directly.

Currently, the exposed API in C is fairly conservative. In the future,
the memory reader, type index, and object index APIs will probably be
exposed for more flexibility.

libdrgn/object.h
@@ -1,0 +1,357 @@
+// Copyright 2018-2019 - Omar Sandoval
+// SPDX-License-Identifier: GPL-3.0+
+
+/**
+ * @file
+ *
+ * Object internals.
+ *
+ * See @ref ObjectInternals.
+ */
+
+#ifndef DRGN_OBJECT_H
+#define DRGN_OBJECT_H
+
+#include <stdint.h>
+
+#include "drgn.h"
+#include "type.h"
+
+/**
+ * @ingroup Internals
+ *
+ * @defgroup ObjectInternals Objects
+ *
+ * Object internals.
+ *
+ * This provides the language-agnostic part of operator implementations. The
+ * operators have defined behavior for various cases where C is undefined or
+ * implementation-defined (e.g., signed arithmetic is modular, signed bitwise
+ * operators operate on the two's complement representation, right shifts are
+ * arithmetic).
+ *
+ * @{
+ */
+
+/** Return an object's buffer. */
+#define drgn_object_buffer(obj) ({					\
+	__auto_type _obj = (obj);					\
+									\
+	(drgn_buffer_object_is_inline(_obj) ? _obj->value.ibuf :	\
+	 _obj->value.bufp);						\
+})
+
+/**
+ * Get the truthiness of an object.
+ *
+ * For a signed, unsigned, or floating-point value, this is true iff the value
+ * is non-zero. For structures and unions, this is true iff it is true for any
+ * of its members. For arrays, this is true iff it is true for any of its
+ * elements.
+ */
+struct drgn_error *drgn_object_truthiness(const struct drgn_object *obj,
+					  bool *ret);
+
+/**
+ * Type of an object.
+ *
+ * This is used to contain the types of operands and operator results.
+ */
+struct drgn_object_type {
+	/** See @ref drgn_qualified_type::type. */
+	struct drgn_type *type;
+	/** See @ref drgn_qualified_type::qualifiers. */
+	enum drgn_qualifiers qualifiers;
+	/**
+	 * Cached underlying type of @c type.
+	 *
+	 * See @ref drgn_underlying_type().
+	 */
+	struct drgn_type *underlying_type;
+	/**
+	 * If the object is a bit field, the size of the field in bits.
+	 * Otherwise, 0.
+	 */
+	uint64_t bit_field_size;
+};
+
+/** Get the @ref drgn_object_type of a @ref drgn_object. */
+static inline struct drgn_object_type
+drgn_object_type(const struct drgn_object *obj)
+{
+	return (struct drgn_object_type){
+		.type = obj->type,
+		.qualifiers = obj->qualifiers,
+		.underlying_type = drgn_underlying_type(obj->type),
+		.bit_field_size = obj->is_bit_field ? obj->bit_size : 0,
+	};
+}
+
+/** Convert a @ref drgn_object_type to a @ref drgn_qualified_type. */
+static inline struct drgn_qualified_type
+drgn_object_type_qualified(const struct drgn_object_type *type)
+{
+	return (struct drgn_qualified_type){
+		.type = type->type,
+		.qualifiers = type->qualifiers,
+	};
+}
+
+/**
+ * Reinitialize the fields of a @ref drgn_object, excluding the program and
+ * value.
+ */
+static inline void drgn_object_reinit(struct drgn_object *obj,
+				      const struct drgn_object_type *type,
+				      enum drgn_object_kind kind,
+				      uint64_t bit_size, bool is_reference)
+{
+	drgn_object_deinit(obj);
+	obj->type = type->type;
+	obj->qualifiers = type->qualifiers;
+	obj->kind = kind;
+	obj->bit_size = bit_size;
+	obj->is_bit_field = type->bit_field_size != 0;
+	obj->is_reference = is_reference;
+}
+
+/**
+ * Get the @ref drgn_object_kind and size in bits for an object given its type.
+ */
+struct drgn_error *
+drgn_object_type_kind_and_size(const struct drgn_object_type *type,
+			       enum drgn_object_kind *kind_ret,
+			       uint64_t *bit_size_ret);
+
+/** Prepare to reinitialize an object. */
+struct drgn_error *
+drgn_object_set_common(struct drgn_qualified_type qualified_type,
+		       uint64_t bit_field_size,
+		       struct drgn_object_type *type_ret,
+		       enum drgn_object_kind *kind_ret, uint64_t *bit_size_ret);
+
+/**
+ * Sanity check that the given bit size and bit field size are valid for the
+ * given kind of object.
+ */
+struct drgn_error *sanity_check_object(enum drgn_object_kind kind,
+				       uint64_t bit_field_size,
+				       uint64_t bit_size);
+
+/**
+ * Convert a @ref drgn_byte_order to a boolean.
+ *
+ * @return @c true if the byte order is little endian, @c false if the byte
+ * order is big endian.
+ */
+struct drgn_error *
+drgn_byte_order_to_little_endian(struct drgn_program *prog,
+				 enum drgn_byte_order byte_order, bool *ret);
+
+/**
+ * Binary operator implementation.
+ *
+ * Operator implementations with this type convert @p lhs and @p rhs to @p type,
+ * apply the operator, and store the result in @p res.
+ *
+ * @param[out] res Operator result. May be the same as @p lhs and/or @p rhs.
+ * @param[in] type Result type.
+ * @param[in] lhs Operator left hand side.
+ * @param[in] rhs Operator right hand side.
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+typedef struct drgn_error *
+drgn_binary_op_impl(struct drgn_object *res,
+		    const struct drgn_object_type *type,
+		    const struct drgn_object *lhs,
+		    const struct drgn_object *rhs);
+/**
+ * Shift operator implementation.
+ *
+ * Operator implementations with this type convert @p lhs to @p lhs_type and @p
+ * rhs to @p rhs_type and store the result in @p res.
+ *
+ * @param[out] res Operator result. May be the same as @p lhs and/or @p rhs.
+ * @param[in] lhs Operator left hand side.
+ * @param[in] lhs_type Type of left hand side and result.
+ * @param[in] rhs Operator right hand side.
+ * @param[in] rhs_type Type of right hand side.
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+typedef struct drgn_error *
+drgn_shift_op_impl(struct drgn_object *res,
+		   const struct drgn_object *lhs,
+		   const struct drgn_object_type *lhs_type,
+		   const struct drgn_object *rhs,
+		   const struct drgn_object_type *rhs_type);
+
+/**
+ * Unary operator implementation.
+ *
+ * Operator implementations with this type convert @p obj to @p type and store
+ * the result in @p res.
+ *
+ * @param[out] res Operator result. May be the same as @p obj.
+ * @param[in] type Result type.
+ * @param[in] obj Operand.
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+typedef struct drgn_error *
+drgn_unary_op_impl(struct drgn_object *res,
+		   const struct drgn_object_type *type,
+		   const struct drgn_object *obj);
+
+/**
+ * Implement addition for signed, unsigned, and floating-point objects.
+ *
+ * Integer results are reduced modulo 2^width.
+ */
+drgn_binary_op_impl drgn_op_add_impl;
+/**
+ * Implement subtraction for signed, unsigned, and floating-point objects.
+ *
+ * Integer results are reduced modulo 2^width.
+ */
+drgn_binary_op_impl drgn_op_sub_impl;
+/**
+ * Implement multiplication for signed, unsigned, and floating-point objects.
+ *
+ * Integer results are reduced modulo 2^width.
+ */
+drgn_binary_op_impl drgn_op_mul_impl;
+/**
+ * Implement division for signed, unsigned, and floating-point objects.
+ *
+ * Integer results are truncated towards zero. A @ref DRGN_ERROR_ZERO_DIVISION
+ * error is returned if @p rhs is zero.
+ */
+drgn_binary_op_impl drgn_op_div_impl;
+/**
+ * Implement modulo for signed and unsigned objects.
+ *
+ * The result has the sign of the dividend. A @ref DRGN_ERROR_ZERO_DIVISION
+ * error is returned if @p rhs is zero.
+ */
+drgn_binary_op_impl drgn_op_mod_impl;
+/**
+ * Implement left shift for signed and unsigned objects.
+ *
+ * For signed integers, this acts on the two's complement representation. The
+ * result is reduced modulo 2^width. In particular, if @p rhs is greater than
+ * the width of the result, then the result is zero. An error is returned if @p
+ * rhs is negative.
+ */
+drgn_shift_op_impl drgn_op_lshift_impl;
+/**
+ * Implement right shift for signed and unsigned objects.
+ *
+ * For signed integers, this is an arithmetic shift. For unsigned integers, it
+ * is logical. The result is reduced modulo 2^width. In particular, if @p rhs is
+ * greater than the width of the result, then the result is zero. An error is
+ * returned if @p rhs is negative.
+ */
+drgn_shift_op_impl drgn_op_rshift_impl;
+/**
+ * Implement bitwise and for signed and unsigned objects.
+ *
+ * For signed integers, this acts on the two's complement representation.
+ */
+drgn_binary_op_impl drgn_op_and_impl;
+/**
+ * Implement bitwise or for signed and unsigned objects.
+ *
+ * For signed integers, this acts on the two's complement representation.
+ */
+drgn_binary_op_impl drgn_op_or_impl;
+/**
+ * Implement bitwise xor for signed and unsigned objects.
+ *
+ * For signed integers, this acts on the two's complement representation.
+ */
+drgn_binary_op_impl drgn_op_xor_impl;
+/**
+ * Implement the unary plus operator for signed, unsigned, and floating-point
+ * objects.
+ *
+ * This converts @p obj without otherwise changing the value.
+ */
+drgn_unary_op_impl drgn_op_pos_impl;
+/**
+ * Implement negation for signed, unsigned, and floating-point objects.
+ *
+ * Integer results are reduced modulo 2^width.
+ */
+drgn_unary_op_impl drgn_op_neg_impl;
+/**
+ * Implement bitwise negation for signed and unsigned objects.
+ *
+ * For signed integers, this acts on the two's complement representation.
+ */
+drgn_unary_op_impl drgn_op_not_impl;
+
+/**
+ * Implement object comparison for signed, unsigned, and floating-point objects.
+ *
+ * This converts @p lhs and @p rhs to @p type before comparing.
+ */
+struct drgn_error *drgn_op_cmp_impl(const struct drgn_object *lhs,
+				    const struct drgn_object *rhs,
+				    const struct drgn_object_type *type,
+				    int *ret);
+
+/**
+ * Implement object comparison for pointers and reference buffer objects.
+ *
+ * When comparing reference buffer objects, their address is used.
+ */
+struct drgn_error *drgn_op_cmp_pointers(const struct drgn_object *lhs,
+					const struct drgn_object *rhs,
+					int *ret);
+
+/**
+ * Implement pointer arithmetic.
+ *
+ * This converts @p ptr to @p type, adds or subtracts
+ * <tt>index * referenced_size</tt>, and stores the result in @p res.
+ *
+ * @param[out] res Operator result. May be the same as @p ptr or @p index.
+ * @param[in] type Result type.
+ * @param[in] referenced_size Size of the object pointed to by @p ptr.
+ * @param[in] negate Subtract @p index instead of adding.
+ * @param[in] ptr Pointer.
+ * @param[in] index Value to add to/subtract from pointer.
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+struct drgn_error *drgn_op_add_to_pointer(struct drgn_object *res,
+					  const struct drgn_object_type *type,
+					  uint64_t referenced_size, bool negate,
+					  const struct drgn_object *ptr,
+					  const struct drgn_object *index);
+
+/**
+ * Implement pointer subtraction.
+ *
+ * This stores <tt>(lhs - rhs) / referenced_size</tt> in @p res.
+ *
+ * @param[out] res Operator result. May be the same as @p lhs and/or @p rhs.
+ * @param[in] referenced_size Size of the object pointed to by @p lhs and @p
+ * rhs.
+ * @param[in] type Result type. Must be a signed integer type.
+ * @param[in] lhs Operator left hand side.
+ * @param[in] rhs Operator right hand side.
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+struct drgn_error *drgn_op_sub_pointers(struct drgn_object *res,
+					const struct drgn_object_type *type,
+					uint64_t referenced_size,
+					const struct drgn_object *lhs,
+					const struct drgn_object *rhs);
+
+/** @} */
+
+#endif /* DRGN_OBJECT_H */

commit 6daaaa9f34f1680800fc93d96e8a9a8bf173abca

libdrgn: object: fix array subscript with negative index
drgn_object_subscript() calls drgn_object_fragment() for arrays and
passes a signed bit offset. drgn_object_fragment(), however, takes the
bit_offset as uint64_t. It then converts this wrapped bit offset to a
byte offset, with expectedly bogus results. Fix it by making
drgn_object_fragment() take bit_offset as a int64_t.

Fixes: 75c3679147a5 ("Rewrite drgn core in C")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/drgn.h
@@ -2624,7 +2624,7 @@ struct drgn_error *drgn_object_copy(struct drgn_object *res,
 struct drgn_error *drgn_object_fragment(struct drgn_object *res,
 					const struct drgn_object *obj,
 					struct drgn_qualified_type qualified_type,
-					uint64_t bit_offset,
+					int64_t bit_offset,
 					uint64_t bit_field_size);
 
 /**

libdrgn/object.c
@@ -570,14 +570,16 @@ struct drgn_error *
 drgn_object_fragment_internal(struct drgn_object *res,
 			      const struct drgn_object *obj,
 			      const struct drgn_object_type *type,
-			      uint64_t bit_offset, uint64_t bit_field_size)
+			      int64_t bit_offset, uint64_t bit_field_size)
 {
 	struct drgn_error *err;
 
 	SWITCH_ENUM(obj->kind) {
 	case DRGN_OBJECT_VALUE: {
 		uint64_t bit_end;
-		if (__builtin_add_overflow(bit_offset, type->bit_size, &bit_end)
+		if (bit_offset < 0
+		    || __builtin_add_overflow(bit_offset, type->bit_size,
+					      &bit_end)
 		    || bit_end > obj->bit_size) {
 			return drgn_error_create(DRGN_ERROR_OUT_OF_BOUNDS,
 						 "out of bounds of value");
@@ -603,14 +605,19 @@ drgn_object_fragment_internal(struct drgn_object *res,
 		return drgn_object_set_from_buffer_internal(res, type, buf,
 							    bit_offset);
 	}
-	case DRGN_OBJECT_REFERENCE:
+	case DRGN_OBJECT_REFERENCE: {
 		// obj->bit_offset + bit_offset can overflow, so apply the
 		// byte-aligned part of bit_offset now.
-		return drgn_object_set_reference_internal(res, type,
-							  obj->address
-							  + (bit_offset / 8),
+		//
+		// / and % truncate towards 0. Here, we want to truncate towards
+		// negative infinity. We can accomplish that by replacing "/ 8"
+		// with an arithmetic shift ">> 3" and "% 8" with "& 7".
+		uint64_t address = obj->address + (bit_offset >> 3);
+		bit_offset &= 7;
+		return drgn_object_set_reference_internal(res, type, address,
 							  obj->bit_offset
-							  + (bit_offset % 8));
+							  + bit_offset);
+	}
 	case DRGN_OBJECT_ABSENT:
 		return &drgn_error_object_absent;
 	default:
@@ -621,7 +628,7 @@ drgn_object_fragment_internal(struct drgn_object *res,
 LIBDRGN_PUBLIC struct drgn_error *
 drgn_object_fragment(struct drgn_object *res, const struct drgn_object *obj,
 		     struct drgn_qualified_type qualified_type,
-		     uint64_t bit_offset, uint64_t bit_field_size)
+		     int64_t bit_offset, uint64_t bit_field_size)
 {
 	struct drgn_error *err;
 	if (drgn_object_program(res) != drgn_object_program(obj)) {

libdrgn/object.h
@@ -214,7 +214,7 @@ struct drgn_error *
 drgn_object_fragment_internal(struct drgn_object *res,
 			      const struct drgn_object *obj,
 			      const struct drgn_object_type *type,
-			      uint64_t bit_offset, uint64_t bit_field_size);
+			      int64_t bit_offset, uint64_t bit_field_size);
 
 /**
  * Binary operator implementation.

tests/test_object.py
@@ -1787,6 +1787,16 @@ class TestGenericOperators(MockProgramTestCase):
         obj = Object(self.prog, "int", value=0)
         self.assertRaises(TypeError, obj.__getitem__, 0)
 
+    def test_negative_subscript(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(obj[-1], Object(self.prog, "int", address=0xFFFEFFFC))
+
+        obj = arr.read_()
+        self.assertRaisesRegex(OutOfBoundsError, "out of bounds", obj.__getitem__, -1)
+
     def test_cast_primitive_value(self):
         obj = Object(self.prog, "long", value=2**32 + 1)
         self.assertIdentical(cast("int", obj), Object(self.prog, "int", value=1))