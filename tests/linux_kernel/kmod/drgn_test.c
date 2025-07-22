osandov/drgn

commit ea121af2ad7873078017b4d4551a62b4cdf4e54d

Add Linux kernel helpers
Beyond providing the raw interface for accessing objects, drgn should
provide helpers for inspecting programs. This introduces the first set
of helpers for the Linux kernel. In the future, it would be cool to have
helpers for common libraries like libstd++.

drgn/helpers/kernel/percpu.py
@@ -1,0 +1,41 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel per-CPU helpers
+
+This module provides helpers for working with per-CPU allocations from
+"linux/percpu.h" and per-CPU counters from "linux/percpu_counter.h".
+"""
+
+from drgn.helpers.kernel.cpumask import for_each_online_cpu
+
+
+__all__ = [
+    'per_cpu_ptr',
+    'percpu_counter_sum',
+]
+
+
+def per_cpu_ptr(ptr, cpu):
+    """
+    type *per_cpu_ptr(type __percpu *ptr, int cpu)
+
+    Return the per-CPU pointer for a given CPU.
+    """
+    offset = ptr.program_['__per_cpu_offset'][cpu].value_()
+    return ptr.program_.object(ptr.type_.type, ptr.value_() + offset)
+
+
+def percpu_counter_sum(fbc):
+    """
+    s64 percpu_counter_sum(struct percpu_counter *fbc)
+
+    Return the sum of a per-CPU counter.
+    """
+    ret = fbc.count.value_()
+    ptr = fbc.counters
+    for cpu in for_each_online_cpu(fbc.program_):
+        pcount = per_cpu_ptr(ptr, cpu)
+        ret += pcount.value_()
+    return ret

drgn/helpers/__init__.py
@@ -1,0 +1,9 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Common program helpers
+
+This package contains subpackages which provide helpers for working with
+particular types of programs.
+"""

drgn/helpers/kernel/__init__.py
@@ -1,0 +1,35 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel helpers
+
+This package contains several modules for working with data structures and
+subsystems in the Linux kernel. The helpers are available from the individual
+modules in which they are defined and from this top-level package. E.g., the
+following are both valid:
+
+>>> from drgn.helpers.kernel.list import list_for_each_entry
+>>> from drgn.helpers.kernel import list_for_each_entry
+
+Alternatively, in interactive mode, the following is the most convenient:
+
+>>> from drgn.helpers.kernel import *
+"""
+
+from drgn.helpers.kernel.cpumask import *
+from drgn.helpers.kernel.idr import *
+from drgn.helpers.kernel.list import *
+from drgn.helpers.kernel.percpu import *
+from drgn.helpers.kernel.radixtree import *
+from drgn.helpers.kernel.rbtree import *
+
+
+__all__ = (
+    cpumask.__all__ +
+    idr.__all__ +
+    list.__all__ +
+    percpu.__all__ +
+    radixtree.__all__ +
+    rbtree.__all__
+)

drgn/helpers/kernel/cpumask.py
@@ -1,0 +1,57 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel CPU mask helpers
+
+This module provides helpers for working with CPU masks from "linux/cpumask.h".
+"""
+
+__all__ = [
+    'for_each_cpu',
+    'for_each_possible_cpu',
+    'for_each_online_cpu',
+    'for_each_present_cpu',
+]
+
+
+def for_each_cpu(mask):
+    """
+    for_each_cpu(struct cpumask)
+
+    Return an iterator over all of the CPUs in the given mask, as ints.
+    """
+    bits = mask.bits
+    word_bits = 8 * bits.type_.type.sizeof()
+    for i in range(bits.type_.size):
+        word = bits[i].value_()
+        for j in range(word_bits):
+            if word & (1 << j):
+                yield (word_bits * i) + j
+
+
+def for_each_possible_cpu(prog):
+    """
+    for_each_possible_cpu()
+
+    Return an iterator over all possible CPUs, as ints.
+    """
+    return for_each_cpu(prog['__cpu_possible_mask'])
+
+
+def for_each_online_cpu(prog):
+    """
+    for_each_online_cpu()
+
+    Return an iterator over all online CPUs, as ints.
+    """
+    return for_each_cpu(prog['__cpu_online_mask'])
+
+
+def for_each_present_cpu(prog):
+    """
+    for_each_present_cpu()
+
+    Return an iterator over all present CPUs, as ints.
+    """
+    return for_each_cpu(prog['__cpu_present_mask'])

drgn/helpers/kernel/idr.py
@@ -1,0 +1,49 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel IDR helpers
+
+This module provides helpers for working with the IDR data structure in
+"linux/idr.h". An IDR provides a mapping from an ID to a pointer. This
+currently only supports Linux v4.11+; before this, IDRs were not based on radix
+trees.
+"""
+
+from drgn.helpers.kernel.radixtree import radix_tree_for_each, radix_tree_lookup
+
+
+__all__ = [
+    'idr_find',
+    'idr_for_each',
+]
+
+
+def idr_find(idr, id):
+    """
+    void *idr_find(struct idr *, unsigned long id)
+
+    Look up the entry with the given id in an IDR. If it is not found, this
+    returns a NULL object.
+    """
+    # idr_base was added in v4.16.
+    try:
+        id -= idr.idr_base
+    except AttributeError:
+        pass
+    return radix_tree_lookup(idr.idr_rt, id)
+
+
+def idr_for_each(idr):
+    """
+    idr_for_each(struct idr *)
+
+    Return an iterator over all of the entries in an IDR. The generated values
+    are (index, entry) tuples.
+    """
+    try:
+        base = idr.idr_base.value_()
+    except AttributeError:
+        base = 0
+    for index, entry in radix_tree_for_each(idr.idr_rt):
+        yield index + base, entry

drgn/helpers/kernel/list.py
@@ -1,0 +1,96 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel linked list helpers
+
+This module provides helpers for working with the doubly-linked list
+implementations in "linux/list.h".
+"""
+
+__all__ = [
+    'list_empty',
+    'list_is_singular',
+    'list_for_each',
+    'list_for_each_entry',
+    'hlist_empty',
+    'hlist_for_each',
+    'hlist_for_each_entry',
+]
+
+
+def list_empty(head):
+    """
+    bool list_empty(struct list_head *)
+
+    Return whether a list is empty.
+    """
+    head = head.read_once_()
+    return head.next == head
+
+
+def list_is_singular(head):
+    """
+    bool list_is_singular(struct list_head *)
+
+    Return whether a list has only one element.
+    """
+    head = head.read_once_()
+    next = head.next
+    return next != head and next == head.prev
+
+
+def list_for_each(head):
+    """
+    list_for_each(struct list_head *)
+
+    Return an iterator over all of the nodes in a list.
+    """
+    head = head.read_once_()
+    pos = head.next.read_once_()
+    while pos != head:
+        yield pos
+        pos = pos.next.read_once_()
+
+
+def list_for_each_entry(type, head, member):
+    """
+    list_for_each_entry(type, struct list_head *, member)
+
+    Return an iterator over all of the entries in a list, given the type of the
+    entry and the struct list_head member in that type.
+    """
+    for pos in list_for_each(head):
+        yield pos.container_of_(type, member)
+
+
+def hlist_empty(head):
+    """
+    bool hlist_empty(struct hlist_head *)
+
+    Return whether a hash list is empty.
+    """
+    return not head.first
+
+
+def hlist_for_each(head):
+    """
+    hlist_for_each(struct hlist_head *)
+
+    Return an iterator over all of the nodes in a hash list.
+    """
+    pos = head.first.read_once_()
+    while pos:
+        yield pos
+        pos = pos.next.read_once_()
+
+
+def hlist_for_each_entry(type, head, member):
+    """
+    hlist_for_each_entry(type, struct hlist_head *, member)
+
+    Return an iterator over all of the entries in a has list, given the type of
+    the entry and the struct hlist_node member in that type.
+    """
+    for pos in hlist_for_each(head):
+        yield pos.container_of_(type, member)

drgn/helpers/kernel/radixtree.py
@@ -1,0 +1,62 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel radix tree helpers
+
+This module provides helpers for working with radix trees from
+"linux/radix-tree.h". Note that it does not support multiorder radix trees yet.
+"""
+
+__all__ = [
+    'radix_tree_lookup',
+    'radix_tree_for_each',
+]
+
+_RADIX_TREE_ENTRY_MASK = 3
+_RADIX_TREE_INTERNAL_NODE = 1
+
+
+def _is_internal_node(node):
+    return (node.value_() & _RADIX_TREE_ENTRY_MASK) == _RADIX_TREE_INTERNAL_NODE
+
+
+def _internal_node(node):
+    return node.program_.object(node.type_,
+                                value=node.value_() & ~_RADIX_TREE_INTERNAL_NODE)
+
+
+def radix_tree_lookup(root, index):
+    """
+    void *radix_tree_lookup(struct radix_tree_root *, unsigned long index)
+
+    Look up the entry at a given index in a radix tree. If it is not found,
+    this returns a NULL object.
+    """
+    node = root.rnode.read_once_()
+    RADIX_TREE_MAP_MASK = node.type_.type.typeof('slots').size - 1
+    while True:
+        if not _is_internal_node(node):
+            break
+        parent = _internal_node(node)
+        offset = (index >> parent.shift) & RADIX_TREE_MAP_MASK
+        node = parent.slots[offset].cast_(parent.type_).read_once_()
+    return node.cast_('void *')
+
+
+def radix_tree_for_each(root):
+    """
+    radix_tree_for_each(struct radix_tree_root *)
+
+    Return an iterator over all of the entries in a radix tree. The generated
+    values are (index, entry) tuples.
+    """
+    def aux(node, index):
+        if _is_internal_node(node):
+            parent = _internal_node(node)
+            for i, slot in enumerate(parent.slots):
+                yield from aux(slot.cast_(parent.type_).read_once_(),
+                               index + (i << parent.shift.value_()))
+        elif node:
+            yield index, node.cast_('void *')
+    yield from aux(root.rnode.read_once_(), 0)

drgn/helpers/kernel/rbtree.py
@@ -1,0 +1,184 @@
+# Copyright 2018 - Omar Sandoval
+# SPDX-License-Identifier: GPL-3.0+
+
+"""
+Linux kernel red-black tree helpers
+
+This module provides helpers for working with red-black trees from
+"linux/rbtree.h"
+"""
+
+__all__ = [
+    'RB_EMPTY_NODE',
+    'rb_parent',
+    'rb_first',
+    'rb_last',
+    'rb_next',
+    'rb_prev',
+    'rbtree_inorder_for_each',
+    'rbtree_inorder_for_each_entry',
+    'rb_find',
+]
+
+
+def RB_EMPTY_NODE(node):
+    """
+    bool RB_EMPTY_NODE(struct rb_node *)
+
+    Return whether a red-black tree node is empty, i.e., not inserted in a
+    tree.
+    """
+    return node.__rb_parent_color.value_() == node.value_()
+
+
+def rb_parent(node):
+    """
+    struct rb_node *rb_parent(struct rb_node *)
+
+    Return the parent node of a red-black tree node.
+    """
+    return node.program_.object(node.type_,
+                                value=node.__rb_parent_color.value_() & ~3)
+
+
+def rb_first(root):
+    """
+    struct rb_node *rb_first(struct rb_root *)
+
+    Return the first node (in sort order) in a red-black tree, or a NULL object
+    if the tree is empty.
+    """
+    node = root.rb_node.read_once_()
+    if not node:
+        return node
+    while True:
+        next = node.rb_left.read_once_()
+        if not next:
+            return node
+        node = next
+
+
+def rb_last(root):
+    """
+    struct rb_node *rb_last(struct rb_root *)
+
+    Return the last node (in sort order) in a red-black tree, or a NULL object
+    if the tree is empty.
+    """
+    node = root.rb_node.read_once_()
+    if not node:
+        return node
+    while True:
+        next = node.rb_right.read_once_()
+        if not next:
+            return node
+        node = next
+
+
+def rb_next(node):
+    """
+    struct rb_node *rb_next(struct rb_node *)
+
+    Return the next node (in sort order) after a red-black node, or a NULL
+    object if the node is the last node in the tree or is empty.
+    """
+    node = node.read_once_()
+
+    if RB_EMPTY_NODE(node):
+        return node.program_.null(node.type_)
+
+    next = node.rb_right.read_once_()
+    if next:
+        node = next
+        while True:
+            next = node.rb_left.read_once_()
+            if not next:
+                return node
+            node = next
+
+    parent = rb_parent(node).read_once_()
+    while parent and node == parent.rb_right:
+        node = parent
+        parent = rb_parent(node).read_once_()
+    return parent
+
+
+def rb_prev(node):
+    """
+    struct rb_node *rb_prev(struct rb_node *)
+
+    Return the previous node (in sort order) before a red-black node, or a NULL
+    object if the node is the first node in the tree or is empty.
+    """
+    node = node.read_once_()
+
+    if RB_EMPTY_NODE(node):
+        return node.program_.null(node.type_)
+
+    next = node.rb_left.read_once_()
+    if next:
+        node = next
+        while True:
+            next = node.rb_right.read_once_()
+            if not next:
+                return node
+            node = next
+
+    parent = rb_parent(node).read_once_()
+    while parent and node == parent.rb_left:
+        node = parent
+        parent = rb_parent(node).read_once_()
+    return parent
+
+
+def rbtree_inorder_for_each(root):
+    """
+    rbtree_inorder_for_each(struct rb_root *)
+
+    Return an iterator over all of the nodes in a red-black tree, in sort
+    order.
+    """
+    def aux(node):
+        if node:
+            yield from aux(node.rb_left.read_once_())
+            yield node
+            yield from aux(node.rb_right.read_once_())
+    yield from aux(root.rb_node.read_once_())
+
+
+def rbtree_inorder_for_each_entry(type, root, member):
+    """
+    rbtree_inorder_for_each_entry(type, struct rb_root *, member)
+
+    Return an iterator over all of the entries in a red-black tree, given the
+    type of the entry and the struct list_head member in that type. The entries
+    are returned in sort order.
+    """
+    for node in rbtree_inorder_for_each(root):
+        yield node.container_of_(type, member)
+
+
+def rb_find(type, root, member, key, cmp):
+    """
+    type *rb_find(type, struct rb_root *, member,
+                  key_type key, int (*cmp)(key_type, type *))
+
+    Find an entry in a red-black tree, given a key and a comparator function
+    which takes the key and an entry. The comparator should return -1 if the
+    key is less than the entry, 1 if it is greater than the entry, or 0 if it
+    matches the entry. This returns a NULL object if no entry matches the key.
+
+    Note that this function does not have an analogue in the Linux kernel
+    source code, as tree searches are all open-coded.
+    """
+    node = root.rb_node.read_once_()
+    while node:
+        entry = node.container_of_(type, member)
+        ret = cmp(key, entry)
+        if ret < 0:
+            node = node.rb_left.read_once_()
+        elif ret > 0:
+            node = node.rb_right.read_once_()
+        else:
+            return entry
+    return node

commit 7db6b61395a27f95ac92209e0e52655fe8ea8d50

drgn.helpers.linux.percpu: fix percpu_counter_sum() on !SMP
!SMP kernels don't have percpu_counter::counters, only
percpu_counter::count. This was one of our first helpers ever, but it
was never tested until get_task_rss_info() was added, so also add a
test.

Fixes: ea121af2ad78 ("Add Linux kernel helpers")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/percpu.py
@@ -45,7 +45,12 @@ def percpu_counter_sum(fbc: Object) -> int:
     :param fbc: ``struct percpu_counter *``
     """
     ret = fbc.count.value_()
-    ptr = fbc.counters
-    for cpu in for_each_online_cpu(fbc.prog_):
-        ret += per_cpu_ptr(ptr, cpu)[0].value_()
+    try:
+        ptr = fbc.counters
+    except AttributeError:
+        # On !SMP kernels, there's nothing to sum.
+        pass
+    else:
+        for cpu in for_each_online_cpu(fbc.prog_):
+            ret += per_cpu_ptr(ptr, cpu)[0].value_()
     return ret

tests/linux_kernel/helpers/test_percpu.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 from drgn.helpers.linux.cpumask import for_each_possible_cpu
-from drgn.helpers.linux.percpu import per_cpu, per_cpu_ptr
+from drgn.helpers.linux.percpu import per_cpu, per_cpu_ptr, percpu_counter_sum
 from tests.linux_kernel import (
     LinuxKernelTestCase,
     prng32,
@@ -37,3 +37,9 @@ class TestPerCpu(LinuxKernelTestCase):
             self.assertEqual(
                 per_cpu_ptr(self.prog["drgn_test_percpu_dynamic"], cpu)[0], expected
             )
+
+
+@skip_unless_have_test_kmod
+class TestPercpuCounter(LinuxKernelTestCase):
+    def test_percpu_counter_sum(self):
+        self.assertEqual(percpu_counter_sum(self.prog["drgn_test_percpu_counter"]), 13)

tests/linux_kernel/kmod/drgn_test.c
@@ -590,9 +590,11 @@ static void drgn_test_page_pool_exit(void)
 
 DEFINE_PER_CPU(u32, drgn_test_percpu_static);
 u32 __percpu *drgn_test_percpu_dynamic;
+struct percpu_counter drgn_test_percpu_counter;
 
 static int drgn_test_percpu_init(void)
 {
+	int ret;
 	int cpu;
 	u32 static_seed = drgn_test_prng32_seed("PCPU");
 	u32 dynamic_seed = drgn_test_prng32_seed("pcpu");
@@ -607,11 +609,19 @@ static int drgn_test_percpu_init(void)
 		dynamic_seed = drgn_test_prng32(dynamic_seed);
 		*per_cpu_ptr(drgn_test_percpu_dynamic, cpu) = dynamic_seed;
 	}
+
+	ret = percpu_counter_init(&drgn_test_percpu_counter,
+				  10, GFP_KERNEL);
+	if (ret)
+		return ret;
+	percpu_counter_add(&drgn_test_percpu_counter, 3);
+
 	return 0;
 }
 
 static void drgn_test_percpu_exit(void)
 {
+	percpu_counter_destroy(&drgn_test_percpu_counter);
 	free_percpu(drgn_test_percpu_dynamic);
 }
 