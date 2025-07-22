osandov/drgn

commit 84326fa4f76748b83b7540148d8456ccea775385

Add get_task_rss_info helper
Signed-off-by: Yassine Larhrissi <yassine.larhrissi@oracle.com>

drgn/helpers/linux/mm.py
@@ -26,18 +26,20 @@ multiple reasons:
 
 import operator
 import re
-from typing import Callable, Iterator, List, Optional
+from typing import Callable, Iterator, List, NamedTuple, Optional
 
 from _drgn import (
     _linux_helper_direct_mapping_offset,
     _linux_helper_follow_phys,
     _linux_helper_read_vm,
 )
-from drgn import NULL, IntegerLike, Object, ObjectAbsentError, Program, cast
+from drgn import NULL, IntegerLike, Object, ObjectAbsentError, Program, TypeKind, cast
 from drgn.helpers.common.format import decode_enum_type_flags
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.list import list_for_each_entry
 from drgn.helpers.linux.mapletree import mt_for_each, mtree_load
+from drgn.helpers.linux.percpu import percpu_counter_sum
+from drgn.helpers.linux.pid import for_each_task_in_group
 from drgn.helpers.linux.rbtree import rb_find
 
 __all__ = (
@@ -77,6 +79,7 @@ __all__ = (
     "vma_find",
     "vmalloc_to_page",
     "vmalloc_to_pfn",
+    "get_task_rss_info",
     # Generated by scripts/generate_page_flag_getters.py.
     "PageActive",
     "PageChecked",
@@ -1435,3 +1438,88 @@ def in_direct_map(prog: Program, addr: IntegerLike) -> bool:
     start_addr = pfn_to_virt(prog["min_low_pfn"]).value_()
     end_addr = (pfn_to_virt(prog["max_low_pfn"]) + prog["PAGE_SIZE"]).value_()
     return start_addr <= addr < end_addr
+
+
+class TaskRss(NamedTuple):
+    """
+    Represents a task's resident set size in pages. See :func:`get_task_rss_info()`.
+    """
+
+    file: int
+    """Number of resident file pages."""
+    anon: int
+    """Number of resident anonymous pages."""
+    shmem: int
+    """Number of resident shared memory pages."""
+    swap: int
+    """Number of swapped-out anonymous private pages."""
+
+    @property
+    def total(self) -> int:
+        return self.file + self.anon + self.shmem
+
+
+@takes_program_or_default
+def get_task_rss_info(prog: Program, task: Object) -> TaskRss:
+    """
+    Return the task's resident set size (RSS) in pages
+
+    The task's RSS is the number of pages which are currently resident in
+    memory. The RSS values can be broken down into anonymous pages (not bound to
+    any file), file pages (those associated with memory mapped files), and
+    shared memory pages (those which aren't associated with on-disk files, but
+    belonging to shared memory mappings). This function returns a tuple
+    containing each category, but the common behavior is to use the "total"
+    value which sums them up.
+
+    :param task: ``struct task_struct *`` for which to compute RSS
+    :returns: the file, anon, and shmem page values
+    """
+
+    # Kthreads have a NULL mm, simply skip them, returning 0.
+    if not task.mm:
+        return TaskRss(0, 0, 0, 0)
+
+    prog = task.prog_
+    rss_stat = task.mm.rss_stat
+
+    MM_FILEPAGES = prog.constant("MM_FILEPAGES").value_()
+    MM_ANONPAGES = prog.constant("MM_ANONPAGES").value_()
+    MM_SWAPENTS = prog.constant("MM_SWAPENTS").value_()
+
+    # This counter was added in Linux kernel commit eca56ff906bd ("mm, shmem:
+    # add internal shmem resident memory accounting") (in v4.5).
+    try:
+        MM_SHMEMPAGES = prog.constant("MM_SHMEMPAGES").value_()
+    except LookupError:
+        MM_SHMEMPAGES = -1
+
+    if rss_stat.type_.kind == TypeKind.ARRAY:
+        # Since v6.2, f1a7941243c10 ("mm: convert mm's rss stats into
+        # percpu_counter"), the "rss_stat" object is an array of percpu
+        # counters. Simply sum them up!
+        filerss = percpu_counter_sum(rss_stat[MM_FILEPAGES].address_of_())
+        anonrss = percpu_counter_sum(rss_stat[MM_ANONPAGES].address_of_())
+        swapents = percpu_counter_sum(rss_stat[MM_SWAPENTS].address_of_())
+        shmemrss = 0
+        if MM_SHMEMPAGES >= 0:
+            shmemrss = percpu_counter_sum(rss_stat[MM_SHMEMPAGES].address_of_())
+    else:
+        # Prior to this, the "rss_stat" was a structure containing counters that
+        # were cached on each task_struct and periodically updated into the
+        # mm_struct. We start with the counter values from the mm_struct and
+        # then sum up the cached copies from each thread.
+        filerss = rss_stat.count[MM_FILEPAGES].counter.value_()
+        anonrss = rss_stat.count[MM_ANONPAGES].counter.value_()
+        swapents = rss_stat.count[MM_SWAPENTS].counter.value_()
+        if MM_SHMEMPAGES >= 0:
+            shmemrss = rss_stat.count[MM_SHMEMPAGES].counter.value_()
+
+        for gtask in for_each_task_in_group(task, include_self=True):
+            filerss += gtask.rss_stat.count[MM_FILEPAGES].value_()
+            anonrss += gtask.rss_stat.count[MM_ANONPAGES].value_()
+            swapents += gtask.rss_stat.count[MM_SWAPENTS].value_()
+            if MM_SHMEMPAGES >= 0:
+                shmemrss += gtask.rss_stat.count[MM_SHMEMPAGES].value_()
+
+    return TaskRss(filerss, anonrss, shmemrss, swapents)

tests/linux_kernel/helpers/test_mm.py
@@ -5,6 +5,8 @@ import contextlib
 import ctypes
 import mmap
 import os
+from pathlib import Path
+import re
 import struct
 import sys
 import tempfile
@@ -34,6 +36,7 @@ from drgn.helpers.linux.mm import (
     follow_phys,
     for_each_vma,
     for_each_vmap_area,
+    get_task_rss_info,
     page_size,
     page_to_pfn,
     page_to_phys,
@@ -452,3 +455,32 @@ class TestMm(LinuxKernelTestCase):
             proc_totalram = 1024 * int(parts[1])
             page_size = self.prog["PAGE_SIZE"].value_()
             self.assertEqual(totalram_pages(self.prog) * page_size, proc_totalram)
+
+    def test_get_task_rss_info(self):
+        with fork_and_stop() as pid:
+            task = find_task(self.prog, pid)
+            rss_info = get_task_rss_info(self.prog, task)
+
+            page_size = self.prog["PAGE_SIZE"].value_()
+            # Get the relevant RSS counters, converting from kB to pages.
+            stats = {
+                key: int(value) * 1024 // page_size
+                for key, value in re.findall(
+                    r"^(VmRSS|RssAnon|RssFile|RssShmem|VmSwap):\s*([0-9]+)",
+                    Path(f"/proc/{pid}/status").read_text(),
+                    flags=re.MULTILINE,
+                )
+            }
+
+            # The kernel code uses percpu_counter_read_positive(), but the
+            # helper uses percpu_counter_sum() for better accuracy. We need to
+            # account for the deviation.
+            delta = self.prog["percpu_counter_batch"].value_() * os.cpu_count()
+
+            self.assertAlmostEqual(rss_info.file, stats["RssFile"], delta=delta)
+            self.assertAlmostEqual(rss_info.anon, stats["RssAnon"], delta=delta)
+            self.assertAlmostEqual(
+                rss_info.shmem, stats.get("RssShmem", 0), delta=delta
+            )
+            self.assertAlmostEqual(rss_info.swap, stats["VmSwap"], delta=delta)
+            self.assertAlmostEqual(rss_info.total, stats["VmRSS"], delta=delta)

commit c8966444696c45f0a5e00d3005c20f3ac2a8a86e

drgn.helpers.linux.mm: fix get_task_rss_info() on Linux < 6.2 with !SMP
The per-thread rss_stat used before Linux 6.2 is only used if NR_CPUS >=
CONFIG_SPLIT_PTLOCK_CPUS (usually 4). Handle the case where it's absent,
and also fix the test case to fall back when percpu_counter_batch
doesn't exist on !SMP.

Fixes: 84326fa4f767 ("Add get_task_rss_info helper")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/mm.py
@@ -1516,10 +1516,16 @@ def get_task_rss_info(prog: Program, task: Object) -> TaskRss:
             shmemrss = rss_stat.count[MM_SHMEMPAGES].counter.value_()
 
         for gtask in for_each_task_in_group(task, include_self=True):
-            filerss += gtask.rss_stat.count[MM_FILEPAGES].value_()
-            anonrss += gtask.rss_stat.count[MM_ANONPAGES].value_()
-            swapents += gtask.rss_stat.count[MM_SWAPENTS].value_()
+            # Kernel configurations with a small NR_CPUS don't have the
+            # per-thread cache.
+            try:
+                rss_stat = gtask.rss_stat
+            except AttributeError:
+                break
+            filerss += rss_stat.count[MM_FILEPAGES].value_()
+            anonrss += rss_stat.count[MM_ANONPAGES].value_()
+            swapents += rss_stat.count[MM_SWAPENTS].value_()
             if MM_SHMEMPAGES >= 0:
-                shmemrss += gtask.rss_stat.count[MM_SHMEMPAGES].value_()
+                shmemrss += rss_stat.count[MM_SHMEMPAGES].value_()
 
     return TaskRss(filerss, anonrss, shmemrss, swapents)

tests/linux_kernel/helpers/test_mm.py
@@ -13,7 +13,7 @@ import tempfile
 import unittest
 
 from _drgn_util.platform import NORMALIZED_MACHINE_NAME
-from drgn import NULL, FaultError
+from drgn import NULL, FaultError, ObjectNotFoundError
 from drgn.helpers.linux.mm import (
     PFN_PHYS,
     PHYS_PFN,
@@ -475,7 +475,11 @@ class TestMm(LinuxKernelTestCase):
             # The kernel code uses percpu_counter_read_positive(), but the
             # helper uses percpu_counter_sum() for better accuracy. We need to
             # account for the deviation.
-            delta = self.prog["percpu_counter_batch"].value_() * os.cpu_count()
+            try:
+                percpu_counter_batch = self.prog["percpu_counter_batch"].value_()
+            except ObjectNotFoundError:
+                percpu_counter_batch = 32
+            delta = percpu_counter_batch * os.cpu_count()
 
             self.assertAlmostEqual(rss_info.file, stats["RssFile"], delta=delta)
             self.assertAlmostEqual(rss_info.anon, stats["RssAnon"], delta=delta)