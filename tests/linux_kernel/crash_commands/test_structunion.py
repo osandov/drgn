osandov/drgn

commit a5ec52188af839274c62ae755d8cc56e1b692b2b

crash: port * command and implicit struct/union
These unfortunately require some ugly hacks in the command running code.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/commands/crash.py
@@ -11,15 +11,18 @@ import shutil
 import subprocess
 import sys
 import textwrap
-from typing import Any, Dict, FrozenSet, List, Literal, Optional, Set, Tuple
+from typing import Any, FrozenSet, List, Literal, Optional, Set, Tuple
 
 from _drgn_util.typingutils import copy_func_params
-from drgn import Object, Program, ProgramFlags
+from drgn import Object, Program, ProgramFlags, Type, TypeKind
 from drgn.commands import (
+    _SHELL_TOKEN_REGEX,
     CommandFuncDecorator,
     CommandNamespace,
+    CommandNotFoundError,
     CustomCommandFuncDecorator,
     DrgnCodeBuilder,
+    _unquote,
     command,
     custom_command,
 )
@@ -33,6 +36,36 @@ def _pid_or_task(s: str) -> Tuple[Literal["pid", "task"], int]:
         return "pid", int(s)
     except ValueError:
         return "task", int(s, 16)
+
+
+def _guess_type(prog: Program, kind: str, name: str) -> Type:
+    if kind != "union":
+        try:
+            return prog.type("struct " + name)
+        except LookupError:
+            pass
+
+    if kind != "struct":
+        try:
+            return prog.type("union " + name)
+        except LookupError:
+            pass
+
+    # Try a typedef.
+    type = prog.type(name)
+
+    # Make sure it's a typedef of our desired type kind.
+    underlying_type = type
+    while underlying_type.kind == TypeKind.TYPEDEF:
+        underlying_type = underlying_type.type
+    if (kind != "union" and underlying_type.kind == TypeKind.STRUCT) or (
+        kind != "struct" and underlying_type.kind == TypeKind.UNION
+    ):
+        return type
+
+    if kind == "*":
+        kind = "struct or union"
+    raise LookupError(f"{type.type_name()} is not a {kind}")
 
 
 def _find_pager(which: Optional[str] = None) -> Optional[List[str]]:
@@ -65,15 +98,41 @@ class _CrashCommandNamespace(CommandNamespace):
             argparse_types=(("pid_or_task", _pid_or_task),),
         )
 
-    def _run(self, prog: Program, command: str, *, globals: Dict[str, Any]) -> Any:
-        match = re.fullmatch(r"\s*!\s*(.*)", command)
-        if match:
-            args = match.group(1)
+    def _run(self, prog: Program, command: str, **kwargs: Any) -> Any:
+        command = command.lstrip()
+        if command.startswith("!"):
+            args = command[1:].lstrip()
             if args:
                 return subprocess.call(["sh", "-c", "--", args])
             else:
                 return subprocess.call(["sh", "-i"])
-        return super()._run(prog, command, globals=globals)
+
+        if command.startswith("*"):
+            command_name = "*"
+            command_obj = self.lookup(prog, "*")
+            args = command[1:].lstrip()
+        else:
+            match = _SHELL_TOKEN_REGEX.match(command)
+            if not match or match.lastgroup != "WORD":
+                raise SyntaxError("expected command name")
+
+            command_name = _unquote(match.group())
+            try:
+                command_obj = self.lookup(prog, command_name)
+            except CommandNotFoundError as e:
+                try:
+                    # Smuggle the type into the command function.
+                    kwargs["type"] = _guess_type(prog, "*", command_name)
+                except LookupError:
+                    raise e
+                else:
+                    command_name = "*"
+                    command_obj = self.lookup(prog, "*")
+                    args = command
+            else:
+                args = command[match.end() :].lstrip()
+
+        return command_obj.run(prog, command_name, args, **kwargs)
 
     def run(self, prog: Program, command: str, **kwargs: Any) -> Any:
         if prog.config.get("crash_scroll", True):

tests/linux_kernel/crash_commands/test_structunion.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import re
 
 from drgn import offsetof, reinterpret
-from drgn.commands import CommandError
+from drgn.commands import CommandError, CommandNotFoundError
 from tests.linux_kernel import parse_range_list, skip_unless_have_test_kmod
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
 
@@ -413,3 +413,84 @@ class TestUnion(CrashCommandTestCase):
 
     def test_anonymous_struct(self):
         self.assertRaises(LookupError, self.run_crash_command, "union atomic_t")
+
+
+class TestAsterisk(CrashCommandTestCase):
+    def test_struct(self):
+        cmd = self.check_crash_command("*list_head")
+        self.assertIn("struct list_head {", cmd.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "struct list_head"
+        )
+
+    def test_struct_with_space(self):
+        cmd = self.check_crash_command("* list_head")
+        self.assertIn("struct list_head {", cmd.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "struct list_head"
+        )
+
+    @skip_unless_have_test_kmod
+    def test_union(self):
+        cmd = self.check_crash_command("*drgn_test_union")
+        self.assertIn("union drgn_test_union {", cmd.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "union drgn_test_union"
+        )
+
+    def test_struct_typedef(self):
+        cmd = self.check_crash_command("*atomic_t")
+        self.assertIn("} atomic_t", cmd.stdout)
+        self.assertIn('prog.type("atomic_t")', cmd.drgn_option.stdout)
+        self.assertEqual(cmd.drgn_option.globals["type"].type_name(), "atomic_t")
+
+    @skip_unless_have_test_kmod
+    def test_union_typedef(self):
+        cmd = self.check_crash_command("*drgn_test_anonymous_union")
+        self.assertIn("} drgn_test_anonymous_union", cmd.stdout)
+        self.assertIn('prog.type("drgn_test_anonymous_union")', cmd.drgn_option.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "drgn_test_anonymous_union"
+        )
+
+    def test_not_found(self):
+        self.assertRaises(
+            LookupError, self.run_crash_command, "*drgn_test_non_existent"
+        )
+
+
+class TestImplicit(CrashCommandTestCase):
+    def test_struct(self):
+        cmd = self.check_crash_command("list_head")
+        self.assertIn("struct list_head {", cmd.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "struct list_head"
+        )
+
+    @skip_unless_have_test_kmod
+    def test_union(self):
+        cmd = self.check_crash_command("drgn_test_union")
+        self.assertIn("union drgn_test_union {", cmd.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "union drgn_test_union"
+        )
+
+    def test_struct_typedef(self):
+        cmd = self.check_crash_command("atomic_t")
+        self.assertIn("} atomic_t", cmd.stdout)
+        self.assertIn('prog.type("atomic_t")', cmd.drgn_option.stdout)
+        self.assertEqual(cmd.drgn_option.globals["type"].type_name(), "atomic_t")
+
+    @skip_unless_have_test_kmod
+    def test_union_typedef(self):
+        cmd = self.check_crash_command("drgn_test_anonymous_union")
+        self.assertIn("} drgn_test_anonymous_union", cmd.stdout)
+        self.assertIn('prog.type("drgn_test_anonymous_union")', cmd.drgn_option.stdout)
+        self.assertEqual(
+            cmd.drgn_option.globals["type"].type_name(), "drgn_test_anonymous_union"
+        )
+
+    def test_not_found(self):
+        self.assertRaises(
+            CommandNotFoundError, self.run_crash_command, "drgn_test_non_existent"
+        )

drgn/commands/_builtin/crash/structunion.py
@@ -9,11 +9,12 @@ import re
 import sys
 from typing import Any, Iterable, List, Optional, Tuple, Union
 
-from drgn import Object, Program, Type, TypeKind, offsetof, sizeof
+from drgn import Object, Program, offsetof, sizeof
 from drgn.commands import CommandError, argument, drgn_argument
 from drgn.commands.crash import (
     Cpuspec,
     CrashDrgnCodeBuilder,
+    _guess_type,
     crash_command,
     parse_cpuspec,
 )
@@ -25,36 +26,6 @@ from drgn.helpers.linux.percpu import per_cpu_ptr
 @functools.wraps(int)
 def _int_or_suppress(s: str) -> Union[int, str]:
     return s if s is argparse.SUPPRESS else int(s)
-
-
-def _guess_type(prog: Program, kind: str, name: str) -> Type:
-    if kind != "union":
-        try:
-            return prog.type("struct " + name)
-        except LookupError:
-            pass
-
-    if kind != "struct":
-        try:
-            return prog.type("union " + name)
-        except LookupError:
-            pass
-
-    # Try a typedef.
-    type = prog.type(name)
-
-    # Make sure it's a typedef of our desired type kind.
-    underlying_type = type
-    while underlying_type.kind == TypeKind.TYPEDEF:
-        underlying_type = underlying_type.type
-    if (kind != "union" and underlying_type.kind == TypeKind.STRUCT) or (
-        kind != "struct" and underlying_type.kind == TypeKind.UNION
-    ):
-        return type
-
-    if kind == "*":
-        kind = "struct or union"
-    raise LookupError(f"{type.type_name()} is not a {kind}")
 
 
 _NAME_PATTERN = r"[a-zA-Z_][a-zA-Z0-9_]*"
@@ -257,7 +228,50 @@ address = prog.symbol({address_or_symbol!r}).address{subtract_offset}
     return
 
 
-def _struct_common(prog: Program, kind: str, args: argparse.Namespace) -> None:
+@crash_command(
+    description="structure contents",
+    arguments=(
+        argument(
+            "name",
+            metavar="struct_name[.member[,member]]",
+            help="name of structure type; one or more comma-separated members "
+            "(each of which can be nested and include array subscripts) "
+            "may also be given to limit the output to those members",
+        ),
+        argument(
+            "address_or_symbol",
+            metavar="address_or_symbol[:cpuspec]",
+            nargs="?",
+            help="hexadecimal address or symbol name of structure. "
+            "If not given, the type and its size are printed instead. "
+            "For per-cpu variables, this may also contain a colon (':') "
+            "followed by a specification of which CPUs to print, "
+            "which may be a comma-separated string of CPU numbers or ranges "
+            "(e.g., '0,3-4'), "
+            "'a' or 'all' (meaning all possible CPUs), "
+            "or an empty string (meaning the CPU of the current context)",
+        ),
+        argument(
+            "-l",
+            dest="offset",
+            help="offset from the beginning of the desired structure to the "
+            "given address or symbol, either as a number of bytes or a "
+            "struct_name.member",
+        ),
+        argument("-c", dest="count", type=int, help="number of consecutive structures"),
+        argument(
+            "count",
+            type=_int_or_suppress,
+            nargs="?",
+            default=argparse.SUPPRESS,
+            help="number of consecutive structures",
+        ),
+        drgn_argument,
+    ),
+)
+def _crash_cmd_struct(
+    prog: Program, kind: str, args: argparse.Namespace, **kwargs: Any
+) -> None:
     name, members = _parse_name_and_members(args.name)
     offset_arg = None if args.offset is None else _parse_offset_arg(args.offset)
 
@@ -280,7 +294,12 @@ def _struct_common(prog: Program, kind: str, args: argparse.Namespace) -> None:
     # name (e.g., "atomic_t", not "struct atomic_t") if it exists. If it
     # doesn't, then it's only a hard error without --drgn.
     try:
-        type = _guess_type(prog, kind, name)
+        # If this was run via an implicit type command, then the type should
+        # already be smuggled in here.
+        try:
+            type = kwargs["type"]
+        except KeyError:
+            type = _guess_type(prog, kind, name)
     except LookupError:
         if not args.drgn:
             raise
@@ -372,53 +391,6 @@ def _struct_common(prog: Program, kind: str, args: argparse.Namespace) -> None:
 
 
 @crash_command(
-    description="structure contents",
-    arguments=(
-        argument(
-            "name",
-            metavar="struct_name[.member[,member]]",
-            help="name of structure type; one or more comma-separated members "
-            "(each of which can be nested and include array subscripts) "
-            "may also be given to limit the output to those members",
-        ),
-        argument(
-            "address_or_symbol",
-            metavar="address_or_symbol[:cpuspec]",
-            nargs="?",
-            help="hexadecimal address or symbol name of structure. "
-            "If not given, the type and its size are printed instead. "
-            "For per-cpu variables, this may also contain a colon (':') "
-            "followed by a specification of which CPUs to print, "
-            "which may be a comma-separated string of CPU numbers or ranges "
-            "(e.g., '0,3-4'), "
-            "'a' or 'all' (meaning all possible CPUs), "
-            "or an empty string (meaning the CPU of the current context)",
-        ),
-        argument(
-            "-l",
-            dest="offset",
-            help="offset from the beginning of the desired structure to the "
-            "given address or symbol, either as a number of bytes or a "
-            "struct_name.member",
-        ),
-        argument("-c", dest="count", type=int, help="number of consecutive structures"),
-        argument(
-            "count",
-            type=_int_or_suppress,
-            nargs="?",
-            default=argparse.SUPPRESS,
-            help="number of consecutive structures",
-        ),
-        drgn_argument,
-    ),
-)
-def _crash_cmd_struct(
-    prog: Program, name: str, args: argparse.Namespace, **kwargs: Any
-) -> None:
-    return _struct_common(prog, "struct", args)
-
-
-@crash_command(
     description="union contents",
     arguments=(
         argument(
@@ -462,4 +434,41 @@ def _crash_cmd_struct(
 def _crash_cmd_union(
     prog: Program, name: str, args: argparse.Namespace, **kwargs: Any
 ) -> None:
-    return _struct_common(prog, "union", args)
+    return _crash_cmd_struct(prog, name, args, **kwargs)
+
+
+@crash_command(
+    name="*",
+    description="shortcut for struct or union",
+    usage=r"\* [*struct or union command arguments*]",
+    long_description="""
+    This is a shortcut that allows typing, e.g., ``*list_head`` instead of
+    ``struct list_head``. Note that if the type name is not also the name of a
+    command, then the ``*`` can also be omitted, e.g., ``list_head``.
+    """,
+    arguments=(
+        argument(
+            "name", metavar="struct_name[.member[,member]]", help=argparse.SUPPRESS
+        ),
+        argument(
+            "address_or_symbol",
+            metavar="address_or_symbol[:cpuspec]",
+            nargs="?",
+            help=argparse.SUPPRESS,
+        ),
+        argument("-l", dest="offset", help=argparse.SUPPRESS),
+        argument("-c", dest="count", type=int, help=argparse.SUPPRESS),
+        argument(
+            "count",
+            type=_int_or_suppress,
+            nargs="?",
+            default=argparse.SUPPRESS,
+            help=argparse.SUPPRESS,
+        ),
+        argument("--drgn", action="store_true", help=argparse.SUPPRESS),
+    ),
+)
+def _crash_cmd_asterisk(
+    prog: Program, name: str, args: argparse.Namespace, **kwargs: Any
+) -> None:
+    return _crash_cmd_struct(prog, name, args, **kwargs)

commit 89dfa85ff7daace8215c3eb2b8eb4bfd2d3dbb07

crash: fix implicit struct/union with members
Implicit struct/union commands should work even if the command includes
members.

Fixes: a5ec52188af8 ("crash: port * command and implicit struct/union")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/commands/crash.py
@@ -122,7 +122,9 @@ class _CrashCommandNamespace(CommandNamespace):
             except CommandNotFoundError as e:
                 try:
                     # Smuggle the type into the command function.
-                    kwargs["type"] = _guess_type(prog, "*", command_name)
+                    kwargs["type"] = _guess_type(
+                        prog, "*", command_name.partition(".")[0]
+                    )
                 except LookupError:
                     raise e
                 else:

tests/linux_kernel/crash_commands/test_structunion.py
@@ -490,6 +490,13 @@ class TestImplicit(CrashCommandTestCase):
             cmd.drgn_option.globals["type"].type_name(), "drgn_test_anonymous_union"
         )
 
+    def test_member(self):
+        cmd = self.check_crash_command("task_struct.pid init_task")
+        self.assertRegex(cmd.stdout, r"pid = \([^)]*\)0")
+        self.assertIn(".pid", cmd.drgn_option.stdout)
+        self.assertIdentical(cmd.drgn_option.globals["object"], self.prog["init_task"])
+        self.assertEqual(cmd.drgn_option.globals["pid"], 0)
+
     def test_not_found(self):
         self.assertRaises(
             CommandNotFoundError, self.run_crash_command, "drgn_test_non_existent"