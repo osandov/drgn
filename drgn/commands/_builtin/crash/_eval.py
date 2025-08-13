7a6f7480af90bc25eb6608de2cc7ca28a7572ceb
Omar Sandoval <osandov@osandov.com>
2025-08-13 15:46:40 -0700

    crash: port eval command
    The nastiest part of this one is handling the ambiguities between
    expressions, command line flags, pipelines, and redirections. Other than
    that, the expression syntax that crash supports is very limited, so it's
    fairly simple.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/commands/_builtin/crash/_eval.py
+++ drgn/commands/_builtin/crash/_eval.py
@@ -4,15 +4,334 @@
 """Crash commands for evaluating and printing values."""
 
 import argparse
+import operator
 import re
 import shutil
-from typing import Any, Tuple
+import sys
+from typing import Any, Callable, Dict, Optional, Tuple
 
-from drgn import Program
-from drgn.commands import argument, drgn_argument
+from drgn import Object, Program, Type
+from drgn.commands import CommandArgumentError, _shell_command, argument, drgn_argument
 from drgn.commands._builtin.crash.structunion import _MEMBER_PATTERN, _NAME_PATTERN
-from drgn.commands.crash import CrashDrgnCodeBuilder, crash_command, parse_cpuspec
+from drgn.commands.crash import (
+    CrashDrgnCodeBuilder,
+    crash_command,
+    crash_custom_command,
+    parse_cpuspec,
+)
 from drgn.helpers.linux.percpu import per_cpu
+
+_UNITS = {
+    "k": 1024,
+    "K": 1024,
+    "m": 1024 * 1024,
+    "M": 1024 * 1024,
+    "g": 1024 * 1024 * 1024,
+    "G": 1024 * 1024 * 1024,
+}
+
+
+_UNARY_OPS: Dict[str, Callable[[Object], Object]] = {
+    "-": operator.neg,
+    "~": operator.inv,
+}
+
+
+_BINARY_OPS: Dict[str, Callable[[Object, Object], Object]] = {
+    "+": operator.add,
+    "-": operator.sub,
+    "&": operator.and_,
+    "|": operator.or_,
+    "^": operator.xor,
+    "*": operator.mul,
+    "%": operator.mod,
+    "/": operator.truediv,
+    "<<": operator.lshift,
+    ">>": operator.rshift,
+}
+
+
+_OPERAND_PATTERN = rf"""
+(?P<unary{{n}}>
+    (?:{'|'.join([re.escape(op) for op in _UNARY_OPS])}\s*)*
+)
+(?P<number{{n}}>
+    (?:0[xX])?[0-9a-fA-F]+
+)
+(?:
+    \s*
+    (?P<unit{{n}}>[{''.join(_UNITS)}])
+)?
+
+"""
+
+
+_EVAL_PATTERN = rf"""
+    \s*
+    (?P<parenthesis>\(\s*)?
+    {_OPERAND_PATTERN.format(n=1)}
+    (?:
+        \s*
+        (?P<operator>{'|'.join([re.escape(op) for op in _BINARY_OPS])})
+        \s*
+        {_OPERAND_PATTERN.format(n=2)}
+    )?
+    (?(parenthesis)\s*\))
+    \s*
+"""
+
+
+def _eval_operand(type: Type, unary: str, number: str, unit: Optional[str]) -> Object:
+    try:
+        number_int = int(number, 10)
+    except ValueError:
+        number_int = int(number, 16)
+
+    value = Object(type.prog, type, number_int)
+
+    if unit is not None:
+        value *= _UNITS[unit]
+
+    for op in reversed(unary):
+        if op.isspace():
+            continue
+        value = _UNARY_OPS[op](value)
+
+    return value
+
+
+def _append_operand(
+    code: CrashDrgnCodeBuilder,
+    type_name: str,
+    unary: str,
+    number: str,
+    unit: Optional[str],
+) -> None:
+    for op in unary:
+        if not op.isspace():
+            code.append(op)
+
+    if unit is not None:
+        code.append("(")
+
+    if re.match(r"[0-9]*[a-fA-F]", number):
+        number = "0x" + number
+
+    code.append(f'Object(prog, "{type_name}", {number})')
+
+    if unit is not None:
+        code.append(f" * {_UNITS[unit]})")
+
+
+@crash_custom_command(
+    description="evaluate an expression",
+    usage="**eval** [**-b**] [**-l**] [**--drgn**] (*expression*)",
+    long_description="Evaluate an expression and print the result in various formats.",
+    arguments=(
+        argument(
+            "-b",
+            action="store_true",
+            help="show which bits are set in the result, where 0 is the least significant bit",
+        ),
+        argument(
+            "-l",
+            action="store_true",
+            help="use 64-bit values even on 32-bit architectures",
+        ),
+        argument(
+            "expression",
+            help=r"""
+        expression to evaluate.
+
+        Expressions are of the form ``value`` or ``value operator value``,
+        where value is a number optionally prefixed by zero or more unary
+        operators and optionally suffixed by a unit.
+
+        Numbers may be decimal or hexadecimal. The "0x" prefix is optional for
+        hexadecimal numbers.
+
+        The following binary operators are supported:
+
+        ``+  -  &  |  ^  *  %  /  <<  >>``
+
+        The following unary operators are supported:
+
+        ``- ~``
+
+        Units may be "K" or "k" to multiply by 1024, "M" or "m" to multiply by
+        1024*1024, and "G" or "g" to multiply by 1024*1024*1024.
+
+        Expressions may be enclosed in parentheses. Parentheses are only
+        required when using the ``|``, ``<<``, or ``>>`` operators.
+        """,
+        ),
+        drgn_argument,
+    ),
+)
+def _crash_cmd_eval(prog: Program, name: str, args: str, **kwargs: Any) -> None:
+    # This command allows redirection and pipelines. To avoid ambiguity with
+    # the <<, >>, and | operators, those operators must be parenthesized.
+    # First, split the arguments from any redirections/pipelines. We count
+    # nested parentheses even though the expression syntax currently doesn't
+    # support nesting.
+    parens = 0
+    for i, c in enumerate(args):
+        if c == "(":
+            parens += 1
+        elif parens:
+            if c == ")":
+                parens -= 1
+        elif c in "<>|":
+            break
+    else:
+        i = len(args)
+
+    # Execute any redirections/pipelines.
+    with _shell_command(args[i:]) as parsed:
+        if parsed.args:
+            # Don't allow extra arguments to be mixed in with redirections.
+            raise CommandArgumentError(
+                "eval does not support arguments after redirections"
+            )
+
+        # Handle (and remove) the --drgn flag after the expression.
+        expr, n = re.subn(r"((^|\s+)--drgn)+\s*$", "", args[:i])
+        drgn_arg = n > 0
+
+        # Handle (and remove) command line flags (-b, -l, and --drgn) before
+        # the expression.
+        show_bits_set = False
+        long_long = False
+
+        i = 0
+        pattern = re.compile(
+            r"""
+            \s*
+            (?:
+                # -l and --drgn are unambiguous.
+                (-[bl]*l[bl]*|--drgn)(?:\s|$)
+                |
+                # -b could be a flag or an expression (equivalent to -0xb).
+                # Crash treats it as a flag unless it is the last argument, so
+                # we do the same.
+                (-b)\s+\S
+            )
+            """,
+            flags=re.VERBOSE,
+        )
+        while match := pattern.match(expr, pos=i):
+            flags = match.group(match.lastindex)  # type: ignore[arg-type]  # lastindex cannot be None
+            if flags == "--drgn":
+                drgn_arg = True
+            else:
+                show_bits_set |= "b" in flags
+                long_long |= "l" in flags
+            i = match.end(match.lastindex)  # type: ignore[arg-type]  # lastindex cannot be None
+        expr = expr[i:]
+
+        match = re.fullmatch(_EVAL_PATTERN, expr, flags=re.VERBOSE)
+        if not match:
+            raise CommandArgumentError("bad expression")
+
+        type_name = "unsigned long long" if long_long else "unsigned long"
+        op = match.group("operator")
+
+        if drgn_arg:
+            code = CrashDrgnCodeBuilder(prog)
+            code.add_from_import("drgn", "Object", "cast")
+            code.add_from_import("drgn.helpers.common.format", "number_in_binary_units")
+            if op is None:
+                code.append("value = ")
+                _append_operand(
+                    code,
+                    type_name,
+                    match.group("unary1"),
+                    match.group("number1"),
+                    match.group("unit1"),
+                )
+                code.append("\n")
+            else:
+                code.append("lhs = ")
+                _append_operand(
+                    code,
+                    type_name,
+                    match.group("unary1"),
+                    match.group("number1"),
+                    match.group("unit1"),
+                )
+                code.append("\nrhs = ")
+                _append_operand(
+                    code,
+                    type_name,
+                    match.group("unary2"),
+                    match.group("number2"),
+                    match.group("unit2"),
+                )
+                code.append(f"\nvalue = lhs {op} rhs\n")
+            code.append(
+                f"""
+signed = cast("{type_name.replace("unsigned ", "")}", value)
+in_units = number_in_binary_units(value)
+in_hex = hex(value)
+in_octal = oct(value)
+in_binary = bin(value)
+"""
+            )
+            if show_bits_set:
+                code.add_from_import("drgn", "sizeof")
+                code.append(
+                    """\
+bits_set = [
+    i for i in range(sizeof(value) * 8) if value & (1 << i)
+]
+"""
+                )
+            code.print()
+            return
+
+        type = prog.type(type_name)
+        bit_size = type.size * 8  # type: ignore[operator]  # type.size cannot be None.
+
+        obj = _eval_operand(
+            type, match.group("unary1"), match.group("number1"), match.group("unit1")
+        )
+        if op is not None:
+            rhs = _eval_operand(
+                type,
+                match.group("unary2"),
+                match.group("number2"),
+                match.group("unit2"),
+            )
+            obj = _BINARY_OPS[op](obj, rhs)
+
+        value = obj.value_()
+
+        with_units = ""
+        if value % (1024 * 1024 * 1024) == 0:
+            with_units = f"  ({value // (1024 * 1024 * 1024)}GB)"
+        elif value % (1024 * 1024) == 0:
+            with_units = f"  ({value // (1024 * 1024)}MB)"
+        elif value % 1024 == 0:
+            with_units = f"  ({value // 1024}KB)"
+
+        signed = ""
+        if value & (1 << (bit_size - 1)):
+            signed = f"  ({value - (1 << bit_size)})"
+
+        bits_set = ""
+        if show_bits_set:
+            bits_set = "\n   bits set:" + "".join(
+                [f" {i}" for i in range(bit_size - 1, -1, -1) if value & (1 << i)]
+            )
+
+        sys.stdout.write(
+            f"""\
+hexadecimal: {value:x}{with_units}
+    decimal: {value}{signed}
+      octal: {value:o}
+     binary: {value:0{bit_size}b}{bits_set}
+"""
+        )
 
 
 def _parse_name_and_optional_member(s: str) -> Tuple[str, str]:
--- tests/linux_kernel/crash_commands/test_eval.py
+++ tests/linux_kernel/crash_commands/test_eval.py
@@ -1,10 +1,166 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+from pathlib import Path
 import re
+import tempfile
 
+from drgn import Object
+from drgn.commands.crash import CRASH_COMMAND_NAMESPACE
 from tests.linux_kernel import possible_cpus, skip_unless_have_test_kmod
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
+
+
+class TestEval(CrashCommandTestCase):
+    def test_value(self):
+        cmd = self.check_crash_command("eval 10")
+        self.assertRegex(cmd.stdout, r"\bhexadecimal: a\b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: 10\b")
+        self.assertRegex(cmd.stdout, r"\boctal: 12\b")
+        self.assertRegex(cmd.stdout, r"\bbinary: 0*1010\b")
+
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", 10)
+        )
+        self.assertIdentical(
+            cmd.drgn_option.globals["signed"], Object(self.prog, "long", 10)
+        )
+        self.assertIsInstance(cmd.drgn_option.globals["in_units"], str)
+        self.assertEqual(cmd.drgn_option.globals["in_hex"], "0xa")
+        self.assertEqual(cmd.drgn_option.globals["in_octal"], "0o12")
+        self.assertEqual(cmd.drgn_option.globals["in_binary"], "0b1010")
+
+    def test_negate(self):
+        cmd = self.check_crash_command("eval -99")
+        self.assertRegex(cmd.stdout, r"\bhexadecimal: f+9d\b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: [0-9]+  \(-99\)")
+
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", -99)
+        )
+        self.assertIdentical(
+            cmd.drgn_option.globals["signed"], Object(self.prog, "long", -99)
+        )
+
+    def test_invert(self):
+        cmd = self.check_crash_command("eval ~99")
+        self.assertRegex(cmd.stdout, r"\bhexadecimal: f+9c\b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: [0-9]+  \(-100\)")
+
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", ~99)
+        )
+        self.assertIdentical(
+            cmd.drgn_option.globals["signed"], Object(self.prog, "long", ~99)
+        )
+
+    def test_multiple_unary(self):
+        cmd = self.check_crash_command("eval -~--99")
+        self.assertRegex(cmd.stdout, r"\bhexadecimal: 64\b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: 100\b")
+
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", 100)
+        )
+        self.assertIdentical(
+            cmd.drgn_option.globals["signed"], Object(self.prog, "long", 100)
+        )
+
+    def test_units(self):
+        for value, in_units in (
+            (1024, "1K"),
+            (1024 * 1024, "1M"),
+            (1024 * 1024 * 1024, "1G"),
+        ):
+            for expr in (str(value), in_units, in_units.lower()):
+                with self.subTest(expr=expr):
+                    cmd = self.check_crash_command("eval " + expr)
+                    self.assertRegex(
+                        cmd.stdout, rf"\bhexadecimal: {value:x}  \({in_units}B\)"
+                    )
+                    self.assertEqual(cmd.drgn_option.globals["in_units"], in_units)
+
+    def test_negate_with_unit(self):
+        cmd = self.check_crash_command("eval -1k")
+        self.assertRegex(cmd.stdout, r"\bdecimal: [0-9]+  \(-1024\)")
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", -1024)
+        )
+
+    def test_bits_set(self):
+        cmd = self.check_crash_command("eval -b 10")
+        self.assertRegex(cmd.stdout, r"\bbits set: 3 1\n")
+        self.assertIdentical(cmd.drgn_option.globals["bits_set"], [1, 3])
+
+    def test_long_long(self):
+        cmd = self.check_crash_command("eval -l 10")
+        self.assertRegex(
+            cmd.stdout,
+            r"\bbinary: 0000000000000000000000000000000000000000000000000000000000001010\b",
+        )
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"],
+            Object(self.prog, "unsigned long long", 10),
+        )
+
+    def test_bits_set_and_long_long(self):
+        cmd = self.check_crash_command("eval -bl 10")
+        self.assertRegex(
+            cmd.stdout,
+            r"\bbinary: 0000000000000000000000000000000000000000000000000000000000001010\b",
+        )
+        self.assertRegex(cmd.stdout, r"\bbits set: 3 1\n")
+        self.assertIdentical(cmd.drgn_option.globals["bits_set"], [1, 3])
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"],
+            Object(self.prog, "unsigned long long", 10),
+        )
+
+    def test_negative_b(self):
+        cmd = self.check_crash_command("eval -b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: [0-9]+  \(-11\)")
+        self.assertNotIn("bits set", cmd.stdout)
+        self.assertIdentical(
+            cmd.drgn_option.globals["value"], Object(self.prog, "unsigned long", -0xB)
+        )
+
+    def test_bits_set_in_negative_b(self):
+        cmd = self.check_crash_command("eval -b -b")
+        self.assertRegex(cmd.stdout, r"\bdecimal: [0-9]+  \(-11\)")
+        self.assertIn("bits set", cmd.stdout)
+        self.assertIn("bits_set", cmd.drgn_option.globals)
+
+    def test_binary(self):
+        for expr, result in (
+            ("3 + 4", 7),
+            ("4 - 3", 1),
+            ("12 & 5", 4),
+            ("(12 | 5)", 13),
+            ("12 ^ 5", 9),
+            ("12 * 5", 60),
+            ("12 % 5", 2),
+            ("16 / 5", 3),
+            ("(3 << 5)", 96),
+            ("(100 >> 1)", 50),
+        ):
+            cmd = self.check_crash_command("eval " + expr)
+            self.assertRegex(cmd.stdout, rf"\bdecimal: {result}\b")
+            self.assertIdentical(
+                cmd.drgn_option.globals["value"],
+                Object(self.prog, "unsigned long", result),
+            )
+
+    def test_pipe(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            path = Path(temp_dir) / "file"
+            CRASH_COMMAND_NAMESPACE.run(self.prog, f"eval (12 | 5) | grep 13 > {path}")
+            self.assertRegex(path.read_text(), r"\bdecimal: 13\b")
+
+    def test_redirect(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            path = Path(temp_dir) / "file"
+            CRASH_COMMAND_NAMESPACE.run(self.prog, f"eval (3 << 5) >> {path}")
+            self.assertRegex(path.read_text(), r"\bdecimal: 96\b")
 
 
 class TestP(CrashCommandTestCase):
