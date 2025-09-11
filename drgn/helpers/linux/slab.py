osandov/drgn

commit 5c66aab23fc5cfab07c8022dea400a467b68fbdb

helpers: add helpers for iterating over allocated slab objects
These are ported from https://github.com/josefbacik/debug-scripts.

Signed-off-by: alexlzhu <alexlzhu@fb.com>
[Omar: make test case check for exception on SLOB]
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/slab.py
@@ -9,16 +9,18 @@ The ``drgn.helpers.linux.slab`` module provides helpers for working with the
 Linux slab allocator.
 """
 
-from typing import Iterator, Optional, Union
+from typing import Iterator, Optional, Set, Union
 
-from drgn import Object, Program
+from drgn import FaultError, Object, Program, Type, cast
 from drgn.helpers import escape_ascii_string
 from drgn.helpers.linux.list import list_for_each_entry
+from drgn.helpers.linux.mm import for_each_page, page_to_virt
 
 __all__ = (
     "find_slab_cache",
     "for_each_slab_cache",
     "print_slab_caches",
+    "slab_cache_for_each_allocated_object",
 )
 
 
@@ -53,3 +55,107 @@ def print_slab_caches(prog: Program) -> None:
     for s in for_each_slab_cache(prog):
         name = escape_ascii_string(s.name.string_(), escape_backslash=True)
         print(f"{name} ({s.type_.type_name()})0x{s.value_():x}")
+
+
+def slab_cache_for_each_allocated_object(
+    slab_cache: Object, type: Union[str, Type]
+) -> Iterator[Object]:
+    """
+    Iterate over all allocated objects in a given slab cache.
+
+    Only the SLUB and SLAB allocators are supported; SLOB does not store enough
+    information to identify objects in a slab cache.
+
+    >>> dentry_cache = find_slab_cache(prog, "dentry")
+    >>> next(slab_cache_for_each_allocated_object(dentry_cache, "struct dentry"))
+    *(struct dentry *)0xffff905e41404000 = {
+        ...
+    }
+
+    :param slab_cache: ``struct kmem_cache *``
+    :param type: Type of object in the slab cache.
+    :return: Iterator of ``type *`` objects.
+    """
+    prog = slab_cache.prog_
+    slab_cache_size = slab_cache.size.value_()
+    pointer_type = prog.pointer_type(prog.type(type))
+
+    try:
+        freelist_type = prog.type("freelist_idx_t *")
+        slub = False
+    except LookupError:
+        freelist_type = prog.type("void **")
+        slub = True
+
+    if slub:
+        try:
+            red_left_pad = slab_cache.red_left_pad.value_()
+        except AttributeError:
+            red_left_pad = 0
+
+        try:
+            freelist_offset = slab_cache.offset.value_()
+        except AttributeError:
+            raise ValueError("SLOB is not supported") from None
+
+        def _slub_freelist(slab: Object, offset: int) -> Set[int]:
+            # In SLUB, the freelist is a linked list with the next pointer
+            # located at ptr + slab_cache->offset.
+            freelist_set = set()
+            freelist = slab.freelist.read_()
+
+            while freelist:
+                freelist_set.add(freelist.value_())
+                freelist = cast(freelist_type, freelist + offset)[0].read_()
+            return freelist_set
+
+        def _slab_page_objects(page: Object, slab: Object) -> Iterator[Object]:
+            freelist_set = _slub_freelist(slab, freelist_offset)
+            addr = page_to_virt(page).value_() + red_left_pad
+            end = addr + slab_cache_size * slab.objects
+            while addr < end:
+                if addr not in freelist_set:
+                    yield Object(prog, pointer_type, value=addr)
+                addr += slab_cache_size
+
+    else:
+        try:
+            obj_offset = slab_cache.obj_offset.value_()
+        except AttributeError:
+            obj_offset = 0
+
+        slab_cache_num = slab_cache.num.value_()
+
+        def _slab_freelist(slab: Object) -> Set[int]:
+            # In SLAB, the freelist is an array of free object indices.
+            freelist = cast(freelist_type, slab.freelist)
+            return {freelist[i].value_() for i in range(slab.active, slab_cache_num)}
+
+        def _slab_page_objects(page: Object, slab: Object) -> Iterator[Object]:
+            freelist_set = _slab_freelist(slab)
+            s_mem = slab.s_mem.value_()
+            for i in range(slab_cache_num):
+                if i in freelist_set:
+                    continue
+                yield Object(
+                    prog, pointer_type, value=s_mem + i * slab_cache_size + obj_offset
+                )
+
+    # Linux kernel commit d122019bf061cccc4583eb9ad40bf58c2fe517be ("mm: Split
+    # slab into its own type") (in v5.17) moved slab information from struct
+    # page to struct slab. The former can be casted to the latter.
+    try:
+        slab_type = prog.type("struct slab *")
+    except LookupError:
+        slab_type = prog.type("struct page *")
+
+    PG_slab_mask = 1 << prog.constant("PG_slab")
+    for page in for_each_page(prog):
+        try:
+            if not page.flags & PG_slab_mask:
+                continue
+        except FaultError:
+            continue
+        slab = cast(slab_type, page)
+        if slab.slab_cache == slab_cache:
+            yield from _slab_page_objects(page, slab)

tests/helpers/linux/test_slab.py
@@ -1,7 +1,12 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from drgn.helpers.linux.slab import find_slab_cache, for_each_slab_cache
+from drgn.helpers.linux.kconfig import get_kconfig
+from drgn.helpers.linux.slab import (
+    find_slab_cache,
+    for_each_slab_cache,
+    slab_cache_for_each_allocated_object,
+)
 from tests.linux_kernel import LinuxKernelTestCase
 
 
@@ -28,6 +33,21 @@ def fallback_slab_cache_names(prog):
 
 
 class TestSlab(LinuxKernelTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        try:
+            kconfig = get_kconfig(cls.prog)
+        except LookupError:
+            cls.allocator = None
+        else:
+            for allocator in ("SLUB", "SLAB", "SLOB"):
+                if kconfig.get("CONFIG_" + allocator, "n") != "n":
+                    break
+            else:
+                raise Exception("couldn't find slab allocator config option")
+            cls.allocator = allocator
+
     def test_for_each_slab_cache(self):
         try:
             slab_cache_names = get_proc_slabinfo_names()
@@ -51,3 +71,23 @@ class TestSlab(LinuxKernelTestCase):
         for name in slab_cache_names:
             slab = find_slab_cache(self.prog, name)
             self.assertEqual(name, slab.name.string_())
+
+    def test_get_allocated_slab_objects(self):
+        if self.allocator is None:
+            self.skipTest("couldn't determine slab allocator")
+        elif self.allocator == "SLOB":
+            self.assertRaisesRegex(
+                ValueError,
+                "SLOB is not supported",
+                next,
+                slab_cache_for_each_allocated_object(
+                    find_slab_cache(self.prog, "dentry"), "struct dentry"
+                ),
+            )
+        else:
+            self.assertIn(
+                self.prog["init_fs"].root.dentry,
+                slab_cache_for_each_allocated_object(
+                    find_slab_cache(self.prog, "dentry"), "struct dentry"
+                ),
+            )

commit b3fcbaed308e310189c2c028ab0fb1dafcdb20a6

drgn.helpers.linux.slab: check SLAB node caches
In addition to per-CPU caches of free objects, SLAB also kept per-node
caches. We aren't checking these when determining whether an object is
allocated, leading to inaccurate results from
slab_cache_for_each_allocated_object() and slab_object_info().

Fixes: 5c66aab23fc5 ("helpers: add helpers for iterating over allocated slab objects")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/slab.py
@@ -44,6 +44,7 @@ from drgn.helpers.linux.mm import (
     page_to_virt,
     virt_to_page,
 )
+from drgn.helpers.linux.nodemask import for_each_online_node
 from drgn.helpers.linux.percpu import per_cpu_ptr
 from drgn.helpers.linux.rbtree import rbtree_inorder_for_each_entry
 
@@ -417,13 +418,29 @@ class _SlabCacheHelperSlab(_SlabCacheHelper):
 
         self._slab_cache_num = slab_cache.num.value_()
 
+        array_caches: Set[int] = set()
+
         cpu_cache = slab_cache.cpu_cache.read_()
-        cpu_caches_avail: Set[int] = set()
         for cpu in for_each_online_cpu(self._prog):
-            ac = per_cpu_ptr(cpu_cache, cpu)
-            for i in range(ac.avail):
-                cpu_caches_avail.add(ac.entry[i].value_())
-        self._cpu_caches_avail = cpu_caches_avail
+            self._slab_add_array_cache(array_caches, per_cpu_ptr(cpu_cache, cpu))
+
+        nodes = slab_cache.node
+        for nid in for_each_online_node(self._prog):
+            n = nodes[nid].read_()
+            shared = n.shared.read_()
+            if shared:
+                self._slab_add_array_cache(array_caches, shared)
+            alien = n.alien.read_()
+            if alien:
+                for nid2 in for_each_online_node(self._prog):
+                    self._slab_add_array_cache(array_caches, alien[nid2])
+
+        self._array_caches = array_caches
+
+    @staticmethod
+    def _slab_add_array_cache(array_caches: Set[int], ac: Object) -> None:
+        for i in range(ac.avail):
+            array_caches.add(ac.entry[i].value_())
 
     def _slab_freelist(self, slab: Object) -> Set[int]:
         # In SLAB, the freelist is an array of free object indices.
@@ -439,7 +456,7 @@ class _SlabCacheHelperSlab(_SlabCacheHelper):
             if i in freelist:
                 continue
             addr = s_mem + i * self._slab_cache_size + self._obj_offset
-            if addr in self._cpu_caches_avail:
+            if addr in self._array_caches:
                 continue
             yield Object(self._prog, pointer_type, value=addr)
 
@@ -451,7 +468,7 @@ class _SlabCacheHelperSlab(_SlabCacheHelper):
             self._slab_cache,
             slab,
             object_address,
-            allocated=object_address not in self._cpu_caches_avail
+            allocated=object_address not in self._array_caches
             and object_index not in self._slab_freelist(slab),
         )
 