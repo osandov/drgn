osandov/drgn

commit b82bb5efea5deb704a47a16cff3bfa18b8e8bf0a

drgn.helpers.linux.hugetlb: add new HugeTLB helpers
These are needed for the crash kmem command.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/hugetlb.py
@@ -1,0 +1,70 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+
+"""
+HugeTLB
+-------
+
+The ``drgn.helpers.linux.hugetlb`` module provides helpers for working with
+HugeTLB pages.
+"""
+
+from typing import Iterator
+
+from drgn import Object, Program
+from drgn.helpers.common.prog import takes_program_or_default
+from drgn.helpers.linux.mm import PageUsage
+
+__all__ = (
+    "for_each_hstate",
+    "huge_page_size",
+    "hugetlb_total_pages",
+    "hugetlb_total_usage",
+)
+
+
+@takes_program_or_default
+def for_each_hstate(prog: Program) -> Iterator[Object]:
+    """
+    Iterate over all HugeTLB page size states.
+
+    >>> [h.name.string_() for h in for_each_hstate()]
+    [b'hugepages-1048576kB', b'hugepages-2048kB']
+
+    :return: Iterator of ``struct hstate *`` objects.
+    """
+    return (h.address_of_() for h in prog["hstates"][: prog["hugetlb_max_hstate"]])
+
+
+def huge_page_size(hstate: Object) -> Object:
+    """
+    Return the size of a HugeTLB state in bytes.
+
+    :param hstate: ``struct hstate *``
+    :return: ``unsigned long``
+    """
+    return hstate.prog_["PAGE_SIZE"] << hstate.order
+
+
+@takes_program_or_default
+def hugetlb_total_pages(prog: Program) -> int:
+    """Get the total number of HugeTLB pages (in ``PAGE_SIZE`` units)."""
+    return sum(
+        h.nr_huge_pages.value_() << h.order.value_() for h in for_each_hstate(prog)
+    )
+
+
+@takes_program_or_default
+def hugetlb_total_usage(prog: Program) -> PageUsage:
+    """
+    Get the total number of HugeTLB pages and the number of free HugeTLB pages
+    (in ``PAGE_SIZE`` units).
+    """
+    pages = 0
+    free_pages = 0
+    for h in for_each_hstate(prog):
+        order = h.order.value_()
+        pages += h.nr_huge_pages.value_() << order
+        free_pages += h.free_huge_pages.value_() << order
+    return PageUsage(pages=pages, free_pages=free_pages)

tests/linux_kernel/helpers/test_hugetlb.py
@@ -1,0 +1,60 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import mmap
+import os
+from pathlib import Path
+
+from drgn.helpers.linux.hugetlb import (
+    for_each_hstate,
+    huge_page_size,
+    hugetlb_total_pages,
+    hugetlb_total_usage,
+)
+from tests.linux_kernel import LinuxKernelTestCase
+
+
+class TestHugetlb(LinuxKernelTestCase):
+    def test_for_each_hstate(self):
+        self.assertCountEqual(
+            [h.name.string_() for h in for_each_hstate(self.prog)],
+            os.listdir(b"/sys/kernel/mm/hugepages"),
+        )
+
+    def test_huge_page_size(self):
+        for hstate in for_each_hstate(self.prog):
+            self.assertEqual(
+                f"hugepages-{huge_page_size(hstate).value_() // 1024}kB".encode(),
+                hstate.name.string_(),
+            )
+            break
+        else:
+            self.skipTest("no HugeTLB sizes")
+
+    # Also tests hugetlb_total_pages().
+    def test_hugetlb_total_usage(self):
+        nr_hugepages_path = Path("/proc/sys/vm/nr_hugepages")
+        old_nr_hugepages = int(nr_hugepages_path.read_text())
+        try:
+            if old_nr_hugepages == 0:
+                nr_hugepages_path.write_text("1")
+
+            expected_total = 0
+            expected_free = 0
+            for path in Path("/sys/kernel/mm/hugepages").glob("hugepages-*kB"):
+                pages = (
+                    int(path.name[len("hugepages-") : -len("kB")])
+                    * 1024
+                    // mmap.PAGESIZE
+                )
+                expected_total += int((path / "nr_hugepages").read_text()) * pages
+                expected_free += int((path / "free_hugepages").read_text()) * pages
+
+            usage = hugetlb_total_usage(self.prog)
+            self.assertEqual(usage.pages, expected_total)
+            self.assertAlmostEqual(usage.free_pages, expected_free, delta=1024)
+
+            self.assertEqual(hugetlb_total_pages(self.prog), expected_total)
+        finally:
+            if old_nr_hugepages == 0:
+                nr_hugepages_path.write_text("0")

commit 2a9fb62e496e15fda45944aaed92b7b387ee6185

drgn.helpers.linux.hugetlb: handle CONFIG_HUGETLBFS=n
On kernels with no HugeTLB support, for_each_hstate() can return an
empty iterator, which in turn fixes the other helpers.

Fixes: b82bb5efea5d ("drgn.helpers.linux.hugetlb: add new HugeTLB helpers")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/hugetlb.py
@@ -12,7 +12,7 @@ HugeTLB pages.
 
 from typing import Iterator
 
-from drgn import Object, Program
+from drgn import Object, ObjectNotFoundError, Program
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.mm import PageUsage
 
@@ -34,7 +34,12 @@ def for_each_hstate(prog: Program) -> Iterator[Object]:
 
     :return: Iterator of ``struct hstate *`` objects.
     """
-    return (h.address_of_() for h in prog["hstates"][: prog["hugetlb_max_hstate"]])
+    try:
+        hstates = prog["hstates"]
+    except ObjectNotFoundError:
+        # CONFIG_HUGETLBFS=n.
+        return iter(())
+    return (h.address_of_() for h in hstates[: prog["hugetlb_max_hstate"]])
 
 
 def huge_page_size(hstate: Object) -> Object:

tests/linux_kernel/helpers/test_hugetlb.py
@@ -16,9 +16,13 @@ from tests.linux_kernel import LinuxKernelTestCase
 
 class TestHugetlb(LinuxKernelTestCase):
     def test_for_each_hstate(self):
+        try:
+            expected = os.listdir(b"/sys/kernel/mm/hugepages")
+        except FileNotFoundError:
+            expected = []
         self.assertCountEqual(
             [h.name.string_() for h in for_each_hstate(self.prog)],
-            os.listdir(b"/sys/kernel/mm/hugepages"),
+            expected,
         )
 
     def test_huge_page_size(self):
@@ -34,7 +38,10 @@ class TestHugetlb(LinuxKernelTestCase):
     # Also tests hugetlb_total_pages().
     def test_hugetlb_total_usage(self):
         nr_hugepages_path = Path("/proc/sys/vm/nr_hugepages")
-        old_nr_hugepages = int(nr_hugepages_path.read_text())
+        try:
+            old_nr_hugepages = int(nr_hugepages_path.read_text())
+        except FileNotFoundError:
+            old_nr_hugepages = None
         try:
             if old_nr_hugepages == 0:
                 nr_hugepages_path.write_text("1")