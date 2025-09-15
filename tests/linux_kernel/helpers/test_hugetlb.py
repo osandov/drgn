e04a7acfe5130a1033c25e253a5080882828003f
Omar Sandoval <osandov@osandov.com>
2025-09-15 14:43:03 -0700

    tests: handle nr_hugepages EOPNOTSUPP in test_hugetlb_total_usage()
    Some of our older s390x vmtest kernels apparently end up with
    nr_hugepages existing but always returning EOPNOTSUPP. Treat it the same
    as a non-existent nr_hugepages.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/linux_kernel/helpers/test_hugetlb.py
+++ tests/linux_kernel/helpers/test_hugetlb.py
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+import errno
 import mmap
 import os
 from pathlib import Path
@@ -40,7 +41,9 @@ class TestHugetlb(LinuxKernelTestCase):
         nr_hugepages_path = Path("/proc/sys/vm/nr_hugepages")
         try:
             old_nr_hugepages = int(nr_hugepages_path.read_text())
-        except FileNotFoundError:
+        except OSError as e:
+            if e.errno not in (errno.ENOENT, errno.EOPNOTSUPP):
+                raise
             old_nr_hugepages = None
         try:
             if old_nr_hugepages == 0:
