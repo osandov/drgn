osandov/drgn

commit 68246b87593f26ac51aad6ff695e6f80ee801712

drgn.helpers.linux.{mm,slab,swap}: add memory statistic helpers
These are various helpers for getting values needed for the crash
kmem -i command.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/mm.py
@@ -38,7 +38,7 @@ from drgn.helpers.common.format import decode_enum_type_flags
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.list import list_for_each_entry
 from drgn.helpers.linux.mapletree import mt_for_each, mtree_load
-from drgn.helpers.linux.percpu import percpu_counter_sum
+from drgn.helpers.linux.percpu import percpu_counter_sum, percpu_counter_sum_positive
 from drgn.helpers.linux.pid import for_each_task_in_group
 from drgn.helpers.linux.rbtree import rb_find
 
@@ -64,6 +64,10 @@ __all__ = (
     "for_each_page",
     "for_each_vma",
     "for_each_vmap_area",
+    "global_node_page_state",
+    "global_zone_page_state",
+    "nr_blockdev_pages",
+    "nr_free_pages",
     "page_size",
     "page_to_pfn",
     "page_to_phys",
@@ -1425,6 +1429,75 @@ def totalram_pages(prog: Program) -> int:
         return prog["_totalram_pages"].counter.value_()
     except KeyError:
         return prog["totalram_pages"].value_()
+
+
+@takes_program_or_default
+def global_node_page_state(prog: Program, item: IntegerLike) -> int:
+    """
+    Get the value of a node VM statistic.
+
+    >>> global_node_page_state(prog["NR_FILE_PAGES"])
+    2257904
+
+    :param item: ``enum node_stat_item``
+    """
+    return max(prog["vm_node_stat"][item].counter.value_(), 0)
+
+
+@takes_program_or_default
+def global_zone_page_state(prog: Program, item: IntegerLike) -> int:
+    """
+    Get the value of a zone VM statistic.
+
+    >>> global_zone_page_state(prog["NR_MLOCK"])
+    1562
+
+    :param item: ``enum zone_stat_item``
+    """
+    return max(prog["vm_zone_stat"][item].counter.value_(), 0)
+
+
+@takes_program_or_default
+def nr_free_pages(prog: Program) -> int:
+    """Get the number of free memory pages."""
+    return global_zone_page_state(prog["NR_FREE_PAGES"])
+
+
+@takes_program_or_default
+def nr_blockdev_pages(prog: Program) -> int:
+    """Get the number of pages used for block device buffers."""
+    return sum(
+        inode.i_mapping.nrpages.value_()
+        for inode in list_for_each_entry(
+            "struct inode",
+            prog["blockdev_superblock"].s_inodes.address_of_(),
+            "i_sb_list",
+        )
+    )
+
+
+@takes_program_or_default
+def vm_commit_limit(prog: Program) -> int:
+    """Get the limit on committed virtual address space in pages."""
+    sysctl_overcommit_kbytes = prog["sysctl_overcommit_kbytes"].value_()
+    if sysctl_overcommit_kbytes:
+        allowed = sysctl_overcommit_kbytes >> (prog["PAGE_SHIFT"].value_() - 10)
+    else:
+        # Avoid circular import.
+        from drgn.helpers.linux.hugetlb import hugetlb_total_pages
+
+        allowed = (
+            (totalram_pages(prog) - hugetlb_total_pages(prog))
+            * prog["sysctl_overcommit_ratio"].value_()
+            // 100
+        )
+    return allowed + prog["total_swap_pages"].value_()
+
+
+@takes_program_or_default
+def vm_memory_committed(prog: Program) -> int:
+    """Get the number of pages of committed virtual address space."""
+    return percpu_counter_sum_positive(prog["vm_committed_as"].address_of_())
 
 
 class PageUsage(NamedTuple):

drgn/helpers/linux/slab.py
@@ -25,6 +25,7 @@ from drgn import (
     FaultError,
     IntegerLike,
     Object,
+    ObjectNotFoundError,
     Program,
     Type,
     cast,
@@ -41,6 +42,7 @@ from drgn.helpers.linux.mm import (
     _get_PageSlab_impl,
     compound_head,
     for_each_page,
+    global_node_page_state,
     in_direct_map,
     page_to_virt,
     virt_to_page,
@@ -62,6 +64,7 @@ __all__ = (
     "slab_cache_pages_per_slab",
     "slab_cache_usage",
     "slab_object_info",
+    "slab_total_usage",
 )
 
 
@@ -871,3 +874,35 @@ def find_containing_slab_cache(prog: Program, addr: IntegerLike) -> Object:
     if result is None:
         return NULL(prog, "struct kmem_cache *")
     return result[0].read_()
+
+
+@takes_program_or_default
+def slab_total_usage(prog: Program) -> "SlabTotalUsage":
+    """Get the total number of reclaimable and unreclaimable slab pages."""
+    # The items were renamed in Linux kernel commit d42f3245c7e2 ("mm: memcg:
+    # convert vmstat slab counters to bytes") (in v5.9).
+    try:
+        return SlabTotalUsage(
+            reclaimable_pages=global_node_page_state(prog["NR_SLAB_RECLAIMABLE_B"]),
+            unreclaimable_pages=global_node_page_state(prog["NR_SLAB_UNRECLAIMABLE_B"]),
+        )
+    except ObjectNotFoundError:
+        return SlabTotalUsage(
+            reclaimable_pages=global_node_page_state(prog["NR_SLAB_RECLAIMABLE"]),
+            unreclaimable_pages=global_node_page_state(prog["NR_SLAB_UNRECLAIMABLE"]),
+        )
+
+
+class SlabTotalUsage(NamedTuple):
+    """Slab memory usage returned by :func:`slab_total_usage()`."""
+
+    reclaimable_pages: int
+    """Number of reclaimable slab pages."""
+
+    unreclaimable_pages: int
+    """Number of unreclaimable slab pages."""
+
+    @property
+    def total_pages(self) -> int:
+        """Total number of slab pages."""
+        return self.reclaimable_pages + self.unreclaimable_pages

drgn/helpers/linux/swap.py
@@ -11,10 +11,10 @@ partitions and swap files.
 
 from typing import Iterator
 
-from drgn import Object, PlatformFlags, Program
+from drgn import Object, ObjectNotFoundError, PlatformFlags, Program
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.fs import d_path
-from drgn.helpers.linux.mm import PageUsage
+from drgn.helpers.linux.mm import PageUsage, global_node_page_state
 
 __all__ = (
     "for_each_swap_info",
@@ -22,6 +22,7 @@ __all__ = (
     "swap_is_file",
     "swap_total_usage",
     "swap_usage_in_pages",
+    "total_swapcache_pages",
 )
 
 
@@ -110,3 +111,34 @@ def swap_total_usage(prog: Program) -> PageUsage:
         pages=prog["total_swap_pages"].value_() + nr_to_be_unused,
         free_pages=prog["nr_swap_pages"].counter.value_() + nr_to_be_unused,
     )
+
+
+@takes_program_or_default
+def total_swapcache_pages(prog: Program) -> int:
+    """
+    Get the number of swap cached pages (pages that are swapped in but still
+    present on a swap device).
+    """
+    # Since Linux kernel commit ("mm: memcg: add swapcache stat for memcg v2")
+    # (in v5.12), we just have to get the NR_SWAPCACHE statistic. Before that,
+    # we have to sum over the swapper spaces.
+    try:
+        NR_SWAPCACHE = prog["NR_SWAPCACHE"]
+    except ObjectNotFoundError:
+        # Since Linux kernel commit 4b3ef9daa4fc ("mm/swap: split swap cache
+        # into 64MB trunks") (in v4.11), there are multiple swapper spaces per
+        # swap file. Before that, there was only one per swap file.
+        try:
+            nr_swapper_spaces = prog["nr_swapper_spaces"]
+        except ObjectNotFoundError:
+            # Before Linux kernel commit 4b3ef9daa4fc ("mm/swap: split swap
+            # cache into 64MB trunks") (in v4.11),
+            return sum(space.nrpages.value_() for space in prog["swapper_spaces"])
+        else:
+            return sum(
+                spaces[j].nrpages.value_()
+                for nr, spaces in zip(nr_swapper_spaces, prog["swapper_spaces"])
+                for j in range(nr)
+            )
+    else:
+        return global_node_page_state(NR_SWAPCACHE)

tests/linux_kernel/__init__.py
@@ -249,6 +249,20 @@ def possible_cpus():
     return parse_range_list(Path("/sys/devices/system/cpu/possible").read_text())
 
 
+def meminfo_field_in_pages(name: str) -> int:
+    return (
+        int(
+            re.search(
+                rf"^{name}:\s*([0-9]+)\s*kB",
+                Path("/proc/meminfo").read_text(),
+                flags=re.M,
+            ).group(1)
+        )
+        * 1024
+        // mmap.PAGESIZE
+    )
+
+
 _c = ctypes.CDLL(None, use_errno=True)
 
 _mount = _c.mount

tests/linux_kernel/helpers/test_mm.py
@@ -37,6 +37,8 @@ from drgn.helpers.linux.mm import (
     for_each_vma,
     for_each_vmap_area,
     get_task_rss_info,
+    nr_blockdev_pages,
+    nr_free_pages,
     page_size,
     page_to_pfn,
     page_to_phys,
@@ -49,6 +51,8 @@ from drgn.helpers.linux.mm import (
     virt_to_page,
     virt_to_pfn,
     virt_to_phys,
+    vm_commit_limit,
+    vm_memory_committed,
     vma_find,
     vmalloc_to_page,
     vmalloc_to_pfn,
@@ -58,6 +62,7 @@ from tests.linux_kernel import (
     LinuxKernelTestCase,
     fork_and_stop,
     iter_maps,
+    meminfo_field_in_pages,
     mlock,
     prng32,
     skip_if_highmem,
@@ -446,15 +451,45 @@ class TestMm(LinuxKernelTestCase):
             )
 
     def test_totalram_pages(self):
-        with open("/proc/meminfo") as f:
-            lines = f.read().splitlines()
-            line = [line for line in lines if line.startswith("MemTotal:")][0]
-            parts = line.split()
-            self.assertEqual(parts[2], "kB")
+        self.assertEqual(totalram_pages(self.prog), meminfo_field_in_pages("MemTotal"))
 
-            proc_totalram = 1024 * int(parts[1])
-            page_size = self.prog["PAGE_SIZE"].value_()
-            self.assertEqual(totalram_pages(self.prog) * page_size, proc_totalram)
+    def test_nr_free_pages(self):
+        self.assertAlmostEqual(
+            nr_free_pages(self.prog),
+            meminfo_field_in_pages("MemFree"),
+            delta=1024 * 1024 * 1024,
+        )
+
+    def test_nr_blockdev_pages(self):
+        self.assertAlmostEqual(
+            nr_blockdev_pages(self.prog),
+            meminfo_field_in_pages("Buffers"),
+            delta=1024 * 1024 * 1024,
+        )
+
+    def test_vm_commit_limit(self):
+        overcommit_kbytes_path = Path("/proc/sys/vm/overcommit_kbytes")
+        orig_overcommit_kbytes = int(overcommit_kbytes_path.read_text())
+        try:
+            for i in range(2):
+                if i == 1:
+                    if orig_overcommit_kbytes == 0:
+                        overcommit_kbytes = 1024 * 1024 * 1024
+                    else:
+                        overcommit_kbytes = 0
+                    overcommit_kbytes_path.write_text(str(overcommit_kbytes))
+                self.assertEqual(
+                    vm_commit_limit(self.prog), meminfo_field_in_pages("CommitLimit")
+                )
+        finally:
+            overcommit_kbytes_path.write_text(str(orig_overcommit_kbytes))
+
+    def test_vm_memory_committed(self):
+        self.assertAlmostEqual(
+            vm_memory_committed(self.prog),
+            meminfo_field_in_pages("Committed_AS"),
+            delta=1024 * 1024 * 1024,
+        )
 
     def test_get_task_rss_info(self):
         with fork_and_stop() as pid:

tests/linux_kernel/helpers/test_slab.py
@@ -18,9 +18,11 @@ from drgn.helpers.linux.slab import (
     slab_cache_pages_per_slab,
     slab_cache_usage,
     slab_object_info,
+    slab_total_usage,
 )
 from tests.linux_kernel import (
     LinuxKernelTestCase,
+    meminfo_field_in_pages,
     skip_unless_have_full_mm_support,
     skip_unless_have_test_kmod,
 )
@@ -341,4 +343,17 @@ class TestSlab(LinuxKernelTestCase):
         self.assertEqual(
             find_containing_slab_cache(self.prog, self.prog["drgn_test_va"]),
             NULL(self.prog, "struct kmem_cache *"),
+        )
+
+    def test_slab_total_usage(self):
+        slab_usage = slab_total_usage(self.prog)
+        self.assertAlmostEqual(
+            slab_usage.reclaimable_pages,
+            meminfo_field_in_pages("SReclaimable"),
+            delta=1024 * 1024 * 1024,
+        )
+        self.assertAlmostEqual(
+            slab_usage.unreclaimable_pages,
+            meminfo_field_in_pages("SUnreclaim"),
+            delta=1024 * 1024 * 1024,
         )

tests/linux_kernel/helpers/test_swap.py
@@ -17,12 +17,14 @@ from drgn.helpers.linux.swap import (
     swap_is_file,
     swap_total_usage,
     swap_usage_in_pages,
+    total_swapcache_pages,
 )
 from tests.linux_kernel import (
     LinuxKernelTestCase,
     MbrPartition,
     MbrPartitionType,
     fallocate,
+    meminfo_field_in_pages,
     mkswap,
     mount,
     skip_unless_have_test_disk,
@@ -164,4 +166,11 @@ class TestSwap(LinuxKernelTestCase):
         # test is running.
         self.assertAlmostEqual(
             swap_usage.free_pages, free_pages, delta=1024 * 1024 * 1024
+        )
+
+    def test_total_swapcache_pages(self):
+        self.assertAlmostEqual(
+            total_swapcache_pages(self.prog),
+            meminfo_field_in_pages("SwapCached"),
+            delta=1024 * 1024 * 1024,
         )

commit 657b4782ad744193145c951c9397496ccec2875c

drgn.helpers.linux.mm: add missing helpers to __all__
And reorder the ones that are out of alphabetical order.

Fixes: 68246b87593f ("drgn.helpers.linux.{mm,slab,swap}: add memory statistic helpers")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/mm.py
@@ -64,6 +64,8 @@ __all__ = (
     "for_each_page",
     "for_each_vma",
     "for_each_vmap_area",
+    "get_page_flags",
+    "get_task_rss_info",
     "page_size",
     "page_to_pfn",
     "page_to_phys",
@@ -76,10 +78,11 @@ __all__ = (
     "virt_to_page",
     "virt_to_pfn",
     "virt_to_phys",
+    "vm_commit_limit",
+    "vm_memory_committed",
     "vma_find",
     "vmalloc_to_page",
     "vmalloc_to_pfn",
-    "get_task_rss_info",
     # Generated by scripts/generate_page_flag_getters.py.
     "PageActive",
     "PageChecked",
@@ -117,7 +120,6 @@ __all__ = (
     "PageWriteback",
     "PageXenRemapped",
     "PageYoung",
-    "get_page_flags",
 )
 
 