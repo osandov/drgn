osandov/drgn

commit 48b9629695407d3183693debcc15df9ee7049c92

libdrgn: python: allow nesting blocking callbacks
The upcoming module API may nest blocking sections. PyEval_SaveThread()
doesn't support nesting. We have to use the lower-level
PyThreadState_GetUnchecked() (named _PyThreadState_UncheckedGet() before
Python 3.13) and PyEval_ReleaseThread() APIs instead.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/program.c
@@ -231,12 +231,16 @@ int Program_type_arg(Program *prog, PyObject *type_obj, bool can_be_none,
 
 static void *drgnpy_begin_blocking(struct drgn_program *prog, void *arg)
 {
-	return PyEval_SaveThread();
+	PyThreadState *state = PyThreadState_GetUnchecked();
+	if (state)
+		PyEval_ReleaseThread(state);
+	return state;
 }
 
 static void drgnpy_end_blocking(struct drgn_program *prog, void *arg, void *state)
 {
-	PyEval_RestoreThread(state);
+	if (state)
+		PyEval_RestoreThread(state);
 }
 
 static Program *Program_new(PyTypeObject *subtype, PyObject *args,

libdrgn/drgn.h
@@ -1307,7 +1307,8 @@ typedef void *drgn_program_begin_blocking_fn(struct drgn_program *prog,
  *
  * @param[in] arg @c callback_arg passed to @ref
  * drgn_program_set_blocking_callback().
- * @param[in] state Return value of @ref drgn_program_begin_blocking_fn().
+ * @param[in] state Return value of matching call to @ref
+ * drgn_program_begin_blocking_fn().
  */
 typedef void drgn_program_end_blocking_fn(struct drgn_program *prog,
 					  void *arg, void *state);
@@ -1319,6 +1320,7 @@ typedef void drgn_program_end_blocking_fn(struct drgn_program *prog,
  * long-running computations. They are intended for things like releasing the
  * [global interpreter
  * lock](https://docs.python.org/3/glossary.html#term-global-interpreter-lock).
+ * Calls to these callbacks may be nested, but they will always be matched.
  *
  * @param[in] begin_callback Callback called before a blocking operation. Can be
  * @c NULL to unset.

libdrgn/python/drgnpy.h
@@ -40,6 +40,10 @@
     } while (0)
 #endif
 
+#if PY_VERSION_HEX < 0x030d00a1
+#define PyThreadState_GetUnchecked _PyThreadState_UncheckedGet
+#endif
+
 #define DRGNPY_PUBLIC __attribute__((__visibility__("default")))
 
 // PyLong_From* and PyLong_As* for stdint.h types. These use _Generic for

commit 2145ab8c03bfe52b22acd892547e96502db4103a

libdrgn: python: fix race on nested drgn_begin_blocking()
If drgn_begin_blocking() is called without the GIL held (because we're
in a nested blocking section), and another thread has acquired the GIL,
then PyThreadState_GetUnchecked() can return the PyThreadState for that
thread, which we then mistakenly release. This causes all sorts of
crashes. I saw it spuriously when running setup.py test -K, which
downloads kernels in a separate thread while running local tests. I was
also able to reproduce it with the following:

  import threading
  import time

  from drgn import Program
  from tests.resources import get_resource

  def lol():
      while True:
          time.sleep(0.0001)  # Releases GIL.

  threading.Thread(target=lol, daemon=True).start()

  crashme_core = get_resource("crashme.core")
  crashme_path = get_resource("crashme")
  while True:
      prog = Program()
      prog.set_core_dump(crashme_core)
      prog.set_enabled_debug_info_finders([])
      prog.load_debug_info([crashme_path], main=True)

Note since CPython commit f8abfa331421 ("gh-103323: Get the "Current"
Thread State from a Thread-Local Variable (gh-103324)") (in v3.12),
PyThreadState_GetUnchecked() will never return the PyThreadState of
another thread, so this only affects earlier Python versions.

The documentation states that the GIL must be held when calling
PyThreadState_Get(). It is unclear if this also holds for
PyThreadState_GetUnchecked(). For the existing versions of CPython, it
looks like it will work without the GIL, but it's sketchy to rely on
this. Unfortunately, with the existing APIs, it's the best we can do.

Fixes: 48b962969540 ("libdrgn: python: allow nesting blocking callbacks")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/program.c
@@ -309,9 +309,13 @@ int Program_type_arg(Program *prog, PyObject *type_obj, bool can_be_none,
 
 void *drgn_begin_blocking(void)
 {
-	PyThreadState *state = PyThreadState_GetUnchecked();
-	if (state)
-		PyEval_ReleaseThread(state);
+	PyThreadState *state = PyGILState_GetThisThreadState();
+	// If the current thread doesn't hold the GIL (because it's not in
+	// Python or because this blocking section is nested in another), don't
+	// do anything.
+	if (!state || state != PyThreadState_GetUnchecked())
+		return NULL;
+	PyEval_ReleaseThread(state);
 	return state;
 }
 