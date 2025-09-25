a456c8d453762e212ad0d06e02251140d1cfbd9c
Stephen Brennan <stephen.s.brennan@oracle.com>
2025-09-25 18:24:11 +0000

    tests: linux_kernel: fix test_pgtable_l5_enabled
    Recently, kernel 4.14's vmtests have intermittently started failing on
    test_pgtable_l5_enabled with "AssertionError: False != True". This
    started after f365f33c ("CI: use KVM") - which means that we're now
    using the host CPU rather than an emulated one.
    
    It appears that some Github CI systems have the LA57 (5-level paging)
    feature enabled. Further, it seems that in 4.14, the LA57 flag is not
    cleared in /proc/cpuinfo when 5-level paging is not in use. So we need a
    better way to detect 5-level paging being enabled.
    
    The userspace address range is expanded with 5-level paging. So we can
    attempt to mmap an address which requires more than 48 VA bits. As long
    as we use MAP_FIXED, or we verify that the hinted address range is not
    used, we should get the hinted address if supported. If we get the high
    address, then we must have 5-level paging enabled. Otherwise, it must be
    disabled or unsupported.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- tests/linux_kernel/helpers/test_boot.py
+++ tests/linux_kernel/helpers/test_boot.py
@@ -1,19 +1,55 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+from ctypes import c_int, c_int64, c_size_t, c_void_p
+import mmap
 import re
 import unittest
 
 from _drgn_util.platform import NORMALIZED_MACHINE_NAME
 from drgn.helpers.linux.boot import pgtable_l5_enabled
-from tests.linux_kernel import LinuxKernelTestCase
+from tests.linux_kernel import LinuxKernelTestCase, _c
+
+
+def first_available_slot(size, min_addr):
+    for line in open("/proc/self/maps"):
+        start_str, end_str = re.match(r"([0-9a-f]+)-([0-9a-f]+).*", line).groups()
+        start = int(start_str, 16)
+        end = int(end_str, 16)
+        if start >= min_addr + size:
+            break
+        elif end >= min_addr:
+            min_addr = end
+    return min_addr
+
+
+def can_mmap_high_address():
+    mmap_func = _c.mmap
+    mmap_func.argtypes = [c_void_p, c_size_t, c_int, c_int, c_int, c_int64]
+    mmap_func.restype = c_void_p
+    munmap_func = _c.munmap
+    munmap_func.argtypes = [c_void_p, c_size_t]
+    hint_addr = first_available_slot(mmap.PAGESIZE, 1 << 48)
+
+    ret = mmap_func(
+        hint_addr,
+        mmap.PAGESIZE,
+        mmap.PROT_READ | mmap.PROT_WRITE,
+        # Ideally we would use MAP_FIXED, but its value is not exposed by the
+        # mmap module, and it varies by architecture. Having identified a free
+        # slot in our memory mappings (and hopefully not changing them since
+        # then), we can be reasonably confident that we should get the address
+        # we hinted anyway.
+        mmap.MAP_PRIVATE | mmap.MAP_ANONYMOUS,
+        -1,
+        0,
+    )
+    if ret != c_void_p(-1).value:
+        munmap_func(ret, mmap.PAGESIZE)
+    return ret == hint_addr
 
 
 class TestBoot(LinuxKernelTestCase):
     @unittest.skipUnless(NORMALIZED_MACHINE_NAME == "x86_64", "machine is not x86_64")
     def test_pgtable_l5_enabled(self):
-        with open("/proc/cpuinfo", "r") as f:
-            self.assertEqual(
-                pgtable_l5_enabled(self.prog),
-                bool(re.search(r"flags\s*:.*\bla57\b", f.read())),
-            )
+        self.assertEqual(pgtable_l5_enabled(self.prog), can_mmap_high_address())
