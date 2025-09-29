osandov/drgn

commit 820d25ae29d831fc77daf6f4a77b098fa6c13d24

helpers: Add cgroup v2 helpers to traverse hierarchy
Add helpers to iterate over all children and all descendants (pre-order
walk only) of a cgroup given its css.

Signed-off-by: Andrey Ignatov <rdna@fb.com>

drgn/helpers/linux/cgroup.py
@@ -12,11 +12,16 @@ supported.
 
 from drgn import NULL, cast, container_of
 from drgn.helpers.linux.kernfs import kernfs_name, kernfs_path
+from drgn.helpers.linux.list import list_for_each_entry
 
 __all__ = [
     "cgroup_name",
     "cgroup_parent",
     "cgroup_path",
+    "css_for_each_child",
+    "css_for_each_descendant_pre",
+    "css_next_child",
+    "css_next_descendant_pre",
     "sock_cgroup_ptr",
 ]
 
@@ -64,3 +69,89 @@ def cgroup_path(cgrp):
     :rtype: bytes
     """
     return kernfs_path(cgrp.kn)
+
+
+def css_next_child(pos, parent):
+    """
+    .. c:function:: struct cgroup_subsys_state *css_next_child(struct cgroup_subsys_state *pos, struct cgroup_subsys_state *parent)
+
+    Get the next child (or ``NULL`` if there is none) of the given parent
+    starting from the given position (``NULL`` to initiate traversal).
+    """
+    if not pos:
+        next_ = container_of(
+            parent.children.next, "struct cgroup_subsys_state", "sibling"
+        )
+    elif not (pos.flags & pos.prog_["CSS_RELEASED"]):
+        next_ = container_of(pos.sibling.next, "struct cgroup_subsys_state", "sibling")
+    else:
+        serial_nr = pos.serial_nr.value_()  # Read once and cache.
+        for next_ in list_for_each_entry(
+            "struct cgroup_subsys_state", parent.children.address_of_(), "sibling"
+        ):
+            if next_.serial_nr > serial_nr:
+                break
+
+    if next_.sibling.address_of_() != parent.children.address_of_():
+        return next_
+    return NULL(next_.prog_, "struct cgroup_subsys_state *")
+
+
+def css_next_descendant_pre(pos, root):
+    """
+    .. c:function:: struct cgroup_subsys_state *css_next_descendant_pre(struct cgroup_subsys_state *pos, struct cgroup_subsys_state *root)
+
+    Get the next pre-order descendant (or ``NULL`` if there is none) of the
+    given css root starting from the given position (``NULL`` to initiate
+    traversal).
+    """
+    # If first iteration, visit root.
+    if not pos:
+        return root
+
+    # Visit the first child if exists.
+    null = NULL(pos.prog_, "struct cgroup_subsys_state *")
+    next_ = css_next_child(null, pos)
+    if next_:
+        return next_
+
+    # No child, visit my or the closest ancestor's next sibling.
+    while pos != root:
+        next_ = css_next_child(pos, pos.parent)
+        if next_:
+            return next_
+        pos = pos.parent
+
+    return NULL(root.prog_, "struct cgroup_subsys_state *")
+
+
+def _css_for_each_impl(next_fn, css):
+    pos = NULL(css.prog_, "struct cgroup_subsys_state *")
+    while True:
+        pos = next_fn(pos, css)
+        if not pos:
+            break
+        if pos.flags & pos.prog_["CSS_ONLINE"]:
+            yield pos
+
+
+def css_for_each_child(css):
+    """
+    .. c:function:: css_for_each_child(struct cgroup_subsys_state *css)
+
+    Iterate through children of the given css.
+
+    :return: Iterator of ``struct cgroup_subsys_state *`` objects.
+    """
+    return _css_for_each_impl(css_next_child, css)
+
+
+def css_for_each_descendant_pre(css):
+    """
+    .. c:function:: css_for_each_descendant_pre(struct cgroup_subsys_state *css)
+
+    Iterate through the given css's descendants in pre-order.
+
+    :return: Iterator of ``struct cgroup_subsys_state *`` objects.
+    """
+    return _css_for_each_impl(css_next_descendant_pre, css)

commit 01e6c45f00d2e724b12df61cfc2ba39218594a7b

drgn.helpers.linux.cgroup: fix UnboundLocalError in css_next_child()
Every once in awhile,
tests.linux_kernel.helpers.test_cgroup.TestCgroup.test_css_for_each_descendant_pre
fails with the following error from css_next_child():

  UnboundLocalError: local variable 'next_' referenced before assignment

This happens in the case that the given pos has the CSS_RELEASED flag
set and the parent doesn't have any more children.

The cause is a subtle translation error from the kernel code, where
list_for_each_entry() leaves the cursor variable set to the head, to
drgn, where an iterator like list_for_each_entry() doesn't set the loop
variable if the iterator is empty.

Fix it by explicitly returning NULL in that case.

Fixes: 820d25ae29d8 ("helpers: Add cgroup v2 helpers to traverse hierarchy")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/cgroup.py
@@ -115,6 +115,8 @@ def css_next_child(pos: Object, parent: Object) -> Object:
         ):
             if next_.serial_nr > serial_nr:
                 break
+        else:
+            return NULL(pos.prog_, "struct cgroup_subsys_state *")
 
     if next_.sibling.address_of_() != parent.children.address_of_():
         return next_