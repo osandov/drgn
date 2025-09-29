osandov/drgn

commit 3c8aee388c7ebc5f7c562bd830c450ff35a36121

drgn.helpers.linux.vmstat: add zone_page_state()
The crash kmem -f command will use this. Also clarify the documentation
for the global counter functions.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/vmstat.py
@@ -9,7 +9,7 @@ The ``drgn.helpers.linux.vmstat`` module provides helpers for reading virtual
 memory statistics.
 """
 
-from drgn import IntegerLike, Program
+from drgn import IntegerLike, Object, Program
 from drgn.helpers.common.prog import takes_program_or_default
 
 __all__ = (
@@ -22,7 +22,7 @@ __all__ = (
 @takes_program_or_default
 def global_node_page_state(prog: Program, item: IntegerLike) -> int:
     """
-    Get the value of a node VM statistic.
+    Get the global value of a node VM statistic.
 
     >>> global_node_page_state(prog["NR_FILE_PAGES"])
     2257904
@@ -32,10 +32,17 @@ def global_node_page_state(prog: Program, item: IntegerLike) -> int:
     return max(prog["vm_node_stat"][item].counter.value_(), 0)
 
 
+def zone_page_state(zone: Object, item: IntegerLike) -> int:
+    """
+    Get the value of a zone VM statistic in a single zone.
+    """
+    return max(zone.vm_stat[item].counter.value_(), 0)
+
+
 @takes_program_or_default
 def global_zone_page_state(prog: Program, item: IntegerLike) -> int:
     """
-    Get the value of a zone VM statistic.
+    Get the global value of a zone VM statistic.
 
     >>> global_zone_page_state(prog["NR_MLOCK"])
     1562

commit 8f7dcf06f17756a9bbe3e4004bd10610852f76a4

drgn.helpers.linux.vmstat: add zone_page_state() to __all__
Fixes: 3c8aee388c7e ("drgn.helpers.linux.vmstat: add zone_page_state()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/vmstat.py
@@ -16,6 +16,7 @@ __all__ = (
     "global_node_page_state",
     "global_zone_page_state",
     "nr_free_pages",
+    "zone_page_state",
 )
 
 