15ddb0246c33ceb771bd5417db44db99817c7291
Omar Sandoval <osandov@osandov.com>
2025-10-07 15:53:54 -0700

    drgn.helpers.linux.vmstat: add global_{numa,vm}_event_state() helpers
    These are needed for the crash kmem -V command.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/helpers/linux/vmstat.py
+++ drgn/helpers/linux/vmstat.py
@@ -9,11 +9,15 @@ The ``drgn.helpers.linux.vmstat`` module provides helpers for reading virtual
 memory statistics.
 """
 
-from drgn import IntegerLike, Object, Program
+from drgn import IntegerLike, Object, ObjectNotFoundError, Program
 from drgn.helpers.common.prog import takes_program_or_default
+from drgn.helpers.linux.cpumask import for_each_online_cpu
+from drgn.helpers.linux.percpu import per_cpu
 
 __all__ = (
     "global_node_page_state",
+    "global_numa_event_state",
+    "global_vm_event_state",
     "global_zone_page_state",
     "nr_free_pages",
     "zone_page_state",
@@ -60,3 +64,49 @@ def global_zone_page_state(prog: Program, item: IntegerLike) -> int:
 def nr_free_pages(prog: Program) -> int:
     """Get the number of free memory pages."""
     return global_zone_page_state(prog["NR_FREE_PAGES"])
+
+
+@takes_program_or_default
+def global_numa_event_state(prog: Program, item: IntegerLike) -> int:
+    """
+    Get the global value of a NUMA statistic.
+
+    .. note::
+
+        This is only valid if the kernel was compiled with ``CONFIG_NUMA``.
+        Before Linux 4.14, these items (e.g., ``NUMA_HIT``) were zone
+        statistics, so on kernel versions before 4.14, this is an alias of
+        :func:`global_zone_page_state()`.
+
+    :param item: ``enum numa_stat_item`` (or ``enum zone_stat_item`` on Linux <
+        4.14)
+    """
+    # The variable was renamed from "vm_numa_stat" to "vm_numa_event" in Linux
+    # kernel commit f19298b9516c ("mm/vmstat: convert NUMA statistics to basic
+    # NUMA counters") (in v5.14). Additionally, before Linux kernel commit
+    # 3a321d2a3dde ("mm: change the call sites of numa statistics items") (in
+    # v4.14), the items were zone statistics and the variable didn't exist.
+    try:
+        array = prog["vm_numa_event"]
+    except ObjectNotFoundError:
+        try:
+            array = prog["vm_numa_stat"]
+        except ObjectNotFoundError:
+            return global_zone_page_state(prog, item)
+    return array[item].counter.value_()
+
+
+@takes_program_or_default
+def global_vm_event_state(prog: Program, item: IntegerLike) -> int:
+    """
+    Get the global value of a virtual memory event statistic.
+
+    .. note::
+
+        This is only valid if the kernel was compiled with
+        ``CONFIG_VM_EVENT_COUNTERS``.
+
+    :param item: ``enum vm_event_item``
+    """
+    event = prog["vm_event_states"].event[item]
+    return sum(per_cpu(event, cpu).value_() for cpu in for_each_online_cpu(prog))
--- tests/linux_kernel/helpers/test_vmstat.py
+++ tests/linux_kernel/helpers/test_vmstat.py
@@ -1,7 +1,13 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from drgn.helpers.linux.vmstat import nr_free_pages
+import re
+
+from drgn.helpers.linux.vmstat import (
+    global_numa_event_state,
+    global_vm_event_state,
+    nr_free_pages,
+)
 from tests.linux_kernel import LinuxKernelTestCase, meminfo_field_in_pages
 
 
@@ -11,4 +17,30 @@ class TestVmstat(LinuxKernelTestCase):
             nr_free_pages(self.prog),
             meminfo_field_in_pages("MemFree"),
             delta=1024 * 1024 * 1024,
+        )
+
+    def test_global_numa_event_state(self):
+        with open("/proc/vmstat", "r") as f:
+            for line in f:
+                match = re.match(r"numa_hit\s+([0-9]+)", line)
+                if match:
+                    expected = int(match.group(1))
+                    break
+            else:
+                self.skipTest("kernel does not support NUMA statistics")
+        self.assertAlmostEqual(
+            global_numa_event_state(self.prog["NUMA_HIT"]), expected, delta=1024 * 1024
+        )
+
+    def test_global_vm_event_state(self):
+        with open("/proc/vmstat", "r") as f:
+            for line in f:
+                match = re.match(r"pgmajfault\s+([0-9]+)", line)
+                if match:
+                    expected = int(match.group(1))
+                    break
+            else:
+                self.skipTest("kernel does not support VM event statistics")
+        self.assertAlmostEqual(
+            global_vm_event_state(self.prog["PGMAJFAULT"]), expected, delta=1024 * 1024
         )
