osandov/drgn

commit 26a05fef0ad52dd225cde5bd55437ec5656e7a7b

crash: port kmem -f command
This one dumps and verifies the page allocator free lists. The format
that crash uses is outdated in several regards:

* "MEM_MAP" is dubious with SPARSEMEM. The field it used to display was
  removed long ago, in Linux kernel commit a0140c1d8563 ("[PATCH] remove
  zone_mem_map") (in v2.6.17). We approximate it as
  pfn_to_page(zone->zone_start_pfn), which is the same thing crash does.
* "START_MAPNR" is similarly dubious with SPARSEMEM, and was also
  removed long ago in Linux kernel history commit e07316f9c849 ("[PATCH]
  free_area_init cleanup") (in v2.5.37). Crash's approximation of it is
  pfn_to_page(zone->zone_start_pfn) - pfn_to_page(pgdat->node_start_pfn).
  From what I can tell, that's different from how zone->zone_start_mapnr
  was actually used (as an index into the global mem_map), and is also
  different from how crash shows the same field in kmem -n.
  zone->zone_start_pfn is a better approximation. At that point, we
  might as well rename it to "START_PFN", which we do.
* "AREA" is too vague. We rename it to "ORDER".
* "FREE_AREA_STRUCT" actually contains each free_list list_head address
  since Linux kernel commit b2a0ac8875a0 ("Split the free lists for
  movable and unmovable allocations") (in v2.6.24), so we rename it to
  "FREE_LIST". We also add a "MIGRATE" column to make it clear why there
  are multiple lists per order.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/commands/_builtin/crash/_mm.py
@@ -16,8 +16,10 @@ from drgn.commands import (
     mutually_exclusive_group,
 )
 from drgn.commands.crash import CrashDrgnCodeBuilder, crash_command
+from drgn.helpers import ValidationError
 from drgn.helpers.common.format import (
     CellFormat,
+    RowOptions,
     escape_ascii_string,
     number_in_binary_units,
     print_table,
@@ -29,12 +31,16 @@ from drgn.helpers.linux.hugetlb import (
     huge_page_size,
     hugetlb_total_usage,
 )
+from drgn.helpers.linux.list import validate_list_count_nodes
 from drgn.helpers.linux.mm import (
+    PFN_PHYS,
     for_each_vmap_area,
+    pfn_to_page,
     totalram_pages,
     vm_commit_limit,
     vm_memory_committed,
 )
+from drgn.helpers.linux.mmzone import for_each_online_pgdat
 from drgn.helpers.linux.percpu import per_cpu_ptr
 from drgn.helpers.linux.slab import (
     SlabCorruptionError,
@@ -52,7 +58,11 @@ from drgn.helpers.linux.swap import (
     swap_usage_in_pages,
     total_swapcache_pages,
 )
-from drgn.helpers.linux.vmstat import global_node_page_state, nr_free_pages
+from drgn.helpers.linux.vmstat import (
+    global_node_page_state,
+    nr_free_pages,
+    zone_page_state,
+)
 
 
 @crash_command(
@@ -128,6 +138,189 @@ def _crash_cmd_ptob(
     PAGE_SHIFT = prog["PAGE_SHIFT"].value_()
     for pfn in args.pfn:
         print(f"{pfn:x}: {pfn << PAGE_SHIFT:x}")
+
+
+def _kmem_free(prog: Program, drgn_arg: bool) -> None:
+    if drgn_arg:
+        code = CrashDrgnCodeBuilder(prog)
+        code.add_from_import("drgn.helpers.linux.list", "validate_list_count_nodes")
+        code.add_from_import("drgn.helpers.linux.mm", "PFN_PHYS", "pfn_to_page")
+        code.add_from_import("drgn.helpers.linux.mmzone", "for_each_online_pgdat")
+        code.add_from_import(
+            "drgn.helpers.linux.vmstat", "nr_free_pages", "zone_page_state"
+        )
+        code.append(
+            """\
+actual_free_pages = 0
+for pgdat in for_each_online_pgdat():
+    for zone in pgdat.node_zones:
+        name = zone.name
+        size = zone.spanned_pages
+        if size == 0:
+            continue
+        free = zone_page_state(zone, prog["NR_FREE_PAGES"])
+        start_pfn = zone.zone_start_pfn.read_()
+        mem_map = pfn_to_page(start_pfn)
+        start_paddr = PFN_PHYS(start_pfn)
+
+        for order, free_area in enumerate(zone.free_area):
+            block_size = prog["PAGE_SIZE"] << order
+            for migrate_type, free_list in enumerate(free_area.free_list):
+"""
+        )
+        if prog.flags & ProgramFlags.IS_LIVE:
+            code.add_from_import("drgn", "FaultError")
+            code.add_from_import("drgn.helpers", "ValidationError")
+            code.append(
+                """\
+                # This is racy. Retry a few times if needed.
+                num_attempts = 10
+                for attempt in range(num_attempts):
+                    try:
+        """
+            )
+        code.append(
+            """\
+                blocks = validate_list_count_nodes(free_list.address_of_())
+"""
+        )
+        if prog.flags & ProgramFlags.IS_LIVE:
+            code.append(
+                """\
+                    except (FaultError, ValidationError):
+                        if attempt == num_attempts - 1:
+                            raise
+"""
+            )
+        code.append(
+            """\
+                pages = blocks << order
+                actual_free_pages += pages
+
+expected_free_pages = nr_free_pages()
+"""
+        )
+        code.print()
+        return
+
+    migrate_types = {
+        enumerator.value: enumerator.name[len("MIGRATE_") :]
+        # enum migratetype was anonymous until Linux kernel commit a6ffdc07847e
+        # ("mm: use is_migrate_highatomic() to simplify the code") (in v4.12).
+        for enumerator in prog["MIGRATE_UNMOVABLE"].type_.enumerators  # type: ignore[union-attr]
+        if enumerator.name.startswith("MIGRATE_")
+    }
+
+    pgdats = list(for_each_online_pgdat(prog))
+    if len(pgdats) > 1:
+        node_header: Sequence[Any] = (CellFormat("NODE", ">"),)
+    else:
+        node_header = ()
+
+    rows: List[Sequence[Any]] = []
+    first = True
+    total_free_pages = 0
+    for pgdat in pgdats:
+        for i, zone in enumerate(pgdat.node_zones):
+            if len(pgdats) > 1:
+                node_row: Sequence[Any] = (pgdat.node_id.value_(),)
+            else:
+                node_row = ()
+            size = zone.spanned_pages.value_()
+
+            if first:
+                first = False
+            else:
+                rows.append(())
+            rows.append(
+                RowOptions(
+                    (
+                        *node_header,
+                        CellFormat("ZONE", "^"),
+                        "NAME",
+                        CellFormat("SIZE", ">"),
+                        CellFormat("FREE", ">"),
+                        CellFormat("MEM_MAP", "^"),
+                        "START_PADDR",
+                        "START_PFN",
+                    ),
+                    group=1,
+                )
+            )
+
+            if size == 0:
+                free = 0
+                mem_map_cell = start_paddr_cell = start_pfn_cell = CellFormat(0, "^")
+            else:
+                free = zone_page_state(zone, prog["NR_FREE_PAGES"])
+                start_pfn = zone.zone_start_pfn.read_()
+                mem_map_cell = CellFormat(pfn_to_page(start_pfn).value_(), "^x")
+                start_paddr_cell = CellFormat(PFN_PHYS(start_pfn).value_(), "^x")
+                start_pfn_cell = CellFormat(start_pfn.value_(), "^")
+            rows.append(
+                RowOptions(
+                    (
+                        *node_row,
+                        CellFormat(i, "^"),
+                        escape_ascii_string(zone.name.string_(), escape_backslash=True),
+                        size,
+                        free,
+                        mem_map_cell,
+                        start_paddr_cell,
+                        start_pfn_cell,
+                    ),
+                    group=1,
+                )
+            )
+
+            if size == 0:
+                continue
+
+            rows.append(
+                (
+                    CellFormat("ORDER", "^"),
+                    CellFormat("SIZE", ">"),
+                    "MIGRATE",
+                    CellFormat("FREE_LIST", "^"),
+                    CellFormat("BLOCKS", ">"),
+                    CellFormat("PAGES", ">"),
+                )
+            )
+            for order, free_area in enumerate(zone.free_area):
+                size = (prog["PAGE_SIZE"].value_() << order) // 1024
+                size_cell = CellFormat(f"{size}k", ">")
+                for migrate_type, free_list in enumerate(free_area.free_list):
+                    blocks: Any = "[CORRUPTED]"
+                    pages: Any = ""
+                    # Walking free lists is racy. Retry a limited number of
+                    # times on live kernels.
+                    for _ in range(10):
+                        try:
+                            blocks = validate_list_count_nodes(free_list.address_of_())
+                        except (FaultError, ValidationError):
+                            if not (prog.flags & ProgramFlags.IS_LIVE):
+                                break
+                        else:
+                            pages = blocks << order
+                            total_free_pages += pages
+                    rows.append(
+                        (
+                            CellFormat(order, "^"),
+                            size_cell,
+                            migrate_types[migrate_type],
+                            CellFormat(free_list.address_, "^x"),
+                            blocks,
+                            pages,
+                        ),
+                    )
+    print_table(rows)
+
+    free_pages = nr_free_pages(prog)
+    if free_pages == total_free_pages:
+        verified = "verified"
+    else:
+        verified = f"found {total_free_pages}"
+    print(f"\nnr_free_pages: {free_pages}  ({verified})")
 
 
 def _kmem_info(
@@ -544,6 +737,12 @@ if cache:
     arguments=(
         mutually_exclusive_group(
             argument(
+                "-f",
+                dest="free",
+                action="store_true",
+                help="display and verify page allocator free lists",
+            ),
+            argument(
                 "-i",
                 dest="info",
                 action="store_true",
@@ -616,6 +815,8 @@ def _crash_cmd_kmem(
     else:
         slab_cache_names = None
 
+    if args.free:
+        return _kmem_free(prog, args.drgn)
     if args.info:
         return _kmem_info(prog, args.drgn)
     if args.vmalloc:

drgn/helpers/linux/mmzone.py
@@ -15,7 +15,6 @@ from drgn import IntegerLike, Object, ObjectNotFoundError, Program
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.nodemask import for_each_online_node
 
-
 __all__ = (
     "NODE_DATA",
     "for_each_online_pgdat",

tests/linux_kernel/crash_commands/test_mm.py
@@ -63,6 +63,56 @@ skip_unless_kmem_s_supported = unittest.skipUnless(
 
 
 class TestKmem(CrashCommandTestCase):
+    def test_f(self):
+        cmd = self.check_crash_command("kmem -f")
+
+        expected = set(
+            re.findall(
+                r"^Node\s+([0-9]+)\s*,\s*zone\s+(\w+)",
+                Path("/proc/zoneinfo").read_text(),
+                flags=re.MULTILINE,
+            )
+        )
+        actual = set()
+        header = 0
+        for line in cmd.stdout.splitlines():
+            if header == 0:
+                match = re.match(r"(NODE\s+)?ZONE", line)
+                if match:
+                    header = 2 if match.group(1) else 1
+            else:
+                if header == 1:
+                    actual.add(("0", re.match(r"\s*[0-9]+\s+(\w+)", line).group(1)))
+                else:
+                    actual.add(re.match(r"\s*([0-9]+)\s+[0-9]+\s+(\w+)", line).groups())
+                header = 0
+        # Since Linux kernel commit b2bd8598195f ("mm, vmstat: print
+        # non-populated zones in zoneinfo") (in v4.12), these should be equal,
+        # but before that, /proc/zoneinfo doesn't include all zones.
+        self.assertGreaterEqual(actual, expected)
+        self.assertRegex(cmd.stdout, r"nr_free_pages: [0-9]+")
+
+        for variable in (
+            "zone",
+            "name",
+            "size",
+            "free",
+            "mem_map",
+            "start_paddr",
+            "start_pfn",
+            "order",
+            "block_size",
+            "migrate_type",
+            "blocks",
+            "pages",
+        ):
+            self.assertIn(variable, cmd.drgn_option.globals)
+        for variable in (
+            "expected_free_pages",
+            "actual_free_pages",
+        ):
+            self.assertIsInstance(cmd.drgn_option.globals[variable], int)
+
     def test_i(self):
         cmd = self.check_crash_command("kmem -i")
         for label in (

commit 71de528c570d7e058afbe83eb8c84ae315ec90be

crash: stop on list validation success in kmem -f
This is the exact same bug as in commit eeb8d70e9400 ("crash: stop on
list validation success in kmem -f --drgn") except it's in the actual
command code.

Fixes: 26a05fef0ad5 ("crash: port kmem -f command")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/commands/_builtin/crash/_mm.py
@@ -424,6 +424,7 @@ expected_free_pages = nr_free_pages()
                         else:
                             pages = blocks << order
                             total_free_pages += pages
+                            break
                     rows.append(
                         (
                             CellFormat(order, "^"),