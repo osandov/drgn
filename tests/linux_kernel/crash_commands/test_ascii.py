98090c8c1d0b807fdd5e8728129a9aa6ecc757c0
Omar Sandoval <osandov@osandov.com>
2025-10-17 14:07:03 -0700

    tests: add test for too large argument to crash ascii command
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/linux_kernel/crash_commands/test_ascii.py
+++ tests/linux_kernel/crash_commands/test_ascii.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025 Oracle and/or its affiliates
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+from drgn.commands import CommandArgumentError
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
 
 
@@ -9,3 +10,12 @@ class TestAscii(CrashCommandTestCase):
         cmd = self.check_crash_command("ascii 62696c2f7273752f")
         self.assertRegex(cmd.stdout, r"(?m)^62696c2f7273752f: /usr/lib$")
         self.assertEqual(cmd.drgn_option.globals["bytestring"], b"/usr/lib")
+
+    def test_too_large(self):
+        command = "ascii fedcba9876543210f"
+        self.assertRaisesRegex(
+            CommandArgumentError, "too large", self.run_crash_command, command
+        )
+        self.assertRaisesRegex(
+            ValueError, "too large", self.run_crash_command_drgn_option, command
+        )
