osandov/drgn

commit 1652183026a89c6e848375581e26a8e463f59158

Add experimental helpers for calling kernel functions and writing to memory
These are highly-requested features that I didn't implement for awhile
because I thought they would be too difficult or hacky. The approach
that I finally came up with is slightly deranged but solid: we manually
generate a kernel module ELF file and machine code to do a function
call. An alternative approach that I tried was generating C source code
for a kernel module and building it, but that relies on having
kernel-devel and a compatible toolchain installed, and it's slow. (Kudos
to Matthew Wilcox for suggesting the kernel module approach last year:
https://lwn.net/Articles/953256/.)

For now, this is under a new drgn.helpers.experimental package, in the
drgn.helpers.experimental.kmodify module, with no stability guarantees,
but we still have tests for it.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

tests/linux_kernel/helpers/test_kmodify.py
@@ -1,0 +1,325 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import os
+import random
+import unittest
+
+from _drgn_util.platform import NORMALIZED_MACHINE_NAME
+from drgn import FaultError, Object
+from drgn.helpers.experimental.kmodify import (
+    call_function,
+    pass_pointer,
+    write_memory,
+    write_object,
+)
+from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
+
+skip_unless_have_kmodify = unittest.skipUnless(
+    NORMALIZED_MACHINE_NAME == "x86_64",
+    f"kmodify is not implemented for {NORMALIZED_MACHINE_NAME}",
+)
+
+
+@skip_unless_have_test_kmod
+@skip_unless_have_kmodify
+class TestCallFunction(LinuxKernelTestCase):
+    def assert_called(self, name, args, expected_return_value=None):
+        if expected_return_value is None:
+            expected_return_value = Object(self.prog, "void")
+        before = self.prog[f"drgn_kmodify_test_{name}_called"].read_()
+        return_value = call_function(self.prog["drgn_kmodify_test_" + name], *args)
+        self.assertEqual(
+            self.prog[f"drgn_kmodify_test_{name}_called"].read_(),
+            before + 1,
+        )
+        self.assertIdentical(return_value, expected_return_value)
+
+    def assert_returns(self, name, expected_return_value):
+        self.assert_called(name, (), expected_return_value)
+
+    def test_void_return(self):
+        self.assert_returns("void_return", Object(self.prog, "void"))
+
+    def test_integer_returns(self):
+        for name, return_value in (
+            ("signed_char", Object(self.prog, "signed char", -66)),
+            ("unsigned_char", Object(self.prog, "unsigned char", 200)),
+            ("short", Object(self.prog, "short", -666)),
+            ("unsigned_short", Object(self.prog, "unsigned short", 7777)),
+            ("int", Object(self.prog, "int", -12345)),
+            ("unsigned_int", Object(self.prog, "unsigned int", 54321)),
+            ("long", Object(self.prog, "long", -2468013579)),
+            ("unsigned_long", Object(self.prog, "unsigned long", 4000000000)),
+            ("long_long", Object(self.prog, "long long", -9080706050403020100)),
+            (
+                "unsigned_long_long",
+                Object(self.prog, "unsigned long long", 12345678909876543210),
+            ),
+        ):
+            with self.subTest(name=name):
+                self.assert_returns(name + "_return", return_value)
+
+    def test_integer_args(self):
+        self.assert_called(
+            "signed_args",
+            (
+                Object(self.prog, "signed char", -66),
+                Object(self.prog, "short", -666),
+                Object(self.prog, "int", -12345),
+                Object(self.prog, "long", -2468013579),
+                Object(self.prog, "long long", -9080706050403020100),
+            ),
+        )
+        self.assert_called(
+            "unsigned_args",
+            (
+                Object(self.prog, "unsigned char", 200),
+                Object(self.prog, "unsigned short", 7777),
+                Object(self.prog, "unsigned int", 54321),
+                Object(self.prog, "unsigned long", 4000000000),
+                Object(self.prog, "unsigned long long", 12345678909876543210),
+            ),
+        )
+
+    def test_integer_literal_args(self):
+        self.assert_called(
+            "signed_args",
+            (
+                -66,
+                -666,
+                -12345,
+                -2468013579,
+                -9080706050403020100,
+            ),
+        )
+        self.assert_called(
+            "unsigned_args",
+            (
+                200,
+                7777,
+                54321,
+                4000000000,
+                12345678909876543210,
+            ),
+        )
+
+    def test_many_args(self):
+        self.assert_called(
+            "many_args",
+            (
+                48,
+                -66,
+                -666,
+                -12345,
+                -2468013579,
+                -9080706050403020100,
+                200,
+                7777,
+                54321,
+                4000000000,
+                12345678909876543210,
+            ),
+        )
+
+    def test_enum_returns(self):
+        self.assert_returns(
+            "enum_return", Object(self.prog, "enum drgn_kmodify_enum", 2)
+        )
+
+    def test_enum_args(self):
+        args = (
+            self.prog["DRGN_KMODIFY_ONE"],
+            pass_pointer(Object(self.prog, "enum drgn_kmodify_enum", 2)),
+        )
+        self.assert_called("enum_args", args)
+        self.assertIdentical(
+            args[1].object, Object(self.prog, "enum drgn_kmodify_enum", 3)
+        )
+
+    def test_pointer_returns(self):
+        self.assert_returns(
+            "pointer_return", self.prog["drgn_kmodify_test_ptr"].read_()
+        )
+
+    def test_pointer_args(self):
+        self.assert_called(
+            "pointer_args", (self.prog["drgn_kmodify_test_ptr"].read_(),)
+        )
+
+    def test_string_args(self):
+        for msg, f in (
+            ("str", lambda t, s: s),
+            ("bytes", lambda t, s: s.encode()),
+            (
+                "array",
+                lambda t, s: Object(
+                    self.prog,
+                    self.prog.array_type(self.prog.type(t), len(s) + 1),
+                    s.encode(),
+                ),
+            ),
+            ("str pointer", lambda t, s: pass_pointer(s)),
+            ("bytes pointer", lambda t, s: pass_pointer(s.encode())),
+            (
+                "array pointer",
+                lambda t, s: pass_pointer(
+                    Object(
+                        self.prog,
+                        self.prog.array_type(self.prog.type(t), len(s) + 1),
+                        s.encode(),
+                    )
+                ),
+            ),
+            (
+                "pointer",
+                lambda t, s: self.prog[f"drgn_kmodify_test_{t.replace(' ', '_')}_str"],
+            ),
+        ):
+            with self.subTest(msg):
+                self.assert_called(
+                    "string_args",
+                    (
+                        f("char", "Hello"),
+                        f("signed char", ", "),
+                        f("unsigned char", "world"),
+                        f("const char", "!"),
+                    ),
+                )
+
+    def test_integer_out_params(self):
+        args = [
+            pass_pointer(Object(self.prog, "signed char", -66)),
+            pass_pointer(Object(self.prog, "short", -666)),
+            pass_pointer(-12345),
+            pass_pointer(Object(self.prog, "long", -2468013579)),
+            pass_pointer(Object(self.prog, "long long", -9080706050403020100)),
+        ]
+        self.assert_called("integer_out_params", args)
+        self.assertIdentical(
+            [ptr.object for ptr in args],
+            [
+                Object(self.prog, "signed char", 33),
+                Object(self.prog, "short", 333),
+                Object(self.prog, "int", 23456),
+                Object(self.prog, "long", 2222222222),
+                Object(self.prog, "long long", 9090909090909090909),
+            ],
+        )
+
+    def test_array_out_params(self):
+        arg = pass_pointer(Object(self.prog, "long [3]", [1, 2, 3]))
+        self.assert_called("array_out_params", (arg,))
+        self.assertIdentical(arg.object, Object(self.prog, "long [3]", [2, 3, 5]))
+
+    def test_array_out_params_extra(self):
+        arg = pass_pointer(Object(self.prog, "long [4]", [1, 2, 3, 100]))
+        self.assert_called("array_out_params", (arg,))
+        self.assertIdentical(arg.object, Object(self.prog, "long [4]", [2, 3, 5, 100]))
+
+    def test_array_out_params_inferred(self):
+        self.assert_called(
+            "array_out_params", (Object(self.prog, "long [3]", [1, 2, 3]),)
+        )
+
+    def test_many_out_params(self):
+        args = [
+            pass_pointer(Object(self.prog, "char", 48)),
+            pass_pointer(Object(self.prog, "signed char", -66)),
+            pass_pointer(Object(self.prog, "short", -666)),
+            pass_pointer(Object(self.prog, "int", -12345)),
+            pass_pointer(Object(self.prog, "long", -2468013579)),
+            pass_pointer(Object(self.prog, "long long", -9080706050403020100)),
+            pass_pointer(Object(self.prog, "unsigned char", 200)),
+            pass_pointer(Object(self.prog, "unsigned short", 7777)),
+            pass_pointer(Object(self.prog, "unsigned int", 54321)),
+            pass_pointer(Object(self.prog, "unsigned long", 4000000000)),
+            pass_pointer(Object(self.prog, "unsigned long long", 12345678909876543210)),
+        ]
+        self.assert_called("many_out_params", args)
+        self.assertIdentical(
+            [arg.object for arg in args],
+            [
+                Object(self.prog, "char", 16),
+                Object(self.prog, "signed char", -22),
+                Object(self.prog, "short", -222),
+                Object(self.prog, "int", -4115),
+                Object(self.prog, "long", -822671193),
+                Object(self.prog, "long long", -3026902016801006700),
+                Object(self.prog, "unsigned char", 66),
+                Object(self.prog, "unsigned short", 2592),
+                Object(self.prog, "unsigned int", 18107),
+                Object(self.prog, "unsigned long", 1333333333),
+                Object(self.prog, "unsigned long long", 4115226303292181070),
+            ],
+        )
+
+
+@skip_unless_have_test_kmod
+@skip_unless_have_kmodify
+class TestWriteMemory(LinuxKernelTestCase):
+    def test_write_memory(self):
+        buf = os.urandom(16)
+        write_memory(self.prog, self.prog["drgn_kmodify_test_memory"].address_, buf)
+        self.assertEqual(
+            self.prog.read(self.prog["drgn_kmodify_test_memory"].address_, len(buf)),
+            buf,
+        )
+
+    def test_fault(self):
+        self.assertRaises(FaultError, write_memory, self.prog, 0, b"asdf")
+
+
+@skip_unless_have_test_kmod
+@skip_unless_have_kmodify
+class TestWriteObject(LinuxKernelTestCase):
+    def test_python_value(self):
+        value = random.randrange(2**31)
+        write_object(self.prog["drgn_kmodify_test_int"], value)
+        self.assertEqual(self.prog["drgn_kmodify_test_int"].value_(), value)
+
+    def test_object_value(self):
+        value = random.randrange(2**31)
+        write_object(
+            self.prog["drgn_kmodify_test_int"], Object(self.prog, "long", value)
+        )
+        self.assertEqual(self.prog["drgn_kmodify_test_int"].value_(), value)
+
+    def test_pointer_dereference(self):
+        value = random.randrange(2**31)
+        write_object(
+            self.prog["drgn_kmodify_test_int"].address_of_(), value, dereference=True
+        )
+        self.assertEqual(self.prog["drgn_kmodify_test_int"].value_(), value)
+
+    def test_pointer_no_dereference(self):
+        write_object(
+            self.prog["drgn_kmodify_test_int_ptr"],
+            self.prog["drgn_kmodify_test_int"].address_of_(),
+            dereference=False,
+        )
+        self.assertEqual(
+            self.prog["drgn_kmodify_test_int_ptr"],
+            self.prog["drgn_kmodify_test_int"].address_of_(),
+        )
+        write_object(self.prog["drgn_kmodify_test_int_ptr"], 0, dereference=False)
+        self.assertEqual(self.prog["drgn_kmodify_test_int_ptr"].value_(), 0)
+
+    def test_pointer_ambiguous(self):
+        self.assertRaisesRegex(
+            TypeError,
+            "use dereference",
+            write_object,
+            self.prog["drgn_kmodify_test_int_ptr"],
+            0,
+        )
+
+    def test_not_pointer(self):
+        self.assertRaisesRegex(
+            TypeError,
+            "not a pointer",
+            write_object,
+            self.prog["drgn_kmodify_test_int"],
+            0,
+            dereference=True,
+        )

commit bc3bfbdf4c7ebe8b1596c8354364f4b7fa3ebc55

drgn.helpers.experimental.kmodify: fix/disallow write_object() for bit fields
write_object() is extremely broken for bit fields. It doesn't take the
bit offset or bit size into account, which means that it writes to the
wrong bits and zeroes additional bits, too.

Byte-aligned bit fields are easy once we consider the offset and size.
We need to be careful for the general case, as a non-atomic
read-modify-write is a huge footgun. Boris Burkov has a use case for
writing to a bit field of size 1, which we can easily do atomically on
x86-64, so implement that. We can implement other cases when they arise.

Fixes: 1652183026a8 ("Add experimental helpers for calling kernel functions and writing to memory")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/experimental/kmodify.py
@@ -427,8 +427,23 @@ class _ReturnIfLastReturnValueNonZero(NamedTuple):
     value: _Integer
 
 
+class _AtomicSetBit(NamedTuple):
+    nr: int
+    address: _Integer
+
+
+class _AtomicClearBit(NamedTuple):
+    nr: int
+    address: _Integer
+
+
 _FunctionBodyNode = Union[
-    _Call, _StoreReturnValue, _Return, _ReturnIfLastReturnValueNonZero
+    _Call,
+    _StoreReturnValue,
+    _Return,
+    _ReturnIfLastReturnValueNonZero,
+    _AtomicSetBit,
+    _AtomicClearBit,
 ]
 
 
@@ -675,6 +690,20 @@ class _CodeGen_x86_64:
         # The destination needs to be fixed up later.
         self._epilogue_jumps.append(len(self.code))
 
+    def atomic_set_bit(self, nr: int, address: _Integer) -> None:
+        # mov $address, %rax
+        self._mov_imm(address, self._rax)
+        # lock; orb $(1 << nr), (%rax)
+        self.code.extend(b"\xf0\x80\x08")
+        self.code.append(1 << nr)
+
+    def atomic_clear_bit(self, nr: int, address: _Integer) -> None:
+        # mov $address, %rax
+        self._mov_imm(address, self._rax)
+        # lock; andb $~(1 << nr), (%rax)
+        self.code.extend(b"\xf0\x80\x20")
+        self.code.append(0xFF ^ (1 << nr))
+
 
 class _Arch_X86_64:
     ELF_MACHINE = 62  # EM_X86_64
@@ -715,6 +744,10 @@ class _Arch_X86_64:
                 code_gen.return_(node.value, last=i == len(func.body) - 1)
             elif isinstance(node, _ReturnIfLastReturnValueNonZero):
                 code_gen.return_if_last_return_value_nonzero(node.value)
+            elif isinstance(node, _AtomicSetBit):
+                code_gen.atomic_set_bit(node.nr, node.address)
+            elif isinstance(node, _AtomicClearBit):
+                code_gen.atomic_clear_bit(node.nr, node.address)
             else:
                 assert_never(node)
 
@@ -965,6 +998,19 @@ class _Kmodify:
                 return 0
 
 
+_COPY_TO_FROM_KERNEL_NOFAULT_NAMES = (
+    # Names used since Linux kernel commit fe557319aa06 ("maccess: rename
+    # probe_kernel_{read,write} to copy_{from,to}_kernel_nofault") (in
+    # v5.8-rc2).
+    ("copy_to_kernel_nofault", "copy_from_kernel_nofault"),
+    # Names used before Linux kernel commit 48c49c0e5f31 ("maccess: remove
+    # various unused weak aliases") (in v5.8-rc1).
+    ("__probe_kernel_write", "__probe_kernel_read"),
+    # Names briefly used between those two commits.
+    ("probe_kernel_write", "probe_kernel_read"),
+)
+
+
 @takes_program_or_default
 def write_memory(prog: Program, address: IntegerLike, value: bytes) -> None:
     """
@@ -989,17 +1035,10 @@ def write_memory(prog: Program, address: IntegerLike, value: bytes) -> None:
     """
     copy_to_kernel_nofault_address = None
     copy_from_kernel_nofault_address = None
-    for copy_to_kernel_nofault, copy_from_kernel_nofault in (
-        # Names used since Linux kernel commit fe557319aa06 ("maccess: rename
-        # probe_kernel_{read,write} to copy_{from,to}_kernel_nofault") (in
-        # v5.8-rc2).
-        ("copy_to_kernel_nofault", "copy_from_kernel_nofault"),
-        # Names used before Linux kernel commit 48c49c0e5f31 ("maccess: remove
-        # various unused weak aliases") (in v5.8-rc1).
-        ("__probe_kernel_write", "__probe_kernel_read"),
-        # Names briefly used between those two commits.
-        ("probe_kernel_write", "probe_kernel_read"),
-    ):
+    for (
+        copy_to_kernel_nofault,
+        copy_from_kernel_nofault,
+    ) in _COPY_TO_FROM_KERNEL_NOFAULT_NAMES:
         try:
             copy_to_kernel_nofault_address = prog[copy_to_kernel_nofault].address_
             copy_from_kernel_nofault_address = prog[copy_from_kernel_nofault].address_
@@ -1084,6 +1123,77 @@ def write_memory(prog: Program, address: IntegerLike, value: bytes) -> None:
             raise ValueError("module init did not run")
 
 
+def _modify_bit(prog: Program, nr: int, address: int, value: bool) -> None:
+    # This and the codegen representations are all in terms of bytes.
+    assert 0 <= nr < 8
+
+    copy_from_kernel_nofault_address = None
+    for _, copy_from_kernel_nofault in _COPY_TO_FROM_KERNEL_NOFAULT_NAMES:
+        try:
+            copy_from_kernel_nofault_address = prog[copy_from_kernel_nofault].address_
+            break
+        except KeyError:
+            pass
+    if copy_from_kernel_nofault_address is None:
+        raise LookupError("copy_from_kernel_nofault not found")
+    sizeof_int = sizeof(prog.type("int"))
+    sizeof_void_p = sizeof(prog.type("void *"))
+    sizeof_size_t = sizeof(prog.type("size_t"))
+
+    kmodify = _Kmodify(prog)
+    if not kmodify.is_little_endian:
+        # Big-endian needs different calculations. kmodify only supports
+        # little-endian architectures at the moment anyways.
+        raise NotImplementedError("_modify_bit() is only implemented for little-endian")
+    code, code_relocations = kmodify.arch.code_gen(
+        _Function(
+            [
+                _Call(
+                    # Catch invalid addresses instead of crashing. Note that
+                    # this won't catch read-only addresses, unfortunately.
+                    _Symbol(copy_from_kernel_nofault),
+                    [
+                        _Symbol(".data", section=True),
+                        _Integer(sizeof_void_p, address),
+                        _Integer(sizeof_size_t, 1),
+                    ],
+                ),
+                _ReturnIfLastReturnValueNonZero(
+                    _Integer(sizeof_int, -errno.EFAULT),
+                ),
+                (_AtomicSetBit if value else _AtomicClearBit)(
+                    nr, _Integer(sizeof_void_p, address)
+                ),
+                _Return(_Integer(sizeof_int, -errno.EINPROGRESS)),
+            ]
+        )
+    )
+    ret = kmodify.insert(
+        name="set_bit" if value else "clear_bit",
+        code=code,
+        code_relocations=code_relocations,
+        data=b"\0",
+        data_alignment=1,
+        symbols=[
+            _ElfSymbol(
+                name=copy_from_kernel_nofault,
+                value=copy_from_kernel_nofault_address,
+                size=0,
+                type=STT.FUNC,
+                binding=STB.LOCAL,
+                section=SHN.ABS,
+            ),
+        ],
+    )
+    if ret != -errno.EINPROGRESS:
+        if ret == -errno.EFAULT:
+            raise FaultError("could not write to memory", address)
+        elif ret:
+            raise OSError(-ret, os.strerror(-ret))
+        else:
+            raise ValueError("module init did not run")
+
+
 def write_object(
     object: Object, value: Any, *, dereference: Optional[bool] = None
 ) -> None:
@@ -1095,6 +1205,10 @@ def write_object(
     >>> write_object(prog["init_time_ns"].offsets.boottime.tv_sec, 1000000000)
     >>> os.system("uptime -p")
     up 3 decades, 1 year, 37 weeks, 1 hour, 59 minutes
+
+    Bit fields are currently only supported if they are either byte-aligned or
+    a single bit. Writes to a bit field are atomic with respect to other bit
+    fields.
 
     .. warning::
         The warnings about :func:`write_memory()` also apply to
@@ -1113,6 +1227,8 @@ def write_object(
     :raises TypeError: if *object* is a pointer and *dereference* is not given
     :raises TypeError: if *object* is not a pointer and *dereference* is
         ``True``
+    :raises NotImplementedError: if *object* is a bit field that is not
+        byte-aligned or a single bit
     """
     type = object.type_
     if type.unaliased_kind() == TypeKind.POINTER:
@@ -1129,11 +1245,23 @@ def write_object(
     address = object.address_
     if address is None:
         raise ValueError("cannot write to value object")
+
+    bit_field_size = object.bit_field_size_
     if isinstance(value, Object):
-        value = implicit_convert(type, value)
+        value = implicit_convert(type, value, bit_field_size=bit_field_size)
     else:
-        value = Object(object.prog_, type, value)
-    write_memory(object.prog_, address, value.to_bytes_())
+        value = Object(object.prog_, type, value, bit_field_size=bit_field_size)
+
+    if bit_field_size is None:
+        bit_field_size = 0
+    bit_offset: int = object.bit_offset_  # type: ignore[assignment]  # address_ is not None, so bit_offset_ is not, either.
+
+    if bit_field_size % 8 == 0 and bit_offset % 8 == 0:
+        write_memory(object.prog_, address, value.to_bytes_())
+    elif bit_field_size == 1:
+        _modify_bit(object.prog_, bit_offset, address, bool(value))
+    else:
+        raise NotImplementedError("only byte-aligned or 1-bit bit fields are supported")
 
 
 def _default_argument_promotions(obj: Object) -> Object:

tests/linux_kernel/helpers/test_kmodify.py
@@ -323,3 +323,41 @@ class TestWriteObject(LinuxKernelTestCase):
             0,
             dereference=True,
         )
+
+    def test_bit_field_size_1(self):
+        for i in range(3):
+            with self.subTest(i=i):
+                write_object(self.prog["drgn_kmodify_test_bit_field"].bit, i)
+                self.assertFalse(self.prog["drgn_kmodify_test_bit_field"].expect0_1)
+                self.assertTrue(self.prog["drgn_kmodify_test_bit_field"].expect1_1)
+                self.assertEqual(
+                    self.prog["drgn_kmodify_test_bit_field"].bit.value_(), i % 2
+                )
+                self.assertFalse(self.prog["drgn_kmodify_test_bit_field"].expect0_2)
+                self.assertTrue(self.prog["drgn_kmodify_test_bit_field"].expect1_2)
+
+    def test_byte_aligned_bit_field(self):
+        for i in range(3):
+            with self.subTest(i=i):
+                write_object(self.prog["drgn_kmodify_test_bit_field"].byte_aligned, i)
+                self.assertFalse(self.prog["drgn_kmodify_test_bit_field"].expect0_1)
+                self.assertTrue(self.prog["drgn_kmodify_test_bit_field"].expect1_1)
+                self.assertEqual(
+                    self.prog["drgn_kmodify_test_bit_field"].byte_aligned.value_(), i
+                )
+                self.assertFalse(self.prog["drgn_kmodify_test_bit_field"].expect0_2)
+                self.assertTrue(self.prog["drgn_kmodify_test_bit_field"].expect1_2)
+
+    def test_unsupported_bit_field(self):
+        self.assertRaises(
+            NotImplementedError,
+            write_object,
+            self.prog["drgn_kmodify_test_bit_field"].two_bits,
+            1,
+        )
+        self.assertRaises(
+            NotImplementedError,
+            write_object,
+            self.prog["drgn_kmodify_test_bit_field"].unaligned,
+            1,
+        )

tests/linux_kernel/kmod/drgn_test.c
@@ -1639,6 +1639,20 @@ char drgn_kmodify_test_memory[16];
 int drgn_kmodify_test_int;
 int *drgn_kmodify_test_int_ptr;
 
+struct {
+	unsigned int expect0_1 : 8;
+	unsigned int byte_aligned : 8;
+	unsigned int expect1_1 : 1;
+	unsigned int bit : 1;
+	unsigned int expect1_2 : 1;
+	unsigned int expect0_2 : 1;
+	unsigned int two_bits : 2;
+	unsigned int unaligned : 8;
+} drgn_kmodify_test_bit_field = {
+	.expect1_1 = 1,
+	.expect1_2 = 1,
+};
+
 #define DEFINE_KMODIFY_TEST_RETURN(name, return_type, return_value)	\
 int drgn_kmodify_test_##name##_called = 0;				\
 return_type drgn_kmodify_test_##name(void);				\