6cff072db547f6562505939a776e8260abe3f683
Omar Sandoval <osandov@osandov.com>
2025-10-29 15:19:22 -0700

    Revert "Temporarily hide commands"
    This reverts commit c7e446b91ae4cbceedf77d63ec6b6120b6369d30. Now that
    0.0.33 is out, we can keep working on commands.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- setup.py
+++ setup.py
@@ -467,6 +467,7 @@ setup(
     },
     entry_points={
         "console_scripts": ["drgn=drgn.cli:_main"],
+        "drgn.plugins": ["builtin_commands=drgn.commands._builtin"],
     },
     python_requires=">=3.8",
     author="Omar Sandoval",
