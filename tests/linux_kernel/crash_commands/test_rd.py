osandov/drgn

commit d6d9feb23e3a1691811dd8bc37fc206e45093167

crash: port rd command
The rd command allows reading and formatting kernel memory in a variety
of ways. In addition to being its own command, it also provides
functionality that other commands (like bt) will use, so it's important
to provide an API for its functionality as well. But since it is just
implementing crash-compatible behavior, it's not exposed as a helper.

Thanks to the recent refactor to identify_address_all(), it is possible
to implement the specific behavior that Crash's rd command implements
for -sS[S]. Unfortunately, the addresses crash identifies and annotates
are a subset of what drgn is capable of annotating. It might be worth
including the entire set of drgn annotations with a dedicated option,
such as when specifying a third "-S" option. That is not implemneted
here.

Userspace memory reading is also supported, using the context task to
determine the address space. Since it's definitely non-trivial to
determine whether an address is user or kernel, we employ a trick: first
we try to read memory using prog.read, and if that fails, we try to read
via access_process_vm(). Based on that, we can "magically" generate the
correct drgn code. Usually the user knows whether they're trying to read
user/kernel memory, so it makes sense to pretend we magically knew it as
well.

The only crash options which are not supported are -m (for Xen host
machine addresses) and -f (for dumpfile offsets). Neither of these are
likely to be supported by drgn.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

tests/linux_kernel/crash_commands/test_rd.py
@@ -1,0 +1,191 @@
+# Copyright (c) 2025, Oracle and/or its affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import ctypes
+import math
+import os
+import tempfile
+
+from drgn import Architecture, FaultError, PlatformFlags
+from drgn.helpers.linux.pid import find_task
+from tests.linux_kernel import skip_if_highmem
+from tests.linux_kernel.crash_commands import CrashCommandTestCase
+
+
+class TestRd(CrashCommandTestCase):
+    def setUp(self):
+        self.w = self.prog.address_size() * 2
+
+    def test_no_options(self):
+        address = self.prog["init_task"].address_
+        cmd = self.check_crash_command(f"rd {address:x}")
+        self.assertRegex(cmd.stdout, rf"^{address:0{self.w}x}:")
+        self.assertRegex(cmd.stdout, r"[0-9a-f]{" + str(self.w) + "}")
+
+    def test_count(self):
+        address = self.prog["init_task"].address_
+        cmd = self.check_crash_command(f"rd -64 {address:x} 3")
+        lines = cmd.stdout.strip().split("\n")
+        self.assertGreaterEqual(len(lines), 2)
+        self.assertRegex(lines[0], rf"^{address:0{self.w}x}:")
+
+    def test_format_decimal(self):
+        address = self.prog["init_task"].address_
+        cmd = self.check_crash_command(f"rd -d {address:x}")
+        self.assertRegex(cmd.stdout, rf"^{address:0{self.w}x}:")
+        # Should have decimal numbers
+        self.assertRegex(cmd.stdout, r"\b-?\d+\b")
+
+    def test_units(self):
+        data = b"Linux ve"
+        address = self.prog.symbol("linux_banner").address
+        if self.prog.platform.flags & PlatformFlags.IS_LITTLE_ENDIAN:
+            endian = "little"
+        else:
+            endian = "big"
+        for unit in (1, 2, 4, 8):
+            with self.subTest(unit=unit):
+                count = 8 // unit
+                cmd = self.check_crash_command(f"rd -{unit * 8} linux_banner {count}")
+                value_list = []
+                for i in range(count):
+                    int_bytes = data[i * unit : (i + 1) * unit]
+                    value_list.append(int.from_bytes(int_bytes, byteorder=endian))
+                values = " ".join(f"{v:0{unit * 2}x}" for v in value_list)
+                self.assertRegex(
+                    cmd.stdout,
+                    rf"^{address:0{self.w}x}:  {values} +Linux ve$",
+                )
+
+    def test_network_byte_order(self):
+        data = b"Linux ve"
+        address = self.prog.symbol("linux_banner").address
+        for unit in (2, 4, 8):
+            with self.subTest(unit=unit):
+                count = 8 // unit
+                cmd = self.check_crash_command(
+                    f"rd -{unit * 8} -N linux_banner {count}"
+                )
+                value_list = []
+                for i in range(count):
+                    int_bytes = data[i * unit : (i + 1) * unit]
+                    value_list.append(int.from_bytes(int_bytes, byteorder="big"))
+                values = " ".join(f"{v:0{unit * 2}x}" for v in value_list)
+                self.assertRegex(
+                    cmd.stdout,
+                    rf"^{address:0{self.w}x}:  {values} +Linux ve$",
+                )
+
+    def test_ascii(self):
+        cmd = self.check_crash_command("rd -a linux_banner")
+        address = self.prog.symbol("linux_banner").address
+        self.assertRegex(cmd.stdout, rf"{address:0{self.w}x}:  Linux version")
+        lines = cmd.stdout.strip().split("\n")
+
+        # The ascii variant formats output over multiple lines, breaking at 79
+        # characters. The linux banner is good test data for this, because it
+        # has no special whitespace and it is generally rather long. While we
+        # don't necessarily care about the specific line break point, it is good
+        # to verify that we get roughly the expected amount of output, as a
+        # smoke test.
+        string = self.prog["linux_banner"].string_()
+        chars_per_line = 79 - self.w - 3
+        line_count = math.ceil(len(string) / chars_per_line)
+        self.assertEqual(len(lines), line_count)
+
+    def test_ascii_count(self):
+        cmd = self.check_crash_command("rd -a linux_banner 5")
+        address = self.prog.symbol("linux_banner").address
+        self.assertEqual(cmd.stdout.strip(), f"{address:0{self.w}x}:  Linux")
+
+    def test_ascii_offset(self):
+        address = self.prog.symbol("linux_banner").address
+        cmd = self.check_crash_command("rd -o 4 -a linux_banner")
+        self.assertRegex(cmd.stdout, f"^{address + 4:0{self.w}x}:  x version.*")
+
+    def test_symbol(self):
+        address = self.prog["init_task"].address_
+        cmd = self.check_crash_command("rd init_task")
+        self.assertRegex(cmd.stdout, rf"^{address:0{self.w}x}:")
+
+    def test_annotate_symbols(self):
+        address = self.prog["slab_caches"].prev.value_()
+        cmd = self.check_crash_command(f"rd -s {address:x} 2")
+        self.assertRegex(cmd.stdout, rf"^{address:0{self.w}x}:  slab_caches\+0")
+
+    def test_annotate_slab(self):
+        address = self.prog.symbol("slab_caches").address
+        cmd = self.check_crash_command("rd -S slab_caches 2")
+        self.assertRegex(
+            cmd.stdout, rf"^{address:0{self.w}x}:  \[kmem_cache\] +\[kmem_cache\]"
+        )
+
+    def test_annotate_slab_verbose(self):
+        address = self.prog.symbol("slab_caches").address
+        cmd = self.check_crash_command("rd -SS slab_caches 2")
+        self.assertRegex(
+            cmd.stdout,
+            rf"^{address:0{self.w}x}:  \[[0-9a-f]+:kmem_cache\] +\[[0-9a-f]+:kmem_cache\]",
+        )
+
+    def test_reverse(self):
+        address = self.prog["init_task"].address_
+        cmd = self.check_crash_command(f"rd -R {address:x} 2")
+        unit = self.prog.address_size()
+        start_addr = address - unit
+        self.assertRegex(cmd.stdout, rf"^{start_addr:0{self.w}x}:  [0-9a-f]+ [0-9a-f]+")
+
+    def test_raw(self):
+        address = self.prog["init_task"].address_
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            temp_file = f.name
+            cmd = self.check_crash_command(f"rd -r {temp_file} {address:x} 16")
+            self.assertIn("16 bytes copied", cmd.stdout)
+            self.assertTrue(os.path.exists(temp_file))
+            with open(temp_file, "rb") as f:
+                data = f.read()
+                self.assertEqual(len(data), 16)
+
+    @skip_if_highmem
+    def test_ascii_user(self):
+        string = "hello, world!"
+        buf = ctypes.create_string_buffer(string.encode("ascii"))
+        address = ctypes.addressof(buf)
+        self.prog.config["crash_context"] = find_task(self.prog, os.getpid())
+
+        # On s390x, the user and kernel address spaces occupy the same range, so
+        # we cannot detect whether an address is a user address. Explicitly pass
+        # the hint here.
+        need_hint = self.prog.platform.arch == Architecture.S390X
+
+        expected_value = f"{address:0{self.w}x}:  hello world, I am a string!"
+
+        if not need_hint:
+            with self.subTest("without -u hint"):
+                cmd = self.check_crash_command(f"rd -a -o {self.w} 0x{address:x}")
+                self.assertEqual(cmd.stdout.strip(), expected_value)
+                self.assertIn("access_process_vm", cmd.drgn_option)
+            self.assertNotIn("prog.read", cmd.drgn_option)
+        with self.subTest("with -u hint"):
+            cmd = self.check_crash_command(f"rd -u -a -o {self.w} 0x{address:x}")
+            self.assertEqual(cmd.stdout.strip(), expected_value)
+            self.assertIn("access_process_vm", cmd.drgn_option)
+            self.assertNotIn("prog.read", cmd.drgn_option)
+
+    def check_failing_command(self, command, exc_type):
+        # Directly running the command should fail
+        with self.assertRaises(exc_type):
+            self.run_crash_command(command)
+
+        # The drgn option should generate valid code that compiles
+        self.run_crash_command_drgn_option(command, mode="compile")
+
+        # But the code should also fail with the same error as the command
+        with self.assertRaises(exc_type):
+            self.run_crash_command_drgn_option(command, mode="exec")
+
+    def test_not_existing_symbol(self):
+        self.check_failing_command("rd this_symbol_does_not_exist", LookupError)
+
+    def test_fault(self):
+        self.check_failing_command("rd -u 0", FaultError)

drgn/commands/crash.py
@@ -39,6 +39,15 @@ def _pid_or_task(s: str) -> Tuple[Literal["pid", "task"], int]:
         return "task", int(s, 16)
 
 
+def _addr_or_sym(
+    s: str,
+) -> Union[Tuple[Literal["addr"], int], Tuple[Literal["sym"], str]]:
+    try:
+        return "addr", int(s, 16)
+    except ValueError:
+        return "sym", s
+
+
 def _guess_type(prog: Program, name: str, kind: str = "*") -> Type:
     if kind != "union":
         try:
@@ -94,7 +103,10 @@ class _CrashCommandNamespace(CommandNamespace):
     def __init__(self) -> None:
         super().__init__(
             func_name_prefix="_crash_cmd_",
-            argparse_types=(("pid_or_task", _pid_or_task),),
+            argparse_types=(
+                ("pid_or_task", _pid_or_task),
+                ("addr_or_sym", _addr_or_sym),
+            ),
         )
 
     def _run(self, prog: Program, command: str, **kwargs: Any) -> Any:

tests/commands/test_crash.py
@@ -1,0 +1,138 @@
+# Copyright (c) 2025, Oracle and/or its affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+import contextlib
+import io
+import struct
+
+from drgn import (
+    Architecture,
+    FaultError,
+    Platform,
+    Program,
+    Symbol,
+    SymbolBinding,
+    SymbolKind,
+)
+from drgn.commands._builtin.crash._rd import _print_memory
+from tests import TestCase
+
+
+def add_fake_memory_segment(prog, start, bytes, is_phys=False):
+
+    def reader(address, count, offset, physical):
+        if physical == is_phys:
+            return bytes[offset : offset + count]
+        raise FaultError("unable to read memory", address)
+
+    prog.add_memory_segment(start, len(bytes), reader, physical=is_phys)
+
+
+class TestPrintMemory(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # The only reason to set a specific architecture is to have a known word
+        # size, which does impact alignment. The rest is not
+        # architecture-specific.
+        cls.prog = Program(platform=Platform(Architecture.X86_64))
+        mem_data = bytes(range(8)) + struct.pack("<Q", 0x12345678) + bytes(range(8, 64))
+        add_fake_memory_segment(cls.prog, 0xFFFF0000, mem_data)
+        add_fake_memory_segment(cls.prog, 0x10000, b"ABCD", is_phys=True)
+        cls.prog.register_symbol_finder(
+            "test",
+            lambda prog, name, address, one: (
+                [
+                    Symbol(
+                        "test_sym",
+                        0x12345678,
+                        0,
+                        SymbolBinding.GLOBAL,
+                        SymbolKind.OBJECT,
+                    )
+                ]
+                if (name == "test_sym" or (name is None and address == 0x12345678))
+                else []
+            ),
+        )
+        cls.prog.set_enabled_symbol_finders(["test"])
+
+    def run_print_memory(self, address: int, count: int, *, unit: int, **kwargs):
+        output = io.StringIO()
+        physical = kwargs.pop("physical", False)
+        memory = self.prog.read(address, count * unit, physical=physical)
+        with contextlib.redirect_stdout(output):
+            _print_memory(self.prog, address, memory, unit=unit, **kwargs)
+        return output.getvalue()
+
+    def test_print_memory_basic(self):
+        result = self.run_print_memory(0xFFFF0000, 4, unit=1)
+        self.assertEqual(
+            result,
+            """\
+00000000ffff0000:  00 01 02 03                                       ....
+""",
+        )
+
+    def test_unit_2(self):
+        result = self.run_print_memory(0xFFFF0000, 2, unit=2)
+        self.assertEqual(
+            result,
+            """\
+00000000ffff0000:  0100 0302                                 ....
+""",
+        )
+
+    def test_physical(self):
+        result = self.run_print_memory(0x10000, 1, physical=True, unit=4)
+        self.assertEqual(
+            result,
+            """\
+0000000000010000:  44434241                              ABCD
+""",
+        )
+
+    def test_no_ascii(self):
+        result = self.run_print_memory(0xFFFF0000, 4, unit=1, show_ascii=False)
+        self.assertEqual(
+            result,
+            """\
+00000000ffff0000:  00 01 02 03
+""",
+        )
+
+    def test_annotate_symbols(self):
+        result = self.run_print_memory(
+            0xFFFF0008, 2, unit=8, annotate="symbols", show_ascii=False
+        )
+        self.assertEqual(
+            result,
+            """\
+00000000ffff0008:  test_sym+0       0f0e0d0c0b0a0908
+""",
+        )
+
+    def test_format_decimal(self):
+        result = self.run_print_memory(0xFFFF0000, 1, unit=4, format="d")
+        self.assertEqual(
+            result,
+            """\
+00000000ffff0000:     50462976                                       ....
+""",
+        )
+
+    def test_endian_big(self):
+        result = self.run_print_memory(0x10000, 1, physical=True, unit=4, endian="big")
+        self.assertEqual(
+            result,
+            """\
+0000000000010000:  41424344                              ABCD
+""",
+        )
+
+    def test_annotate_wrong_unit(self):
+        with self.assertRaises(ValueError):
+            self.run_print_memory(0xFFFF0000, 1, unit=4, annotate="symbols")
+
+    def test_annotate_wrong_format(self):
+        with self.assertRaises(ValueError):
+            self.run_print_memory(0xFFFF0000, 1, unit=4, format="d", annotate="symbols")

commit 76f78d2b8b627df0b50371effa31077616fda0c8

tests: crash: rd: fix annotation tests
The test_annotate_slab() and test_annotate_slab_verbose() cases rely on
the slab_caches variable to contain pointers to slab objects (those
allocated from the kmem_cache slab). This is normally the case, but it
can't be relied upon:

1. Some of the alternative configurations have "kmem_cache_boot" as the
head or the tail of this list. Since this is a symbol and not a slab
object, the test fails.
2. Kernel configurations with SLOB cannot detect which slab cache a
pointer belongs to, so they should output "unknown slab object". While I
would refer to crash's implementation to determine what it would output,
I cannot get crash to run on a relevant SLOB-enabled kernel. So, we'll
just define our own output - "unknown slab object".

Fixes: d6d9feb2 ("crash: port rd command")

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn/commands/_builtin/crash/_rd.py
@@ -32,15 +32,18 @@ def _crash_annotate(
             "slab",
             "verbose",
         ):
-            slab_cache = identified.slab_object_info.slab_cache
-            if slab_cache:
-                cache_name = escape_ascii_string(slab_cache.name.string_())
-                if level == "slab":
-                    return f"[{cache_name}]"
-                else:
-                    # Crash does not pad the address at all, which can help with
-                    # output alignment on some architectures.
-                    return f"[{addr:x}:{cache_name}]"
+            if identified.slab_object_info.address:
+                cache_name = escape_ascii_string(
+                    identified.slab_object_info.slab_cache.name.string_()
+                )
+            else:  # SLOB
+                cache_name = "unknown slab object"
+            if level == "slab":
+                return f"[{cache_name}]"
+            else:
+                # Crash does not pad the address at all, which can help with
+                # output alignment on some architectures.
+                return f"[{addr:x}:{cache_name}]"
     return None
 
 

tests/linux_kernel/crash_commands/test_rd.py
@@ -8,7 +8,7 @@ import tempfile
 
 from drgn import Architecture, FaultError, PlatformFlags
 from drgn.helpers.linux.pid import find_task
-from tests.linux_kernel import skip_if_highmem
+from tests.linux_kernel import skip_if_highmem, skip_unless_have_test_kmod
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
 
 
@@ -113,20 +113,35 @@ class TestRd(CrashCommandTestCase):
         cmd = self.check_crash_command(f"rd -s {address:x} 2")
         self.assertRegex(cmd.stdout, rf"^{address:0{self.w}x}:  slab_caches\+0")
 
+    @skip_unless_have_test_kmod
     def test_annotate_slab(self):
-        address = self.prog.symbol("slab_caches").address
-        cmd = self.check_crash_command("rd -S slab_caches 2")
-        self.assertRegex(
-            cmd.stdout, rf"^{address:0{self.w}x}:  \[kmem_cache\] +\[kmem_cache\]"
-        )
+        address = self.prog.symbol("drgn_test_small_slab_objects").address
+        cmd = self.check_crash_command("rd -S drgn_test_small_slab_objects 2")
+        if self.prog["drgn_test_slob"]:
+            self.assertRegex(
+                cmd.stdout,
+                rf"^{address:0{self.w}x}:  \[unknown slab object\] +\[unknown slab object\]",
+            )
+        else:
+            self.assertRegex(
+                cmd.stdout,
+                rf"^{address:0{self.w}x}:  \[drgn_test_small\] +\[drgn_test_small\]",
+            )
 
+    @skip_unless_have_test_kmod
     def test_annotate_slab_verbose(self):
-        address = self.prog.symbol("slab_caches").address
-        cmd = self.check_crash_command("rd -SS slab_caches 2")
-        self.assertRegex(
-            cmd.stdout,
-            rf"^{address:0{self.w}x}:  \[[0-9a-f]+:kmem_cache\] +\[[0-9a-f]+:kmem_cache\]",
-        )
+        address = self.prog.symbol("drgn_test_small_slab_objects").address
+        cmd = self.check_crash_command("rd -SS drgn_test_small_slab_objects 2")
+        if self.prog["drgn_test_slob"]:
+            self.assertRegex(
+                cmd.stdout,
+                rf"^{address:0{self.w}x}:  \[[0-9a-f]+:unknown slab object\] +\[[0-9a-f]+:unknown slab object\]",
+            )
+        else:
+            self.assertRegex(
+                cmd.stdout,
+                rf"^{address:0{self.w}x}:  \[[0-9a-f]+:drgn_test_small\] +\[[0-9a-f]+:drgn_test_small\]",
+            )
 
     def test_reverse(self):
         address = self.prog["init_task"].address_