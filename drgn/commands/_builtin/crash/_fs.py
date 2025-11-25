b1ae9ce0747c4a129eb939e5d3394dde5a330497
Omar Sandoval <osandov@osandov.com>
2025-11-25 13:35:17 -0800

    crash: port files command
    I thought that we'd be able to factor some helpers out of
    tools/fsrefs.py for -R, but the semantics are pretty different, so this
    does it manually. The rest is straightforward.
    
    Co-authored-by: Yassine Larhrissi <yassine.larhrissi@oracle.com>
    Signed-off-by: Yassine Larhrissi <yassine.larhrissi@oracle.com>
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/commands/_builtin/crash/_fs.py
+++ drgn/commands/_builtin/crash/_fs.py
@@ -4,13 +4,406 @@
 # Filesystem-related commands.
 
 import argparse
-from typing import Any
+import operator
+import os
+import sys
+from typing import Any, List
 
-from drgn import Program
-from drgn.commands import argument, drgn_argument
-from drgn.commands.crash import CrashDrgnCodeBuilder, crash_command, crash_get_context
+from drgn import Object, Program
+from drgn.commands import _repr_black, argument, drgn_argument, mutually_exclusive_group
+from drgn.commands._builtin.crash._kmem import _print_pages_default_members
+from drgn.commands.crash import (
+    CrashDrgnCodeBuilder,
+    crash_command,
+    crash_get_context,
+    print_task_header,
+)
 from drgn.helpers.common.format import CellFormat, escape_ascii_string, print_table
-from drgn.helpers.linux.fs import for_each_mount, mount_dst, mount_fstype, mount_src
+from drgn.helpers.linux.fs import (
+    address_space_for_each_page,
+    d_path,
+    decode_file_type,
+    for_each_file,
+    for_each_mount,
+    mount_dst,
+    mount_fstype,
+    mount_src,
+)
+
+
+def _print_dentry(prog: Program, address: int, drgn_arg: bool) -> None:
+    if drgn_arg:
+        sys.stdout.write(
+            f"""\
+from drgn import Object
+from drgn.helpers.linux.fs import d_path, decode_file_type
+
+
+dentry = Object(prog, "struct dentry *", {hex(address)})
+inode = dentry.d_inode
+sb = inode.i_sb
+type = decode_file_type(inode.i_mode)
+path = d_path(dentry)
+"""
+        )
+        return
+
+    dentry = Object(prog, "struct dentry *", address)
+    inode = dentry.d_inode.read_()
+    sb = inode.i_sb.read_()
+    print_table(
+        (
+            (
+                CellFormat("DENTRY", "^"),
+                CellFormat("INODE", "^"),
+                CellFormat("SUPERBLK", "^"),
+                "TYPE",
+                "PATH",
+            ),
+            (
+                CellFormat(dentry.value_(), "^x"),
+                CellFormat(inode.value_(), "^x"),
+                CellFormat(sb.value_(), "^x"),
+                decode_file_type(inode.i_mode),
+                escape_ascii_string(d_path(dentry), escape_backslash=True),
+            ),
+        )
+    )
+
+
+def _print_inode(prog: Program, address: int, drgn_arg: bool) -> None:
+    if drgn_arg:
+        code = CrashDrgnCodeBuilder(prog)
+        code.add_from_import("drgn", "Object")
+        code.add_from_import("drgn.helpers.linux.fs", "inode_for_each_page")
+        code.add_from_import(
+            "drgn.helpers.linux.mm",
+            "decode_page_flags_value",
+            "page_flags",
+            "page_to_phys",
+        )
+        code.append(
+            f"""\
+inode = Object(prog, "struct inode *", {hex(address)})
+i_mapping = inode.i_mapping
+nrpages = i_mapping.nrpages
+
+for index, page in inode_for_each_page(inode):
+    physical = page_to_phys(page)
+    cnt = page._refcount.counter
+    flags = page_flags(page)
+    decoded_flags = decode_page_flags_value(flags)
+"""
+        )
+        return code.print()
+
+    inode = Object(prog, "struct inode *", address)
+    mapping = inode.i_mapping.read_()
+    print_table(
+        (
+            (CellFormat("INODE", "^"), CellFormat("NRPAGES", ">")),
+            (CellFormat(inode.value_(), "^x"), mapping.nrpages.value_()),
+        )
+    )
+
+    print()
+    mapping_value = mapping.value_()
+    _print_pages_default_members(
+        prog,
+        address_space_for_each_page(mapping),
+        get_page=operator.itemgetter(1),
+        get_mapping=lambda _: mapping_value,
+        get_index=operator.itemgetter(0),
+    )
+
+
+@crash_command(
+    description="file information",
+    long_description="""
+    Show file information. By default, display the root directory, current
+    working directory, and all open files of one or more tasks, including the
+    file descriptor, file structure, dentry, inode, file type, and path.
+    """,
+    arguments=(
+        mutually_exclusive_group(
+            argument(
+                "-d",
+                dest="dentry",
+                type="hexadecimal",
+                help="""
+                display the inode, super block, file type, and full path of a
+                dentry, given as a hexadecimal address
+                """,
+            ),
+            argument(
+                "-p",
+                dest="inode",
+                type="hexadecimal",
+                help="""
+                display the pages in the page cache of an inode, given as a
+                hexadecimal address
+                """,
+            ),
+            argument(
+                "tasks",
+                metavar="pid|task",
+                type="pid_or_task",
+                nargs="*",
+                # Work around https://github.com/python/cpython/issues/72795
+                # before Python 3.13.
+                default=[],
+                help="""
+                display open files for this task, given as either a decimal process
+                ID or a hexadecimal ``task_struct`` address. May be given multiple
+                times. Defaults to the current context
+                """,
+            ),
+        ),
+        argument(
+            "-c",
+            dest="cache",
+            action="store_true",
+            help="""
+            for each open file, display the i_mapping and number of cached
+            pages instead of the file structure and dentry
+            """,
+        ),
+        argument(
+            "-R",
+            dest="reference",
+            help="""
+            search for references to this file descriptor, path, or dentry,
+            inode, address_space, or file structure address
+            """,
+        ),
+        drgn_argument,
+    ),
+)
+def _crash_cmd_files(
+    prog: Program,
+    name: str,
+    args: argparse.Namespace,
+    *,
+    parser: argparse.ArgumentParser,
+    **kwargs: Any,
+) -> None:
+    # -c and -R are mutually exclusive with -d and -p, but not with each other
+    # or with tasks. argparse can't express this.
+    if (args.cache or args.reference is not None) and (args.dentry or args.inode):
+        parser.error("-c/-R not allowed with -d/-p")
+
+    if args.dentry is not None:
+        return _print_dentry(prog, args.dentry, args.drgn)
+    elif args.inode is not None:
+        return _print_inode(prog, args.inode, args.drgn)
+
+    if not args.tasks:
+        args.tasks.append(None)
+
+    reference_int = None
+    if args.reference is not None:
+        try:
+            reference_int = int(args.reference, 10)
+            reference_int_base = 10
+        except ValueError:
+            try:
+                reference_int = int(args.reference, 16)
+                reference_int_base = 16
+            except ValueError:
+                pass
+
+        reference_path = os.fsencode(args.reference)
+
+    if args.drgn:
+        code = CrashDrgnCodeBuilder(prog)
+
+        if reference_int is not None:
+            if reference_int_base == 10:
+                code.append(f"reference = {reference_int}\n")
+            else:
+                code.append(f"reference = {hex(reference_int)}\n")
+        if args.reference is not None:
+            code.add_import("os")
+            code.append(
+                f"reference_path = os.fsencode({_repr_black(args.reference)})\n\n"
+            )
+
+        if len(args.tasks) > 1:
+            code.append("tasks = []\n")
+        for task_arg in args.tasks:
+            if len(args.tasks) > 1:
+                code.append("\n")
+            code.append_crash_context(task_arg)
+            if len(args.tasks) > 1:
+                code.append("tasks.append(task)\n")
+
+        code.append("\n")
+        if len(args.tasks) > 1:
+            code.append("for task in tasks:\n")
+            indent = "    "
+        else:
+            indent = ""
+
+        code.append_task_header(indent)
+
+        code.add_from_import(
+            "drgn.helpers.linux.fs", "d_path", "decode_file_type", "for_each_file"
+        )
+
+        code.append(
+            f"""
+{indent}root = d_path(task.fs.root)
+{indent}cwd = d_path(task.fs.pwd)
+
+"""
+        )
+        if args.reference is not None:
+            code.append(
+                f"""\
+{indent}is_match = (reference_path in root) or (reference_path in cwd)
+
+"""
+            )
+
+        code.append(f"{indent}for fd, file in for_each_file(task):\n")
+        if args.cache:
+            code.append(
+                f"""\
+{indent}    inode = file.f_inode
+{indent}    i_mapping = inode.i_mapping
+{indent}    nrpages = i_mapping.nrpages
+"""
+            )
+        else:
+            code.append(
+                f"""\
+{indent}    dentry = file.f_path.dentry
+{indent}    inode = file.f_inode
+"""
+            )
+        code.append(
+            f"""\
+{indent}    type = decode_file_type(inode.i_mode)
+{indent}    path = d_path(file.f_path)
+"""
+        )
+        if reference_int is not None:
+            code.append(
+                f"""\
+{indent}    is_match = (
+{indent}        fd == reference
+"""
+            )
+            if args.cache:
+                code.append(
+                    f"""\
+{indent}        or inode.value_() == reference
+{indent}        or i_mapping.value_() == reference
+"""
+                )
+            else:
+                code.append(
+                    f"""\
+{indent}        or file.value_() == reference
+{indent}        or dentry.value_() == reference
+{indent}        or inode.value_() == reference
+"""
+                )
+            code.append(
+                f"""\
+{indent}        or reference_path in path
+{indent}    )
+"""
+            )
+        elif args.reference is not None:
+            code.append(
+                f"""\
+{indent}    is_match = reference_path in path
+"""
+            )
+        return code.print()
+
+    first = True
+    for task_arg in args.tasks:
+        task = crash_get_context(prog, task_arg)
+
+        row: List[Any] = [CellFormat("FD", ">")]
+        if args.cache:
+            row.append(CellFormat("INODE", "^"))
+            row.append(CellFormat("I_MAPPING", "^"))
+            row.append(CellFormat("NRPAGES", ">"))
+        else:
+            row.append(CellFormat("FILE", "^"))
+            row.append(CellFormat("DENTRY", "^"))
+            row.append(CellFormat("INODE", "^"))
+        row.append("TYPE")
+        row.append("PATH")
+        rows = [row]
+
+        for fd, file in for_each_file(task):
+            f_path = file.f_path.read_()
+            inode = file.f_inode.read_()
+            if args.cache:
+                mapping = inode.i_mapping.read_()
+            else:
+                dentry = f_path.dentry.read_()
+            path = d_path(f_path)
+
+            if args.reference is not None and not (
+                (
+                    reference_int is not None
+                    and (
+                        fd == reference_int
+                        or inode.value_() == reference_int
+                        # Crash only matches against the columns that it prints
+                        # depending on -c.
+                        or (args.cache and mapping.value_() == reference_int)
+                        or (not args.cache and file.value_() == reference_int)
+                        or (not args.cache and dentry.value_() == reference_int)
+                    )
+                )
+                or reference_path in path
+            ):
+                continue
+
+            row = [fd]
+            if args.cache:
+                row.append(CellFormat(inode.value_(), "^x"))
+                row.append(CellFormat(mapping.value_(), "^x"))
+                row.append(mapping.nrpages.value_())
+            else:
+                row.append(CellFormat(file.value_(), "^x"))
+                row.append(CellFormat(dentry.value_(), "^x"))
+                row.append(CellFormat(inode.value_(), "^x"))
+            row.append(decode_file_type(inode.i_mode))
+            row.append(escape_ascii_string(path, escape_backslash=True))
+            rows.append(row)
+
+        fs = task.fs
+        root_path = d_path(fs.root)
+        cwd_path = d_path(fs.pwd)
+        if (
+            args.reference is None
+            or len(rows) > 1
+            or reference_path in root_path
+            or reference_path in cwd_path
+        ):
+            if first:
+                first = False
+            else:
+                print()
+            print_task_header(task)
+
+            fs = task.fs
+            print(
+                f"ROOT: {escape_ascii_string(root_path, escape_backslash=True)}"
+                f"  CWD: {escape_ascii_string(cwd_path, escape_backslash=True)}"
+            )
+
+            if len(rows) > 1:
+                print_table(rows)
+            elif args.reference is None:
+                print("No open files")
 
 
 @crash_command(
--- tests/linux_kernel/crash_commands/test_fs.py
+++ tests/linux_kernel/crash_commands/test_fs.py
@@ -1,9 +1,229 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+import ctypes
+import mmap
+import os
+import os.path
+import re
+import tempfile
+
+from drgn import Object
+from drgn.helpers.linux.fs import fget
 from drgn.helpers.linux.pid import find_task
-from tests.linux_kernel import CLONE_NEWNS, fork_and_stop, unshare
+from tests.linux_kernel import CLONE_NEWNS, fork_and_stop, mlock, unshare
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
+
+
+class TestFiles(CrashCommandTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.run_crash_command("set -p")
+
+    def _test_drgn_common(self, cmd, cache=False):
+        self.assertIn("for_each_file(", cmd.drgn_option.stdout)
+
+        for variable in (
+            "task",
+            "pid",
+            "command",
+            "inode",
+        ):
+            with self.subTest(variable=variable):
+                self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+
+        if cache:
+            for variable in ("i_mapping", "nrpages"):
+                with self.subTest(variable=variable):
+                    self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+
+            self.assertNotIn("dentry", cmd.drgn_option.globals)
+        else:
+            for variable in ("file", "dentry"):
+                with self.subTest(variable=variable):
+                    self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+
+            self.assertNotIn("i_mapping", cmd.drgn_option.globals)
+            self.assertNotIn("nrpages", cmd.drgn_option.globals)
+
+        self.assertIsInstance(cmd.drgn_option.globals["cpu"], int)
+        self.assertIsInstance(cmd.drgn_option.globals["fd"], int)
+        self.assertIsInstance(cmd.drgn_option.globals["type"], str)
+        for variable in (
+            "root",
+            "cwd",
+            "path",
+        ):
+            with self.subTest(variable=variable):
+                self.assertIsInstance(cmd.drgn_option.globals[variable], bytes)
+
+    def test_no_options(self):
+        with tempfile.NamedTemporaryFile() as f:
+            cmd = self.check_crash_command("files")
+            self.assertRegex(cmd.stdout, r"(?m)^ROOT: .* CWD:")
+            self.assertRegex(
+                cmd.stdout,
+                rf"(?m)^\s*{f.fileno()}\s+[0-9a-f]+\s+[0-9a-f]+\s+[0-9a-f]+\s+REG\s+{re.escape(f.name)}",
+            )
+
+        self._test_drgn_common(cmd)
+
+    def test_c(self):
+        with tempfile.NamedTemporaryFile() as f:
+            cmd = self.check_crash_command("files -c")
+            self.assertRegex(cmd.stdout, r"(?m)^ROOT: .* CWD:")
+            self.assertRegex(
+                cmd.stdout,
+                rf"(?m)^\s*{f.fileno()}\s+[0-9a-f]+\s+[0-9a-f]+\s+[0-9]+\s+REG\s+{re.escape(f.name)}",
+            )
+
+        self._test_drgn_common(cmd, cache=True)
+
+    def test_d(self):
+        with tempfile.NamedTemporaryFile() as f:
+            dentry = fget(
+                find_task(self.prog, os.getpid()), f.fileno()
+            ).f_path.dentry.read_()
+            cmd = self.check_crash_command(f"files -d {dentry.value_():x}")
+
+        self.assertRegex(
+            cmd.stdout,
+            rf"(?m)^\s*{dentry.value_():x}\s+[0-9a-f]+\s+[0-9a-f]+\s+REG\s+.*{re.escape(os.path.basename(f.name))}",
+        )
+
+        self.assertEqual(cmd.drgn_option.globals["dentry"], dentry)
+
+        for variable in ("inode", "sb"):
+            with self.subTest(variable=variable):
+                self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+        self.assertEqual(cmd.drgn_option.globals["type"], "REG")
+        self.assertIsInstance(cmd.drgn_option.globals["path"], bytes)
+
+    def test_p(self):
+        with tempfile.TemporaryFile(dir="/dev/shm") as f:
+            f.write(os.urandom(2 * mmap.PAGESIZE))
+            f.flush()
+            inode = fget(find_task(self.prog, os.getpid()), f.fileno()).f_inode.read_()
+            with mmap.mmap(f.fileno(), 2 * mmap.PAGESIZE) as map:
+                f.close()
+                address = ctypes.addressof(ctypes.c_char.from_buffer(map))
+                # Make sure the pages are faulted in and stay that way.
+                mlock(address, 2 * mmap.PAGESIZE)
+
+                cmd = self.check_crash_command(f"files -p {inode.value_():x}")
+
+        self.assertRegex(cmd.stdout, r"(?m)^\s*INODE\s+NRPAGES.*\n\s*[0-9a-f]+\s+2")
+        self.assertRegex(
+            cmd.stdout, r"(?m)^\s*PAGE\s+PHYSICAL\s+MAPPING\s+INDEX\s+CNT\s+FLAGS.*\n"
+        )
+        self.assertRegex(cmd.stdout, r"(?m)^\s*[0-9a-f]+\s+[0-9a-f]+\s+[0-9a-f]+\s+\b")
+
+        self.assertIn("inode_for_each_page(", cmd.drgn_option.stdout)
+        for variable in (
+            "inode",
+            "i_mapping",
+            "nrpages",
+            "page",
+            "physical",
+            "cnt",
+            "flags",
+        ):
+            with self.subTest(variable=variable):
+                self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+
+        self.assertIsInstance(cmd.drgn_option.globals["index"], int)
+        self.assertIsInstance(cmd.drgn_option.globals["decoded_flags"], str)
+
+    def _test_R(self, arg_fn, expect_no_cache, expect_cache):
+        with tempfile.NamedTemporaryFile() as f:
+            arg = arg_fn(f)
+
+            cmd = self.check_crash_command("files -R " + arg)
+            if expect_no_cache:
+                self.assertRegex(
+                    cmd.stdout,
+                    rf"(?m)^\s*{f.fileno()}\s+[0-9a-f]+\s+[0-9a-f]+\s+[0-9a-f]+\s+REG\s+{re.escape(f.name)}",
+                )
+            else:
+                self.assertFalse(cmd.stdout)
+            self._test_drgn_common(cmd)
+            self.assertIsInstance(cmd.drgn_option.globals["is_match"], bool)
+
+            cmd = self.check_crash_command("files -c -R " + arg)
+            if expect_cache:
+                self.assertRegex(
+                    cmd.stdout,
+                    rf"(?m)^\s*{f.fileno()}\s+[0-9a-f]+\s+[0-9a-f]+\s+[0-9]+\s+REG\s+{re.escape(f.name)}",
+                )
+            else:
+                self.assertFalse(cmd.stdout)
+            self._test_drgn_common(cmd, cache=True)
+            self.assertIsInstance(cmd.drgn_option.globals["is_match"], bool)
+
+    def test_R_fd(self):
+        self._test_R(lambda f: str(f.fileno()), True, True)
+
+    def test_R_filename(self):
+        self._test_R(lambda f: f.name, True, True)
+
+    def test_R_dentry(self):
+        self._test_R(
+            lambda f: hex(
+                fget(find_task(self.prog, os.getpid()), f.fileno()).f_path.dentry
+            ),
+            True,
+            False,
+        )
+
+    def test_R_inode(self):
+        self._test_R(
+            lambda f: hex(fget(find_task(self.prog, os.getpid()), f.fileno()).f_inode),
+            True,
+            True,
+        )
+
+    def test_R_address_space(self):
+        self._test_R(
+            lambda f: hex(
+                fget(find_task(self.prog, os.getpid()), f.fileno()).f_inode.i_mapping
+            ),
+            False,
+            True,
+        )
+
+    def test_R_file(self):
+        self._test_R(
+            lambda f: hex(fget(find_task(self.prog, os.getpid()), f.fileno())),
+            True,
+            False,
+        )
+
+    def test_R_cwd(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with fork_and_stop(os.chdir, tmp_dir) as (pid, _):
+                cmd = self.check_crash_command(f"files -R {tmp_dir} {pid}")
+
+        self.assertRegex(cmd.stdout, rf"CWD: {re.escape(tmp_dir)}\n$")
+
+        self.assertEqual(cmd.drgn_option.globals["cwd"], os.fsencode(tmp_dir))
+        self.assertIsInstance(cmd.drgn_option.globals["is_match"], bool)
+
+    def test_tasks(self):
+        with tempfile.NamedTemporaryFile() as f:
+            with fork_and_stop() as pid:
+                cmd = self.check_crash_command(f"files {os.getpid()} {pid}")
+
+            self.assertIn(f"PID: {os.getpid()}", cmd.stdout)
+            self.assertIn(f"PID: {pid}", cmd.stdout)
+
+            self.assertEqual(
+                len(re.findall(rf"^\s*{f.fileno()}\b", cmd.stdout, flags=re.M)), 2
+            )
+
+    def test_no_files(self):
+        cmd = self.check_crash_command(f"files {hex(self.prog['init_task'].address_)}")
+        self.assertRegex(cmd.stdout, r"\nNo open files\n$")
 
 
 class TestMount(CrashCommandTestCase):
