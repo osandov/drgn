osandov/drgn

commit 6274f69ce1d3f129eb7c27ba41eb13e0e538b220

Allow passing name string to sizeof(), alignof(), etc.
sizeof() requires a Type or Object argument, and alignof(), offsetof(),
member_at_offset(), and typeof_member() all require a Type argument. If
you only have the name, this requires writing out sizeof(prog["foo"]) or
sizeof(prog.type("struct bar")), which is annoying and looks
inconsistent with functions that get the Program from elsewhere (e.g.,
cast()).

Make these functions accept a type name (or object name for sizeof()) as
a string, which will be looked up in the default program.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -3882,17 +3882,19 @@ class Qualifiers(enum.Flag):
     ATOMIC = ...
     """Atomic type."""
 
-def sizeof(__type_or_obj: Union[Type, Object]) -> int:
+def sizeof(type_or_obj: Union[Type, Object, str], /) -> int:
     """
     Get the size of a :class:`Type` or :class:`Object` in bytes.
 
-    :param type_or_obj: Entity to get the size of.
+    :param type_or_obj: Entity to get the size of. If given as a string, it is
+        looked up (first as a type, then as an object) in the
+        :ref:`default-program <default program>`.
     :raises TypeError: if the type does not have a size (e.g., because it is
         incomplete or void)
     """
     ...
 
-def alignof(__type: Type) -> int:
+def alignof(type: Union[Type, str], /) -> int:
     """
     Get the alignment requirement (in bytes) of a :class:`Type`.
 
@@ -3901,11 +3903,13 @@ def alignof(__type: Type) -> int:
     .. |alignof()| replace:: ``_Alignof()``
     .. _alignof(): https://en.cppreference.com/w/c/language/_Alignof
 
+    :param type: Type. If given as a string, it is looked up in the
+        :ref:`default-program <default program>`.
     :raises TypeError: if *type* is a function type or an incomplete type
     """
     ...
 
-def offsetof(type: Type, member: str) -> int:
+def offsetof(type: Union[Type, str], member: str) -> int:
     """
     Get the offset (in bytes) of a member in a :class:`Type`.
 
@@ -3914,7 +3918,8 @@ def offsetof(type: Type, member: str) -> int:
     .. |offsetof()| replace:: ``offsetof()``
     .. _offsetof(): https://en.cppreference.com/w/c/types/offsetof
 
-    :param type: Structure, union, or class type.
+    :param type: Structure, union, or class type. If given as a string, it is
+        looked up in the :ref:`default-program <default program>`.
     :param member: Name of member. May include one or more member references
         and zero or more array subscripts.
     :raises TypeError: if *type* is not a structure, union, or class type

drgn/helpers/common/type.py
@@ -12,9 +12,17 @@ with types in ways that aren't provided by the core drgn library.
 import enum
 import operator
 import typing
-from typing import Container, List, Tuple
+from typing import Container, List, Optional, Tuple, Union
 
-from drgn import IntegerLike, Object, Type, TypeKind, TypeMember, sizeof
+from drgn import (
+    IntegerLike,
+    Object,
+    Type,
+    TypeKind,
+    TypeMember,
+    get_default_prog,
+    sizeof,
+)
 
 __all__ = (
     "enum_type_to_class",
@@ -45,62 +53,62 @@ def enum_type_to_class(
     return enum.IntEnum(name, enumerators)  # type: ignore  # python/mypy#4865
 
 
-def member_at_offset(type: Type, offset: IntegerLike) -> str:
+def member_at_offset(type: Union[Type, str], offset: IntegerLike) -> str:
     """
     Return the name of the member at an offset in a type.
 
     This is effectively the opposite of :func:`~drgn.offsetof()`.
 
-    >>> prog.type('struct list_head')
+    >>> prog.type("struct list_head")
     struct list_head {
             struct list_head *next;
             struct list_head *prev;
     }
-    >>> member_at_offset(prog.type('struct list_head'), 0)
+    >>> member_at_offset("struct list_head", 0)
     'next'
-    >>> member_at_offset(prog.type('struct list_head'), 8)
+    >>> member_at_offset("struct list_head", 8)
     'prev'
 
     This includes nested structures and array elements:
 
-    >>> prog.type('struct sigpending')
+    >>> prog.type("struct sigpending")
     struct sigpending {
             struct list_head list;
             sigset_t signal;
     }
-    >>> prog.type('sigset_t')
+    >>> prog.type("sigset_t")
     typedef struct {
             unsigned long sig[1];
     } sigset_t
-    >>> member_at_offset(prog.type('struct sigpending'), 0)
+    >>> member_at_offset("struct sigpending", 0)
     'list.next'
-    >>> member_at_offset(prog.type('struct sigpending'), 8)
+    >>> member_at_offset("struct sigpending", 8)
     'list.prev'
-    >>> member_at_offset(prog.type('struct sigpending'), 16)
+    >>> member_at_offset("struct sigpending", 16)
     'signal.sig[0]'
 
     This also includes all possible matches for a union:
 
-    >>> prog.type('union mc_target')
+    >>> prog.type("union mc_target")
     union mc_target {
             struct folio *folio;
             swp_entry_t ent;
     }
-    >>> prog.type('swp_entry_t')
+    >>> prog.type("swp_entry_t")
     typedef struct {
             unsigned long val;
     } swp_entry_t
-    >>> member_at_offset(prog.type('union mc_target'), 0)
+    >>> member_at_offset("union mc_target", 0)
     'folio or ent.val'
 
     Offsets in the middle of a member are represented:
 
-    >>> member_at_offset(prog.type('struct list_head'), 4)
+    >>> member_at_offset("struct list_head", 4)
     'next+0x4'
 
     Offsets in padding or past the end of the type are also represented:
 
-    >>> prog.type('struct autogroup')
+    >>> prog.type("struct autogroup")
     struct autogroup {
             struct kref kref;
             struct task_group *tg;
@@ -108,22 +116,25 @@ def member_at_offset(type: Type, offset: IntegerLike) -> str:
             unsigned long id;
             int nice;
     }
-    >>> member_at_offset(prog.type('struct autogroup'), 4)
+    >>> member_at_offset("struct autogroup", 4)
     '<padding between kref and tg>'
-    >>> member_at_offset(prog.type('struct autogroup'), 70)
+    >>> member_at_offset("struct autogroup", 70)
     '<padding at end>'
-    >>> member_at_offset(prog.type('struct autogroup'), 72)
+    >>> member_at_offset("struct autogroup", 72)
     '<end>'
-    >>> member_at_offset(prog.type('struct autogroup'), 80)
+    >>> member_at_offset("struct autogroup", 80)
     '<past end>'
 
-    :param type: Type to check.
+    :param type: Type to check. If given as a string, it is looked up in the
+        :ref:`default-program <default program>`.
     :param offset: Offset in bytes.
     :raises TypeError: if *type* is not a structure, union, class, or array
         type (or a typedef of one of those)
     """
     bit_offset = operator.index(offset) * 8
 
+    if not isinstance(type, Type):
+        type = get_default_prog().type(type)
     type = type.unaliased()
     if type.kind not in (
         TypeKind.STRUCT,
@@ -190,7 +201,7 @@ def member_at_offset(type: Type, offset: IntegerLike) -> str:
                 if not emit_and_pop_member():
                     break
         else:
-            members = getattr(type, "members", None)
+            members: Optional[List[TypeMember]] = getattr(type, "members", None)
             if members is None:
                 if not emit_and_pop_member():
                     break
@@ -290,17 +301,24 @@ def member_at_offset(type: Type, offset: IntegerLike) -> str:
     return " or ".join(results)
 
 
-def typeof_member(type: Type, member: str) -> Type:
+def typeof_member(type: Union[Type, str], member: str) -> Type:
     """
     Get the type of a member in a :class:`~drgn.Type`.
 
     This corresponds to the ``typeof_member()`` macro used in the Linux kernel
     and other projects.
 
-    :param type: Structure, union, or class type.
+    :param type: Structure, union, or class type. If given as a string, it is
+        looked up in the :ref:`default-program <default program>`.
     :param member: Name of member. May include one or more member references
         and zero or more array subscripts.
     :raises TypeError: if *type* is not a structure, union, or class type
     :raises LookupError: if *type* does not have a member with the given name
     """
-    return Object(type.prog, type, address=0).subobject_(member).type_
+    return (
+        Object(
+            type.prog if isinstance(type, Type) else get_default_prog(), type, address=0
+        )
+        .subobject_(member)
+        .type_
+    )

libdrgn/python/drgnpy.h
@@ -346,6 +346,17 @@ int init_logging(void);
 
 bool set_drgn_in_python(void);
 void clear_drgn_in_python(void);
+
+static inline void drgn_in_python_cleanup(bool *clearp)
+{
+	if (*clearp)
+		clear_drgn_in_python();
+}
+
+#define drgn_in_python_guard()							\
+	__attribute__((__cleanup__(drgn_in_python_cleanup), __unused__))	\
+	bool PP_UNIQUE(clear) = set_drgn_in_python()
+
 struct drgn_error *drgn_error_from_python(void);
 void *set_drgn_error(struct drgn_error *err);
 void *set_error_type_name(const char *format,

libdrgn/python/main.c
@@ -4,6 +4,7 @@
 #include <elfutils/libdwfl.h>
 
 #include "drgnpy.h"
+#include "../error.h"
 #include "../path.h"
 
 /* Basically PyModule_AddType(), which is only available since Python 3.9. */
@@ -38,7 +39,7 @@ static PyObject *NoDefaultProgramError;
 PyObject *ObjectAbsentError;
 PyObject *OutOfBoundsError;
 
-static _Thread_local PyObject *default_prog;
+static _Thread_local Program *default_prog;
 
 static PyObject *get_default_prog(PyObject *self, PyObject *_)
 {
@@ -47,7 +48,7 @@ static PyObject *get_default_prog(PyObject *self, PyObject *_)
 		return NULL;
 	}
 	Py_INCREF(default_prog);
-	return default_prog;
+	return (PyObject *)default_prog;
 }
 
 static PyObject *set_default_prog(PyObject *self, PyObject *arg)
@@ -56,7 +57,7 @@ static PyObject *set_default_prog(PyObject *self, PyObject *arg)
 		Py_CLEAR(default_prog);
 	} else if (PyObject_TypeCheck(arg, &Program_type)) {
 		Py_INCREF(arg);
-		Py_XSETREF(default_prog, arg);
+		Py_XSETREF(default_prog, (Program *)arg);
 	} else {
 		PyErr_SetString(PyExc_TypeError,
 				"prog must be Program or None");
@@ -106,9 +107,35 @@ static PyObject *sizeof_(PyObject *self, PyObject *arg)
 		err = drgn_type_sizeof(((DrgnType *)arg)->type, &size);
 	} else if (PyObject_TypeCheck(arg, &DrgnObject_type)) {
 		err = drgn_object_sizeof(&((DrgnObject *)arg)->obj, &size);
+	} else if (PyUnicode_Check(arg)) {
+		if (!default_prog) {
+			PyErr_SetString(NoDefaultProgramError,
+					"no default program");
+			return NULL;
+		}
+
+		const char *name = PyUnicode_AsUTF8(arg);
+		if (!name)
+			return NULL;
+
+		drgn_in_python_guard();
+		struct drgn_qualified_type qualified_type;
+		err = drgn_program_find_type(&default_prog->prog, name, NULL,
+					     &qualified_type);
+		if (!err) {
+			err = drgn_type_sizeof(qualified_type.type, &size);
+		} else if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP)) {
+			DRGN_OBJECT(obj, &default_prog->prog);
+			err = drgn_program_find_object(&default_prog->prog,
+						       name, NULL,
+						       DRGN_FIND_OBJECT_ANY,
+						       &obj);
+			if (!err)
+				err = drgn_object_sizeof(&obj, &size);
+		}
 	} else {
 		return PyErr_Format(PyExc_TypeError,
-				    "expected Type or Object, not %s",
+				    "expected Type, Object, or str, not %s",
 				    Py_TYPE(arg)->tp_name);
 	}
 	if (err)
@@ -116,17 +143,46 @@ static PyObject *sizeof_(PyObject *self, PyObject *arg)
 	return PyLong_FromUint64(size);
 }
 
+static bool
+default_prog_find_type(PyObject *arg, struct drgn_qualified_type *ret)
+{
+	struct drgn_error *err;
+
+	if (!default_prog) {
+		PyErr_SetString(NoDefaultProgramError, "no default program");
+		return false;
+	}
+
+	const char *name = PyUnicode_AsUTF8(arg);
+	if (!name)
+		return false;
+
+	bool clear = set_drgn_in_python();
+	err = drgn_program_find_type(&default_prog->prog, name, NULL, ret);
+	if (clear)
+		clear_drgn_in_python();
+	if (err) {
+		set_drgn_error(err);
+		return false;
+	}
+	return true;
+}
+
 static PyObject *alignof_(PyObject *self, PyObject *arg)
 {
 	struct drgn_error *err;
-	if (!PyObject_TypeCheck(arg, &DrgnType_type)) {
-		return PyErr_Format(PyExc_TypeError, "expected Type not %s",
+	struct drgn_qualified_type qualified_type;
+	if (PyObject_TypeCheck(arg, &DrgnType_type)) {
+		qualified_type.type = ((DrgnType *)arg)->type;
+		qualified_type.qualifiers = ((DrgnType *)arg)->qualifiers;
+	} else if (PyUnicode_Check(arg)) {
+		if (!default_prog_find_type(arg, &qualified_type))
+			return NULL;
+	} else {
+		return PyErr_Format(PyExc_TypeError,
+				    "expected Type or str, not %s",
 				    Py_TYPE(arg)->tp_name);
 	}
-	struct drgn_qualified_type qualified_type = {
-		.type = ((DrgnType *)arg)->type,
-		.qualifiers = ((DrgnType *)arg)->qualifiers,
-	};
 	uint64_t size;
 	err = drgn_type_alignof(qualified_type, &size);
 	if (err)
@@ -139,13 +195,28 @@ static PyObject *offsetof_(PyObject *self, PyObject *args, PyObject *kwds)
 	struct drgn_error *err;
 
 	static char *keywords[] = {"type", "member", NULL};
-	DrgnType *type;
+	PyObject *arg;
 	const char *member;
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!s:offsetof", keywords,
-					 &DrgnType_type, &type, &member))
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "Os:offsetof", keywords,
+					 &arg, &member))
 		return NULL;
+
+	struct drgn_type *type;
+	if (PyObject_TypeCheck(arg, &DrgnType_type)) {
+		type = ((DrgnType *)arg)->type;
+	} else if (PyUnicode_Check(arg)) {
+		struct drgn_qualified_type qualified_type;
+		if (!default_prog_find_type(arg, &qualified_type))
+			return NULL;
+		type = qualified_type.type;
+	} else {
+		return PyErr_Format(PyExc_TypeError,
+				    "expected Type or str, not %s",
+				    Py_TYPE(arg)->tp_name);
+	}
+
 	uint64_t offset;
-	err = drgn_type_offsetof(type->type, member, &offset);
+	err = drgn_type_offsetof(type, member, &offset);
 	if (err)
 		return set_drgn_error(err);
 	return PyLong_FromUint64(offset);

tests/__init__.py
@@ -16,6 +16,7 @@ from drgn import (
     Architecture,
     FindObjectFlags,
     Language,
+    NoDefaultProgramError,
     Object,
     Platform,
     PlatformFlags,
@@ -27,6 +28,8 @@ from drgn import (
     TypeMember,
     TypeParameter,
     TypeTemplateParameter,
+    get_default_prog,
+    set_default_prog,
 )
 
 try:
@@ -448,3 +451,16 @@ def drgn_log_level(level: int):
         yield
     finally:
         logger.setLevel(old_level)
+
+
+@contextlib.contextmanager
+def with_default_prog(prog):
+    try:
+        old_default_prog = get_default_prog()
+    except NoDefaultProgramError:
+        old_default_prog = None
+    try:
+        set_default_prog(prog)
+        yield
+    finally:
+        set_default_prog(old_default_prog)

tests/helpers/common/test_type.py
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 
-from drgn import TypeMember
+from drgn import NoDefaultProgramError, TypeMember
 from drgn.helpers.common.type import member_at_offset, typeof_member
-from tests import TestCase, mock_program
+from tests import TestCase, mock_program, with_default_prog
 
 
 class TestMemberAtOffset(TestCase):
@@ -451,6 +451,25 @@ class TestMemberAtOffset(TestCase):
         self.assertEqual(member_at_offset(line_segment_type, 8), "b.x")
         self.assertEqual(member_at_offset(line_segment_type, 12), "b.y")
 
+    def test_default_prog(self):
+        self.assertRaises(NoDefaultProgramError, member_at_offset, "struct point", 0)
+
+        types = []
+        prog = mock_program(types=types)
+        types.append(
+            prog.struct_type(
+                "point",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.int_type("int", 4, True), "y", 32),
+                ),
+            )
+        )
+        with with_default_prog(prog):
+            self.assertIdentical(member_at_offset("struct point", 0), "x")
+            self.assertRaises(LookupError, member_at_offset, "struct foo", 0)
+
 
 class TestTypeofMember(TestCase):
     def test_simple(self):
@@ -466,3 +485,24 @@ class TestTypeofMember(TestCase):
         self.assertIdentical(
             typeof_member(point_type, "x"), prog.int_type("int", 4, True)
         )
+
+    def test_default_prog(self):
+        self.assertRaises(NoDefaultProgramError, typeof_member, "struct point", "x")
+
+        types = []
+        prog = mock_program(types=types)
+        types.append(
+            prog.struct_type(
+                "point",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.int_type("int", 4, True), "y", 32),
+                ),
+            )
+        )
+        with with_default_prog(prog):
+            self.assertIdentical(
+                typeof_member("struct point", "x"), prog.int_type("int", 4, True)
+            )
+            self.assertRaises(LookupError, typeof_member, "struct foo", "x")

tests/linux_kernel/crash_commands/__init__.py
@@ -7,23 +7,12 @@ import os
 import sys
 import types
 
-import drgn
 from drgn.commands.crash import CRASH_COMMAND_NAMESPACE
+from tests import with_default_prog
 from tests.linux_kernel import LinuxKernelTestCase
 
 
 class CrashCommandTestCase(LinuxKernelTestCase):
-    @contextlib.contextmanager
-    def with_default_prog(self):
-        try:
-            old_default_prog = drgn.get_default_prog()
-        except drgn.NoDefaultProgramError:
-            old_default_prog = None
-        try:
-            drgn.set_default_prog(self.prog)
-            yield
-        finally:
-            drgn.set_default_prog(old_default_prog)
 
     # Run a crash command and capture its stdout and stderr.
     @classmethod
@@ -68,7 +57,7 @@ class CrashCommandTestCase(LinuxKernelTestCase):
 
         if mode == "exec":
             ret.globals = {"prog": self.prog}
-            with self.with_default_prog():
+            with with_default_prog(self.prog):
                 exec(ret.stdout, ret.globals)
 
         return ret

tests/linux_kernel/crash_commands/test_kmem.py
@@ -11,6 +11,7 @@ from drgn.commands import CommandArgumentError
 from drgn.commands.crash import CRASH_COMMAND_NAMESPACE
 from drgn.helpers.linux.mm import PageUsage
 from drgn.helpers.linux.slab import SlabTotalUsage
+from tests import with_default_prog
 from tests.linux_kernel import possible_cpus, skip_unless_have_test_kmod
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
 from tests.linux_kernel.helpers.test_slab import fallback_slab_cache_names
@@ -343,7 +344,7 @@ class TestKmem(CrashCommandTestCase):
 
         drgn_option = self.run_crash_command_drgn_option("kmem -p", mode="capture")
         drgn_option_globals = {"prog": self.prog}
-        with self.with_default_prog():
+        with with_default_prog(self.prog):
             exec(drgn_option.stdout + "\n    break", drgn_option_globals)
         for variable in ("physical", "mapping", "index", "cnt", "flags"):
             self.assertIsInstance(drgn_option_globals[variable], Object)
@@ -375,7 +376,7 @@ class TestKmem(CrashCommandTestCase):
             f"kmem -m {members}", mode="capture"
         )
         drgn_option_globals = {"prog": self.prog}
-        with self.with_default_prog():
+        with with_default_prog(self.prog):
             exec(drgn_option.stdout + "\n    break", drgn_option_globals)
         for variable in ("mapping", "private", "_refcount", "lru", "flags"):
             self.assertIsInstance(drgn_option_globals[variable], Object)

commit 8213fd8acf7cf207ad14e6a47f6a23d0cd8e9029

docs: fix references to <default program>
I swapped where the reference and the title go, causing "undefined
label: 'default program'" warnings from Sphinx.

Fixes: 6274f69ce1d3 ("Allow passing name string to sizeof(), alignof(), etc.")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -3888,7 +3888,7 @@ def sizeof(type_or_obj: Union[Type, Object, str], /) -> int:
 
     :param type_or_obj: Entity to get the size of. If given as a string, it is
         looked up (first as a type, then as an object) in the
-        :ref:`default-program <default program>`.
+        :ref:`default program <default-program>`.
     :raises TypeError: if the type does not have a size (e.g., because it is
         incomplete or void)
     """
@@ -3904,7 +3904,7 @@ def alignof(type: Union[Type, str], /) -> int:
     .. _alignof(): https://en.cppreference.com/w/c/language/_Alignof
 
     :param type: Type. If given as a string, it is looked up in the
-        :ref:`default-program <default program>`.
+        :ref:`default program <default-program>`.
     :raises TypeError: if *type* is a function type or an incomplete type
     """
     ...
@@ -3919,7 +3919,7 @@ def offsetof(type: Union[Type, str], member: str) -> int:
     .. _offsetof(): https://en.cppreference.com/w/c/types/offsetof
 
     :param type: Structure, union, or class type. If given as a string, it is
-        looked up in the :ref:`default-program <default program>`.
+        looked up in the :ref:`default program <default-program>`.
     :param member: Name of member. May include one or more member references
         and zero or more array subscripts.
     :raises TypeError: if *type* is not a structure, union, or class type

drgn/helpers/common/type.py
@@ -126,7 +126,7 @@ def member_at_offset(type: Union[Type, str], offset: IntegerLike) -> str:
     '<past end>'
 
     :param type: Type to check. If given as a string, it is looked up in the
-        :ref:`default-program <default program>`.
+        :ref:`default program <default-program>`.
     :param offset: Offset in bytes.
     :raises TypeError: if *type* is not a structure, union, class, or array
         type (or a typedef of one of those)
@@ -309,7 +309,7 @@ def typeof_member(type: Union[Type, str], member: str) -> Type:
     and other projects.
 
     :param type: Structure, union, or class type. If given as a string, it is
-        looked up in the :ref:`default-program <default program>`.
+        looked up in the :ref:`default program <default-program>`.
     :param member: Name of member. May include one or more member references
         and zero or more array subscripts.
     :raises TypeError: if *type* is not a structure, union, or class type