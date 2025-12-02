osandov/drgn

commit 53a0b3fa57ae4b062c5efe127b89a09900114a41

Add class to represent stack frame source location
This both documents the return value of StackFrame.source() better and
prepares for adding an API for getting source locations without a stack
trace.

This also adds StackFrame.source().name() as yet another variant of the
stack frame name (one that does fall back to a symbol but not to the
hexadecimal address), which is probably overkill but is necessary for
the aforementioned API.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -3189,11 +3189,10 @@ class StackFrame:
         """
         ...
 
-    def source(self) -> Tuple[str, int, int]:
+    def source(self) -> SourceLocation:
         """
         Get the source code location of this frame.
 
-        :return: Location as a ``(filename, line, column)`` triple.
         :raises LookupError: if the source code location is not available
         """
         ...
@@ -3224,6 +3223,46 @@ class StackFrame:
         """
         Get the values of all available registers at this stack frame as a
         dictionary with the register names as keys.
+        """
+        ...
+
+    def _repr_pretty_(self, p: Any, cycle: bool) -> None: ...
+
+# This isn't really a NamedTuple, but it's the best approximation since we
+# allow unpacking the attributes.
+class SourceLocation(NamedTuple):
+    """
+    File, line, and column location in source code.
+
+    The attributes can be accessed by name:
+
+    >>> filename = loc.filename
+    >>> line = loc.line
+    >>> column = loc.column
+
+    or unpacked:
+
+    >>> filename, line, column = loc
+
+    :class:`str() <str>` returns a pretty-printed location:
+
+    >>> loc
+    context_switch at kernel/sched/core.c:5381:9
+    """
+
+    filename: str
+    """Source file name, or empty string if unknown."""
+
+    line: int
+    """Source line number, or 0 if unknown."""
+
+    column: int
+    """Source column, or 0 if unknown."""
+
+    def name(self) -> Optional[str]:
+        """
+        Get the name of the function or symbol at this source location, or
+        ``None`` if it could not be determined.
         """
         ...
 

libdrgn/python/drgnpy.h
@@ -208,6 +208,15 @@ typedef struct {
 
 typedef struct {
 	PyObject_HEAD
+	PyObject *filename;
+	PyObject *line;
+	PyObject *column;
+	PyObject *obj;
+	size_t i;
+} SourceLocation;
+
+typedef struct {
+	PyObject_HEAD
 	struct drgn_stack_trace *trace;
 } StackTrace;
 
@@ -301,6 +310,7 @@ extern PyTypeObject Program_type;
 extern PyTypeObject Register_type;
 extern PyTypeObject RelocatableModule_type;
 extern PyTypeObject SharedLibraryModule_type;
+extern PyTypeObject SourceLocation_type;
 extern PyTypeObject StackFrame_type;
 extern PyTypeObject StackTrace_type;
 extern PyTypeObject SymbolIndex_type;
@@ -382,6 +392,9 @@ int Program_type_arg(Program *prog, PyObject *type_obj, bool can_be_none,
 Program *program_from_core_dump(PyObject *self, PyObject *args, PyObject *kwds);
 Program *program_from_kernel(PyObject *self);
 Program *program_from_pid(PyObject *self, PyObject *args, PyObject *kwds);
+
+PyObject *SourceLocation_wrap(const char *filename, int line, int column,
+			      PyObject *obj, size_t i);
 
 PyObject *Symbol_wrap(struct drgn_symbol *sym, PyObject *name_obj);
 PyObject *Symbol_list_wrap(struct drgn_symbol **symbols, size_t count,

libdrgn/python/main.c
@@ -312,6 +312,7 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	    add_type(m, &Platform_type) ||
 	    add_type(m, &Program_type) ||
 	    add_type(m, &Register_type) ||
+	    add_type(m, &SourceLocation_type) ||
 	    add_type(m, &StackFrame_type) ||
 	    add_type(m, &StackTrace_type) ||
 	    add_type(m, &Symbol_type) ||

libdrgn/python/source_location.c
@@ -1,0 +1,130 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "drgnpy.h"
+
+PyObject *SourceLocation_wrap(const char *filename, int line, int column,
+			      PyObject *obj, size_t i)
+{
+	SourceLocation *ret = call_tp_alloc(SourceLocation);
+	if (!ret)
+		return NULL;
+	ret->filename = PyUnicode_FromString(filename);
+	if (!ret->filename)
+		return NULL;
+	ret->line = PyLong_FromLong(line);
+	if (!ret->line)
+		return NULL;
+	ret->column = PyLong_FromLong(column);
+	if (!ret->column)
+		return NULL;
+	Py_INCREF(obj);
+	ret->obj = obj;
+	ret->i = i;
+	return (PyObject *)ret;
+}
+
+static void SourceLocation_dealloc(SourceLocation *self)
+{
+	PyObject_GC_UnTrack(self);
+	Py_XDECREF(self->obj);
+	Py_XDECREF(self->column);
+	Py_XDECREF(self->line);
+	Py_XDECREF(self->filename);
+	Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static int SourceLocation_traverse(SourceLocation *self, visitproc visit,
+				   void *arg)
+{
+	Py_VISIT(self->filename);
+	Py_VISIT(self->line);
+	Py_VISIT(self->column);
+	Py_VISIT(self->obj);
+	return 0;
+}
+
+static PyObject *SourceLocation_str(SourceLocation *self)
+{
+	struct drgn_error *err;
+	_cleanup_free_ char *str = NULL;
+	StackTrace *trace = (StackTrace *)self->obj;
+	err = drgn_format_stack_frame_source(trace->trace, self->i, &str);
+	if (err)
+		return set_drgn_error(err);
+	return PyUnicode_FromString(str);
+}
+
+static PyObject *SourceLocation_name(SourceLocation *self)
+{
+	struct drgn_error *err;
+	_cleanup_free_ char *str = NULL;
+	StackTrace *trace = (StackTrace *)self->obj;
+	err = drgn_stack_frame_source_name(trace->trace, self->i, &str);
+	if (err)
+		return set_drgn_error(err);
+	if (!str)
+		Py_RETURN_NONE;
+	return PyUnicode_FromString(str);
+}
+
+static Py_ssize_t SourceLocation_length(PyObject *self)
+{
+	return 3;
+}
+
+static PyObject *SourceLocation_item(SourceLocation *self, Py_ssize_t i)
+{
+	switch (i) {
+	case 0:
+		Py_INCREF(self->filename);
+		return self->filename;
+	case 1:
+		Py_INCREF(self->line);
+		return self->line;
+	case 2:
+		Py_INCREF(self->column);
+		return self->column;
+	default:
+		PyErr_SetString(PyExc_IndexError,
+				"SourceLocation index out of range");
+		return NULL;
+	}
+}
+
+static PySequenceMethods SourceLocation_as_sequence = {
+	.sq_length = SourceLocation_length,
+	.sq_item = (ssizeargfunc)SourceLocation_item,
+};
+
+static PyMemberDef SourceLocation_members[] = {
+	{"filename", T_OBJECT, offsetof(SourceLocation, filename), READONLY,
+	 drgn_SourceLocation_filename_DOC},
+	{"line", T_OBJECT, offsetof(SourceLocation, line), READONLY,
+	 drgn_SourceLocation_line_DOC},
+	{"column", T_OBJECT, offsetof(SourceLocation, column), READONLY,
+	 drgn_SourceLocation_column_DOC},
+	{},
+};
+
+static PyMethodDef SourceLocation_methods[] = {
+	{"name", (PyCFunction)SourceLocation_name, METH_NOARGS,
+	 drgn_SourceLocation_name_DOC},
+	{"_repr_pretty_", (PyCFunction)repr_pretty_from_str,
+	 METH_VARARGS | METH_KEYWORDS},
+	{},
+};
+
+PyTypeObject SourceLocation_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.SourceLocation",
+	.tp_basicsize = sizeof(SourceLocation),
+	.tp_dealloc = (destructor)SourceLocation_dealloc,
+	.tp_str = (reprfunc)SourceLocation_str,
+	.tp_as_sequence = &SourceLocation_as_sequence,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,
+	.tp_doc = drgn_SourceLocation_DOC,
+	.tp_traverse = (traverseproc)SourceLocation_traverse,
+	.tp_methods = SourceLocation_methods,
+	.tp_members = SourceLocation_members,
+};

libdrgn/python/stack_trace.c
@@ -216,7 +216,8 @@ static PyObject *StackFrame_source(StackFrame *self)
 				"source code location not available");
 		return NULL;
 	}
-	return Py_BuildValue("sii", filename, line, column);
+	return SourceLocation_wrap(filename, line, column,
+				   (PyObject *)self->trace, self->i);
 }
 
 static PyObject *StackFrame_symbol(StackFrame *self)

libdrgn/stack_trace.c
@@ -226,6 +226,54 @@ drgn_format_stack_frame(struct drgn_stack_trace *trace, size_t frame, char **ret
 	return NULL;
 }
 
+static struct drgn_error *
+drgn_format_stack_frame_source_impl(struct drgn_stack_trace *trace, size_t frame,
+				    struct string_builder *sb)
+{
+	struct drgn_error *err;
+
+	{
+		_cleanup_free_ char *name = NULL;
+		err = drgn_stack_frame_source_name(trace, frame, &name);
+		if (err)
+			return err;
+
+		if (!string_builder_append(sb, name ? name : "???"))
+			return &drgn_enomem;
+	}
+
+	int line, column;
+	const char *filename = drgn_stack_frame_source(trace, frame, &line,
+						       &column);
+	if (filename && column) {
+		if (!string_builder_appendf(sb, " at %s:%d:%d", filename, line,
+					    column))
+			return &drgn_enomem;
+	} else if (filename) {
+		if (!string_builder_appendf(sb, " at %s:%d", filename, line))
+			return &drgn_enomem;
+	} else {
+		if (!string_builder_append(sb, " at ??:?"))
+			return &drgn_enomem;
+	}
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_format_stack_frame_source(struct drgn_stack_trace *trace, size_t frame,
+			       char **ret)
+{
+	struct drgn_error *err;
+	STRING_BUILDER(sb);
+	err = drgn_format_stack_frame_source_impl(trace, frame, &sb);
+	if (err)
+		return err;
+	if (!string_builder_null_terminate(&sb))
+		return &drgn_enomem;
+	*ret = string_builder_steal(&sb);
+	return NULL;
+}
+
 LIBDRGN_PUBLIC
 struct drgn_error *drgn_stack_frame_name(struct drgn_stack_trace *trace,
 					 size_t frame, char **ret)
@@ -269,6 +317,38 @@ const char *drgn_stack_frame_function_name(struct drgn_stack_trace *trace,
 	if (function_scope >= num_scopes)
 		return NULL;
 	return dwarf_diename(&scopes[function_scope]);
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_stack_frame_source_name(struct drgn_stack_trace *trace, size_t frame,
+			     char **ret)
+{
+	struct drgn_error *err;
+	char *name = NULL;
+	const char *function_name = drgn_stack_frame_function_name(trace, frame);
+	if (function_name) {
+		name = strdup(function_name);
+		if (!name)
+			return &drgn_enomem;
+	} else {
+		struct drgn_register_state *regs = trace->frames[frame].regs;
+		struct optional_uint64 pc = drgn_register_state_get_pc(regs);
+		if (pc.has_value) {
+			_cleanup_symbol_ struct drgn_symbol *sym = NULL;
+			err = drgn_program_find_symbol_by_address_internal(trace->prog,
+									   pc.value - !regs->interrupted,
+									   &sym);
+			if (err)
+				return err;
+			if (sym) {
+				name = strdup(sym->name);
+				if (!name)
+					return &drgn_enomem;
+			}
+		}
+	}
+	*ret = name;
+	return NULL;
 }
 
 LIBDRGN_PUBLIC bool drgn_stack_frame_is_inline(struct drgn_stack_trace *trace,

drgn/__init__.py
@@ -76,6 +76,7 @@ from _drgn import (
     Register,
     RelocatableModule,
     SharedLibraryModule,
+    SourceLocation,
     StackFrame,
     StackTrace,
     SupplementaryFileKind,
@@ -150,6 +151,7 @@ __all__ = (
     "Register",
     "RelocatableModule",
     "SharedLibraryModule",
+    "SourceLocation",
     "StackFrame",
     "StackTrace",
     "SupplementaryFileKind",

drgn/cli.py
@@ -185,7 +185,15 @@ def _displayhook(value: Any) -> None:
         except drgn.FaultError as e:
             logger.warning("can't print value: %s", e)
             text = repr(value)
-    elif isinstance(value, (drgn.StackFrame, drgn.StackTrace, drgn.Type)):
+    elif isinstance(
+        value,
+        (
+            drgn.SourceLocation,
+            drgn.StackFrame,
+            drgn.StackTrace,
+            drgn.Type,
+        ),
+    ):
         text = str(value)
     else:
         text = repr(value)

commit 2f12f6346417fa7faf4a4bc30bd722e97e42b85b

libdrgn: python: make drgn.SourceLocation actually a NamedTuple
The SourceLocation class was intended to be fully backwards compatible
with the tuple previously returned by drgn.StackTrace.source(). However,
Stephen Brennan reported that it broke printf-style string formatting,
i.e., "%s:%d:%d" % frame.source(). This is because printf-style
formatting handles tuples specially, and although SourceLocation behaves
like a NamedTuple, it is not truly a tuple.

Fix it by replacing the custom class with an actual NamedTuple subclass.
This is tricky because the SourceLocation.name() method uses state
beyond the NamedTuple attributes. The least awful way to support this is
by creating a NamedTuple base class and then subclassing it in Python.
This results in instances with __dict__, allowing us to add the name
callback as an extra attribute.

Fixes: 53a0b3fa57ae ("Add class to represent stack frame source location")
Reported-by: Stephen Brennan <stephen.s.brennan@oracle.com>
Signed-off-by: Omar Sandoval <osandov@osandov.com>

_drgn.pyi
@@ -3265,8 +3265,6 @@ class StackFrame:
 
     def _repr_pretty_(self, p: Any, cycle: bool) -> None: ...
 
-# This isn't really a NamedTuple, but it's the best approximation since we
-# allow unpacking the attributes.
 class SourceLocation(NamedTuple):
     """
     File, line, and column location in source code.

libdrgn/python/drgnpy.h
@@ -208,16 +208,6 @@ typedef struct {
 
 typedef struct {
 	PyObject_HEAD
-	PyObject *filename;
-	PyObject *line;
-	PyObject *column;
-	PyObject *obj;
-	size_t i;
-	bool is_stack_trace;
-} SourceLocation;
-
-typedef struct {
-	PyObject_HEAD
 	struct drgn_source_location_list *locs;
 } SourceLocationList;
 
@@ -317,7 +307,7 @@ extern PyTypeObject Register_type;
 extern PyTypeObject RelocatableModule_type;
 extern PyTypeObject SharedLibraryModule_type;
 extern PyTypeObject SourceLocationList_type;
-extern PyTypeObject SourceLocation_type;
+extern PyObject *SourceLocation_type;
 extern PyTypeObject StackFrame_type;
 extern PyTypeObject StackTrace_type;
 extern PyTypeObject SymbolIndex_type;
@@ -411,8 +401,7 @@ Program *program_from_core_dump(PyObject *self, PyObject *args, PyObject *kwds);
 Program *program_from_kernel(PyObject *self);
 Program *program_from_pid(PyObject *self, PyObject *args, PyObject *kwds);
 
-PyObject *SourceLocation_wrap(const char *filename, int line, int column,
-			      PyObject *obj, size_t i, bool is_stack_trace);
+int add_SourceLocation(PyObject *m);
 PyObject *SourceLocationList_wrap(struct drgn_source_location_list *locs);
 
 PyObject *Symbol_wrap(struct drgn_symbol *sym, PyObject *name_obj);

libdrgn/python/main.c
@@ -384,7 +384,7 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	    add_type(m, &Program_type) ||
 	    add_type(m, &Register_type) ||
 	    add_type(m, &SourceLocationList_type) ||
-	    add_type(m, &SourceLocation_type) ||
+	    add_SourceLocation(m) ||
 	    add_type(m, &StackFrame_type) ||
 	    add_type(m, &StackTrace_type) ||
 	    add_type(m, &Symbol_type) ||

libdrgn/python/source_location.c
@@ -4,145 +4,61 @@
 #include "drgnpy.h"
 #include "../util.h"
 
-PyObject *SourceLocation_wrap(const char *filename, int line, int column,
-			      PyObject *obj, size_t i, bool is_stack_trace)
-{
-	SourceLocation *ret = call_tp_alloc(SourceLocation);
-	if (!ret)
-		return NULL;
-	ret->filename = PyUnicode_FromString(filename);
-	if (!ret->filename)
-		return NULL;
-	ret->line = PyLong_FromLong(line);
-	if (!ret->line)
-		return NULL;
-	ret->column = PyLong_FromLong(column);
-	if (!ret->column)
-		return NULL;
-	Py_INCREF(obj);
-	ret->obj = obj;
-	ret->i = i;
-	ret->is_stack_trace = is_stack_trace;
-	return (PyObject *)ret;
-}
+PyObject *SourceLocation_type;
 
-static void SourceLocation_dealloc(SourceLocation *self)
+int add_SourceLocation(PyObject *m)
 {
-	PyObject_GC_UnTrack(self);
-	Py_XDECREF(self->obj);
-	Py_XDECREF(self->column);
-	Py_XDECREF(self->line);
-	Py_XDECREF(self->filename);
-	Py_TYPE(self)->tp_free((PyObject *)self);
-}
+	_cleanup_pydecref_ PyObject *globals = PyDict_New();
+	if (!globals)
+		return -1;
 
-static int SourceLocation_traverse(SourceLocation *self, visitproc visit,
-				   void *arg)
-{
-	Py_VISIT(self->filename);
-	Py_VISIT(self->line);
-	Py_VISIT(self->column);
-	Py_VISIT(self->obj);
+	_cleanup_pydecref_ PyObject *res = PyRun_String(
+"from typing import Callable, NamedTuple, Optional\n"
+"\n"
+"\n"
+"class SourceLocation(\n"
+"    NamedTuple(\n"
+"        'SourceLocationBase', [('filename', str), ('line', int), ('column', int)]\n"
+"    )\n"
+"):\n"
+"    def __new__(\n"
+"        cls,\n"
+"        filename: str,\n"
+"        line: int,\n"
+"        column: int,\n"
+"        name: Callable[[], Optional[str]] = lambda: None,\n"
+"    ) -> 'Foo':\n"
+"        ret = super().__new__(cls, filename, line, column)\n"
+"        ret.name = name\n"
+"        return ret\n"
+"\n"
+"    # Keep this in sync with drgn_format_stack_frame_source_impl().\n"
+"    def __str__(self) -> str:\n"
+"        name = self.name()\n"
+"        if name is None:\n"
+"            name = '\?\?\?'\n"
+"        if self.filename:\n"
+"            if self.column:\n"
+"                return f'{name} at {self.filename}:{self.line}:{self.column}'\n"
+"            else:\n"
+"                return f'{name} at {self.filename}:{self.line}'\n"
+"        else:\n"
+"            return f'{name} at \?\?:\?'\n"
+, Py_file_input, globals, globals);
+	if (!res)
+		return -1;
+
+	SourceLocation_type = PyDict_GetItemString(globals, "SourceLocation");
+	if (!SourceLocation_type)
+		return -1;
+	Py_INCREF(SourceLocation_type);
+
+	if (PyModule_AddObject(m, "SourceLocation", SourceLocation_type) == -1) {
+		Py_DECREF(SourceLocation_type);
+		return -1;
+	}
 	return 0;
 }
-
-static PyObject *SourceLocation_str(SourceLocation *self)
-{
-	struct drgn_error *err;
-	_cleanup_free_ char *str = NULL;
-	if (self->is_stack_trace) {
-		StackTrace *trace = (StackTrace *)self->obj;
-		err = drgn_format_stack_frame_source(trace->trace, self->i,
-						     &str);
-	} else {
-		SourceLocationList *locs = (SourceLocationList *)self->obj;
-		err = drgn_format_source_location_list_at(locs->locs, self->i,
-							  &str);
-	}
-	if (err)
-		return set_drgn_error(err);
-	return PyUnicode_FromString(str);
-}
-
-static PyObject *SourceLocation_name(SourceLocation *self)
-{
-	struct drgn_error *err;
-	_cleanup_free_ char *str = NULL;
-	if (self->is_stack_trace) {
-		StackTrace *trace = (StackTrace *)self->obj;
-		err = drgn_stack_frame_source_name(trace->trace, self->i, &str);
-	} else {
-		SourceLocationList *locs = (SourceLocationList *)self->obj;
-		err = drgn_source_location_list_name_at(locs->locs, self->i,
-							&str);
-	}
-	if (err)
-		return set_drgn_error(err);
-	if (!str)
-		Py_RETURN_NONE;
-	return PyUnicode_FromString(str);
-}
-
-static Py_ssize_t SourceLocation_length(PyObject *self)
-{
-	return 3;
-}
-
-static PyObject *SourceLocation_item(SourceLocation *self, Py_ssize_t i)
-{
-	switch (i) {
-	case 0:
-		Py_INCREF(self->filename);
-		return self->filename;
-	case 1:
-		Py_INCREF(self->line);
-		return self->line;
-	case 2:
-		Py_INCREF(self->column);
-		return self->column;
-	default:
-		PyErr_SetString(PyExc_IndexError,
-				"SourceLocation index out of range");
-		return NULL;
-	}
-}
-
-static PySequenceMethods SourceLocation_as_sequence = {
-	.sq_length = SourceLocation_length,
-	.sq_item = (ssizeargfunc)SourceLocation_item,
-};
-
-static PyMemberDef SourceLocation_members[] = {
-	{"filename", T_OBJECT, offsetof(SourceLocation, filename), READONLY,
-	 drgn_SourceLocation_filename_DOC},
-	{"line", T_OBJECT, offsetof(SourceLocation, line), READONLY,
-	 drgn_SourceLocation_line_DOC},
-	{"column", T_OBJECT, offsetof(SourceLocation, column), READONLY,
-	 drgn_SourceLocation_column_DOC},
-	{},
-};
-
-static PyMethodDef SourceLocation_methods[] = {
-	{"name", (PyCFunction)SourceLocation_name, METH_NOARGS,
-	 drgn_SourceLocation_name_DOC},
-	{"_repr_pretty_", (PyCFunction)repr_pretty_from_str,
-	 METH_VARARGS | METH_KEYWORDS},
-	{},
-};
-
-PyTypeObject SourceLocation_type = {
-	PyVarObject_HEAD_INIT(NULL, 0)
-	.tp_name = "_drgn.SourceLocation",
-	.tp_basicsize = sizeof(SourceLocation),
-	.tp_dealloc = (destructor)SourceLocation_dealloc,
-	.tp_str = (reprfunc)SourceLocation_str,
-	.tp_as_sequence = &SourceLocation_as_sequence,
-	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,
-	.tp_doc = drgn_SourceLocation_DOC,
-	.tp_traverse = (traverseproc)SourceLocation_traverse,
-	.tp_methods = SourceLocation_methods,
-	.tp_members = SourceLocation_members,
-};
 
 PyObject *SourceLocationList_wrap(struct drgn_source_location_list *locs)
 {
@@ -201,6 +117,23 @@ static Py_ssize_t SourceLocationList_length(SourceLocationList *self)
 	return drgn_source_location_list_length(self->locs);
 }
 
+static PyObject *SourceLocationList_name(PyObject *self)
+{
+	SourceLocationList *locs =
+		(SourceLocationList *)PyTuple_GET_ITEM(self, 0);
+	Py_ssize_t i = PyLong_AsSize_t(PyTuple_GET_ITEM(self, 1));
+	if (i == -1 && PyErr_Occurred())
+		return NULL;
+	_cleanup_free_ char *str = NULL;
+	struct drgn_error *err = drgn_source_location_list_name_at(locs->locs,
+								   i, &str);
+	if (err)
+		return set_drgn_error(err);
+	if (!str)
+		Py_RETURN_NONE;
+	return PyUnicode_FromString(str);
+}
+
 static PyObject *SourceLocationList_item(SourceLocationList *self, Py_ssize_t i)
 {
 	if (i < 0 || i >= drgn_source_location_list_length(self->locs)) {
@@ -216,8 +149,18 @@ static PyObject *SourceLocationList_item(SourceLocationList *self, Py_ssize_t i)
 		filename = "";
 		line = column = 0;
 	}
-	return SourceLocation_wrap(filename, line, column, (PyObject *)self, i,
-				   false);
+
+	static PyMethodDef meth = {
+		.ml_name = "name",
+		.ml_meth = (PyCFunction)SourceLocationList_name,
+		.ml_flags = METH_NOARGS,
+	};
+	_cleanup_pydecref_ PyObject *name_arg = Py_BuildValue("On", self, i);
+	if (!name_arg)
+		return NULL;
+	return PyObject_CallFunction(SourceLocation_type, "siiN", filename,
+				     line, column,
+				     PyCFunction_New(&meth, name_arg));
 }
 
 static PyMethodDef SourceLocationList_methods[] = {

libdrgn/python/stack_trace.c
@@ -205,6 +205,19 @@ static int StackFrame_contains(StackFrame *self, PyObject *key)
 	}
 }
 
+static PyObject *StackFrame_source_name(StackFrame *self)
+{
+	_cleanup_free_ char *str = NULL;
+	struct drgn_error *err =
+		drgn_stack_frame_source_name(self->trace->trace, self->i,
+					     &str);
+	if (err)
+		return set_drgn_error(err);
+	if (!str)
+		Py_RETURN_NONE;
+	return PyUnicode_FromString(str);
+}
+
 static PyObject *StackFrame_source(StackFrame *self)
 {
 	int line;
@@ -216,8 +229,15 @@ static PyObject *StackFrame_source(StackFrame *self)
 				"source code location not available");
 		return NULL;
 	}
-	return SourceLocation_wrap(filename, line, column,
-				   (PyObject *)self->trace, self->i, true);
+
+	static PyMethodDef meth = {
+		.ml_name = "name",
+		.ml_meth = (PyCFunction)StackFrame_source_name,
+		.ml_flags = METH_NOARGS,
+	};
+	return PyObject_CallFunction(SourceLocation_type, "siiN", filename,
+				     line, column,
+				     PyCFunction_New(&meth, (PyObject *)self));
 }
 
 static PyObject *StackFrame_symbol(StackFrame *self)