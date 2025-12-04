osandov/drgn

commit 0cf3320a89b3ebbbfca8b76928cd46054a58aa30

Add type annotations to helpers
Now that drgndoc can handle overloads and we have the IntegerLike and
Path aliases, we can add type annotations to all helpers. There are also
a couple of functional changes that snuck in here to make annotating
easier.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/list.py
@@ -10,7 +10,9 @@ doubly-linked list implementations (``struct list_head`` and ``struct
 hlist_head``) in :linux:`include/linux/list.h`.
 """
 
-from drgn import NULL, container_of
+from typing import Iterator, Union
+
+from drgn import NULL, Object, Type, container_of
 
 
 __all__ = (
@@ -31,47 +33,55 @@ __all__ = (
 )
 
 
-def list_empty(head):
+def list_empty(head: Object) -> bool:
     """
-    .. c:function:: bool list_empty(struct list_head *head)
-
     Return whether a list is empty.
+
+    :param head: ``struct list_head *``
     """
     head = head.read_()
     return head.next == head
 
 
-def list_is_singular(head):
+def list_is_singular(head: Object) -> bool:
     """
-    .. c:function:: bool list_is_singular(struct list_head *head)
-
     Return whether a list has only one element.
+
+    :param head: ``struct list_head *``
     """
     head = head.read_()
     next = head.next
     return next != head and next == head.prev
 
 
-def list_first_entry(head, type, member):
+def list_first_entry(head: Object, type: Union[str, Type], member: str) -> Object:
     """
-    .. c:function:: type *list_first_entry(struct list_head *head, type, member)
-
     Return the first entry in a list.
 
     The list is assumed to be non-empty.
 
     See also :func:`list_first_entry_or_null()`.
+
+    :param head: ``struct list_head *``
+    :param type: Entry type.
+    :param member: Name of list node member in entry type.
+    :return: ``type *``
     """
     return container_of(head.next, type, member)
 
 
-def list_first_entry_or_null(head, type, member):
+def list_first_entry_or_null(
+    head: Object, type: Union[str, Type], member: str
+) -> Object:
     """
-    .. c:function:: type *list_first_entry_or_null(struct list_head *head, type, member)
-
     Return the first entry in a list or ``NULL`` if the list is empty.
 
     See also :func:`list_first_entry()`.
+
+    :param head: ``struct list_head *``
+    :param type: Entry type.
+    :param member: Name of list node member in entry type.
+    :return: ``type *``
     """
     head = head.read_()
     pos = head.next.read_()
@@ -81,41 +91,47 @@ def list_first_entry_or_null(head, type, member):
         return container_of(pos, type, member)
 
 
-def list_last_entry(head, type, member):
+def list_last_entry(head: Object, type: Union[str, Type], member: str) -> Object:
     """
-    .. c:function:: type *list_last_entry(struct list_head *head, type, member)
-
     Return the last entry in a list.
 
     The list is assumed to be non-empty.
+
+    :param head: ``struct list_head *``
+    :param type: Entry type.
+    :param member: Name of list node member in entry type.
+    :return: ``type *``
     """
     return container_of(head.prev, type, member)
 
 
-def list_next_entry(pos, member):
+def list_next_entry(pos: Object, member: str) -> Object:
     """
-    .. c:function:: type *list_next_entry(type *pos, member)
-
     Return the next entry in a list.
+
+    :param pos: ``type*``
+    :param member: Name of list node member in entry type.
+    :return: ``type *``
     """
     return container_of(getattr(pos, member).next, pos.type_.type, member)
 
 
-def list_prev_entry(pos, member):
+def list_prev_entry(pos: Object, member: str) -> Object:
     """
-    .. c:function:: type *list_prev_entry(type *pos, member)
-
     Return the previous entry in a list.
+
+    :param pos: ``type*``
+    :param member: Name of list node member in entry type.
+    :return: ``type *``
     """
     return container_of(getattr(pos, member).prev, pos.type_.type, member)
 
 
-def list_for_each(head):
+def list_for_each(head: Object) -> Iterator[Object]:
     """
-    .. c:function:: list_for_each(struct list_head *head)
-
     Iterate over all of the nodes in a list.
 
+    :param head: ``struct list_head *``
     :return: Iterator of ``struct list_head *`` objects.
     """
     head = head.read_()
@@ -125,12 +141,11 @@ def list_for_each(head):
         pos = pos.next.read_()
 
 
-def list_for_each_reverse(head):
+def list_for_each_reverse(head: Object) -> Iterator[Object]:
     """
-    .. c:function:: list_for_each_reverse(struct list_head *head)
-
     Iterate over all of the nodes in a list in reverse order.
 
+    :param head: ``struct list_head *``
     :return: Iterator of ``struct list_head *`` objects.
     """
     head = head.read_()
@@ -140,47 +155,48 @@ def list_for_each_reverse(head):
         pos = pos.prev.read_()
 
 
-def list_for_each_entry(type, head, member):
+def list_for_each_entry(type: str, head: Object, member: str) -> Iterator[Object]:
     """
-    .. c:function:: list_for_each_entry(type, struct list_head *head, member)
+    Iterate over all of the entries in a list.
 
-    Iterate over all of the entries in a list, given the type of the entry and
-    the ``struct list_head`` member in that type.
-
+    :param type: Entry type.
+    :param head: ``struct list_head *``
+    :param member: Name of list node member in entry type.
     :return: Iterator of ``type *`` objects.
     """
     for pos in list_for_each(head):
         yield container_of(pos, type, member)
 
 
-def list_for_each_entry_reverse(type, head, member):
+def list_for_each_entry_reverse(
+    type: str, head: Object, member: str
+) -> Iterator[Object]:
     """
-    .. c:function:: list_for_each_entry_reverse(type, struct list_head *head, member)
+    Iterate over all of the entries in a list in reverse order.
 
-    Iterate over all of the entries in a list in reverse order, given the type
-    of the entry and the ``struct list_head`` member in that type.
-
+    :param type: Entry type.
+    :param head: ``struct list_head *``
+    :param member: Name of list node member in entry type.
     :return: Iterator of ``type *`` objects.
     """
     for pos in list_for_each_reverse(head):
         yield container_of(pos, type, member)
 
 
-def hlist_empty(head):
+def hlist_empty(head: Object) -> bool:
     """
-    .. c:function:: bool hlist_empty(struct hlist_head *head)
-
     Return whether a hash list is empty.
+
+    :param head: ``struct hlist_head *``
     """
     return not head.first
 
 
-def hlist_for_each(head):
+def hlist_for_each(head: Object) -> Iterator[Object]:
     """
-    .. c:function:: hlist_for_each(struct hlist_head *head)
-
     Iterate over all of the nodes in a hash list.
 
+    :param head: ``struct hlist_head *``
     :return: Iterator of ``struct hlist_node *`` objects.
     """
     pos = head.first.read_()
@@ -189,13 +205,13 @@ def hlist_for_each(head):
         pos = pos.next.read_()
 
 
-def hlist_for_each_entry(type, head, member):
+def hlist_for_each_entry(type: str, head: Object, member: str) -> Iterator[Object]:
     """
-    .. c:function:: hlist_for_each_entry(type, struct hlist_head *head, member)
+    Iterate over all of the entries in a hash list.
 
-    Iterate over all of the entries in a has list, given the type of the entry
-    and the ``struct hlist_node`` member in that type.
-
+    :param type: Entry type.
+    :param head: ``struct hlist_head *``
+    :param member: Name of list node member in entry type.
     :return: Iterator of ``type *`` objects.
     """
     for pos in hlist_for_each(head):

_drgn.pyi
@@ -1721,79 +1721,82 @@ class OutOfBoundsError(Exception):
 
 _with_libkdumpfile: bool
 
-def _linux_helper_read_vm(prog, pgtable, address, size): ...
-def _linux_helper_radix_tree_lookup(root, index):
+def _linux_helper_read_vm(
+    prog: Program, pgtable: Object, address: IntegerLike, size: IntegerLike
+) -> bytes: ...
+def _linux_helper_radix_tree_lookup(root: Object, index: IntegerLike) -> Object:
     """
-    .. c:function:: void *radix_tree_lookup(struct radix_tree_root *root, unsigned long index)
+    Look up the entry at a given index in a radix tree.
 
-    Look up the entry at a given index in a radix tree. If it is not found,
-    this returns a ``NULL`` object.
-    """
-    ...
-
-def _linux_helper_idr_find(idr, id):
-    """
-    .. c:function:: void *idr_find(struct idr *idr, unsigned long id)
-
-    Look up the entry with the given id in an IDR. If it is not found, this
-    returns a ``NULL`` object.
+    :param root: ``struct radix_tree_root *``
+    :param index: Entry index.
+    :return: ``void *`` found entry, or ``NULL`` if not found.
     """
     ...
 
-def _linux_helper_find_pid(ns, pid):
+def _linux_helper_idr_find(idr: Object, id: IntegerLike) -> Object:
     """
-    .. c:function:: struct pid *find_pid(struct pid_namespace *ns, int nr)
+    Look up the entry with the given ID in an IDR.
 
-    Return the ``struct pid *`` for the given PID number in the given
-    namespace. If given a :class:`Program` instead, the initial PID namespace
-    is used.
+    :param idr: ``struct idr *``
+    :param id: Entry ID.
+    :return: ``void *`` found entry, or ``NULL`` if not found.
     """
     ...
 
-def _linux_helper_pid_task(pid, pid_type):
+def _linux_helper_find_pid(
+    prog_or_ns: Union[Program, Object], pid: IntegerLike
+) -> Object:
     """
-    .. c:function:: struct task_struct *pid_task(struct pid *pid, enum pid_type pid_type)
+    Return the ``struct pid *`` for the given PID number.
 
+    :param prog_or_ns: ``struct pid_namespace *`` object, or :class:`Program`
+        to use initial PID namespace.
+    :return: ``struct pid *``
+    """
+    ...
+
+def _linux_helper_pid_task(pid: Object, pid_type: IntegerLike) -> Object:
+    """
     Return the ``struct task_struct *`` containing the given ``struct pid *``
     of the given type.
+
+    :param pid: ``struct pid *``
+    :param pid_type: ``enum pid_type``
+    :return: ``struct task_struct *``
     """
     ...
 
-def _linux_helper_find_task(prog_or_ns, pid):
+def _linux_helper_find_task(
+    prog_or_ns: Union[Program, Object], pid: IntegerLike
+) -> Object:
     """
-    .. c:function:: struct task_struct *find_task(struct pid_namespace *ns, int pid)
+    Return the task with the given PID.
 
-    Return the task with the given PID in the given namespace. If given a
-    :class:`Program` instead, the initial PID namespace is used.
+    :param prog_or_ns: ``struct pid_namespace *`` object, or :class:`Program`
+        to use initial PID namespace.
+    :return: ``struct task_struct *``
     """
     ...
 
-def _linux_helper_task_state_to_char(task):
+def _linux_helper_task_state_to_char(task: Object) -> str:
     """
-    .. c:function char task_state_to_char(struct task_struct *task)
-
     Get the state of the task as a character (e.g., ``'R'`` for running). See
     `ps(1)
     <http://man7.org/linux/man-pages/man1/ps.1.html#PROCESS_STATE_CODES>`_ for
     a description of the process state codes.
 
-    :rtype: str
+    :param task: ``struct task_struct *``
     """
     ...
 
-def _linux_helper_kaslr_offset(prog):
+def _linux_helper_kaslr_offset(prog: Program) -> int:
     """
-    .. c:function:: unsigned long kaslr_offset(void)
-
     Get the kernel address space layout randomization offset (zero if it is
     disabled).
     """
     ...
 
-def _linux_helper_pgtable_l5_enabled(prog):
-    """
-    .. c:function:: bool pgtable_l5_enabled(void)
-
-    Return whether 5-level paging is enabled.
-    """
+def _linux_helper_pgtable_l5_enabled(prog: Program) -> bool:
+    """Return whether 5-level paging is enabled."""
     ...

drgn/helpers/linux/block.py
@@ -10,7 +10,9 @@ Linux block layer, including disks (``struct gendisk``) and partitions
 (``struct hd_struct``).
 """
 
-from drgn import container_of
+from typing import Iterator
+
+from drgn import Object, Program, container_of
 from drgn.helpers import escape_ascii_string
 from drgn.helpers.linux.device import MAJOR, MINOR, MKDEV
 from drgn.helpers.linux.list import list_for_each_entry
@@ -27,34 +29,33 @@ __all__ = (
 )
 
 
-def disk_devt(disk):
+def disk_devt(disk: Object) -> Object:
     """
-    .. c:function:: dev_t disk_devt(struct gendisk *disk)
-
     Get a disk's device number.
+
+    :param disk: ``struct gendisk *``
+    :return: ``dev_t``
     """
-    return MKDEV(disk.major, disk.first_minor)
+    return Object(disk.prog_, "dev_t", MKDEV(disk.major, disk.first_minor))
 
 
-def disk_name(disk):
+def disk_name(disk: Object) -> bytes:
     """
-    .. c:function:: char *disk_name(struct gendisk *disk)
-
     Get the name of a disk (e.g., ``sda``).
 
-    :rtype: bytes
+    :param disk: ``struct gendisk *``
     """
     return disk.disk_name.string_()
 
 
-def _for_each_block_device(prog):
+def _for_each_block_device(prog: Program) -> Iterator[Object]:
     try:
         class_in_private = prog.cache["knode_class_in_device_private"]
     except KeyError:
         # We need a proper has_member(), but this is fine for now.
         class_in_private = any(
             member.name == "knode_class"
-            for member in prog.type("struct device_private").members
+            for member in prog.type("struct device_private").members  # type: ignore[union-attr]
         )
         prog.cache["knode_class_in_device_private"] = class_in_private
     devices = prog["block_class"].p.klist_devices.k_list.address_of_()
@@ -67,7 +68,7 @@ def _for_each_block_device(prog):
         yield from list_for_each_entry("struct device", devices, "knode_class.n_node")
 
 
-def for_each_disk(prog):
+def for_each_disk(prog: Program) -> Iterator[Object]:
     """
     Iterate over all disks in the system.
 
@@ -79,7 +80,7 @@ def for_each_disk(prog):
             yield container_of(device, "struct gendisk", "part0.__dev")
 
 
-def print_disks(prog):
+def print_disks(prog: Program) -> None:
     """Print all of the disks in the system."""
     for disk in for_each_disk(prog):
         major = disk.major.value_()
@@ -88,27 +89,26 @@ def print_disks(prog):
         print(f"{major}:{minor} {name} ({disk.type_.type_name()})0x{disk.value_():x}")
 
 
-def part_devt(part):
+def part_devt(part: Object) -> Object:
     """
-    .. c:function:: dev_t part_devt(struct hd_struct *part)
-
     Get a partition's device number.
+
+    :param part: ``struct hd_struct *``
+    :return: ``dev_t``
     """
     return part.__dev.devt
 
 
-def part_name(part):
+def part_name(part: Object) -> bytes:
     """
-    .. c:function:: char *part_name(struct hd_struct *part)
-
     Get the name of a partition (e.g., ``sda1``).
 
-    :rtype: bytes
+    :param part: ``struct hd_struct *``
     """
     return part.__dev.kobj.name.string_()
 
 
-def for_each_partition(prog):
+def for_each_partition(prog: Program) -> Iterator[Object]:
     """
     Iterate over all partitions in the system.
 
@@ -118,7 +118,7 @@ def for_each_partition(prog):
         yield container_of(device, "struct hd_struct", "__dev")
 
 
-def print_partitions(prog):
+def print_partitions(prog: Program) -> None:
     """Print all of the partitions in the system."""
     for part in for_each_partition(prog):
         devt = part_devt(part).value_()

drgn/helpers/linux/bpf.py
@@ -12,8 +12,9 @@ etc.
 
 
 import itertools
+from typing import Iterator
 
-from drgn import cast
+from drgn import IntegerLike, Object, Program, cast
 from drgn.helpers.linux.idr import idr_for_each
 from drgn.helpers.linux.list import list_for_each_entry
 
@@ -26,11 +27,9 @@ __all__ = (
 )
 
 
-def bpf_map_for_each(prog):
+def bpf_map_for_each(prog: Program) -> Iterator[Object]:
     """
-    .. c:function:: bpf_map_for_each(prog)
-
-    Iterate over all bpf maps.
+    Iterate over all BPF maps.
 
     :return: Iterator of ``struct bpf_map *`` objects.
     """
@@ -38,11 +37,9 @@ def bpf_map_for_each(prog):
         yield cast("struct bpf_map *", entry)
 
 
-def bpf_prog_for_each(prog):
+def bpf_prog_for_each(prog: Program) -> Iterator[Object]:
     """
-    .. c:function:: bpf_prog_for_each(prog)
-
-    Iterate over all bpf programs.
+    Iterate over all BPF programs.
 
     :return: Iterator of ``struct bpf_prog *`` objects.
     """
@@ -50,13 +47,15 @@ def bpf_prog_for_each(prog):
         yield cast("struct bpf_prog *", entry)
 
 
-def cgroup_bpf_prog_for_each(cgrp, bpf_attach_type):
+def cgroup_bpf_prog_for_each(
+    cgrp: Object, bpf_attach_type: IntegerLike
+) -> Iterator[Object]:
     """
-    .. c:function:: cgroup_bpf_prog_for_each(struct cgroup *cgrp, int bpf_attach_type)
-
-    Iterate over all cgroup bpf programs of the given attach type attached to
+    Iterate over all cgroup BPF programs of the given attach type attached to
     the given cgroup.
 
+    :param cgrp: ``struct cgroup *``
+    :param bpf_attach_type: ``enum bpf_attach_type``
     :return: Iterator of ``struct bpf_prog *`` objects.
     """
     progs_head = cgrp.bpf.progs[bpf_attach_type]
@@ -66,13 +65,15 @@ def cgroup_bpf_prog_for_each(cgrp, bpf_attach_type):
         yield pl.prog
 
 
-def cgroup_bpf_prog_for_each_effective(cgrp, bpf_attach_type):
+def cgroup_bpf_prog_for_each_effective(
+    cgrp: Object, bpf_attach_type: IntegerLike
+) -> Iterator[Object]:
     """
-    .. c:function:: cgroup_bpf_prog_for_each(struct cgroup *cgrp, int bpf_attach_type)
-
-    Iterate over all effective cgroup bpf programs of the given attach type for
+    Iterate over all effective cgroup BPF programs of the given attach type for
     the given cgroup.
 
+    :param cgrp: ``struct cgroup *``
+    :param bpf_attach_type: ``enum bpf_attach_type``
     :return: Iterator of ``struct bpf_prog *`` objects.
     """
     prog_array_items = cgrp.bpf.effective[bpf_attach_type].items

drgn/helpers/linux/cgroup.py
@@ -10,8 +10,9 @@ cgroup interface in :linux:`include/linux/cgroup.h`. Only cgroup v2 is
 supported.
 """
 
+from typing import Callable, Iterator
 
-from drgn import NULL, cast, container_of
+from drgn import NULL, Object, cast, container_of
 from drgn.helpers.linux.kernfs import kernfs_name, kernfs_path
 from drgn.helpers.linux.list import list_for_each_entry
 
@@ -27,22 +28,24 @@ __all__ = (
 )
 
 
-def sock_cgroup_ptr(skcd):
+def sock_cgroup_ptr(skcd: Object) -> Object:
     """
-    .. c:function:: struct cgroup *sock_cgroup_ptr(struct sock_cgroup_data *skcd)
-
     Get the cgroup for a socket from the given ``struct sock_cgroup_data *``
     (usually from ``struct sock::sk_cgrp_data``).
+
+    :param skcd: ``struct sock_cgroup_data *``
+    :return: ``struct cgroup *``
     """
     return cast("struct cgroup *", skcd.val)
 
 
-def cgroup_parent(cgrp):
+def cgroup_parent(cgrp: Object) -> Object:
     """
-    .. c:function:: struct cgroup *cgroup_parent(struct cgroup *cgrp)
-
     Return the parent cgroup of the given cgroup if it exists, ``NULL``
     otherwise.
+
+    :param cgrp: ``struct cgroup *``
+    :return: ``struct cgroup *``
     """
     parent_css = cgrp.self.parent
     if parent_css:
@@ -50,34 +53,32 @@ def cgroup_parent(cgrp):
     return NULL(cgrp.prog_, "struct cgroup *")
 
 
-def cgroup_name(cgrp):
+def cgroup_name(cgrp: Object) -> bytes:
     """
-    .. c:function:: char *cgroup_name(struct cgroup *cgrp)
-
     Get the name of the given cgroup.
 
-    :rtype: bytes
+    :param cgrp: ``struct cgroup *``
     """
     return kernfs_name(cgrp.kn)
 
 
-def cgroup_path(cgrp):
+def cgroup_path(cgrp: Object) -> bytes:
     """
-    .. c:function:: char *cgroup_path(struct cgroup *cgrp)
-
     Get the full path of the given cgroup.
 
-    :rtype: bytes
+    :param cgrp: ``struct cgroup *``
     """
     return kernfs_path(cgrp.kn)
 
 
-def css_next_child(pos, parent):
+def css_next_child(pos: Object, parent: Object) -> Object:
     """
-    .. c:function:: struct cgroup_subsys_state *css_next_child(struct cgroup_subsys_state *pos, struct cgroup_subsys_state *parent)
-
     Get the next child (or ``NULL`` if there is none) of the given parent
     starting from the given position (``NULL`` to initiate traversal).
+
+    :param pos: ``struct cgroup_subsys_state *``
+    :param parent: ``struct cgroup_subsys_state *``
+    :return: ``struct cgroup_subsys_state *``
     """
     if not pos:
         next_ = container_of(
@@ -98,13 +99,15 @@ def css_next_child(pos, parent):
     return NULL(next_.prog_, "struct cgroup_subsys_state *")
 
 
-def css_next_descendant_pre(pos, root):
+def css_next_descendant_pre(pos: Object, root: Object) -> Object:
     """
-    .. c:function:: struct cgroup_subsys_state *css_next_descendant_pre(struct cgroup_subsys_state *pos, struct cgroup_subsys_state *root)
-
     Get the next pre-order descendant (or ``NULL`` if there is none) of the
     given css root starting from the given position (``NULL`` to initiate
     traversal).
+
+    :param pos: ``struct cgroup_subsys_state *``
+    :param root: ``struct cgroup_subsys_state *``
+    :return: ``struct cgroup_subsys_state *``
     """
     # If first iteration, visit root.
     if not pos:
@@ -126,7 +129,9 @@ def css_next_descendant_pre(pos, root):
     return NULL(root.prog_, "struct cgroup_subsys_state *")
 
 
-def _css_for_each_impl(next_fn, css):
+def _css_for_each_impl(
+    next_fn: Callable[[Object, Object], Object], css: Object
+) -> Iterator[Object]:
     pos = NULL(css.prog_, "struct cgroup_subsys_state *")
     while True:
         pos = next_fn(pos, css)
@@ -136,23 +141,21 @@ def _css_for_each_impl(next_fn, css):
             yield pos
 
 
-def css_for_each_child(css):
+def css_for_each_child(css: Object) -> Iterator[Object]:
     """
-    .. c:function:: css_for_each_child(struct cgroup_subsys_state *css)
-
     Iterate through children of the given css.
 
+    :param css: ``struct cgroup_subsys_state *``
     :return: Iterator of ``struct cgroup_subsys_state *`` objects.
     """
     return _css_for_each_impl(css_next_child, css)
 
 
-def css_for_each_descendant_pre(css):
+def css_for_each_descendant_pre(css: Object) -> Iterator[Object]:
     """
-    .. c:function:: css_for_each_descendant_pre(struct cgroup_subsys_state *css)
-
     Iterate through the given css's descendants in pre-order.
 
+    :param css: ``struct cgroup_subsys_state *``
     :return: Iterator of ``struct cgroup_subsys_state *`` objects.
     """
     return _css_for_each_impl(css_next_descendant_pre, css)

drgn/helpers/linux/cpumask.py
@@ -9,6 +9,10 @@ The ``drgn.helpers.linux.cpumask`` module provides helpers for working with CPU
 masks from :linux:`include/linux/cpumask.h`.
 """
 
+from typing import Iterator
+
+from drgn import Object, Program, sizeof
+
 __all__ = (
     "for_each_cpu",
     "for_each_online_cpu",
@@ -17,45 +21,31 @@ __all__ = (
 )
 
 
-def for_each_cpu(mask):
+def for_each_cpu(mask: Object) -> Iterator[int]:
     """
-    .. c:function:: for_each_cpu(struct cpumask mask)
-
     Iterate over all of the CPUs in the given mask.
 
-    :rtype: Iterator[int]
+    :param mask: ``struct cpumask``
     """
     bits = mask.bits
-    word_bits = 8 * bits.type_.type.size
-    for i in range(bits.type_.length):
+    word_bits = 8 * sizeof(bits.type_.type)
+    for i in range(bits.type_.length):  # type: ignore
         word = bits[i].value_()
         for j in range(word_bits):
             if word & (1 << j):
                 yield (word_bits * i) + j
 
 
-def for_each_possible_cpu(prog):
-    """
-    Iterate over all possible CPUs.
-
-    :rtype: Iterator[int]
-    """
+def for_each_possible_cpu(prog: Program) -> Iterator[int]:
+    """Iterate over all possible CPUs."""
     return for_each_cpu(prog["__cpu_possible_mask"])
 
 
-def for_each_online_cpu(prog):
-    """
-    Iterate over all online CPUs.
-
-    :rtype: Iterator[int]
-    """
+def for_each_online_cpu(prog: Program) -> Iterator[int]:
+    """Iterate over all online CPUs."""
     return for_each_cpu(prog["__cpu_online_mask"])
 
 
-def for_each_present_cpu(prog):
-    """
-    Iterate over all present CPUs.
-
-    :rtype: Iterator[int]
-    """
+def for_each_present_cpu(prog: Program) -> Iterator[int]:
+    """Iterate over all present CPUs."""
     return for_each_cpu(prog["__cpu_present_mask"])

drgn/helpers/linux/device.py
@@ -9,7 +9,9 @@ The ``drgn.helpers.linux.device`` module provides helpers for working with
 Linux devices, including the kernel encoding of ``dev_t``.
 """
 
-from drgn import Object, cast
+import operator
+
+from drgn import IntegerLike
 
 __all__ = (
     "MAJOR",
@@ -23,37 +25,29 @@ _MINORBITS = 20
 _MINORMASK = (1 << _MINORBITS) - 1
 
 
-def MAJOR(dev):
+def MAJOR(dev: IntegerLike) -> int:
     """
-    .. c:function:: unsigned int MAJOR(dev_t dev)
-
     Return the major ID of a kernel ``dev_t``.
+
+    :param dev: ``dev_t`` object or :class:``int``.
     """
-    major = dev >> _MINORBITS
-    if isinstance(major, Object):
-        return cast("unsigned int", major)
-    return major
+    return operator.index(dev) >> _MINORBITS
 
 
-def MINOR(dev):
+def MINOR(dev: IntegerLike) -> int:
     """
-    .. c:function:: unsigned int MINOR(dev_t dev)
-
     Return the minor ID of a kernel ``dev_t``.
+
+    :param dev: ``dev_t`` object or :class:``int``.
     """
-    minor = dev & _MINORMASK
-    if isinstance(minor, Object):
-        return cast("unsigned int", minor)
-    return minor
+    return operator.index(dev) & _MINORMASK
 
 
-def MKDEV(major, minor):
+def MKDEV(major: IntegerLike, minor: IntegerLike) -> int:
     """
-    .. c:function:: dev_t MKDEV(unsigned int major, unsigned int minor)
-
     Return a kernel ``dev_t`` from the major and minor IDs.
+
+    :param major: Device major ID.
+    :param minor: Device minor ID.
     """
-    dev = (major << _MINORBITS) | minor
-    if isinstance(dev, Object):
-        return cast("dev_t", dev)
-    return dev
+    return (operator.index(major) << _MINORBITS) | operator.index(minor)

commit 47fbfbf65d4b1db35eb352e5e4e08d7442a00268

drgn.helpers.linux.{,l}list: fix docstring type formatting in *_{next,prev}_entry()
Fixes: 0cf3320a89b3 ("Add type annotations to helpers")
Fixes: 585bc6a3be4a ("Add helpers for lockless single lists (llist).")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/list.py
@@ -123,7 +123,7 @@ def list_next_entry(pos: Object, member: str) -> Object:
     """
     Return the next entry in a list.
 
-    :param pos: ``type*``
+    :param pos: ``type *``
     :param member: Name of list node member in entry type.
     :return: ``type *``
     """
@@ -134,7 +134,7 @@ def list_prev_entry(pos: Object, member: str) -> Object:
     """
     Return the previous entry in a list.
 
-    :param pos: ``type*``
+    :param pos: ``type *``
     :param member: Name of list node member in entry type.
     :return: ``type *``
     """

drgn/helpers/linux/llist.py
@@ -85,7 +85,7 @@ def llist_next_entry(pos: Object, member: str) -> Object:
     """
     Return the next entry in an llist.
 
-    :param pos: ``type*``
+    :param pos: ``type *``
     :param member: Name of ``struct llist_node`` member in entry type.
     :return: ``type *``
     """