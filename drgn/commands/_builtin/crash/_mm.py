c6c59f43dab7455f4c9877f5504e3510a5192b3f
Omar Sandoval <osandov@osandov.com>
2025-12-23 16:43:19 -0800

    crash: move vm command to _mm.py
    Similar to commit 27e033006bc2 ("crash: move ptov command to _mm.py"),
    even once we support all of the options to vm, it still won't really
    warrant its own module.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- drgn/commands/_builtin/crash/_mm.py
+++ drgn/commands/_builtin/crash/_mm.py
@@ -9,9 +9,16 @@ from typing import Any, List, Sequence
 
 from drgn import Object, Program
 from drgn.commands import argument, drgn_argument
-from drgn.commands.crash import CrashDrgnCodeBuilder, crash_command, parse_cpuspec
+from drgn.commands.crash import (
+    CrashDrgnCodeBuilder,
+    _crash_foreach_subcommand,
+    _TaskSelector,
+    crash_command,
+    parse_cpuspec,
+    print_task_header,
+)
 from drgn.helpers.common.format import CellFormat, escape_ascii_string, print_table
-from drgn.helpers.linux.mm import phys_to_virt
+from drgn.helpers.linux.mm import for_each_vma, phys_to_virt, task_rss, vma_name
 from drgn.helpers.linux.percpu import per_cpu_ptr
 from drgn.helpers.linux.swap import (
     for_each_swap_info,
@@ -254,3 +261,118 @@ for si in for_each_swap_info():
             ),
         )
     print_table(rows)
+
+
+@_crash_foreach_subcommand(
+    arguments=(drgn_argument,),
+)
+def _crash_foreach_vm(task_selector: _TaskSelector, args: argparse.Namespace) -> None:
+    prog = task_selector.prog
+
+    if args.drgn:
+        code = CrashDrgnCodeBuilder(prog)
+        with task_selector.begin_task_loop(code):
+            code.append_task_header()
+            code.add_from_import(
+                "drgn.helpers.linux.mm", "for_each_vma", "task_rss", "vma_name"
+            )
+            code.append(
+                """\
+
+mm = task.mm.read_()
+if mm:
+    pgd = mm.pgd
+    rss = task_rss(task)
+    total_vm = mm.total_vm
+
+    for vma in for_each_vma(mm):
+        start = vma.vm_start
+        end = vma.vm_end
+        flags = vma.vm_flags
+        file = vma_name(vma)
+"""
+            )
+        return code.print()
+
+    first = True
+    for task in task_selector.tasks():
+        if first:
+            first = False
+        else:
+            print()
+        print_task_header(task)
+
+        mm = task.mm.read_()
+        if mm:
+            pgd_value = mm.pgd.value_()
+            rss_total = task_rss(task).total
+            total_vm = mm.total_vm.value_()
+        else:
+            pgd_value = rss_total = total_vm = 0
+        page_size = prog["PAGE_SIZE"].value_()
+        print_table(
+            (
+                (
+                    CellFormat("MM", "^"),
+                    CellFormat("PGD", "^"),
+                    CellFormat("RSS", "^"),
+                    CellFormat("TOTAL_VM", "^"),
+                ),
+                (
+                    CellFormat(mm.value_(), "^x"),
+                    CellFormat(pgd_value, "^x"),
+                    CellFormat(f"{rss_total * page_size // 1024}k", "^"),
+                    CellFormat(f"{total_vm * page_size // 1024}k", "^"),
+                ),
+            )
+        )
+        if not mm:
+            return
+
+        rows: List[Sequence[Any]] = [
+            (
+                CellFormat("VMA", "^"),
+                CellFormat("START", "^"),
+                CellFormat("END", "^"),
+                CellFormat("FLAGS", "<"),
+                CellFormat("FILE", "<"),
+            )
+        ]
+        for vma in for_each_vma(mm):
+            rows.append(
+                (
+                    CellFormat(vma.value_(), "^x"),
+                    CellFormat(vma.vm_start.value_(), "^x"),
+                    CellFormat(vma.vm_end.value_(), "^x"),
+                    CellFormat(vma.vm_flags.value_(), "<x"),
+                    vma_name(vma),
+                )
+            )
+        print_table(rows)
+
+
+@crash_command(
+    description="virtual memory",
+    long_description="""This command displays basic virtual memory information of a context,
+consisting of a pointer to its mm_struct and page directory, its RSS and
+total virtual memory size; and a list of pointers to each vm_area_struct,
+its starting and ending address, vm_flags value, and file pathname. If no
+arguments are entered, the current context is used.
+""",
+    arguments=(
+        argument(
+            "tasks",
+            metavar="pid|task",
+            nargs="*",
+            type="pid_or_task",
+            help="one or more process PIDs or hexadecimal task_struct pointers",
+        ),
+        drgn_argument,
+    ),
+)
+def _crash_cmd_vm(
+    prog: Program, name: str, args: argparse.Namespace, **kwargs: Any
+) -> None:
+    if not args.tasks:
+        args.tasks.append(None)
+    return _crash_foreach_vm(_TaskSelector(prog, args.tasks), args)
--- tests/linux_kernel/crash_commands/test_mm.py
+++ tests/linux_kernel/crash_commands/test_mm.py
@@ -8,7 +8,7 @@ import re
 
 from drgn import Object
 from drgn.helpers.linux.cpumask import for_each_online_cpu
-from drgn.helpers.linux.mm import phys_to_virt
+from drgn.helpers.linux.mm import TaskRss, phys_to_virt
 from drgn.helpers.linux.percpu import per_cpu_ptr
 from tests.linux_kernel import skip_unless_have_test_disk
 from tests.linux_kernel.crash_commands import CrashCommandTestCase
@@ -136,3 +136,65 @@ class TestSwap(CrashCommandTestCase):
         self.assertIn("used_pages", cmd.drgn_option.globals)
         self.assertIn("priority", cmd.drgn_option.globals)
         self.assertIsInstance(cmd.drgn_option.globals["path"], bytes)
+
+
+class TestVm(CrashCommandTestCase):
+    def _test_common(self, cmd):
+        self.assertRegex(
+            cmd.stdout,
+            r"\bMM\s+PGD\s+RSS\s+TOTAL_VM(?:\s+[0-9a-f]+){2}(?:\s+[0-9]+k){2}\b",
+        )
+        self.assertRegex(
+            cmd.stdout, r"\bVMA\s+START\s+END\s+FLAGS\s+FILE(?:\s+[0-9a-f]+){4}"
+        )
+        self.assertRegex(cmd.stdout, r"\bVMA(?s:.)*/")  # Check for any file path.
+
+    def _test_drgn_option_common(self, cmd):
+        for variable in (
+            "pid",
+            "task",
+            "command",
+            "mm",
+            "pgd",
+            "vma",
+            "start",
+            "end",
+            "flags",
+        ):
+            with self.subTest(variable=variable):
+                self.assertIsInstance(cmd.drgn_option.globals[variable], Object)
+        self.assertIsInstance(cmd.drgn_option.globals["cpu"], int)
+        self.assertIsInstance(cmd.drgn_option.globals["rss"], TaskRss)
+        self.assertIsInstance(cmd.drgn_option.globals["file"], str)
+
+    def test_no_args(self):
+        self.run_crash_command("set -p")
+
+        cmd = self.check_crash_command("vm")
+        self.assertIn(f"PID: {os.getpid()}", cmd.stdout)
+        self._test_common(cmd)
+
+        self._test_drgn_option_common(cmd)
+
+    def test_tasks(self):
+        cmd = self.check_crash_command(f"vm 1 {os.getpid()}")
+        foreach_cmd = self.check_crash_command(
+            f"foreach 1 {os.getpid()} vm", mode="capture"
+        )
+
+        for c in (cmd, foreach_cmd):
+            self.assertIn("PID: 1", c.stdout)
+            self.assertIn(f"PID: {os.getpid()}", c.stdout)
+            self._test_common(c)
+
+        self._test_drgn_option_common(cmd)
+
+        self.assertEqual(cmd.drgn_option.stdout, foreach_cmd.drgn_option.stdout)
+
+    def test_kernel_thread(self):
+        cmd = self.check_crash_command("vm 2")
+
+        self.assertRegex(cmd.stdout, r"\bMM\s+PGD\s+RSS\s+TOTAL_VM\s+0\s+0\s+0k\s+0k\b")
+        self.assertNotIn("VMA", cmd.stdout)
+
+        self.assertFalse(cmd.drgn_option.globals["mm"])
