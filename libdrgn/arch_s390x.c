osandov/drgn

commit 3aba57b6daaba2609d016d030bbf0ae0a8707c9e

libdrgn: platform: add alignment requirements to struct drgn_architecture_info
In preparation for the next change implementing alignof(), we need to
know how base types are aligned. Add this information to struct
drgn_architecture_info. I populated this using a special C file that
doesn't compile but produces a compiler diagnostic which includes the
alignment requirements.

There may be exotic types (e.g., _BitInt, __m64, etc.) that these don't
apply to, but we'll deal with that another time.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/arch_s390x.c
@@ -490,6 +490,7 @@ const struct drgn_architecture_info arch_info_s390 = {
 	.name = "s390",
 	.arch = DRGN_ARCH_S390,
 	.default_flags = 0,
+	.scalar_alignment = { 1, 2, 4, 8, 8 },
 	.register_by_name = drgn_register_by_name_unknown,
 	.apply_elf_reloc = apply_elf_reloc_s390,
 };

libdrgn/arch_aarch64.c
@@ -464,6 +464,7 @@ const struct drgn_architecture_info arch_info_aarch64 = {
 	.arch = DRGN_ARCH_AARCH64,
 	.default_flags = (DRGN_PLATFORM_IS_64_BIT |
 			  DRGN_PLATFORM_IS_LITTLE_ENDIAN),
+	.scalar_alignment = { 1, 2, 4, 8, 16 },
 	DRGN_ARCHITECTURE_REGISTERS,
 	.default_dwarf_cfi_row = &default_dwarf_cfi_row_aarch64,
 	.demangle_cfi_registers = demangle_cfi_registers_aarch64,

libdrgn/arch_arm.c
@@ -35,6 +35,7 @@ const struct drgn_architecture_info arch_info_arm = {
 	.name = "Arm",
 	.arch = DRGN_ARCH_ARM,
 	.default_flags = DRGN_PLATFORM_IS_LITTLE_ENDIAN,
+	.scalar_alignment = { 1, 2, 4, 8, 8 },
 	.register_by_name = drgn_register_by_name_unknown,
 	.apply_elf_reloc = apply_elf_reloc_arm,
 };

libdrgn/arch_i386.c
@@ -29,6 +29,7 @@ const struct drgn_architecture_info arch_info_i386 = {
 	.name = "i386",
 	.arch = DRGN_ARCH_I386,
 	.default_flags = DRGN_PLATFORM_IS_LITTLE_ENDIAN,
+	.scalar_alignment = { 1, 2, 4, 4, 16 },
 	.register_by_name = drgn_register_by_name_unknown,
 	.apply_elf_reloc = apply_elf_reloc_i386,
 };

libdrgn/arch_ppc64.c
@@ -456,6 +456,7 @@ const struct drgn_architecture_info arch_info_ppc64 = {
 	.arch = DRGN_ARCH_PPC64,
 	.default_flags = (DRGN_PLATFORM_IS_64_BIT |
 			  DRGN_PLATFORM_IS_LITTLE_ENDIAN),
+	.scalar_alignment = { 1, 2, 4, 8, 16 },
 	DRGN_ARCHITECTURE_REGISTERS,
 	.default_dwarf_cfi_row = &default_dwarf_cfi_row_ppc64,
 	.fallback_unwind = fallback_unwind_ppc64,

libdrgn/arch_riscv.c
@@ -115,6 +115,7 @@ const struct drgn_architecture_info arch_info_riscv64 = {
 	.arch = DRGN_ARCH_RISCV64,
 	.default_flags = (DRGN_PLATFORM_IS_64_BIT |
 			  DRGN_PLATFORM_IS_LITTLE_ENDIAN),
+	.scalar_alignment = { 1, 2, 4, 8, 16 },
 	.register_by_name = drgn_register_by_name_unknown,
 	.apply_elf_reloc = apply_elf_reloc_riscv,
 };
@@ -122,6 +123,7 @@ const struct drgn_architecture_info arch_info_riscv64 = {
 const struct drgn_architecture_info arch_info_riscv32 = {
 	.name = "RISC-V 32",
 	.arch = DRGN_ARCH_RISCV32,
+	.scalar_alignment = { 1, 2, 4, 8, 16 },
 	.default_flags = DRGN_PLATFORM_IS_LITTLE_ENDIAN,
 	.register_by_name = drgn_register_by_name_unknown,
 	.apply_elf_reloc = apply_elf_reloc_riscv,

libdrgn/arch_x86_64.c
@@ -674,6 +674,7 @@ const struct drgn_architecture_info arch_info_x86_64 = {
 	.arch = DRGN_ARCH_X86_64,
 	.default_flags = (DRGN_PLATFORM_IS_64_BIT |
 			  DRGN_PLATFORM_IS_LITTLE_ENDIAN),
+	.scalar_alignment = { 1, 2, 4, 8, 16 },
 	DRGN_ARCHITECTURE_REGISTERS,
 	.default_dwarf_cfi_row = &default_dwarf_cfi_row_x86_64,
 	.orc_to_cfi = orc_to_cfi_x86_64,

libdrgn/platform.h
@@ -163,6 +163,7 @@ typedef struct drgn_error *
  *     - @ref name
  *     - @ref arch
  *     - @ref default_flags
+ *     - @ref scalar_alignment
  *     - @ref register_by_name
  * - Add an `extern` declaration of `arch_info_foo` to `libdrgn/platform.h`.
  * - Handle the architecture in @ref drgn_platform_from_kdump(), @ref
@@ -250,6 +251,19 @@ struct drgn_architecture_info {
 	 * elsewhere (like from an ELF file).
 	 */
 	enum drgn_platform_flags default_flags;
+	/**
+	 * Default alignment of scalar types by size.
+	 *
+	 * `scalar_alignment[i]` is the default alignment of a scalar type with
+	 * 2<sup>i</sup> &le; `sizeof(type)` < 2<sup>i+1</sup>.
+	 *
+	 * This may not be enough to get the correct alignment of some
+	 * implementation-defined extended types, but for now it's good enough.
+	 *
+	 * You can generate this for a new architecture using
+	 * `scripts/scalar_alignment.c`.
+	 */
+	unsigned char scalar_alignment[5];
 	/**
 	 * Registers visible to the public API.
 	 *

commit 8f7931b8c6090456f53762c859318b81d6b68046

libdrgn: s390x: add missing scalar_alignment definition
When I originally added these, I mistakenly only added them for 32-bit
s390, not 64-bit s390x. They appear to be the same (as determined by
`clang --target=systemz scripts/scalar_alignment.c` and
`clang -m32 --target=systemz scripts/scalar_alignment.c`).

Fixes: 3aba57b6daab ("libdrgn: platform: add alignment requirements to struct drgn_architecture_info")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/arch_s390x.c
@@ -482,6 +482,7 @@ const struct drgn_architecture_info arch_info_s390x = {
 	.name = "s390x",
 	.arch = DRGN_ARCH_S390X,
 	.default_flags = DRGN_PLATFORM_IS_64_BIT,
+	.scalar_alignment = { 1, 2, 4, 8, 8 },
 	DRGN_ARCHITECTURE_REGISTERS,
 	.default_dwarf_cfi_row = &default_dwarf_cfi_row_s390x,
 	.fallback_unwind = fallback_unwind_s390x,