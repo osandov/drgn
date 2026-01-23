osandov/drgn

commit e22490a8412c3001aaa0101d18f0d868e14888bf

drgn.helpers.linux.completion: Add helpers for completion variables.
These can be used to find state or waiters of a completion variable.

Signed-off-by: Imran Khan <imran.f.khan@oracle.com>

drgn/helpers/linux/completion.py
@@ -1,0 +1,53 @@
+# Copyright (c) 2026, Oracle and/or its affiliates.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+Completion variables
+------------------
+
+The ``drgn.helpers.linux.completion`` module provides helpers for working with
+completion variables (``struct completion``) from :linux:`include/linux/completion.h`.
+
+"""
+
+from typing import Iterator
+
+from drgn import Object
+from drgn.helpers.linux.swait import swait_for_each_task
+from drgn.helpers.linux.wait import waitqueue_for_each_task
+
+__all__ = (
+    "completion_done",
+    "completion_for_each_task",
+)
+
+
+def completion_done(completion: Object) -> bool:
+    """
+    Test if a completion has any waiters.
+
+    :param completion: ``struct completion *``
+    """
+    return bool(completion.done)
+
+
+def completion_for_each_task(completion: Object) -> Iterator[Object]:
+    """
+    Iterate over all tasks waiting on a completion variable.
+
+    :param completion: ``struct completion *``
+    :return: Iterator of ``struct task_struct *`` objects.
+    """
+    wait = completion.wait.address_of_()
+    # completion->wait is a simple wait queue since Linux kernel commit
+    # a5c6234e1028 ("completion: Use simple wait queues") (in v5.7).
+    # Also Linux kernel commit 2055da97389a ("sched/wait: Disambiguate
+    # wq_entry->task_list and wq_head->task_list naming") (in v4.13) renamed
+    # the task_list member to head.
+    # So completion->wait in kernels v5.7 and later and in kernels prior to v4.13,
+    # have task_list member, but of different types. So use type of completion->wait
+    # to differentiate between completion backends.
+    if wait.type_.type_name() == "struct swait_queue_head *":
+        return swait_for_each_task(wait)
+    else:
+        return waitqueue_for_each_task(wait)

commit 7c2b291459899ce47d9b98452417bdc3f39b53b8

drgn.helpers.linux.completion: fix Sphinx underline warning
Sphinx warns that the title underline is too short. Also clean up the
capitalization and line wrapping.

Fixes: e22490a8412c ("drgn.helpers.linux.completion: Add helpers for completion variables.")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/completion.py
@@ -2,12 +2,12 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 """
-Completion variables
-------------------
+Completion Variables
+--------------------
 
 The ``drgn.helpers.linux.completion`` module provides helpers for working with
-completion variables (``struct completion``) from :linux:`include/linux/completion.h`.
-
+completion variables (``struct completion``) from
+:linux:`include/linux/completion.h`.
 """
 
 from typing import Iterator