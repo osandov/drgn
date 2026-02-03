osandov/drgn

commit fc8550f67d11794a1e226b5be5961d04579c56f5

vmtest.rootfsbuild: cross-compile for foreign architectures when possible
Currently, we build drgn for foreign architectures using user mode
emulation. Especially with parallel vmtest, this ends up being a big
bottleneck. We can avoid this by cross-compiling instead. So that we
don't have to worry about what distribution the host is running and
whether it has the necessary cross-compilers, we build from a compatible
rootfs for the host architecture.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

vmtest/config.py
@@ -271,6 +271,8 @@ class Architecture:
     kernel_srcarch: str
     # Name of the architecture in Debian.
     debian_arch: str
+    # GCC target triplet on Debian.
+    debian_gcc_target: str
     # Linux kernel configuration options.
     kernel_config: str
     # Flavor-specific Linux kernel configuration options.
@@ -292,6 +294,7 @@ ARCHITECTURES = {
             kernel_arch="arm64",
             kernel_srcarch="arm64",
             debian_arch="arm64",
+            debian_gcc_target="aarch64-linux-gnu",
             kernel_config="""
                 CONFIG_PCI_HOST_GENERIC=y
                 CONFIG_RTC_CLASS=y
@@ -319,6 +322,7 @@ ARCHITECTURES = {
             kernel_arch="arm",
             kernel_srcarch="arm",
             debian_arch="armhf",
+            debian_gcc_target="arm-linux-gnueabihf",
             kernel_config="""
                 CONFIG_NR_CPUS=8
                 # Debian armhf userspace assumes EABI and VFP.
@@ -363,6 +367,7 @@ ARCHITECTURES = {
             kernel_arch="powerpc",
             kernel_srcarch="powerpc",
             debian_arch="ppc64el",
+            debian_gcc_target="powerpc64le-linux-gnu",
             kernel_config="""
                 CONFIG_PPC64=y
                 CONFIG_CPU_LITTLE_ENDIAN=y
@@ -386,6 +391,7 @@ ARCHITECTURES = {
             kernel_arch="s390",
             kernel_srcarch="s390",
             debian_arch="s390x",
+            debian_gcc_target="s390x-linux-gnu",
             kernel_config="""
                 # Needed for CONFIG_KEXEC_FILE.
                 CONFIG_CRYPTO_SHA256_S390=y
@@ -400,6 +406,7 @@ ARCHITECTURES = {
             kernel_arch="x86_64",
             kernel_srcarch="x86",
             debian_arch="amd64",
+            debian_gcc_target="x86_64-linux-gnu",
             kernel_config="""
                 CONFIG_RTC_CLASS=y
                 CONFIG_RTC_DRV_CMOS=y

vmtest/rootfsbuild.py
@@ -13,7 +13,7 @@ from vmtest.config import ARCHITECTURES, HOST_ARCHITECTURE, Architecture
 logger = logging.getLogger(__name__)
 
 
-_ROOTFS_PACKAGES = [
+_ROOTFS_PACKAGES = (
     # drgn build dependencies.
     "autoconf",
     "automake",
@@ -42,7 +42,7 @@ _ROOTFS_PACKAGES = [
     "python3-pytest",
     "python3-pytest-subtests",
     "zstd",
-]
+)
 
 
 def build_rootfs(
@@ -54,6 +54,12 @@ def build_rootfs(
     if path.exists():
         logger.info("%s already exists", path)
         return
+
+    packages = list(_ROOTFS_PACKAGES)
+    if arch is HOST_ARCHITECTURE:
+        for other_arch in ARCHITECTURES.values():
+            if other_arch is not arch:
+                packages.append(f"gcc-{other_arch.debian_gcc_target}")
 
     logger.info("creating debootstrap rootfs %s", path)
     path.parent.mkdir(parents=True, exist_ok=True)
@@ -93,7 +99,7 @@ container=lxc debootstrap --variant=minbase --foreign --include="$packages" --ar
                 "sh",
                 arch.debian_arch,
                 tmp_dir / path.name,
-                ",".join(_ROOTFS_PACKAGES),
+                ",".join(packages),
             ]
         )
         (tmp_dir / path.name).rename(path)
@@ -105,7 +111,57 @@ container=lxc debootstrap --variant=minbase --foreign --include="$packages" --ar
         logger.info("created snapshot %s", snapshot_dir)
 
 
-def build_drgn_in_rootfs(rootfs: Path, outfile: Optional[TextIO] = None) -> None:
+def _cross_compile_drgn(
+    target: Architecture, directory: Path, *, outfile: Optional[TextIO] = None
+) -> None:
+    assert HOST_ARCHITECTURE is not None
+    host_rootfs = directory / HOST_ARCHITECTURE.name / "rootfs"
+    target_rootfs = directory / target.name / "rootfs"
+    logger.info("cross-compiling drgn in %s for %s", host_rootfs, target_rootfs)
+    subprocess.check_call(
+        [
+            "unshare",
+            "--map-root-user",
+            "--map-auto",
+            "--mount",
+            "sh",
+            "-c",
+            rf"""
+set -e
+
+mount --bind . "$1/mnt"
+mount --bind "$3" "$1/srv"
+{chroot_sh_cmd('"$1"')} '
+set -e
+
+extension_suffix="$(/srv/usr/bin/python3-config --extension-suffix)"
+build_temp="/mnt/build/temp${{extension_suffix%.so}}"
+mkdir -p "$build_temp"
+cd "$build_temp"
+export CFLAGS="--sysroot=/srv"
+export LDFLAGS="--sysroot=/srv"
+export PYTHON_CPPFLAGS="$(/srv/usr/bin/python3-config --includes)"
+export PKG_CONFIG_LIBDIR="$(pkg-config --variable pc_path pkg-config | sed -r -e "s!(^|:)/!\1/srv/!g" -e "s/$1/$2/g")"
+export PKG_CONFIG_SYSROOT_DIR=/srv
+if [ ! -e Makefile ]; then
+    /mnt/libdrgn/configure --host="$2" --with-sysroot=/srv --disable-static --disable-libdrgn --enable-python-extension
+fi
+make -j"$(nproc)"
+cp -v ".libs/_drgn.so" "/mnt/_drgn$extension_suffix"
+' sh "$2" "$4"
+""",
+            "sh",
+            host_rootfs,
+            HOST_ARCHITECTURE.debian_gcc_target,
+            target_rootfs,
+            target.debian_gcc_target,
+        ],
+        stdout=outfile,
+        stderr=outfile,
+    )
+
+
+def _build_drgn_in_rootfs(rootfs: Path, *, outfile: Optional[TextIO] = None) -> None:
     logger.info("building drgn using %s", rootfs)
     subprocess.check_call(
         [
@@ -127,6 +183,17 @@ mount --bind . "$1/mnt"
         stdout=outfile,
         stderr=outfile,
     )
+
+
+def build_drgn_for_arch(
+    target: Architecture, directory: Path, *, outfile: Optional[TextIO] = None
+) -> None:
+    if target is not HOST_ARCHITECTURE and HOST_ARCHITECTURE is not None:
+        return _cross_compile_drgn(target, directory, outfile=outfile)
+    else:
+        return _build_drgn_in_rootfs(
+            directory / target.name / "rootfs", outfile=outfile
+        )
 
 
 if __name__ == "__main__":
@@ -188,8 +255,17 @@ if __name__ == "__main__":
         else:
             architectures.append(ARCHITECTURES[name])
 
+    # Create the host architecture rootfs first in case it's needed for
+    # building drgn for other architectures.
+    try:
+        index = architectures.index(HOST_ARCHITECTURE)  # type: ignore[arg-type]
+    except ValueError:
+        pass
+    else:
+        architectures.insert(0, architectures.pop(index))
+
     for arch in architectures:
         dir = args.directory / arch.name / "rootfs"
         build_rootfs(arch, dir, btrfs=args.btrfs)
         if args.build_drgn:
-            build_drgn_in_rootfs(dir)
+            build_drgn_for_arch(arch, args.directory)

vmtest/__main__.py
@@ -39,7 +39,7 @@ from vmtest.config import (
     Kernel,
 )
 from vmtest.download import Downloader
-from vmtest.rootfsbuild import build_drgn_in_rootfs
+from vmtest.rootfsbuild import build_drgn_for_arch
 from vmtest.vm import LostVMError, TestKmodMode, run_in_vm
 
 logger = logging.getLogger(__name__)
@@ -318,15 +318,14 @@ class _TestRunner:
                 outfile = exit_stack.enter_context(
                     (self._directory / "log" / f"{arch.name}-build.log").open("w")
                 )
-            rootfs = self._rootfs(arch)
-            if rootfs == Path("/"):
+            if self._use_host_rootfs and arch is HOST_ARCHITECTURE:
                 subprocess.check_call(
                     [sys.executable, "setup.py", "build_ext", "-i"],
                     stdout=outfile,
                     stderr=outfile,
                 )
             else:
-                build_drgn_in_rootfs(rootfs, outfile=outfile)
+                build_drgn_for_arch(arch, self._directory, outfile=outfile)
         return functools.partial(self._drgn_build_done, arch)
 
     def _drgn_build_done(self, arch: Architecture) -> bool:

commit b0057dbd2069314d8d82332db5b6c5151270ee88

vmtest.rootfsbuild: run autoreconf outside of chroot
If make inside of the chroot automatically runs build-aux/missing, and
the autoconf version inside of the chroot is different than that of the
host, then it fails with an error like:

  /mnt/libdrgn/build-aux/missing: line 81: aclocal-1.18: command not found

Fix it by running autoreconf explicitly outside of the chroot. (This
worked before the cross-compiling change because setup.py would run
autoreconf explicitly.)

vmtest.config does not make much sense for _run_autoreconf(), but the
eventual migration to Meson will make it go away.

Fixes: fc8550f67d11 ("vmtest.rootfsbuild: cross-compile for foreign architectures when possible")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

setup.py
@@ -33,8 +33,8 @@ try:
 except ImportError:
     from distutils.errors import DistutilsError as BaseError
 
-from util import nproc, out_of_date
-from vmtest.config import KERNEL_FLAVORS, SUPPORTED_KERNEL_VERSIONS
+from util import nproc
+from vmtest.config import KERNEL_FLAVORS, SUPPORTED_KERNEL_VERSIONS, _run_autoreconf
 
 logger = logging.getLogger(__name__)
 
@@ -72,19 +72,6 @@ class build_ext(_build_ext):
 
     help_options = []
 
-    def _run_autoreconf(self):
-        if out_of_date(
-            "libdrgn/Makefile.in", "libdrgn/Makefile.am", "libdrgn/configure.ac"
-        ) or out_of_date("libdrgn/configure", "libdrgn/configure.ac"):
-            try:
-                subprocess.check_call(["autoreconf", "-i", "libdrgn"])
-            except Exception:
-                with contextlib.suppress(FileNotFoundError):
-                    os.remove("libdrgn/configure")
-                with contextlib.suppress(FileNotFoundError):
-                    os.remove("libdrgn/Makefile.in")
-                raise
-
     def _run_configure(self):
         self.mkpath(self.build_temp)
         makefile = os.path.join(self.build_temp, "Makefile")
@@ -118,7 +105,7 @@ class build_ext(_build_ext):
         subprocess.check_call(args)
 
     def make(self, *make_args):
-        self._run_autoreconf()
+        _run_autoreconf()
         self._run_configure()
         self._run_make(*make_args)
 

vmtest/config.py
@@ -6,9 +6,11 @@ import dataclasses
 import inspect
 import os
 from pathlib import Path
+import subprocess
 from typing import Dict, Mapping, NamedTuple, Sequence
 
 from _drgn_util.platform import NORMALIZED_MACHINE_NAME
+from util import out_of_date
 
 # Kernel versions that we run tests on and therefore support. Keep this in sync
 # with docs/support_matrix.rst.
@@ -505,3 +507,21 @@ CONFIG_LOCALVERSION_AUTO=n
 
 {inspect.cleandoc(arch.kernel_flavor_configs.get(flavor.name, ""))}
 """
+
+
+def _run_autoreconf() -> None:
+    if out_of_date(
+        "libdrgn/Makefile.in", "libdrgn/Makefile.am", "libdrgn/configure.ac"
+    ) or out_of_date("libdrgn/configure", "libdrgn/configure.ac"):
+        try:
+            subprocess.check_call(["autoreconf", "-i", "libdrgn"])
+        except BaseException:
+            try:
+                os.remove("libdrgn/configure")
+            except FileNotFoundError:
+                pass
+            try:
+                os.remove("libdrgn/Makefile.in")
+            except FileNotFoundError:
+                pass
+            raise

vmtest/rootfsbuild.py
@@ -8,7 +8,12 @@ import tempfile
 from typing import Literal, Optional, TextIO
 
 from vmtest.chroot import chroot_sh_cmd
-from vmtest.config import ARCHITECTURES, HOST_ARCHITECTURE, Architecture
+from vmtest.config import (
+    ARCHITECTURES,
+    HOST_ARCHITECTURE,
+    Architecture,
+    _run_autoreconf,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -188,6 +193,7 @@ mount --bind . "$1/mnt"
 def build_drgn_for_arch(
     target: Architecture, directory: Path, *, outfile: Optional[TextIO] = None
 ) -> None:
+    _run_autoreconf()
     if target is not HOST_ARCHITECTURE and HOST_ARCHITECTURE is not None:
         return _cross_compile_drgn(target, directory, outfile=outfile)
     else: