osandov/drgn

commit a5cd92f24eb42d86970a614a80cc73a34d6ff9fa

libdrgn: make vmcoreinfo accessible before loading debug info
UTS_RELEASE is currently only accessible once debug info is loaded with
prog.load_debug_info(main=True). This makes it difficult to get the
release, find the appropriate vmlinux, then load the found vmlinux. We
can add vmcoreinfo_object_find as part of set_core_dump(), which makes
it possible to do the following:

  prog = drgn.Program()
  prog.set_core_dump(core_dump_path)
  release = prog['UTS_RELEASE'].string_()
  vmlinux_path = find_vmlinux(release)
  prog.load_debug_info([vmlinux_path])

The only downside is that this ends up using the default definition of
char rather than what we would get from the debug info, but that
shouldn't be a big problem.

libdrgn/kdump.c
@@ -125,6 +125,10 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 	}
 
 	prog->flags |= DRGN_PROGRAM_IS_LINUX_KERNEL;
+	err = drgn_program_add_object_finder(prog, vmcoreinfo_object_find,
+					     prog);
+	if (err)
+		goto err;
 	drgn_program_set_platform(prog, &platform);
 	return NULL;
 

libdrgn/linux_kernel.c
@@ -227,7 +227,7 @@ out:
 	return err;
 }
 
-static struct drgn_error *
+struct drgn_error *
 vmcoreinfo_object_find(const char *name, size_t name_len, const char *filename,
 		       enum drgn_find_object_flags flags, void *arg,
 		       struct drgn_object *ret)
@@ -1431,15 +1431,6 @@ linux_kernel_report_debug_info(struct drgn_program *prog,
 	size_t i, num_kmods = 0;
 	bool need_module_definition = false;
 	bool vmlinux_is_pending = false;
-
-	if (report_main && !prog->added_vmcoreinfo_object_finder) {
-		err = drgn_program_add_object_finder(prog,
-						     vmcoreinfo_object_find,
-						     prog);
-		if (err)
-			return err;
-		prog->added_vmcoreinfo_object_finder = true;
-	}
 
 	if (n) {
 		kmods = malloc_array(n, sizeof(*kmods));

libdrgn/linux_kernel.h
@@ -20,6 +20,11 @@ struct drgn_error *read_vmcoreinfo_fallback(struct drgn_memory_reader *reader,
 					    struct vmcoreinfo *ret);
 
 struct drgn_error *
+vmcoreinfo_object_find(const char *name, size_t name_len, const char *filename,
+		       enum drgn_find_object_flags flags, void *arg,
+		       struct drgn_object *ret);
+
+struct drgn_error *
 linux_kernel_report_debug_info(struct drgn_program *prog,
 			       struct drgn_dwarf_index *dindex,
 			       const char **paths, size_t n,

libdrgn/program.c
@@ -404,6 +404,13 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 	} else if (vmcoreinfo_note) {
 		prog->flags |= DRGN_PROGRAM_IS_LINUX_KERNEL;
 	}
+	if (prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL) {
+		err = drgn_program_add_object_finder(prog,
+						     vmcoreinfo_object_find,
+						     prog);
+		if (err)
+			goto out_segments;
+	}
 
 	drgn_program_set_platform(prog, &platform);
 	return NULL;

libdrgn/program.h
@@ -90,7 +90,6 @@ struct drgn_program {
 	enum drgn_program_flags flags;
 	struct drgn_platform platform;
 	bool has_platform;
-	bool added_vmcoreinfo_object_finder;
 	bool attached_dwfl_state;
 	bool prstatus_cached;
 

tests/helpers/linux/test_uts.py
@@ -1,5 +1,7 @@
 import os
 
+import drgn
+
 from tests.helpers.linux import LinuxHelperTestCase
 
 
@@ -7,4 +9,11 @@ class TestUts(LinuxHelperTestCase):
     def test_uts_release(self):
         self.assertEqual(
             self.prog["UTS_RELEASE"].string_().decode(), os.uname().release,
+        )
+
+    def test_uts_release_no_debug_info(self):
+        prog = drgn.Program()
+        prog.set_kernel()
+        self.assertEqual(
+            prog["UTS_RELEASE"].string_().decode(), os.uname().release,
         )

commit 12cdfbb805ef53cdc6b5a5a8d933ab35ecf0cf5c

libdrgn: fix memory segment leak on kdump initialization failure
If drgn_program_finish_set_kernel() fails, then we need to clear the
memory segments.

Fixes: a5cd92f24eb4 ("libdrgn: make vmcoreinfo accessible before loading debug info")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/kdump.c
@@ -261,20 +261,20 @@ struct drgn_error *drgn_program_set_kdump(struct drgn_program *prog)
 		goto err_platform;
 	err = drgn_program_add_memory_segment(prog, 0, UINT64_MAX,
 					      drgn_read_kdump, ctx, true);
-	if (err) {
-		drgn_memory_reader_deinit(&prog->reader);
-		drgn_memory_reader_init(&prog->reader);
-		goto err_platform;
-	}
+	if (err)
+		goto err_segments;
 
 	prog->flags |= DRGN_PROGRAM_IS_LINUX_KERNEL;
 	err = drgn_program_finish_set_kernel(prog);
 	if (err)
-		goto err_platform;
+		goto err_segments;
 	prog->kdump_ctx = ctx;
 	drgn_call_plugins_prog("drgn_prog_set", prog);
 	return NULL;
 
+err_segments:
+	drgn_memory_reader_deinit(&prog->reader);
+	drgn_memory_reader_init(&prog->reader);
 err_platform:
 	prog->has_platform = had_platform;
 err: