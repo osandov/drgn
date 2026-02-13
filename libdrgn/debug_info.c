osandov/drgn

commit 26ac1f065fd25d83d89a2683761697c9fa01055a

Disallow module address range overlap
I originally wasn't sure whether to allow this or not. It doesn't make
sense for modules to have overlapping address ranges, but I also wanted
to be permissive in the case that a module was unloaded and then a new
module reused its address ranges. However, the module address tree
doesn't handle overlaps (it is keyed only on start address), so it was
always questionable. Even worse, inserting duplicate start addresses can
corrupt the tree:

1. Module A is created, set to start address X, and inserted in the
   tree.
2. Module B is created, set to start address Y, and inserted in the
   tree.
3. Module A is reset to start address Y.
   a. First, this deletes module A from the tree. Note that this doesn't
      reinitialize the tree node.
   b. Then, this attempts to insert module A at address Y. However,
      drgn_module_address_tree_insert() doesn't allow duplicates, so it
      doesn't get inserted. Instead, the node is left in whatever state
      it was in after it was deleted.
4. The tree is modified more.
5. Module A is reset to start address Z. This attempts to delete module
   A from the tree, but it's not actually in the tree, so deleting the
   node corrupts the tree.

So, let's be conservative and disallow overlap entirely. Hopefully this
doesn't break anyone, but it can be worked around by unsetting the
address range on the overlapping module.

Fixes #574.

Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Reported-by: Stephen Brennan <stephen.s.brennan@oracle.com>
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -539,10 +539,33 @@ LIBDRGN_PUBLIC bool drgn_module_address_range(const struct drgn_module *module,
 	return true;
 }
 
+static struct drgn_error *
+drgn_check_module_address_range_overlap(struct drgn_module *module,
+					uint64_t start, uint64_t end)
+{
+	for (auto it = drgn_module_address_tree_search_le(&module->prog->dbinfo.modules_by_address,
+							  &start);
+	     it.entry && it.entry->start < end;
+	     it = drgn_module_address_tree_next(it)) {
+		if (start < it.entry->end && it.entry->module != module) {
+			return drgn_error_format(DRGN_ERROR_INVALID_ARGUMENT,
+						 "%s [0x%" PRIx64 ", 0x%" PRIx64 ") overlaps "
+						 "%s [0x%" PRIx64 ", 0x%" PRIx64 ")",
+						 module->name, start, end,
+						 it.entry->module->name,
+						 it.entry->start,
+						 it.entry->end);
+		}
+	}
+	return NULL;
+}
+
 LIBDRGN_PUBLIC
 struct drgn_error *drgn_module_set_address_range(struct drgn_module *module,
 						 uint64_t start, uint64_t end)
 {
+	struct drgn_error *err;
+
 	// This is a special case instead of a wrapper around
 	// drgn_module_set_address_ranges() so we can avoid allocating memory.
 	// Since the old address range might be module->single_address_range,
@@ -553,22 +576,31 @@ struct drgn_error *drgn_module_set_address_range(struct drgn_module *module,
 					 "invalid module address range");
 	}
 
+	err = drgn_check_module_address_range_overlap(module, start, end);
+	if (err)
+		return err;
+
 	drgn_module_delete_address_ranges(module);
 
 	module->single_address_range.start = start;
 	module->single_address_range.end = end;
 	module->single_address_range.module = module;
 
-	// We don't bother checking for overlapping address ranges, which
-	// shouldn't happen with well-formed programs and at worst causes
-	// spurious failed lookups. We may need to revisit this if it's a
-	// problem in practice.
-	drgn_module_address_tree_insert(&module->prog->dbinfo.modules_by_address,
-					&module->single_address_range, NULL);
+	int r = drgn_module_address_tree_insert(&module->prog->dbinfo.modules_by_address,
+						&module->single_address_range,
+						NULL);
+	assert(r > 0); // We checked for overlap.
 
 	module->address_ranges = &module->single_address_range;
 	module->num_address_ranges = 1;
 	return NULL;
+}
+
+static int drgn_module_address_range_compare(const void *_a, const void *_b)
+{
+	const struct drgn_module_address_range *a = _a;
+	const struct drgn_module_address_range *b = _b;
+	return (a->start > b->start) - (a->start < b->start);
 }
 
 LIBDRGN_PUBLIC
@@ -576,6 +608,8 @@ struct drgn_error *drgn_module_set_address_ranges(struct drgn_module *module,
 						  uint64_t ranges[][2],
 						  size_t num_ranges)
 {
+	struct drgn_error *err;
+
 	if (num_ranges == 1) {
 		return drgn_module_set_address_range(module, ranges[0][0],
 						     ranges[0][1]);
@@ -592,19 +626,40 @@ struct drgn_error *drgn_module_set_address_ranges(struct drgn_module *module,
 				return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
 							 "invalid module address range");
 			}
+			err = drgn_check_module_address_range_overlap(module,
+								      ranges[i][0],
+								      ranges[i][1]);
+			if (err)
+				return err;
 			address_ranges[i].start = ranges[i][0];
 			address_ranges[i].end = ranges[i][1];
 			address_ranges[i].module = module;
+		}
+
+		qsort(address_ranges, num_ranges, sizeof(address_ranges[0]),
+		      drgn_module_address_range_compare);
+
+		for (size_t i = 1; i < num_ranges; i++) {
+			if (address_ranges[i].start < address_ranges[i - 1].end) {
+				return drgn_error_format(DRGN_ERROR_INVALID_ARGUMENT,
+							 "%s [0x%" PRIx64 ", 0x%" PRIx64 ") overlaps "
+							 "[0x%" PRIx64 ", 0x%" PRIx64 ")",
+							 module->name,
+							 address_ranges[i - 1].start,
+							 address_ranges[i - 1].end,
+							 address_ranges[i].start,
+							 address_ranges[i].end);
+			}
 		}
 	}
 
 	drgn_module_delete_address_ranges(module);
 
 	for (size_t i = 0; i < num_ranges; i++) {
-		// We don't bother checking for overlapping address ranges; see
-		// drgn_module_set_address_range().
-		drgn_module_address_tree_insert(&module->prog->dbinfo.modules_by_address,
-						&address_ranges[i], NULL);
+		int r = drgn_module_address_tree_insert(&module->prog->dbinfo.modules_by_address,
+							&address_ranges[i],
+							NULL);
+		assert(r > 0); // We checked for overlap.
 	}
 
 	if (num_ranges) {

_drgn.pyi
@@ -1694,6 +1694,8 @@ class Module:
     This is a sequence of tuples of the start (inclusive) and end (exclusive)
     addresses. For each range, the start address is strictly less than the end
     address. If the module is not loaded in memory, then the sequence is empty.
+    Address ranges may not overlap.
+
     If not known yet, then this is ``None``.
 
     :meth:`Program.loaded_modules()` sets this automatically from the program

tests/test_module.py
@@ -415,6 +415,44 @@ class TestModule(TestCase):
         with self.assertRaisesRegex(ValueError, "invalid module address range"):
             module.address_ranges = ((0, 0),)
 
+    def test_address_range_overlap(self):
+        prog = Program()
+        prog.extra_module("/foo/bar", create=True).address_range = (
+            0x10000000,
+            0x10010000,
+        )
+        with self.assertRaisesRegex(ValueError, "overlaps /foo/bar"):
+            prog.extra_module("/foo/baz", create=True).address_range = (
+                0x10001000,
+                0x10010000,
+            )
+
+    def test_address_ranges_overlap(self):
+        prog = Program()
+        prog.extra_module("/foo/bar", create=True).address_range = (
+            0x10000000,
+            0x10010000,
+        )
+        with self.assertRaisesRegex(ValueError, "overlaps /foo/bar"):
+            prog.extra_module("/foo/baz", create=True).address_ranges = (
+                (0x1000000, 0x10000000),
+                (0x10001000, 0x10010000),
+            )
+
+    def test_address_ranges_self_overlap(self):
+        module = Program().extra_module("/foo/bar", create=True)
+        with self.assertRaisesRegex(ValueError, "overlaps"):
+            module.address_ranges = (
+                (0x10000000, 0x10010000),
+                (0x10001000, 0x10020000),
+            )
+
+    def test_address_range_expand(self):
+        module = Program().extra_module("/foo/bar", create=True)
+        module.address_range = (0x10000000, 0x10010000)
+        module.address_range = (0x1000000, 0x10020000)
+        self.assertEqual(module.address_range, (0x1000000, 0x10020000))
+
     def test_build_id(self):
         module = Program().extra_module("/foo/bar", create=True)
 

commit 3c1cd4e15adf0783a10476319f9f5610d02660c3

libdrgn: debug_info: fix another unused variable warning with NDEBUG
I missed this one in commit ba3cad86d2dd ("libdrgn: debug_info: fix
unused variable warning with NDEBUG").

Fixes: 26ac1f065fd2 ("Disallow module address range overlap")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -660,6 +660,7 @@ struct drgn_error *drgn_module_set_address_ranges(struct drgn_module *module,
 		int r = drgn_module_address_tree_insert(&module->prog->dbinfo.modules_by_address,
 							&address_ranges[i],
 							NULL);
+		(void)r;
 		assert(r > 0); // We checked for overlap.
 	}
 