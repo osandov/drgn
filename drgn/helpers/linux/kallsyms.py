osandov/drgn

commit 203c1beddd2edd0c01b0798934a3ad6d4ec4041c

helpers: linux: add vmlinux kallsyms helpers
When properly configured, Linux contains its own symbol table within
kernel memory at runtime. It is exposed as the /proc/kallsyms file,
which is the easiest way to consume it, for live kernels.

However, with recent changes to the Linux kernel in 6.0, necessary
symbols are exposed within VMCOREINFO that allow us to interpret the
data structures inside kernel memory, without needing debuginfo. This
allows us to write helpers that can load kallsyms on vmcores, or on live
systems.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

drgn/helpers/linux/kallsyms.py
@@ -1,0 +1,75 @@
+# Copyright (c) 2024 Oracle and/or its affiliates
+# SPDX-License-Identifier: LGPL-2.1-or-later
+"""
+Kallsyms
+--------
+
+The ``drgn.helpers.linux.kallsyms`` module contains helpers which allow you to
+use the built-in kallsyms symbol table for drgn symbol lookup. Combined with an
+alternative type information source, this can enable debugging Linux kernel core
+dumps without the corresponding DWARF debuginfo files. Even without type
+information, kallsyms can be used to help locate objects, and drgn's low-level
+memory reading functions can be used to do basic debugging tasks.
+"""
+import os
+import re
+from typing import Dict
+
+from _drgn import (
+    _linux_helper_load_builtin_kallsyms,
+    _linux_helper_load_proc_kallsyms as _load_proc_kallsyms,
+)
+from drgn import Program, ProgramFlags, SymbolIndex
+
+__all__ = (
+    "load_vmlinux_kallsyms",
+)
+
+
+def _vmcoreinfo_symbols(prog: Program) -> Dict[str, int]:
+    vmcoreinfo_data = prog["VMCOREINFO"].string_().decode("ascii")
+    vmcoreinfo_symbols = {}
+    sym_re = re.compile(r"SYMBOL\(([^)]+)\)=([A-Fa-f0-9]+)")
+    for line in vmcoreinfo_data.strip().split("\n"):
+        match = sym_re.fullmatch(line)
+        if match:
+            vmcoreinfo_symbols[match.group(1)] = int(match.group(2), 16)
+    return vmcoreinfo_symbols
+
+
+def _load_builtin_kallsyms(prog: Program) -> SymbolIndex:
+    symbol_reqd = [
+        "kallsyms_names",
+        "kallsyms_token_table",
+        "kallsyms_token_index",
+        "kallsyms_num_syms",
+        "kallsyms_offsets",
+        "kallsyms_relative_base",
+        "kallsyms_addresses",
+        "_stext",
+    ]
+    symbols = _vmcoreinfo_symbols(prog)
+    args = []
+    for sym in symbol_reqd:
+        args.append(symbols.get(sym, 0))
+    return _linux_helper_load_builtin_kallsyms(prog, *args)
+
+
+def load_vmlinux_kallsyms(prog: Program) -> SymbolIndex:
+    """
+    Create a kallsyms index for vmlinux
+
+    This function loads the kallsyms for the core kernel and returns a symbol
+    index. This function does not require that any debuginfo is loaded for the
+    kernel: it either relies on ``/proc/kallsyms`` (which requires running drgn
+    as root) or it parses internal data structures using information found from
+    the VMCOREINFO note (which requires Linux 6.0 or later, or a backport of
+    commit ``f09bddbd86619 ("vmcoreinfo: add kallsyms_num_syms symbol")`` and
+    its dependencies).
+
+    :returns: a symbol index containing kallsyms for the core kernel (vmlinux)
+    """
+    if prog.flags & ProgramFlags.IS_LIVE and os.geteuid() == 0:
+        return _load_proc_kallsyms()
+    else:
+        return _load_builtin_kallsyms(prog)

_drgn.pyi
@@ -2839,3 +2839,8 @@ def _linux_helper_pid_task(pid: Object, pid_type: IntegerLike) -> Object:
 def _linux_helper_find_task(__ns: Object, __pid: IntegerLike) -> Object: ...
 def _linux_helper_kaslr_offset(__prog: Program) -> int: ...
 def _linux_helper_pgtable_l5_enabled(__prog: Program) -> bool: ...
+def _linux_helper_load_proc_kallsyms(
+    filename: Optional[str] = None,
+    modules: bool = False,
+) -> SymbolIndex: ...
+def _linux_helper_load_builtin_kallsyms(prog: Program) -> SymbolIndex: ...

libdrgn/Makefile.am
@@ -73,6 +73,8 @@ libdrgnimpl_la_SOURCES = $(ARCH_DEFS_PYS:_defs.py=.c) \
 			 helpers.h \
 			 io.c \
 			 io.h \
+			 kallsyms.c \
+			 kallsyms.h \
 			 language.c \
 			 language.h \
 			 language_c.c \

libdrgn/kallsyms.h
@@ -1,0 +1,46 @@
+// Copyright (c) 2024 Oracle and/or its affiliates
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+/**
+ * @file
+ *
+ * Kallsyms data handling
+ *
+ * See @ref Kallsyms
+ */
+
+#ifndef DRGN_KALLSYMS_H
+#define DRGN_KALLSYMS_H
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#include "hash_table.h"
+#include "symbol.h"
+
+struct kallsyms_locations {
+	uint64_t kallsyms_names;
+	uint64_t kallsyms_token_table;
+	uint64_t kallsyms_token_index;
+	uint64_t kallsyms_num_syms;
+	uint64_t kallsyms_offsets;
+	uint64_t kallsyms_relative_base;
+	uint64_t kallsyms_addresses;
+	uint64_t _stext;
+};
+
+/**
+ * Initialize a symbol index containing symbols from /proc/kallsyms
+ */
+struct drgn_error *drgn_load_proc_kallsyms(const char *filename, bool modules,
+					   struct drgn_symbol_index *ret);
+
+/**
+ * Initialize a symbol index containing symbols from built-in kallsyms tables
+ */
+struct drgn_error *
+drgn_load_builtin_kallsyms(struct drgn_program *prog,
+			   struct kallsyms_locations *loc,
+			   struct drgn_symbol_index *ret);
+
+#endif // DRGN_KALLSYMS_H

libdrgn/python/drgnpy.h
@@ -353,6 +353,8 @@ struct index_arg {
 };
 int index_converter(PyObject *o, void *p);
 
+int u64_converter(PyObject *o, void *p);
+
 struct path_arg {
 	bool allow_fd;
 	bool allow_none;
@@ -396,5 +398,9 @@ DrgnObject *drgnpy_linux_helper_pid_task(PyObject *self, PyObject *args,
 DrgnObject *drgnpy_linux_helper_find_task(PyObject *self, PyObject *args);
 PyObject *drgnpy_linux_helper_kaslr_offset(PyObject *self, PyObject *arg);
 PyObject *drgnpy_linux_helper_pgtable_l5_enabled(PyObject *self, PyObject *arg);
+PyObject *drgnpy_linux_helper_load_proc_kallsyms(PyObject *self, PyObject *args,
+						 PyObject *kwds);
+PyObject *drgnpy_linux_helper_load_builtin_kallsyms(PyObject *self, PyObject *args,
+						    PyObject *kwds);
 
 #endif /* DRGNPY_H */

libdrgn/python/helpers.c
@@ -3,6 +3,7 @@
 
 #include "drgnpy.h"
 #include "../helpers.h"
+#include "../kallsyms.h"
 #include "../program.h"
 
 PyObject *drgnpy_linux_helper_direct_mapping_offset(PyObject *self, PyObject *arg)
@@ -290,4 +291,57 @@ PyObject *drgnpy_linux_helper_pgtable_l5_enabled(PyObject *self, PyObject *arg)
 	if (!(prog->prog.flags & DRGN_PROGRAM_IS_LINUX_KERNEL))
 		return PyErr_Format(PyExc_ValueError, "not Linux kernel");
 	Py_RETURN_BOOL(prog->prog.vmcoreinfo.pgtable_l5_enabled);
+}
+
+PyObject *drgnpy_linux_helper_load_proc_kallsyms(PyObject *self, PyObject *args,
+						 PyObject *kwds)
+
+{
+	static char *kwnames[] = {"filename", "modules", NULL};
+	const char *filename = "/proc/kallsyms";
+	int modules = 0;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|sp:load_proc_kallsyms",
+					 kwnames, &filename, &modules))
+		return NULL;
+
+	_cleanup_pydecref_ SymbolIndex *index = call_tp_alloc(SymbolIndex);
+	if (!index)
+		return set_drgn_error(&drgn_enomem);
+
+	struct drgn_error *err = drgn_load_proc_kallsyms(filename, modules, &index->index);
+	if (err)
+		return set_drgn_error(err);
+	return (PyObject *)no_cleanup_ptr(index);
+}
+
+PyObject *
+drgnpy_linux_helper_load_builtin_kallsyms(PyObject *self, PyObject *args,
+					  PyObject *kwds)
+{
+	static char *kwnames[] = {"prog", "names", "token_table", "token_index", "num_syms",
+	                          "offsets", "relative_base", "addresses", "_stext", NULL};
+	struct kallsyms_locations kl;
+	PyObject *prog_obj;
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!O&O&O&O&O&O&O&O&:load_builtin_kallsyms",
+					 kwnames, &Program_type, &prog_obj,
+					 u64_converter, &kl.kallsyms_names,
+					 u64_converter, &kl.kallsyms_token_table,
+					 u64_converter, &kl.kallsyms_token_index,
+					 u64_converter, &kl.kallsyms_num_syms,
+					 u64_converter, &kl.kallsyms_offsets,
+					 u64_converter, &kl.kallsyms_relative_base,
+					 u64_converter, &kl.kallsyms_addresses,
+					 u64_converter, &kl._stext))
+		return NULL;
+
+	struct drgn_program *prog = &((Program *)prog_obj)->prog;
+	_cleanup_pydecref_ SymbolIndex *index = call_tp_alloc(SymbolIndex);
+	if (!index)
+		return set_drgn_error(&drgn_enomem);
+
+	struct drgn_error *err = drgn_load_builtin_kallsyms(prog, &kl, &index->index);
+	if (err)
+		return set_drgn_error(err);
+	return (PyObject *)no_cleanup_ptr(index);
 }

libdrgn/python/main.c
@@ -208,6 +208,12 @@ static PyMethodDef drgn_methods[] = {
 	 METH_O},
 	{"_linux_helper_pgtable_l5_enabled",
 	 drgnpy_linux_helper_pgtable_l5_enabled, METH_O},
+	{"_linux_helper_load_proc_kallsyms",
+	 (PyCFunction)drgnpy_linux_helper_load_proc_kallsyms,
+	 METH_VARARGS | METH_KEYWORDS},
+	{"_linux_helper_load_builtin_kallsyms",
+	 (PyCFunction)drgnpy_linux_helper_load_builtin_kallsyms,
+	 METH_VARARGS | METH_KEYWORDS},
 	{},
 };
 

commit ba821bcd974aae3e6e0bc4f7cb284363668e00e2

drgn.helpers.linux.kallsyms: only use kallsyms for local kernel
For live remote kernels, we can't use the local /proc/kallsyms.

Fixes: 203c1beddd2e ("helpers: linux: add vmlinux kallsyms helpers")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/kallsyms.py
@@ -80,7 +80,9 @@ def load_vmlinux_kallsyms(prog: Program) -> SymbolIndex:
 
     :returns: a symbol index containing kallsyms for the core kernel (vmlinux)
     """
-    if prog.flags & ProgramFlags.IS_LIVE and os.geteuid() == 0:
+    if (prog.flags & (ProgramFlags.IS_LIVE | ProgramFlags.IS_LOCAL)) == (
+        ProgramFlags.IS_LIVE | ProgramFlags.IS_LOCAL
+    ) and os.geteuid() == 0:
         return _load_proc_kallsyms()
     else:
         return _load_builtin_kallsyms(prog)