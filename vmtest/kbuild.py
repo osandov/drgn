osandov/drgn

commit 07b9ee5f366036107b1c31d1164c6917c63f5a02

vmtest.kbuild: generate package in final directory
vmtest.kbuild -b $build_dir -p $package_dir --package-format directory
fails when attempting to rename the temporary package directory if
$build_dir and $package_dir are on different filesystems or Btrfs
subvolumes. Put the temporary package directory in $package_dir so that
the rename can succeed.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

vmtest/kbuild.py
@@ -467,7 +467,7 @@ MODULE_LICENSE("GPL");
         )
 
         with tempfile.TemporaryDirectory(
-            prefix="install.", dir=self._build_dir
+            prefix=package.name + ".tmp.", dir=package.parent
         ) as tmp_name:
             install_dir = Path(tmp_name)
             modules_dir = install_dir / "lib" / "modules" / kernel_release

commit e48b796f31631e7afdbee216030ce23cd9930f7f

vmtest.kbuild: don't fail if package parent directory doesn't exist
We're trying to create it before moving the package to its final
location, but it's too late there since earlier we try to create our
temporary directory in that same parent directory.

Fixes: 07b9ee5f3660 ("vmtest.kbuild: generate package in final directory")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

vmtest/kbuild.py
@@ -676,6 +676,7 @@ MODULE_DESCRIPTION("Module for testing build");
                 .strip()
             )
 
+        package.parent.mkdir(parents=True, exist_ok=True)
         with tempfile.TemporaryDirectory(
             prefix=package.name + ".tmp.", dir=package.parent
         ) as tmp_name:
@@ -743,7 +744,6 @@ MODULE_DESCRIPTION("Module for testing build");
             self._copy_module_build(modules_build_dir)
             await self._test_external_module_build(modules_build_dir)
 
-            package.parent.mkdir(parents=True, exist_ok=True)
             if format == "directory":
                 logger.info("renaming directory")
                 try: