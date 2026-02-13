osandov/drgn

commit 91da9ac0bad52416836b7b5b8e0b4f440593566e

Migrate runq related helpers from drgn-tools
Signed-off-by: Richard Li <tianqi.li@oracle.com>

drgn/helpers/linux/sched.py
@@ -20,6 +20,7 @@ from _drgn import (
 )
 from drgn import IntegerLike, Object, Program
 from drgn.helpers.common.prog import takes_program_or_default
+from drgn.helpers.linux.percpu import per_cpu
 
 __all__ = (
     "cpu_curr",
@@ -163,3 +164,47 @@ def loadavg(prog: Program) -> Tuple[float, float, float]:
     avenrun = prog["avenrun"]
     vals = [avenrun[i].value_() / (1 << 11) for i in range(3)]
     return (vals[0], vals[1], vals[2])
+
+
+@takes_program_or_default
+def cpu_rq(prog: Program, cpu: IntegerLike) -> Object:
+    """
+    Get the runqueue for a given cpu.
+
+    :param cpu: CPU number.
+    :returns: ``struct rq``
+    """
+    return per_cpu(prog["runqueues"], cpu)
+
+
+def task_rq(task: Object) -> Object:
+    """
+    Get the runqueue for a given task.
+
+    :param task: ``struct task_struct *``
+    :returns: ``struct rq``
+    """
+    return cpu_rq(task.prog_, task_cpu(task))
+
+
+def task_lastrun2now(task: Object) -> int:
+    """
+    Get the number of nanoseconds since a task last started running.
+
+    The return duration will cover task's last run time on cpu and also
+    the time staying in current status, usually the time slice for task
+    on cpu will be short, so this can roughly tell how long this task
+    has been staying in current status.
+    For task status in "R" status, if it's still on cpu, then this return
+    the duration time this task has been running, otherwise it roughly tell
+    how long this task has been staying in runqueue.
+
+    Note that task.sched_info only exists if CONFIG_SCHED_INFO=y, enabled by CONFIG_SCHEDSTATS or CONFIG_TASK_DELAY_ACCT.
+
+    :param task: ``struct task_struct *``
+    :returns: duration in ns granularity
+    """
+    arrival_time = task.sched_info.last_arrival.value_()
+    rq_clock = task_rq(task).clock.value_()
+
+    return rq_clock - arrival_time

tests/linux_kernel/helpers/test_sched.py
@@ -3,6 +3,7 @@
 
 import os
 import signal
+import time
 
 from drgn.helpers.linux.cpumask import for_each_possible_cpu
 from drgn.helpers.linux.pid import find_task
@@ -12,6 +13,7 @@ from drgn.helpers.linux.sched import (
     idle_task,
     loadavg,
     task_cpu,
+    task_lastrun2now,
     task_on_cpu,
     task_state_to_char,
     task_thread_info,
@@ -101,3 +103,9 @@ class TestSched(LinuxKernelTestCase):
         values = loadavg(self.prog)
         self.assertEqual(len(values), 3)
         self.assertTrue(all(v >= 0.0 for v in values))
+
+    def test_task_lastrun2now(self):
+        with fork_and_stop() as pid:
+            time.sleep(0.01)
+            task = find_task(self.prog, pid)
+            self.assertGreaterEqual(task_lastrun2now(task), 10000000)

commit f75494e47d07c3861c99edbfedcee8bf8016ae69

drgn.helpers.linux.sched: return struct rq * from {cpu,task}_rq()
They are currently returning struct rq (not a pointer), which was an
oversight. Hopefully no one is depending on the incorrect type.

Fixes: 91da9ac0bad5 ("Migrate runq related helpers from drgn-tools")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/sched.py
@@ -176,9 +176,9 @@ def cpu_rq(prog: Program, cpu: IntegerLike) -> Object:
     Get the runqueue for a given cpu.
 
     :param cpu: CPU number.
-    :returns: ``struct rq``
+    :returns: ``struct rq *``
     """
-    return per_cpu(prog["runqueues"], cpu)
+    return per_cpu(prog["runqueues"], cpu).address_of_()
 
 
 def task_rq(task: Object) -> Object:
@@ -186,7 +186,7 @@ def task_rq(task: Object) -> Object:
     Get the runqueue for a given task.
 
     :param task: ``struct task_struct *``
-    :returns: ``struct rq``
+    :returns: ``struct rq *``
     """
     return cpu_rq(task.prog_, task_cpu(task))
 