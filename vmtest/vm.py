osandov/drgn

commit 82cff79dc1d778e42c9e13d3fc9789b6bdcf4535

drgn.helpers.linux.sched: add sched_entity_is_task() and sched_entity_to_task()
sched_entity_to_task() will be used for the crash runq command, and
sched_entity_is_task() is useful along with it.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

vmtest/vm.py
@@ -77,6 +77,7 @@ mount -t proc -o nosuid,nodev,noexec proc /tmp/merged/proc
 mount -t sysfs -o nosuid,nodev,noexec sys /tmp/merged/sys
 # cgroup2 was added in Linux v4.5.
 mount -t cgroup2 -o nosuid,nodev,noexec cgroup2 /tmp/merged/sys/fs/cgroup || true
+echo +cpu > /tmp/merged/sys/fs/cgroup/cgroup.subtree_control 2>/dev/null || true
 # Ideally we'd just be able to create an opaque directory for /tmp on the upper
 # layer. However, before Linux kernel commit 51f7e52dc943 ("ovl: share inode
 # for hard link") (in v4.8), overlayfs doesn't handle hard links correctly,

drgn/helpers/linux/sched.py
@@ -19,7 +19,7 @@ from _drgn import (
     _linux_helper_task_on_cpu as task_on_cpu,
     _linux_helper_task_thread_info as task_thread_info,
 )
-from drgn import IntegerLike, Object, Program
+from drgn import IntegerLike, Object, Program, container_of
 from drgn.helpers.common.prog import takes_program_or_default
 from drgn.helpers.linux.list import list_for_each_entry
 from drgn.helpers.linux.percpu import per_cpu
@@ -32,6 +32,8 @@ __all__ = (
     "loadavg",
     "rq_for_each_fair_task",
     "rq_for_each_rt_task",
+    "sched_entity_is_task",
+    "sched_entity_to_task",
     "task_cpu",
     "task_on_cpu",
     "task_rq",
@@ -221,6 +223,31 @@ def rq_for_each_rt_task(rq: Object) -> Iterator[Object]:
         yield from list_for_each_entry(
             "struct task_struct", queue.address_of_(), "rt.run_list"
         )
+
+
+def sched_entity_is_task(se: Object) -> bool:
+    """
+    Return whether a scheduler entity is a task.
+
+    :param se: ``struct sched_entity *``
+    """
+    try:
+        return not se.my_q
+    except AttributeError:
+        return True
+
+
+def sched_entity_to_task(se: Object) -> Object:
+    """
+    Get the task represented by a scheduler entity.
+
+    Note that this does not check whether the entity is actually a task; see
+    :func:`sched_entity_is_task()`.
+
+    :param se: ``struct sched_entity *``
+    :return: ``struct task_struct *``
+    """
+    return container_of(se, "struct task_struct", "se")
 
 
 def task_since_last_arrival_ns(task: Object) -> int:

tests/linux_kernel/helpers/test_cgroup.py
@@ -28,23 +28,29 @@ from tests.linux_kernel import (
 
 
 @contextlib.contextmanager
-def tmp_cgroups():
+def tmp_cgroup():
     for mnt in iter_mounts():
         if mnt.fstype == "cgroup2":
             break
     else:
         raise unittest.SkipTest("cgroup2 not mounted")
 
-    parent = Path(tempfile.mkdtemp(prefix="drgn-tests-", dir=mnt.mount_point))
+    path = Path(tempfile.mkdtemp(prefix="drgn-tests-", dir=mnt.mount_point))
     try:
+        yield path
+    finally:
+        path.rmdir()
+
+
+@contextlib.contextmanager
+def tmp_cgroups():
+    with tmp_cgroup() as parent:
         child = parent / "child"
         child.mkdir()
         try:
             yield parent, child
         finally:
             child.rmdir()
-    finally:
-        parent.rmdir()
 
 
 class TestCgroup(LinuxKernelTestCase):

tests/linux_kernel/helpers/test_sched.py
@@ -1,6 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+import contextlib
 import os
 import resource
 import signal
@@ -8,6 +9,8 @@ from threading import Condition, Thread
 import time
 import unittest
 
+from drgn import container_of
+from drgn.helpers.linux.cgroup import cgroup_get_from_path
 from drgn.helpers.linux.cpumask import for_each_possible_cpu
 from drgn.helpers.linux.pid import find_task
 from drgn.helpers.linux.sched import (
@@ -18,6 +21,8 @@ from drgn.helpers.linux.sched import (
     loadavg,
     rq_for_each_fair_task,
     rq_for_each_rt_task,
+    sched_entity_is_task,
+    sched_entity_to_task,
     task_cpu,
     task_on_cpu,
     task_since_last_arrival_ns,
@@ -32,7 +37,16 @@ from tests.linux_kernel import (
     skip_unless_have_test_kmod,
     wait_until,
 )
+from tests.linux_kernel.helpers.test_cgroup import tmp_cgroup
 from util import KernelVersion
+
+
+@contextlib.contextmanager
+def tmp_cgroup_with_cpu_controller():
+    with tmp_cgroup() as cgroup_dir:
+        if "cpu" not in (cgroup_dir / "cgroup.controllers").read_text().split():
+            raise unittest.SkipTest("cgroup CPU controller not enabled")
+        yield cgroup_dir
 
 
 class TestSched(LinuxKernelTestCase):
@@ -160,6 +174,23 @@ class TestSched(LinuxKernelTestCase):
         rq = cpu_rq(self.prog, cpu)
         pids = [task.pid.value_() for task in rq_for_each_rt_task(rq)]
         self.assertIn(os.getpid(), pids)
+
+    def test_sched_entity_is_task(self):
+        task = find_task(self.prog, os.getpid())
+        self.assertTrue(sched_entity_is_task(task.se.address_of_()))
+
+    def test_sched_entity_is_task_false(self):
+        with tmp_cgroup_with_cpu_controller() as cgroup_dir:
+            cgrp = cgroup_get_from_path(self.prog, cgroup_dir.name)
+            css = cgrp.subsys[self.prog["cpu_cgrp_id"]]
+            task_group = container_of(css, "struct task_group", "css")
+            cpu = min(os.sched_getaffinity(0))
+            se = task_group.se[cpu].read_()
+            self.assertFalse(sched_entity_is_task(se))
+
+    def test_sched_entity_to_task(self):
+        task = find_task(self.prog, os.getpid())
+        self.assertEqual(sched_entity_to_task(task.se.address_of_()), task)
 
     def test_thread_group_leader(self):
         condition = Condition()

commit ffdd412c8ba7defcaaa9d18ed65d8a51e1709ae2

vmtest.vm: enable cgroup controllers after pivoting
We're redirecting stderr to /dev/null with the intention of being silent
if cgroups or the CPU controller aren't available, but /dev doesn't
exist at that point, so instead we get errors like:

  cannot create /dev/null: Read-only file system

Fix it by enabling it after we pivot to the new root filesystem.

Fixes: 82cff79dc1d7 ("drgn.helpers.linux.sched: add sched_entity_is_task() and sched_entity_to_task()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

vmtest/vm.py
@@ -77,7 +77,6 @@ mount -t proc -o nosuid,nodev,noexec proc /tmp/merged/proc
 mount -t sysfs -o nosuid,nodev,noexec sys /tmp/merged/sys
 # cgroup2 was added in Linux v4.5.
 mount -t cgroup2 -o nosuid,nodev,noexec cgroup2 /tmp/merged/sys/fs/cgroup || true
-echo +cpu > /tmp/merged/sys/fs/cgroup/cgroup.subtree_control 2>/dev/null || true
 # Ideally we'd just be able to create an opaque directory for /tmp on the upper
 # layer. However, before Linux kernel commit 51f7e52dc943 ("ovl: share inode
 # for hard link") (in v4.8), overlayfs doesn't handle hard links correctly,
@@ -88,6 +87,9 @@ mount -t tmpfs -o nosuid,nodev tmpfs /tmp/merged/tmp
 pivot_root /tmp/merged /tmp/merged/mnt
 cd /
 umount -n -l /mnt
+
+# Enable cgroup controllers.
+echo +cpu > /sys/fs/cgroup/cgroup.subtree_control 2>/dev/null || true
 
 # Load kernel modules.
 mkdir -p "/lib/modules/$RELEASE"