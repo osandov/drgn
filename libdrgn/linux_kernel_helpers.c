osandov/drgn

commit f5ac4989d349b4deb41ec94a5bae761b3a5226eb

libdrgn: add linux_kernel_direct_mapping arch hook
And use it for linux_helper_direct_mapping_offset(). Unlike like the
fallback hook, this one uses proper object lookup, and can be used
anywhere: not just on local live kernels.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/linux_kernel_helpers.c
@@ -73,11 +73,21 @@ struct drgn_error *linux_helper_direct_mapping_offset(struct drgn_program *prog,
 		return NULL;
 	}
 
+	if (prog->platform.arch->linux_kernel_direct_mapping_offset) {
+		err = prog->platform.arch->linux_kernel_direct_mapping_offset(
+			prog, &prog->direct_mapping_offset);
+		if (err)
+			return err;
+		prog->direct_mapping_offset_cached = true;
+		*ret = prog->direct_mapping_offset;
+		return NULL;
+	}
+
 	// The direct mapping offset can vary depending on architecture, kernel
-	// version, configuration options, and KASLR. Rather than dealing with
-	// all of that, get a virtual address in the direct mapping and
-	// translate it to a physical address via the page table. The difference
-	// is the offset.
+	// version, configuration options, and KASLR. For architectures that
+	// don't handle all of that, get a virtual address in the direct mapping
+	// and translate it to a physical address via the page table. The
+	// difference is the offset.
 	//
 	// The virtual address we pick doesn't matter as long as:
 	//

libdrgn/arch_x86_64.c
@@ -552,6 +552,20 @@ linux_kernel_live_direct_mapping_fallback_x86_64(struct drgn_program *prog,
 	}
 }
 
+static struct drgn_error *
+linux_kernel_direct_mapping_offset_x86_64(struct drgn_program *prog,
+					  uint64_t *address_ret)
+{
+	DRGN_OBJECT(page_offset_base, prog);
+	struct drgn_error *err = drgn_program_find_object(prog, "page_offset_base",
+							  NULL, DRGN_FIND_OBJECT_VARIABLE,
+							  &page_offset_base);
+	if (err)
+		return err;
+
+	return drgn_object_read_unsigned(&page_offset_base, address_ret);
+}
+
 struct pgtable_iterator_x86_64 {
 	struct pgtable_iterator it;
 	uint16_t index[5];
@@ -707,6 +721,8 @@ const struct drgn_architecture_info arch_info_x86_64 = {
 	.apply_elf_reloc = apply_elf_reloc_x86_64,
 	.linux_kernel_live_direct_mapping_fallback =
 		linux_kernel_live_direct_mapping_fallback_x86_64,
+	.linux_kernel_direct_mapping_offset =
+		linux_kernel_direct_mapping_offset_x86_64,
 	.linux_kernel_pgtable_iterator_create =
 		linux_kernel_pgtable_iterator_create_x86_64,
 	.linux_kernel_pgtable_iterator_destroy =

libdrgn/platform.h
@@ -434,6 +434,15 @@ struct drgn_architecture_info {
 	struct drgn_error *(*linux_kernel_live_direct_mapping_fallback)(struct drgn_program *prog,
 									uint64_t *address_ret,
 									uint64_t *size_ret);
+	/**
+	 * Return the address of the direct mapping virtual address range.
+	 *
+	 * This hook is used for @ref linux_helper_direct_mapping_offset(). It
+	 * is optional: if not provided, we can infer it using a page table walk
+	 * and a symbol from the direct mapping.
+	 */
+	struct drgn_error *(*linux_kernel_direct_mapping_offset)(struct drgn_program *prog,
+								 uint64_t *address_ret);
 	/** Allocate a Linux kernel page table iterator. */
 	struct drgn_error *(*linux_kernel_pgtable_iterator_create)(struct drgn_program *,
 								   struct pgtable_iterator **);

commit 99b98ae138d592825959cdfb1c0193dc5d1a7482

libdrgn: fall back to address translation for direct mapping offset if page_offset_base isn't found
Before Linux kernel commit 18ec1eaf58fb ("x86/mm: Enable 5-level paging
support by default") (in v5.5), non-KASLR builds of the kernel default
to CONFIG_DYNAMIC_MEMORY_LAYOUT=n, in which case page_offset_base is not
defined. Unfortunately, the default offset changed in Linux kernel
commit d52888aa2753 ("x86/mm: Move LDT remap out of KASLR region on
5-level paging") (in v4.20), so we can't easily hardcode it. Fall back
to the original address translation method in this case.

Fixes: f5ac4989d349 ("libdrgn: add linux_kernel_direct_mapping arch hook")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/linux_kernel_helpers.c
@@ -76,11 +76,15 @@ struct drgn_error *linux_helper_direct_mapping_offset(struct drgn_program *prog,
 	if (prog->platform.arch->linux_kernel_direct_mapping_offset) {
 		err = prog->platform.arch->linux_kernel_direct_mapping_offset(
 			prog, &prog->direct_mapping_offset);
-		if (err)
+		if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP)) {
+			// Fall back to address translation.
+		} else if (err) {
 			return err;
-		prog->direct_mapping_offset_cached = true;
-		*ret = prog->direct_mapping_offset;
-		return NULL;
+		} else {
+			prog->direct_mapping_offset_cached = true;
+			*ret = prog->direct_mapping_offset;
+			return NULL;
+		}
 	}
 
 	// The direct mapping offset can vary depending on architecture, kernel