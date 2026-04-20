osandov/drgn

commit d7d75c01c921071b494f0f8560ca4e3a264ad59b

drgn.helpers.linux.kernfs: add kernfs_children() helper
Add a helper, kernfs_children(), that returns an iterator over the
children of a directory in kernfs.

The implementation of this must check kn.flags to ensure it is a
KERNFS_DIR. struct kernfs_node unions dir and attr and without this
check, calling kernfs_children() on a KERNFS_FILE kernfs node may return
corrupt data or trigger a page fault that is confusing to the user.

Signed-off-by: Joanne Koong <joannelkoong@gmail.com>

drgn/helpers/linux/kernfs.py
@@ -10,6 +10,7 @@ kernfs pseudo filesystem interface in :linux:`include/linux/kernfs.h`.
 """
 
 import os
+from typing import Iterator, Optional
 
 from drgn import NULL, Object, Path
 from drgn.helpers.linux.rbtree import rbtree_inorder_for_each_entry
@@ -20,6 +21,7 @@ __all__ = (
     "kernfs_path",
     "kernfs_root",
     "kernfs_walk",
+    "kernfs_children",
 )
 
 
@@ -110,3 +112,22 @@ def kernfs_walk(parent: Object, path: Path) -> Object:
         else:
             return NULL(parent.prog_, kernfs_nodep_type)
     return parent
+
+
+def kernfs_children(kn: Object) -> Optional[Iterator[Object]]:
+    """
+    Get an iterator over the children of the given kernfs node if the node
+    represents a directory.
+
+    :param kn: ``struct kernfs_node *``
+    :return: Iterator of ``struct kernfs_node *`` objects.
+    """
+    KERNFS_TYPE_MASK = 0x000F
+    kernfs_dir = kn.prog_.constant("KERNFS_DIR")
+
+    if (kn.flags & KERNFS_TYPE_MASK) != kernfs_dir:
+        raise ValueError("not a directory")
+
+    return rbtree_inorder_for_each_entry(
+        "struct kernfs_node", kn.dir.children.address_of_(), "rb"
+    )

commit 40c59f5b2edaa3ead58b9d3d3d42c3219cf2d3a8

drgn.helpers.linux.kernfs: remove incorrect Optional annotation from kernfs_children()
An earlier version returned None instead of raising an exception, but
the version that was merged doesn't. Also clarify the documentation.

Fixes: d7d75c01c921 ("drgn.helpers.linux.kernfs: add kernfs_children() helper")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

drgn/helpers/linux/kernfs.py
@@ -10,7 +10,7 @@ kernfs pseudo filesystem interface in :linux:`include/linux/kernfs.h`.
 """
 
 import os
-from typing import Iterator, Optional
+from typing import Iterator
 
 from drgn import NULL, Object, Path
 from drgn.helpers.linux.rbtree import rbtree_inorder_for_each_entry
@@ -136,13 +136,13 @@ def kernfs_walk(parent: Object, path: Path, follow_symlinks: bool = True) -> Obj
     return parent
 
 
-def kernfs_children(kn: Object) -> Optional[Iterator[Object]]:
+def kernfs_children(kn: Object) -> Iterator[Object]:
     """
-    Get an iterator over the children of the given kernfs node if the node
-    represents a directory.
+    Get an iterator over the children of the given kernfs node.
 
     :param kn: ``struct kernfs_node *``
     :return: Iterator of ``struct kernfs_node *`` objects.
+    :raises ValueError: if the kernfs node is not a directory
     """
     if not _kernfs_node_type(kn, "KERNFS_DIR"):
         raise ValueError("not a directory")