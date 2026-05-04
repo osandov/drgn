osandov/drgn

commit f20b41c8c05d9231ee5098e34378497fd1fd9640

Allow naming and configuring order of symbol finders
Currently, the bare-bones add_symbol_finder() interface only allows
adding a symbol finder that is called before any existing finders. It'd
be useful to be able to specify the order that symbol finders should be
called in and to selectively enable and disable them. To do that, we
also need finders to have a name to identify them by. So, replace
add_symbol_finder() (which hasn't been in a release yet) with a set of
interfaces providing this flexibility: register_symbol_finder(),
set_enabled_symbol_finders(), registered_symbol_finders(), and
enabled_symbol_finders(). Also change the callback signature to take the
program.

In particular, this flexibility will be very useful for a plugin system:
pre-installed plugins can register symbol finders that the user can
choose to enable or disable.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -112,17 +112,6 @@ void drgn_program_init(struct drgn_program *prog,
 	drgn_object_init(&prog->vmemmap, prog);
 }
 
-static void drgn_program_deinit_symbol_finders(struct drgn_program *prog)
-{
-	struct drgn_symbol_finder *finder = prog->symbol_finders;
-	while (finder) {
-		struct drgn_symbol_finder *next = finder->next;
-		if (finder->free)
-			free(finder);
-		finder = next;
-	}
-}
-
 void drgn_program_deinit(struct drgn_program *prog)
 {
 	if (prog->core_dump_notes_cached) {
@@ -145,8 +134,12 @@ void drgn_program_deinit(struct drgn_program *prog)
 
 	drgn_object_deinit(&prog->vmemmap);
 
+	drgn_handler_list_deinit(struct drgn_symbol_finder, finder,
+				 &prog->symbol_finders,
+		if (finder->ops.destroy)
+			finder->ops.destroy(finder->arg);
+	);
 	drgn_object_index_deinit(&prog->oindex);
-	drgn_program_deinit_symbol_finders(prog);
 	drgn_program_deinit_types(prog);
 	drgn_memory_reader_deinit(&prog->reader);
 
@@ -217,6 +210,81 @@ drgn_program_add_object_finder(struct drgn_program *prog,
 {
 	return drgn_program_add_object_finder_impl(prog, NULL, fn, arg);
 }
+
+#define DRGN_PROGRAM_FINDER(which)						\
+struct drgn_error *								\
+drgn_program_register_##which##_finder_impl(struct drgn_program *prog,		\
+					    struct drgn_##which##_finder *finder,\
+					    const char *name,			\
+					    const struct drgn_##which##_finder_ops *ops,\
+					    void *arg, size_t enable_index)	\
+{										\
+	struct drgn_error *err;							\
+	if (finder) {								\
+		finder->handler.name = name;					\
+		finder->handler.free = false;					\
+	} else {								\
+		finder = malloc(sizeof(*finder));				\
+		if (!finder)							\
+			return &drgn_enomem;					\
+		finder->handler.name = strdup(name);				\
+		if (!finder->handler.name) {					\
+			free(finder);						\
+			return &drgn_enomem;					\
+		}								\
+		finder->handler.free = true;					\
+	}									\
+	memcpy(&finder->ops, ops, sizeof(finder->ops));				\
+	finder->arg = arg;							\
+	err = drgn_handler_list_register(&prog->which##_finders,		\
+					 &finder->handler, enable_index,	\
+					 #which " finder");			\
+	if (err && finder->handler.free) {					\
+		free((char *)finder->handler.name);				\
+		free(finder);							\
+	}									\
+	return err;								\
+}										\
+										\
+LIBDRGN_PUBLIC struct drgn_error *						\
+drgn_program_register_##which##_finder(struct drgn_program *prog, const char *name,\
+				       const struct drgn_##which##_finder_ops *ops,\
+				       void *arg, size_t enable_index)		\
+{										\
+	return drgn_program_register_##which##_finder_impl(prog, NULL, name,	\
+							   ops, arg,		\
+							   enable_index);	\
+}										\
+										\
+LIBDRGN_PUBLIC struct drgn_error *						\
+drgn_program_registered_##which##_finders(struct drgn_program *prog,		\
+					  const char ***names_ret,		\
+					  size_t *count_ret)			\
+{										\
+	return drgn_handler_list_registered(&prog->which##_finders, names_ret,	\
+					    count_ret);				\
+}										\
+										\
+LIBDRGN_PUBLIC struct drgn_error *						\
+drgn_program_set_enabled_##which##_finders(struct drgn_program *prog,		\
+					   const char * const *names,		\
+					   size_t count)			\
+{										\
+	return drgn_handler_list_set_enabled(&prog->which##_finders, names,	\
+					     count, #which "finder");		\
+}										\
+										\
+LIBDRGN_PUBLIC struct drgn_error *						\
+drgn_program_enabled_##which##_finders(struct drgn_program *prog,		\
+				       const char ***names_ret,			\
+				       size_t *count_ret)			\
+{										\
+	return drgn_handler_list_enabled(&prog->which##_finders, names_ret,	\
+					 count_ret);				\
+}
+
+DRGN_PROGRAM_FINDER(symbol)
+#undef DRGN_PROGRAM_FINDER
 
 static struct drgn_error *
 drgn_program_check_initialized(struct drgn_program *prog)
@@ -1801,44 +1869,15 @@ drgn_program_symbols_search(struct drgn_program *prog, const char *name,
 			    struct drgn_symbol_result_builder *builder)
 {
 	struct drgn_error *err = NULL;
-	struct drgn_symbol_finder *finder = prog->symbol_finders;
-	while (finder) {
-		err = finder->fn(name, addr, flags, finder->arg, builder);
+	drgn_handler_list_for_each_enabled(struct drgn_symbol_finder, finder,
+					   &prog->symbol_finders) {
+		err = finder->ops.find(name, addr, flags, finder->arg, builder);
 		if (err ||
 		    ((flags & DRGN_FIND_SYMBOL_ONE)
 		     && drgn_symbol_result_builder_count(builder) > 0))
 			break;
-		finder = finder->next;
 	}
 	return err;
-}
-
-struct drgn_error *
-drgn_program_add_symbol_finder_impl(struct drgn_program *prog,
-				    struct drgn_symbol_finder *finder,
-				    drgn_symbol_find_fn fn, void *arg)
-{
-	if (finder) {
-		finder->free = false;
-	} else {
-		finder = malloc(sizeof(*finder));
-		if (!finder)
-			return &drgn_enomem;
-		finder->free = true;
-	}
-	finder->fn = fn;
-	finder->arg = arg;
-	finder->next = prog->symbol_finders;
-	prog->symbol_finders = finder;
-	return NULL;
-}
-
-LIBDRGN_PUBLIC struct drgn_error *
-drgn_program_add_symbol_finder(struct drgn_program *prog,
-			       drgn_symbol_find_fn fn,
-			       void *arg)
-{
-	return drgn_program_add_symbol_finder_impl(prog, NULL, fn, arg);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *

libdrgn/python/program.c
@@ -493,9 +493,10 @@ static struct drgn_error *py_object_find_fn(const char *name, size_t name_len,
 	return drgn_object_copy(ret, &((DrgnObject *)obj)->obj);
 }
 
-static struct drgn_error *py_symbol_find_fn(const char *name, uint64_t addr,
-					    enum drgn_find_symbol_flags flags,
-					    void *data, struct drgn_symbol_result_builder *builder)
+static struct drgn_error *
+py_symbol_find_fn(const char *name, uint64_t addr,
+		  enum drgn_find_symbol_flags flags, void *arg,
+		  struct drgn_symbol_result_builder *builder)
 {
 	PyGILState_guard();
 
@@ -521,8 +522,10 @@ static struct drgn_error *py_symbol_find_fn(const char *name, uint64_t addr,
 
 	_cleanup_pydecref_ PyObject *one_obj = PyBool_FromLong(flags & DRGN_FIND_SYMBOL_ONE);
 
-	_cleanup_pydecref_ PyObject *tmp = PyObject_CallFunction(data, "OOO", name_obj,
-								 address_obj, one_obj);
+	_cleanup_pydecref_ PyObject *tmp =
+		PyObject_CallFunction(PyTuple_GET_ITEM(arg, 1), "OOOO",
+				      PyTuple_GET_ITEM(arg, 0), name_obj,
+				      address_obj, one_obj);
 	if (!tmp)
 		return drgn_error_from_python();
 
@@ -557,6 +560,153 @@ static struct drgn_error *py_symbol_find_fn(const char *name, uint64_t addr,
 	return NULL;
 }
 
+#define symbol_finder_arg(self, fn)						\
+	_cleanup_pydecref_ PyObject *arg = Py_BuildValue("OO", self, fn);	\
+	if (!arg)								\
+		return NULL;
+
+#define DEFINE_PROGRAM_FINDER_METHODS(which)					\
+static PyObject *Program_register_##which##_finder(Program *self,		\
+						   PyObject *args,		\
+						   PyObject *kwds)		\
+{										\
+	struct drgn_error *err;							\
+	static char *keywords[] = {"name", "fn", "enable_index", NULL};		\
+	const char *name;							\
+	PyObject *fn;								\
+	PyObject *enable_index_obj = Py_None;					\
+	if (!PyArg_ParseTupleAndKeywords(args, kwds,				\
+					 "sO|$O:register_" #which "_finder",	\
+					 keywords, &name, &fn,			\
+					 &enable_index_obj))			\
+		return NULL;							\
+										\
+	if (!PyCallable_Check(fn)) {						\
+		PyErr_SetString(PyExc_TypeError, "fn must be callable");	\
+		return NULL;							\
+	}									\
+										\
+	size_t enable_index;							\
+	if (enable_index_obj == Py_None) {					\
+		enable_index = DRGN_HANDLER_REGISTER_DONT_ENABLE;		\
+	} else {								\
+		_cleanup_pydecref_ PyObject *negative_one = PyLong_FromLong(-1);\
+		if (!negative_one)						\
+			return NULL;						\
+		int eq = PyObject_RichCompareBool(enable_index_obj,		\
+						  negative_one, Py_EQ);		\
+		if (eq < 0)							\
+			return NULL;						\
+		if (eq) {							\
+			enable_index = DRGN_HANDLER_REGISTER_ENABLE_LAST;	\
+		} else {							\
+			enable_index = PyLong_AsSize_t(enable_index_obj);	\
+			if (enable_index == (size_t)-1 && PyErr_Occurred())	\
+				return NULL;					\
+			/*							\
+			 * If the index happens to be the			\
+			 * DRGN_HANDLER_REGISTER_DONT_ENABLE sentinel		\
+			 * (SIZE_MAX - 1), set it to something else; it's	\
+			 * impossible to have this many finders anyways.	\
+			 */							\
+			if (enable_index == DRGN_HANDLER_REGISTER_DONT_ENABLE)	\
+				enable_index--;					\
+		}								\
+	}									\
+										\
+	which##_finder_arg(self, fn)						\
+	if (!Program_hold_reserve(self, 1))					\
+		return NULL;							\
+	const struct drgn_##which##_finder_ops ops = {				\
+		.find = py_##which##_find_fn,					\
+	};									\
+	err = drgn_program_register_##which##_finder(&self->prog, name, &ops,	\
+						     arg, enable_index);	\
+	if (err)								\
+		return set_drgn_error(err);					\
+	Program_hold_object(self, arg);						\
+	Py_RETURN_NONE;								\
+										\
+}										\
+										\
+static PyObject *Program_registered_##which##_finders(Program *self)		\
+{										\
+	struct drgn_error *err;							\
+	_cleanup_free_ const char **names = NULL;				\
+	size_t count;								\
+	err = drgn_program_registered_##which##_finders(&self->prog, &names,	\
+							&count);		\
+	if (err)								\
+		return set_drgn_error(err);					\
+	_cleanup_pydecref_ PyObject *res = PySet_New(NULL);			\
+	if (!res)								\
+		return NULL;							\
+	for (size_t i = 0; i < count; i++) {					\
+		_cleanup_pydecref_ PyObject *name =				\
+			PyUnicode_FromString(names[i]);				\
+		if (!name)							\
+			return NULL;						\
+		if (PySet_Add(res, name))					\
+			return NULL;						\
+	}									\
+	return_ptr(res);							\
+}										\
+										\
+static PyObject *Program_set_enabled_##which##_finders(Program *self,		\
+						       PyObject *args,		\
+						       PyObject *kwds)		\
+{										\
+	struct drgn_error *err;							\
+	static char *keywords[] = {"names", NULL};				\
+	PyObject *names_obj;							\
+	if (!PyArg_ParseTupleAndKeywords(args, kwds,				\
+					 "O:set_enabled_" #which "_finders",	\
+					 keywords, &names_obj))			\
+		return NULL;							\
+	_cleanup_pydecref_ PyObject *names_seq =				\
+		PySequence_Fast(names_obj, "names must be sequence");		\
+	if (!names_seq)								\
+		return NULL;							\
+	size_t count = PySequence_Fast_GET_SIZE(names_seq);			\
+	_cleanup_free_ const char **names =					\
+		malloc_array(count, sizeof(names[0]));				\
+	if (!names)								\
+		return NULL;							\
+	for (size_t i = 0; i < count; i++) {					\
+		names[i] = PyUnicode_AsUTF8(PySequence_Fast_GET_ITEM(names_seq, i));\
+		if (!names[i])							\
+			return NULL;						\
+	}									\
+	err = drgn_program_set_enabled_##which##_finders(&self->prog, names,	\
+							 count);		\
+	if (err)								\
+		return set_drgn_error(err);					\
+	Py_RETURN_NONE;								\
+}										\
+										\
+static PyObject *Program_enabled_##which##_finders(Program *self)		\
+{										\
+	struct drgn_error *err;							\
+	_cleanup_free_ const char **names = NULL;				\
+	size_t count;								\
+	err = drgn_program_enabled_##which##_finders(&self->prog, &names,	\
+						     &count);			\
+	if (err)								\
+		return set_drgn_error(err);					\
+	_cleanup_pydecref_ PyObject *res = PyList_New(count);			\
+	if (!res)								\
+		return NULL;							\
+	for (size_t i = 0; i < count; i++) {					\
+		PyObject *name = PyUnicode_FromString(names[i]);		\
+		if (!name)							\
+			return NULL;						\
+		PyList_SET_ITEM(res, i, name);					\
+	}									\
+	return_ptr(res);							\
+}
+
+DEFINE_PROGRAM_FINDER_METHODS(symbol)
+
 static PyObject *Program_add_object_finder(Program *self, PyObject *args,
 					   PyObject *kwds)
 {
@@ -577,34 +727,6 @@ static PyObject *Program_add_object_finder(Program *self, PyObject *args,
 		return NULL;
 
 	err = drgn_program_add_object_finder(&self->prog, py_object_find_fn,
-					     fn);
-	if (err)
-		return set_drgn_error(err);
-	Py_RETURN_NONE;
-}
-
-static PyObject *Program_add_symbol_finder(Program *self, PyObject *args,
-					   PyObject *kwds)
-{
-	static char *keywords[] = {"fn", NULL};
-	struct drgn_error *err;
-	PyObject *fn;
-	int ret;
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O:add_symbol_finder",
-					 keywords, &fn))
-	    return NULL;
-
-	if (!PyCallable_Check(fn)) {
-		PyErr_SetString(PyExc_TypeError, "fn must be callable");
-		return NULL;
-	}
-
-	ret = Program_hold_object(self, fn);
-	if (ret == -1)
-		return NULL;
-
-	err = drgn_program_add_symbol_finder(&self->prog, py_symbol_find_fn,
 					     fn);
 	if (err)
 		return set_drgn_error(err);
@@ -1255,15 +1377,30 @@ static int Program_set_language(Program *self, PyObject *value, void *arg)
 	return 0;
 }
 
+#define PROGRAM_FINDER_METHOD_DEFS(which)					\
+	{"register_" #which "_finder",						\
+	 (PyCFunction)Program_register_##which##_finder,			\
+	 METH_VARARGS | METH_KEYWORDS,						\
+	 drgn_Program_register_##which##_finder_DOC},				\
+	{"registered_" #which "_finders",					\
+	 (PyCFunction)Program_registered_##which##_finders, METH_NOARGS,	\
+	 drgn_Program_registered_##which##_finders_DOC},			\
+	{"set_enabled_" #which "_finders",					\
+	 (PyCFunction)Program_set_enabled_##which##_finders,			\
+	 METH_VARARGS | METH_KEYWORDS,						\
+	 drgn_Program_set_enabled_##which##_finders_DOC},			\
+	{"enabled_" #which "_finders",						\
+	 (PyCFunction)Program_enabled_##which##_finders, METH_NOARGS,		\
+	 drgn_Program_enabled_##which##_finders_DOC}
+
 static PyMethodDef Program_methods[] = {
 	{"add_memory_segment", (PyCFunction)Program_add_memory_segment,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_add_memory_segment_DOC},
+	PROGRAM_FINDER_METHOD_DEFS(symbol),
 	{"add_type_finder", (PyCFunction)Program_add_type_finder,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_add_type_finder_DOC},
 	{"add_object_finder", (PyCFunction)Program_add_object_finder,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_add_object_finder_DOC},
-	{"add_symbol_finder", (PyCFunction)Program_add_symbol_finder,
-	 METH_VARARGS | METH_KEYWORDS, drgn_Program_add_symbol_finder_DOC},
 	{"set_core_dump", (PyCFunction)Program_set_core_dump,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_set_core_dump_DOC},
 	{"set_kernel", (PyCFunction)Program_set_kernel, METH_NOARGS,

_drgn.pyi
@@ -21,6 +21,7 @@ from typing import (
     Mapping,
     Optional,
     Sequence,
+    Set,
     Tuple,
     Union,
     overload,
@@ -435,6 +436,64 @@ class Program:
             another :ref:`buffer <python:binaryseq>` type.
         """
         ...
+    def register_symbol_finder(
+        self,
+        name: str,
+        fn: Callable[[Program, Optional[str], Optional[int], bool], Sequence[Symbol]],
+        *,
+        enable_index: Optional[int] = None,
+    ) -> None:
+        """
+        Register a callback for finding symbols in the program.
+
+        This does not enable the finder unless *enable_index* is given.
+
+        The callback should take four arguments: the program, a *name*, an
+        *address*, and a boolean flag *one*. It should return a list of symbols
+        or an empty list if no matches are found.
+
+        If *name* is not ``None``, then only symbols with that name should be
+        returned. If *address* is not ``None``, then only symbols containing
+        that address should be returned. If neither is ``None``, then the
+        returned symbols must match both. If both are ``None``, then all
+        symbols should be considered matching.
+
+        When the *one* flag is ``False``, the callback should return a list of
+        all matching symbols. When it is ``True``, it should return a list with
+        at most one symbol which is the best match.
+
+        :param name: Finder name.
+        :param fn: Callable taking ``(prog, name, address, one)`` and returning
+            a sequence of :class:`Symbol`\\ s.
+        :param enable_index: Insert the finder into the list of enabled finders
+            at the given index. If -1 or greater than the number of enabled
+            finders, insert it at the end. If ``None`` or not given, don't
+            enable the finder.
+        :raises ValueError: if there is already a finder with the given name
+        """
+        ...
+    def registered_symbol_finders(self) -> Set[str]:
+        """Return the names of all registered symbol finders."""
+        ...
+    def set_enabled_symbol_finders(self, names: Sequence[str]) -> None:
+        """
+        Set the list of enabled symbol finders.
+
+        Finders are called in the same order as the list. When the *one* flag
+        is set, the search will short-circuit after the first finder which
+        returns a result, and subsequent finders will not be called. Otherwise,
+        all callbacks will be called, and all results will be returned.
+
+        Finders that are not in the list are not called.
+
+        :param names: Names of finders to enable, in order.
+        :raises ValueError: if no finder has a given name or the same name is
+            given more than once
+        """
+        ...
+    def enabled_symbol_finders(self) -> List[str]:
+        """Return the names of enabled symbol finders, in order."""
+        ...
     def add_type_finder(
         self, fn: Callable[[TypeKind, str, Optional[str]], Optional[Type]]
     ) -> None:
@@ -466,33 +525,6 @@ class Program:
             and filename: ``(prog, name, flags, filename)``. The filename
             should be matched with :func:`filename_matches()`. This should
             return an :class:`Object` or ``None`` if not found.
-        """
-        ...
-    def add_symbol_finder(
-        self, fn: Callable[[Optional[str], Optional[int], bool], Sequence[Symbol]]
-    ) -> None:
-        """
-        Register a callback for finding symbols in the program.
-
-        The callback should take three arguments: a search name, a search
-        address, and a boolean flag 'one' indicating whether to return only
-        the single best match. When the 'one' flag is True, the callback should
-        return a list containing at most one :class:`Symbol`. When the flag is
-        False, the callback should return a list of all matching
-        :class:`Symbol`\\ s. Both the name and address arguments are optional.
-        If both are provided, then the result(s) should match both. If neither
-        are provided, the finder should return all available symbols. If no
-        result is found, the return should be an empty list.
-
-        Callbacks are called in reverse order of the order they were added
-        (i.e,, the most recently added callback is called first). When the
-        'one' flag is set, the search will short-circuit after the first
-        finder which returns a result, and subsequent finders will not be
-        called. Otherwise, all callbacks will be called, and all results will be
-        returned.
-
-        :param fn: Callable taking name, address, and 'one' flag, and
-            returning a sequence of :class:`Symbol`\\ s.
         """
         ...
     def set_core_dump(self, path: Union[Path, int]) -> None:

libdrgn/debug_info.c
@@ -2186,8 +2186,12 @@ void drgn_debug_info_init(struct drgn_debug_info *dbinfo,
 	drgn_program_add_object_finder_impl(prog, &dbinfo->object_finder,
 					    drgn_debug_info_find_object,
 					    dbinfo);
-	drgn_program_add_symbol_finder_impl(prog, &dbinfo->symbol_finder,
-					    elf_symbols_search, prog);
+	const struct drgn_symbol_finder_ops symbol_finder_ops = {
+		.find = elf_symbols_search,
+	};
+	drgn_program_register_symbol_finder_impl(prog, &dbinfo->symbol_finder,
+						 "elf", &symbol_finder_ops,
+						 prog, 0);
 	drgn_module_table_init(&dbinfo->modules);
 	c_string_set_init(&dbinfo->module_names);
 	drgn_dwarf_info_init(dbinfo);

libdrgn/drgn.h
@@ -937,7 +937,7 @@ struct drgn_error *drgn_program_find_symbols_by_address(struct drgn_program *pro
 							struct drgn_symbol ***syms_ret,
 							size_t *count_ret);
 
-/** Flags for @ref drgn_symbol_find_fn() */
+/** Flags for @ref drgn_symbol_finder_ops::find() */
 enum drgn_find_symbol_flags {
 	/** Find symbols whose name matches the name argument */
 	DRGN_FIND_SYMBOL_NAME = 1 << 0,
@@ -947,7 +947,7 @@ enum drgn_find_symbol_flags {
 	DRGN_FIND_SYMBOL_ONE = 1 << 2,
 };
 
-/** Result builder for @ref drgn_symbol_find_fn() */
+/** Result builder for @ref drgn_symbol_finder_ops::find() */
 struct drgn_symbol_result_builder;
 
 /**
@@ -965,44 +965,97 @@ drgn_symbol_result_builder_add(struct drgn_symbol_result_builder *builder,
 /** Get the current number of results in a symbol search result. */
 size_t drgn_symbol_result_builder_count(const struct drgn_symbol_result_builder *builder);
 
-/**
- * Callback for finding one or more symbols.
- *
- * The callback should perform a symbol lookup based on the flags given in @a
- * flags. When multiple flags are provided, the effect should be treated as a
- * logical AND. Symbol results should be added to the result builder @a builder,
- * via @ref drgn_symbol_result_builder_add(). When @ref DRGN_FIND_SYMBOL_ONE is
- * set, then the finding function should only return the single best symbol
- * result, and short-circuit return.
- *
- * When no symbol is found, simply do not add any result to the builder. No
- * error should be returned in this case.
- *
- * @param[in] name Name of the symbol to match
- * @param[in] addr Address of the symbol to match
- * @param[in] flags Flags indicating the desired behavior of the search
- * @param[in] arg Argument passed to @ref drgn_program_add_symbol_finder().
- * @param[in] builder Used to build the resulting symbol output
- */
-typedef struct drgn_error *
-(*drgn_symbol_find_fn)(const char *name, uint64_t addr,
-		       enum drgn_find_symbol_flags flags, void *arg,
-		       struct drgn_symbol_result_builder *builder);
+/** Symbol finder callback table. */
+struct drgn_symbol_finder_ops {
+	/**
+	 * Callback to destroy the symbol finder.
+	 *
+	 * This may be @c NULL.
+	 *
+	 * @param[in] arg Argument passed to @ref
+	 * drgn_program_register_symbol_finder().
+	 */
+	void (*destroy)(void *arg);
+	/**
+	 * Callback for finding one or more symbols.
+	 *
+	 * The callback should perform a symbol lookup based on the flags given
+	 * in @p flags. When multiple flags are provided, the effect should be
+	 * treated as a logical AND. Symbol results should be added to the
+	 * result builder @p builder, via @ref drgn_symbol_result_builder_add().
+	 * When @ref DRGN_FIND_SYMBOL_ONE is set, then the finding function
+	 * should only return the single best symbol result, and short-circuit
+	 * return.
+	 *
+	 * When no symbol is found, simply do not add any result to the builder.
+	 * No error should be returned in this case.
+	 *
+	 * @param[in] name Name of the symbol to match
+	 * @param[in] addr Address of the symbol to match
+	 * @param[in] flags Flags indicating the desired behavior of the search
+	 * @param[in] arg Argument passed to @ref
+	 * drgn_program_register_symbol_finder().
+	 * @param[in] builder Used to build the resulting symbol output
+	 */
+	struct drgn_error *(*find)(const char *name, uint64_t addr,
+				   enum drgn_find_symbol_flags flags, void *arg,
+				   struct drgn_symbol_result_builder *builder);
+};
 
 /**
  * Register a symbol finding callback.
  *
- * Callbacks are called in reverse order that they were originally added. In
- * case of a search for multiple symbols, then the results of all callbacks are
- * concatenated. If the search is for a single symbol, then the first callback
- * which finds a symbol will short-circuit the search.
- *
- * @param[in] fn Symbol search function
- * @param[in] arg Argument to pass to the callback
+ * @param[in] name Finder name. This is copied.
+ * @param[in] ops Callback table. This is copied.
+ * @param[in] arg Argument to pass to callbacks.
+ * @param[in] enable_index Insert the finder into the list of enabled finders at
+ * the given index. If @ref DRGN_HANDLER_REGISTER_ENABLE_LAST or greater than
+ * the number of enabled finders, insert it at the end. If @ref
+ * DRGN_HANDLER_REGISTER_DONT_ENABLE, don’t enable the finder.
  */
 struct drgn_error *
-drgn_program_add_symbol_finder(struct drgn_program *prog,
-			       drgn_symbol_find_fn fn, void *arg);
+drgn_program_register_symbol_finder(struct drgn_program *prog, const char *name,
+				    const struct drgn_symbol_finder_ops *ops,
+				    void *arg, size_t enable_index);
+
+/**
+ * Get the names of all registered symbol finders.
+ *
+ * The order of the names is arbitrary.
+ *
+ * @param[out] names_ret Returned array of names.
+ * @param[out] count_ret Returned number of names in @p names_ret.
+ */
+struct drgn_error *
+drgn_program_registered_symbol_finders(struct drgn_program *prog,
+				       const char ***names_ret,
+				       size_t *count_ret);
+
+/**
+ * Set the list of enabled symbol finders.
+ *
+ * Finders are called in the same order as the list. In case of a search for
+ * multiple symbols, then the results of all callbacks are concatenated. If the
+ * search is for a single symbol, then the first callback which finds a symbol
+ * will short-circuit the search.
+ *
+ * @param[in] names Names of finders to enable, in order.
+ * @param[in] count Number of names in @p names.
+ */
+struct drgn_error *
+drgn_program_set_enabled_symbol_finders(struct drgn_program *prog,
+					const char * const *names,
+					size_t count);
+
+/**
+ * Get the names of enabled symbol finders, in order.
+ *
+ * @param[out] names_ret Returned array of names.
+ * @param[out] count_ret Returned number of names in @p names_ret.
+ */
+struct drgn_error *
+drgn_program_enabled_symbol_finders(struct drgn_program *prog,
+				    const char ***names_ret, size_t *count_ret);
 
 /** Element type and size. */
 struct drgn_element_info {

libdrgn/program.h
@@ -21,6 +21,7 @@
 
 #include "debug_info.h"
 #include "drgn.h"
+#include "handler.h"
 #include "hash_table.h"
 #include "language.h"
 #include "memory_reader.h"
@@ -30,6 +31,8 @@
 #include "symbol.h"
 #include "type.h"
 #include "vector.h"
+
+struct drgn_symbol_finder;
 
 /**
  * @defgroup Internals Internals
@@ -110,7 +113,7 @@ struct drgn_program {
 	 */
 	struct drgn_object_index oindex;
 	struct drgn_debug_info dbinfo;
-	struct drgn_symbol_finder *symbol_finders;
+	struct drgn_handler_list symbol_finders;
 
 	/*
 	 * Program information.
@@ -381,9 +384,12 @@ drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
 					     struct drgn_symbol **ret);
 
 struct drgn_error *
-drgn_program_add_symbol_finder_impl(struct drgn_program *prog,
-				    struct drgn_symbol_finder *finder,
-				    drgn_symbol_find_fn fn, void *arg);
+drgn_program_register_symbol_finder_impl(struct drgn_program *prog,
+					 struct drgn_symbol_finder *finder,
+					 const char *name,
+					 const struct drgn_symbol_finder_ops *ops,
+					 void *arg, size_t enable_index);
+
 /**
  * Call before a blocking (I/O or long-running) operation.
  *

libdrgn/symbol.h
@@ -8,6 +8,7 @@
 
 #include "cleanup.h"
 #include "drgn.h"
+#include "handler.h"
 #include "vector.h"
 
 struct drgn_symbol {
@@ -20,10 +21,9 @@ struct drgn_symbol {
 };
 
 struct drgn_symbol_finder {
-	drgn_symbol_find_fn fn;
+	struct drgn_handler handler;
+	struct drgn_symbol_finder_ops ops;
 	void *arg;
-	struct drgn_symbol_finder *next;
-	bool free;
 };
 
 DEFINE_VECTOR_TYPE(symbolp_vector, struct drgn_symbol *);

commit bae600a678229a55dbed00daab094c3163cd80e9

libdrgn: fix missing space in drgn_program_set_enabled_*_finders()
Fixes: f20b41c8c05d ("Allow naming and configuring order of symbol finders")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -274,7 +274,7 @@ drgn_program_set_enabled_##which##_finders(struct drgn_program *prog,		\
 					   size_t count)			\
 {										\
 	return drgn_handler_list_set_enabled(&prog->which##_finders, names,	\
-					     count, #which "finder");		\
+					     count, #which " finder");		\
 }										\
 										\
 LIBDRGN_PUBLIC struct drgn_error *						\