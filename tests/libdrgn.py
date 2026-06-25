7603b1ef3686a77f62688e66752aefd5213d2a48
Omar Sandoval <osandov@osandov.com>
2026-06-25 10:13:46 -0700

    tests: remove unused libdrgn ctypes wrappers
    Fixes: baba1ff3f02e ("libdrgn: make program components pluggable")
    Fixes: 7d251fee6e31 ("Translate C lexer tests to C unit tests")
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- tests/libdrgn.py
+++ tests/libdrgn.py
@@ -8,34 +8,6 @@ import _drgn
 _drgn_cdll = ctypes.CDLL(_drgn.__file__)
 
 
-class _drgn_error(ctypes.Structure):
-    _fields_ = [
-        ("code", ctypes.c_uint),
-        ("errnum", ctypes.c_int),
-        ("path", ctypes.c_char_p),
-        ("msg", ctypes.c_char_p),
-    ]
-
-
-class _drgn_type(ctypes.Structure):
-    pass
-
-
-class _drgn_qualified_type(ctypes.Structure):
-    _fields_ = [
-        ("type", ctypes.POINTER(_drgn_type)),
-        ("qualifiers", ctypes.c_uint),
-    ]
-
-
-class _drgn_token(ctypes.Structure):
-    _fields_ = [
-        ("kind", ctypes.c_int),
-        ("value", ctypes.c_void_p),
-        ("len", ctypes.c_size_t),
-    ]
-
-
 _drgn_cdll.drgn_test_serialize_bits.restype = None
 _drgn_cdll.drgn_test_serialize_bits.argtypes = [
     ctypes.c_void_p,
