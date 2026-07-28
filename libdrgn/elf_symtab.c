osandov/drgn

commit 683886d9bcf6bf28e5e58bc8738da24069141374

elf_symtab: add support for .gnu_debugdata
The .gnu_debugdata section, also known as "MiniDebuginfo"[1], is an ELF
section which contains a second, compressed ELF file. This contained
file typically has all of its data stripped, except for a symbol table.
The symbols contained in this section are meant to be used in addition
to symbols that might be found in the ".dynsym" section.

For distributions like Fedora, this is intended to be used in
combination with .eh_frame information that is provided in the loadable
ELF file, so that stack traces can be automatically created for
userspace crashes, even without debuginfo. To that end, GDB has support
for these sections.

It makes sense for drgn to also include support for these symbol tables,
as we move toward supporting use cases where full DWARF data may not be
available.

Closes #465.

[1]: https://fedoraproject.org/wiki/Features/MiniDebugInfo

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/elf_symtab.c
@@ -3,8 +3,13 @@
 
 #include <gelf.h>
 #include <libelf.h>
+#ifdef WITH_LZMA
+#include <lzma.h>
+#endif
+#include <stdio.h>
 #include <stdlib.h>
 
+#include "cleanup.h"
 #include "debug_info.h"
 #include "elf_file.h"
 #include "elf_symtab.h"
@@ -12,6 +17,7 @@
 #include "log.h"
 #include "minmax.h"
 #include "serialize.h"
+#include "string_builder.h"
 #include "util.h"
 
 static struct drgn_error *find_elf_file_symtab(struct drgn_elf_file *file,
@@ -21,13 +27,22 @@ static struct drgn_error *find_elf_file_symtab(struct drgn_elf_file *file,
 					       Elf_Scn **scn_ret,
 					       GElf_Word *strtab_idx_ret,
 					       GElf_Word *num_local_symbols_ret,
-					       bool *full_symtab_ret)
+					       bool *full_symtab_ret,
+					       Elf_Scn **gnu_debugdata_ret)
 {
 	Elf_Scn *scn = NULL;
+	size_t shstrndx;
+	if (elf_getshdrstrndx(file->elf, &shstrndx))
+		return drgn_error_libelf();
 	while ((scn = elf_nextscn(file->elf, scn))) {
 		GElf_Shdr shdr_mem, *shdr = gelf_getshdr(scn, &shdr_mem);
 		if (!shdr)
 			return drgn_error_libelf();
+
+		const char *scnname = elf_strptr(file->elf, shstrndx, shdr->sh_name);
+		if (scnname && gnu_debugdata_ret && shdr->sh_type == SHT_PROGBITS
+		    && strcmp(".gnu_debugdata", scnname) == 0)
+			*gnu_debugdata_ret = scn;
 
 		if (shdr->sh_type == SHT_SYMTAB
 		    || shdr->sh_type == SHT_DYNSYM) {
@@ -44,6 +59,107 @@ static struct drgn_error *find_elf_file_symtab(struct drgn_elf_file *file,
 	}
 	return NULL;
 }
+
+#ifdef WITH_LZMA
+static struct drgn_error *
+drgn_error_lzma(lzma_ret code)
+{
+	switch (code) {
+		case LZMA_MEM_ERROR:
+			return &drgn_enomem;
+		case LZMA_OPTIONS_ERROR:
+			return drgn_error_format(DRGN_ERROR_INVALID_ARGUMENT,
+						 "lzma: invalid options");
+		case LZMA_FORMAT_ERROR:
+		case LZMA_DATA_ERROR:
+		case LZMA_BUF_ERROR:
+			return drgn_error_format(DRGN_ERROR_INVALID_ARGUMENT,
+						 "lzma: format error (%d)", code);
+		default:
+			return drgn_error_format(DRGN_ERROR_OTHER,
+						 "lzma: unknown error (%d)", code);
+	}
+}
+
+static struct drgn_error *
+load_gnu_debugdata_file(struct drgn_module *module, Elf_Scn *gnu_debugdata_scn,
+			struct drgn_elf_file **file_ret)
+{
+	Elf_Data *gnu_debugdata_data;
+	struct drgn_error *err;
+	err = read_elf_section(gnu_debugdata_scn, &gnu_debugdata_data);
+	if (err)
+		return err;
+
+	_cleanup_(lzma_end) lzma_stream stream = LZMA_STREAM_INIT;
+	lzma_ret ret = lzma_stream_decoder(&stream, UINT64_MAX, 0);
+	if (ret != LZMA_OK)
+		return drgn_error_lzma(ret);
+
+	stream.next_in = gnu_debugdata_data->d_buf;
+	stream.avail_in = gnu_debugdata_data->d_size;
+
+	// Use the input buffer size as the initial capacity. We'll expand it as
+	// needed.
+	size_t capacity = gnu_debugdata_data->d_size;
+	_cleanup_free_ void *data = malloc(capacity);
+	if (!data)
+		return &drgn_enomem;
+
+	stream.next_out = data;
+	stream.avail_out = capacity;
+
+	size_t bytes_decoded;
+	while (1) {
+		ret = lzma_code(&stream, LZMA_RUN);
+		if (ret != LZMA_OK && ret != LZMA_STREAM_END)
+			return drgn_error_lzma(ret);
+
+		bytes_decoded = (char *)stream.next_out - (char *)data;
+		if (ret == LZMA_STREAM_END) {
+			void *tmp = realloc(data, bytes_decoded);
+			if (tmp)
+				data = tmp;
+			break;
+		} else if (__builtin_mul_overflow(capacity, 2U, &capacity)) {
+			return &drgn_enomem;
+		} else {
+			void *tmp = realloc(data, capacity);
+			if (!tmp)
+				return &drgn_enomem;
+			data = tmp;
+			stream.next_out = (uint8_t *)data + bytes_decoded;
+			stream.avail_out = capacity - bytes_decoded;
+		}
+	}
+
+	STRING_BUILDER(path);
+	if (!string_builder_appendf(&path, "%s[.gnu_debugdata]", module->loaded_file->path)
+	    || !string_builder_null_terminate(&path))
+		return &drgn_enomem;
+
+	Elf *elf = elf_memory(data, bytes_decoded);
+	if (!elf)
+		return drgn_error_libelf();
+
+	err = drgn_elf_file_create(module, path.str, -1, data, elf, file_ret);
+	if (err)
+		elf_end(elf);
+	else
+		data = NULL;
+	return err;
+}
+#else
+static struct drgn_error *
+load_gnu_debugdata_file(struct drgn_module *module, Elf_Scn *gnu_debugdata_scn,
+			struct drgn_elf_file **ret)
+{
+	drgn_log_info(module->prog,
+		      "module \"%s\": .gnu_debugdata is available, but drgn was built without liblzma support",
+		      module->name);
+	return NULL;
+}
+#endif
 
 static struct drgn_error *
 set_elf_symtab(struct drgn_elf_symbol_table *symtab, struct drgn_elf_file *file,
@@ -90,6 +206,14 @@ set_elf_symtab(struct drgn_elf_symbol_table *symtab, struct drgn_elf_file *file,
 	return NULL;
 }
 
+static void
+cleanup_elf_file(struct drgn_elf_file **pfile)
+{
+	if (*pfile) {
+		drgn_elf_file_destroy(*pfile);
+	}
+}
+
 static struct drgn_error *
 find_module_elf_symtab(struct drgn_module *module)
 {
@@ -103,6 +227,16 @@ find_module_elf_symtab(struct drgn_module *module)
 		return NULL;
 	}
 
+	// The goal is to have the most complete symbol information, which we
+	// can get from the following, in order of preference:
+	// 1. A .symtab from the loaded or debug file (i.e. module->full_symtab
+	//    is true)
+	// 2. A .dynsym from the loaded file, as well as the .symtab from an
+	//    embedded .gnu_debugdata file. (The .gnu_debugdata usually only
+	//    contains complete function symbols, so we prefer #1 where
+	//    possible)
+	// 3. A .dynsym and no .gnu_debugdata
+
 	struct drgn_elf_file *file = NULL;
 	uint64_t bias;
 	Elf_Scn *symtab_scn;
@@ -113,24 +247,35 @@ find_module_elf_symtab(struct drgn_module *module)
 		err = find_elf_file_symtab(module->debug_file,
 					   module->debug_file_bias, &file,
 					   &bias, &symtab_scn, &strtab_idx,
-					   &num_local_symbols, &full_symtab);
+					   &num_local_symbols, &full_symtab,
+					   NULL);
 		if (err)
 			return err;
 	}
 
+	Elf_Scn *gnu_debugdata_scn = NULL;
 	if (!full_symtab &&
 	    (module->elf_symtab_pending_files & DRGN_MODULE_FILE_MASK_LOADED)) {
 		err = find_elf_file_symtab(module->loaded_file,
 					   module->loaded_file_bias, &file,
 					   &bias, &symtab_scn, &strtab_idx,
-					   &num_local_symbols, &full_symtab);
+					   &num_local_symbols, &full_symtab,
+					   &gnu_debugdata_scn);
 		if (err)
 			return err;
 	}
 
-	if (!file) {
+	if (!file && !gnu_debugdata_scn) {
 		drgn_log_debug(module->prog, "%s: no ELF symbol table",
 			       module->name);
+		module->elf_symtab_pending_files = 0;
+		return NULL;
+	}
+
+	// If we've found a dynamic symbol table, but we already saw a dynamic
+	// table, don't bother replacing it, unless the new file contains
+	// .gnu_debugdata (and thus the old one didn't).
+	if (module->elf_symtab.num_symbols && !full_symtab && !gnu_debugdata_scn) {
 		module->elf_symtab_pending_files = 0;
 		return NULL;
 	}
@@ -147,6 +292,55 @@ find_module_elf_symtab(struct drgn_module *module)
 			module->name, full_symtab ? "" : "dynamic ",
 			module->elf_symtab.num_symbols);
 	}
+
+	if (full_symtab && module->gnu_debugdata_symtab.num_symbols) {
+		// With a full symbol table (likely from the debug file), there
+		// is no need to keep around the gnu_debugdata symbol table.
+		// We cannot free the memory associated with it, because we may
+		// have returned symbols that refer to the strings in this file.
+		// Stop using the symbol table, but delay freeing until the
+		// program is freed.
+		memset(&module->gnu_debugdata_symtab, 0,
+		       sizeof(module->gnu_debugdata_symtab));
+	} else if (!full_symtab && gnu_debugdata_scn) {
+		// We only search for .gnu_debugdata in the loaded file, not the
+		// debug file. Once attached to a module, files can't be
+		// detached, so there should be no case where we load
+		// .gnu_debugdata twice. Let's assert that precondition here.
+		assert(module->gnu_debugdata_file == NULL);
+
+		_cleanup_(cleanup_elf_file) struct drgn_elf_file *gnu_debugdata_file = NULL;
+
+		err = load_gnu_debugdata_file(module, gnu_debugdata_scn,
+					      &gnu_debugdata_file);
+		if (err)
+			return err;
+
+		if (gnu_debugdata_file) {
+			file = NULL;
+			err = find_elf_file_symtab(gnu_debugdata_file,
+						   module->loaded_file_bias, &file,
+						   &bias, &symtab_scn, &strtab_idx,
+						   &num_local_symbols, &full_symtab,
+						   NULL);
+			if (err)
+				return err;
+
+			if (file) {
+				err = set_elf_symtab(&module->gnu_debugdata_symtab,
+						     file, bias, symtab_scn,
+						     strtab_idx, num_local_symbols);
+				if (err)
+					return err;
+
+				module->gnu_debugdata_file = no_cleanup_ptr(gnu_debugdata_file);
+				drgn_log_debug(module->prog,
+					"%s: found ELF .gnu_debugdata symbol table with %zu symbols",
+					module->name, module->gnu_debugdata_symtab.num_symbols);
+			}
+		}
+	}
+
 	module->elf_symtab_pending_files = 0;
 	return NULL;
 }
@@ -464,6 +658,13 @@ drgn_module_elf_symbols_search(struct drgn_module *module, const char *name,
 	if (module->elf_symtab.num_symbols) {
 		err = drgn_elf_symbol_table_search(&module->elf_symtab, name, addr,
 						   flags, &state, builder);
+		if (err)
+			return err;
+	}
+
+	if (module->gnu_debugdata_symtab.num_symbols) {
+		err = drgn_elf_symbol_table_search(&module->gnu_debugdata_symtab, name,
+						   addr, flags, &state, builder);
 		if (err)
 			return err;
 	}

libdrgn/Makefile.am
@@ -140,7 +140,7 @@ libdrgn_common_la_SOURCES = $(ARCH_DEFS_PYS:_defs.py=.c) \
 
 libdrgn_common_la_CFLAGS = $(AM_CFLAGS) -fvisibility=hidden $(OPENMP_CFLAGS) \
 			   $(elfutils_CFLAGS)
-libdrgn_common_la_LIBADD = $(OPENMP_LIBS) $(elfutils_LIBS) -lm
+libdrgn_common_la_LIBADD = $(OPENMP_LIBS) $(elfutils_LIBS) $(lzma_LIBS) -lm
 
 if WITH_DEBUGINFOD
 if ENABLE_DLOPEN_DEBUGINFOD

libdrgn/configure.ac
@@ -117,6 +117,19 @@ AS_CASE(["x$with_libkdumpfile"],
 AM_CONDITIONAL([WITH_LIBKDUMPFILE], [test "x$with_libkdumpfile" = xyes])
 AM_COND_IF([WITH_LIBKDUMPFILE], [AC_DEFINE(WITH_LIBKDUMPFILE)])
 
+AC_ARG_WITH([liblzma],
+	    [AS_HELP_STRING([--with-liblzma],
+			    [build with support for lzma decompression of ELF sections
+			     @<:@default=auto@:>@])],
+			     [], [with_liblzma=auto])
+AS_CASE(["x$with_liblzma"],
+	[xyes], [PKG_CHECK_MODULES(lzma, [liblzma])],
+	[xauto], [PKG_CHECK_MODULES(lzma, [liblzma],
+				    [with_liblzma=yes],
+				    [with_liblzma=no])])
+AM_CONDITIONAL([WITH_LZMA], [test "x$with_liblzma" != xno])
+AM_COND_IF([WITH_LZMA], [AC_DEFINE(WITH_LZMA)])
+
 dnl We need check for running tests, but we don't want to fail the build over
 dnl it. Instead, if it's not found, set variables so that only `make check`
 dnl fails.

libdrgn/debug_info.c
@@ -447,6 +447,7 @@ static void drgn_module_destroy(struct drgn_module *module)
 	drgn_module_orc_info_deinit(module);
 	drgn_module_dwarf_info_deinit(module);
 	drgn_module_clear_wanted_supplementary_debug_file(module);
+	drgn_elf_file_destroy(module->gnu_debugdata_file);
 	drgn_elf_file_destroy(module->supplementary_debug_file);
 	if (module->debug_file != module->loaded_file)
 		drgn_elf_file_destroy(module->debug_file);

libdrgn/debug_info.h
@@ -246,6 +246,7 @@ struct drgn_module {
 	struct drgn_elf_file *loaded_file;
 	struct drgn_elf_file *debug_file;
 	struct drgn_elf_file *supplementary_debug_file;
+	struct drgn_elf_file *gnu_debugdata_file;
 	/** Table mapping libdw handle to corresponding @ref drgn_elf_file. */
 	struct drgn_elf_file_dwarf_table split_dwarf_files;
 	uint64_t loaded_file_bias;
@@ -260,6 +261,8 @@ struct drgn_module {
 	struct drgn_module_orc_info orc;
 	/** ELF symbol table. */
 	struct drgn_elf_symbol_table elf_symtab;
+	/** Symbol table from the gnu_debugdata_file */
+	struct drgn_elf_symbol_table gnu_debugdata_symtab;
 
 	/** Whether .debug_frame has been parsed. */
 	bool parsed_debug_frame;

commit ff79328ea13ba0d3796e100eca5c35c1b8f240b3

libdrgn: elf_symtab: fix double-free in load_gnu_debugdata_file()
If either capacity (which came from gnu_debugdata_data->d_size) is 0 or
bytes_decoded is 0, realloc() is allowed to free data and return NULL,
at which point _cleanup_free_ will free data again. Guard against those
cases specifically.

Fixes: 683886d9bcf6 ("elf_symtab: add support for .gnu_debugdata")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/elf_symtab.c
@@ -96,6 +96,13 @@ load_gnu_debugdata_file(struct drgn_module *module, Elf_Scn *gnu_debugdata_scn,
 	if (err)
 		return err;
 
+	if (gnu_debugdata_data->d_size == 0) {
+		drgn_log_debug(module->prog,
+			       "%s: .gnu_debugdata is empty; ignoring",
+			       module->loaded_file->path);
+		return NULL;
+	}
+
 	_cleanup_(lzma_end) lzma_stream stream = LZMA_STREAM_INIT;
 	lzma_ret ret = lzma_stream_decoder(&stream, UINT64_MAX, 0);
 	if (ret != LZMA_OK)
@@ -122,6 +129,12 @@ load_gnu_debugdata_file(struct drgn_module *module, Elf_Scn *gnu_debugdata_scn,
 
 		bytes_decoded = (char *)stream.next_out - (char *)data;
 		if (ret == LZMA_STREAM_END) {
+			if (bytes_decoded == 0) {
+				drgn_log_debug(module->prog,
+					       "%s: .gnu_debugdata decompressed to empty; ignoring",
+					       module->loaded_file->path);
+				return NULL;
+			}
 			void *tmp = realloc(data, bytes_decoded);
 			if (tmp)
 				data = tmp;

tests/test_symbol.py
@@ -25,6 +25,7 @@ def create_elf_symbol_file(
     symbols=(),
     dynamic_symbols=(),
     gnu_debugdata_symbols=(),
+    gnu_debugdata_contents=None,
     dwarf=False,
     loadable=True,
 ):
@@ -35,6 +36,7 @@ def create_elf_symbol_file(
         return max(symbol.value + max(symbol.size, 1) for symbol in symbols)
 
     assert symbols or dynamic_symbols or gnu_debugdata_symbols
+    assert not gnu_debugdata_symbols or gnu_debugdata_contents is None
     start = float("inf")
     end = float("-inf")
     if symbols:
@@ -93,6 +95,15 @@ def create_elf_symbol_file(
                 sh_type=SHT.PROGBITS,
                 memsz=len(gds_compressed),
                 data=gds_compressed,
+            )
+        )
+
+    if gnu_debugdata_contents is not None:
+        sections.append(
+            ElfSection(
+                name=".gnu_debugdata",
+                sh_type=SHT.PROGBITS,
+                data=gnu_debugdata_contents,
             )
         )
 
@@ -762,6 +773,22 @@ class TestGnuDebugdata(TestCase):
                 if action != "combine":
                     self.assert_all_symbols_found_by_address(prog, expected)
                 self.assert_all_symbols_returned_by_lookup(prog, expected)
+
+    def _test_malformed(self, contents):
+        prog = Program()
+        program_add_elf_symbol_file(
+            prog,
+            "module0",
+            dynamic_symbols=[ElfSymbol("sym", 0xFFFF0000, 0x8, STT.FUNC, STB.LOCAL)],
+            gnu_debugdata_contents=contents,
+        )
+        self.assertIn("sym", [symbol.name for symbol in prog.symbols()])
+
+    def test_empty_section(self):
+        self._test_malformed(b"")
+
+    def test_decompresses_to_empty(self):
+        self._test_malformed(lzma.compress(b""))
 
 
 class TestSymbolFinder(TestCase):