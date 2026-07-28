osandov/drgn

commit 37c712c4d87eb6d4a47c9f359923881f8b58b12e

libdrgn: ignore ELF symbols in relocatable files with missing section address
ELF symbols in relocatable files need to be relocated relative to their
corresponding section address. Currently, if a section address was not
reported (with drgn_module_set_section_address()), then we use whatever
the sh_addr value was in the ELF section header, which isn't meaningful.
As a result, we're returning symbols with arbitrary addresses even
though they're essentially undefined.

In practice, this mainly affects symbols coming from the .init* sections
of Linux kernel modules. These sections are unloaded after the module is
initialized, so the symbols don't really exist anymore.

We already ignore undefined symbols, i.e., symbols with an st_shndx of
SHN_UNDEF. So, let's ignore symbols from sections with a missing
address, too. To do so, we track which sections have a valid address in
a bitmap and check that bitmap.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/elf_symtab.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "bitmap.h"
 #include "cleanup.h"
 #include "debug_info.h"
 #include "elf_file.h"
@@ -393,6 +394,15 @@ static bool elf_symbol_address(struct drgn_elf_symbol_table *symtab,
 		    && (shndx < SHN_LORESERVE || shndx > SHN_HIRESERVE)) {
 			Elf_Scn *scn = elf_getscn(symtab->file->elf, shndx);
 			if (!scn)
+				return false;
+			// If the section's address is not known, then the
+			// symbol is essentially undefined, so ignore it. This
+			// filters out things like symbols from the .init*
+			// sections in Linux kernel modules which are unloaded
+			// after initialization.
+			// NB: shndx is bounds-checked by elf_getscn().
+			if (!drgn_bitmap_test_bit(symtab->file->sections_with_address,
+						  shndx))
 				return false;
 			GElf_Shdr shdr_mem, *shdr = gelf_getshdr(scn,
 								 &shdr_mem);

libdrgn/debug_info.c
@@ -27,6 +27,7 @@
 #include "array.h"
 #include "binary_buffer.h"
 #include "binary_search.h"
+#include "bitmap.h"
 #include "cleanup.h"
 #include "crc32.h"
 #include "debug_info.h"
@@ -1167,26 +1168,38 @@ drgn_module_set_wanted_gnu_debugaltlink(struct drgn_module *module,
 	return NULL;
 }
 
-static bool
-drgn_module_copy_section_addresses(struct drgn_module *module, Elf *elf)
+static struct drgn_error *
+drgn_module_copy_section_addresses(struct drgn_module *module,
+				   struct drgn_elf_file *file)
 {
+	if (!file->is_relocatable)
+		return NULL;
+
+	size_t shdrnum;
+	if (elf_getshdrnum(file->elf, &shdrnum))
+		return drgn_error_libelf();
+
+	file->sections_with_address = drgn_bitmap_create(shdrnum);
+	if (!file->sections_with_address)
+		return &drgn_enomem;
+
 	if (drgn_module_section_address_map_empty(&module->section_addresses))
-		return true;
+		return NULL;
 
 	size_t shstrndx;
-	if (elf_getshdrstrndx(elf, &shstrndx))
-		return false;
+	if (elf_getshdrstrndx(file->elf, &shstrndx))
+		return drgn_error_libelf();
 
 	Elf_Scn *scn = NULL;
-	while ((scn = elf_nextscn(elf, scn))) {
+	while ((scn = elf_nextscn(file->elf, scn))) {
 		GElf_Shdr *shdr, shdr_mem;
 		shdr = gelf_getshdr(scn, &shdr_mem);
 		if (!shdr)
-			return false;
+			return drgn_error_libelf();
 
-		char *scnname = elf_strptr(elf, shstrndx, shdr->sh_name);
+		char *scnname = elf_strptr(file->elf, shstrndx, shdr->sh_name);
 		if (!scnname)
-			return false;
+			return drgn_error_libelf();
 
 		struct drgn_module_section_address_map_iterator it =
 			drgn_module_section_address_map_search(&module->section_addresses,
@@ -1196,9 +1209,11 @@ drgn_module_copy_section_addresses(struct drgn_module *module, Elf *elf)
 
 		shdr->sh_addr = it.entry->value;
 		if (!gelf_update_shdr(scn, shdr))
-			return false;
+			return drgn_error_libelf();
+		drgn_bitmap_set_bit(file->sections_with_address,
+				    elf_ndxscn(scn));
 	}
-	return true;
+	return NULL;
 }
 
 static bool elf_main_bias(struct drgn_program *prog, Elf *elf, uint64_t *ret)
@@ -1417,11 +1432,17 @@ drgn_module_maybe_use_elf_file(struct drgn_module *module,
 		}
 	}
 
-	if (file != module->loaded_file && file != module->debug_file
-	    && !drgn_module_copy_section_addresses(module, file->elf)) {
-		drgn_log_debug(prog, "%s: %s", file->path, elf_errmsg(-1));
-		err = NULL;
-		goto unused;
+	if (file != module->loaded_file && file != module->debug_file) {
+		err = drgn_module_copy_section_addresses(module, file);
+		if (err) {
+			if (!drgn_error_is_fatal(err)) {
+				drgn_error_log_debug(prog, err, "%s: ",
+						     file->path);
+				drgn_error_destroy(err);
+				err = NULL;
+			}
+			goto unused;
+		}
 	}
 
 	uint64_t bias;

libdrgn/elf_file.c
@@ -218,6 +218,7 @@ struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
 void drgn_elf_file_destroy(struct drgn_elf_file *file)
 {
 	if (file) {
+		free(file->sections_with_address);
 		dwarf_end(file->_dwarf);
 		elf_end(file->elf);
 		if (file->fd >= 0)

libdrgn/elf_file.h
@@ -119,6 +119,11 @@ struct drgn_elf_file {
 	 * `.debug_str` section data.
 	 */
 	Elf_Data *alt_debug_str_data;
+	/**
+	 * For relocatable files, a bitmap of which sections have their address
+	 * set.
+	 */
+	unsigned long *sections_with_address;
 };
 
 /**

commit 0cc6190d2f21a2b8767451b4b13549786e64fd4d

libdrgn: elf_symtab: reject ET_REL .gnu_debugdata
elf_symbol_address() checks the elf_file->sections_with_address bitmap
if elf_file->is_relocatable. That is populated by
drgn_module_copy_section_addresses(), which is never called for a
.gnu_debugdata file. As a result, an ET_REL .gnu_debugdata file with
symbols can cause a null pointer dereference. We could call
drgn_module_copy_section_addresses() on the .gnu_debugdata file, but it
doesn't make sense for it to be ET_REL in the first place (per the GDB
manual [1], it's supposed to be created from the final linked file).
Ignore such a file instead.

[1]: https://sourceware.org/gdb/current/onlinedocs/gdb.html/MiniDebugInfo.html#MiniDebugInfo

Fixes: 37c712c4d87e ("libdrgn: ignore ELF symbols in relocatable files with missing section address")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/elf_symtab.c
@@ -156,16 +156,30 @@ load_gnu_debugdata_file(struct drgn_module *module, Elf_Scn *gnu_debugdata_scn,
 	    || !string_builder_null_terminate(&path))
 		return &drgn_enomem;
 
-	Elf *elf = elf_memory(data, bytes_decoded);
+	_cleanup_elf_end_ Elf *elf = elf_memory(data, bytes_decoded);
 	if (!elf)
 		return drgn_error_libelf();
 
+	// Relocatable files must have their section addresses set. We could do
+	// that here, but it doesn't really make sense for a .gnu_debugdata file
+	// to be relocatable. Reject it early instead.
+	GElf_Ehdr ehdr_mem, *ehdr = gelf_getehdr(elf, &ehdr_mem);
+	if (!ehdr)
+		return drgn_error_libelf();
+	if (ehdr->e_type == ET_REL) {
+		drgn_log_debug(module->prog,
+			       "%s: .gnu_debugdata is relocatable; ignoring",
+			       module->loaded_file->path);
+		return NULL;
+	}
+
 	err = drgn_elf_file_create(module, path.str, -1, data, elf, file_ret);
 	if (err)
-		elf_end(elf);
-	else
-		data = NULL;
-	return err;
+		return err;
+	// Owned by *file_ret now.
+	data = NULL;
+	elf = NULL;
+	return NULL;
 }
 #else
 static struct drgn_error *

tests/test_symbol.py
@@ -790,6 +790,21 @@ class TestGnuDebugdata(TestCase):
     def test_decompresses_to_empty(self):
         self._test_malformed(lzma.compress(b""))
 
+    def test_relocatable(self):
+        reloc = create_elf_file(
+            ET.REL,
+            sections=[
+                ElfSection(
+                    name=".text",
+                    sh_type=SHT.PROGBITS,
+                    sh_flags=SHF.ALLOC,
+                    data=bytes(8),
+                ),
+            ],
+            symbols=[ElfSymbol("gds_sym", 0x0, 0x8, STT.FUNC, STB.GLOBAL, shindex=1)],
+        )
+        self._test_malformed(lzma.compress(reloc))
+
 
 class TestSymbolFinder(TestCase):
     TEST_SYMS = [