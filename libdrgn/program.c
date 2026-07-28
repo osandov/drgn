osandov/drgn

commit 61befc160601f00d99bfcd5887fa92b98d3ec9f8

libdrgn: parse AArch64 PAC mask from core dumps
In order to support removing the pointer authentication code (PAC) from
return addresses on AArch64, we need to know what bits are being used
for the PAC. We can get this from the NT_ARM_PAC_MASK note in userspace
core dumps and from the NUMBER(KERNELPACMASK) field in VMCOREINFO for
Linux kernel core dumps.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -232,7 +232,7 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 	struct drgn_error *err;
 	GElf_Ehdr ehdr_mem, *ehdr;
 	bool had_platform;
-	bool is_64_bit, is_kdump;
+	bool is_64_bit, little_endian, is_kdump;
 	size_t phnum, i;
 	size_t num_file_segments, j;
 	bool have_phys_addrs = false;
@@ -279,6 +279,7 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 		drgn_program_set_platform(prog, &platform);
 	}
 	is_64_bit = ehdr->e_ident[EI_CLASS] == ELFCLASS64;
+	little_endian = ehdr->e_ident[EI_DATA] == ELFDATA2LSB;
 
 	if (elf_getphdrnum(prog->core, &phnum) != 0) {
 		err = drgn_error_libelf();
@@ -330,6 +331,19 @@ drgn_program_set_core_dump(struct drgn_program *prog, const char *path)
 				    memcmp(name, "CORE", sizeof("CORE")) == 0) {
 					if (nhdr.n_type == NT_TASKSTRUCT)
 						have_nt_taskstruct = true;
+				} else if (nhdr.n_namesz == sizeof("LINUX") &&
+					   memcmp(name, "LINUX",
+						  sizeof("LINUX")) == 0) {
+					if (nhdr.n_type == NT_ARM_PAC_MASK &&
+					    nhdr.n_descsz >=
+					    2 * sizeof(uint64_t)) {
+						memcpy(&prog->aarch64_insn_pac_mask,
+						       (uint64_t *)desc + 1,
+						       sizeof(uint64_t));
+						if (little_endian !=
+						    HOST_LITTLE_ENDIAN)
+							bswap_64(prog->aarch64_insn_pac_mask);
+					}
 				} else if (nhdr.n_namesz == sizeof("VMCOREINFO") &&
 					   memcmp(name, "VMCOREINFO",
 						  sizeof("VMCOREINFO")) == 0) {
@@ -531,6 +545,7 @@ out_segments:
 	prog->file_segments = NULL;
 out_notes:
 	// Reset anything we parsed from ELF notes.
+	prog->aarch64_insn_pac_mask = 0;
 	memset(&prog->vmcoreinfo, 0, sizeof(prog->vmcoreinfo));
 out_platform:
 	prog->has_platform = had_platform;

libdrgn/drgn_program_parse_vmcoreinfo.inc.strswitch
@@ -66,6 +66,12 @@ struct drgn_error *drgn_program_parse_vmcoreinfo(struct drgn_program *prog,
 			prog->vmcoreinfo.pgtable_l5_enabled = tmp;
 			break;
 		}
+		@case "NUMBER(KERNELPACMASK)"@
+			err = parse_vmcoreinfo_u64(value, newline, 16,
+						   &prog->aarch64_insn_pac_mask);
+			if (err)
+				return err;
+			break;
 		@endswitch@
 	}
 	if (!prog->vmcoreinfo.osrelease[0]) {
@@ -80,6 +86,6 @@ struct drgn_error *drgn_program_parse_vmcoreinfo(struct drgn_program *prog,
 		return drgn_error_create(DRGN_ERROR_OTHER,
 					 "VMCOREINFO does not contain valid swapper_pg_dir");
 	}
-	/* KERNELOFFSET and pgtable_l5_enabled are optional. */
+	/* KERNELOFFSET, pgtable_l5_enabled, and KERNELPACMASK are optional. */
 	return NULL;
 }

libdrgn/program.h
@@ -134,6 +134,11 @@ struct drgn_program {
 	};
 	struct drgn_thread *main_thread;
 	struct drgn_thread *crashed_thread;
+	/*
+	 * AArch64 instruction pointer authentication code mask, parsed either
+	 * from NT_ARM_PAC_MASK or VMCOREINFO.
+	 */
+	uint64_t aarch64_insn_pac_mask;
 	bool core_dump_notes_cached;
 	bool prefer_orc_unwinder;
 

commit f9da3fe53f76aa41c03d676ba00b4e2fa2762b02

libdrgn: program: fix prog->aarch64_insn_pac_mask byte swap
We're not assigning the byte-swapped value anywhere.

Fixes: 61befc160601 ("libdrgn: parse AArch64 PAC mask from core dumps")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -445,7 +445,8 @@ drgn_program_set_core_dump_fd_internal(struct drgn_program *prog, int fd,
 						       sizeof(uint64_t));
 						if (little_endian !=
 						    HOST_LITTLE_ENDIAN)
-							bswap_64(prog->aarch64_insn_pac_mask);
+							prog->aarch64_insn_pac_mask =
+								bswap_64(prog->aarch64_insn_pac_mask);
 					}
 				} else if (nhdr.n_namesz == sizeof("VMCOREINFO") &&
 					   memcmp(name, "VMCOREINFO",