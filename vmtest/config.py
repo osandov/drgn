osandov/drgn

commit 0e939045a1a588a72c9c4a49d1c0855997a8d55d

ci: update to Noble / 24.04 LTS
libbpf on Jammy is 0.5.0, which is rather old. There are multiple
breaking chances in libbpf 1.0.0, in addition to the added support for
BTF_ENUM64 and a few other features of BTF. Given the upcoming use of
libbpf to support BTF, it would be nice to set a minimum version of
libbpf 1.0.0.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

.github/workflows/vmtest-build.yml
@@ -13,7 +13,7 @@ jobs:
         arch: [x86_64, aarch64, ppc64, s390x, arm, riscv64]
       fail-fast: false
       max-parallel: 6
-    runs-on: ubuntu-22.04
+    runs-on: ubuntu-24.04
     permissions:
       contents: write
     env:

.github/workflows/ci.yml
@@ -35,7 +35,7 @@ concurrency:
 
 jobs:
   test:
-    runs-on: ubuntu-22.04
+    runs-on: ubuntu-24.04
     strategy:
       matrix:
         python-version: ${{ (github.event_name == 'push' || inputs.test_all_python_versions)

commit bc9e020971ba855852ed11331603e6159a860a78

vmtest: roll vmtest-build back to Ubuntu 22.04
Ubuntu 24.04 uses glibc 2.39, so it produces kernel build tools (e.g.,
objtool) that are too new for my development machines on CentOS Stream
9, which has glibc 2.34. Roll back to Ubuntu 22.04.

Fixes: 0e939045a1a5 ("ci: update to Noble / 24.04 LTS")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

.github/workflows/vmtest-build.yml
@@ -13,7 +13,7 @@ jobs:
         arch: [x86_64, aarch64, ppc64, s390x, arm, riscv64]
       fail-fast: false
       max-parallel: 6
-    runs-on: ubuntu-24.04
+    runs-on: ubuntu-22.04
     permissions:
       contents: write
     env:

vmtest/config.py
@@ -561,7 +561,7 @@ def kconfig_localversion(arch: Architecture, flavor: KernelFlavor, version: str)
     vmtest_kernel_version = [
         # Increment the major version to rebuild every
         # architecture/flavor/version combination.
-        41,
+        42,
         # The minor version makes the default flavor the "latest" version.
         1 if flavor.name == "default" else 0,
     ]