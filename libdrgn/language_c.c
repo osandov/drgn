osandov/drgn

commit 243f6fb7d5677e570b60165433d8edb6118b9826

libdrgn: support value objects with >64-bit integer types
The Linux kernel's struct task_struct on AArch64 contains an array of
__uint128_t:

  >>> task = find_task(prog, 1)
  >>> task.type_
  struct task_struct *
  >>> task.thread.type_
  struct thread_struct {
          struct cpu_context cpu_context;
          struct {
                  unsigned long tp_value;
                  unsigned long tp2_value;
                  struct user_fpsimd_state fpsimd_state;
          } uw;
          enum fp_type fp_type;
          unsigned int fpsimd_cpu;
          void *sve_state;
          void *sme_state;
          unsigned int vl[2];
          unsigned int vl_onexec[2];
          unsigned long fault_address;
          unsigned long fault_code;
          struct debug_info debug;
          struct ptrauth_keys_user keys_user;
          struct ptrauth_keys_kernel keys_kernel;
          u64 mte_ctrl;
          u64 sctlr_user;
          u64 svcr;
          u64 tpidr2_el0;
  }
  >>> task.thread.uw.fpsimd_state.type_
  struct user_fpsimd_state {
          __int128 unsigned vregs[32];
          __u32 fpsr;
          __u32 fpcr;
          __u32 __reserved[2];
  }

As a result, printing a task_struct fails:

  >>> task
  Traceback (most recent call last):
    File "<console>", line 1, in <module>
    File "/host/home/osandov/repos/drgn3/drgn/cli.py", line 140, in _displayhook
      text = value.format_(columns=shutil.get_terminal_size((0, 0)).columns)
  NotImplementedError: integer values larger than 64 bits are not yet supported

PR #311 suggested treating >64-bit integers as byte arrays for now; I
tried an alternate hack of handling >64-bit integers only in the
pretty-printing code. Both of these had issues, though.

Instead, let's push >64-bit integer support a little further and allow
storing "big integer" value objects. We still don't support any
operations on them, so this still doesn't complete #170. We store the

libdrgn/language_c.c
@@ -690,30 +690,70 @@ c_format_int_object(const struct drgn_object *obj,
 		return NULL;
 	}
 
+	union drgn_value value_mem;
+	const union drgn_value *value;
+	err = drgn_object_read_value(obj, &value_mem, &value);
+	if (err)
+		return err;
 	switch (obj->encoding) {
-	case DRGN_OBJECT_ENCODING_SIGNED: {
-		int64_t svalue;
-
-		err = drgn_object_read_signed(obj, &svalue);
-		if (err)
-			return err;
-		if (!string_builder_appendf(sb, "%" PRId64, svalue))
-			return &drgn_enomem;
-		return NULL;
-	}
-	case DRGN_OBJECT_ENCODING_UNSIGNED: {
-		uint64_t uvalue;
-
-		err = drgn_object_read_unsigned(obj, &uvalue);
-		if (err)
-			return err;
-		if (!string_builder_appendf(sb, "%" PRIu64, uvalue))
-			return &drgn_enomem;
-		return NULL;
+	case DRGN_OBJECT_ENCODING_SIGNED:
+		if (!string_builder_appendf(sb, "%" PRId64, value->svalue)) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		break;
+	case DRGN_OBJECT_ENCODING_UNSIGNED:
+		if (!string_builder_appendf(sb, "%" PRIu64, value->uvalue)) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		break;
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+		if (!string_builder_append(sb, "0x")) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		const uint8_t *buf = (uint8_t *)value->bufp;
+		size_t bytes = drgn_object_size(obj);
+		if (obj->little_endian) {
+			size_t i = bytes - 1;
+			while (i > 0 && buf[i] == 0)
+				i--;
+			if (!string_builder_appendf(sb, "%" PRIx8, buf[i])) {
+				err = &drgn_enomem;
+				goto out;
+			}
+			while (i-- > 0) {
+				if (!string_builder_appendf(sb, "%02" PRIx8, buf[i])) {
+					err = &drgn_enomem;
+					goto out;
+				}
+			}
+		} else {
+			size_t i = 0;
+			while (i < bytes - 1 && buf[i] == 0)
+				i++;
+			if (!string_builder_appendf(sb, "%" PRIx8, buf[i])) {
+				err = &drgn_enomem;
+				goto out;
+			}
+			while (++i < bytes) {
+				if (!string_builder_appendf(sb, "%02" PRIx8, buf[i])) {
+					err = &drgn_enomem;
+					goto out;
+				}
+			}
+		}
+		break;
 	}
 	default:
 		UNREACHABLE();
 	}
+	err = NULL;
+out:
+	drgn_object_deinit_value(obj, value);
+	return err;
 }
 
 static struct drgn_error *

tests/test_language_c.py
@@ -2409,3 +2409,123 @@ class TestPrettyPrintObject(MockProgramTestCase):
             else:
                 type_name = type_
             self.assertEqual(str(Object(self.prog, type_)), f"({type_name})<absent>")
+
+    def test_bigint(self):
+        segment = bytearray(16)
+        self.add_memory_segment(segment, virt_addr=0xFFFF0000)
+
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                obj = Object(
+                    self.prog,
+                    self.prog.int_type("unsigned __int128", 16, False, byteorder),
+                    address=0xFFFF0000,
+                )
+                for value in (
+                    0,
+                    0x100,
+                    0x112233445566778899AABBCCDDEEFF,
+                    0xFEDCBA9876543210,
+                ):
+                    segment[:] = value.to_bytes(16, byteorder)
+                    self.assertEqual(str(obj), "(unsigned __int128)" + hex(value))
+
+    def test_bigint_in_array(self):
+        segment = bytearray(16 * 3)
+        self.add_memory_segment(segment, virt_addr=0xFFFF0000)
+
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                segment[:16] = (0x112233445566778899AABBCCDDEEFF).to_bytes(
+                    16, byteorder
+                )
+                segment[16:32] = (0xDEADBEEF).to_bytes(16, byteorder)
+                obj = Object(
+                    self.prog,
+                    self.prog.array_type(
+                        self.prog.int_type("unsigned __int128", 16, False, byteorder), 3
+                    ),
+                    address=0xFFFF0000,
+                )
+                self.assertEqual(
+                    str(obj),
+                    "(unsigned __int128 [3]){ 0x112233445566778899aabbccddeeff, 0xdeadbeef }",
+                )
+                self.assertEqual(
+                    obj.format_(implicit_elements=True),
+                    "(unsigned __int128 [3]){ 0x112233445566778899aabbccddeeff, 0xdeadbeef, 0x0 }",
+                )
+
+    def test_bigint_in_struct(self):
+        segment = bytearray(16 * 3)
+        self.add_memory_segment(segment, virt_addr=0xFFFF0000)
+
+        for byteorder in ("little", "big"):
+            with self.subTest(byteorder=byteorder):
+                segment[:16] = (0x112233445566778899AABBCCDDEEFF).to_bytes(
+                    16, byteorder
+                )
+                segment[16:32] = (0xDEADBEEF).to_bytes(16, byteorder)
+                type = self.prog.int_type("unsigned __int128", 16, False, byteorder)
+                obj = Object(
+                    self.prog,
+                    self.prog.struct_type(
+                        "foo",
+                        16 * 3,
+                        (
+                            TypeMember(type, "a"),
+                            TypeMember(type, "b", 16 * 8),
+                            TypeMember(type, "c", 16 * 8 * 2),
+                        ),
+                    ),
+                    address=0xFFFF0000,
+                )
+                self.assertEqual(
+                    str(obj),
+                    """\
+(struct foo){
+	.a = (unsigned __int128)0x112233445566778899aabbccddeeff,
+	.b = (unsigned __int128)0xdeadbeef,
+	.c = (unsigned __int128)0x0,
+}""",
+                )
+                self.assertEqual(
+                    obj.format_(implicit_members=False),
+                    """\
+(struct foo){
+	.a = (unsigned __int128)0x112233445566778899aabbccddeeff,
+	.b = (unsigned __int128)0xdeadbeef,
+}""",
+                )
+
+    def test_bigint_typedef(self):
+        self.add_memory_segment(
+            (0xDEADBEEF).to_bytes(16, "little"), virt_addr=0xFFFF0000
+        )
+        type = self.prog.typedef_type(
+            "__uint128_t", self.prog.int_type("unsigned __int128", 16, False)
+        )
+
+        self.assertEqual(
+            str(Object(self.prog, type, address=0xFFFF0000)),
+            "(__uint128_t)0xdeadbeef",
+        )
+
+        self.assertEqual(
+            str(Object(self.prog, self.prog.array_type(type, 1), address=0xFFFF0000)),
+            "(__uint128_t [1]){ 0xdeadbeef }",
+        )
+
+        self.assertEqual(
+            str(
+                Object(
+                    self.prog,
+                    self.prog.struct_type("foo", 16, (TypeMember(type, "a"),)),
+                    address=0xFFFF0000,
+                )
+            ),
+            """\
+(struct foo){
+	.a = (__uint128_t)0xdeadbeef,
+}""",
+        )

tests/test_object.py
@@ -144,7 +144,7 @@ class TestInit(MockProgramTestCase):
 def _int_bits_cases(prog):
     for signed in (True, False):
         for byteorder in ("little", "big"):
-            for bit_size in range(1, 65):
+            for bit_size in range(1, 129):
                 if bit_size <= 8:
                     size = 1
                 else:
@@ -263,8 +263,60 @@ class TestReference(MockProgramTestCase):
             bit_offset=7,
         )
 
+    def test_signed_big(self):
+        buffer = (-4).to_bytes(16, "little", signed=True)
+        self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
+        obj = Object(
+            self.prog,
+            self.prog.int_type("__int128", 16, True),
+            address=0xFFFF0000,
+        )
+        self.assertIs(obj.prog_, self.prog)
+        self.assertFalse(obj.absent_)
+        self.assertEqual(obj.address_, 0xFFFF0000)
+        self.assertEqual(obj.bit_offset_, 0)
+        self.assertIsNone(obj.bit_field_size_)
+        self.assertEqual(obj.type_.size, 16)
+        self.assertEqual(obj.value_(), -4)
+        self.assertEqual(obj.to_bytes_(), buffer)
+        self.assertEqual(repr(obj), "Object(prog, '__int128', address=0xffff0000)")
+
+        self.assertIdentical(
+            obj.read_(),
+            Object(self.prog, self.prog.int_type("__int128", 16, True), value=-4),
+        )
+
+    def test_unsigned_big(self):
+        buffer = (1000).to_bytes(16, "little")
+        self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
+        obj = Object(
+            self.prog,
+            self.prog.int_type("unsigned __int128", 16, False),
+            address=0xFFFF0000,
+        )
+        self.assertIs(obj.prog_, self.prog)
+        self.assertFalse(obj.absent_)
+        self.assertEqual(obj.address_, 0xFFFF0000)
+        self.assertEqual(obj.bit_offset_, 0)
+        self.assertIsNone(obj.bit_field_size_)
+        self.assertEqual(obj.type_.size, 16)
+        self.assertEqual(obj.value_(), 1000)
+        self.assertEqual(obj.to_bytes_(), buffer)
+        self.assertEqual(
+            repr(obj), "Object(prog, 'unsigned __int128', address=0xffff0000)"
+        )
+
+        self.assertIdentical(
+            obj.read_(),
+            Object(
+                self.prog,
+                self.prog.int_type("unsigned __int128", 16, False),
+                value=1000,
+            ),
+        )
+
     def test_int_bits(self):
-        buffer = bytearray(9)
+        buffer = bytearray(17)
         self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
         for (
             signed,
@@ -525,30 +577,6 @@ class TestReference(MockProgramTestCase):
             Object(self.prog, self.point_type, address=0xFFFF0004),
         )
 
-    def test_big_int(self):
-        buffer = (1000).to_bytes(16, "little")
-        self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
-        obj = Object(
-            self.prog,
-            self.prog.int_type("unsigned __int128", 16, False),
-            address=0xFFFF0000,
-        )
-        self.assertIs(obj.prog_, self.prog)
-        self.assertFalse(obj.absent_)
-        self.assertEqual(obj.address_, 0xFFFF0000)
-        self.assertEqual(obj.bit_offset_, 0)
-        self.assertIsNone(obj.bit_field_size_)
-        self.assertEqual(obj.type_.size, 16)
-        self.assertRaisesRegex(
-            NotImplementedError,
-            "integer values larger than 64 bits are not yet supported",
-            obj.value_,
-        )
-        self.assertEqual(obj.to_bytes_(), buffer)
-        self.assertEqual(
-            repr(obj), "Object(prog, 'unsigned __int128', address=0xffff0000)"
-        )
-
     def test_bit_field_of_big_int(self):
         buffer = (1000).to_bytes(4, "little")
         self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
@@ -643,18 +671,6 @@ class TestValue(MockProgramTestCase):
         self.assertEqual(obj.value_(), -8)
         self.assertEqual(repr(obj), "Object(prog, 'int', value=-8, bit_field_size=4)")
 
-        value = 12345678912345678989
-        for bit_size in range(1, 65):
-            tmp = value & ((1 << bit_size) - 1)
-            mask = 1 << (bit_size - 1)
-            tmp = (tmp ^ mask) - mask
-            self.assertEqual(
-                Object(
-                    self.prog, "long", value=value, bit_field_size=bit_size
-                ).value_(),
-                tmp,
-            )
-
     def test_unsigned(self):
         obj = Object(self.prog, "unsigned int", value=2**32 - 1)
         self.assertIs(obj.prog_, self.prog)
@@ -701,6 +717,113 @@ class TestValue(MockProgramTestCase):
                 ).value_(),
                 value & ((1 << bit_size) - 1),
             )
+
+    def _test_big_int_operators(self, type):
+        big_obj = Object(self.prog, type, 1000)
+        obj = Object(self.prog, "int", 0)
+        for op in (
+            operator.lt,
+            operator.le,
+            operator.eq,
+            operator.ge,
+            operator.gt,
+            operator.add,
+            operator.and_,
+            operator.lshift,
+            operator.mod,
+            operator.mul,
+            operator.or_,
+            operator.rshift,
+            operator.sub,
+            operator.truediv,
+            operator.xor,
+        ):
+            self.assertRaises(NotImplementedError, op, big_obj, obj)
+            self.assertRaises(NotImplementedError, op, obj, big_obj)
+
+        for op in (
+            operator.inv,
+            operator.neg,
+            operator.pos,
+        ):
+            self.assertRaises(NotImplementedError, op, big_obj)
+
+        self.assertFalse(not big_obj)
+        self.assertTrue(bool(big_obj))
+        for op in (
+            operator.index,
+            round,
+            math.trunc,
+            math.floor,
+            math.ceil,
+        ):
+            self.assertEqual(op(big_obj), 1000)
+
+    def test_signed_big(self):
+        type = self.prog.int_type("__int128", 16, True)
+        obj = Object(self.prog, type, -4)
+        self.assertIs(obj.prog_, self.prog)
+        self.assertIdentical(obj.type_, self.prog.int_type("__int128", 16, True))
+        self.assertFalse(obj.absent_)
+        self.assertIsNone(obj.address_)
+        self.assertIsNone(obj.bit_offset_)
+        self.assertIsNone(obj.bit_field_size_)
+        self.assertEqual(obj.value_(), -4)
+        self.assertEqual(repr(obj), "Object(prog, '__int128', value=-4)")
+
+        self.assertIdentical(Object(self.prog, type, value=2**128 - 4), obj)
+        self.assertIdentical(Object(self.prog, type, value=-4.6), obj)
+
+        self.assertIdentical(
+            Object(self.prog, type, value=2**128 + 4),
+            Object(self.prog, type, value=4),
+        )
+
+        self.assertRaisesRegex(
+            TypeError,
+            "'__int128' value must be number",
+            Object,
+            self.prog,
+            type,
+            value=b"asdf",
+        )
+
+        self._test_big_int_operators(type)
+
+    def test_unsigned_big(self):
+        type = self.prog.int_type("unsigned __int128", 16, False)
+        obj = Object(self.prog, type, 2**128 - 1)
+        self.assertIs(obj.prog_, self.prog)
+        self.assertIdentical(
+            obj.type_, self.prog.int_type("unsigned __int128", 16, False)
+        )
+        self.assertFalse(obj.absent_)
+        self.assertIsNone(obj.address_)
+        self.assertIsNone(obj.bit_offset_)
+        self.assertIsNone(obj.bit_field_size_)
+        self.assertEqual(obj.value_(), 2**128 - 1)
+        self.assertEqual(
+            repr(obj),
+            "Object(prog, 'unsigned __int128', value=340282366920938463463374607431768211455)",
+        )
+
+        self.assertIdentical(Object(self.prog, type, value=-1), obj)
+        self.assertIdentical(Object(self.prog, type, value=2**128 - 1), obj)
+        self.assertIdentical(Object(self.prog, type, value=2**129 - 1), obj)
+        self.assertIdentical(
+            Object(self.prog, type, value=0.1), Object(self.prog, type, value=0)
+        )
+
+        self.assertRaisesRegex(
+            TypeError,
+            "'unsigned __int128' value must be number",
+            Object,
+            self.prog,
+            type,
+            value="foo",
+        )
+
+        self._test_big_int_operators(type)
 
     def test_int_bits(self):
         for (
@@ -940,6 +1063,57 @@ class TestValue(MockProgramTestCase):
                     self.assertEqual(obj.a.value_(), 1234.0)
                     self.assertEqual(obj.b.value_(), -3.125)
 
+    def test_compound_bit_fields(self):
+        a = 0xF8935CF44C45202748DE66B49BA0CBAC
+        b = -0xC256D5AAFFDC3179A6AC84E7154A215D
+        for signed in (True, False):
+            if signed:
+
+                def truncate(x, bit_size):
+                    sign = 1 << (bit_size - 1)
+                    return (x & (sign - 1)) - (x & sign)
+
+            else:
+
+                def truncate(x, bit_size):
+                    return x & ((1 << bit_size) - 1)
+
+            for byteorder in ("little", "big"):
+                for bit_size in range(1, 128):
+                    with self.subTest(
+                        signed=signed, byteorder=byteorder, bit_size=bit_size
+                    ):
+                        type = self.prog.int_type(
+                            ("" if signed else "unsigned ") + "__int128", 16, signed
+                        )
+                        obj = Object(
+                            self.prog,
+                            self.prog.struct_type(
+                                None,
+                                type.size * 2,
+                                (
+                                    TypeMember(
+                                        Object(
+                                            self.prog, type, bit_field_size=bit_size
+                                        ),
+                                        "a",
+                                    ),
+                                    TypeMember(
+                                        Object(
+                                            self.prog,
+                                            type,
+                                            bit_field_size=128 - bit_size,
+                                        ),
+                                        "b",
+                                        bit_size,
+                                    ),
+                                ),
+                            ),
+                            value={"a": a, "b": b},
+                        )
+                        self.assertEqual(obj.a.value_(), truncate(a, bit_size))
+                        self.assertEqual(obj.b.value_(), truncate(b, 128 - bit_size))
+
     def test_pointer(self):
         obj = Object(self.prog, "int *", value=0xFFFF0000)
         self.assertFalse(obj.absent_)
@@ -995,29 +1169,7 @@ class TestValue(MockProgramTestCase):
             ValueError, "non-scalar must be byte-aligned", obj.member_, "point"
         )
 
-    def test_big_int(self):
-        for type in [
-            self.prog.int_type("unsigned __int128", 16, False),
-            self.prog.int_type("__int128", 16, True),
-        ]:
-            self.assertRaisesRegex(
-                NotImplementedError,
-                "integer values larger than 64 bits are not yet supported",
-                Object,
-                self.prog,
-                type,
-                0,
-            )
-            self.assertRaisesRegex(
-                NotImplementedError,
-                "integer values larger than 64 bits are not yet supported",
-                Object.from_bytes_,
-                self.prog,
-                type,
-                (0).to_bytes(16, "little"),
-            )
-
-    def test_bit_field_of_big_int(self):
+    def test_small_bit_field_of_big_int(self):
         obj = Object(
             self.prog,
             self.prog.int_type("unsigned __int128", 16, False),

commit dcae5c7f4d434f204a3ecd7c63d85ce78a1b2a24

libdrgn: fix >64-bit pointer object handling
drgn_object_type_impl() always assigns DRGN_TYPE_POINTER the
DRGN_OBJECT_ENCODING_UNSIGNED encoding. If the object is larger than 64
bits, this breaks our assumptions. Specifically,
drgn_object_read_reference() uses an on-stack char buf[9] for
DRGN_OBJECT_ENCODING_UNSIGNED. Passing it a >64-bit pointer object will
abort on the assert(read_size <= sizeof(buf)) or overflow the buffer
depending on NDEBUG.

Fix drgn_object_type_impl() to use DRGN_OBJECT_ENCODING_UNSIGNED_BIG for
>64-bit pointer objects and handle formatting such objects. The latter
should be the only other fixup needed to be at parity with >64-bit
integer functionality.

Fixes: 243f6fb7d567 ("libdrgn: support value objects with >64-bit integer types")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/language_c.c
@@ -1633,8 +1633,9 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 	 * Pointers are special because they can have an asterisk prefix if
 	 * we're dereferencing them.
 	 */
-	if (drgn_type_kind(underlying_type) == DRGN_TYPE_POINTER &&
-	    obj->kind != DRGN_OBJECT_ABSENT) {
+	if (drgn_type_kind(underlying_type) == DRGN_TYPE_POINTER
+	    && obj->kind != DRGN_OBJECT_ABSENT
+	    && obj->encoding == DRGN_OBJECT_ENCODING_UNSIGNED) {
 		return c_format_pointer_object(obj, underlying_type, indent,
 					       one_line_columns,
 					       multi_line_columns, options, sb);
@@ -1670,6 +1671,7 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 					 "cannot format void object");
 	case DRGN_TYPE_INT:
 	case DRGN_TYPE_BOOL:
+	case DRGN_TYPE_POINTER:
 		return c_format_int_object(obj, options, sb);
 	case DRGN_TYPE_FLOAT:
 		return c_format_float_object(obj, sb);
@@ -1689,7 +1691,6 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 	case DRGN_TYPE_FUNCTION:
 		return c_format_function_object(obj, sb);
 	case DRGN_TYPE_TYPEDEF:
-	case DRGN_TYPE_POINTER:
 	default:
 		UNREACHABLE();
 	}

libdrgn/object.c
@@ -137,7 +137,10 @@ drgn_object_type_impl(struct drgn_type *type, struct drgn_type *underlying_type,
 		break;
 	}
 	case DRGN_TYPE_POINTER:
-		ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED;
+		if (ret->bit_size <= 64)
+			ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED;
+		else
+			ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED_BIG;
 		break;
 	case DRGN_TYPE_FLOAT:
 		if (ret->bit_size < 1 || ret->bit_size > 256) {

tests/test_language_c.py
@@ -3420,6 +3420,16 @@ class TestPrettyPrintObject(MockProgramTestCase):
 }""",
         )
 
+    def test_big_pointer(self):
+        self.assertEqual(
+            Object(
+                self.prog,
+                self.prog.pointer_type(self.prog.void_type(), size=16),
+                0x0123456789ABCDEFFEDCBA9876543210,
+            ).format_(),
+            "(void *)0x123456789abcdeffedcba9876543210",
+        )
+
     def test_integer_base_array(self):
         self.assertEqual(
             Object(self.prog, "int [2]", [0, 13]).format_(integer_base=16),

tests/test_object.py
@@ -347,6 +347,17 @@ class TestReference(MockProgramTestCase):
             ),
         )
 
+    def test_big_pointer(self):
+        value = 0x0123456789ABCDEFFEDCBA9876543210
+        self.add_memory_segment(value.to_bytes(16, "little"), virt_addr=0xFFFF0000)
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.void_type(), size=16),
+            address=0xFFFF0000,
+        )
+        self.assertEqual(obj.value_(), value)
+        self.assertEqual(repr(obj), "Object(prog, 'void *', address=0xffff0000)")
+
     def test_int_bits(self):
         buffer = bytearray(17)
         self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
@@ -861,6 +872,16 @@ class TestValue(MockProgramTestCase):
         )
 
         self._test_big_int_operators(type)
+
+    def test_big_pointer(self):
+        value = 0x0123456789ABCDEFFEDCBA9876543210
+        obj = Object(
+            self.prog, self.prog.pointer_type(self.prog.void_type(), size=16), value
+        )
+        self.assertEqual(obj.value_(), value)
+        self.assertEqual(
+            repr(obj), "Object(prog, 'void *', value=0x123456789abcdeffedcba9876543210)"
+        )
 
     def test_int_bits(self):
         for (
@@ -1800,6 +1821,15 @@ class TestGenericOperators(MockProgramTestCase):
         obj = arr.read_()
         self.assertRaisesRegex(OutOfBoundsError, "out of bounds", obj.__getitem__, -1)
 
+    def test_big_pointer_subscript(self):
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.int_type("int", 4, True), size=16),
+            0x0123456789ABCDEFFEDCBA9876543210,
+        )
+        with self.assertRaises(OverflowError):
+            obj[0]
+
     def test_slice(self):
         arr = Object(self.prog, "int [4]", address=0xFFFF0000)
         incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
@@ -1914,6 +1944,15 @@ class TestGenericOperators(MockProgramTestCase):
             )
 
         self.assertIdentical(arr.read_()[:-2], Object(self.prog, "int [0]", []))
+
+    def test_big_pointer_slice(self):
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.int_type("int", 4, True), size=16),
+            0x0123456789ABCDEFFEDCBA9876543210,
+        )
+        with self.assertRaises(OverflowError):
+            obj[0:2]
 
     def test_cast_primitive_value(self):
         obj = Object(self.prog, "long", value=2**32 + 1)