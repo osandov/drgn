osandov/drgn

commit 9fecb2728322f4df7eb7d367597a74b5c4879a70

Add drgn.Program.stack_trace_from_pcs()
This function is useful when you only have a list of PCs
and not the full stack trace, for example when working with
the stack depot.

Signed-off-by: Peter Collingbourne <pcc@google.com>

libdrgn/stack_trace.c
@@ -1131,6 +1131,37 @@ drgn_program_stack_trace(struct drgn_program *prog, uint32_t tid,
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_stack_trace_from_pcs(struct drgn_program *prog, const uint64_t *pcs,
+				  size_t pcs_size, struct drgn_stack_trace **ret)
+{
+	struct drgn_stack_trace *trace = malloc_flexible_array(
+		struct drgn_stack_trace, frames, pcs_size);
+	struct drgn_error *err;
+	size_t trace_capacity = pcs_size;
+
+	if (!trace)
+		return &drgn_enomem;
+
+	trace->prog = prog;
+	trace->num_frames = 0;
+	for (size_t i = 0; i != pcs_size; ++i) {
+		struct drgn_register_state *regs =
+			drgn_register_state_create_impl(0, 0, false);
+		drgn_register_state_set_pc(prog, regs, pcs[i]);
+
+		err = drgn_stack_trace_add_frames(&trace, &trace_capacity, regs);
+		if (err) {
+			drgn_stack_trace_destroy(trace);
+			return err;
+		}
+	}
+
+	drgn_stack_trace_shrink_to_fit(&trace, trace_capacity);
+	*ret = trace;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
 drgn_object_stack_trace(const struct drgn_object *obj,
 			struct drgn_stack_trace **ret)
 {

_drgn.pyi
@@ -271,6 +271,13 @@ class Program:
             ``struct task_struct *`` object.
         """
         ...
+    def stack_trace_from_pcs(self, pcs: Sequence[IntegerLike]) -> StackTrace:
+        """
+        Get a stack trace with the supplied list of program counters.
+
+        :param pcs: List of program counters.
+        """
+        ...
     @overload
     def type(self, name: str, filename: Optional[str] = None) -> Type:
         """

libdrgn/drgn.h
@@ -3095,6 +3095,19 @@ struct drgn_error *drgn_program_stack_trace(struct drgn_program *prog,
 					    struct drgn_stack_trace **ret);
 
 /**
+ * Get a stack trace with the supplied list of program counters.
+ *
+ * @param[out] ret Returned stack trace. On success, it should be freed with
+ * @ref drgn_stack_trace_destroy(). On error, its contents are undefined.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_program_stack_trace_from_pcs(struct drgn_program *prog,
+				  const uint64_t *pcs,
+				  size_t pcs_size,
+				  struct drgn_stack_trace **ret);
+
+/**
  * Get a stack trace for the thread represented by @p obj.
  *
  * @sa drgn_program_stack_trace().

libdrgn/python/program.c
@@ -864,6 +864,43 @@ static PyObject *Program_stack_trace(Program *self, PyObject *args,
 	return ret;
 }
 
+static PyObject *Program_stack_trace_from_pcs(Program *self, PyObject *args,
+					      PyObject *kwds)
+{
+	static char *keywords[] = {"pcs", NULL};
+	struct drgn_error *err;
+	PyObject *pypcs;
+	struct drgn_stack_trace *trace;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O:stack_trace_from_pcs",
+					 keywords, &pypcs))
+		return NULL;
+
+	_cleanup_pydecref_ PyObject *pypcseq = PySequence_Fast(
+		pypcs, "stack_trace_from_pcs() argument 1 must be a list");
+	if (!pypcseq)
+		return NULL;
+
+	size_t size = PySequence_Fast_GET_SIZE(pypcseq);
+	_cleanup_free_ uint64_t *pcs = malloc_array(size, sizeof(uint64_t));
+	for (uint64_t i = 0; i != size; ++i) {
+		struct index_arg pc = {};
+
+		if (!index_converter(PySequence_Fast_GET_ITEM(pypcseq, i), &pc))
+			return NULL;
+		pcs[i] = pc.uvalue;
+	}
+
+	err = drgn_program_stack_trace_from_pcs(&self->prog, pcs, size, &trace);
+	if (err)
+		return set_drgn_error(err);
+
+	PyObject *ret = StackTrace_wrap(trace);
+	if (!ret)
+		drgn_stack_trace_destroy(trace);
+	return ret;
+}
+
 static PyObject *Program_symbols(Program *self, PyObject *args)
 {
 	struct drgn_error *err;
@@ -1158,6 +1195,8 @@ static PyMethodDef Program_methods[] = {
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_variable_DOC},
 	{"stack_trace", (PyCFunction)Program_stack_trace,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_stack_trace_DOC},
+	{"stack_trace_from_pcs", (PyCFunction)Program_stack_trace_from_pcs,
+	 METH_VARARGS | METH_KEYWORDS, drgn_Program_stack_trace_from_pcs_DOC},
 	{"symbols", (PyCFunction)Program_symbols, METH_VARARGS,
 	 drgn_Program_symbols_DOC},
 	{"symbol", (PyCFunction)Program_symbol, METH_O,

commit 937dd0521706b163c5dbe40bce24d6903509d8ac

libdrgn: stack_trace: add missing drgn_register_state_create_impl() failure checks
drgn_program_stack_trace_from_pcs() and drgn_program_source_location()
aren't checking whether drgn_register_state_create_impl() returned NULL.

Fixes: 9fecb2728322 ("Add drgn.Program.stack_trace_from_pcs()")
Fixes: 24e11807842d ("Add drgn.Program.source_location()/drgn_program_source_location()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/stack_trace.c
@@ -1375,6 +1375,8 @@ drgn_program_stack_trace_from_pcs(struct drgn_program *prog, const uint64_t *pcs
 	for (size_t i = 0; i != pcs_size; ++i) {
 		struct drgn_register_state *regs =
 			drgn_register_state_create_impl(0, 0, false);
+		if (!regs)
+			return &drgn_enomem;
 		drgn_register_state_set_pc(prog, regs, pcs[i]);
 
 		err = drgn_stack_trace_add_frames(&trace, &trace_capacity, regs);
@@ -1519,6 +1521,8 @@ drgn_program_source_location(struct drgn_program *prog, uint64_t address,
 
 	struct drgn_register_state *regs =
 		drgn_register_state_create_impl(0, 0, true);
+	if (!regs)
+		return &drgn_enomem;
 	drgn_register_state_set_pc(prog, regs, address);
 	err = drgn_stack_trace_add_frames(&trace, &trace_capacity, regs);
 	if (err)