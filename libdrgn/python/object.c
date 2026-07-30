osandov/drgn

commit 243f6fb7d5677e570b60165433d8edb6118b9826

libdrgn: support value objects with >64-bit integer types
The Linux kernel's struct task_struct on AArch64 contains an array of
__uint128_t:

  >>> task = find_task(prog, 1)
  >>> task.type_
  struct task_struct *
  >>> task.thread.type_
  struct thread_struct {
          struct cpu_context cpu_context;
          struct {
                  unsigned long tp_value;
                  unsigned long tp2_value;
                  struct user_fpsimd_state fpsimd_state;
          } uw;
          enum fp_type fp_type;
          unsigned int fpsimd_cpu;
          void *sve_state;
          void *sme_state;
          unsigned int vl[2];
          unsigned int vl_onexec[2];
          unsigned long fault_address;
          unsigned long fault_code;
          struct debug_info debug;
          struct ptrauth_keys_user keys_user;
          struct ptrauth_keys_kernel keys_kernel;
          u64 mte_ctrl;
          u64 sctlr_user;
          u64 svcr;
          u64 tpidr2_el0;
  }
  >>> task.thread.uw.fpsimd_state.type_
  struct user_fpsimd_state {
          __int128 unsigned vregs[32];
          __u32 fpsr;
          __u32 fpcr;
          __u32 __reserved[2];
  }

As a result, printing a task_struct fails:

  >>> task
  Traceback (most recent call last):
    File "<console>", line 1, in <module>
    File "/host/home/osandov/repos/drgn3/drgn/cli.py", line 140, in _displayhook
      text = value.format_(columns=shutil.get_terminal_size((0, 0)).columns)
  NotImplementedError: integer values larger than 64 bits are not yet supported

PR #311 suggested treating >64-bit integers as byte arrays for now; I
tried an alternate hack of handling >64-bit integers only in the
pretty-printing code. Both of these had issues, though.

Instead, let's push >64-bit integer support a little further and allow
storing "big integer" value objects. We still don't support any
operations on them, so this still doesn't complete #170. We store the

libdrgn/python/object.c
@@ -5,6 +5,7 @@
 #include <math.h>
 
 #include "drgnpy.h"
+#include "../cleanup.h"
 #include "../error.h"
 #include "../object.h"
 #include "../serialize.h"
@@ -48,6 +49,43 @@ static int DrgnObject_literal(struct drgn_object *res, PyObject *literal)
 		return -1;
 	}
 	return 0;
+}
+
+static void *
+py_long_to_bytes_for_object_type(PyObject *value_obj,
+				 const struct drgn_object_type *type)
+{
+	if (!PyNumber_Check(value_obj)) {
+		return set_error_type_name("'%s' value must be number",
+					   drgn_object_type_qualified(type));
+	}
+	_cleanup_pydecref_ PyObject *long_obj = PyNumber_Long(value_obj);
+	if (!long_obj)
+		return NULL;
+	uint64_t size = drgn_value_size(type->bit_size);
+	_cleanup_free_ void *buf = malloc64(size);
+	if (!buf) {
+		PyErr_NoMemory();
+		return NULL;
+	}
+	// _PyLong_AsByteArray() still returns the least significant bytes on
+	// OverflowError unless the object is negative and is_signed is false.
+	// So, we always pass is_signed as true.
+	int r = _PyLong_AsByteArray((PyLongObject *)long_obj, buf, size,
+				    type->little_endian, true);
+	if (r) {
+		PyObject *exc_type, *exc_value, *exc_traceback;
+		PyErr_Fetch(&exc_type, &exc_value, &exc_traceback);
+		if (PyErr_GivenExceptionMatches(exc_type, PyExc_OverflowError)) {
+			Py_XDECREF(exc_traceback);
+			Py_XDECREF(exc_value);
+			Py_DECREF(exc_type);
+		} else {
+			PyErr_Restore(exc_type, exc_value, exc_traceback);
+			return NULL;
+		}
+	}
+	return_ptr(buf);
 }
 
 static int serialize_py_object(struct drgn_program *prog, char *buf,
@@ -220,6 +258,19 @@ static int serialize_py_object(struct drgn_program *prog, char *buf,
 			       type->little_endian);
 		return 0;
 	}
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+		_cleanup_free_ void *tmp =
+			py_long_to_bytes_for_object_type(value_obj, type);
+		if (!tmp)
+			return -1;
+		int src_bit_offset = 0;
+		if (!type->little_endian)
+			src_bit_offset = -type->bit_size % 8;
+		copy_bits(buf + bit_offset / 8, bit_offset % 8, tmp,
+			  src_bit_offset, type->bit_size, type->little_endian);
+		return 0;
+	}
 	case DRGN_OBJECT_ENCODING_FLOAT: {
 		if (!PyNumber_Check(value_obj)) {
 			set_error_type_name("'%s' value must be number",
@@ -270,19 +321,10 @@ static int serialize_py_object(struct drgn_program *prog, char *buf,
 }
 
 static int buffer_object_from_value(struct drgn_object *res,
-				    struct drgn_qualified_type qualified_type,
+				    const struct drgn_object_type *type,
 				    PyObject *value_obj)
 {
-	struct drgn_error *err;
-
-	struct drgn_object_type type;
-	err = drgn_object_type(qualified_type, 0, &type);
-	if (err) {
-		set_drgn_error(err);
-		return -1;
-	}
-
-	uint64_t size = drgn_value_size(type.bit_size);
+	uint64_t size = drgn_value_size(type->bit_size);
 	if (size > SIZE_MAX) {
 		PyErr_NoMemory();
 		return -1;
@@ -301,14 +343,14 @@ static int buffer_object_from_value(struct drgn_object *res,
 	}
 	memset(buf, 0, size);
 
-	if (serialize_py_object(drgn_object_program(res), buf, type.bit_size, 0,
-				value_obj, &type) == -1) {
+	if (serialize_py_object(drgn_object_program(res), buf, type->bit_size,
+				0, value_obj, type) == -1) {
 		if (buf != value.ibuf)
 			free(buf);
 		return -1;
 	}
 
-	drgn_object_reinit(res, &type, DRGN_OBJECT_VALUE);
+	drgn_object_reinit(res, type, DRGN_OBJECT_VALUE);
 	res->value = value;
 	return 0;
 }
@@ -392,25 +434,15 @@ static DrgnObject *DrgnObject_new(PyTypeObject *subtype, PyObject *args,
 			return NULL;
 		}
 
-		enum drgn_object_encoding encoding =
-			drgn_type_object_encoding(qualified_type.type);
-		if (!drgn_object_encoding_is_complete(encoding)) {
-			err = drgn_error_incomplete_type("cannot create value with %s type",
-							 qualified_type.type);
+		struct drgn_object_type object_type;
+		err = drgn_object_type(qualified_type, bit_field_size.uvalue,
+				       &object_type);
+		if (err)
 			return set_drgn_error(err);
 
-		}
-		if (!bit_field_size.is_none &&
-		    encoding != DRGN_OBJECT_ENCODING_SIGNED &&
-		    encoding != DRGN_OBJECT_ENCODING_UNSIGNED) {
-			PyErr_SetString(PyExc_ValueError,
-					"bit field must be integer");
-			return NULL;
-		}
-
-		switch (encoding) {
+		SWITCH_ENUM(object_type.encoding,
 		case DRGN_OBJECT_ENCODING_BUFFER:
-			if (buffer_object_from_value(&obj->obj, qualified_type,
+			if (buffer_object_from_value(&obj->obj, &object_type,
 						     value_obj) == -1)
 				return NULL;
 			err = NULL;
@@ -433,17 +465,31 @@ static DrgnObject *DrgnObject_new(PyTypeObject *subtype, PyObject *args,
 			};
 			if (tmp.uvalue == (uint64_t)-1 && PyErr_Occurred())
 				return NULL;
-			if (encoding == DRGN_OBJECT_ENCODING_SIGNED) {
-				err = drgn_object_set_signed(&obj->obj,
-							     qualified_type,
-							     tmp.svalue,
-							     bit_field_size.uvalue);
+			if (object_type.encoding == DRGN_OBJECT_ENCODING_SIGNED) {
+				err = drgn_object_set_signed_internal(&obj->obj,
+								      &object_type,
+								      tmp.svalue);
 			} else {
-				err = drgn_object_set_unsigned(&obj->obj,
-							       qualified_type,
-							       tmp.uvalue,
-							       bit_field_size.uvalue);
+				err = drgn_object_set_unsigned_internal(&obj->obj,
+									&object_type,
+									tmp.uvalue);
 			}
+			break;
+		}
+		case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+		case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+			_cleanup_free_ void *tmp =
+				py_long_to_bytes_for_object_type(value_obj,
+								 &object_type);
+			if (!tmp)
+				return NULL;
+			uint64_t src_bit_offset = 0;
+			if (!object_type.little_endian)
+				src_bit_offset = -object_type.bit_size % 8;
+			err = drgn_object_set_from_buffer_internal(&obj->obj,
+								   &object_type,
+								   tmp,
+								   src_bit_offset);
 			break;
 		}
 		case DRGN_OBJECT_ENCODING_FLOAT: {
@@ -454,13 +500,18 @@ static DrgnObject *DrgnObject_new(PyTypeObject *subtype, PyObject *args,
 			double fvalue = PyFloat_AsDouble(value_obj);
 			if (fvalue == -1.0 && PyErr_Occurred())
 				return NULL;
-			err = drgn_object_set_float(&obj->obj, qualified_type,
-						    fvalue);
+			err = drgn_object_set_float_internal(&obj->obj,
+							     &object_type,
+							     fvalue);
 			break;
 		}
-		default:
-			UNREACHABLE();
-		}
+		case DRGN_OBJECT_ENCODING_NONE:
+		case DRGN_OBJECT_ENCODING_INCOMPLETE_BUFFER:
+		case DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER:
+			err = drgn_error_incomplete_type("cannot create value with %s type",
+							 qualified_type.type);
+			break;
+		)
 	} else {
 		if (!qualified_type.type) {
 			PyErr_SetString(PyExc_ValueError,
@@ -636,6 +687,19 @@ static PyObject *DrgnObject_value_impl(struct drgn_object *obj)
 			Py_RETURN_BOOL(uvalue);
 		else
 			return PyLong_FromUint64(uvalue);
+	}
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+		union drgn_value value_mem;
+		const union drgn_value *value;
+		err = drgn_object_read_value(obj, &value_mem, &value);
+		if (err)
+			return set_drgn_error(err);
+		return _PyLong_FromByteArray((void *)value->bufp,
+					     drgn_object_size(obj),
+					     obj->little_endian,
+					     obj->encoding
+					     == DRGN_OBJECT_ENCODING_SIGNED_BIG);
 	}
 	case DRGN_OBJECT_ENCODING_FLOAT: {
 		double fvalue;
@@ -1104,100 +1168,79 @@ static int DrgnObject_bool(DrgnObject *self)
 static PyObject *DrgnObject_int(DrgnObject *self)
 {
 	struct drgn_error *err;
-	union drgn_value value_mem;
-	const union drgn_value *value;
-	PyObject *ret;
-
-	if (!drgn_type_is_scalar(self->obj.type)) {
+	SWITCH_ENUM(self->obj.encoding,
+	case DRGN_OBJECT_ENCODING_SIGNED:
+	case DRGN_OBJECT_ENCODING_UNSIGNED:
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG:
+		return DrgnObject_value(self);
+	case DRGN_OBJECT_ENCODING_FLOAT: {
+		double fvalue;
+		err = drgn_object_read_float(&self->obj, &fvalue);
+		if (err)
+			return set_drgn_error(err);
+		return PyLong_FromDouble(fvalue);
+	}
+	case DRGN_OBJECT_ENCODING_BUFFER:
+	case DRGN_OBJECT_ENCODING_NONE:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_BUFFER:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER:
 		return set_error_type_name("cannot convert '%s' to int",
 					   drgn_object_qualified_type(&self->obj));
-	}
-
-	err = drgn_object_read_value(&self->obj, &value_mem, &value);
-	if (err)
-		return set_drgn_error(err);
-
-	switch (self->obj.encoding) {
-	case DRGN_OBJECT_ENCODING_SIGNED:
-		ret = PyLong_FromInt64(value->svalue);
-		break;
-	case DRGN_OBJECT_ENCODING_UNSIGNED:
-		ret = PyLong_FromUint64(value->uvalue);
-		break;
-	case DRGN_OBJECT_ENCODING_FLOAT:
-		ret = PyLong_FromDouble(value->fvalue);
-		break;
-	default:
-		UNREACHABLE();
-	}
-	drgn_object_deinit_value(&self->obj, value);
-	return ret;
+	)
 }
 
 static PyObject *DrgnObject_float(DrgnObject *self)
 {
 	struct drgn_error *err;
-	union drgn_value value_mem;
-	const union drgn_value *value;
-	PyObject *ret;
-
-	if (!drgn_type_is_arithmetic(self->obj.type)) {
+	SWITCH_ENUM(self->obj.encoding,
+	case DRGN_OBJECT_ENCODING_FLOAT: {
+		double fvalue;
+		err = drgn_object_read_float(&self->obj, &fvalue);
+		if (err)
+			return set_drgn_error(err);
+		return PyFloat_FromDouble(fvalue);
+	}
+	case DRGN_OBJECT_ENCODING_SIGNED:
+	case DRGN_OBJECT_ENCODING_UNSIGNED:
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+		if (drgn_type_kind(drgn_underlying_type(self->obj.type))
+		    != DRGN_TYPE_POINTER) {
+			_cleanup_pydecref_ PyObject *value =
+				DrgnObject_value(self);
+			if (!value)
+				return NULL;
+			return PyObject_CallFunctionObjArgs((PyObject *)&PyFloat_Type,
+							    value, NULL);
+		}
+		fallthrough;
+	}
+	case DRGN_OBJECT_ENCODING_BUFFER:
+	case DRGN_OBJECT_ENCODING_NONE:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_BUFFER:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER:
 		return set_error_type_name("cannot convert '%s' to float",
 					   drgn_object_qualified_type(&self->obj));
-	}
-
-	err = drgn_object_read_value(&self->obj, &value_mem, &value);
-	if (err)
-		return set_drgn_error(err);
-
-	switch (self->obj.encoding) {
-	case DRGN_OBJECT_ENCODING_SIGNED:
-		ret = PyFloat_FromDouble(value->svalue);
-		break;
-	case DRGN_OBJECT_ENCODING_UNSIGNED:
-		ret = PyFloat_FromDouble(value->uvalue);
-		break;
-	case DRGN_OBJECT_ENCODING_FLOAT:
-		ret = PyFloat_FromDouble(value->fvalue);
-		break;
-	default:
-		UNREACHABLE();
-	}
-	drgn_object_deinit_value(&self->obj, value);
-	return ret;
+	)
 }
 
 static PyObject *DrgnObject_index(DrgnObject *self)
 {
-	struct drgn_error *err;
-	struct drgn_type *underlying_type;
-	union drgn_value value_mem;
-	const union drgn_value *value;
-	PyObject *ret;
-
-	underlying_type = drgn_underlying_type(self->obj.type);
-	if (!drgn_type_is_integer(underlying_type) &&
-	    drgn_type_kind(underlying_type) != DRGN_TYPE_POINTER) {
+	SWITCH_ENUM(self->obj.encoding,
+	case DRGN_OBJECT_ENCODING_SIGNED:
+	case DRGN_OBJECT_ENCODING_UNSIGNED:
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG:
+		return DrgnObject_value(self);
+	case DRGN_OBJECT_ENCODING_FLOAT:
+	case DRGN_OBJECT_ENCODING_BUFFER:
+	case DRGN_OBJECT_ENCODING_NONE:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_BUFFER:
+	case DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER:
 		return set_error_type_name("'%s' object cannot be interpreted as an integer",
 					   drgn_object_qualified_type(&self->obj));
-	}
-
-	err = drgn_object_read_value(&self->obj, &value_mem, &value);
-	if (err)
-		return set_drgn_error(err);
-
-	switch (self->obj.encoding) {
-	case DRGN_OBJECT_ENCODING_SIGNED:
-		ret = PyLong_FromInt64(value->svalue);
-		break;
-	case DRGN_OBJECT_ENCODING_UNSIGNED:
-		ret = PyLong_FromUint64(value->uvalue);
-		break;
-	default:
-		UNREACHABLE();
-	}
-	drgn_object_deinit_value(&self->obj, value);
-	return ret;
+	)
 }
 
 static PyObject *DrgnObject_round(DrgnObject *self, PyObject *args,
@@ -1237,33 +1280,19 @@ static PyObject *DrgnObject_round(DrgnObject *self, PyObject *args,
 #define DrgnObject_round_method(func)					\
 static PyObject *DrgnObject_##func(DrgnObject *self)			\
 {									\
-	struct drgn_error *err;						\
-	union drgn_value value_mem;					\
-	const union drgn_value *value;					\
-	PyObject *ret;							\
-									\
 	if (!drgn_type_is_arithmetic(self->obj.type)) {			\
 		return set_error_type_name("cannot round '%s'",		\
 					   drgn_object_qualified_type(&self->obj));\
 	}								\
-									\
-	err = drgn_object_read_value(&self->obj, &value_mem, &value);	\
+	if (self->obj.encoding != DRGN_OBJECT_ENCODING_FLOAT)		\
+		return DrgnObject_value(self);				\
+	union drgn_value value_mem;					\
+	const union drgn_value *value;					\
+	struct drgn_error *err =					\
+		drgn_object_read_value(&self->obj, &value_mem, &value);	\
 	if (err)							\
 		return set_drgn_error(err);				\
-									\
-	switch (self->obj.encoding) {					\
-	case DRGN_OBJECT_ENCODING_SIGNED:				\
-		ret = PyLong_FromInt64(value->svalue);			\
-		break;							\
-	case DRGN_OBJECT_ENCODING_UNSIGNED:				\
-		ret = PyLong_FromUint64(value->uvalue);			\
-		break;							\
-	case DRGN_OBJECT_ENCODING_FLOAT:				\
-		ret = PyLong_FromDouble(func(value->fvalue));		\
-		break;							\
-	default:							\
-		UNREACHABLE();						\
-	}								\
+	PyObject *ret = PyLong_FromDouble(func(value->fvalue));		\
 	drgn_object_deinit_value(&self->obj, value);			\
 	return ret;							\
 }

libdrgn/drgn.h.in
@@ -1138,24 +1138,37 @@ enum drgn_object_encoding {
 	 * Memory buffer.
 	 *
 	 * This is used for objects with a structure, union, class, or array
-	 * type. The value is a buffer of the contents of that object's memory
-	 * in the program.
+	 * type.
 	 */
 	DRGN_OBJECT_ENCODING_BUFFER,
 	/**
 	 * Signed integer.
 	 *
 	 * This is used for objects with a signed integer or signed enumerated
-	 * type.
+	 * type no larger than 64 bits.
 	 */
 	DRGN_OBJECT_ENCODING_SIGNED,
 	/**
 	 * Unsigned integer.
 	 *
 	 * This is used for objects with a unsigned integer, boolean, or pointer
-	 * type.
+	 * type no larger than 64 bits.
 	 */
 	DRGN_OBJECT_ENCODING_UNSIGNED,
+	/**
+	 * Big signed integer.
+	 *
+	 * This is used for objects with a signed integer or signed enumerated
+	 * type larger than 64 bits.
+	 */
+	DRGN_OBJECT_ENCODING_SIGNED_BIG,
+	/**
+	 * Big unsigned integer.
+	 *
+	 * This is used for objects with a unsigned integer, boolean, or pointer
+	 * type larger than 64 bits.
+	 */
+	DRGN_OBJECT_ENCODING_UNSIGNED_BIG,
 	/**
 	 * Floating-point value.
 	 *
@@ -1198,23 +1211,30 @@ drgn_object_encoding_is_complete(enum drgn_object_encoding encoding)
 /** Value of a @ref drgn_object. */
 union drgn_value {
 	/**
-	 * Pointer to an external buffer for a @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_BUFFER value.
+	 * Pointer to an external buffer for a @ref DRGN_OBJECT_ENCODING_BUFFER,
+	 * @ref DRGN_OBJECT_ENCODING_SIGNED_BIG, or @ref
+	 * DRGN_OBJECT_ENCODING_UNSIGNED_BIG value.
+	 *
+	 * For @ref DRGN_OBJECT_ENCODING_BUFFER, this contains the object's
+	 * representation in the memory of the program.
+	 *
+	 * For @ref DRGN_OBJECT_ENCODING_SIGNED_BIG and @ref
+	 * DRGN_OBJECT_ENCODING_UNSIGNED_BIG, the representation of the value is
+	 * an implementation detail which may change.
 	 */
 	char *bufp;
 	/**
-	 * Inline buffer for a @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_BUFFER value.
+	 * Inline buffer for a @ref DRGN_OBJECT_ENCODING_BUFFER value.
 	 *
 	 * Tiny buffers (see @ref drgn_value_is_inline()) are stored inline here
 	 * instead of in a separate allocation.
 	 */
 	char ibuf[8];
-	/** @ref drgn_object_encoding::DRGN_OBJECT_ENCODING_SIGNED value. */
+	/** @ref DRGN_OBJECT_ENCODING_SIGNED value. */
 	int64_t svalue;
-	/** @ref drgn_object_encoding::DRGN_OBJECT_ENCODING_UNSIGNED value. */
+	/** @ref DRGN_OBJECT_ENCODING_UNSIGNED value. */
 	uint64_t uvalue;
-	/** @ref drgn_object_encoding::DRGN_OBJECT_ENCODING_FLOAT value. */
+	/** @ref DRGN_OBJECT_ENCODING_FLOAT value. */
 	double fvalue;
 };
 
@@ -1277,11 +1297,11 @@ struct drgn_object {
 	/**
 	 * Whether this object is little-endian.
 	 *
-	 * Valid only for scalars (i.e., @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_SIGNED, @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_UNSIGNED, @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_FLOAT, or @ref
-	 * drgn_object_encoding::DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER).
+	 * Valid only for scalars (i.e., @ref DRGN_OBJECT_ENCODING_SIGNED, @ref
+	 * DRGN_OBJECT_ENCODING_UNSIGNED, @ref DRGN_OBJECT_ENCODING_SIGNED_BIG,
+	 * @ref DRGN_OBJECT_ENCODING_UNSIGNED_BIG, @ref
+	 * DRGN_OBJECT_ENCODING_FLOAT, or @ref
+	 * DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER).
 	 */
 	bool little_endian;
 	/**

libdrgn/language_c.c
@@ -690,30 +690,70 @@ c_format_int_object(const struct drgn_object *obj,
 		return NULL;
 	}
 
+	union drgn_value value_mem;
+	const union drgn_value *value;
+	err = drgn_object_read_value(obj, &value_mem, &value);
+	if (err)
+		return err;
 	switch (obj->encoding) {
-	case DRGN_OBJECT_ENCODING_SIGNED: {
-		int64_t svalue;
-
-		err = drgn_object_read_signed(obj, &svalue);
-		if (err)
-			return err;
-		if (!string_builder_appendf(sb, "%" PRId64, svalue))
-			return &drgn_enomem;
-		return NULL;
-	}
-	case DRGN_OBJECT_ENCODING_UNSIGNED: {
-		uint64_t uvalue;
-
-		err = drgn_object_read_unsigned(obj, &uvalue);
-		if (err)
-			return err;
-		if (!string_builder_appendf(sb, "%" PRIu64, uvalue))
-			return &drgn_enomem;
-		return NULL;
+	case DRGN_OBJECT_ENCODING_SIGNED:
+		if (!string_builder_appendf(sb, "%" PRId64, value->svalue)) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		break;
+	case DRGN_OBJECT_ENCODING_UNSIGNED:
+		if (!string_builder_appendf(sb, "%" PRIu64, value->uvalue)) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		break;
+	case DRGN_OBJECT_ENCODING_SIGNED_BIG:
+	case DRGN_OBJECT_ENCODING_UNSIGNED_BIG: {
+		if (!string_builder_append(sb, "0x")) {
+			err = &drgn_enomem;
+			goto out;
+		}
+		const uint8_t *buf = (uint8_t *)value->bufp;
+		size_t bytes = drgn_object_size(obj);
+		if (obj->little_endian) {
+			size_t i = bytes - 1;
+			while (i > 0 && buf[i] == 0)
+				i--;
+			if (!string_builder_appendf(sb, "%" PRIx8, buf[i])) {
+				err = &drgn_enomem;
+				goto out;
+			}
+			while (i-- > 0) {
+				if (!string_builder_appendf(sb, "%02" PRIx8, buf[i])) {
+					err = &drgn_enomem;
+					goto out;
+				}
+			}
+		} else {
+			size_t i = 0;
+			while (i < bytes - 1 && buf[i] == 0)
+				i++;
+			if (!string_builder_appendf(sb, "%" PRIx8, buf[i])) {
+				err = &drgn_enomem;
+				goto out;
+			}
+			while (++i < bytes) {
+				if (!string_builder_appendf(sb, "%02" PRIx8, buf[i])) {
+					err = &drgn_enomem;
+					goto out;
+				}
+			}
+		}
+		break;
 	}
 	default:
 		UNREACHABLE();
 	}
+	err = NULL;
+out:
+	drgn_object_deinit_value(obj, value);
+	return err;
 }
 
 static struct drgn_error *

libdrgn/object.h
@@ -154,6 +154,15 @@ drgn_object_set_unsigned_internal(struct drgn_object *res,
 				  uint64_t uvalue);
 
 /**
+ * Like @ref drgn_object_set_float() but @ref drgn_object_type() was already
+ * called and the type is already known to be a floating-point type.
+ */
+struct drgn_error *
+drgn_object_set_float_internal(struct drgn_object *res,
+			       const struct drgn_object_type *type,
+			       double fvalue);
+
+/**
  * Like @ref drgn_object_set_from_buffer() but @ref drgn_object_type() was
  * already called and the bounds of the buffer have already been checked.
  */

libdrgn/python/drgnpy.h
@@ -16,6 +16,7 @@
 // IWYU pragma: end_exports
 
 #include "../hash_table.h"
+#include "../hash_table.h"
 #include "../program.h"
 
 /* These were added in Python 3.7. */

libdrgn/serialize.h
@@ -55,11 +55,12 @@ static inline uint8_t truncate_unsigned8(uint8_t uvalue, int bit_size)
  * least-significant bytes of @p dst.
  *
  * If `src_size > dst_size`, the extra bytes are discarded. If `src_size <
- * dst_size`, the extra bytes are zero-filled.
+ * dst_size`, the extra bytes are filled with @p fill.
  */
-static inline void copy_lsbytes(void *dst, size_t dst_size,
-				bool dst_little_endian, const void *src,
-				size_t src_size, bool src_little_endian)
+static inline void copy_lsbytes_fill(void *dst, size_t dst_size,
+				     bool dst_little_endian, const void *src,
+				     size_t src_size, bool src_little_endian,
+				     int fill)
 {
 	char *d = dst;
 	const char *s = src;
@@ -71,9 +72,9 @@ static inline void copy_lsbytes(void *dst, size_t dst_size,
 			for (size_t i = 0; i < size; i++)
 				d[i] = s[src_size - 1 - i];
 		}
-		memset(d + size, 0, dst_size - size);
+		memset(d + size, fill, dst_size - size);
 	} else {
-		memset(d, 0, dst_size - size);
+		memset(d, fill, dst_size - size);
 		if (src_little_endian) {
 			for (size_t i = dst_size - size; i < dst_size; i++)
 				d[i] = s[dst_size - 1 - i];
@@ -81,6 +82,21 @@ static inline void copy_lsbytes(void *dst, size_t dst_size,
 			memcpy(d + dst_size - size, s + src_size - size, size);
 		}
 	}
+}
+
+/**
+ * Copy the @p src_size least-significant bytes from @p src to the @p dst_size
+ * least-significant bytes of @p dst.
+ *
+ * If `src_size > dst_size`, the extra bytes are discarded. If `src_size <
+ * dst_size`, the extra bytes are zero-filled.
+ */
+static inline void copy_lsbytes(void *dst, size_t dst_size,
+				bool dst_little_endian, const void *src,
+				size_t src_size, bool src_little_endian)
+{
+	return copy_lsbytes_fill(dst, dst_size, dst_little_endian, src,
+				 src_size, src_little_endian, 0);
 }
 
 /**

libdrgn/type.c
@@ -1200,37 +1200,6 @@ struct drgn_error *drgn_type_bit_size(struct drgn_type *type, uint64_t *ret)
 	return NULL;
 }
 
-enum drgn_object_encoding drgn_type_object_encoding(struct drgn_type *type)
-{
-	SWITCH_ENUM(drgn_type_kind(type),
-	case DRGN_TYPE_INT:
-		return (drgn_type_is_signed(type) ?
-			DRGN_OBJECT_ENCODING_SIGNED :
-			DRGN_OBJECT_ENCODING_UNSIGNED);
-	case DRGN_TYPE_BOOL:
-	case DRGN_TYPE_POINTER:
-		return DRGN_OBJECT_ENCODING_UNSIGNED;
-	case DRGN_TYPE_FLOAT:
-		return DRGN_OBJECT_ENCODING_FLOAT;
-	case DRGN_TYPE_STRUCT:
-	case DRGN_TYPE_UNION:
-	case DRGN_TYPE_CLASS:
-	case DRGN_TYPE_ARRAY:
-		return (drgn_type_is_complete(type) ?
-			DRGN_OBJECT_ENCODING_BUFFER :
-			DRGN_OBJECT_ENCODING_INCOMPLETE_BUFFER);
-	case DRGN_TYPE_ENUM:
-		if (!drgn_type_is_complete(type))
-			return DRGN_OBJECT_ENCODING_INCOMPLETE_INTEGER;
-		fallthrough;
-	case DRGN_TYPE_TYPEDEF:
-		return drgn_type_object_encoding(drgn_type_type(type).type);
-	case DRGN_TYPE_VOID:
-	case DRGN_TYPE_FUNCTION:
-		return DRGN_OBJECT_ENCODING_NONE;
-	)
-}
-
 struct drgn_error *drgn_type_error(const char *format, struct drgn_type *type)
 {
 	struct drgn_qualified_type qualified_type = { type };

commit 6ff5ef5f97b9a4f51a69adc2d1d10d7379d417ac

libdrgn: python: don't leak big integer value in DrgnObject_value_impl()
If the object is a big integer reference, then the read value is
allocated and needs to be freed with drgn_object_deinit_value().

Fixes: 243f6fb7d567 ("libdrgn: support value objects with >64-bit integer types")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -686,11 +686,14 @@ static PyObject *DrgnObject_value_impl(struct drgn_object *obj)
 		err = drgn_object_read_value(obj, &value_mem, &value);
 		if (err)
 			return set_drgn_error(err);
-		return _PyLong_FromByteArray((void *)value->bufp,
-					     drgn_object_size(obj),
-					     obj->little_endian,
-					     obj->encoding
-					     == DRGN_OBJECT_ENCODING_SIGNED_BIG);
+		PyObject *ret =
+			_PyLong_FromByteArray((void *)value->bufp,
+					      drgn_object_size(obj),
+					      obj->little_endian,
+					      obj->encoding
+					      == DRGN_OBJECT_ENCODING_SIGNED_BIG);
+		drgn_object_deinit_value(obj, value);
+		return ret;
 	}
 	case DRGN_OBJECT_ENCODING_FLOAT: {
 		double fvalue;