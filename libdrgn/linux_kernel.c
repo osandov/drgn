osandov/drgn

commit 6c8ee7ea672251bc3d2e9147106b14740aff90e9

libdrgn: linux_kernel: add SECTIONS_PER_ROOT object finder
Continuing with SPARSEMEM constants, this one we can derive from type of
the mem_section variable.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/linux_kernel.c
@@ -20,6 +20,7 @@
 
 #include "array.h"
 #include "binary_buffer.h"
+#include "bitops.h"
 #include "cleanup.h"
 #include "debug_info.h"
 #include "drgn_internal.h"
@@ -489,6 +490,68 @@ linux_kernel_get_nr_section_roots_impl(struct drgn_program *prog, uint64_t *ret)
 	return NULL;
 }
 LINUX_KERNEL_GET_PRIMITIVE_WRAPPER(nr_section_roots, DRGN_C_TYPE_UNSIGNED_LONG)
+
+static struct drgn_error *
+linux_kernel_get_sections_per_root_impl(struct drgn_program *prog, uint64_t *ret)
+{
+	struct drgn_error *err;
+
+	if (prog->cached_sections_per_root) {
+		*ret = prog->cached_sections_per_root;
+		return NULL;
+	}
+
+	if (!prog->vmcoreinfo.mem_section_length) // !SPARSEMEM
+		return &drgn_not_found;
+
+	DRGN_OBJECT(mem_section, prog);
+	err = drgn_program_find_object(prog, "mem_section", NULL,
+				       DRGN_FIND_OBJECT_VARIABLE, &mem_section);
+	if (err)
+		return err;
+
+	// For SPARSEMEM_STATIC, mem_section is always an array of arrays. For
+	// SPARSEMEM_EXTREME, since Linux kernel commit 83e3c48729d9
+	// ("mm/sparsemem: Allocate mem_section at runtime for
+	// CONFIG_SPARSEMEM_EXTREME=y") (in v4.15), it is a pointer to a pointer
+	// to struct mem_section. Before that, it is an array of pointers to
+	// struct mem_section.
+
+	struct drgn_type *outer_type = drgn_underlying_type(mem_section.type);
+	enum drgn_type_kind outer_kind = drgn_type_kind(outer_type);
+	if (outer_kind != DRGN_TYPE_POINTER && outer_kind != DRGN_TYPE_ARRAY) {
+		return drgn_type_error("mem_section has unrecognized type: %s",
+				       outer_type);
+	}
+
+	struct drgn_type *inner_type =
+		drgn_underlying_type(drgn_type_type(outer_type).type);
+	enum drgn_type_kind inner_kind = drgn_type_kind(inner_type);
+	if (outer_kind == DRGN_TYPE_ARRAY && inner_kind == DRGN_TYPE_ARRAY) {
+		// SPARSEMEM_STATIC: SECTIONS_PER_ROOT = 1
+		*ret = 1;
+		return NULL;
+	}
+	if (inner_kind != DRGN_TYPE_POINTER) {
+		return drgn_type_error("mem_section[0] has unrecognized type: %s",
+				       inner_type);
+	}
+
+	// SPARSEMEM_EXTREME: SECTIONS_PER_ROOT = PAGE_SIZE / sizeof(struct mem_section)
+	uint64_t sizeof_mem_section;
+	err = drgn_type_sizeof(drgn_underlying_type(drgn_type_type(inner_type).type),
+			       &sizeof_mem_section);
+	if (err)
+		return err;
+	if (!is_power_of_two(sizeof_mem_section)) {
+		return drgn_error_create(DRGN_ERROR_OTHER,
+					 "struct mem_section has invalid size");
+	}
+	*ret = prog->cached_sections_per_root =
+		prog->vmcoreinfo.page_size / sizeof_mem_section;
+	return err;
+}
+LINUX_KERNEL_GET_PRIMITIVE_WRAPPER(sections_per_root, DRGN_C_TYPE_UNSIGNED_LONG)
 
 #include "linux_kernel_object_find.inc" // IWYU pragma: keep
 

libdrgn/linux_kernel_object_find.inc.strswitch
@@ -46,6 +46,10 @@ linux_kernel_object_find(const char *name, size_t name_len,
 			if (flags & DRGN_FIND_OBJECT_CONSTANT)
 				return linux_kernel_get_nr_section_roots(prog, ret);
 			break;
+		@case "SECTIONS_PER_ROOT"@
+			if (flags & DRGN_FIND_OBJECT_CONSTANT)
+				return linux_kernel_get_sections_per_root(prog, ret);
+			break;
 		@endswitch@
 	}
 	return &drgn_not_found;

libdrgn/program.h
@@ -228,6 +228,11 @@ struct drgn_program {
 			 * cached yet.
 			 */
 			uint64_t thread_size_cached;
+			/**
+			 * Value of `SECTIONS_PER_ROOT` in the kernel, or 0 if
+			 * not cached yet.
+			 */
+			uint64_t cached_sections_per_root;
 			/*
 			 * Difference between a virtual address in the direct
 			 * mapping and the physical address it maps to.

tests/linux_kernel/kmod/drgn_test.c
@@ -133,6 +133,9 @@ unsigned long drgn_test_THREAD_SIZE;
 #ifdef NR_SECTION_ROOTS
 unsigned long drgn_test_NR_SECTION_ROOTS;
 #endif
+#ifdef SECTIONS_PER_ROOT
+unsigned long drgn_test_SECTIONS_PER_ROOT;
+#endif
 
 static void drgn_test_constants_init(void)
 {
@@ -141,6 +144,9 @@ static void drgn_test_constants_init(void)
 	drgn_test_THREAD_SIZE = THREAD_SIZE;
 #ifdef NR_SECTION_ROOTS
 	drgn_test_NR_SECTION_ROOTS = NR_SECTION_ROOTS;
+#endif
+#ifdef SECTIONS_PER_ROOT
+	drgn_test_SECTIONS_PER_ROOT = SECTIONS_PER_ROOT;
 #endif
 }
 

tests/linux_kernel/test_special_objects.py
@@ -59,6 +59,7 @@ class TestVmcoreinfo(LinuxKernelTestCase):
         for constant in (
             "THREAD_SIZE",
             "NR_SECTION_ROOTS",
+            "SECTIONS_PER_ROOT",
         ):
             with self.subTest(constant=constant):
                 try:

commit a499fdd5f895ad1d6a03d92c01d1f2e2e52b7cf9

libdrgn: linux_kernel: prevent ilog2(0) on invalid sizeof(struct mem_section)
If sizeof(struct mem_section) > PAGE_SIZE, then
linux_kernel_get_sections_per_root_impl() calculates sections_per_root
as 0, and the ilog2(sections_per_root) in
linux_kernel_get_max_physmem_bits_impl() is undefined. That case is
invalid, so reject it early.

Fixes: 6c8ee7ea6722 ("libdrgn: linux_kernel: add SECTIONS_PER_ROOT object finder")
Fixes: 7c4c1737d858 ("libdrgn: linux_kernel: add MAX_PHYSMEM_BITS object finder")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/linux_kernel.c
@@ -552,7 +552,8 @@ linux_kernel_get_sections_per_root_impl(struct drgn_program *prog, uint64_t *ret
 			       &sizeof_mem_section);
 	if (err)
 		return err;
-	if (!is_power_of_two(sizeof_mem_section)) {
+	if (!is_power_of_two(sizeof_mem_section)
+	    || sizeof_mem_section > prog->vmcoreinfo.page_size) {
 		return drgn_error_create(DRGN_ERROR_BAD_DATA,
 					 "struct mem_section has invalid size");
 	}