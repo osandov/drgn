osandov/drgn

commit d1ebf5e9fe81ffb04416c397f9ca9a993ed67613

python: Allow construction of Symbol objects
Previously, Symbol objects could not be constructed in Python. However,
in order to allow Python Symbol finders, this needs to be changed.
Unfortunately, Symbol name lifetimes are tricky to manage. We introduce
a lifetime enumeration to handle this. The lifetime may be "static",
i.e. longer than the life of the program; "external", i.e. longer than
the life of the symbol, but no guarantees beyond that; or "owned", i.e.
owned by the Symbol itself.

Symbol objects constructed in Python are "external". The Symbol struct
owns the pointer to the drgn_symbol, and it holds a reference to the
Python object keeping the name valid (either the program, or a PyUnicode
object).

The added complexity is justified by the fact that most symbols are from
the ELF file, and thus share a lifetime with the Program. It would be a
waste to constantly strdup() these strings, just to support a small
number of Symbols created by Python code.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/symbol.c
@@ -11,6 +11,11 @@
 
 LIBDRGN_PUBLIC void drgn_symbol_destroy(struct drgn_symbol *sym)
 {
+	if (sym && sym->name_lifetime == DRGN_LIFETIME_OWNED)
+		/* Cast here is necessary - we want symbol users to
+		 * never modify sym->name, but when we own the name,
+		 * we must modify it by freeing it. */
+		free((char *)sym->name);
 	free(sym);
 }
 
@@ -26,6 +31,7 @@ void drgn_symbol_from_elf(const char *name, uint64_t address,
 			  const GElf_Sym *elf_sym, struct drgn_symbol *ret)
 {
 	ret->name = name;
+	ret->name_lifetime = DRGN_LIFETIME_STATIC;
 	ret->address = address;
 	ret->size = elf_sym->st_size;
 	int binding = GELF_ST_BIND(elf_sym->st_info);
@@ -38,6 +44,24 @@ void drgn_symbol_from_elf(const char *name, uint64_t address,
 		ret->kind = type;
 	else
 		ret->kind = DRGN_SYMBOL_KIND_UNKNOWN;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_symbol_create(const char *name, uint64_t address, uint64_t size,
+		   enum drgn_symbol_binding binding, enum drgn_symbol_kind kind,
+		   enum drgn_lifetime name_lifetime, struct drgn_symbol **ret)
+{
+	struct drgn_symbol *sym = malloc(sizeof(*sym));
+	if (!sym)
+		return &drgn_enomem;
+	sym->name = name;
+	sym->address = address;
+	sym->size = size;
+	sym->binding = binding;
+	sym->kind = kind;
+	sym->name_lifetime = name_lifetime;
+	*ret = sym;
+	return NULL;
 }
 
 LIBDRGN_PUBLIC const char *drgn_symbol_name(struct drgn_symbol *sym)

libdrgn/python/symbol.c
@@ -3,23 +3,62 @@
 
 #include <inttypes.h>
 
+#include "drgn.h"
 #include "drgnpy.h"
+#include "symbol.h"
 
-PyObject *Symbol_wrap(struct drgn_symbol *sym, Program *prog)
+PyObject *Symbol_wrap(struct drgn_symbol *sym, PyObject *name_obj)
 {
 	Symbol *ret = call_tp_alloc(Symbol);
 	if (ret) {
 		ret->sym = sym;
-		ret->prog = prog;
-		Py_INCREF(prog);
+		ret->name_obj = name_obj;
+		Py_XINCREF(name_obj);
 	}
 	return (PyObject *)ret;
+}
+
+static PyObject *Symbol_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds)
+{
+	struct drgn_symbol *sym;
+	static char *keywords[] = {"name", "address", "size", "binding", "kind", NULL};
+	PyObject *name_obj;
+	struct index_arg address = {}, size = {};
+	struct enum_arg binding = {
+		.type = SymbolBinding_class,
+	};
+	struct enum_arg kind = {
+		.type = SymbolKind_class,
+	};
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!O&O&O&O&:Symbol", keywords,
+					 &PyUnicode_Type, &name_obj,
+					 index_converter, &address,
+					 index_converter, &size,
+					 enum_converter, &binding,
+					 enum_converter, &kind))
+		return NULL;
+
+	const char *name = PyUnicode_AsUTF8(name_obj);
+	if (!name)
+		return NULL;
+
+	struct drgn_error *err = drgn_symbol_create(
+		name, address.uvalue,size.uvalue, binding.value, kind.value,
+		DRGN_LIFETIME_EXTERNAL, &sym);
+	if (err)
+		return set_drgn_error(err);
+
+	PyObject *ret = Symbol_wrap(sym, name_obj);
+	if (!ret)
+		drgn_symbol_destroy(sym);
+	return ret;
 }
 
 static void Symbol_dealloc(Symbol *self)
 {
 	drgn_symbol_destroy(self->sym);
-	Py_XDECREF(self->prog);
+	Py_XDECREF(self->name_obj);
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
@@ -100,4 +139,5 @@ PyTypeObject Symbol_type = {
 	.tp_doc = drgn_Symbol_DOC,
 	.tp_richcompare = (richcmpfunc)Symbol_richcompare,
 	.tp_getset = Symbol_getset,
+	.tp_new = Symbol_new,
 };

libdrgn/drgn.h
@@ -2966,6 +2966,44 @@ enum drgn_symbol_kind {
 	DRGN_SYMBOL_KIND_IFUNC = 10, /* STT_GNU_IFUNC */
 } __attribute__((__packed__));
 
+/** Describes the lifetime of an object provided to drgn */
+enum drgn_lifetime {
+	/**
+	 * DRGN_LIFETIME_STATIC: the object is guaranteed to outlive the
+	 * drgn_program itself. drgn will not free or copy the object.
+	 */
+	DRGN_LIFETIME_STATIC,
+	/**
+	 * DRGN_LIFETIME_EXTERNAL: the object is externally managed. It will
+	 * live as long as the object it is associated with, but may be freed
+	 * after. drgn will never free the object. If drgn must copy a data
+	 * structure, the object will be duplicated, and drgn will own the new
+	 * object.
+	 */
+	DRGN_LIFETIME_EXTERNAL,
+	/**
+	 * DRGN_LIFETIME_OWNED: the object lifetime is managed by drgn. It
+	 * should be freed when the containing object is freed. If the
+	 * containing object is copied, it must also be copied.
+	 */
+	DRGN_LIFETIME_OWNED,
+} __attribute__((__packed__));
+
+/**
+ * Create a new @ref drgn_symbol with the given values
+ *
+ * All parameters should be self-explanatory, except for @a name_lifetime.
+ * Clients can use this to describe how drgn should treat the string @a name.
+ * Strings with lifetime @c STATIC will never be copied or freed. Strings with
+ * lifetime @c OWNED will always be copied or and freed with the symbol. Strings
+ * with lifetime EXTERNAL will not be freed, but if the Symbol is copied, they
+ * will be copied.
+ */
+struct drgn_error *
+drgn_symbol_create(const char *name, uint64_t address, uint64_t size,
+		   enum drgn_symbol_binding binding, enum drgn_symbol_kind kind,
+		   enum drgn_lifetime name_lifetime, struct drgn_symbol **ret);
+
 /** Destroy a @ref drgn_symbol. */
 void drgn_symbol_destroy(struct drgn_symbol *sym);
 

libdrgn/python/drgnpy.h
@@ -173,7 +173,7 @@ typedef struct {
 
 typedef struct {
 	PyObject_HEAD
-	Program *prog;
+	PyObject *name_obj; /* object owning the reference to the symbol name */
 	struct drgn_symbol *sym;
 } Symbol;
 
@@ -288,7 +288,7 @@ Program *program_from_core_dump(PyObject *self, PyObject *args, PyObject *kwds);
 Program *program_from_kernel(PyObject *self);
 Program *program_from_pid(PyObject *self, PyObject *args, PyObject *kwds);
 
-PyObject *Symbol_wrap(struct drgn_symbol *sym, Program *prog);
+PyObject *Symbol_wrap(struct drgn_symbol *sym, PyObject *name_obj);
 
 PyObject *Thread_wrap(struct drgn_thread *drgn_thread);
 

libdrgn/python/program.c
@@ -937,7 +937,7 @@ static PyObject *Program_symbols(Program *self, PyObject *args)
 		return NULL;
 	}
 	for (size_t i = 0; i < count; i++) {
-		PyObject *pysym = Symbol_wrap(symbols[i], self);
+		PyObject *pysym = Symbol_wrap(symbols[i], (PyObject *)self);
 		if (!pysym) {
 			/* Free symbols which aren't yet added to list. */
 			drgn_symbols_destroy(symbols, count);
@@ -973,7 +973,7 @@ static PyObject *Program_symbol(Program *self, PyObject *arg)
 	}
 	if (err)
 		return set_drgn_error(err);
-	ret = Symbol_wrap(sym, self);
+	ret = Symbol_wrap(sym, (PyObject *)self);
 	if (!ret) {
 		drgn_symbol_destroy(sym);
 		return NULL;

libdrgn/python/stack_trace.c
@@ -209,7 +209,7 @@ static PyObject *StackFrame_symbol(StackFrame *self)
 	err = drgn_stack_frame_symbol(self->trace->trace, self->i, &sym);
 	if (err)
 		return set_drgn_error(err);
-	PyObject *ret = Symbol_wrap(sym, prog);
+	PyObject *ret = Symbol_wrap(sym, (PyObject *)prog);
 	if (!ret) {
 		drgn_symbol_destroy(sym);
 		return NULL;

libdrgn/symbol.h
@@ -16,6 +16,7 @@ struct drgn_symbol {
 	uint64_t size;
 	enum drgn_symbol_binding binding;
 	enum drgn_symbol_kind kind;
+	enum drgn_lifetime name_lifetime;
 };
 
 struct drgn_symbol_finder {

tests/test_symbol.py
@@ -1,9 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 import tempfile
-from typing import NamedTuple
 
-from drgn import Program, SymbolBinding, SymbolKind
+from drgn import Program, Symbol, SymbolBinding, SymbolKind
 from tests import TestCase
 from tests.dwarfwriter import dwarf_sections
 from tests.elf import ET, PT, SHT, STB, STT
@@ -45,35 +44,13 @@ def elf_symbol_program(*modules):
     return prog
 
 
-# We don't want to support creating drgn.Symbol instances yet, so use this dumb
-# class for testing.
-class Symbol(NamedTuple):
-    name: str
-    address: int
-    size: int
-    binding: SymbolBinding
-    kind: SymbolKind
-
-
 class TestElfSymbol(TestCase):
-    def assert_symbol_equal(self, drgn_symbol, symbol):
-        self.assertEqual(
-            Symbol(
-                drgn_symbol.name,
-                drgn_symbol.address,
-                drgn_symbol.size,
-                drgn_symbol.binding,
-                drgn_symbol.kind,
-            ),
-            symbol,
-        )
-
     def assert_symbols_equal_unordered(self, drgn_symbols, symbols):
         self.assertEqual(len(drgn_symbols), len(symbols))
         drgn_symbols = sorted(drgn_symbols, key=lambda x: (x.address, x.name))
         symbols = sorted(symbols, key=lambda x: (x.address, x.name))
         for drgn_symbol, symbol in zip(drgn_symbols, symbols):
-            self.assert_symbol_equal(drgn_symbol, symbol)
+            self.assertEqual(drgn_symbol, symbol)
 
     def test_by_address(self):
         elf_first = ElfSymbol("first", 0xFFFF0000, 0x8, STT.OBJECT, STB.LOCAL)
@@ -91,13 +68,13 @@ class TestElfSymbol(TestCase):
                 prog = elf_symbol_program(*modules)
                 self.assertRaises(LookupError, prog.symbol, 0xFFFEFFFF)
                 self.assertEqual(prog.symbols(0xFFFEFFFF), [])
-                self.assert_symbol_equal(prog.symbol(0xFFFF0000), first)
+                self.assertEqual(prog.symbol(0xFFFF0000), first)
                 self.assert_symbols_equal_unordered(prog.symbols(0xFFFF0000), [first])
-                self.assert_symbol_equal(prog.symbol(0xFFFF0004), first)
+                self.assertEqual(prog.symbol(0xFFFF0004), first)
                 self.assert_symbols_equal_unordered(prog.symbols(0xFFFF0004), [first])
-                self.assert_symbol_equal(prog.symbol(0xFFFF0008), second)
+                self.assertEqual(prog.symbol(0xFFFF0008), second)
                 self.assert_symbols_equal_unordered(prog.symbols(0xFFFF0008), [second])
-                self.assert_symbol_equal(prog.symbol(0xFFFF000C), second)
+                self.assertEqual(prog.symbol(0xFFFF000C), second)
                 self.assert_symbols_equal_unordered(prog.symbols(0xFFFF000C), [second])
                 self.assertRaises(LookupError, prog.symbol, 0xFFFF0010)
 
@@ -171,8 +148,8 @@ class TestElfSymbol(TestCase):
         for modules in same_module, different_modules:
             with self.subTest(modules=len(modules)):
                 prog = elf_symbol_program(*modules)
-                self.assert_symbol_equal(prog.symbol("first"), first)
-                self.assert_symbol_equal(prog.symbol("second"), second)
+                self.assertEqual(prog.symbol("first"), first)
+                self.assertEqual(prog.symbol("second"), second)
                 self.assertRaises(LookupError, prog.symbol, "third")
 
                 self.assert_symbols_equal_unordered(prog.symbols("first"), [first])
@@ -258,7 +235,7 @@ class TestElfSymbol(TestCase):
                     (ElfSymbol("foo", 0xFFFF0000, 1, elf_type, STB.GLOBAL),)
                 )
                 symbol = Symbol("foo", 0xFFFF0000, 1, SymbolBinding.GLOBAL, drgn_kind)
-                self.assert_symbol_equal(prog.symbol("foo"), symbol)
+                self.assertEqual(prog.symbol("foo"), symbol)
                 symbols = prog.symbols("foo")
                 self.assert_symbols_equal_unordered(symbols, [symbol])
 

commit b9017b429f581450439637247538e5845893b6d7

libdrgn: symbol: check binding and kind validity in drgn_symbol_create()
Fixes: d1ebf5e9fe81 ("python: Allow construction of Symbol objects")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/symbol.c
@@ -59,6 +59,32 @@ drgn_symbol_create(const char *name, uint64_t address, uint64_t size,
 		   enum drgn_symbol_binding binding, enum drgn_symbol_kind kind,
 		   enum drgn_lifetime name_lifetime, struct drgn_symbol **ret)
 {
+	SWITCH_ENUM(binding) {
+	case DRGN_SYMBOL_BINDING_UNKNOWN:
+	case DRGN_SYMBOL_BINDING_LOCAL:
+	case DRGN_SYMBOL_BINDING_GLOBAL:
+	case DRGN_SYMBOL_BINDING_WEAK:
+	case DRGN_SYMBOL_BINDING_UNIQUE:
+		break;
+	default:
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "invalid symbol binding");
+	}
+	SWITCH_ENUM(kind) {
+	case DRGN_SYMBOL_KIND_UNKNOWN:
+	case DRGN_SYMBOL_KIND_OBJECT:
+	case DRGN_SYMBOL_KIND_FUNC:
+	case DRGN_SYMBOL_KIND_SECTION:
+	case DRGN_SYMBOL_KIND_FILE:
+	case DRGN_SYMBOL_KIND_COMMON:
+	case DRGN_SYMBOL_KIND_TLS:
+	case DRGN_SYMBOL_KIND_IFUNC:
+		break;
+	default:
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "invalid symbol kind");
+	}
+
 	struct drgn_symbol *sym = malloc(sizeof(*sym));
 	if (!sym)
 		return &drgn_enomem;