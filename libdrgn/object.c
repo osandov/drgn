osandov/drgn

commit b7e2099e28226b179c4f14de318de2e9dd9d0a0b

Add array slicing
This was suggested by septatrix on GitHub, and I really like the idea as
a solution to getting a complete array from a pointer/incomplete array,
among other things. It may be useful for the crash struct command. The
implementation is a fairly straightforward variant of
drgn_object_subscript().

Closes #377, closes #475.

Suggested-by: Septatrix <24257556+Septatrix@users.noreply.github.com>
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/object.c
@@ -1600,6 +1600,44 @@ drgn_object_subscript(struct drgn_object *res, const struct drgn_object *obj,
 	}
 }
 
+LIBDRGN_PUBLIC
+struct drgn_error *drgn_object_slice(struct drgn_object *res,
+				     const struct drgn_object *obj,
+				     int64_t start, int64_t end)
+{
+	struct drgn_error *err;
+	struct drgn_program *prog = drgn_object_program(obj);
+
+	if (drgn_object_program(res) != prog) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "objects are from different programs");
+	}
+
+	struct drgn_element_info element;
+	err = drgn_program_element_info(drgn_object_program(obj), obj->type,
+					&element);
+	if (err)
+		return err;
+
+	struct drgn_qualified_type array_type;
+	err = drgn_array_type_create(prog, element.qualified_type,
+				     end > start ? end - start : 0,
+				     drgn_type_language(element.qualified_type.type),
+				     &array_type.type);
+	if (err)
+		return err;
+	array_type.qualifiers = 0;
+
+	if (obj->encoding == DRGN_OBJECT_ENCODING_UNSIGNED) {
+		return drgn_object_dereference_offset(res, obj, array_type,
+						      start * element.bit_size,
+						      0);
+	} else {
+		return drgn_object_fragment(res, obj, array_type,
+					    start * element.bit_size, 0);
+	}
+}
+
 LIBDRGN_PUBLIC struct drgn_error *
 drgn_object_member(struct drgn_object *res, const struct drgn_object *obj,
 		   const char *member_name)

libdrgn/python/object.c
@@ -1434,13 +1434,79 @@ static DrgnObject *DrgnObject_subscript_impl(DrgnObject *self,
 	return_ptr(res);
 }
 
-static DrgnObject *DrgnObject_subscript(DrgnObject *self, PyObject *key)
+static int64_t index_to_int64(PyObject *number)
 {
-	struct index_arg index = { .is_signed = true };
+	_cleanup_pydecref_ PyObject *index = PyNumber_Index(number);
+	if (!index)
+		return -1;
+	return PyLong_AsInt64(index);
+}
 
-	if (!index_converter(key, &index))
+static DrgnObject *DrgnObject_subscript(DrgnObject *self, PyObject *item)
+{
+	if (PyIndex_Check(item)) {
+		int64_t index = index_to_int64(item);
+		if (index == -1 && PyErr_Occurred())
+			return NULL;
+		return DrgnObject_subscript_impl(self, index);
+	} else if (PySlice_Check(item)) {
+		PySliceObject *slice = (PySliceObject *)item;
+		Py_ssize_t start, stop;
+		if (slice->start == Py_None) {
+			start = 0;
+		} else {
+			start = index_to_int64(slice->start);
+			if (start == -1 && PyErr_Occurred())
+				return NULL;
+		}
+		if (slice->stop == Py_None) {
+			struct drgn_type *underlying_type =
+				drgn_underlying_type(self->obj.type);
+			if (drgn_type_kind(underlying_type) != DRGN_TYPE_ARRAY
+			    || !drgn_type_is_complete(underlying_type)) {
+				set_error_type_name("'%s' has no length; slice stop must be given",
+						    drgn_object_qualified_type(&self->obj));
+				return NULL;
+			}
+			uint64_t length = drgn_type_length(underlying_type);
+			if (length > INT64_MAX) {
+				PyErr_SetString(PyExc_OverflowError,
+						"length is too large");
+				return NULL;
+			}
+			stop = length;
+		} else {
+			stop = index_to_int64(slice->stop);
+			if (stop == -1 && PyErr_Occurred())
+				return NULL;
+		}
+		if (slice->step != Py_None) {
+			Py_ssize_t step =
+				PyNumber_AsSsize_t(slice->step,
+						   PyExc_OverflowError);
+			if (step == -1 && PyErr_Occurred())
+				return NULL;
+			if (step != 1) {
+				PyErr_SetString(PyExc_ValueError,
+						"object slice step must be 1");
+				return NULL;
+			}
+		}
+		struct drgn_error *err;
+		_cleanup_pydecref_ DrgnObject *res =
+			DrgnObject_alloc(DrgnObject_prog(self));
+		if (!res)
+			return NULL;
+		err = drgn_object_slice(&res->obj, &self->obj, start, stop);
+		if (err)
+			return set_drgn_error(err);
+		return_ptr(res);
+	} else {
+		PyErr_Format(PyExc_TypeError,
+			     "object subscript must be integer or slice, not %.200s",
+			     Py_TYPE(item)->tp_name);
 		return NULL;
-	return DrgnObject_subscript_impl(self, index.svalue);
+	}
 }
 
 static ObjectIterator *DrgnObject_iter(DrgnObject *self)

_drgn.pyi
@@ -2350,11 +2350,14 @@ class Object:
         """
         ...
 
-    def __getitem__(self, idx: IntegerLike) -> Object:
+    @overload
+    def __getitem__(self, /, i: IntegerLike) -> Object:
         """
-        Implement ``self[idx]``. Get the array element at the given index.
+        Implement ``self[i]``. Get the array element at the given index.
 
-        >>> print(prog['init_task'].comm[1])
+        This is only valid for pointers and arrays.
+
+        >>> print(prog["init_task"].comm[1])
         (char)119
 
         ``[0]`` is also the equivalent of the pointer dereference (``*``)
@@ -2365,24 +2368,83 @@ class Object:
         >>> print(ptr_to_ptr[0])
         (void *)0xffff9b86801e2460
 
-        This is only valid for pointers and arrays.
-
         .. note::
 
-            Negative indices behave as they would in the object's language (as
-            opposed to the Python semantics of indexing from the end of the
-            array).
+            Negative indices are relative to the start of the pointer/array
+            (like in C), not relative to the end (like for Python lists).
 
-        :param idx: The array index.
+            >>> ptr[-2].address_of_() == ptr - 2
+            True
+
+        :param i: Index.
         :raises TypeError: if this object is not a pointer or array
         """
         ...
+
+    @overload
+    def __getitem__(self, /, s: slice) -> Object:
+        """
+        Implement ``self[start:stop]``. Get an array :term:`slice`.
+
+        This is only valid for pointers and arrays. It creates a new array for
+        the range of elements from the (inclusive) start index to the
+        (exclusive) stop index. The length of the resulting array is therefore
+        the stop index minus the start index, or zero if the stop index is less
+        than the start index.
+
+        If the start index is omitted, it defaults to 0. If the stop index is
+        omitted, it defaults to the length of the array (in which case the
+        object must be a complete array).
+
+        For example, this can be used to get a subset of an array:
+
+        >>> prog["init_task"].comm[1:3]
+        (char [2])"wa"
+
+        Or to get a complete array from a pointer/incomplete array with a
+        separate length:
+
+        >>> poll_list
+        *(struct poll_list *)0xffffa3d3c126fa70 = {
+                .next = (struct poll_list *)0x0,
+                .len = (unsigned int)2,
+                .entries = (struct pollfd []){},
+        }
+        >>> poll_list.entries[:poll_list.len]
+        (struct pollfd [2]){
+                {
+                        .fd = (int)6,
+                        .events = (short)1,
+                        .revents = (short)0,
+                },
+                {
+                        .fd = (int)14,
+                        .events = (short)1,
+                        .revents = (short)0,
+                },
+        }
+
+        .. note::
+
+            Negative indices are relative to the start of the pointer/array
+            (like in C), not relative to the end (like for Python lists).
+
+            >>> prog['init_task'].comm[-2:]
+            (char [18])""
+            >>> prog['init_task'].comm[:-2]
+            (char [0]){}
+
+        :param s: Slice.
+        :raises TypeError: if this object is not a pointer or array
+        :raises TypeError: if the stop index is omitted and this object is not
+            an array with complete type
+        """
 
     def __len__(self) -> int:
         """
         Implement ``len(self)``. Get the number of elements in this object.
 
-        >>> len(prog['init_task'].comm)
+        >>> len(prog["init_task"].comm)
         16
 
         This is only valid for arrays.

libdrgn/drgn.h
@@ -3026,6 +3026,22 @@ drgn_object_dereference(struct drgn_object *res, const struct drgn_object *obj)
 }
 
 /**
+ * Slice (i.e., get an array from a range of another array) a @ref drgn_object.
+ *
+ * This is applicable to pointers and arrays.
+ *
+ * @param[out] res Resulting array. May be the same as @p obj.
+ * @param[in] obj Object to slice.
+ * @param[in] start Start index (inclusive).
+ * @param[in] end End index (exclusive).
+ * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
+ * error.
+ */
+struct drgn_error *drgn_object_slice(struct drgn_object *res,
+				     const struct drgn_object *obj,
+				     int64_t start, int64_t end);
+
+/**
  * Get a member of a structure, union, or class @ref drgn_object (@c .).
  *
  * @param[out] res Returned member. May be the same as @p obj.

tests/test_object.py
@@ -1797,6 +1797,121 @@ class TestGenericOperators(MockProgramTestCase):
         obj = arr.read_()
         self.assertRaisesRegex(OutOfBoundsError, "out of bounds", obj.__getitem__, -1)
 
+    def test_slice(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[1:3], Object(self.prog, "int [2]", address=0xFFFF0004)
+            )
+
+        obj = arr.read_()
+        self.assertIdentical(obj[1:3], Object(self.prog, "int [2]", [1, 2]))
+
+    def test_slice_step(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[1:3:1], Object(self.prog, "int [2]", address=0xFFFF0004)
+            )
+
+    def test_slice_invalid_step(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        with self.assertRaisesRegex(ValueError, "object slice step must be 1"):
+            arr[0:4:2]
+
+    def test_slice_negative_start(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[-2:2], Object(self.prog, "int [4]", address=0xFFFEFFF8)
+            )
+
+        obj = arr.read_()
+        with self.assertRaisesRegex(OutOfBoundsError, "out of bounds"):
+            obj[-2:2]
+
+    def test_slice_both_negative(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[-4:-2], Object(self.prog, "int [2]", address=0xFFFEFFF0)
+            )
+
+        obj = arr.read_()
+        with self.assertRaisesRegex(OutOfBoundsError, "out of bounds"):
+            obj[-4:-2]
+
+    def test_slice_both_none(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+
+        self.assertIdentical(arr[:], Object(self.prog, "int [4]", address=0xFFFF0000))
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            incomplete_arr[:]
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            ptr[:]
+
+        self.assertIdentical(arr.read_()[:], Object(self.prog, "int [4]", [0, 1, 2, 3]))
+
+    def test_slice_start_none(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[:3], Object(self.prog, "int [3]", address=0xFFFF0000)
+            )
+
+        self.assertIdentical(arr.read_()[:3], Object(self.prog, "int [3]", [0, 1, 2]))
+
+    def test_slice_stop_none(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+
+        self.assertIdentical(arr[1:], Object(self.prog, "int [3]", address=0xFFFF0004))
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            incomplete_arr[1:]
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            ptr[1:]
+
+        self.assertIdentical(arr.read_()[1:], Object(self.prog, "int [3]", [1, 2, 3]))
+
+    def test_slice_start_negative_stop_none(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+
+        self.assertIdentical(arr[-2:], Object(self.prog, "int [6]", address=0xFFFEFFF8))
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            incomplete_arr[-2:]
+        with self.assertRaisesRegex(TypeError, "has no length"):
+            ptr[-2:]
+
+        obj = arr.read_()
+        with self.assertRaisesRegex(OutOfBoundsError, "out of bounds"):
+            obj[-2:]
+
+    def test_slice_start_none_stop_negative(self):
+        arr = Object(self.prog, "int [4]", address=0xFFFF0000)
+        incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
+        ptr = Object(self.prog, "int *", value=0xFFFF0000)
+        for obj in [arr, incomplete_arr, ptr]:
+            self.assertIdentical(
+                obj[:-2], Object(self.prog, "int [0]", address=0xFFFF0000)
+            )
+
+        self.assertIdentical(arr.read_()[:-2], Object(self.prog, "int [0]", []))
+
     def test_cast_primitive_value(self):
         obj = Object(self.prog, "long", value=2**32 + 1)
         self.assertIdentical(cast("int", obj), Object(self.prog, "int", value=1))

commit 54ce785b9ac7691c700aa5ad48068817fc61d0fd

libdrgn: object: avoid signed integer overflow in drgn_object_slice()
end - start can overflow an int64_t. Convert to uint64_t first, which
produces the mathematically correct result without undefined behavior.

Fixes: b7e2099e2822 ("Add array slicing")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/object.c
@@ -1650,7 +1650,8 @@ struct drgn_error *drgn_object_slice(struct drgn_object *res,
 
 	struct drgn_qualified_type array_type;
 	err = drgn_array_type_create(prog, element_type,
-				     end > start ? end - start : 0,
+				     end > start
+				     ? (uint64_t)end - (uint64_t)start : 0,
 				     drgn_type_language(element_type.type),
 				     &array_type.type);
 	if (err)