osandov/drgn

commit 984ab04c5e3c519264e45ad39a649ef63e7b89c0

module: add object
This allows users to get the object which the module was created from.
The primary use case is for Linux kernel modules, to return the "struct
module" associated with the drgn module object.

To simplify the implementation, we've added the restriction that
Program.linux_kernel_loadable_module(), and the associated C APIs, will
only accept objects of type "struct module *".

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/python/module.c
@@ -284,6 +284,35 @@ static int Module_set_build_id(Module *self, PyObject *value, void *arg)
 	return 0;
 }
 
+static DrgnObject *Module_get_object(Module *self, void *arg)
+{
+
+	Program *prog_obj = Module_prog(self);
+	_cleanup_pydecref_ DrgnObject *ret = DrgnObject_alloc(prog_obj);
+	if (!ret)
+		return NULL;
+
+	struct drgn_error *err = drgn_module_object(self->module, &ret->obj);
+	if (err)
+		return set_drgn_error(err);
+	return_ptr(ret);
+}
+
+static int Module_set_object(Module *self, PyObject *value, void *arg)
+{
+	SETTER_NO_DELETE("object", value);
+	if (!PyObject_TypeCheck(value, &DrgnObject_type)) {
+		PyErr_SetString(PyExc_TypeError, "object must be a drgn.Object");
+		return -1;
+	}
+	DrgnObject *object = (DrgnObject *)value;
+
+	struct drgn_error *err = drgn_module_set_object(self->module, &object->obj);
+	if (err)
+		set_drgn_error(err);
+	return 0;
+}
+
 #define MODULE_FILE_STATUS_GETSET(which)					\
 static PyObject *Module_wants_##which##_file(Module *self)			\
 {										\
@@ -400,6 +429,8 @@ static PyGetSetDef Module_getset[] = {
 	 (setter)Module_set_address_range, drgn_Module_address_range_DOC},
 	{"build_id", (getter)Module_get_build_id, (setter)Module_set_build_id,
 	 drgn_Module_build_id_DOC},
+	{"object", (getter)Module_get_object, (setter)Module_set_object,
+	 drgn_Module_object_DOC},
 	{"loaded_file_status", (getter)Module_get_loaded_file_status,
 	 (setter)Module_set_loaded_file_status,
 	 drgn_Module_loaded_file_status_DOC},

_drgn.pyi
@@ -838,12 +838,11 @@ class Program:
         self, module_obj: Object, *, create: Literal[False] = False
     ) -> RelocatableModule:
         """
-        Find a Linux kernel loadable module from a ``struct module`` object.
+        Find a Linux kernel loadable module from a ``struct module *`` object.
 
         Note that kernel modules are represented as relocatable modules.
 
-        :param module_obj: ``struct module`` or ``struct module *`` object for
-            the kernel module.
+        :param module_obj: ``struct module *`` object for the kernel module.
         :return: Relocatable module with a name and address matching
             *module_obj*.
         :raises LookupError: if no matching module has been created
@@ -855,14 +854,13 @@ class Program:
         self, module_obj: Object, *, create: Literal[True]
     ) -> Tuple[RelocatableModule, bool]:
         """
-        Find or create a Linux kernel loadable module from a ``struct module``
+        Find or create a Linux kernel loadable module from a ``struct module *``
         object.
 
         If a new module is created, its :attr:`~Module.address_range` and
         :attr:`~RelocatableModule.section_addresses` are set from *module_obj*.
 
-        :param module_obj: ``struct module`` or ``struct module *`` object for
-            the kernel module.
+        :param module_obj: `struct module *`` object for the kernel module.
         :return: Module and ``True`` if it was newly created or ``False`` if it
             was found.
         """
@@ -1665,6 +1663,14 @@ class Module:
     state/core dump when possible. Otherwise, when a file is assigned to the
     module, it is set to the file's build ID if it is not already set. It can
     also be set manually.
+    """
+    object: Object
+    """
+    The object associated with this module.
+
+    For Linux kernel loadable modules, this is the ``struct module *``
+    associated with the kernel module. For other kinds, this is currently an
+    absent object. The object may be set manually.
     """
     loaded_file_status: ModuleFileStatus
     """Status of the module's :ref:`loaded file <module-loaded-file>`."""

libdrgn/debug_info.c
@@ -321,6 +321,7 @@ struct drgn_error *drgn_module_find_or_create(struct drgn_program *prog,
 
 	module->prog = prog;
 	module->kind = key->kind;
+	drgn_object_init(&module->object, prog);
 	// Linux userspace core dumps usually filter out file-backed mappings
 	// (see coredump_filter in core(5)), so we need the loaded file to read
 	// the text. Additionally, .eh_frame is in the loaded file and not the
@@ -412,6 +413,7 @@ struct drgn_error *drgn_module_find_or_create(struct drgn_program *prog,
 err_name:
 	free(module->name);
 err_module:
+	drgn_object_deinit(&module->object);
 	free(module);
 	return err;
 }
@@ -513,6 +515,7 @@ static void drgn_module_destroy(struct drgn_module *module)
 	drgn_elf_file_destroy(module->loaded_file);
 	free(module->build_id);
 	free(module->name);
+	drgn_object_deinit(&module->object);
 	free(module);
 }
 
@@ -983,6 +986,18 @@ drgn_module_wanted_supplementary_debug_file(struct drgn_module *module,
 	return wanted
 	       ? DRGN_SUPPLEMENTARY_FILE_GNU_DEBUGALTLINK
 	       : DRGN_SUPPLEMENTARY_FILE_NONE;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_module_object(const struct drgn_module *module, struct drgn_object *ret)
+{
+	return drgn_object_copy(ret, &module->object);
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_module_set_object(struct drgn_module *module, const struct drgn_object *obj)
+{
+	return drgn_object_copy(&module->object, obj);
 }
 
 static struct drgn_error *

libdrgn/debug_info.h
@@ -260,6 +260,8 @@ struct drgn_module {
 	struct drgn_module_wanted_supplementary_file *wanted_supplementary_debug_file;
 	/** Node in @ref drgn_debug_info::modules_pending_indexing. */
 	struct drgn_module *pending_indexing_next;
+	/** Object the module was created from */
+	struct drgn_object object;
 };
 
 struct drgn_error *drgn_module_find_or_create(struct drgn_program *prog,

libdrgn/drgn.h
@@ -1314,7 +1314,7 @@ drgn_module_find_or_create_relocatable(struct drgn_program *prog,
 				       struct drgn_module **ret, bool *new_ret);
 
 /**
- * Find a created Linux kernel loadable module from a ``struct module`` object.
+ * Find a created Linux kernel loadable module from a ``struct module *`` object.
  *
  * @param[out] new_ret @c true if the module was newly created, @c false if it
  * was found.
@@ -1324,7 +1324,7 @@ drgn_module_find_linux_kernel_loadable(const struct drgn_object *module_obj,
 				       struct drgn_module **ret);
 
 /**
- * Find a Linux kernel loadable module from a ``struct module`` object, creating
+ * Find a Linux kernel loadable module from a ``struct module *`` object, creating
  * it if it doesn't already exist.
  *
  * @param[out] new_ret @c true if the module was newly created, @c false if it
@@ -1569,6 +1569,26 @@ drgn_module_wanted_supplementary_debug_file(struct drgn_module *module,
 					    const char **supplementary_path_ret,
 					    const void **checksum_ret,
 					    size_t *checksum_len_ret);
+
+/**
+ * Return the object associated with this module.
+ *
+ * For some modules, there may be an object related to it. For example, drgn
+ * automatically identifies the Linux kernel `struct module *` associated with
+ * loadable modules, and associates it with them. Users may set or replace an
+ * associated object with @ref drgn_set_module_object().
+ *
+ * @param[out] ret Initialized object where the module object is placed
+ */
+struct drgn_error *
+drgn_module_object(const struct drgn_module *module, struct drgn_object *ret);
+
+/**
+ * Set the object associated with this module.
+ * @param[in] obj A new (or replacement) object for the module
+ */
+struct drgn_error *
+drgn_module_set_object(struct drgn_module *module, const struct drgn_object *obj);
 
 /** Debugging information finder callback table. */
 struct drgn_debug_info_finder_ops {

libdrgn/linux_kernel.c
@@ -1797,7 +1797,8 @@ kernel_module_set_section_addresses(struct drgn_module *module,
 }
 
 static struct drgn_error *
-kernel_module_find_or_create_internal(const struct drgn_object *module_obj,
+kernel_module_find_or_create_internal(const struct drgn_object *module_ptr,
+				      const struct drgn_object *module_obj,
 				      struct drgn_module **ret, bool *new_ret,
 				      bool create, bool log)
 {
@@ -1896,6 +1897,10 @@ kernel_module_find_or_create_internal(const struct drgn_object *module_obj,
 		return NULL;
 	}
 
+	err = drgn_module_set_object(module, module_ptr);
+	if (err)
+		return err;
+
 	if (layout_in_module)
 		err = drgn_object_member(&val, module_obj, "core_size");
 	else
@@ -1939,60 +1944,47 @@ kernel_module_find_or_create_internal(const struct drgn_object *module_obj,
 }
 
 static struct drgn_error *
-drgn_module_find_or_create_linux_kernel_loadable_internal(const struct drgn_object *module_obj,
+drgn_module_find_or_create_linux_kernel_loadable_internal(const struct drgn_object *module_ptr,
 							  struct drgn_module **ret,
 							  bool *new_ret,
 							  bool create)
 {
 	struct drgn_error *err;
+	struct drgn_program *prog = drgn_object_program(module_ptr);
 
-	// kernel_module_find_or_create_internal() expects a `struct module`
-	// value.
-	struct drgn_object mod;
-	if (drgn_type_kind(drgn_underlying_type(module_obj->type))
-	    == DRGN_TYPE_POINTER) {
-		drgn_object_init(&mod, drgn_object_program(module_obj));
-		err = drgn_object_dereference(&mod, module_obj);
-		if (!err)
-			err = drgn_object_read(&mod, &mod);
-		module_obj = &mod;
-		if (err)
-			goto out;
-	} else if (module_obj->kind != DRGN_OBJECT_VALUE) {
-		drgn_object_init(&mod, drgn_object_program(module_obj));
-		err = drgn_object_read(&mod, module_obj);
-		module_obj = &mod;
-		if (err)
-			goto out;
-	}
+	if (drgn_type_kind(drgn_underlying_type(module_ptr->type))
+	    != DRGN_TYPE_POINTER)
+		return drgn_error_create(DRGN_ERROR_TYPE,
+					 "struct module * is required");
 
-	err = kernel_module_find_or_create_internal(module_obj, ret, new_ret,
-						    create, false);
-out:
-	if (module_obj == &mod)
-		drgn_object_deinit(&mod);
-	return err;
+	DRGN_OBJECT(module_obj, prog);
+	err = drgn_object_dereference(&module_obj, module_ptr);
+	if (err)
+		return err;
+
+	err = drgn_object_read(&module_obj, &module_obj);
+	if (err)
+		return err;
+
+	return kernel_module_find_or_create_internal(module_ptr, &module_obj, ret, new_ret,
+						     create, false);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
-drgn_module_find_linux_kernel_loadable(const struct drgn_object *module_obj,
+drgn_module_find_linux_kernel_loadable(const struct drgn_object *module_ptr,
 				       struct drgn_module **ret)
 {
-	return drgn_module_find_or_create_linux_kernel_loadable_internal(module_obj,
-									 ret,
-									 NULL,
-									 false);
+	return drgn_module_find_or_create_linux_kernel_loadable_internal(module_ptr, ret,
+									 NULL, false);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
-drgn_module_find_or_create_linux_kernel_loadable(const struct drgn_object *module_obj,
+drgn_module_find_or_create_linux_kernel_loadable(const struct drgn_object *module_ptr,
 						 struct drgn_module **ret,
 						 bool *new_ret)
 {
-	return drgn_module_find_or_create_linux_kernel_loadable_internal(module_obj,
-									 ret,
-									 new_ret,
-									 true);
+	return drgn_module_find_or_create_linux_kernel_loadable_internal(module_ptr, ret,
+									 new_ret, true);
 }
 
 static struct drgn_error *
@@ -2003,6 +1995,7 @@ yield_kernel_module(struct linux_kernel_loaded_module_iterator *it,
 	struct drgn_program *prog = it->it.prog;
 
 	DRGN_OBJECT(mod, prog);
+	DRGN_OBJECT(mod_ptr, prog);
 	for (;;) {
 		uint64_t addr;
 		err = drgn_object_read_unsigned(&it->node, &addr);
@@ -2034,12 +2027,12 @@ list_walk_err:
 		}
 		it->module_list_iterations_remaining--;
 
-		err = drgn_object_container_of(&mod, &it->node, it->module_type,
+		err = drgn_object_container_of(&mod_ptr, &it->node, it->module_type,
 					       "list");
 		if (err)
 			goto list_walk_err;
 
-		err = drgn_object_dereference(&mod, &mod);
+		err = drgn_object_dereference(&mod, &mod_ptr);
 		if (err)
 			goto list_walk_err;
 		// We need several fields from the `struct module`. Especially
@@ -2057,8 +2050,8 @@ list_walk_err:
 		if (err)
 			goto list_walk_err;
 
-		err = kernel_module_find_or_create_internal(&mod, ret, new_ret,
-							    true, true);
+		err = kernel_module_find_or_create_internal(&mod_ptr, &mod, ret,
+							    new_ret, true, true);
 		if (err && !drgn_error_is_fatal(err)) {
 			drgn_error_log_warning(prog, err, "ignoring module: ");
 			drgn_error_destroy(err);

commit 2a1e16e33286dc5014668acda475f08234331553

libdrgn: python: don't return success from Module_set_object() with error set
Fixes: 984ab04c5e3c ("module: add object")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/module.c
@@ -386,8 +386,10 @@ static int Module_set_object(Module *self, PyObject *value, void *arg)
 	DrgnObject *object = (DrgnObject *)value;
 
 	struct drgn_error *err = drgn_module_set_object(self->module, &object->obj);
-	if (err)
+	if (err) {
 		set_drgn_error(err);
+		return -1;
+	}
 	return 0;
 }
 