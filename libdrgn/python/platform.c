osandov/drgn

commit b899a10836c00c28420906f9491c50548550932f

Remove register numbers from API and add register aliases
enum drgn_register_number in the public libdrgn API and
drgn.Register.number in the Python bindings are basically exports of
DWARF register numbers. They only exist as a way to identify registers
that's lighter weight than string lookups. libdrgn already has struct
drgn_register, so we can use that to identify registers in the public
API and remove enum drgn_register_number. This has a couple of benefits:
we don't depend on DWARF numbering in our API, and we don't have to
generate drgn.h from the architecture files. The Python bindings can
just use string names for now. If it seems useful, StackFrame.register()
can take a Register in the future, we'll just need to be careful to not
allow Registers from the wrong platform.

While we're changing the API anyways, also change it so that registers
have a list of names instead of one name. This isn't needed for x86-64
at the moment, but will be for architectures that have multiple names
for the same register (like ARM).

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/platform.c
@@ -83,38 +83,19 @@ static PyObject *Platform_get_flags(Platform *self, void *arg)
 
 static PyObject *Platform_get_registers(Platform *self, void *arg)
 {
-	PyObject *tuple;
-	size_t num_registers, i;
-
-	num_registers = drgn_platform_num_registers(self->platform);
-	tuple = PyTuple_New(num_registers);
+	size_t num_registers = drgn_platform_num_registers(self->platform);
+	PyObject *tuple = PyTuple_New(num_registers);
 	if (!tuple)
 		return NULL;
-	for (i = 0; i < num_registers; i++) {
-		const struct drgn_register *reg;
-		PyObject *item;
-		PyObject *tmp;
-
-		reg = drgn_platform_register(self->platform, i);
-		item = PyStructSequence_New(&Register_type);
+	for (size_t i = 0; i < num_registers; i++) {
+		const struct drgn_register *reg =
+			drgn_platform_register(self->platform, i);
+		PyObject *item = Register_type.tp_alloc(&Register_type, 0);
 		if (!item) {
 			Py_DECREF(tuple);
 			return NULL;
 		}
-		tmp = PyUnicode_FromString(drgn_register_name(reg));
-		if (!tmp) {
-			Py_DECREF(item);
-			Py_DECREF(tuple);
-			return NULL;
-		}
-		PyStructSequence_SET_ITEM(item, 0, tmp);
-		tmp = PyLong_FromLong(drgn_register_number(reg));
-		if (!tmp) {
-			Py_DECREF(item);
-			Py_DECREF(tuple);
-			return NULL;
-		}
-		PyStructSequence_SET_ITEM(item, 1, tmp);
+		((Register *)item)->reg = reg;
 		PyTuple_SET_ITEM(tuple, i, item);
 	}
 	return tuple;
@@ -159,17 +140,55 @@ PyTypeObject Platform_type = {
 	.tp_new = (newfunc)Platform_new,
 };
 
-static PyStructSequence_Field Register_fields[] = {
-	{"name", drgn_Register_name_DOC},
-	{"number", drgn_Register_number_DOC},
+static PyObject *Register_richcompare(Register *self, PyObject *other, int op)
+{
+	if (!PyObject_TypeCheck(other, &Register_type) ||
+	    (op != Py_EQ && op != Py_NE))
+		Py_RETURN_NOTIMPLEMENTED;
+	bool ret = self->reg == ((Register *)other)->reg;
+	if (op == Py_NE)
+		ret = !ret;
+	Py_RETURN_BOOL(ret);
+}
+
+static PyObject *Register_get_names(Register *self, void *arg)
+{
+	size_t num_names;
+	const char * const *names = drgn_register_names(self->reg, &num_names);
+	PyObject *ret = PyTuple_New(num_names);
+	for (size_t i = 0; i < num_names; i++) {
+		PyObject *item = PyUnicode_FromString(names[i]);
+		if (!item) {
+			Py_DECREF(ret);
+			return NULL;
+		}
+		PyTuple_SET_ITEM(ret, i, item);
+	}
+	return ret;
+}
+
+static PyObject *Register_repr(Register *self)
+{
+	PyObject *names_obj = Register_get_names(self, NULL);
+	if (!names_obj)
+		return NULL;
+	PyObject *ret = PyUnicode_FromFormat("Register(%R)", names_obj);
+	Py_DECREF(names_obj);
+	return ret;
+}
+
+static PyGetSetDef Register_getset[] = {
+	{"names", (getter)Register_get_names, NULL, drgn_Register_names_DOC},
 	{},
 };
 
-PyStructSequence_Desc Register_desc = {
-	"Register",
-	drgn_Register_DOC,
-	Register_fields,
-	2,
+PyTypeObject Register_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.Register",
+	.tp_basicsize = sizeof(Register),
+	.tp_repr = (reprfunc)Register_repr,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = drgn_Register_DOC,
+	.tp_richcompare = (richcmpfunc)Register_richcompare,
+	.tp_getset = Register_getset,
 };
-
-PyTypeObject Register_type;

libdrgn/platform.c
@@ -124,13 +124,20 @@ drgn_platform_register(const struct drgn_platform *platform, size_t n)
 	return &platform->arch->registers[n];
 }
 
-LIBDRGN_PUBLIC const char *drgn_register_name(const struct drgn_register *reg)
+LIBDRGN_PUBLIC const struct drgn_register *
+drgn_platform_register_by_name(const struct drgn_platform *platform,
+			       const char *name)
 {
-	return reg->name;
+	return drgn_architecture_register_by_name(platform->arch, name);
 }
 
-LIBDRGN_PUBLIC enum drgn_register_number
-drgn_register_number(const struct drgn_register *reg)
+LIBDRGN_PUBLIC const char * const *
+drgn_register_names(const struct drgn_register *reg, size_t *num_names_ret)
 {
-	return reg->number;
+	/*
+	 * We don't support any architectures that have multiple names for the
+	 * same register yet.
+	 */
+	*num_names_ret = 1;
+	return &reg->name;
 }

_drgn.pyi
@@ -847,13 +847,8 @@ class PlatformFlags(enum.Flag):
 class Register:
     """A ``Register`` represents information about a processor register."""
 
-    name: str
-    """Name of this register."""
-
-    number: int
-    """
-    Arbitrary number which uniquely identifies this register on its platform.
-    """
+    names: Sequence[str]
+    """Names of this register."""
 
 host_platform: Platform
 """The platform of the host which is running drgn."""
@@ -1444,13 +1439,13 @@ class StackFrame:
         instruction instead of the return address.
         """
         ...
-    def register(self, reg: Union[str, IntegerLike, Register]) -> int:
+    def register(self, reg: str) -> int:
         """
-        Get the value of the given register at this stack frame. The register
-        can be specified by name (e.g., ``'rax'``), number (see
-        :attr:`Register.number`), or as a :class:`Register`.
+        Get the value of the given register at this stack frame.
 
-        :param reg: Register to get.
+        :param reg: Register name.
+        :raises ValueError: if the register name is not recognized
+        :raises LookupError: if the register value is not known
         """
         ...
     def registers(self) -> Dict[str, int]:

libdrgn/Makefile.am
@@ -80,13 +80,15 @@ libdrgnimpl_la_CFLAGS += $(libkdumpfile_CFLAGS)
 libdrgnimpl_la_LIBADD += $(libkdumpfile_LIBS)
 endif
 
-arch_%.c: arch_%.c.in build-aux/gen_arch.awk build-aux/parse_arch.awk
-	$(AM_V_GEN)gawk -f $(word 3, $^) -f $(word 2, $^) $< > $@
+arch_%.c: arch_%.c.in build-aux/gen_arch.awk
+	$(AM_V_GEN)gawk -f $(word 2, $^) $< > $@
 
-drgn.h: configure.ac build-aux/gen_drgn_h.awk build-aux/parse_arch.awk $(ARCH_INS) drgn.h.in
-	$(AM_V_GEN)gawk -f $(word 2, $^) -f $(word 3, $^) \
-		-v version=@PACKAGE_VERSION@ \
-		$(wordlist 4, $(words $^), $^) > $@
+drgn.h: drgn.h.in configure.ac
+	$(AM_V_GEN)sed \
+		-e 's/@DRGN_VERSION_MAJOR@/$(word 1,$(subst ., ,@PACKAGE_VERSION@))/g' \
+		-e 's/@DRGN_VERSION_MINOR@/$(word 2,$(subst ., ,@PACKAGE_VERSION@))/g' \
+		-e 's/@DRGN_VERSION_PATCH@/$(word 3,$(subst ., ,@PACKAGE_VERSION@))/g' \
+		$< > $@
 
 elfutils_LIBS = elfutils/libdw/libdw.a elfutils/libelf/libelf.a -lz -llzma -lbz2 -ldl
 
@@ -144,8 +146,7 @@ python/docstrings.c: ../_drgn.pyi $(drgndoc_docstrings_deps)
 python/docstrings.h: ../_drgn.pyi $(drgndoc_docstrings_deps)
 	$(AM_V_GEN)$(drgndoc_docstrings) -H -m _drgn:drgn $< > $@
 
-EXTRA_DIST = $(ARCH_INS) build-aux/gen_arch.awk build-aux/gen_constants.py \
-	     build-aux/gen_drgn_h.awk build-aux/parse_arch.awk
+EXTRA_DIST = $(ARCH_INS) build-aux/gen_arch.awk build-aux/gen_constants.py
 
 EXTRA_PROGRAMS = examples/load_debug_info
 

libdrgn/build-aux/.gitignore
@@ -2,5 +2,3 @@
 !/.gitignore
 !/gen_arch.awk
 !/gen_constants.py
-!/gen_drgn_h.awk
-!/parse_arch.awk

libdrgn/build-aux/gen_arch.awk
@@ -1,8 +1,36 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # SPDX-License-Identifier: GPL-3.0+
 
-# This script generates "arch_foo.c" from "arch_foo.c.in". It uses
-# "parse_arch.awk" to parse the input file and generates three definitions:
+# This script generates a drgn architecture support file ("arch_foo.c") from an
+# input file ("arch_foo.c.in").
+#
+# The overall format of the input file is:
+#
+# %{
+# prologue
+# %}
+# declarations
+# %%
+# registers
+# %%
+# epilogue
+#
+# (This is similar to the format used by flex, bison, and gperf.)
+#
+# The declarations section contains additional options. Currently, the only
+# option is the architecture name, which is indicated by a line not starting
+# with "%".
+#
+# The registers section contains a list of register definitions formatted as
+# "name, number", one per line. The ", number" may be omitted, in which case it
+# defaults to the previous number plus one (or zero for the first register).
+# These are saved in the "registers" array; the indices are the register names,
+# and the values are the numbers.
+#
+# Lines outside of the prologue and epilogue sections that start with "#" are
+# ignored.
+#
+# This generates three definitions:
 #
 # 1. An array of register definitions:
 #    static const struct drgn_register registers[];
@@ -17,6 +45,45 @@
 #
 # The prologue and epilogue are copied before and after these definitions,
 # respectively.
+
+BEGIN {
+	state = "DECLARATIONS"
+	arch_name = ""
+	split("", registers)
+	regno = 0
+
+	print "/* Generated by libdrgn/build-aux/gen_arch.awk. */"
+}
+
+function error(msg) {
+	print FILENAME ":" FNR ": error: " msg > "/dev/stderr"
+	exit 1
+}
+
+# Comments.
+
+state != "PROLOGUE" && state != "EPILOGUE" && /^#/ {
+	next
+}
+
+# State transitions.
+
+state == "DECLARATIONS" && $0 == "%{" {
+	state = "PROLOGUE"
+	next
+}
+
+state == "DECLARATIONS" && $0 == "%%" {
+	if (length(arch_name) == 0)
+		error("missing architecture name")
+	state = "REGISTERS"
+	next
+}
+
+state == "PROLOGUE" && $0 == "%}" {
+	state = "DECLARATIONS"
+	next
+}
 
 function add_to_trie(node, s, value,     char) {
 	if (length(s) == 0) {
@@ -49,13 +116,13 @@ function trie_to_switch(node, indent,     char) {
 	print indent "}"
 }
 
-ENDFILE {
-	print "/* Generated by libdrgn/build-aux/gen_arch.awk. */"
+function sanitize(s) {
+	gsub(/[^a-zA-Z0-9_]/, "_", s)
+	return s
+}
 
-	if (length(prologue) != 0)
-		print prologue
-
-	print "static const struct drgn_register registers[] = {"
+state == "REGISTERS" && $0 == "%%" {
+	print "\nstatic const struct drgn_register registers[] = {"
 	i = 0
 	split("", trie)
 	PROCINFO["sorted_in"] = "@val_num_asc"
@@ -78,6 +145,39 @@ ENDFILE {
 	print "\t.registers = registers, \\"
 	print "\t.num_registers = " i ", \\"
 	print "\t.register_by_name = register_by_name"
+	state = "EPILOGUE"
+	next
+}
 
-	printf "%s", epilogue
+# States.
+
+state == "PROLOGUE" || state == "EPILOGUE" {
+	print
+	next
+}
+
+state == "DECLARATIONS" && !/^%/ &&
+match($0, /^\s*(\S+)\s*$/, group) {
+	if (length(arch_name) != 0)
+		error("architecture name redefined")
+	arch_name = group[1]
+	next
+}
+
+state == "REGISTERS" &&
+match($0, /^\s*([^[:space:],]+)\s*(,\s*([[:digit:]]+|0[xX][[:xdigit:]]+))?\s*$/, group) {
+	name = group[1] ""
+	if (3 in group)
+		regno = strtonum(group[3] "")
+	registers[name] = regno++
+	next
+}
+
+/\S/ {
+	error("invalid input in " state)
+}
+
+END {
+	if (state != "EPILOGUE")
+		error("file ended in " state)
 }

libdrgn/build-aux/gen_drgn_h.awk
@@ -1,39 +1,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# SPDX-License-Identifier: GPL-3.0+
-
-# This script generates "drgn.h" from "drgn.h.in" and all of the
-# "arch_foo.c.in" files. It replaces @DRGN_VERSION_*@ based on the "version"
-# variable given on the command line and @ENUM_DRGN_REGISTER_NUMBER@ with the
-# definition generated from the "arch_foo.c.in" files.
-
-BEGIN {
-	if (!match(version, /^([0-9]+)\.([0-9]+)\.([0-9]+)/, version_arr)) {
-		print "could not parse version" > "/dev/stderr"
-		exit 1
-	}
-	enum = "enum drgn_register_number {\n"
-}
-
-ARGIND == ARGC - 1 {
-	gsub(/@DRGN_VERSION_MAJOR@/, version_arr[1])
-	gsub(/@DRGN_VERSION_MINOR@/, version_arr[2])
-	gsub(/@DRGN_VERSION_PATCH@/, version_arr[3])
-	gsub(/@ENUM_DRGN_REGISTER_NUMBER@/, enum)
-	print
-	next
-}
-
-ENDFILE {
-	if (ARGIND == ARGC - 1)
-		exit
-	if (!match(FILENAME, /^([^\/]*\/)*arch_([^\/]*)\.c\.in$/, group)) {
-		print FILENAME ": error: could not parse architecture name" > "/dev/stderr"
-		exit 1
-	}
-	prefix = "DRGN_REGISTER_" toupper(sanitize(arch_name)) "_"
-	PROCINFO["sorted_in"] = "@val_num_asc"
-	for (reg in registers)
-		enum = enum "\t" prefix sanitize(reg) " = " registers[reg] ",\n"
-	if (ARGIND == ARGC - 2)
-		enum = enum "};"
-}

libdrgn/build-aux/parse_arch.awk
@@ -1,122 +1,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# SPDX-License-Identifier: GPL-3.0+
-
-# This script parses the architecture support files in drgn (arch_foo.c.in).
-# The overall format of these files is:
-#
-# %{
-# prologue
-# %}
-# declarations
-# %%
-# registers
-# %%
-# epilogue
-#
-# (This is similar to the format used by flex, bison, and gperf.)
-#
-# This script does not generate any output; it parses the input into several
-# output variables which can be consumed by another script.
-#
-# The prologue and epilogue sections are copied verbatim into the "prologue"
-# and "epilogue" variables.
-#
-# The declarations section contains additional options. Currently, the only
-# option is the architecture name, which is indicated by a line not starting
-# with "%". The name is assigned to the "arch_name" variable.
-#
-# The registers section contains a list of register definitions formatted as
-# "name, number", one per line. The ", number" may be omitted, in which case it
-# defaults to the previous number plus one (or zero for the first register).
-# These are saved in the "registers" array; the indices are the register names,
-# and the values are the numbers.
-#
-# Lines outside of the prologue and epilogue sections that start with "#" are
-# ignored.
-
-function error(msg,    filename) {
-	print FILENAME ":" FNR ": error: " msg > "/dev/stderr"
-	exit 1
-}
-
-function sanitize(s) {
-	gsub(/[^a-zA-Z0-9_]/, "_", s)
-	return s
-}
-
-BEGINFILE {
-	state = "DECLARATIONS"
-	prologue = ""
-	epilogue = ""
-	arch_name = ""
-	split("", registers)
-	regno = 0
-}
-
-# Comments.
-
-state != "PROLOGUE" && state != "EPILOGUE" && /^#/ {
-	next
-}
-
-# State transitions.
-
-state == "DECLARATIONS" && $0 == "%{" {
-	state = "PROLOGUE"
-	next
-}
-
-state == "DECLARATIONS" && $0 == "%%" {
-	if (length(arch_name) == 0)
-		error("missing architecture name")
-	state = "REGISTERS"
-	next
-}
-
-state == "PROLOGUE" && $0 == "%}" {
-	state = "DECLARATIONS"
-	next
-}
-
-state == "REGISTERS" && $0 == "%%" {
-	state = "EPILOGUE"
-	next
-}
-
-# States.
-
-state == "PROLOGUE" {
-	prologue = prologue $0 "\n"
-	next
-}
-
-state == "DECLARATIONS" && !/^%/ &&
-match($0, /^\s*(\S+)\s*$/, group) {
-	if (length(arch_name) != 0)
-		error("architecture name redefined")
-	arch_name = group[1]
-	next
-}
-
-state == "REGISTERS" &&
-match($0, /^\s*([^[:space:],]+)\s*(,\s*([[:digit:]]+|0[xX][[:xdigit:]]+))?\s*$/, group) {
-	name = group[1] ""
-	if (3 in group)
-		regno = strtonum(group[3] "")
-	registers[name] = regno++
-	next
-}
-
-state == "EPILOGUE" {
-	epilogue = epilogue $0 "\n"
-	next
-}
-
-/\S/ {
-	error("invalid input in " state)
-}
-
-ENDFILE {
-	if (state != "EPILOGUE")
-		error("file ended in " state)
-}

commit 41f59f4be88f06a3ce29f59c09bb22f94df74fa0

libdrgn: python: check allocation failure in Register_get_names()
We're missing a NULL check on the return value of PyTuple_New().

Fixes: b899a10836c0 ("Remove register numbers from API and add register aliases")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/platform.c
@@ -147,6 +147,8 @@ static PyObject *Register_get_names(Register *self, void *arg)
 	size_t num_names;
 	const char * const *names = drgn_register_names(self->reg, &num_names);
 	_cleanup_pydecref_ PyObject *ret = PyTuple_New(num_names);
+	if (!ret)
+		return NULL;
 	for (size_t i = 0; i < num_names; i++) {
 		PyObject *item = PyUnicode_FromString(names[i]);
 		if (!item)