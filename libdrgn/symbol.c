osandov/drgn

commit ad57fa1c994f49d7727abbd8a99483603056131a

libdrgn, python: add SymbolIndex
The Symbol Finder API gives us the ability to register a dynamic
callback for symbol lookup. However, many common use cases are satisfied
by a simple static list of symbols. Correct and efficient lookup in this
simple case is rather tricky. Implement a new type, SymbolIndex, which
can take a list of symbols and index them for efficient lookup by name
or address.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/symbol.c
@@ -2,12 +2,17 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <elf.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "binary_search.h"
 #include "drgn_internal.h"
+#include "string_builder.h"
 #include "symbol.h"
 #include "util.h"
+
+DEFINE_VECTOR_FUNCTIONS(symbol_vector);
 
 LIBDRGN_PUBLIC void drgn_symbol_destroy(struct drgn_symbol *sym)
 {
@@ -173,4 +178,268 @@ void drgn_symbol_result_builder_array(struct drgn_symbol_result_builder *builder
 {
 	symbolp_vector_shrink_to_fit(&builder->vector);
 	symbolp_vector_steal(&builder->vector, syms_ret, count_ret);
+}
+
+static int name_compar(const void *lhs, const void *rhs, void *arg)
+{
+	struct drgn_symbol_index *ix = arg;
+	uint32_t left_ix = *(const uint32_t *)lhs;
+	uint32_t right_ix = *(const uint32_t *)rhs;
+	return strcmp(ix->symbols[left_ix].name, ix->symbols[right_ix].name);
+}
+
+static int addr_compar(const void *lhs, const void *rhs)
+{
+	const struct drgn_symbol *left = lhs;
+	const struct drgn_symbol *right = rhs;
+	// returning a simple subtraction would not work well since these are
+	// unsigned
+	if (left->address < right->address)
+		return -1;
+	else if (left->address > right->address)
+		return 1;
+	else
+		return 0;
+}
+
+struct drgn_error *
+drgn_symbol_index_init(struct drgn_symbol *symbols, uint32_t count,
+		       char *buffer, struct drgn_symbol_index *ret)
+{
+	ret->symbols = symbols;
+	ret->num_syms = count;
+	ret->strings = buffer;
+	ret->name_sort = NULL;
+	ret->max_addrs = NULL;
+	drgn_symbol_name_table_init(&ret->htab);
+	ret->name_sort = malloc_array(count, sizeof(ret->name_sort[0]));
+	if (!ret->name_sort)
+		goto enomem;
+	ret->max_addrs = malloc_array(count, sizeof(ret->max_addrs[0]));
+	if (!ret->max_addrs)
+		goto enomem;
+
+	// In many cases (e.g kallsyms), symbols are already sorted by address,
+	// but not always.  Check whether sorted, and if not, sort.
+	for (uint32_t i = 1; i < ret->num_syms; i++) {
+		if (ret->symbols[i - 1].address > ret->symbols[i].address) {
+			qsort(ret->symbols, count, sizeof(ret->symbols[0]), addr_compar);
+			break;
+		}
+	}
+
+	// Kallsyms doesn't include symbol lengths, so symbols are
+	// non-overlapping. But this is not true in general! Symbols may
+	// overlap, which makes address lookup complicated. Rather than using a
+	// complex range data structure, we can use two binary searches, one to
+	// find the first symbol which could overlap with an address, and one to
+	// find the last symbol, and then linearly search that array. This
+	// performs poorly if there are symbols which span many others, but
+	// that's a rare case. In order to do this strategy, we need an array
+	// that contains the maximum address spanned by any symbol at or before
+	// that index.
+	if (ret->num_syms > 0) // in case num_syms == 0
+		ret->max_addrs[0] = ret->symbols[0].address + ret->symbols[0].size;
+	for (uint32_t i = 1; i < ret->num_syms; i++) {
+		uint64_t max_addr = ret->symbols[i].address + ret->symbols[i].size;
+		ret->max_addrs[i] = max(ret->max_addrs[i - 1], max_addr);
+	}
+
+	// Sort the "name_sort" array by name so we get runs of symbols with the
+	// same name
+	for (uint32_t i = 0; i < ret->num_syms; i++)
+		ret->name_sort[i] = i;
+	qsort_arg(ret->name_sort, ret->num_syms, sizeof(ret->name_sort[0]),
+		  name_compar, ret);
+
+	// For each unique symbol name, insert the range of symbol indexes
+	// into the hash table for fast name lookup
+	struct drgn_symbol_name_table_entry entry;
+	uint32_t current = 0;
+	while (current < ret->num_syms) {
+		const char *current_str = ret->symbols[ret->name_sort[current]].name;
+		uint32_t next = current + 1;
+		while (next < ret->num_syms) {
+			const char *next_str = ret->symbols[ret->name_sort[next]].name;
+			if (strcmp(current_str, next_str) != 0)
+				break;
+			next++;
+		}
+
+		entry.key = current_str;
+		entry.value.start = current;
+		entry.value.end = next;
+		if (drgn_symbol_name_table_insert(&ret->htab, &entry, NULL) < 0)
+			goto enomem;
+
+		current = next;
+	}
+	return NULL;
+
+enomem:
+	drgn_symbol_index_deinit(ret);
+	return &drgn_enomem;
+}
+
+void
+drgn_symbol_index_deinit(struct drgn_symbol_index *index)
+{
+	// The symbol array is contiguous and all names come from strings
+	free(index->symbols);
+	free(index->max_addrs);
+	drgn_symbol_name_table_deinit(&index->htab);
+	free(index->strings);
+	free(index->name_sort);
+	// Simplify error handling by ensuring deinit is safe to call twice
+	memset(index, 0, sizeof(*index));
+}
+
+static void address_search_range(struct drgn_symbol_index *index, uint64_t address,
+				 uint32_t *start_ret, uint32_t *end_ret)
+{
+	// First, identify the maximum symbol index which could possibly contain
+	// this address. Think of this as:
+	//   end_ret = bisect_right([s.address for s in symbols], address)
+	#define less_than_start(a, b) (*(a) < (b)->address)
+	*end_ret = binary_search_gt(index->symbols, index->num_syms, &address,
+				    less_than_start);
+	#undef less_than_start
+
+	// Second, identify first symbol index which could possibly contain this
+	// address. We need to use "max_addrs" for this task:
+	//    bisect_right(max_addrs, address)
+	#define less_than_end(a, b) (*(a) < *(b))
+	*start_ret = binary_search_gt(index->max_addrs, index->num_syms, &address,
+				    less_than_end);
+	#undef less_than_end
+}
+
+/** Allocate a copy of the symbol and add to it the builder */
+static bool add_symbol_result(struct drgn_symbol_result_builder *builder,
+			      struct drgn_symbol *symbol)
+{
+	struct drgn_symbol *copy = malloc(sizeof(*copy));
+	if (!copy)
+		return false;
+	*copy = *symbol;
+	if (!drgn_symbol_result_builder_add(builder, copy)) {
+		free(copy);
+		return false;
+	}
+	return true;
+}
+
+struct drgn_error *
+drgn_symbol_index_find(const char *name, uint64_t address,
+		       enum drgn_find_symbol_flags flags, void *arg,
+		       struct drgn_symbol_result_builder *builder)
+{
+	struct drgn_symbol_index *index = arg;
+
+	// Unlike the ELF symbol finder, we don't have any particular rules
+	// about which symbols get priority when looking up a single symbol.
+	// If we decide this logic is critical, it would probably make sense to
+	// move it into the symbol finder's API via the result builder, rather
+	// than reimplementing it here.
+
+	if (flags & DRGN_FIND_SYMBOL_ADDR) {
+		uint32_t start, end;
+		address_search_range(index, address, &start, &end);
+		for (uint32_t i = start; i < end; i++) {
+			struct drgn_symbol *s = &index->symbols[i];
+			if (s->address > address || address >= s->address + s->size)
+				continue;
+			if ((flags & DRGN_FIND_SYMBOL_NAME) &&
+			    strcmp(s->name, name) != 0)
+				continue;
+			if (!add_symbol_result(builder, s))
+				return &drgn_enomem;
+			if (flags & DRGN_FIND_SYMBOL_ONE)
+				break;
+		}
+	} else if (flags & DRGN_FIND_SYMBOL_NAME) {
+		struct drgn_symbol_name_table_iterator it =
+			drgn_symbol_name_table_search(&index->htab, &name);
+		if (!it.entry)
+			return NULL;
+		for (uint32_t i = it.entry->value.start; i < it.entry->value.end; i++) {
+			struct drgn_symbol *s = &index->symbols[index->name_sort[i]];
+			if (!add_symbol_result(builder, s))
+				return &drgn_enomem;
+			if (flags & DRGN_FIND_SYMBOL_ONE)
+				break;
+		}
+	} else {
+		for (int i = 0; i < index->num_syms; i++) {
+			struct drgn_symbol *s = &index->symbols[i];
+			if (!add_symbol_result(builder, s))
+				return &drgn_enomem;
+			if (flags & DRGN_FIND_SYMBOL_ONE)
+				break;
+		}
+	}
+	return NULL;
+}
+
+void
+drgn_symbol_index_builder_init(struct drgn_symbol_index_builder *builder)
+{
+	builder->names = (struct string_builder)STRING_BUILDER_INIT;
+	symbol_vector_init(&builder->symbols);
+}
+
+void
+drgn_symbol_index_builder_deinit(struct drgn_symbol_index_builder *builder)
+{
+	string_builder_deinit(&builder->names);
+	symbol_vector_deinit(&builder->symbols);
+}
+
+bool
+drgn_symbol_index_builder_add(struct drgn_symbol_index_builder *builder,
+			      const struct drgn_symbol *ptr)
+{
+	struct drgn_symbol copy = *ptr;
+
+	// Temporarily store the index into the name
+	copy.name = (char *)builder->names.len;
+	return string_builder_append(&builder->names, ptr->name)
+		&& string_builder_appendc(&builder->names, '\0')
+		&& symbol_vector_append(&builder->symbols, &copy);
+}
+
+struct drgn_error *
+drgn_symbol_index_init_from_builder(struct drgn_symbol_index *index,
+				    struct drgn_symbol_index_builder *builder)
+{
+	size_t names_len = builder->names.len;
+	char *names = string_builder_steal(&builder->names);
+	char *tmp_names = realloc(names, names_len);
+	if (tmp_names)
+		names = tmp_names;
+
+	symbol_vector_shrink_to_fit(&builder->symbols);
+	struct drgn_symbol *symbols;
+	size_t num_syms;
+	symbol_vector_steal(&builder->symbols, &symbols, &num_syms);
+
+	// Now that the name array is finalized, resolve the names to real
+	// pointers. Update the name lifetime to static, reflecting that the
+	// symbol name is owned by the finder whose lifetime is bound to the
+	// program's once it is attached.
+	for (size_t i = 0; i < num_syms; i++) {
+		size_t string_index = (size_t)symbols[i].name;
+		symbols[i].name = &names[string_index];
+		symbols[i].name_lifetime = DRGN_LIFETIME_STATIC;
+	}
+
+	if (num_syms > UINT32_MAX) {
+		free(names);
+		free(symbols);
+		return drgn_error_format(DRGN_ERROR_OUT_OF_BOUNDS,
+					 "too many symbols provided: %zu > %" PRIu32,
+					 num_syms, UINT32_MAX);
+	}
+
+	return drgn_symbol_index_init(symbols, num_syms, names, index);
 }

_drgn.pyi
@@ -1898,6 +1898,69 @@ class Symbol:
     kind: Final[SymbolKind]
     """Kind of entity represented by this symbol."""
 
+class SymbolIndex:
+    """
+    A ``SymbolIndex`` contains a static set of symbols and allows efficient
+    lookup by name and address.
+
+    With :meth:`Program.register_symbol_finder()`, you can add a callback to
+    provide custom symbol finding logic. However, in many cases, all that is
+    necessary is to provide drgn with a list of symbols that you know to be part
+    of the program. This object allows you to do that. It efficiently implements
+    the Symbol Finder API given a static set of symbols. For example::
+
+        >>> prog = drgn.Program()
+        >>> symbol = drgn.Symbol("foo", 0x123, 1, drgn.SymbolBinding.GLOBAL, drgn.SymbolKind.OBJECT)
+        >>> finder = drgn.SymbolIndex([symbol])
+        >>> prog.register_symbol_finder("SymbolIndex", finder, enable_index=0)
+        >>> prog.symbols()
+        [Symbol(name='foo', address=0x123, size=0x1, binding=<SymbolBinding.GLOBAL: 2>, kind=<SymbolKind.OBJECT: 1>)]
+        >>> prog.symbol("bar")
+        Traceback (most recent call last):
+          File "<console>", line 1, in <module>
+        LookupError: not found
+        >>> prog.symbol("foo")
+        Symbol(name='foo', address=0x123, size=0x1, binding=<SymbolBinding.GLOBAL: 2>, kind=<SymbolKind.OBJECT: 1>)
+        >>> prog.symbol(0x100)
+        Traceback (most recent call last):
+          File "<console>", line 1, in <module>
+        LookupError: not found
+        >>> prog.symbol(0x123)
+        Symbol(name='foo', address=0x123, size=0x1, binding=<SymbolBinding.GLOBAL: 2>, kind=<SymbolKind.OBJECT: 1>)
+    """
+
+    def __init__(self, symbols: Iterable[Symbol]) -> None:
+        """
+        Create a ``SymbolIndex`` from a sequence of symbols
+
+        The returned symbol index satisfies the Symbol Finder API. It supports
+        overlapping symbol address ranges and duplicate symbol names. However,
+        in the case of these sorts of conflicts, it doesn't provide any
+        guarantee on the order of the results, or which result is returned when
+        a single symbol is requested.
+
+        :param symbols: An iterable of symbols
+        :returns: A callable object suitable to provide to
+          :meth:`Program.register_symbol_finder()`.
+        """
+
+    def __call__(
+        self,
+        prog: Program,
+        name: Optional[str],
+        address: Optional[int],
+        one: bool,
+    ) -> List[Symbol]:
+        """
+        Lookup symbol by name, address, or both.
+
+        :param prog: (unused) the program looking up this symbol
+        :param name: if given, only return symbols with this name
+        :param address: if given, only return symbols spanning this address
+        :param one: if given, limit the result to a single symbol
+        :returns: a list of matching symbols (empty if none are found)
+        """
+
 class SymbolBinding(enum.Enum):
     """
     A ``SymbolBinding`` describes the linkage behavior and visibility of a

drgn/__init__.py
@@ -70,6 +70,7 @@ from _drgn import (
     StackTrace,
     Symbol,
     SymbolBinding,
+    SymbolIndex,
     SymbolKind,
     Thread,
     Type,
@@ -127,6 +128,7 @@ __all__ = (
     "StackTrace",
     "Symbol",
     "SymbolBinding",
+    "SymbolIndex",
     "SymbolKind",
     "Thread",
     "Type",

libdrgn/Makefile.am
@@ -171,6 +171,7 @@ _drgn_la_SOURCES = python/constants.c \
 		   python/program.c \
 		   python/stack_trace.c \
 		   python/symbol.c \
+		   python/symbol_index.c \
 		   python/test.c \
 		   python/thread.c \
 		   python/type.c \

libdrgn/python/drgnpy.h
@@ -18,6 +18,7 @@
 #include "../hash_table.h"
 #include "../pp.h"
 #include "../program.h"
+#include "../symbol.h"
 
 /* These were added in Python 3.7. */
 #ifndef Py_UNREACHABLE
@@ -107,6 +108,11 @@ typedef struct {
 	 */
 	PyObject *attr_cache;
 } DrgnType;
+
+typedef struct {
+	PyObject_HEAD
+	struct drgn_symbol_index index;
+} SymbolIndex;
 
 typedef struct {
 	PyObject_HEAD
@@ -242,6 +248,7 @@ extern PyTypeObject Register_type;
 extern PyTypeObject StackFrame_type;
 extern PyTypeObject StackTrace_type;
 extern PyTypeObject Symbol_type;
+extern PyTypeObject SymbolIndex_type;
 extern PyTypeObject Thread_type;
 extern PyTypeObject ThreadIterator_type;
 extern PyTypeObject TypeEnumerator_type;

libdrgn/python/main.c
@@ -297,6 +297,7 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	    add_type(m, &StackFrame_type) ||
 	    add_type(m, &StackTrace_type) ||
 	    add_type(m, &Symbol_type) ||
+	    add_type(m, &SymbolIndex_type) ||
 	    add_type(m, &DrgnType_type) ||
 	    add_type(m, &Thread_type) ||
 	    add_type(m, &ThreadIterator_type) ||

libdrgn/python/program.c
@@ -504,6 +504,16 @@ py_symbol_find_fn(const char *name, uint64_t addr,
 		  enum drgn_find_symbol_flags flags, void *arg,
 		  struct drgn_symbol_result_builder *builder)
 {
+	// Fast path for SymbolIndex: don't bother converting to and from Python
+	// types, as this is a C finder. Use Py_TYPE and pointer comparison
+	// directly here to avoid needing to take the GIL for
+	// PyObject_TypeCheck(). SymbolIndex cannot be subclassed, so the logic
+	// for subclass checking is unnecessary anyway.
+	if (Py_TYPE(PyTuple_GET_ITEM(arg, 1)) == &SymbolIndex_type) {
+		SymbolIndex *ix = (SymbolIndex *)PyTuple_GET_ITEM(arg, 1);
+		return drgn_symbol_index_find(name, addr, flags, &ix->index, builder);
+	}
+
 	PyGILState_guard();
 
 	_cleanup_pydecref_ PyObject *name_obj = NULL;

libdrgn/python/symbol_index.c
@@ -1,0 +1,122 @@
+// Copyright (c) 2024 Oracle and/or its affiliates
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "drgnpy.h"
+#include "../symbol.h"
+
+static void SymbolIndex_dealloc(SymbolIndex *self)
+{
+	drgn_symbol_index_deinit(&self->index);
+	Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *SymbolIndex_call(SymbolIndex *self, PyObject *args, PyObject *kwargs)
+{
+	PyObject *prog_obj;
+	struct index_arg address = { .allow_none = true };
+	const char *name;
+	static char *kwnames[] = {"prog", "name", "address", "one", NULL};
+	int single; // 'p' format specifier expects an int, not bool
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OzO&p:__call__", kwnames,
+					 &prog_obj, &name, index_converter, &address,
+					 &single))
+		return NULL;
+
+	unsigned int flags = 0;
+	if (single)
+		flags |= DRGN_FIND_SYMBOL_ONE;
+	if (!address.is_none)
+		flags |= DRGN_FIND_SYMBOL_ADDR;
+	if (name)
+		flags |= DRGN_FIND_SYMBOL_NAME;
+
+	struct drgn_symbol_result_builder builder;
+	drgn_symbol_result_builder_init(&builder, flags & DRGN_FIND_SYMBOL_ONE);
+
+	struct drgn_error *err =
+		drgn_symbol_index_find(name, address.uvalue, flags, &self->index, &builder);
+	if (err)
+		goto error;
+
+	/* We return a list regardless */
+	if (single) {
+		struct drgn_symbol *symbol = drgn_symbol_result_builder_single(&builder);
+		_cleanup_pydecref_ PyObject *list = PyList_New(symbol ? 1 : 0);
+		if (!list)
+			goto error;
+		if (symbol) {
+			PyObject *pysym = Symbol_wrap(symbol, (PyObject *)self);
+			if (!pysym)
+				goto error;
+			PyList_SET_ITEM(list, 0, pysym);
+		}
+		return_ptr(list);
+	} else {
+		struct drgn_symbol **syms;
+		size_t count;
+		drgn_symbol_result_builder_array(&builder, &syms, &count);
+		return Symbol_list_wrap(syms, count, (PyObject *)self);
+	}
+
+	return NULL;
+error:
+	drgn_symbol_result_builder_abort(&builder);
+	return err ? set_drgn_error(err) : NULL;
+}
+
+static PyObject *SymbolIndex_new(PyTypeObject *subtype, PyObject *args, PyObject *kwds)
+{
+	static char *kwnames[] = {"symbols", NULL};
+	PyObject *list_obj;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O", kwnames, &list_obj))
+		return NULL;
+
+	_cleanup_pydecref_ PyObject *iter =
+		PyObject_GetIter(list_obj);
+	if (!iter)
+		return NULL;
+
+	_cleanup_(drgn_symbol_index_builder_deinit)
+		struct drgn_symbol_index_builder builder;
+	drgn_symbol_index_builder_init(&builder);
+
+	for (;;) {
+		_cleanup_pydecref_ PyObject *item = PyIter_Next(iter);
+		if (!item)
+			break;
+		if (!PyObject_TypeCheck(item, &Symbol_type))
+			return PyErr_Format(PyExc_TypeError, "expected sequence of Symbols");
+		Symbol *sym = (Symbol *)item;
+		if (!drgn_symbol_index_builder_add(&builder, sym->sym))
+			return PyErr_NoMemory();
+	}
+
+	if (PyErr_Occurred())
+		return NULL;
+
+	_cleanup_pydecref_ SymbolIndex *index_obj = call_tp_alloc(SymbolIndex);
+	if (!index_obj)
+		return NULL;
+
+	struct drgn_error *err =
+		drgn_symbol_index_init_from_builder(&index_obj->index,
+						    &builder);
+	// On error, the builder and index are already deinitialized
+	if (err)
+		return set_drgn_error(err);
+
+	return (PyObject *)no_cleanup_ptr(index_obj);
+}
+
+PyTypeObject SymbolIndex_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.SymbolIndex",
+	.tp_basicsize = sizeof(SymbolIndex),
+	.tp_dealloc = (destructor)SymbolIndex_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT,
+	.tp_doc = drgn_SymbolIndex_DOC,
+	.tp_call = (ternaryfunc)SymbolIndex_call,
+	.tp_new = SymbolIndex_new,
+};

commit 4ffc0339e91b6d1ff50dd3c152a2e1cb9cd6b5f5

libdrgn: symbol: avoid overflow when iterating up to symbol index num_syms
drgn_symbol_index::num_syms is a uint32_t, so
for (int i = 0; i < index->num_syms; i++) can overflow, resulting in
out-of-bounds reads. Use size_t instead.

Fixes: ad57fa1c994f ("libdrgn, python: add SymbolIndex")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/symbol.c
@@ -391,7 +391,7 @@ drgn_symbol_index_find(const char *name, uint64_t address,
 				break;
 		}
 	} else {
-		for (int i = 0; i < index->num_syms; i++) {
+		for (size_t i = 0; i < index->num_syms; i++) {
 			struct drgn_symbol *s = &index->symbols[i];
 			if (!drgn_symbol_result_builder_add(builder, s))
 				return &drgn_enomem;