osandov/drgn

commit dcae5c7f4d434f204a3ecd7c63d85ce78a1b2a24

libdrgn: fix >64-bit pointer object handling
drgn_object_type_impl() always assigns DRGN_TYPE_POINTER the
DRGN_OBJECT_ENCODING_UNSIGNED encoding. If the object is larger than 64
bits, this breaks our assumptions. Specifically,
drgn_object_read_reference() uses an on-stack char buf[9] for
DRGN_OBJECT_ENCODING_UNSIGNED. Passing it a >64-bit pointer object will
abort on the assert(read_size <= sizeof(buf)) or overflow the buffer
depending on NDEBUG.

Fix drgn_object_type_impl() to use DRGN_OBJECT_ENCODING_UNSIGNED_BIG for
>64-bit pointer objects and handle formatting such objects. The latter
should be the only other fixup needed to be at parity with >64-bit
integer functionality.

Fixes: 243f6fb7d567 ("libdrgn: support value objects with >64-bit integer types")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/object.c
@@ -137,7 +137,10 @@ drgn_object_type_impl(struct drgn_type *type, struct drgn_type *underlying_type,
 		break;
 	}
 	case DRGN_TYPE_POINTER:
-		ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED;
+		if (ret->bit_size <= 64)
+			ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED;
+		else
+			ret->encoding = DRGN_OBJECT_ENCODING_UNSIGNED_BIG;
 		break;
 	case DRGN_TYPE_FLOAT:
 		if (ret->bit_size < 1 || ret->bit_size > 256) {

tests/test_object.py
@@ -347,6 +347,17 @@ class TestReference(MockProgramTestCase):
             ),
         )
 
+    def test_big_pointer(self):
+        value = 0x0123456789ABCDEFFEDCBA9876543210
+        self.add_memory_segment(value.to_bytes(16, "little"), virt_addr=0xFFFF0000)
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.void_type(), size=16),
+            address=0xFFFF0000,
+        )
+        self.assertEqual(obj.value_(), value)
+        self.assertEqual(repr(obj), "Object(prog, 'void *', address=0xffff0000)")
+
     def test_int_bits(self):
         buffer = bytearray(17)
         self.add_memory_segment(buffer, virt_addr=0xFFFF0000)
@@ -861,6 +872,16 @@ class TestValue(MockProgramTestCase):
         )
 
         self._test_big_int_operators(type)
+
+    def test_big_pointer(self):
+        value = 0x0123456789ABCDEFFEDCBA9876543210
+        obj = Object(
+            self.prog, self.prog.pointer_type(self.prog.void_type(), size=16), value
+        )
+        self.assertEqual(obj.value_(), value)
+        self.assertEqual(
+            repr(obj), "Object(prog, 'void *', value=0x123456789abcdeffedcba9876543210)"
+        )
 
     def test_int_bits(self):
         for (
@@ -1800,6 +1821,15 @@ class TestGenericOperators(MockProgramTestCase):
         obj = arr.read_()
         self.assertRaisesRegex(OutOfBoundsError, "out of bounds", obj.__getitem__, -1)
 
+    def test_big_pointer_subscript(self):
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.int_type("int", 4, True), size=16),
+            0x0123456789ABCDEFFEDCBA9876543210,
+        )
+        with self.assertRaises(OverflowError):
+            obj[0]
+
     def test_slice(self):
         arr = Object(self.prog, "int [4]", address=0xFFFF0000)
         incomplete_arr = Object(self.prog, "int []", address=0xFFFF0000)
@@ -1914,6 +1944,15 @@ class TestGenericOperators(MockProgramTestCase):
             )
 
         self.assertIdentical(arr.read_()[:-2], Object(self.prog, "int [0]", []))
+
+    def test_big_pointer_slice(self):
+        obj = Object(
+            self.prog,
+            self.prog.pointer_type(self.prog.int_type("int", 4, True), size=16),
+            0x0123456789ABCDEFFEDCBA9876543210,
+        )
+        with self.assertRaises(OverflowError):
+            obj[0:2]
 
     def test_cast_primitive_value(self):
         obj = Object(self.prog, "long", value=2**32 + 1)

libdrgn/language_c.c
@@ -1633,8 +1633,9 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 	 * Pointers are special because they can have an asterisk prefix if
 	 * we're dereferencing them.
 	 */
-	if (drgn_type_kind(underlying_type) == DRGN_TYPE_POINTER &&
-	    obj->kind != DRGN_OBJECT_ABSENT) {
+	if (drgn_type_kind(underlying_type) == DRGN_TYPE_POINTER
+	    && obj->kind != DRGN_OBJECT_ABSENT
+	    && obj->encoding == DRGN_OBJECT_ENCODING_UNSIGNED) {
 		return c_format_pointer_object(obj, underlying_type, indent,
 					       one_line_columns,
 					       multi_line_columns, options, sb);
@@ -1670,6 +1671,7 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 					 "cannot format void object");
 	case DRGN_TYPE_INT:
 	case DRGN_TYPE_BOOL:
+	case DRGN_TYPE_POINTER:
 		return c_format_int_object(obj, options, sb);
 	case DRGN_TYPE_FLOAT:
 		return c_format_float_object(obj, sb);
@@ -1689,7 +1691,6 @@ c_format_object_impl(const struct drgn_object *obj, size_t indent,
 	case DRGN_TYPE_FUNCTION:
 		return c_format_function_object(obj, sb);
 	case DRGN_TYPE_TYPEDEF:
-	case DRGN_TYPE_POINTER:
 	default:
 		UNREACHABLE();
 	}

tests/test_language_c.py
@@ -3420,6 +3420,16 @@ class TestPrettyPrintObject(MockProgramTestCase):
 }""",
         )
 
+    def test_big_pointer(self):
+        self.assertEqual(
+            Object(
+                self.prog,
+                self.prog.pointer_type(self.prog.void_type(), size=16),
+                0x0123456789ABCDEFFEDCBA9876543210,
+            ).format_(),
+            "(void *)0x123456789abcdeffedcba9876543210",
+        )
+
     def test_integer_base_array(self):
         self.assertEqual(
             Object(self.prog, "int [2]", [0, 13]).format_(integer_base=16),

commit c200dad9fc0a23ba459894c6ff3c6a168e5fad52

libdrgn: object: don't use drgn_object_value_unsigned() for pointers
drgn_object_read_c_string() and drgn_object_container_of() use
drgn_object_value_unsigned() to read the pointer value, which asserts
that obj->encoding == DRGN_OBJECT_ENCODING_UNSIGNED. This isn't true for
big pointers. Use drgn_object_read_unsigned() instead, which gracefully
returns an error.

Fixes: dcae5c7f4d43 ("libdrgn: fix >64-bit pointer object handling")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/object.c
@@ -1058,7 +1058,7 @@ drgn_object_read_c_string(const struct drgn_object *obj, char **ret)
 	underlying_type = drgn_underlying_type(obj->type);
 	switch (drgn_type_kind(underlying_type)) {
 	case DRGN_TYPE_POINTER:
-		err = drgn_object_value_unsigned(obj, &address);
+		err = drgn_object_read_unsigned(obj, &address);
 		if (err)
 			return err;
 		max_size = SIZE_MAX;
@@ -1804,7 +1804,7 @@ drgn_object_container_of(struct drgn_object *res, const struct drgn_object *obj,
 		return err;
 
 	uint64_t address;
-	err = drgn_object_value_unsigned(obj, &address);
+	err = drgn_object_read_unsigned(obj, &address);
 	if (err)
 		return err;
 

tests/test_object.py
@@ -15,6 +15,7 @@ from drgn import (
     Qualifiers,
     TypeMember,
     cast,
+    container_of,
     reinterpret,
     sizeof,
 )
@@ -1852,14 +1853,27 @@ class TestGenericOperators(MockProgramTestCase):
         obj = arr.read_()
         self.assertRaisesRegex(OutOfBoundsError, "out of bounds", obj.__getitem__, -1)
 
-    def test_big_pointer_subscript(self):
+    def test_big_pointer(self):
+        big_pointer_type = self.prog.pointer_type(
+            self.prog.int_type("int", 4, True), size=16
+        )
         obj = Object(
             self.prog,
-            self.prog.pointer_type(self.prog.int_type("int", 4, True), size=16),
+            big_pointer_type,
             0x0123456789ABCDEFFEDCBA9876543210,
         )
         with self.assertRaises(OverflowError):
             obj[0]
+        with self.assertRaises(OverflowError):
+            obj.string_()
+        with self.assertRaises(OverflowError):
+            container_of(
+                obj,
+                self.prog.struct_type(
+                    "foo", 16, (TypeMember(big_pointer_type, "ptr"),)
+                ),
+                "ptr",
+            )
 
     def test_slice(self):
         arr = Object(self.prog, "int [4]", address=0xFFFF0000)