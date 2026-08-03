osandov/drgn

commit 5ad6c222987001a5f92cac5dd4d51cf0b8b9d520

python: raise drgn.ObjectNotFoundError instead of KeyError/LookupError
Add a new drgn.ObjectNotFoundError exception type to use for failed
prog[], prog.object(), prog.variable(), prog.constant(), and
prog.function() lookups. This has a few benefits:

1. It allows catching object lookup errors without the risk of catching
   a stray error from a dict or attribute lookup. See #28.
2. It allows us to have a more descriptive error message. See #527.
3. It makes it possible to get the name that could not be found from the
   exception.

It is a subclass of KeyError (and thus also of LookupError), so this
should be backwards compatible. The implementation is inspired by
AttributeError in CPython.

If we end up liking this, we might want to do the same for object member
lookups.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/error.c
@@ -53,6 +53,34 @@ PyTypeObject FaultError_type = {
 	.tp_init = (initproc)FaultError_init,
 };
 
+static int ObjectNotFoundError_init(PyObject *self, PyObject *args,
+				    PyObject *kwds)
+{
+	if (((PyTypeObject *)PyExc_BaseException)->tp_init(self, args, NULL) < 0)
+		return -1;
+
+	static char *keywords[] = {"name", NULL};
+	_cleanup_pydecref_ PyObject *empty_tuple = PyTuple_New(0);
+	if (!empty_tuple)
+		return -1;
+	PyObject *name;
+	if (!PyArg_ParseTupleAndKeywords(empty_tuple, kwds,
+					 "|$O:ObjectNotFoundError", keywords,
+					 &name))
+		return -1;
+
+	return PyObject_SetAttrString(self, "name", name);
+}
+
+PyTypeObject ObjectNotFoundError_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "_drgn.ObjectNotFoundError",
+	.tp_basicsize = sizeof(PyBaseExceptionObject),
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+	.tp_doc = drgn_ObjectNotFoundError_DOC,
+	.tp_init = (initproc)ObjectNotFoundError_init,
+};
+
 static struct drgn_error drgn_error_python = {
 	.code = DRGN_ERROR_OTHER,
 	.message = "error in Python callback",

_drgn.pyi
@@ -123,9 +123,7 @@ class Program:
         Implement ``self[name]``. Get the object (variable, constant, or
         function) with the given name.
 
-        This is equivalent to ``prog.object(name)`` except that this raises
-        :exc:`KeyError` instead of :exc:`LookupError` if no objects with the
-        given name are found.
+        This is equivalent to ``prog.object(name)``.
 
         If there are multiple objects with the same name, one is returned
         arbitrarily. In this case, the :meth:`variable()`, :meth:`constant()`,
@@ -160,8 +158,8 @@ class Program:
         :param name: The variable name.
         :param filename: The source code file that contains the definition. See
             :ref:`api-filenames`.
-        :raises LookupError: if no variables with the given name are found in
-            the given file
+        :raises ObjectNotFoundError: if no variables with the given name are
+            found in the given file
         """
         ...
 
@@ -182,8 +180,8 @@ class Program:
         :param name: The constant name.
         :param filename: The source code file that contains the definition. See
             :ref:`api-filenames`.
-        :raises LookupError: if no constants with the given name are found in
-            the given file
+        :raises ObjectNotFoundError: if no constants with the given name are
+            found in the given file
         """
         ...
 
@@ -200,8 +198,8 @@ class Program:
         :param name: The function name.
         :param filename: The source code file that contains the definition. See
             :ref:`api-filenames`.
-        :raises LookupError: if no functions with the given name are found in
-            the given file
+        :raises ObjectNotFoundError: if no functions with the given name are
+            found in the given file
         """
         ...
 
@@ -222,8 +220,8 @@ class Program:
         :param flags: Flags indicating what kind of object to look for.
         :param filename: The source code file that contains the definition. See
             :ref:`api-filenames`.
-        :raises LookupError: if no objects with the given name are found in
-            the given file
+        :raises ObjectNotFoundError: if no objects with the given name are
+            found in the given file
         """
         ...
 
@@ -1350,6 +1348,13 @@ class FindObjectFlags(enum.Flag):
     ""
     ANY = ...
     ""
+
+class ObjectNotFoundError(KeyError):
+    def __init__(self, *args: object, name: str) -> None:
+        """Error raised when an object is not found in a program."""
+        ...
+    name: str
+    """Object name that was not found."""
 
 class DebugInfoOptions:
     """

drgn/__init__.py
@@ -64,6 +64,7 @@ from _drgn import (
     NoDefaultProgramError,
     Object,
     ObjectAbsentError,
+    ObjectNotFoundError,
     OutOfBoundsError,
     Path,
     Platform,
@@ -137,6 +138,7 @@ __all__ = (
     "NoDefaultProgramError",
     "Object",
     "ObjectAbsentError",
+    "ObjectNotFoundError",
     "OutOfBoundsError",
     "Path",
     "Platform",

libdrgn/python/drgnpy.h
@@ -295,6 +295,7 @@ extern PyTypeObject ModuleIterator_type;
 extern PyTypeObject ModuleSectionAddressesIterator_type;
 extern PyTypeObject Module_type;
 extern PyTypeObject ObjectIterator_type;
+extern PyTypeObject ObjectNotFoundError_type;
 extern PyTypeObject Platform_type;
 extern PyTypeObject Program_type;
 extern PyTypeObject Register_type;

libdrgn/python/main.c
@@ -338,6 +338,14 @@ DRGNPY_PUBLIC PyMODINIT_FUNC PyInit__drgn(void)
 	if (add_type(m, &FaultError_type))
 		goto err;
 
+	ObjectNotFoundError_type.tp_base = (PyTypeObject *)PyExc_KeyError;
+	// KeyError.__str__() returns repr(args[0]). Use BaseException.__str__()
+	// instead, which returns str(args[0]).
+	ObjectNotFoundError_type.tp_str =
+		((PyTypeObject *)PyExc_BaseException)->tp_str;
+	if (add_type(m, &ObjectNotFoundError_type))
+		goto err;
+
 	PyObject *host_platform_obj = Platform_wrap(&drgn_host_platform);
 	if (!host_platform_obj)
 		goto err;

libdrgn/python/program.c
@@ -1527,24 +1527,53 @@ static PyObject *Program_find_type(Program *self, PyObject *args, PyObject *kwds
 	return DrgnType_wrap(qualified_type);
 }
 
-static DrgnObject *Program_find_object(Program *self, const char *name,
-				       struct path_arg *filename,
+static void *set_object_not_found_error(struct drgn_error *err, PyObject *name)
+{
+	_cleanup_pydecref_ PyObject *args = Py_BuildValue("(s)", err->message);
+	drgn_error_destroy(err);
+	if (!args)
+		return NULL;
+
+	_cleanup_pydecref_ PyObject *kwargs =
+		Py_BuildValue("{sO}", "name", name);
+	if (!kwargs)
+		return NULL;
+
+	_cleanup_pydecref_ PyObject *exc =
+		PyObject_Call((PyObject *)&ObjectNotFoundError_type, args,
+			      kwargs);
+	if (exc)
+		PyErr_SetObject((PyObject *)&ObjectNotFoundError_type, exc);
+	return NULL;
+}
+
+static DrgnObject *Program_find_object(Program *self, PyObject *name_obj,
+				       const char *filename,
 				       enum drgn_find_object_flags flags)
 {
 	struct drgn_error *err;
+
+	if (!PyUnicode_Check(name_obj)) {
+		PyErr_Format(PyExc_TypeError, "name must be str, not %.200s",
+			     Py_TYPE(name_obj)->tp_name);
+		return NULL;
+	}
+	const char *name = PyUnicode_AsUTF8(name_obj);
+	if (!name)
+		return NULL;
 
 	_cleanup_pydecref_ DrgnObject *ret = DrgnObject_alloc(self);
 	if (!ret)
 		return NULL;
 	bool clear = set_drgn_in_python();
-	err = drgn_program_find_object(&self->prog, name, filename->path, flags,
+	err = drgn_program_find_object(&self->prog, name, filename, flags,
 				       &ret->obj);
 	if (clear)
 		clear_drgn_in_python();
-	if (err) {
-		set_drgn_error(err);
-		return NULL;
-	}
+	if (err && err->code == DRGN_ERROR_LOOKUP)
+		return set_object_not_found_error(err, name_obj);
+	else if (err)
+		return set_drgn_error(err);
 	return_ptr(ret);
 }
 
@@ -1552,33 +1581,31 @@ static DrgnObject *Program_object(Program *self, PyObject *args,
 				  PyObject *kwds)
 {
 	static char *keywords[] = {"name", "flags", "filename", NULL};
-	const char *name;
+	PyObject *name;
 	struct enum_arg flags = {
 		.type = FindObjectFlags_class,
 		.value = DRGN_FIND_OBJECT_ANY,
 	};
 	PATH_ARG(filename, .allow_none = true);
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|O&O&:object", keywords,
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|O&O&:object", keywords,
 					 &name, enum_converter, &flags,
 					 path_converter, &filename))
 		return NULL;
 
-	return Program_find_object(self, name, &filename, flags.value);
+	return Program_find_object(self, name, filename.path, flags.value);
 }
 
 static DrgnObject *Program_constant(Program *self, PyObject *args,
 				    PyObject *kwds)
 {
 	static char *keywords[] = {"name", "filename", NULL};
-	const char *name;
+	PyObject *name;
 	PATH_ARG(filename, .allow_none = true);
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|O&:constant", keywords,
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|O&:constant", keywords,
 					 &name, path_converter, &filename))
 		return NULL;
 
-	return Program_find_object(self, name, &filename,
+	return Program_find_object(self, name, filename.path,
 				   DRGN_FIND_OBJECT_CONSTANT);
 }
 
@@ -1586,14 +1613,13 @@ static DrgnObject *Program_function(Program *self, PyObject *args,
 				    PyObject *kwds)
 {
 	static char *keywords[] = {"name", "filename", NULL};
-	const char *name;
+	PyObject *name;
 	PATH_ARG(filename, .allow_none = true);
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|O&:function", keywords,
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|O&:function", keywords,
 					 &name, path_converter, &filename))
 		return NULL;
 
-	return Program_find_object(self, name, &filename,
+	return Program_find_object(self, name, filename.path,
 				   DRGN_FIND_OBJECT_FUNCTION);
 }
 
@@ -1601,14 +1627,13 @@ static DrgnObject *Program_variable(Program *self, PyObject *args,
 				    PyObject *kwds)
 {
 	static char *keywords[] = {"name", "filename", NULL};
-	const char *name;
+	PyObject *name;
 	PATH_ARG(filename, .allow_none = true);
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "s|O&:variable", keywords,
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|O&:variable", keywords,
 					 &name, path_converter, &filename))
 		return NULL;
 
-	return Program_find_object(self, name, &filename,
+	return Program_find_object(self, name, filename.path,
 				   DRGN_FIND_OBJECT_VARIABLE);
 }
 
@@ -1818,36 +1843,11 @@ static PyObject *Program__log(Program *self, PyObject *args, PyObject *kwds)
 
 static DrgnObject *Program_subscript(Program *self, PyObject *key)
 {
-	struct drgn_error *err;
-
 	if (!PyUnicode_Check(key)) {
 		PyErr_SetObject(PyExc_KeyError, key);
 		return NULL;
 	}
-
-	const char *name = PyUnicode_AsUTF8(key);
-	if (!name)
-		return NULL;
-
-	_cleanup_pydecref_ DrgnObject *ret = DrgnObject_alloc(self);
-	if (!ret)
-		return NULL;
-
-	bool clear = set_drgn_in_python();
-	err = drgn_program_find_object(&self->prog, name, NULL,
-				       DRGN_FIND_OBJECT_ANY, &ret->obj);
-	if (clear)
-		clear_drgn_in_python();
-	if (err) {
-		if (err->code == DRGN_ERROR_LOOKUP) {
-			drgn_error_destroy(err);
-			PyErr_SetObject(PyExc_KeyError, key);
-		} else {
-			set_drgn_error(err);
-		}
-		return NULL;
-	}
-	return_ptr(ret);
+	return Program_find_object(self, key, NULL, DRGN_FIND_OBJECT_ANY);
 }
 
 static int Program_contains(Program *self, PyObject *key)

tests/test_dwarf.py
@@ -15,6 +15,7 @@ from drgn import (
     FindObjectFlags,
     Language,
     Object,
+    ObjectNotFoundError,
     Program,
     ProgramFlags,
     Qualifiers,
@@ -4570,7 +4571,7 @@ class TestObjects(TestCase):
         )
         self.assertIdentical(prog.object("abs", FindObjectFlags.FUNCTION), prog["abs"])
         self.assertRaisesRegex(
-            LookupError,
+            ObjectNotFoundError,
             "could not find variable",
             prog.object,
             "abs",
@@ -4792,7 +4793,7 @@ class TestObjects(TestCase):
         )
         self.assertIdentical(prog.object("x", FindObjectFlags.VARIABLE), prog["x"])
         self.assertRaisesRegex(
-            LookupError,
+            ObjectNotFoundError,
             "could not find constant",
             prog.object,
             "x",
@@ -6824,7 +6825,7 @@ class TestObjects(TestCase):
 
     def test_not_found(self):
         prog = dwarf_program(int_die)
-        self.assertRaisesRegex(LookupError, "could not find", prog.object, "y")
+        self.assertRaisesRegex(ObjectNotFoundError, "could not find", prog.object, "y")
 
 
 class TestScopes(TestCase):

tests/test_program.py
@@ -15,6 +15,7 @@ from drgn import (
     Language,
     NoDefaultProgramError,
     Object,
+    ObjectNotFoundError,
     Platform,
     PlatformFlags,
     Program,
@@ -105,46 +106,65 @@ class TestProgram(TestCase):
 
         self.assertEqual(prog.read(ctypes.addressof(buf), len(data)), data)
 
-    def test_lookup_error(self):
+    def test_object_not_found_error(self):
         prog = mock_program()
-        self.assertRaisesRegex(
-            LookupError, "^could not find constant 'foo'$", prog.constant, "foo"
-        )
-        self.assertRaisesRegex(
-            LookupError,
-            "^could not find constant 'foo' in 'foo.c'$",
-            prog.constant,
-            "foo",
-            "foo.c",
-        )
-        self.assertRaisesRegex(
-            LookupError, "^could not find function 'foo'$", prog.function, "foo"
-        )
-        self.assertRaisesRegex(
-            LookupError,
-            "^could not find function 'foo' in 'foo.c'$",
-            prog.function,
-            "foo",
-            "foo.c",
-        )
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find constant 'foo'$"
+        ) as cm:
+            prog.constant("foo")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find constant 'foo' in 'foo.c'$"
+        ) as cm:
+            prog.constant("foo", "foo.c")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find function 'foo'$"
+        ) as cm:
+            prog.function("foo")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find function 'foo' in 'foo.c'$"
+        ) as cm:
+            prog.function("foo", "foo.c")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find variable 'foo'$"
+        ) as cm:
+            prog.variable("foo")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find variable 'foo' in 'foo.c'$"
+        ) as cm:
+            prog.variable("foo", "foo.c")
+        self.assertEqual(cm.exception.name, "foo")
+
+        with self.assertRaisesRegex(
+            ObjectNotFoundError, "^could not find 'foo'$"
+        ) as cm:
+            prog["foo"]
+        self.assertEqual(cm.exception.name, "foo")
+
+        # If name isn't a string, prog.object(name) should raise TypeError, and
+        # prog[name] should raise KeyError (not ObjectNotFoundError).
+        self.assertRaises(TypeError, prog.object, 9)
+        with self.assertRaises(KeyError) as cm:
+            prog[9]
+        self.assertIs(type(cm.exception), KeyError)
+
+    def test_type_lookup_error(self):
+        prog = mock_program()
+
         self.assertRaisesRegex(LookupError, "^could not find 'foo'$", prog.type, "foo")
         self.assertRaisesRegex(
             LookupError, "^could not find 'foo' in 'foo.c'$", prog.type, "foo", "foo.c"
         )
-        self.assertRaisesRegex(
-            LookupError, "^could not find variable 'foo'$", prog.variable, "foo"
-        )
-        self.assertRaisesRegex(
-            LookupError,
-            "^could not find variable 'foo' in 'foo.c'$",
-            prog.variable,
-            "foo",
-            "foo.c",
-        )
-        # prog[key] should raise KeyError instead of LookupError.
-        self.assertRaises(KeyError, prog.__getitem__, "foo")
-        # Even for non-strings.
-        self.assertRaises(KeyError, prog.__getitem__, 9)
 
     def test_flags(self):
         self.assertIsInstance(mock_program().flags, ProgramFlags)

commit b6452bfd92173ee1f212e895816bbdc980eb2a69

libdrgn: python: fix ObjectNotFoundError() call without name
If name isn't passed, we pass uninitialized garbage to
PyObject_SetAttrString(). Initialize it to Py_None.

Fixes: 5ad6c2229870 ("python: raise drgn.ObjectNotFoundError instead of KeyError/LookupError")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/error.c
@@ -64,7 +64,7 @@ static int ObjectNotFoundError_init(PyObject *self, PyObject *args,
 	_cleanup_pydecref_ PyObject *empty_tuple = PyTuple_New(0);
 	if (!empty_tuple)
 		return -1;
-	PyObject *name;
+	PyObject *name = Py_None;
 	if (!PyArg_ParseTupleAndKeywords(empty_tuple, kwds,
 					 "|$O:ObjectNotFoundError", keywords,
 					 &name))