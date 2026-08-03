osandov/drgn

commit 72b4aa9669a2494a735d72dd7ae09bed0c3d194d

libdrgn: clean up object initialization
Rename struct drgn_object_type to struct drgn_operand_type, add a new
struct drgn_object_type which contains all of the type-related fields
from struct drgn_object, and use it to implement drgn_object_type() and
drgn_object_type_operand(), which are replacements for
drgn_object_set_common() and drgn_object_type_encoding_and_size(). This
cleans up a lot of the boilerplate around initializing objects.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -52,13 +52,13 @@ static int DrgnObject_literal(struct drgn_object *res, PyObject *literal)
 static int serialize_py_object(struct drgn_program *prog, char *buf,
 			       uint64_t buf_bit_size, uint64_t bit_offset,
 			       PyObject *value_obj,
-			       struct drgn_object_type *type,
+			       const struct drgn_object_type *type,
 			       bool little_endian);
 
 static int serialize_compound_value(struct drgn_program *prog, char *buf,
 				    uint64_t buf_bit_size, uint64_t bit_offset,
 				    PyObject *value_obj,
-				    struct drgn_object_type *type,
+				    const struct drgn_object_type *type,
 				    bool little_endian)
 {
 	struct drgn_error *err;
@@ -117,13 +117,11 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 			goto out;
 		}
 
-		struct drgn_object_type member_type = {
-			.type = member_qualified_type.type,
-			.qualifiers = member_qualified_type.qualifiers,
-			.underlying_type =
-				drgn_underlying_type(member_qualified_type.type),
-			.bit_field_size = member_bit_field_size,
-		};
+		struct drgn_object_type member_type;
+		err = drgn_object_type(member_qualified_type,
+				       member_bit_field_size, &member_type);
+		if (err)
+			goto out;
 		if (serialize_py_object(prog, buf, buf_bit_size,
 					bit_offset + member->bit_offset,
 					PyTuple_GET_ITEM(item, 1),
@@ -140,35 +138,26 @@ out:
 static int serialize_array_value(struct drgn_program *prog, char *buf,
 				 uint64_t buf_bit_size, uint64_t bit_offset,
 				 PyObject *value_obj,
-				 struct drgn_object_type *type,
+				 const struct drgn_object_type *type,
 				 bool little_endian)
 {
 	struct drgn_error *err;
-	struct drgn_qualified_type qualified_type;
-	struct drgn_object_type element_type;
-	uint64_t element_bit_size, length;
-	PyObject *seq;
-	size_t seq_length, i;
 
-	qualified_type = drgn_type_type(type->underlying_type);
-	element_type.type = qualified_type.type;
-	element_type.qualifiers = qualified_type.qualifiers;
-	element_type.underlying_type = drgn_underlying_type(element_type.type);
-	element_type.bit_field_size = 0;
-	err = drgn_type_bit_size(element_type.underlying_type,
-				 &element_bit_size);
+	struct drgn_object_type element_type;
+	err = drgn_object_type(drgn_type_type(type->underlying_type), 0,
+			       &element_type);
 	if (err) {
 		set_drgn_error(err);
 		return -1;
 	}
 
-	length = drgn_type_length(type->underlying_type);
+	uint64_t length = drgn_type_length(type->underlying_type);
 	if (length > PY_SSIZE_T_MAX) {
 		PyErr_NoMemory();
 		return -1;
 	}
 
-	seq = PySequence_Fast(value_obj, "");
+	PyObject *seq = PySequence_Fast(value_obj, "");
 	if (!seq) {
 		if (PyErr_ExceptionMatches(PyExc_TypeError)) {
 			set_error_type_name("'%s' value must be iterable",
@@ -176,7 +165,7 @@ static int serialize_array_value(struct drgn_program *prog, char *buf,
 		}
 		return -1;
 	}
-	seq_length = PySequence_Fast_GET_SIZE(seq);
+	size_t seq_length = PySequence_Fast_GET_SIZE(seq);
 	if (seq_length > length) {
 		Py_DECREF(seq);
 		PyErr_SetString(PyExc_ValueError,
@@ -184,9 +173,9 @@ static int serialize_array_value(struct drgn_program *prog, char *buf,
 		return -1;
 	}
 
-	for (i = 0; i < seq_length; i++) {
+	for (size_t i = 0; i < seq_length; i++) {
 		if (serialize_py_object(prog, buf, buf_bit_size,
-					bit_offset + i * element_bit_size,
+					bit_offset + i * element_type.bit_size,
 					PySequence_Fast_GET_ITEM(seq, i),
 					&element_type, little_endian) == -1) {
 			Py_DECREF(seq);
@@ -201,26 +190,13 @@ static int serialize_array_value(struct drgn_program *prog, char *buf,
 static int serialize_py_object(struct drgn_program *prog, char *buf,
 			       uint64_t buf_bit_size, uint64_t bit_offset,
 			       PyObject *value_obj,
-			       struct drgn_object_type *type,
+			       const struct drgn_object_type *type,
 			       bool little_endian)
 {
 	struct drgn_error *err;
-	enum drgn_object_encoding encoding;
-	uint64_t bit_size, bit_end;
 
-	err = drgn_object_type_encoding_and_size(type, &encoding, &bit_size);
-	if (err) {
-		set_drgn_error(err);
-		return -1;
-	}
-
-	err = sanity_check_object(encoding, type->bit_field_size, bit_size);
-	if (err) {
-		set_drgn_error(err);
-		return -1;
-	}
-
-	if (__builtin_add_overflow(bit_offset, bit_size, &bit_end) ||
+	uint64_t bit_end;
+	if (__builtin_add_overflow(bit_offset, type->bit_size, &bit_end) ||
 	    bit_end > buf_bit_size) {
 		err = drgn_error_create(DRGN_ERROR_OUT_OF_BOUNDS,
 					"out of bounds of value");
@@ -228,56 +204,55 @@ static int serialize_py_object(struct drgn_program *prog, char *buf,
 		return -1;
 	}
 
-	switch (encoding) {
+	switch (type->encoding) {
 	case DRGN_OBJECT_ENCODING_SIGNED:
 	case DRGN_OBJECT_ENCODING_UNSIGNED: {
-		PyObject *long_obj;
-		union {
-			int64_t svalue;
-			uint64_t uvalue;
-		} tmp;
-
 		if (!PyNumber_Check(value_obj)) {
 			set_error_type_name("'%s' value must be number",
 					    drgn_object_type_qualified(type));
 			return -1;
 		}
-		long_obj = PyNumber_Long(value_obj);
+		PyObject *long_obj = PyNumber_Long(value_obj);
 		if (!long_obj)
 			return -1;
+		union {
+			int64_t svalue;
+			uint64_t uvalue;
+		} tmp;
 		tmp.uvalue = PyLong_AsUnsignedLongLongMask(long_obj);
 		Py_DECREF(long_obj);
 		if (tmp.uvalue == (unsigned long long)-1 && PyErr_Occurred())
 			return -1;
-		if (encoding == DRGN_OBJECT_ENCODING_SIGNED)
-			tmp.svalue = truncate_signed(tmp.svalue, bit_size);
-		else
-			tmp.uvalue = truncate_unsigned(tmp.uvalue, bit_size);
-		serialize_bits(buf, bit_offset, tmp.uvalue, bit_size,
+		if (type->encoding == DRGN_OBJECT_ENCODING_SIGNED) {
+			tmp.svalue = truncate_signed(tmp.svalue,
+						     type->bit_size);
+		} else {
+			tmp.uvalue = truncate_unsigned(tmp.uvalue,
+						       type->bit_size);
+		}
+		serialize_bits(buf, bit_offset, tmp.uvalue, type->bit_size,
 			       little_endian);
 		return 0;
 	}
 	case DRGN_OBJECT_ENCODING_FLOAT: {
+		if (!PyNumber_Check(value_obj)) {
+			set_error_type_name("'%s' value must be number",
+					    drgn_object_type_qualified(type));
+			return -1;
+		}
+		double fvalue = PyFloat_AsDouble(value_obj);
+		if (fvalue == -1.0 && PyErr_Occurred())
+			return -1;
 		union {
 			uint64_t uvalue;
 			double fvalue64;
 			float fvalue32;
 		} tmp;
-		double fvalue;
-
-		if (!PyNumber_Check(value_obj)) {
-			set_error_type_name("'%s' value must be number",
-					    drgn_object_type_qualified(type));
-			return -1;
-		}
-		fvalue = PyFloat_AsDouble(value_obj);
-		if (fvalue == -1.0 && PyErr_Occurred())
-			return -1;
-		if (bit_size == 64)
+		if (type->bit_size == 64)
 			tmp.fvalue64 = fvalue;
 		else
 			tmp.fvalue32 = fvalue;
-		serialize_bits(buf, bit_offset, tmp.uvalue, bit_size,
+		serialize_bits(buf, bit_offset, tmp.uvalue, type->bit_size,
 			       little_endian);
 		return 0;
 	}
@@ -319,16 +294,13 @@ static int buffer_object_from_value(struct drgn_object *res,
 	}
 
 	struct drgn_object_type type;
-	enum drgn_object_encoding encoding;
-	uint64_t bit_size;
-	err = drgn_object_set_common(qualified_type, 0, &type, &encoding,
-				     &bit_size);
+	err = drgn_object_type(qualified_type, 0, &type);
 	if (err) {
 		set_drgn_error(err);
 		return -1;
 	}
 
-	uint64_t size = drgn_value_size(bit_size);
+	uint64_t size = drgn_value_size(type.bit_size);
 	if (size > SIZE_MAX) {
 		PyErr_NoMemory();
 		return -1;
@@ -347,14 +319,14 @@ static int buffer_object_from_value(struct drgn_object *res,
 	}
 	memset(buf, 0, size);
 
-	if (serialize_py_object(drgn_object_program(res), buf, bit_size, 0,
+	if (serialize_py_object(drgn_object_program(res), buf, type.bit_size, 0,
 				value_obj, &type, little_endian) == -1) {
 		if (buf != value.ibuf)
 			free(buf);
 		return -1;
 	}
 
-	drgn_object_reinit(res, &type, encoding, bit_size, DRGN_OBJECT_VALUE);
+	drgn_object_reinit(res, &type, DRGN_OBJECT_VALUE);
 	res->value = value;
 	res->little_endian = little_endian;
 	return 0;

libdrgn/debug_info.c
@@ -1324,11 +1324,7 @@ drgn_object_from_dwarf_constant(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
 				Dwarf_Attribute *attr, struct drgn_object *ret)
 {
 	struct drgn_object_type type;
-	enum drgn_object_encoding encoding;
-	uint64_t bit_size;
-	struct drgn_error *err = drgn_object_set_common(qualified_type, 0,
-							&type, &encoding,
-							&bit_size);
+	struct drgn_error *err = drgn_object_type(qualified_type, 0, &type);
 	if (err)
 		return err;
 	Dwarf_Block block;
@@ -1337,30 +1333,29 @@ drgn_object_from_dwarf_constant(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
 		err = dwarf_die_is_little_endian(die, true, &little_endian);
 		if (err)
 			return err;
-		if (block.length < drgn_value_size(bit_size)) {
+		if (block.length < drgn_value_size(type.bit_size)) {
 			return drgn_error_create(DRGN_ERROR_OTHER,
 						 "DW_AT_const_value block is too small");
 		}
 		return drgn_object_set_from_buffer_internal(ret, &type,
-							    encoding, bit_size,
 							    block.data, 0,
 							    little_endian);
-	} else if (encoding == DRGN_OBJECT_ENCODING_SIGNED) {
+	} else if (type.encoding == DRGN_OBJECT_ENCODING_SIGNED) {
 		Dwarf_Sword svalue;
 		if (dwarf_formsdata(attr, &svalue)) {
 			return drgn_error_create(DRGN_ERROR_OTHER,
 						 "invalid DW_AT_const_value");
 		}
-		return drgn_object_set_signed_internal(ret, &type, bit_size,
-						       svalue);
-	} else if (encoding == DRGN_OBJECT_ENCODING_UNSIGNED) {
+		drgn_object_set_signed_internal(ret, &type, svalue);
+		return NULL;
+	} else if (type.encoding == DRGN_OBJECT_ENCODING_UNSIGNED) {
 		Dwarf_Word uvalue;
 		if (dwarf_formudata(attr, &uvalue)) {
 			return drgn_error_create(DRGN_ERROR_OTHER,
 						 "invalid DW_AT_const_value");
 		}
-		return drgn_object_set_unsigned_internal(ret, &type, bit_size,
-							 uvalue);
+		drgn_object_set_unsigned_internal(ret, &type, uvalue);
+		return NULL;
 	} else {
 		return drgn_error_create(DRGN_ERROR_OTHER,
 					 "unknown DW_AT_const_value form");

libdrgn/error.h
@@ -24,7 +24,7 @@
  * @{
  */
 
-struct drgn_object_type;
+struct drgn_operand_type;
 
 /**
  * Global stop iteration error.
@@ -98,13 +98,13 @@ struct drgn_error *drgn_error_incomplete_type(const char *format,
 
 /** Create a @ref drgn_error for invalid types to a binary operator. */
 struct drgn_error *drgn_error_binary_op(const char *op_name,
-					struct drgn_object_type *type1,
-					struct drgn_object_type *type2)
+					struct drgn_operand_type *type1,
+					struct drgn_operand_type *type2)
 	__attribute__((returns_nonnull));
 
 /** Create a @ref drgn_error for an invalid type to a unary operator. */
 struct drgn_error *drgn_error_unary_op(const char *op_name,
-				       struct drgn_object_type *type)
+				       struct drgn_operand_type *type)
 	__attribute__((returns_nonnull));
 
 /** Create a @ref drgn_error for a failed symbol lookup. */

libdrgn/language_c.c
@@ -2813,7 +2813,7 @@ static bool c_can_represent_all_values(struct drgn_type *type1,
 }
 
 static struct drgn_error *c_integer_promotions(struct drgn_program *prog,
-					       struct drgn_object_type *type)
+					       struct drgn_operand_type *type)
 {
 	struct drgn_error *err;
 	enum drgn_primitive_type primitive;
@@ -2943,9 +2943,9 @@ c_corresponding_unsigned_type(struct drgn_program *prog,
 }
 
 static struct drgn_error *c_common_real_type(struct drgn_program *prog,
-					     struct drgn_object_type *type1,
-					     struct drgn_object_type *type2,
-					     struct drgn_object_type *ret)
+					     struct drgn_operand_type *type1,
+					     struct drgn_operand_type *type2,
+					     struct drgn_operand_type *ret)
 {
 	struct drgn_error *err;
 	enum drgn_primitive_type primitive1, primitive2;
@@ -3133,13 +3133,13 @@ ret2:
 }
 
 static struct drgn_error *c_operand_type(const struct drgn_object *obj,
-					 struct drgn_object_type *type_ret,
+					 struct drgn_operand_type *type_ret,
 					 bool *is_pointer_ret,
 					 uint64_t *referenced_size_ret)
 {
 	struct drgn_error *err;
 
-	*type_ret = drgn_object_type(obj);
+	*type_ret = drgn_object_operand_type(obj);
 	switch (drgn_type_kind(type_ret->underlying_type)) {
 	case DRGN_TYPE_ARRAY: {
 		uint8_t word_size;
@@ -3209,8 +3209,7 @@ struct drgn_error *c_op_cast(struct drgn_object *res,
 			     const struct drgn_object *obj)
 {
 	struct drgn_error *err;
-	struct drgn_object_type type;
-
+	struct drgn_operand_type type;
 	err = c_operand_type(obj, &type, NULL, NULL);
 	if (err)
 		return err;
@@ -3221,8 +3220,8 @@ struct drgn_error *c_op_cast(struct drgn_object *res,
  * It's too expensive to check that two pointer types are compatible, so we just
  * check that they refer to the same kind of type with equal size.
  */
-static bool c_pointers_similar(const struct drgn_object_type *lhs_type,
-			       const struct drgn_object_type *rhs_type,
+static bool c_pointers_similar(const struct drgn_operand_type *lhs_type,
+			       const struct drgn_operand_type *rhs_type,
 			       uint64_t lhs_size, uint64_t rhs_size)
 {
 	struct drgn_type *lhs_referenced_type, *rhs_referenced_type;
@@ -3260,9 +3259,9 @@ struct drgn_error *c_op_cmp(const struct drgn_object *lhs,
 			    const struct drgn_object *rhs, int *ret)
 {
 	struct drgn_error *err;
-	struct drgn_object_type lhs_type, rhs_type;
-	bool lhs_pointer, rhs_pointer;
 
+	struct drgn_operand_type lhs_type, rhs_type;
+	bool lhs_pointer, rhs_pointer;
 	err = c_operand_type(lhs, &lhs_type, &lhs_pointer, NULL);
 	if (err)
 		return err;
@@ -3275,8 +3274,7 @@ struct drgn_error *c_op_cmp(const struct drgn_object *lhs,
 	} else if (lhs_pointer || rhs_pointer) {
 		goto type_error;
 	} else {
-		struct drgn_object_type type;
-
+		struct drgn_operand_type type;
 		if (!drgn_type_is_arithmetic(lhs_type.underlying_type) ||
 		    !drgn_type_is_arithmetic(rhs_type.underlying_type))
 			goto type_error;
@@ -3297,10 +3295,10 @@ struct drgn_error *c_op_add(struct drgn_object *res,
 			    const struct drgn_object *rhs)
 {
 	struct drgn_error *err;
-	struct drgn_object_type lhs_type, rhs_type;
+
+	struct drgn_operand_type lhs_type, rhs_type;
 	bool lhs_pointer, rhs_pointer;
 	uint64_t lhs_size, rhs_size;
-
 	err = c_operand_type(lhs, &lhs_type, &lhs_pointer, &lhs_size);
 	if (err)
 		return err;
@@ -3317,8 +3315,7 @@ struct drgn_error *c_op_add(struct drgn_object *res,
 			goto type_error;
 		return drgn_op_add_to_pointer(res, &rhs_type, rhs_size, false, rhs, lhs);
 	} else {
-		struct drgn_object_type type;
-
+		struct drgn_operand_type type;
 		if (!drgn_type_is_arithmetic(lhs_type.underlying_type) ||
 		    !drgn_type_is_arithmetic(rhs_type.underlying_type))
 			goto type_error;
@@ -3339,10 +3336,10 @@ struct drgn_error *c_op_sub(struct drgn_object *res,
 			    const struct drgn_object *rhs)
 {
 	struct drgn_error *err;
-	struct drgn_object_type lhs_type, rhs_type;
+
+	struct drgn_operand_type lhs_type, rhs_type;
 	bool lhs_pointer, rhs_pointer;
 	uint64_t lhs_size, rhs_size;
-
 	err = c_operand_type(lhs, &lhs_type, &lhs_pointer, &lhs_size);
 	if (err)
 		return err;
@@ -3351,8 +3348,7 @@ struct drgn_error *c_op_sub(struct drgn_object *res,
 		return err;
 
 	if (lhs_pointer && rhs_pointer) {
-		struct drgn_object_type type = {};
-
+		struct drgn_operand_type type = {};
 		err = drgn_program_find_primitive_type(drgn_object_program(lhs),
 						       DRGN_C_TYPE_PTRDIFF_T,
 						       &type.type);
@@ -3369,8 +3365,7 @@ struct drgn_error *c_op_sub(struct drgn_object *res,
 		return drgn_op_add_to_pointer(res, &lhs_type, lhs_size, true,
 					      lhs, rhs);
 	} else {
-		struct drgn_object_type type;
-
+		struct drgn_operand_type type;
 		if (!drgn_type_is_arithmetic(lhs_type.underlying_type) ||
 		    !drgn_type_is_arithmetic(rhs_type.underlying_type))
 			goto type_error;
@@ -3392,8 +3387,8 @@ struct drgn_error *c_op_##op_name(struct drgn_object *res,			\
 				  const struct drgn_object *rhs)		\
 {										\
 	struct drgn_error *err;							\
-	struct drgn_object_type lhs_type, rhs_type, type;			\
 										\
+	struct drgn_operand_type lhs_type, rhs_type, type;			\
 	err = c_operand_type(lhs, &lhs_type, NULL, NULL);			\
 	if (err)								\
 		return err;							\
@@ -3426,8 +3421,8 @@ struct drgn_error *c_op_##op_name(struct drgn_object *res,			\
 					 const struct drgn_object *rhs)		\
 {										\
 	struct drgn_error *err;							\
-	struct drgn_object_type lhs_type, rhs_type;				\
 										\
+	struct drgn_operand_type lhs_type, rhs_type;				\
 	err = c_operand_type(lhs, &lhs_type, NULL, NULL);			\
 	if (err)								\
 		return err;							\
@@ -3457,8 +3452,8 @@ struct drgn_error *c_op_##op_name(struct drgn_object *res,		\
 					 const struct drgn_object *obj)	\
 {									\
 	struct drgn_error *err;						\
-	struct drgn_object_type type;					\
 									\
+	struct drgn_operand_type type;					\
 	err = c_operand_type(obj, &type, NULL, NULL);			\
 	if (err)							\
 		return err;						\

libdrgn/object.h
@@ -41,40 +41,16 @@
 struct drgn_error *drgn_object_is_zero(const struct drgn_object *obj,
 				       bool *ret);
 
-/**
- * Type of an object.
- *
- * This is used to contain the types of operands and operator results.
- */
+/** Type-related fields from @ref drgn_object. */
 struct drgn_object_type {
-	/** See @ref drgn_qualified_type::type. */
 	struct drgn_type *type;
-	/** See @ref drgn_qualified_type::qualifiers. */
-	enum drgn_qualifiers qualifiers;
-	/**
-	 * Cached underlying type of @c type.
-	 *
-	 * See @ref drgn_underlying_type().
-	 */
+	/* Cached underlying type of @c type. */
 	struct drgn_type *underlying_type;
-	/**
-	 * If the object is a bit field, the size of the field in bits.
-	 * Otherwise, 0.
-	 */
-	uint64_t bit_field_size;
+	uint64_t bit_size;
+	enum drgn_qualifiers qualifiers;
+	enum drgn_object_encoding encoding;
+	bool is_bit_field;
 };
-
-/** Get the @ref drgn_object_type of a @ref drgn_object. */
-static inline struct drgn_object_type
-drgn_object_type(const struct drgn_object *obj)
-{
-	return (struct drgn_object_type){
-		.type = obj->type,
-		.qualifiers = obj->qualifiers,
-		.underlying_type = drgn_underlying_type(obj->type),
-		.bit_field_size = obj->is_bit_field ? obj->bit_size : 0,
-	};
-}
 
 /** Convert a @ref drgn_object_type to a @ref drgn_qualified_type. */
 static inline struct drgn_qualified_type
@@ -87,66 +63,70 @@ drgn_object_type_qualified(const struct drgn_object_type *type)
 }
 
 /**
- * Reinitialize the fields of a @ref drgn_object, excluding the program and
- * value.
+ * Type of an operand or operator result.
+ *
+ * This is basically @ref drgn_qualified_type plus a bit field size and cached
+ * underlying type.
+ */
+struct drgn_operand_type {
+	struct drgn_type *type;
+	enum drgn_qualifiers qualifiers;
+	struct drgn_type *underlying_type;
+	uint64_t bit_field_size;
+};
+
+/** Get the @ref drgn_operand_type of a @ref drgn_object. */
+static inline struct drgn_operand_type
+drgn_object_operand_type(const struct drgn_object *obj)
+{
+	return (struct drgn_operand_type){
+		.type = obj->type,
+		.qualifiers = obj->qualifiers,
+		.underlying_type = drgn_underlying_type(obj->type),
+		.bit_field_size = obj->is_bit_field ? obj->bit_size : 0,
+	};
+}
+
+/**
+ * Deinitialize the value of a @ref drgn_object and reinitialize the kind and
+ * type fields.
  */
 static inline void drgn_object_reinit(struct drgn_object *obj,
 				      const struct drgn_object_type *type,
-				      enum drgn_object_encoding encoding,
-				      uint64_t bit_size,
 				      enum drgn_object_kind kind)
 {
 	drgn_object_deinit(obj);
 	obj->type = type->type;
 	obj->qualifiers = type->qualifiers;
-	obj->encoding = encoding;
-	obj->bit_size = bit_size;
-	obj->is_bit_field = type->bit_field_size != 0;
+	obj->bit_size = type->bit_size;
+	obj->encoding = type->encoding;
+	obj->is_bit_field = type->is_bit_field;
 	obj->kind = kind;
 }
 
 /**
- * Get the @ref drgn_object_encoding and size in bits for an object given its
- * type.
+ * Compute the type-related fields of a @ref drgn_object from a @ref
+ * drgn_qualified_type and a bit field size.
  */
 struct drgn_error *
-drgn_object_type_encoding_and_size(const struct drgn_object_type *type,
-				   enum drgn_object_encoding *encoding_ret,
-				   uint64_t *bit_size_ret);
-
-/** Prepare to reinitialize an object. */
-struct drgn_error *
-drgn_object_set_common(struct drgn_qualified_type qualified_type,
-		       uint64_t bit_field_size,
-		       struct drgn_object_type *type_ret,
-		       enum drgn_object_encoding *encoding_ret,
-		       uint64_t *bit_size_ret);
-
-/**
- * Sanity check that the given bit size and bit field size are valid for the
- * given object encoding.
- */
-struct drgn_error *sanity_check_object(enum drgn_object_encoding encoding,
-				       uint64_t bit_field_size,
-				       uint64_t bit_size);
+drgn_object_type(struct drgn_qualified_type qualified_type,
+		 uint64_t bit_field_size, struct drgn_object_type *ret);
 
 /**
  * Like @ref drgn_object_set_signed() but @ref drgn_object_set_common() was
- * already called.
+ * already called and the type is already known to be a signed integer type.
  */
-struct drgn_error *
-drgn_object_set_signed_internal(struct drgn_object *res,
-				const struct drgn_object_type *type,
-				uint64_t bit_size, int64_t svalue);
+void drgn_object_set_signed_internal(struct drgn_object *res,
+				     const struct drgn_object_type *type,
+				     int64_t svalue);
 
 /**
  * Like @ref drgn_object_set_unsigned() but @ref drgn_object_set_common() was
- * already called.
+ * already called and the type is already known to be an unsigned integer type.
  */
-struct drgn_error *
-drgn_object_set_unsigned_internal(struct drgn_object *res,
-				  const struct drgn_object_type *type,
-				  uint64_t bit_size, uint64_t uvalue);
+void drgn_object_set_unsigned_internal(struct drgn_object *res,
+				       const struct drgn_object_type *type,
+				       uint64_t uvalue);
 
 /**
  * Like @ref drgn_object_set_from_buffer() but @ref drgn_object_set_common() was
@@ -155,9 +135,8 @@ drgn_object_set_unsigned_internal(struct drgn_object *res,
 struct drgn_error *
 drgn_object_set_from_buffer_internal(struct drgn_object *res,
 				     const struct drgn_object_type *type,
-				     enum drgn_object_encoding encoding,
-				     uint64_t bit_size, const void *buf,
-				     uint64_t bit_offset, bool little_endian);
+				     const void *buf, uint64_t bit_offset,
+				     bool little_endian);
 
 /** Convert a @ref drgn_byte_order to a boolean. */
 struct drgn_error *
@@ -179,7 +158,7 @@ drgn_byte_order_to_little_endian(struct drgn_program *prog,
  */
 typedef struct drgn_error *
 drgn_binary_op_impl(struct drgn_object *res,
-		    const struct drgn_object_type *type,
+		    const struct drgn_operand_type *op_type,
 		    const struct drgn_object *lhs,
 		    const struct drgn_object *rhs);
 /**
@@ -199,9 +178,9 @@ drgn_binary_op_impl(struct drgn_object *res,
 typedef struct drgn_error *
 drgn_shift_op_impl(struct drgn_object *res,
 		   const struct drgn_object *lhs,
-		   const struct drgn_object_type *lhs_type,
+		   const struct drgn_operand_type *lhs_type,
 		   const struct drgn_object *rhs,
-		   const struct drgn_object_type *rhs_type);
+		   const struct drgn_operand_type *rhs_type);
 
 /**
  * Unary operator implementation.
@@ -217,7 +196,7 @@ drgn_shift_op_impl(struct drgn_object *res,
  */
 typedef struct drgn_error *
 drgn_unary_op_impl(struct drgn_object *res,
-		   const struct drgn_object_type *type,
+		   const struct drgn_operand_type *op_type,
 		   const struct drgn_object *obj);
 
 /**
@@ -317,7 +296,7 @@ drgn_unary_op_impl drgn_op_not_impl;
 struct drgn_error *drgn_op_cast(struct drgn_object *res,
 				struct drgn_qualified_type qualified_type,
 				const struct drgn_object *obj,
-				struct drgn_object_type *obj_type);
+				const struct drgn_operand_type *obj_type);
 
 /**
  * Implement object comparison for signed, unsigned, and floating-point objects.
@@ -326,7 +305,7 @@ struct drgn_error *drgn_op_cast(struct drgn_object *res,
  */
 struct drgn_error *drgn_op_cmp_impl(const struct drgn_object *lhs,
 				    const struct drgn_object *rhs,
-				    const struct drgn_object_type *type,
+				    const struct drgn_operand_type *op_type,
 				    int *ret);
 
 /**
@@ -353,11 +332,12 @@ struct drgn_error *drgn_op_cmp_pointers(const struct drgn_object *lhs,
  * @return @c NULL on success, non-@c NULL on error. @p res is not modified on
  * error.
  */
-struct drgn_error *drgn_op_add_to_pointer(struct drgn_object *res,
-					  const struct drgn_object_type *type,
-					  uint64_t referenced_size, bool negate,
-					  const struct drgn_object *ptr,
-					  const struct drgn_object *index);
+struct drgn_error *
+drgn_op_add_to_pointer(struct drgn_object *res,
+		       const struct drgn_operand_type *op_type,
+		       uint64_t referenced_size, bool negate,
+		       const struct drgn_object *ptr,
+		       const struct drgn_object *index);
 
 /**
  * Implement pointer subtraction.
@@ -374,7 +354,7 @@ struct drgn_error *drgn_op_add_to_pointer(struct drgn_object *res,
  * error.
  */
 struct drgn_error *drgn_op_sub_pointers(struct drgn_object *res,
-					const struct drgn_object_type *type,
+					const struct drgn_operand_type *op_type,
 					uint64_t referenced_size,
 					const struct drgn_object *lhs,
 					const struct drgn_object *rhs);

tests/test_object.py
@@ -128,6 +128,34 @@ class TestInit(MockProgramTestCase):
             bit_offset=4,
         )
 
+    def test_integer_size(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "unsupported integer bit size",
+            Object,
+            self.prog,
+            self.prog.int_type("ZERO", 0, True),
+        )
+        self.assertRaisesRegex(
+            ValueError,
+            "unsupported integer bit size",
+            Object,
+            self.prog,
+            self.prog.int_type("BIG", 16, True),
+        )
+
+    def test_float_size(self):
+        for i in range(10):
+            if i == 4 or i == 8:
+                continue
+            self.assertRaisesRegex(
+                ValueError,
+                "unsupported floating-point bit size",
+                Object,
+                self.prog,
+                self.prog.float_type("FLOAT", i),
+            )
+
 
 class TestReference(MockProgramTestCase):
     def test_basic(self):

commit a89219b83054308c4bd307c499e1c2344e12ada3

libdrgn: python: add missing set_drgn_error() in serialize_compound_value()
Fixes: 72b4aa9669a2 ("libdrgn: clean up object initialization")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/object.c
@@ -150,8 +150,10 @@ static int serialize_compound_value(struct drgn_program *prog, char *buf,
 		struct drgn_object_type member_type;
 		err = drgn_object_type(member_qualified_type,
 				       member_bit_field_size, &member_type);
-		if (err)
+		if (err) {
+			set_drgn_error(err);
 			return -1;
+		}
 		if (serialize_py_object(prog, buf, buf_bit_size,
 					bit_offset + member_bit_offset,
 					PyTuple_GET_ITEM(item, 1),