osandov/drgn

commit ca6d0fa96e989984378da79c3676078e0d272ccf

libdrgn: don't return error if malloc(0) returns NULL
glibc apparently returns non-NULL, but let's be portable.

Fixes: 7bb3f0cd5a0a ("libdrgn: add interface for registering chains of named handlers")
Fixes: ad57fa1c994f ("libdrgn, python: add SymbolIndex")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/symbol.c
@@ -274,6 +274,8 @@ drgn_symbol_index_init(struct drgn_symbol *symbols, uint32_t count,
 	ret->name_sort = NULL;
 	ret->max_addrs = NULL;
 	drgn_symbol_name_table_init(&ret->htab);
+	if (count == 0)
+		return NULL;
 	ret->name_sort = malloc_array(count, sizeof(ret->name_sort[0]));
 	if (!ret->name_sort)
 		goto enomem;

libdrgn/handler.c
@@ -44,6 +44,11 @@ struct drgn_error *drgn_handler_list_registered(struct drgn_handler_list *list,
 	size_t n = 0;
 	drgn_handler_list_for_each_registered(handler, list)
 		n++;
+	if (n == 0) {
+		*names_ret = NULL;
+		*count_ret = 0;
+		return NULL;
+	}
 	const char **names = malloc_array(n, sizeof(names[0]));
 	if (!names)
 		return &drgn_enomem;
@@ -122,6 +127,11 @@ struct drgn_error *drgn_handler_list_enabled(struct drgn_handler_list *list,
 	size_t n = 0;
 	drgn_handler_list_for_each_enabled(struct drgn_handler, handler, list)
 		n++;
+	if (n == 0) {
+		*names_ret = NULL;
+		*count_ret = 0;
+		return NULL;
+	}
 	const char **names = malloc_array(n, sizeof(names[0]));
 	if (!names)
 		return &drgn_enomem;

commit 149dbd7bffc8d87b4dfe9a3b35c80275f673b541

libdrgn: symbol: remove redundant num_syms check in drgn_symbol_index_init()
We return early if count is zero, so this check is dead.

Fixes: ca6d0fa96e98 ("libdrgn: don't return error if malloc(0) returns NULL")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/symbol.c
@@ -302,8 +302,7 @@ drgn_symbol_index_init(struct drgn_symbol *symbols, uint32_t count,
 	// that's a rare case. In order to do this strategy, we need an array
 	// that contains the maximum address spanned by any symbol at or before
 	// that index.
-	if (ret->num_syms > 0) // in case num_syms == 0
-		ret->max_addrs[0] = ret->symbols[0].address + ret->symbols[0].size;
+	ret->max_addrs[0] = ret->symbols[0].address + ret->symbols[0].size;
 	for (uint32_t i = 1; i < ret->num_syms; i++) {
 		uint64_t max_addr = ret->symbols[i].address + ret->symbols[i].size;
 		ret->max_addrs[i] = max(ret->max_addrs[i - 1], max_addr);