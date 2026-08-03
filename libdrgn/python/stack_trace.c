osandov/drgn

commit bc85767e5fde24ceefb45b6b123092363c10a8e0

libdrgn: support looking up parameters and variables in stack traces
After all of the preparatory work, the last two missing pieces are a way
to find a variable by name in the list of scopes that we saved while
unwinding, and a way to find the containing scopes of an inlined
function. With that, we can finally look up parameters and variables in
stack traces.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/stack_trace.c
@@ -84,6 +84,63 @@ static PyObject *StackFrame_str(StackFrame *self)
 	return ret;
 }
 
+static DrgnObject *StackFrame_subscript(StackFrame *self, PyObject *key)
+{
+	struct drgn_error *err;
+	Program *prog = container_of(self->trace->trace->prog, Program, prog);
+	if (!PyUnicode_Check(key)) {
+		PyErr_SetObject(PyExc_KeyError, key);
+		return NULL;
+	}
+	const char *name = PyUnicode_AsUTF8(key);
+	if (!name)
+		return NULL;
+	DrgnObject *ret = DrgnObject_alloc(prog);
+	if (!ret)
+		return NULL;
+	bool clear = set_drgn_in_python();
+	err = drgn_stack_frame_find_object(self->trace->trace, self->i, name,
+					   &ret->obj);
+	if (clear)
+		clear_drgn_in_python();
+	if (err) {
+		if (err->code == DRGN_ERROR_LOOKUP) {
+			drgn_error_destroy(err);
+			PyErr_SetObject(PyExc_KeyError, key);
+		} else {
+			set_drgn_error(err);
+		}
+		Py_DECREF(ret);
+		return NULL;
+	}
+	return ret;
+}
+
+static int StackFrame_contains(StackFrame *self, PyObject *key)
+{
+	struct drgn_error *err;
+	if (!PyUnicode_Check(key)) {
+		PyErr_SetObject(PyExc_KeyError, key);
+		return -1;
+	}
+	const char *name = PyUnicode_AsUTF8(key);
+	if (!name)
+		return -1;
+	struct drgn_object tmp;
+	drgn_object_init(&tmp, self->trace->trace->prog);
+	err = drgn_stack_frame_find_object(self->trace->trace, self->i, name,
+					   &tmp);
+	drgn_object_deinit(&tmp);
+	if (!err) {
+		return 1;
+	} else if (err->code == DRGN_ERROR_LOOKUP) {
+		drgn_error_destroy(err);
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
 static PyObject *StackFrame_source(StackFrame *self)
 {
 	int line;
@@ -207,6 +264,10 @@ static PyObject *StackFrame_get_pc(StackFrame *self, void *arg)
 }
 
 static PyMethodDef StackFrame_methods[] = {
+	{"__getitem__", (PyCFunction)StackFrame_subscript,
+	 METH_O | METH_COEXIST, drgn_StackFrame___getitem___DOC},
+	{"__contains__", (PyCFunction)StackFrame_contains,
+	 METH_O | METH_COEXIST, drgn_StackFrame___contains___DOC},
 	{"source", (PyCFunction)StackFrame_source, METH_NOARGS,
 	 drgn_StackFrame_source_DOC},
 	{"symbol", (PyCFunction)StackFrame_symbol, METH_NOARGS,
@@ -228,11 +289,21 @@ static PyGetSetDef StackFrame_getset[] = {
 	{},
 };
 
+static PyMappingMethods StackFrame_as_mapping = {
+	.mp_subscript = (binaryfunc)StackFrame_subscript,
+};
+
+static PySequenceMethods StackFrame_as_sequence = {
+	.sq_contains = (objobjproc)StackFrame_contains,
+};
+
 PyTypeObject StackFrame_type = {
 	PyVarObject_HEAD_INIT(NULL, 0)
 	.tp_name = "_drgn.StackFrame",
 	.tp_basicsize = sizeof(StackFrame),
 	.tp_dealloc = (destructor)StackFrame_dealloc,
+	.tp_as_sequence = &StackFrame_as_sequence,
+	.tp_as_mapping = &StackFrame_as_mapping,
 	.tp_str = (reprfunc)StackFrame_str,
 	.tp_flags = Py_TPFLAGS_DEFAULT,
 	.tp_doc = drgn_StackFrame_DOC,

libdrgn/stack_trace.c
@@ -352,6 +352,87 @@ drgn_stack_frame_symbol(struct drgn_stack_trace *trace, size_t frame,
 	return NULL;
 }
 
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_stack_frame_find_object(struct drgn_stack_trace *trace, size_t frame_i,
+			     const char *name, struct drgn_object *ret)
+{
+	struct drgn_error *err;
+	struct drgn_stack_frame *frame = &trace->frames[frame_i];
+
+	if (frame->num_scopes == 0)
+		goto not_found;
+
+	Dwarf_Die die, type_die;
+	err = drgn_find_in_dwarf_scopes(frame->scopes, frame->num_scopes, name,
+					&die, &type_die);
+	if (err)
+		return err;
+	if (!die.addr && frame->function_scope == 0) {
+		/*
+		 * Scope 0 must be a DW_TAG_inlined_subroutine, and we didn't
+		 * find the name in the concrete inlined instance tree. We need
+		 * to find the scopes that contain the the abstract instance
+		 * root (i.e, the DW_TAG_subprogram definition). (We could do
+		 * this ahead of time when unwinding the stack, but for
+		 * efficiency we do it lazily.)
+		 */
+		Dwarf_Attribute attr_mem, *attr;
+		if (!(attr = dwarf_attr(frame->scopes, DW_AT_abstract_origin,
+					&attr_mem)))
+			goto not_found;
+		Dwarf_Die abstract_origin;
+		if (!dwarf_formref_die(attr, &abstract_origin))
+			return drgn_error_libdw();
+
+		Dwarf_Die *ancestors;
+		size_t num_ancestors;
+		err = drgn_find_die_ancestors(&abstract_origin, &ancestors,
+					      &num_ancestors);
+		if (err)
+			return err;
+
+		size_t new_num_scopes = num_ancestors + frame->num_scopes;
+		Dwarf_Die *new_scopes = realloc(ancestors,
+						new_num_scopes *
+						sizeof(*new_scopes));
+		if (!new_scopes) {
+			free(ancestors);
+			return &drgn_enomem;
+		}
+		memcpy(&new_scopes[num_ancestors], frame->scopes,
+		       frame->num_scopes * sizeof(*new_scopes));
+		free(frame->scopes);
+		frame->scopes = new_scopes;
+		frame->num_scopes = new_num_scopes;
+		frame->function_scope = num_ancestors;
+
+		/* Look for the name in the new scopes. */
+		err = drgn_find_in_dwarf_scopes(frame->scopes, num_ancestors,
+						name, &die, &type_die);
+		if (err)
+			return err;
+	}
+	if (!die.addr) {
+not_found:;
+		const char *frame_name = drgn_stack_frame_name(trace, frame_i);
+		if (frame_name) {
+			return drgn_error_format(DRGN_ERROR_LOOKUP,
+						 "could not find '%s' in '%s'",
+						 name, frame_name);
+		} else {
+			return drgn_error_format(DRGN_ERROR_LOOKUP,
+						 "could not find '%s'", name);
+		}
+	}
+
+	Dwarf_Die function_die = frame->scopes[frame->function_scope];
+	return drgn_object_from_dwarf(trace->prog->_dbinfo, frame->regs->module,
+				      &die,
+				      dwarf_tag(&die) == DW_TAG_enumerator ?
+				      &type_die : NULL,
+				      &function_die, frame->regs, ret);
+}
+
 LIBDRGN_PUBLIC bool drgn_stack_frame_register(struct drgn_stack_trace *trace,
 					      size_t frame,
 					      const struct drgn_register *reg,

_drgn.pyi
@@ -1417,6 +1417,14 @@ class StackFrame:
     This includes more information than when printing the full stack trace. The
     format is subject to change. The drgn CLI is set up so that stack frames
     are displayed with ``str()`` by default.
+
+    The :meth:`[] <.__getitem__>` operator can look up function parameters,
+    local variables, and global variables in the scope of the stack frame:
+
+    >>> prog.stack_trace(1)[0]['prev'].pid
+    (pid_t)1
+    >>> prog.stack_trace(1)[0]['scheduler_running']
+    (int)1
     """
 
     name: Optional[str]
@@ -1450,6 +1458,25 @@ class StackFrame:
 
     pc: int
     """Program counter at this stack frame."""
+    def __getitem__(self, name: str) -> Object:
+        """
+        Implement ``self[name]``. Get the object (variable, function parameter,
+        constant, or function) with the given name in the scope of this frame.
+
+        If the object exists but has been optimized out, this returns an
+        :ref:`absent object <absent-objects>`.
+
+        :param name: Object name.
+        """
+        ...
+    def __contains__(self, name: str) -> bool:
+        """
+        Implement ``name in self``. Return whether an object with the given
+        name exists in the scope of this frame.
+
+        :param name: Object name.
+        """
+        ...
     def source(self) -> Tuple[str, int, int]:
         """
         Get the source code location of this frame.

libdrgn/debug_info.c
@@ -2885,7 +2885,7 @@ drgn_object_from_dwarf_constant(struct drgn_debug_info *dbinfo, Dwarf_Die *die,
 	}
 }
 
-static struct drgn_error *
+struct drgn_error *
 drgn_object_from_dwarf(struct drgn_debug_info *dbinfo,
 		       struct drgn_debug_info_module *module,
 		       Dwarf_Die *die, Dwarf_Die *type_die,
@@ -2933,6 +2933,82 @@ drgn_object_from_dwarf(struct drgn_debug_info *dbinfo,
 	return drgn_object_from_dwarf_location(dbinfo->prog, module, die,
 					       qualified_type, expr, expr_size,
 					       function_die, regs, ret);
+}
+
+static struct drgn_error *find_dwarf_enumerator(Dwarf_Die *enumeration_type,
+						const char *name,
+						Dwarf_Die *ret)
+{
+	int r = dwarf_child(enumeration_type, ret);
+	while (r == 0) {
+		if (dwarf_tag(ret) == DW_TAG_enumerator &&
+		    strcmp(dwarf_diename(ret), name) == 0)
+			return NULL;
+		r = dwarf_siblingof(ret, ret);
+	}
+	if (r < 0)
+		return drgn_error_libdw();
+	ret->addr = NULL;
+	return NULL;
+}
+
+struct drgn_error *drgn_find_in_dwarf_scopes(Dwarf_Die *scopes,
+					     size_t num_scopes,
+					     const char *name,
+					     Dwarf_Die *die_ret,
+					     Dwarf_Die *type_ret)
+{
+	struct drgn_error *err;
+	Dwarf_Die die;
+	for (size_t scope = num_scopes; scope--;) {
+		bool have_declaration = false;
+		if (dwarf_child(&scopes[scope], &die) != 0)
+			continue;
+		do {
+			switch (dwarf_tag(&die)) {
+			case DW_TAG_variable:
+			case DW_TAG_formal_parameter:
+			case DW_TAG_subprogram:
+				if (strcmp(dwarf_diename(&die), name) == 0) {
+					*die_ret = die;
+					bool declaration;
+					if (dwarf_flag(&die, DW_AT_declaration,
+						       &declaration))
+						return drgn_error_libdw();
+					if (declaration)
+						have_declaration = true;
+					else
+						return NULL;
+				}
+				break;
+			case DW_TAG_enumeration_type: {
+				bool enum_class;
+				if (dwarf_flag_integrate(&die, DW_AT_enum_class,
+							 &enum_class))
+					return drgn_error_libdw();
+				if (!enum_class) {
+					Dwarf_Die enumerator;
+					err = find_dwarf_enumerator(&die, name,
+								    &enumerator);
+					if (err)
+						return err;
+					if (enumerator.addr) {
+						*die_ret = enumerator;
+						*type_ret = die;
+						return NULL;
+					}
+				}
+				break;
+			}
+			default:
+				continue;
+			}
+		} while (dwarf_siblingof(&die, &die) == 0);
+		if (have_declaration)
+			return NULL;
+	}
+	die_ret->addr = NULL;
+	return NULL;
 }
 
 static struct drgn_error *

libdrgn/debug_info.h
@@ -419,6 +419,44 @@ drgn_debug_info_find_object(const char *name, size_t name_len,
 			    struct drgn_object *ret);
 
 /**
+ * Find an object DIE in an array of DWARF scopes.
+ *
+ * @param[in] scopes Array of scopes, from outermost to innermost.
+ * @param[in] num_scopes Number of scopes in @p scopes.
+ * @param[out] die_ret Returned object DIE.
+ * @param[out] type_ret If @p die_ret is a `DW_TAG_enumerator` DIE, its parent.
+ * Otherwise, undefined.
+ */
+struct drgn_error *drgn_find_in_dwarf_scopes(Dwarf_Die *scopes,
+					     size_t num_scopes,
+					     const char *name,
+					     Dwarf_Die *die_ret,
+					     Dwarf_Die *type_ret);
+
+/**
+ * Create a @ref drgn_object from a `Dwarf_Die`.
+ *
+ * @param[in] die Object DIE (e.g., `DW_TAG_subprogram`, `DW_TAG_variable`,
+ * `DW_TAG_formal_parameter`, `DW_TAG_enumerator`,
+ * `DW_TAG_template_value_parameter`).
+ * @param[in] type_die DIE of object's type. If @c NULL, use the `DW_AT_type`
+ * attribute of @p die. If @p die is a `DW_TAG_enumerator` DIE, this should be
+ * its parent.
+ * @param[in] function_die DIE of current function. @c NULL if not in function
+ * context.
+ * @param[in] regs Registers of current stack frame. @c NULL if not in stack
+ * frame context.
+ * @param[out] ret Returned object.
+ */
+struct drgn_error *
+drgn_object_from_dwarf(struct drgn_debug_info *dbinfo,
+		       struct drgn_debug_info_module *module,
+		       Dwarf_Die *die, Dwarf_Die *type_die,
+		       Dwarf_Die *function_die,
+		       const struct drgn_register_state *regs,
+		       struct drgn_object *ret);
+
+/**
  * Get the Call Frame Information in a @ref drgn_debug_info_module at a given
  * program counter.
  *

libdrgn/drgn.h.in
@@ -2663,6 +2663,19 @@ bool drgn_stack_frame_pc(struct drgn_stack_trace *trace, size_t frame,
 struct drgn_error *drgn_stack_frame_symbol(struct drgn_stack_trace *trace,
 					   size_t frame,
 					   struct drgn_symbol **ret);
+
+/**
+ * Find an object in the scope of a stack frame.
+ *
+ * @param[in] name Object name.
+ * @param[out] ret Returned object. This must have already been initialized with
+ * @ref drgn_object_init().
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_stack_frame_find_object(struct drgn_stack_trace *trace, size_t frame,
+			     const char *name, struct drgn_object *ret);
+
 /**
  * Get the value of a register in a stack frame.
  *

tests/helpers/linux/test_stack_trace.py
@@ -44,6 +44,23 @@ class TestStackTrace(LinuxHelperTestCase):
     def test_by_pid_orc(self):
         self._test_by_pid(True)
 
+    def test_local_variable(self):
+        pid = fork_and_pause()
+        wait_until(lambda: proc_state(pid) == "S")
+        for frame in self.prog.stack_trace(pid):
+            if frame.name in ("context_switch", "__schedule"):
+                try:
+                    prev = frame["prev"]
+                except KeyError:
+                    continue
+                if not prev.absent_:
+                    self.assertEqual(prev.pid, pid)
+                    break
+        else:
+            self.skipTest("prev not found in context_switch or __schedule")
+        os.kill(pid, signal.SIGKILL)
+        os.waitpid(pid, 0)
+
     def test_pt_regs(self):
         # This won't unwind anything useful, but at least make sure it accepts
         # a struct pt_regs.

commit be910f7ef047cca1c3fcd0da8df8b475b4a7cec3

libdrgn: python: add missing set_drgn_error() in StackFrame_contains()
Fixes: bc85767e5fde ("libdrgn: support looking up parameters and variables in stack traces")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/stack_trace.c
@@ -191,12 +191,14 @@ static int StackFrame_contains(StackFrame *self, PyObject *key)
 	DRGN_OBJECT(tmp, self->trace->trace->prog);
 	err = drgn_stack_frame_find_object(self->trace->trace, self->i, name,
 					   &tmp);
-	if (!err)
+	if (!err) {
 		return 1;
-	else if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP))
+	} else if (drgn_error_catch(&err, DRGN_ERROR_LOOKUP)) {
 		return 0;
-	else
+	} else {
+		set_drgn_error(err);
 		return -1;
+	}
 }
 
 static PyObject *StackFrame_source_name(StackFrame *self)