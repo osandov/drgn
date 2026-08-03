osandov/drgn

commit 38573cfdde8065cc2cc900f1b6b4b4d0b83b308b

libdrgn: stack_trace: pretty print frames and add frames for inline functions
If we want to access a parameter or local variable in an inlined
function, then we need a stack frame for that function. It's also much
more useful to see inlined functions in the stack trace in general. So,
when we've unwound the registers for a stack frame, walk the debugging
information to find all of the (possibly inlined) functions at the
program counter, and add a drgn stack frame for each of those.

Also add StackFrame.name and StackFrame.is_inline so that we can
distinguish inline frames. Also add StackFrame.source() to get the
filename and line and column numbers. Finally, add the source code
location to pretty-printed stack traces and add pretty-printing for
individual stack frames that includes extra information.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/stack_trace.c
@@ -3,6 +3,8 @@
 
 #include <assert.h>
 #include <byteswap.h>
+#include <dwarf.h>
+#include <elfutils/libdw.h>
 #include <elfutils/libdwfl.h>
 #include <inttypes.h>
 #include <stdlib.h>
@@ -25,16 +27,18 @@
 #include "type.h"
 #include "util.h"
 
-static bool drgn_stack_trace_append_frame(struct drgn_stack_trace **trace,
-					  size_t *capacity,
-					  struct drgn_register_state *regs)
+static struct drgn_error *
+drgn_stack_trace_append_frame(struct drgn_stack_trace **trace, size_t *capacity,
+			      struct drgn_register_state *regs,
+			      Dwarf_Die *scopes, size_t num_scopes,
+			      size_t function_scope)
 {
 	if ((*trace)->num_frames == *capacity) {
 		static const size_t max_capacity =
 			(SIZE_MAX - sizeof(struct drgn_stack_trace)) /
 			sizeof(struct drgn_stack_frame);
 		if (*capacity == max_capacity)
-			return false;
+			return &drgn_enomem;
 		size_t new_capacity;
 		if (*capacity > max_capacity / 2)
 			new_capacity = max_capacity;
@@ -45,14 +49,17 @@ static bool drgn_stack_trace_append_frame(struct drgn_stack_trace **trace,
 				offsetof(struct drgn_stack_trace,
 					 frames[new_capacity]));
 		if (!new_trace)
-			return false;
+			return &drgn_enomem;
 		*trace = new_trace;
 		*capacity = new_capacity;
 	}
 	struct drgn_stack_frame *frame =
 		&(*trace)->frames[(*trace)->num_frames++];
 	frame->regs = regs;
-	return true;
+	frame->scopes = scopes;
+	frame->num_scopes = num_scopes;
+	frame->function_scope = function_scope;
+	return NULL;
 }
 
 static void drgn_stack_trace_shrink_to_fit(struct drgn_stack_trace **trace,
@@ -71,8 +78,15 @@ static void drgn_stack_trace_shrink_to_fit(struct drgn_stack_trace **trace,
 
 LIBDRGN_PUBLIC void drgn_stack_trace_destroy(struct drgn_stack_trace *trace)
 {
-	for (size_t i = 0; i < trace->num_frames; i++)
-		drgn_register_state_destroy(trace->frames[i].regs);
+	struct drgn_register_state *regs = NULL;
+	for (size_t i = 0; i < trace->num_frames; i++) {
+		if (trace->frames[i].regs != regs) {
+			drgn_register_state_destroy(regs);
+			regs = trace->frames[i].regs;
+		}
+		free(trace->frames[i].scopes);
+	}
+	drgn_register_state_destroy(regs);
 	free(trace);
 }
 
@@ -91,8 +105,12 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 			goto enomem;
 
 		struct drgn_register_state *regs = trace->frames[frame].regs;
-		struct optional_uint64 pc = drgn_register_state_get_pc(regs);
-		if (pc.has_value) {
+		struct optional_uint64 pc;
+		const char *name = drgn_stack_frame_name(trace, frame);
+		if (name) {
+			if (!string_builder_append(&str, name))
+				goto enomem;
+		} else if ((pc = drgn_register_state_get_pc(regs)).has_value) {
 			Dwfl_Module *dwfl_module =
 				regs->module ? regs->module->dwfl_module : NULL;
 			struct drgn_symbol sym;
@@ -117,6 +135,19 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 				goto enomem;
 		}
 
+		int line, column;
+		const char *filename = drgn_stack_frame_source(trace, frame,
+							       &line, &column);
+		if (filename && column) {
+			if (!string_builder_appendf(&str, " (%s:%d:%d)",
+						    filename, line, column))
+				goto enomem;
+		} else if (filename) {
+			if (!string_builder_appendf(&str, " (%s:%d)", filename,
+						    line))
+				goto enomem;
+		}
+
 		if (frame != trace->num_frames - 1 &&
 		    !string_builder_appendc(&str, '\n'))
 			goto enomem;
@@ -128,6 +159,154 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 enomem:
 	free(str.str);
 	return &drgn_enomem;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_format_stack_frame(struct drgn_stack_trace *trace, size_t frame, char **ret)
+{
+	struct string_builder str = {};
+	struct drgn_register_state *regs = trace->frames[frame].regs;
+	if (!string_builder_appendf(&str, "#%zu at ", frame))
+		goto enomem;
+
+	struct optional_uint64 pc = drgn_register_state_get_pc(regs);
+	if (pc.has_value) {
+		if (!string_builder_appendf(&str, "%#" PRIx64, pc.value))
+			goto enomem;
+
+		Dwfl_Module *dwfl_module =
+			regs->module ? regs->module->dwfl_module : NULL;
+		struct drgn_symbol sym;
+		if (dwfl_module &&
+		    drgn_program_find_symbol_by_address_internal(trace->prog,
+								 pc.value - !regs->interrupted,
+								 dwfl_module,
+								 &sym) &&
+		    !string_builder_appendf(&str, " (%s+0x%" PRIx64 "/0x%" PRIx64 ")",
+					    sym.name, pc.value - sym.address,
+					    sym.size))
+				goto enomem;
+	} else {
+		if (!string_builder_append(&str, "???"))
+			goto enomem;
+	}
+
+	const char *name = drgn_stack_frame_name(trace, frame);
+	if (name && !string_builder_appendf(&str, " in %s", name))
+		goto enomem;
+
+	int line, column;
+	const char *filename = drgn_stack_frame_source(trace, frame, &line,
+						       &column);
+	if (filename && column) {
+		if (!string_builder_appendf(&str, " at %s:%d:%d", filename,
+					    line, column))
+			goto enomem;
+	} else if (filename) {
+		if (!string_builder_appendf(&str, " at %s:%d", filename, line))
+			goto enomem;
+	}
+
+	if (drgn_stack_frame_is_inline(trace, frame) &&
+	    !string_builder_append(&str, " (inlined)"))
+		goto enomem;
+
+	if (!string_builder_finalize(&str, ret))
+		goto enomem;
+	return NULL;
+
+enomem:
+	free(str.str);
+	return &drgn_enomem;
+}
+
+LIBDRGN_PUBLIC const char *drgn_stack_frame_name(struct drgn_stack_trace *trace,
+						 size_t frame)
+{
+	Dwarf_Die *scopes = trace->frames[frame].scopes;
+	size_t num_scopes = trace->frames[frame].num_scopes;
+	size_t function_scope = trace->frames[frame].function_scope;
+	if (function_scope >= num_scopes)
+		return NULL;
+	return dwarf_diename(&scopes[function_scope]);
+}
+
+LIBDRGN_PUBLIC bool drgn_stack_frame_is_inline(struct drgn_stack_trace *trace,
+					       size_t frame)
+{
+	Dwarf_Die *scopes = trace->frames[frame].scopes;
+	size_t num_scopes = trace->frames[frame].num_scopes;
+	size_t function_scope = trace->frames[frame].function_scope;
+	return (function_scope < num_scopes &&
+		dwarf_tag(&scopes[function_scope]) ==
+		DW_TAG_inlined_subroutine);
+}
+
+LIBDRGN_PUBLIC const char *
+drgn_stack_frame_source(struct drgn_stack_trace *trace, size_t frame,
+			int *line_ret, int *column_ret)
+{
+	if (frame > 0 &&
+	    trace->frames[frame].regs == trace->frames[frame - 1].regs) {
+		/*
+		 * This frame is the caller of an inline frame. Get the call
+		 * location from the inlined_subroutine of the callee.
+		 */
+		Dwarf_Die *inlined_scopes = trace->frames[frame - 1].scopes;
+		size_t inlined_num_scopes = trace->frames[frame - 1].num_scopes;
+		size_t inlined_function_scope =
+			trace->frames[frame - 1].function_scope;
+		if (inlined_function_scope >= inlined_num_scopes)
+			return NULL;
+		Dwarf_Die *inlined = &inlined_scopes[inlined_function_scope];
+
+		Dwarf_Die inlined_cu;
+		Dwarf_Files *files;
+		if (!dwarf_diecu(inlined, &inlined_cu, NULL, NULL) ||
+		    dwarf_getsrcfiles(&inlined_cu, &files, NULL))
+			return NULL;
+
+		Dwarf_Attribute attr;
+		Dwarf_Word value;
+		if (dwarf_formudata(dwarf_attr(inlined, DW_AT_call_file, &attr),
+				    &value))
+			return NULL;
+
+		const char *filename = dwarf_filesrc(files, value, NULL, NULL);
+		if (!filename)
+			return NULL;
+		if (line_ret) {
+			if (dwarf_formudata(dwarf_attr(inlined, DW_AT_call_line,
+						       &attr), &value))
+				*line_ret = 0;
+			else
+				*line_ret = value;
+		}
+		if (column_ret) {
+			if (dwarf_formudata(dwarf_attr(inlined,
+						       DW_AT_call_column,
+						       &attr), &value))
+				*column_ret = 0;
+			else
+				*column_ret = value;
+		}
+		return filename;
+	} else {
+		struct drgn_register_state *regs = trace->frames[frame].regs;
+		Dwfl_Module *dwfl_module =
+			regs->module ? regs->module->dwfl_module : NULL;
+		if (!dwfl_module)
+			return NULL;
+		struct optional_uint64 pc = drgn_register_state_get_pc(regs);
+		if (!pc.has_value)
+			return NULL;
+		pc.value -= !regs->interrupted;
+		Dwfl_Line *line = dwfl_module_getsrc(dwfl_module, pc.value);
+		if (!line)
+			return NULL;
+		return dwfl_lineinfo(line, NULL, line_ret, column_ret, NULL,
+				     NULL);
+	}
 }
 
 LIBDRGN_PUBLIC bool drgn_stack_frame_interrupted(struct drgn_stack_trace *trace,
@@ -441,6 +620,132 @@ static void drgn_add_to_register(void *dst, size_t dst_size, const void *src,
 
 
 static struct drgn_error *
+drgn_stack_trace_add_frames(struct drgn_stack_trace **trace,
+			    size_t *trace_capacity,
+			    struct drgn_register_state *regs)
+{
+	struct drgn_error *err;
+
+	if (!regs->module) {
+		err = drgn_stack_trace_append_frame(trace, trace_capacity, regs,
+						    NULL, 0, 0);
+		goto out;
+	}
+
+	uint64_t pc = regs->_pc - !regs->interrupted;
+	uint64_t bias;
+	Dwarf_Die *scopes;
+	size_t num_scopes;
+	err = drgn_debug_info_module_find_dwarf_scopes(regs->module, pc, &bias,
+						       &scopes, &num_scopes);
+	if (err)
+		goto out;
+	pc -= bias;
+
+	size_t orig_num_frames = (*trace)->num_frames;
+	/*
+	 * Walk backwards through scopes, splitting into frames. Stop at index 1
+	 * because 0 must be a unit DIE.
+	 */
+	size_t frame_end = num_scopes;
+	for (size_t i = num_scopes; i-- > 1;) {
+		bool has_pc;
+		if (i == num_scopes - 1) {
+			/*
+			 * The last scope is guaranteed to contain PC, so avoid
+			 * a call to dwarf_haspc().
+			 */
+			has_pc = true;
+		} else {
+			int r = dwarf_haspc(&scopes[i], pc);
+			if (r < 0) {
+				err = drgn_error_libdw();
+				goto out_scopes;
+			}
+			has_pc = r > 0;
+		}
+		if (has_pc) {
+			Dwarf_Die *frame_scopes;
+			switch (dwarf_tag(&scopes[i])) {
+			case DW_TAG_subprogram:
+				/*
+				 * Reuse the original scopes array (shrinking it
+				 * if necessary).
+				 */
+				if (frame_end == num_scopes ||
+				    !(frame_scopes = realloc(scopes,
+							     frame_end *
+							     sizeof(scopes[i]))))
+					frame_scopes = scopes;
+				err = drgn_stack_trace_append_frame(trace,
+								    trace_capacity,
+								    regs,
+								    frame_scopes,
+								    frame_end,
+								    i);
+				if (err) {
+					free(frame_scopes);
+					/*
+					 * We stole scopes for frame_scopes, so
+					 * not out_scopes.
+					 */
+					goto out;
+				}
+				/*
+				 * Added the DW_TAG_subprogram frame. We're
+				 * done.
+				 */
+				return NULL;
+			case DW_TAG_inlined_subroutine:
+				frame_scopes = memdup(&scopes[i],
+						      (frame_end - i) *
+						      sizeof(scopes[i]));
+				if (!frame_scopes) {
+					err = &drgn_enomem;
+					goto out_scopes;
+				}
+				err = drgn_stack_trace_append_frame(trace,
+								    trace_capacity,
+								    regs,
+								    frame_scopes,
+								    frame_end - i,
+								    0);
+				if (err) {
+					free(frame_scopes);
+					goto out_scopes;
+				}
+				frame_end = i;
+				break;
+			default:
+				break;
+			}
+		} else {
+			/*
+			 * This DIE doesn't contain PC. Ignore it and everything
+			 * after it.
+			 */
+			frame_end = i;
+		}
+	}
+
+	/*
+	 * We didn't find a matching DW_TAG_subprogram. Free any matching
+	 * DW_TAG_inlined_subroutine frames we found and add a scopeless frame.
+	 */
+	for (size_t i = orig_num_frames; i < (*trace)->num_frames; i++)
+		free((*trace)->frames[i].scopes);
+	(*trace)->num_frames = orig_num_frames;
+	err = drgn_stack_trace_append_frame(trace, trace_capacity, regs, NULL,
+					    0, 0);
+out_scopes:
+	free(scopes);
+out:
+	if (err)
+		drgn_register_state_destroy(regs);
+	return err;
+}
+
+static struct drgn_error *
 drgn_unwind_one_register(struct drgn_program *prog,
 			 const struct drgn_cfi_rule *rule,
 			 const struct drgn_register_state *regs, void *buf,
@@ -640,12 +945,10 @@ static struct drgn_error *drgn_get_stack_trace(struct drgn_program *prog,
 
 	/* Limit iterations so we don't get caught in a loop. */
 	for (int i = 0; i < 1024; i++) {
-		if (!drgn_stack_trace_append_frame(&trace, &trace_capacity,
-						   regs)) {
-			err = &drgn_enomem;
-			drgn_register_state_destroy(regs);
+		err = drgn_stack_trace_add_frames(&trace, &trace_capacity,
+						  regs);
+		if (err)
 			goto out;
-		}
 
 		err = drgn_unwind_with_cfi(prog, &row, regs, &regs);
 		if (err == &drgn_not_found) {

libdrgn/python/stack_trace.c
@@ -72,6 +72,32 @@ static void StackFrame_dealloc(StackFrame *self)
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
+static PyObject *StackFrame_str(StackFrame *self)
+{
+	struct drgn_error *err;
+	char *str;
+	err = drgn_format_stack_frame(self->trace->trace, self->i, &str);
+	if (err)
+		return set_drgn_error(err);
+	PyObject *ret = PyUnicode_FromString(str);
+	free(str);
+	return ret;
+}
+
+static PyObject *StackFrame_source(StackFrame *self)
+{
+	int line;
+	int column;
+	const char *filename = drgn_stack_frame_source(self->trace->trace,
+						       self->i, &line, &column);
+	if (!filename) {
+		PyErr_SetString(PyExc_LookupError,
+				"source code location not available");
+		return NULL;
+	}
+	return Py_BuildValue("sii", filename, line, column);
+}
+
 static PyObject *StackFrame_symbol(StackFrame *self)
 {
 	struct drgn_error *err;
@@ -148,6 +174,20 @@ static PyObject *StackFrame_registers(StackFrame *self)
 	return dict;
 }
 
+static PyObject *StackFrame_get_name(StackFrame *self, void *arg)
+{
+	const char *name = drgn_stack_frame_name(self->trace->trace, self->i);
+	if (name)
+		return PyUnicode_FromString(name);
+	else
+		Py_RETURN_NONE;
+}
+
+static PyObject *StackFrame_get_is_inline(StackFrame *self, void *arg)
+{
+	Py_RETURN_BOOL(drgn_stack_frame_is_inline(self->trace->trace, self->i));
+}
+
 static PyObject *StackFrame_get_interrupted(StackFrame *self, void *arg)
 {
 	Py_RETURN_BOOL(drgn_stack_frame_interrupted(self->trace->trace,
@@ -167,6 +207,8 @@ static PyObject *StackFrame_get_pc(StackFrame *self, void *arg)
 }
 
 static PyMethodDef StackFrame_methods[] = {
+	{"source", (PyCFunction)StackFrame_source, METH_NOARGS,
+	 drgn_StackFrame_source_DOC},
 	{"symbol", (PyCFunction)StackFrame_symbol, METH_NOARGS,
 	 drgn_StackFrame_symbol_DOC},
 	{"register", (PyCFunction)StackFrame_register,
@@ -177,6 +219,9 @@ static PyMethodDef StackFrame_methods[] = {
 };
 
 static PyGetSetDef StackFrame_getset[] = {
+	{"name", (getter)StackFrame_get_name, NULL, drgn_StackFrame_name_DOC},
+	{"is_inline", (getter)StackFrame_get_is_inline, NULL,
+	 drgn_StackFrame_is_inline_DOC},
 	{"interrupted", (getter)StackFrame_get_interrupted, NULL,
 	 drgn_StackFrame_interrupted_DOC},
 	{"pc", (getter)StackFrame_get_pc, NULL, drgn_StackFrame_pc_DOC},
@@ -188,6 +233,7 @@ PyTypeObject StackFrame_type = {
 	.tp_name = "_drgn.StackFrame",
 	.tp_basicsize = sizeof(StackFrame),
 	.tp_dealloc = (destructor)StackFrame_dealloc,
+	.tp_str = (reprfunc)StackFrame_str,
 	.tp_flags = Py_TPFLAGS_DEFAULT,
 	.tp_doc = drgn_StackFrame_DOC,
 	.tp_methods = StackFrame_methods,

_drgn.pyi
@@ -19,6 +19,7 @@ from typing import (
     Mapping,
     Optional,
     Sequence,
+    Tuple,
     Union,
     overload,
 )
@@ -1382,23 +1383,24 @@ class StackTrace:
     .. code-block:: python3
 
         for frame in trace:
-            if frame.symbol().name == 'io_schedule':
+            if frame.name == 'io_schedule':
                 print('Thread is doing I/O')
 
     :class:`str() <str>` returns a pretty-printed stack trace:
 
-    >>> print(prog.stack_trace(1))
-    #0  __schedule+0x25c/0x8ba
-    #1  schedule+0x3c/0x7e
-    #2  schedule_hrtimeout_range_clock+0x10c/0x118
-    #3  ep_poll+0x3ca/0x40a
-    #4  do_epoll_wait+0xb0/0xc6
-    #5  __x64_sys_epoll_wait+0x1a/0x1d
-    #6  do_syscall_64+0x55/0x17c
-    #7  entry_SYSCALL_64+0x7c/0x156
+    >>> prog.stack_trace(1)
+    #0  context_switch (kernel/sched/core.c:4339:2)
+    #1  __schedule (kernel/sched/core.c:5147:8)
+    #2  schedule (kernel/sched/core.c:5226:3)
+    #3  do_wait (kernel/exit.c:1534:4)
+    #4  kernel_wait4 (kernel/exit.c:1678:8)
+    #5  __do_sys_wait4 (kernel/exit.c:1706:13)
+    #6  do_syscall_64 (arch/x86/entry/common.c:47:14)
+    #7  entry_SYSCALL_64+0x7c/0x15b (arch/x86/entry/entry_64.S:112)
+    #8  0x4d49dd
 
-    The drgn CLI is set up so that stack traces are displayed with ``str()`` by
-    default.
+    The format is subject to change. The drgn CLI is set up so that stack
+    traces are displayed with ``str()`` by default.
     """
 
     def __getitem__(self, idx: IntegerLike) -> StackFrame: ...
@@ -1406,6 +1408,30 @@ class StackTrace:
 class StackFrame:
     """
     A ``StackFrame`` represents a single *frame* in a thread's call stack.
+
+    :class:`str() <str>` returns a pretty-printed stack frame:
+
+    >>> prog.stack_trace(1)[0]
+    #0 at 0xffffffffb64ac287 (__schedule+0x227/0x606) in context_switch at kernel/sched/core.c:4339:2 (inlined)
+
+    This includes more information than when printing the full stack trace. The
+    format is subject to change. The drgn CLI is set up so that stack frames
+    are displayed with ``str()`` by default.
+    """
+
+    name: Optional[str]
+    """
+    Name of the function at this frame, or ``None`` if it could not be
+    determined.
+    """
+
+    is_inline: bool
+    """
+    Whether this frame is for an inlined call.
+
+    An inline frame shares the same stack frame in memory as its caller.
+    Therefore, it has the same registers (including program counter and thus
+    symbol).
     """
 
     interrupted: bool
@@ -1421,8 +1447,17 @@ class StackFrame:
     particular, the program counter is the return address, which is typically
     the instruction after the call instruction.
     """
+
     pc: int
     """Program counter at this stack frame."""
+    def source(self) -> Tuple[str, int, int]:
+        """
+        Get the source code location of this frame.
+
+        :return: Location as a ``(filename, line, column)`` triple.
+        :raises LookupError: if the source code location is not available
+        """
+        ...
     def symbol(self) -> Symbol:
         """
         Get the function symbol at this stack frame.

drgn/internal/cli.py
@@ -23,7 +23,7 @@ def displayhook(value: Any) -> None:
     setattr(builtins, "_", None)
     if isinstance(value, drgn.Object):
         text = value.format_(columns=shutil.get_terminal_size((0, 0)).columns)
-    elif isinstance(value, (drgn.StackTrace, drgn.Type)):
+    elif isinstance(value, (drgn.StackFrame, drgn.StackTrace, drgn.Type)):
         text = str(value)
     else:
         text = repr(value)

libdrgn/drgn.h.in
@@ -2609,6 +2609,42 @@ struct drgn_error *drgn_format_stack_trace(struct drgn_stack_trace *trace,
 bool drgn_stack_frame_interrupted(struct drgn_stack_trace *trace, size_t frame);
 
 /**
+ * Format a stack frame as a string.
+ *
+ * @param[out] ret Returned string. On success, it must be freed with @c free().
+ * On error, its contents are undefined.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *drgn_format_stack_frame(struct drgn_stack_trace *trace,
+					   size_t frame, char **ret);
+
+/**
+ * Get the name of the function at a stack frame.
+ *
+ * @return Function name. This is valid until the stack trace is destroyed; it
+ * should not be freed. @c NULL if the name could not be determined.
+ */
+const char *drgn_stack_frame_name(struct drgn_stack_trace *trace, size_t frame);
+
+/** Return whether a stack frame is for an inlined call. */
+bool drgn_stack_frame_is_inline(struct drgn_stack_trace *trace, size_t frame);
+
+/**
+ * Get the source code location of a stack frame.
+ *
+ * @param[out] line_ret Returned line number. Returned as 0 if unknown. May be
+ * @c NULL if not needed.
+ * @param[out] column_ret Returned column number. Returned as 0 if unknown. May
+ * be @c NULL if not needed.
+ * @return Filename. This is valid until the stack trace is destroyed; it should
+ * not be freed. @c NULL if the location could not be determined (in which case
+ * `*line_ret` and `*column_ret` are undefined).
+ */
+const char *drgn_stack_frame_source(struct drgn_stack_trace *trace,
+				    size_t frame, int *line_ret,
+				    int *column_ret);
+
+/**
  * Get the program counter at a stack frame.
  *
  * @param[out] ret Returned program counter.

libdrgn/stack_trace.h
@@ -28,6 +28,9 @@
 
 struct drgn_stack_frame {
 	struct drgn_register_state *regs;
+	Dwarf_Die *scopes;
+	size_t num_scopes;
+	size_t function_scope;
 };
 
 struct drgn_stack_trace {

libdrgn/util.h
@@ -118,6 +118,14 @@ static inline void *malloc64(uint64_t size)
 	return malloc(size);
 }
 
+static inline void *memdup(void *ptr, size_t size)
+{
+	void *copy = malloc(size);
+	if (copy)
+		memcpy(copy, ptr, size);
+	return copy;
+}
+
 /** Return the maximum value of an @p n-byte unsigned integer. */
 static inline uint64_t uint_max(int n)
 {

commit 86fa471472304426b2d60f285f18f9e9398dbc29

libdrgn: stack_trace: fix double-free in drgn_stack_trace_add_frames() error path
We're leaving regs in the trace after it is freed, which
drgn_stack_trace_destroy() will then try to free again. Fix it by
resetting the trace like we do if we don't find any subprogram DIEs.

Fixes: 38573cfdde80 ("libdrgn: stack_trace: pretty print frames and add frames for inline functions")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/stack_trace.c
@@ -956,6 +956,8 @@ drgn_stack_trace_add_frames(struct drgn_stack_trace **trace,
 {
 	struct drgn_error *err;
 
+	size_t orig_num_frames = (*trace)->num_frames;
+
 	if (!regs->module) {
 		err = drgn_stack_trace_append_frame(trace, trace_capacity, regs,
 						    NULL, 0, 0);
@@ -972,7 +974,6 @@ drgn_stack_trace_add_frames(struct drgn_stack_trace **trace,
 		goto out;
 	pc -= bias;
 
-	size_t orig_num_frames = (*trace)->num_frames;
 	/*
 	 * Walk backwards through scopes, splitting into frames. Stop at index 1
 	 * because 0 must be a unit DIE.
@@ -1084,8 +1085,14 @@ drgn_stack_trace_add_frames(struct drgn_stack_trace **trace,
 out_scopes:
 	free(scopes);
 out:
-	if (err)
+	if (err) {
+		// We may have already appended frames referring to regs, so get
+		// rid of them before destroying it.
+		for (size_t i = orig_num_frames; i < (*trace)->num_frames; i++)
+			free((*trace)->frames[i].scopes);
+		(*trace)->num_frames = orig_num_frames;
 		drgn_register_state_destroy(regs);
+	}
 	return err;
 }
 