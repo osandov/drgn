osandov/drgn

commit 26291647eb605bff59b3fc15825745a630fe6080

libdrgn: dwarf_index: handle DW_AT_specification DIEs with two passes
We currently handle DIEs with a DW_AT_specification attribute by parsing
the corresponding declaration to get the name and inserting the DIE as
usual. This has a couple of problems:

1. It only works if DW_AT_specification refers to the same compilation
   unit, which is true for DW_FORM_ref{1,2,4,8,_udata}, but not
   DW_FORM_ref_addr. As a result, drgn doesn't support the latter.
2. It assumes that the DIE with DW_AT_specification is in the correct
   "scope". Unfortunately, this is not true for g++: for a variable
   definition in a C++ namespace, it generates a DIE with
   DW_AT_declaration as a child of the DW_TAG_namespace DIE and a DIE
   which refers to the declaration with DW_AT_specification _outside_ of
   the DW_TAG_namespace as a child of the DW_TAG_compilation_unit DIE.

Supporting both of these cases requires reworking how we handle
DW_AT_specification. This commit takes an approach of parsing the DWARF
data in two passes: the first pass reads the abbrevation and file name
tables and builds a map of instances of DW_AT_specification; the second
pass indexes DIEs as before, but ignores DIEs with DW_AT_specification
and handles DIEs with DW_AT_declaration by looking them up in the map
built by the first pass.

This approach is a 10-20% regression in indexing time in the benchmarks
I ran. Thankfully, it is not 100% slower for a couple of reasons. The
first is that the two passes are simpler than the original combined
pass. The second is that a decent part of the indexing time is spent
faulting in the mapped debugging information, which only needs to happen
once (even if the file is cached, minor page faults add non-negligible
overhead).

This doesn't handle DW_AT_specification "chains" yet, but neither did
the original code. If it is necessary, it shouldn't be too difficult to
add.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

tests/test_dwarf.py
@@ -3471,6 +3471,39 @@ class TestObjects(TestCase):
         )
         self.assertRaisesRegex(Exception, "too small", prog.variable, "p")
 
+    def test_specification(self):
+        prog = dwarf_program(
+            test_type_dies(
+                (
+                    int_die,
+                    DwarfDie(
+                        DW_TAG.variable,
+                        (
+                            DwarfAttrib(DW_AT.name, DW_FORM.string, "x"),
+                            DwarfAttrib(DW_AT.type, DW_FORM.ref4, 0),
+                            DwarfAttrib(DW_AT.declaration, DW_FORM.flag_present, True),
+                        ),
+                    ),
+                    DwarfDie(
+                        DW_TAG.variable,
+                        (
+                            DwarfAttrib(DW_AT.specification, DW_FORM.ref4, 1),
+                            DwarfAttrib(
+                                DW_AT.location,
+                                DW_FORM.exprloc,
+                                b"\x03\x04\x03\x02\x01\xff\xff\xff\xff",
+                            ),
+                        ),
+                    ),
+                )
+            )
+        )
+
+        self.assertEqual(
+            prog["x"],
+            Object(prog, prog.int_type("int", 4, True), address=0xFFFFFFFF01020304),
+        )
+
     def test_not_found(self):
         prog = dwarf_program(int_die)
         self.assertRaisesRegex(LookupError, "could not find", prog.object, "y")

libdrgn/dwarf_index.h
@@ -69,6 +69,10 @@ DEFINE_VECTOR_TYPE(drgn_dwarf_index_die_vector, struct drgn_dwarf_index_die)
 struct drgn_dwarf_index_shard {
 	/** @privatesection */
 	omp_lock_t lock;
+	/*
+	 * Map from name to list of DIEs with that name (as the index in
+	 * drgn_dwarf_index_shard::dies of the first DIE with that name).
+	 */
 	struct drgn_dwarf_index_die_map map;
 	/*
 	 * We store all entries in a shard as a single array, which is more
@@ -78,6 +82,28 @@ struct drgn_dwarf_index_shard {
 };
 
 #define DRGN_DWARF_INDEX_SHARD_BITS 8
+
+/* A DIE with a DW_AT_specification attribute. */
+struct drgn_dwarf_index_specification {
+	/*
+	 * Address of non-defining declaration DIE referenced by
+	 * DW_AT_specification.
+	 */
+	uintptr_t declaration;
+	/* Module and offset of DIE. */
+	Dwfl_Module *module;
+	size_t offset;
+};
+
+static inline uintptr_t
+drgn_dwarf_index_specification_to_key(const struct drgn_dwarf_index_specification *entry)
+{
+	return entry->declaration;
+}
+
+DEFINE_HASH_TABLE_TYPE(drgn_dwarf_index_specification_map,
+		       struct drgn_dwarf_index_specification,
+		       drgn_dwarf_index_specification_to_key)
 
 /** State of a @ref drgn_dwarf_module or a @c Dwfl_Module. */
 enum drgn_dwarf_module_state {
@@ -184,6 +210,15 @@ struct drgn_dwarf_index {
 	 * This is sharded to reduce lock contention.
 	 */
 	struct drgn_dwarf_index_shard shards[1 << DRGN_DWARF_INDEX_SHARD_BITS];
+	/**
+	 * Map from address of DIE referenced by DW_AT_specification to DIE that
+	 * references it. This is used to resolve DIEs with DW_AT_declaration to
+	 * their definition.
+	 *
+	 * This is not sharded because there typically aren't enough of these in
+	 * a program to cause contention.
+	 */
+	struct drgn_dwarf_index_specification_map specifications;
 	Dwfl *dwfl;
 	/**
 	 * Formatted errors reported by @ref drgn_dwarf_index_report_error().

commit d6a71b0273d5476d7acfb352c98f9152d9139097

libdrgn: dwarf_info: handle enum type declaration with enumerators
It doesn't make sense for a DW_TAG_enumeration_type DIE with
DW_AT_declaration to have DW_TAG_enumerator children, so the DWARF index
doesn't check for this and still indexes them. However,
drgn_enum_type_from_dwarf() ignores them, which breaks the assumption in
drgn_object_from_dwarf_enumerator() that it should always find the
indexed enumerator. Therefore, a crafted file could make us abort in
drgn_object_from_dwarf_enumerator().

Handle it gracefully in drgn_debug_info_find_object() and
drgn_object_from_dwarf_enumerator() rather than adding overhead to the
DWARF index.

Fixes: 26291647eb60 ("libdrgn: dwarf_index: handle DW_AT_specification DIEs with two passes")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/dwarf_info.c
@@ -4701,7 +4701,12 @@ drgn_object_from_dwarf_enumerator(struct drgn_debug_info *dbinfo,
 							0);
 		}
 	}
-	UNREACHABLE();
+	// This can happen if the DWARF index and drgn_type_from_dwarf()
+	// disagree. In particular, the DWARF index doesn't check for
+	// DW_AT_declaration on DW_TAG_enumeration_type DIEs, so if such a DIE
+	// has DW_TAG_enumerator children (which is malformed), the DWARF index
+	// will include them but drgn_type_from_dwarf() won't.
+	return &drgn_not_found;
 }
 
 static struct drgn_error *
@@ -6779,9 +6784,11 @@ drgn_debug_info_find_object(const char *name, size_t name_len,
 		if (!die_matches_filename(&die, filename))
 			continue;
 		if (dwarf_tag(&die) == DW_TAG_enumeration_type) {
-			return drgn_object_from_dwarf_enumerator(dbinfo, file,
-								 &die, name,
-								 ret);
+			err = drgn_object_from_dwarf_enumerator(dbinfo, file,
+								&die, name,
+								ret);
+			if (err != &drgn_not_found)
+				return err;
 		} else {
 			return drgn_object_from_dwarf(dbinfo, file, &die, NULL,
 						      NULL, NULL, ret);

tests/test_dwarf.py
@@ -4551,6 +4551,31 @@ class TestObjects(TestCase):
             ),
         )
 
+    def test_constant_declaration_enum(self):
+        # enumerator children on a declaration enumeration_type should be
+        # ignored.
+        prog = dwarf_program(
+            (
+                DwarfDie(
+                    DW_TAG.enumeration_type,
+                    (
+                        DwarfAttrib(DW_AT.name, DW_FORM.string, "color"),
+                        DwarfAttrib(DW_AT.declaration, DW_FORM.flag_present, True),
+                    ),
+                    (
+                        DwarfDie(
+                            DW_TAG.enumerator,
+                            (
+                                DwarfAttrib(DW_AT.name, DW_FORM.string, "RED"),
+                                DwarfAttrib(DW_AT.const_value, DW_FORM.data1, 0),
+                            ),
+                        ),
+                    ),
+                ),
+            )
+        )
+        self.assertRaises(ObjectNotFoundError, prog.constant, "RED")
+
     def test_function(self):
         prog = dwarf_program(
             wrap_test_type_dies(