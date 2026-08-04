osandov/drgn

commit db66952b2e31b51c89ebacd9438d1090557436b8

libdrgn: add DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS

libdrgn/language_c.c
@@ -1035,8 +1035,27 @@ compound_initializer_iter_next(struct initializer_iter *iter_,
 		 */
 		if (member->name ||
 		    !(iter->flags & DRGN_FORMAT_OBJECT_MEMBER_NAMES) ||
-		    !drgn_type_has_members(member_type.type))
+		    !drgn_type_has_members(member_type.type)) {
+			err = drgn_object_slice(obj_ret, iter->obj, member_type,
+						top->bit_offset + member->bit_offset,
+						member->bit_field_size);
+			if (err)
+				return err;
+
+			/* If we're including names, we can skip zeroes. */
+			if ((iter->flags & (DRGN_FORMAT_OBJECT_MEMBER_NAMES |
+					    DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS)) ==
+			     DRGN_FORMAT_OBJECT_MEMBER_NAMES) {
+				bool zero;
+
+				err = drgn_object_is_zero(obj_ret, &zero);
+				if (err)
+					return err;
+				if (zero)
+					continue;
+			}
 			break;
+		}
 
 		new = compound_initializer_stack_append_entry(&iter->stack);
 		if (!new)
@@ -1046,11 +1065,6 @@ compound_initializer_iter_next(struct initializer_iter *iter_,
 		new->bit_offset = top->bit_offset + member->bit_offset;
 	}
 
-	err = drgn_object_slice(obj_ret, iter->obj, member_type,
-				top->bit_offset + member->bit_offset,
-				member->bit_field_size);
-	if (err)
-		return err;
 	*flags_ret = iter->member_flags;
 	return NULL;
 }
@@ -1133,6 +1147,45 @@ c_format_compound_object(const struct drgn_object *obj,
 	new->member = drgn_type_members(underlying_type);
 	new->end = new->member + drgn_type_num_members(underlying_type);
 	new->bit_offset = 0;
+
+	/*
+	 * If we don't want zero members, ignore any at the end. If we're
+	 * including member names, then we'll skip past zero members as we
+	 * iterate, so we don't need to do this.
+	 */
+	if (!(flags & (DRGN_FORMAT_OBJECT_MEMBER_NAMES |
+		       DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS)) &&
+	    new->member < new->end) {
+		struct drgn_object member;
+
+		drgn_object_init(&member, obj->prog);
+		do {
+			struct drgn_qualified_type member_type;
+			bool zero;
+
+			err = drgn_member_type(&new->end[-1], &member_type);
+			if (err)
+				break;
+
+			err = drgn_object_slice(&member, obj, member_type,
+						new->end[-1].bit_offset,
+						new->end[-1].bit_field_size);
+			if (err)
+				break;
+
+			err = drgn_object_is_zero(&member, &zero);
+			if (err)
+				break;
+			if (zero)
+				new->end--;
+			else
+				break;
+		} while (new->member < new->end);
+		drgn_object_deinit(&member);
+		if (err)
+			return err;
+	}
+
 	err = c_format_initializer(obj->prog, &iter.iter, indent,
 				   one_line_columns, multi_line_columns,
 				   flags & DRGN_FORMAT_OBJECT_MEMBERS_SAME_LINE,

libdrgn/python/object.c
@@ -980,7 +980,8 @@ static PyObject *DrgnObject_format(DrgnObject *self, PyObject *args,
 	X(members_same_line, DRGN_FORMAT_OBJECT_MEMBERS_SAME_LINE)	\
 	X(elements_same_line, DRGN_FORMAT_OBJECT_ELEMENTS_SAME_LINE)	\
 	X(member_names, DRGN_FORMAT_OBJECT_MEMBER_NAMES)		\
-	X(element_indices, DRGN_FORMAT_OBJECT_ELEMENT_INDICES)
+	X(element_indices, DRGN_FORMAT_OBJECT_ELEMENT_INDICES)		\
+	X(implicit_members, DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS)
 
 	static char *keywords[] = {
 #define X(name, value) #name,

libdrgn/drgn.h.in
@@ -2013,6 +2013,7 @@ enum drgn_format_object_flags {
 	DRGN_FORMAT_OBJECT_ELEMENTS_SAME_LINE = 1 << 8,
 	DRGN_FORMAT_OBJECT_MEMBER_NAMES = 1 << 9,
 	DRGN_FORMAT_OBJECT_ELEMENT_INDICES = 1 << 10,
+	DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS = 1 << 11,
 	/** Default "pretty" flags. */
 	DRGN_FORMAT_OBJECT_PRETTY = (DRGN_FORMAT_OBJECT_DEREFERENCE |
 				     DRGN_FORMAT_OBJECT_SYMBOLIZE |
@@ -2020,8 +2021,9 @@ enum drgn_format_object_flags {
 				     DRGN_FORMAT_OBJECT_TYPE_NAME |
 				     DRGN_FORMAT_OBJECT_MEMBER_TYPE_NAMES |
 				     DRGN_FORMAT_OBJECT_ELEMENTS_SAME_LINE |
-				     DRGN_FORMAT_OBJECT_MEMBER_NAMES),
-	DRGN_FORMAT_OBJECT_VALID_FLAGS = (1 << 11) - 1,
+				     DRGN_FORMAT_OBJECT_MEMBER_NAMES |
+				     DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS),
+	DRGN_FORMAT_OBJECT_VALID_FLAGS = (1 << 12) - 1,
 };
 
 /**

libdrgn/language.h
@@ -134,7 +134,8 @@ drgn_passthrough_format_object_flags(enum drgn_format_object_flags flags)
 			 DRGN_FORMAT_OBJECT_MEMBERS_SAME_LINE |
 			 DRGN_FORMAT_OBJECT_ELEMENTS_SAME_LINE |
 			 DRGN_FORMAT_OBJECT_MEMBER_NAMES |
-			 DRGN_FORMAT_OBJECT_ELEMENT_INDICES));
+			 DRGN_FORMAT_OBJECT_ELEMENT_INDICES |
+			 DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS));
 }
 
 /** Return flags that should be passed when formatting object members. */

tests/test_object.py
@@ -1352,6 +1352,28 @@ class TestCPretty(ObjectTestCase):
         type_ = struct_type('foo', 0, ())
         self.assertEqual(str(Object(prog, type_, address=0)), '(struct foo){}')
 
+        obj = Object(prog, point_type, value={'x': 1})
+        self.assertEqual(obj.format_(implicit_members=False), """\
+(struct point){
+	.x = (int)1,
+}""")
+        self.assertEqual(
+            obj.format_(member_names=False, implicit_members=False), """\
+(struct point){
+	(int)1,
+}""")
+        obj = Object(prog, point_type, value={'y': 1})
+        self.assertEqual(obj.format_(implicit_members=False), """\
+(struct point){
+	.y = (int)1,
+}""")
+        self.assertEqual(
+            obj.format_(member_names=False, implicit_members=False), """\
+(struct point){
+	(int)0,
+	(int)1,
+}""")
+
     def test_bit_field(self):
         segment = b'\x07\x10\x5e\x5f\x1f\0\0\0'
         prog = mock_program(segments=[

commit 37442ac59faefbc900ffcef50976c430b5a28c8f

libdrgn: c: fix DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS with incomplete members
drgn_object_is_zero() fails with "object cannot be converted to boolean"
for incomplete members (e.g., flexible array members), so
pretty-printing an object containing such a member fails. Fix it by
skipping them as if they were zero.

Fixes: db66952b2e31 ("libdrgn: add DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/language_c.c
@@ -1125,8 +1125,7 @@ compound_initializer_iter_next(struct initializer_iter *iter_,
 					    DRGN_FORMAT_OBJECT_IMPLICIT_MEMBERS)) ==
 			     DRGN_FORMAT_OBJECT_MEMBER_NAMES) {
 				bool zero;
-
-				err = drgn_object_is_zero(ret, &zero);
+				err = drgn_object_is_zero_or_incomplete(ret, &zero);
 				if (err)
 					return err;
 				if (zero)
@@ -1247,7 +1246,7 @@ c_format_compound_object(const struct drgn_object *obj,
 				goto out;
 
 			bool zero;
-			err = drgn_object_is_zero(&member, &zero);
+			err = drgn_object_is_zero_or_incomplete(&member, &zero);
 			if (err)
 				goto out;
 			if (zero)
@@ -1454,8 +1453,6 @@ array_initializer_iter_next(struct initializer_iter *iter_,
 		container_of(iter_, struct array_initializer_iter, iter);
 
 	for (;;) {
-		bool zero;
-
 		if (iter->i >= iter->length)
 			return &drgn_stop;
 		err = drgn_object_fragment(ret, iter->obj, iter->element_type,
@@ -1470,6 +1467,7 @@ array_initializer_iter_next(struct initializer_iter *iter_,
 		    DRGN_FORMAT_OBJECT_ELEMENT_INDICES)
 			break;
 
+		bool zero;
 		err = drgn_object_is_zero(ret, &zero);
 		if (err)
 			return err;
@@ -1568,8 +1566,6 @@ c_format_array_object(const struct drgn_object *obj,
 	    && iter.length) {
 		DRGN_OBJECT(element, drgn_object_program(obj));
 		do {
-			bool zero;
-
 			err = drgn_object_fragment(&element, obj,
 						   iter.element_type,
 						   (iter.length - 1)
@@ -1578,6 +1574,7 @@ c_format_array_object(const struct drgn_object *obj,
 			if (err)
 				return err;
 
+			bool zero;
 			err = drgn_object_is_zero(&element, &zero);
 			if (err)
 				return err;

libdrgn/object.c
@@ -1431,6 +1431,16 @@ struct drgn_error *drgn_object_is_zero(const struct drgn_object *obj, bool *ret)
 	return drgn_object_is_zero_impl(obj, ret);
 }
 
+struct drgn_error *
+drgn_object_is_zero_or_incomplete(const struct drgn_object *obj, bool *ret)
+{
+	if (!drgn_object_encoding_is_complete(obj->encoding)) {
+		*ret = true;
+		return NULL;
+	}
+	return drgn_object_is_zero(obj, ret);
+}
+
 LIBDRGN_PUBLIC struct drgn_error *
 drgn_object_cast(struct drgn_object *res,
 		 struct drgn_qualified_type qualified_type,

libdrgn/object.h
@@ -69,6 +69,13 @@ static inline bool drgn_value_zalloc(uint64_t size, union drgn_value *value_ret,
 struct drgn_error *drgn_object_is_zero(const struct drgn_object *obj,
 				       bool *ret);
 
+/**
+ * Like @ref drgn_object_is_zero(), but also returns true for incomplete
+ * objects.
+ */
+struct drgn_error *
+drgn_object_is_zero_or_incomplete(const struct drgn_object *obj, bool *ret);
+
 /** Type-related fields from @ref drgn_object. */
 struct drgn_object_type {
 	struct drgn_type *type;

tests/test_language_c.py
@@ -3274,6 +3274,48 @@ class TestPrettyPrintObject(MockProgramTestCase):
         self.assertEqual(str(Object(self.prog, "int []", address=0)), "(int []){}")
         self.assertEqual(str(Object(self.prog, "int [0]", address=0)), "(int [0]){}")
 
+    def test_flexible_array_member(self):
+        self.add_memory_segment((99).to_bytes(4, "little"), virt_addr=0xFFFF0000)
+        obj = Object(
+            self.prog,
+            self.prog.struct_type(
+                "foo",
+                4,
+                (
+                    TypeMember(self.prog.int_type("int", 4, True), "n", 0),
+                    TypeMember(
+                        self.prog.array_type(self.prog.int_type("int", 4, True)),
+                        "a",
+                        32,
+                    ),
+                ),
+            ),
+            address=0xFFFF0000,
+        )
+        self.assertEqual(
+            str(obj),
+            """\
+(struct foo){
+	.n = (int)99,
+	.a = (int []){},
+}""",
+        )
+        self.assertEqual(
+            str(obj.read_()),
+            """\
+(struct foo){
+	.n = (int)99,
+	.a = (int [])<absent>,
+}""",
+        )
+
+        expected = """\
+(struct foo){
+	.n = (int)99,
+}"""
+        self.assertEqual(obj.format_(implicit_members=False), expected)
+        self.assertEqual(obj.read_().format_(implicit_members=False), expected)
+
     def test_array_zeroes(self):
         segment = bytearray(16)
         self.add_memory_segment(segment, virt_addr=0xFFFF0000)