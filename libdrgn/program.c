osandov/drgn

commit 0e635df5294ebf34660fb12ee2ee371dd5143391

Handle truncated pr_fname
It appears that some core dumps may have a "pr_fname" which isn't
nul-terminated, which leads to the following exception, even if you're
not interested in the thread name:

>>> next(prog.threads()).stack_trace()
Traceback (most recent call last):
  File "<console>", line 1, in <module>
Exception: pr_fname is not null terminated

Handle this by creating a copy and nul-terminating it.

Closes: #483
Fixes: e6cc9b07 ("Add name to drgn.Thread")

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/program.c
@@ -137,12 +137,12 @@ void drgn_program_init(struct drgn_program *prog,
 void drgn_program_deinit(struct drgn_program *prog)
 {
 	drgn_thread_set_deinit(&prog->thread_set);
-	/*
-	 * For userspace core dumps, main_thread and crashed_thread are in
-	 * prog->thread_set and thus freed by the above call to
-	 * drgn_thread_set_deinit().
-	 */
-	if (!drgn_program_is_userspace_core(prog)) {
+	if (drgn_program_is_userspace_core(prog)) {
+		free(prog->core_dump_fname_cached);
+	} else {
+		// For userspace core dumps, main_thread and crashed_thread are
+		// in prog->thread_set and thus freed by the above call to
+		// drgn_thread_set_deinit().
 		drgn_thread_destroy(prog->crashed_thread);
 		drgn_thread_destroy(prog->main_thread);
 	}
@@ -924,7 +924,7 @@ static struct drgn_error *get_prpsinfo_pid(struct drgn_program *prog,
 
 static struct drgn_error *get_prpsinfo_fname(struct drgn_program *prog,
 					   const char *data, size_t size,
-					   const char **ret)
+					   char **ret)
 {
 	bool is_64_bit;
 	struct drgn_error *err = drgn_program_is_64_bit(prog, &is_64_bit);
@@ -938,15 +938,10 @@ static struct drgn_error *get_prpsinfo_fname(struct drgn_program *prog,
 		return drgn_error_create(DRGN_ERROR_OTHER,
 					 "NT_PRPSINFO is truncated");
 	}
-	// No need to make a copy: the data returned by elf_getdata_rawchunk()
-	// is valid for the lifetime of the Elf handle, and prog->core is valid for
-	// the lifetime of prog.
-	const char *tmp = data + offset;
-	size_t len = strnlen(tmp, PR_FNAME_LEN);
-	if (len == PR_FNAME_LEN)
+	char *tmp = strndup(data + offset, PR_FNAME_LEN);
 #undef PR_FNAME_LEN
-		return drgn_error_create(DRGN_ERROR_OTHER,
-					 "pr_fname is not null terminated");
+	if (!tmp)
+		return &drgn_enomem;
 	*ret = tmp;
 	return NULL;
 }
@@ -1030,7 +1025,7 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 	uint32_t first_prstatus_tid;
 	bool found_prpsinfo = false;
 	uint32_t prpsinfo_pid;
-	const char *prpsinfo_fname = NULL;
+	_cleanup_free_ char *prpsinfo_fname = NULL;
 
 	if (prog->core_dump_threads_cached)
 		return NULL;
@@ -1130,7 +1125,7 @@ out:
 						       &prpsinfo_pid);
 			/* If the PID isn't found, then this is NULL. */
 			prog->main_thread = it.entry;
-			prog->core_dump_fname_cached = prpsinfo_fname;
+			prog->core_dump_fname_cached = no_cleanup_ptr(prpsinfo_fname);
 		}
 		if (found_prstatus) {
 			/*

libdrgn/program.h
@@ -153,7 +153,7 @@ struct drgn_program {
 		 */
 		struct {
 			/** Cached `pr_fname` from `NT_PRPSINFO` note. */
-			const char *core_dump_fname_cached;
+			char *core_dump_fname_cached;
 			/** Cache of important parts of auxiliary vector. */
 			struct {
 				uint64_t at_phdr;

tests/test_thread.py
@@ -3,6 +3,7 @@
 
 import os
 import os.path
+import tempfile
 
 from drgn import Program
 from tests import TestCase
@@ -95,3 +96,20 @@ class TestCoreDump(TestCase):
         for tid in self.TIDS:
             if tid != self.MAIN_TID:
                 self.assertIsNone(self.prog.thread(tid).name)
+
+
+class TestCoreDumpLongName(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.prog = Program()
+        with open(get_resource("crashme_static_pie.core"), "rb") as f:
+            data = f.read()
+        data = data.replace(b"crashme_static_\x00", b"crashme_static_p")
+        with tempfile.NamedTemporaryFile("wb") as f:
+            f.write(data)
+            f.flush()
+            cls.prog.set_core_dump(f.name)
+
+    def test_thread_name(self):
+        self.assertEqual(self.prog.main_thread().name, "crashme_static_p")

commit 9f6bd02f2053178ee6dc297d12e9a52f3ec66a5e

libdrgn: program: fix memory leak on multiple NT_PRPSINFO
drgn_program_cache_core_dump_threads() allocates a copy of the fname of
every NT_PRPSINFO note without freeing a previous one, so all but the
last is leaked. It doesn't make sense for there to be multiple, so only
use the first one.

We can also remove the redundant found_prpsinfo while we're here.

Fixes: 0e635df5294e ("Handle truncated pr_fname")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -1120,7 +1120,6 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 	size_t phnum, i;
 	bool found_prstatus = false;
 	uint32_t first_prstatus_tid;
-	bool found_prpsinfo = false;
 	uint32_t prpsinfo_pid;
 	_cleanup_free_ char *prpsinfo_fname = NULL;
 
@@ -1178,7 +1177,7 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 			if (strncmp(name, "CORE", nhdr.n_namesz) != 0)
 				continue;
 
-			if (nhdr.n_type == NT_PRPSINFO) {
+			if (nhdr.n_type == NT_PRPSINFO && !prpsinfo_fname) {
 				err = get_prpsinfo_pid(prog,
 						       (char *)data->d_buf + desc_offset,
 						       nhdr.n_descsz,
@@ -1191,7 +1190,6 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 						       &prpsinfo_fname);
 				if (err)
 					goto err;
-				found_prpsinfo = true;
 			} else if (nhdr.n_type == NT_PRSTATUS) {
 				uint32_t tid;
 				err = drgn_program_cache_prstatus_entry(prog,
@@ -1216,7 +1214,7 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 out:
 	prog->core_dump_threads_cached = true;
 	if (!(prog->flags & DRGN_PROGRAM_IS_LINUX_KERNEL)) {
-		if (found_prpsinfo) {
+		if (prpsinfo_fname) {
 			struct drgn_thread_set_iterator it =
 				drgn_thread_set_search(&prog->thread_set,
 						       &prpsinfo_pid);