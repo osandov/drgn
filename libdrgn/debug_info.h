osandov/drgn

commit 7dc474d75790c6bd3c11ca2723d1000a8c1e0b15

Support searching for kernel modules without depmod metadata
For live kernel debugging, you usually have a properly installed kernel
that has run depmod. However, for core dumps, it's common to get an
archive containing all of the kernel module files and no depmod
metadata. Peter Collingbourne also reported that the depmod situation on
Android is complicated: Android doesn't have a modules.dep.bin file, and
modules are split among different images.

Let's add support for walking kernel module directories if
modules.dep.bin isn't found and a debug info option for controlling
whether depmod, walking, or both are tried. We default to using depmod
if it is available and walking if not.

Closes #369.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.h
@@ -287,10 +287,37 @@ struct depmod_index {
 	size_t len;
 };
 
+DEFINE_VECTOR_TYPE(char_p_vector, char *);
+
+DEFINE_HASH_MAP_TYPE(drgn_kmod_walk_module_map, const char *,
+		     struct char_p_vector);
+
+DEFINE_VECTOR_TYPE(drgn_kmod_walk_stack,
+		   struct drgn_kmod_walk_stack_entry);
+
+struct drgn_kmod_walk_inode {
+	dev_t dev;
+	ino_t ino;
+};
+
+DEFINE_HASH_SET_TYPE(drgn_kmod_walk_inode_set, struct drgn_kmod_walk_inode);
+
+struct drgn_kmod_walk_state {
+	struct drgn_kmod_walk_module_map modules;
+	struct drgn_kmod_walk_stack stack;
+	struct string_builder path;
+	struct drgn_kmod_walk_inode_set visited_dirs;
+	const char * const *next_kernel_dir;
+	const char * const *next_debug_dir;
+};
+
 // State kept by standard debug info finder for all modules it's working on.
 // Currently it's only used to cache locations of Linux kernel loadable modules.
 struct drgn_standard_debug_info_find_state {
+	struct drgn_module * const *modules;
+	size_t num_modules;
 	struct depmod_index modules_dep;
+	struct drgn_kmod_walk_state kmod_walk;
 };
 
 void

libdrgn/linux_kernel.c
@@ -598,9 +598,60 @@ not_found:
 	return NULL;
 }
 
+DEFINE_VECTOR_FUNCTIONS(char_p_vector);
+
+DEFINE_HASH_MAP_FUNCTIONS(drgn_kmod_walk_module_map, c_string_key_hash_pair,
+			  c_string_key_eq);
+
+struct drgn_kmod_walk_stack_entry {
+	DIR *dir;
+	size_t path_len;
+};
+
+DEFINE_VECTOR_FUNCTIONS(drgn_kmod_walk_stack);
+
+static inline struct hash_pair
+drgn_kmod_walk_inode_hash_pair(const struct drgn_kmod_walk_inode *entry)
+{
+	return hash_pair_from_avalanching_hash(hash_combine(entry->dev, entry->ino));
+}
+
+static inline bool
+drgn_kmod_walk_inode_eq(const struct drgn_kmod_walk_inode *a,
+			const struct drgn_kmod_walk_inode *b)
+{
+	return a->dev == b->dev && a->ino == b->ino;
+}
+
+DEFINE_HASH_SET_FUNCTIONS(drgn_kmod_walk_inode_set,
+			  drgn_kmod_walk_inode_hash_pair,
+			  drgn_kmod_walk_inode_eq);
+
+static void
+drgn_kmod_walk_module_map_entry_deinit(struct drgn_kmod_walk_module_map_entry *entry)
+{
+	vector_for_each(char_p_vector, path, &entry->value)
+		free(*path);
+	char_p_vector_deinit(&entry->value);
+}
+
+static void
+drgn_kmod_walk_state_deinit(struct drgn_kmod_walk_state *state)
+{
+	drgn_kmod_walk_inode_set_deinit(&state->visited_dirs);
+	string_builder_deinit(&state->path);
+	vector_for_each(drgn_kmod_walk_stack, entry, &state->stack)
+		closedir(entry->dir);
+	drgn_kmod_walk_stack_deinit(&state->stack);
+	hash_table_for_each(drgn_kmod_walk_module_map, it, &state->modules)
+		drgn_kmod_walk_module_map_entry_deinit(it.entry);
+	drgn_kmod_walk_module_map_deinit(&state->modules);
+}
+
 void
 drgn_standard_debug_info_find_state_deinit(struct drgn_standard_debug_info_find_state *state)
 {
+	drgn_kmod_walk_state_deinit(&state->kmod_walk);
 	depmod_index_deinit(&state->modules_dep);
 }
 
@@ -805,28 +856,21 @@ drgn_module_try_depmod_in_debug_directories(struct drgn_module *module,
 	return NULL;
 }
 
-struct drgn_error *
-drgn_module_try_linux_kmod_files(struct drgn_module *module,
-				 const struct drgn_debug_info_options *options,
-				 struct drgn_standard_debug_info_find_state *state)
+static struct drgn_error *
+drgn_module_try_linux_kmod_depmod(struct drgn_module *module,
+				  const struct drgn_debug_info_options *options,
+				  struct drgn_standard_debug_info_find_state *state)
 {
 	struct drgn_error *err;
 	struct drgn_program *prog = module->prog;
-	struct depmod_index *modules_dep = &state->modules_dep;
-
-	err = drgn_open_modules_dep(prog, options, modules_dep);
-	if (err)
-		return err;
-	if (modules_dep->len == 0)
-		return NULL;
 
 	const char *depmod_path;
 	size_t depmod_path_len;
-	err = depmod_index_find(modules_dep, module->name, &depmod_path,
+	err = depmod_index_find(&state->modules_dep, module->name, &depmod_path,
 				&depmod_path_len);
 	if (err) {
-		drgn_error_log_debug(prog, err,
-				     "couldn't parse depmod index: ");
+		drgn_error_log_warning(prog, err,
+				       "couldn't parse depmod index: ");
 		drgn_error_destroy(err);
 		return NULL;
 	}
@@ -895,6 +939,301 @@ drgn_module_try_linux_kmod_files(struct drgn_module *module,
 		if (err || !drgn_module_wants_file(module))
 			return err;
 	}
+	return NULL;
+}
+
+static struct drgn_error *
+drgn_kmod_walk_next_dir(struct drgn_program *prog,
+			const struct drgn_debug_info_options *options,
+			struct drgn_kmod_walk_state *state)
+{
+	struct string_builder *path = &state->path;
+	for (;;) {
+		if (state->next_debug_dir) {
+			const char *debug_dir = *state->next_debug_dir++;
+			if (debug_dir && debug_dir[0] != '/')
+				continue;
+
+			path->len = 0;
+			if (debug_dir) {
+				if (!string_builder_append(path, debug_dir))
+					return &drgn_enomem;
+			} else {
+				state->next_debug_dir = NULL;
+			}
+			if (!string_builder_append(path, "/lib/modules/")
+			    || !string_builder_append(path,
+						      prog->vmcoreinfo.osrelease))
+				return &drgn_enomem;
+		} else {
+			const char *kernel_dir = *state->next_kernel_dir;
+			if (!kernel_dir)
+				return &drgn_stop;
+			state->next_kernel_dir++;
+			if (kernel_dir[0]) {
+				path->len = 0;
+				if (!string_builder_append(path, kernel_dir))
+					return &drgn_enomem;
+			} else {
+				state->next_debug_dir = options->directories;
+				continue;
+			}
+		}
+
+		if (!string_builder_null_terminate(path))
+			return &drgn_enomem;
+		struct drgn_kmod_walk_stack_entry entry = {
+			.dir = opendir(path->str),
+			.path_len = path->len,
+		};
+		if (!entry.dir) {
+			drgn_log_debug(prog, "opendir: %s: %m", path->str);
+			continue;
+		}
+		if (!drgn_kmod_walk_stack_append(&state->stack, &entry)) {
+			closedir(entry.dir);
+			return &drgn_enomem;
+		}
+		drgn_log_debug(prog, "searching for kernel modules in %s",
+			       path->str);
+		return NULL;
+	}
+}
+
+static struct drgn_error *
+drgn_kmod_walk(struct drgn_program *prog,
+	       const struct drgn_debug_info_options *options,
+	       struct drgn_kmod_walk_state *state,
+	       struct drgn_kmod_walk_module_map_entry *current)
+{
+	struct drgn_error *err;
+	struct string_builder *path = &state->path;
+
+	for (;;) {
+		if (drgn_kmod_walk_stack_empty(&state->stack)) {
+			err = drgn_kmod_walk_next_dir(prog, options, state);
+			if (err)
+				return err;
+		}
+
+		struct drgn_kmod_walk_stack_entry *top =
+			drgn_kmod_walk_stack_last(&state->stack);
+		errno = 0;
+		struct dirent *ent = readdir(top->dir);
+		if (!ent) {
+			if (errno) {
+				path->str[top->path_len] = '\0';
+				drgn_log_debug(prog, "%s: readdir: %m",
+					       path->str);
+			}
+			closedir(top->dir);
+			drgn_kmod_walk_stack_pop(&state->stack);
+			continue;
+		}
+
+		// Skip "." and "..".
+		if (ent->d_name[0] == '.'
+		    && (!ent->d_name[1]
+			|| (ent->d_name[1] == '.' && !ent->d_name[2])))
+			continue;
+
+		bool is_directory = false;
+		if (ent->d_type == DT_LNK || ent->d_type == DT_UNKNOWN) {
+			struct stat st;
+			if (fstatat(dirfd(top->dir), ent->d_name, &st, 0) < 0) {
+				path->str[top->path_len] = '\0';
+				drgn_log_debug(prog, "%s/%s: fstatat: %m",
+					       path->str, ent->d_name);
+				continue;
+			}
+			if (S_ISDIR(st.st_mode))
+				is_directory = true;
+			else if (!S_ISREG(st.st_mode))
+				continue;
+		} else if (ent->d_type == DT_DIR) {
+			is_directory = true;
+		} else if (ent->d_type != DT_REG) {
+			continue;
+		}
+
+		if (is_directory) {
+			path->len = top->path_len;
+			if (!string_builder_appendc(path, '/')
+			    || !string_builder_append(path, ent->d_name)
+			    || !string_builder_null_terminate(path))
+				return &drgn_enomem;
+
+			_cleanup_close_ int fd =
+				openat(dirfd(top->dir), ent->d_name,
+				       O_RDONLY | O_DIRECTORY);
+			if (fd < 0) {
+				drgn_log_debug(prog, "openat: %s: %m",
+					       path->str);
+				continue;
+			}
+
+			struct stat st;
+			if (fstat(fd, &st) < 0) {
+				drgn_log_debug(prog, "fstat: %s: %m",
+					       path->str);
+				continue;
+			}
+			struct drgn_kmod_walk_inode inode = {
+				.dev = st.st_dev,
+				.ino = st.st_ino,
+			};
+			int r = drgn_kmod_walk_inode_set_insert(&state->visited_dirs,
+								&inode, NULL);
+			if (r < 0)
+				return &drgn_enomem;
+			if (r == 0) {
+				drgn_log_debug(prog,
+					       "%s is cycle or duplicate; skipping",
+					       path->str);
+				continue;
+			}
+
+			struct drgn_kmod_walk_stack_entry entry = {
+				.dir = fdopendir(fd),
+				.path_len = path->len,
+			};
+			if (!entry.dir) {
+				drgn_log_debug(prog, "fdopendir: %s: %m",
+					       path->str);
+				continue;
+			}
+			fd = -1; // entry.dir owns fd now.
+			if (!drgn_kmod_walk_stack_append(&state->stack,
+							 &entry)) {
+				closedir(entry.dir);
+				return &drgn_enomem;
+			}
+		} else {
+			// Match anything where the first extension is ".ko".
+			char *dot = strchr(ent->d_name, '.');
+			if (!dot || dot[1] != 'k' || dot[2] != 'o'
+			    || (dot[3] != '\0' && dot[3] != '.'))
+				continue;
+
+			// Borrow the path string builder to build the module
+			// name (removing extensions and replacing '-' with
+			// '_').
+			path->len = top->path_len;
+			if (!string_builder_appendn(path, ent->d_name,
+						    dot - ent->d_name)
+			    || !string_builder_null_terminate(path))
+				return &drgn_enomem;
+			char *dash = &path->str[top->path_len];
+			while ((dash = strchr(dash, '-')))
+				*dash++ = '_';
+
+			// Find the module (if wanted).
+			const char *module_name = &path->str[top->path_len];
+			auto it = drgn_kmod_walk_module_map_search(&state->modules,
+								   &module_name);
+			if (!it.entry)
+				continue;
+
+			size_t name_len = strlen(ent->d_name);
+			size_t path_len;
+			if (__builtin_add_overflow(top->path_len, name_len,
+						   &path_len)
+			    || __builtin_add_overflow(path_len, 2, &path_len))
+				return &drgn_enomem;
+			_cleanup_free_ char *file_path = malloc(path_len);
+			if (!file_path)
+				return &drgn_enomem;
+			memcpy(file_path, path->str, top->path_len);
+			file_path[top->path_len] = '/';
+			memcpy(&file_path[top->path_len + 1], ent->d_name,
+			       name_len + 1);
+			drgn_log_debug(prog, "found kernel module %s", file_path);
+
+			if (!char_p_vector_append(&it.entry->value, &file_path))
+				return &drgn_enomem;
+			file_path = NULL; // it.entry->value owns file_path now.
+
+			// If the file matches the current module, return it.
+			// Otherwise, keep going.
+			if (it.entry == current)
+				return NULL;
+		}
+	}
+}
+
+struct drgn_error *
+drgn_module_try_linux_kmod_files(struct drgn_module *module,
+				 const struct drgn_debug_info_options *options,
+				 struct drgn_standard_debug_info_find_state *state)
+{
+	struct drgn_error *err;
+
+	if (options->try_kmod == DRGN_KMOD_SEARCH_NONE)
+		return NULL;
+
+	if (options->try_kmod != DRGN_KMOD_SEARCH_WALK) {
+		err = drgn_open_modules_dep(module->prog, options,
+					    &state->modules_dep);
+		if (err)
+			return err;
+		if (state->modules_dep.len > 0) {
+			err = drgn_module_try_linux_kmod_depmod(module, options,
+								state);
+			if (err
+			    || options->try_kmod != DRGN_KMOD_SEARCH_DEPMOD_AND_WALK
+			    || !drgn_module_wants_file(module))
+				return err;
+		}
+		if (options->try_kmod == DRGN_KMOD_SEARCH_DEPMOD)
+			return NULL;
+	}
+
+	if (drgn_kmod_walk_module_map_empty(&state->kmod_walk.modules)) {
+		for (size_t i = 0; i < state->num_modules; i++) {
+			if (!drgn_module_wants_file(state->modules[i]))
+				continue;
+			struct drgn_kmod_walk_module_map_entry entry = {
+				.key = state->modules[i]->name,
+				.value = VECTOR_INIT,
+			};
+			if (drgn_kmod_walk_module_map_insert(&state->kmod_walk.modules,
+							     &entry, NULL) < 0)
+				return &drgn_enomem;
+		}
+	}
+
+	const char *module_name = module->name;
+	auto it = drgn_kmod_walk_module_map_search(&state->kmod_walk.modules,
+						   &module_name);
+	size_t i = 0;
+	for (;;) {
+		if (i >= char_p_vector_size(&it.entry->value)) {
+			// No matches remaining for this module. Clear the old
+			// matches and find another one.
+			vector_for_each(char_p_vector, path, &it.entry->value)
+				free(*path);
+			char_p_vector_clear(&it.entry->value);
+			i = 0;
+
+			err = drgn_kmod_walk(module->prog, options,
+					     &state->kmod_walk, it.entry);
+			if (err == &drgn_stop)
+				break;
+			else if (err)
+				return err;
+		}
+		char *path = *char_p_vector_at(&it.entry->value, i++);
+		err = drgn_module_try_standard_file(module, options, path, -1,
+						    true, NULL);
+		if (err)
+			return err;
+		if (!drgn_module_wants_file(module))
+			break;
+	}
+	// We won't need any more matches for this module.
+	drgn_kmod_walk_module_map_entry_deinit(it.entry);
+	drgn_kmod_walk_module_map_delete_iterator(&state->kmod_walk.modules,
+						  it);
 	return NULL;
 }
 

_drgn.pyi
@@ -1415,6 +1415,7 @@ class DebugInfoOptions:
         try_reuse: bool = ...,
         try_supplementary: bool = ...,
         kernel_directories: Iterable[Path] = ...,
+        try_kmod: KmodSearchMethod = ...,
     ) -> None:
         """
         Create a ``DebugInfoOptions``.
@@ -1511,6 +1512,62 @@ class DebugInfoOptions:
     An empty path means to check standard paths (e.g.,
     :file:`/boot/vmlinux-{release}`, :file:`/lib/modules/{release}`) absolutely
     and under each absolute path in :attr:`directories`.
+    """
+    try_kmod: KmodSearchMethod
+    """
+    How to search for loadable kernel modules.
+
+    Defaults to :attr:`KmodSearchMethod.DEPMOD_OR_WALK`.
+    """
+
+class KmodSearchMethod(enum.Enum):
+    """
+    Methods of searching for loadable kernel module debugging information.
+
+    In addition to searching by build ID, there are currently two methods of
+    searching for debugging information specific to loadable kernel modules:
+
+    1. Using :manpage:`depmod(8)` metadata. This looks for :command:`depmod`
+       metadata (specifically, :file:`modules.dep.bin`) at the top level of
+       each directory in :attr:`DebugInfoOptions.kernel_directories` (an empty
+       path means :file:`/lib/modules/{release}`). The metadata is used to
+       quickly find the path of each module, which is then checked relative to
+       each directory specified by :attr:`DebugInfoOptions.kernel_directories`.
+
+       This method is faster but typically only applicable to installed
+       kernels.
+    2. Walking kernel directories. This traverses each directory specified by
+       :attr:`DebugInfoOptions.kernel_directories` looking for ``.ko`` files.
+       Module names are matched to filenames before the ``.ko`` extension and
+       with dashes (``-``) replaced with underscores (``_``).
+
+       This method is slower but not limited to installed kernels.
+
+    Debugging information searches can be configured to use one, both, or
+    neither method.
+    """
+
+    NONE = ...
+    """Don't search using kernel module-specific methods."""
+    DEPMOD = ...
+    """Search using :command:`depmod` metadata."""
+    WALK = ...
+    """Search by walking kernel directories."""
+    DEPMOD_OR_WALK = ...
+    """
+    Search using :command:`depmod` metadata, falling back to walking kernel
+    directories only if no :command:`depmod` metadata is found.
+
+    Since :command:`depmod` metadata is expected to be reliable if present,
+    this is the default.
+    """
+    DEPMOD_AND_WALK = ...
+    """
+    Search using :command:`depmod` metadata and by walking kernel directories.
+
+    Unlike :attr:`DEPMOD_OR_WALK`, if :command:`depmod` metadata is found but
+    doesn't result in the desired debugging information, this will still walk
+    kernel directories.
     """
 
 def get_default_prog() -> Program:

drgn/__init__.py
@@ -54,6 +54,7 @@ from _drgn import (
     FaultError,
     FindObjectFlags,
     IntegerLike,
+    KmodSearchMethod,
     Language,
     MainModule,
     MissingDebugInfoError,
@@ -121,6 +122,7 @@ __all__ = (
     "FaultError",
     "FindObjectFlags",
     "IntegerLike",
+    "KmodSearchMethod",
     "Language",
     "MainModule",
     "MissingDebugInfoError",

drgn/cli.py
@@ -188,11 +188,20 @@ class _DebugInfoOptionAction(argparse.Action):
 
 
 class _TryDebugInfoOptionAction(_DebugInfoOptionAction):
-    _choices = _DebugInfoOptionAction._bool_options(True)
+    _choices = {
+        **_DebugInfoOptionAction._bool_options(True),
+        "kmod=depmod": ("try_kmod", drgn.KmodSearchMethod.DEPMOD),
+        "kmod=walk": ("try_kmod", drgn.KmodSearchMethod.WALK),
+        "kmod=depmod-or-walk": ("try_kmod", drgn.KmodSearchMethod.DEPMOD_OR_WALK),
+        "kmod=depmod-and-walk": ("try_kmod", drgn.KmodSearchMethod.DEPMOD_AND_WALK),
+    }
 
 
 class _NoDebugInfoOptionAction(_DebugInfoOptionAction):
-    _choices = _DebugInfoOptionAction._bool_options(False)
+    _choices = {
+        **_DebugInfoOptionAction._bool_options(False),
+        "kmod": ("try_kmod", drgn.KmodSearchMethod.NONE),
+    }
 
 
 def _main() -> None:

libdrgn/build-aux/gen_constants.py
@@ -16,6 +16,7 @@ class ConstantClass(NamedTuple):
 CONSTANTS = (
     ConstantClass("Architecture", "Enum", r"DRGN_ARCH_([a-zA-Z0-9_]+)"),
     ConstantClass("FindObjectFlags", "Flag", r"DRGN_FIND_OBJECT_([a-zA-Z0-9_]+)"),
+    ConstantClass("KmodSearchMethod", "Enum", r"DRGN_KMOD_SEARCH_([a-zA-Z0-9_]+)"),
     ConstantClass("ModuleFileStatus", "Enum", r"DRGN_MODULE_FILE_([a-zA-Z0-9_]+)"),
     ConstantClass(
         "PlatformFlags",

libdrgn/debug_info.c
@@ -2370,7 +2370,17 @@ drgn_standard_debug_info_find(struct drgn_module * const *modules,
 	}
 
 	_cleanup_(drgn_standard_debug_info_find_state_deinit)
-		struct drgn_standard_debug_info_find_state state = {};
+		struct drgn_standard_debug_info_find_state state = {
+			.modules = modules,
+			.num_modules = num_modules,
+			.kmod_walk = {
+				.modules = HASH_TABLE_INIT,
+				.stack = VECTOR_INIT,
+				.path = STRING_BUILDER_INIT,
+				.visited_dirs = HASH_TABLE_INIT,
+				.next_kernel_dir = options->kernel_directories,
+			},
+		};
 	for (size_t i = 0; i < num_modules; i++) {
 		err = drgn_module_try_standard_files(modules[i], options,
 						     &state);

libdrgn/debug_info_options.c
@@ -248,6 +248,38 @@ static bool drgn_format_debug_info_options_bool(struct string_builder *sb,
 	       && string_builder_append(sb, value ? "True" : "False");
 }
 
+static bool
+drgn_kmod_search_method_format(struct string_builder *sb, const char *name,
+			       bool *first, enum drgn_kmod_search_method value,
+			       enum drgn_kmod_search_method default_value)
+{
+	// Skip options set to the default.
+	if (value == default_value)
+		return true;
+	const char *s;
+	SWITCH_ENUM(value) {
+	case DRGN_KMOD_SEARCH_NONE:
+		s = "NONE";
+		break;
+	case DRGN_KMOD_SEARCH_DEPMOD:
+		s = "DEPMOD";
+		break;
+	case DRGN_KMOD_SEARCH_WALK:
+		s = "WALK";
+		break;
+	case DRGN_KMOD_SEARCH_DEPMOD_OR_WALK:
+		s = "DEPMOD_OR_WALK";
+		break;
+	case DRGN_KMOD_SEARCH_DEPMOD_AND_WALK:
+		s = "DEPMOD_AND_WALK";
+		break;
+	default:
+		UNREACHABLE();
+	}
+	return drgn_format_debug_info_options_common(sb, name, first)
+	       && string_builder_append(sb, s);
+}
+
 char *drgn_format_debug_info_options(struct drgn_debug_info_options *options)
 {
 	STRING_BUILDER(sb);

commit c50df223cdfb65c05c73a0ad95ca558c01c2f1df

libdrgn: linux_kernel: handle duplicate module names in kmod walk
drgn_module_try_linux_kmod_files() assumes that a module will always be
found in the kmod walk modules map, then it deletes the entry after
processing the module to save memory. A corrupted or crafted Linux
kernel target could have kernel modules with duplicate names.
drgn_module_try_linux_kmod_files() would then not find the entry for the
duplicate and crash with a null pointer dereference.

Fix it by not doing this optimization if there are duplicate names.

Fixes: 7dc474d75790 ("Support searching for kernel modules without depmod metadata")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.h
@@ -343,6 +343,7 @@ struct drgn_kmod_walk_state {
 	struct drgn_kmod_walk_inode_set visited_dirs;
 	const char * const *next_kernel_dir;
 	const char * const *next_debug_dir;
+	bool duplicate_names;
 };
 
 // State kept by standard debug info finder for all modules it's working on.

libdrgn/linux_kernel.c
@@ -1532,9 +1532,12 @@ drgn_module_try_linux_kmod_files(struct drgn_module *module,
 				.key = state->modules[i]->name,
 				.value = VECTOR_INIT,
 			};
-			if (drgn_kmod_walk_module_map_insert(&state->kmod_walk.modules,
-							     &entry, NULL) < 0)
+			int r = drgn_kmod_walk_module_map_insert(&state->kmod_walk.modules,
+								 &entry, NULL);
+			if (r < 0)
 				return &drgn_enomem;
+			if (r == 0)
+				state->kmod_walk.duplicate_names = true;
 		}
 	}
 
@@ -1545,11 +1548,14 @@ drgn_module_try_linux_kmod_files(struct drgn_module *module,
 	for (;;) {
 		if (i >= char_p_vector_size(&it.entry->value)) {
 			// No matches remaining for this module. Clear the old
-			// matches and find another one.
-			vector_for_each(char_p_vector, path, &it.entry->value)
-				free(*path);
-			char_p_vector_clear(&it.entry->value);
-			i = 0;
+			// matches if we can, then find another one.
+			if (!state->kmod_walk.duplicate_names) {
+				vector_for_each(char_p_vector, path,
+						&it.entry->value)
+					free(*path);
+				char_p_vector_clear(&it.entry->value);
+				i = 0;
+			}
 
 			err = drgn_kmod_walk(module->prog, options,
 					     &state->kmod_walk, it.entry);
@@ -1566,10 +1572,12 @@ drgn_module_try_linux_kmod_files(struct drgn_module *module,
 		if (!drgn_module_wants_file(module))
 			break;
 	}
-	// We won't need any more matches for this module.
-	drgn_kmod_walk_module_map_entry_deinit(it.entry);
-	drgn_kmod_walk_module_map_delete_iterator(&state->kmod_walk.modules,
-						  it);
+	if (!state->kmod_walk.duplicate_names) {
+		// We won't need any more matches for this module.
+		drgn_kmod_walk_module_map_entry_deinit(it.entry);
+		drgn_kmod_walk_module_map_delete_iterator(&state->kmod_walk.modules,
+							  it);
+	}
 	return NULL;
 }
 