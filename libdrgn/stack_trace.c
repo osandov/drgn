osandov/drgn

commit 37024146eb75e313a90f1aff4a7e8d4ea809ec8a

libdrgn: Use Symbol Finder API in find_symbol_by_address_internal()
The drgn_program_find_symbol_by_address_internal() function is used when
libdrgn itself may want to lookup a symbol: in particular, when
formatting stack traces or objects. It does less work by possibly
already having a Dwfl_Module looked up, and by avoiding memory
allocation of a symbol, and it's more convenient because it doesn't
return any errors, including on lookup failure.

Unfortunately, the new symbol finder API breaks all of these properties:
the returned symbol is now allocated via malloc() which needs cleanup on
error, and errors can be returned by any finder via the lookup API.
What's more, the finder API doesn't allow specifying an already-known
module. Thankfully, error handling can be improved using the cleanup
API, and looking up a module for an address is usually a reasonably
cheap binary tree operation.

Switch the internal method over to the new finder API. The major
difference now is simply that lookup failures don't result in an error:
they simply result in a NULL symbol.

Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

libdrgn/stack_trace.c
@@ -109,6 +109,7 @@ drgn_stack_trace_num_frames(struct drgn_stack_trace *trace)
 LIBDRGN_PUBLIC struct drgn_error *
 drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 {
+	struct drgn_error *err;
 	STRING_BUILDER(str);
 	for (size_t frame = 0; frame < trace->num_frames; frame++) {
 		if (!string_builder_appendf(&str, "#%-2zu ", frame))
@@ -121,19 +122,19 @@ drgn_format_stack_trace(struct drgn_stack_trace *trace, char **ret)
 			if (!string_builder_append(&str, name))
 				return &drgn_enomem;
 		} else if ((pc = drgn_register_state_get_pc(regs)).has_value) {
-			Dwfl_Module *dwfl_module =
-				regs->module ? regs->module->dwfl_module : NULL;
-			struct drgn_symbol sym;
-			if (dwfl_module &&
-			    drgn_program_find_symbol_by_address_internal(trace->prog,
-									 pc.value - !regs->interrupted,
-									 dwfl_module,
-									 &sym)) {
+			_cleanup_symbol_ struct drgn_symbol *sym = NULL;
+			err = drgn_program_find_symbol_by_address_internal(trace->prog,
+									   pc.value - !regs->interrupted,
+									   &sym);
+			if (err)
+				return err;
+
+			if (sym) {
 				if (!string_builder_appendf(&str,
 							    "%s+0x%" PRIx64 "/0x%" PRIx64,
-							    sym.name,
-							    pc.value - sym.address,
-							    sym.size))
+							    sym->name,
+							    pc.value - sym->address,
+							    sym->size))
 					return &drgn_enomem;
 			} else {
 				if (!string_builder_appendf(&str, "0x%" PRIx64,
@@ -173,6 +174,7 @@ drgn_format_stack_frame(struct drgn_stack_trace *trace, size_t frame, char **ret
 {
 	STRING_BUILDER(str);
 	struct drgn_register_state *regs = trace->frames[frame].regs;
+	struct drgn_error *err;
 	if (!string_builder_appendf(&str, "#%zu at ", frame))
 		return &drgn_enomem;
 
@@ -181,17 +183,15 @@ drgn_format_stack_frame(struct drgn_stack_trace *trace, size_t frame, char **ret
 		if (!string_builder_appendf(&str, "%#" PRIx64, pc.value))
 			return &drgn_enomem;
 
-		Dwfl_Module *dwfl_module =
-			regs->module ? regs->module->dwfl_module : NULL;
-		struct drgn_symbol sym;
-		if (dwfl_module &&
-		    drgn_program_find_symbol_by_address_internal(trace->prog,
-								 pc.value - !regs->interrupted,
-								 dwfl_module,
-								 &sym) &&
-		    !string_builder_appendf(&str, " (%s+0x%" PRIx64 "/0x%" PRIx64 ")",
-					    sym.name, pc.value - sym.address,
-					    sym.size))
+		_cleanup_symbol_ struct drgn_symbol *sym;
+		err = drgn_program_find_symbol_by_address_internal(trace->prog,
+								   pc.value - !regs->interrupted,
+								   &sym);
+		if (err)
+			return err;
+		if (sym && !string_builder_appendf(&str, " (%s+0x%" PRIx64 "/0x%" PRIx64 ")",
+						   sym->name, pc.value - sym->address,
+						   sym->size))
 			return &drgn_enomem;
 	} else {
 		if (!string_builder_append(&str, "???"))
@@ -368,17 +368,15 @@ drgn_stack_frame_symbol(struct drgn_stack_trace *trace, size_t frame,
 					 "program counter is not known at stack frame");
 	}
 	pc.value -= !regs->interrupted;
-	Dwfl_Module *dwfl_module =
-		regs->module ? regs->module->dwfl_module : NULL;
-	if (!dwfl_module)
-		return drgn_error_symbol_not_found(pc.value);
-	_cleanup_free_ struct drgn_symbol *sym = malloc(sizeof(*sym));
+	struct drgn_symbol *sym = NULL;
+	struct drgn_error *err;
+	err = drgn_program_find_symbol_by_address_internal(trace->prog, pc.value,
+							   &sym);
+	if (err)
+		return err;
 	if (!sym)
-		return &drgn_enomem;
-	if (!drgn_program_find_symbol_by_address_internal(trace->prog, pc.value,
-							  dwfl_module, sym))
 		return drgn_error_symbol_not_found(pc.value);
-	*ret = no_cleanup_ptr(sym);
+	*ret = sym;
 	return NULL;
 }
 

libdrgn/language_c.c
@@ -1261,9 +1261,8 @@ c_format_pointer_object(const struct drgn_object *obj,
 	bool c_string =
 		((flags & DRGN_FORMAT_OBJECT_STRING) &&
 		 is_character_type(drgn_type_type(underlying_type).type));
-	bool have_symbol;
 	uint64_t uvalue;
-	struct drgn_symbol sym;
+	_cleanup_symbol_ struct drgn_symbol *sym = NULL;
 	size_t start, type_start, type_end, value_start, value_end;
 
 	start = sb->len;
@@ -1287,18 +1286,17 @@ c_format_pointer_object(const struct drgn_object *obj,
 	if (err)
 		return err;
 
-	have_symbol = ((flags & DRGN_FORMAT_OBJECT_SYMBOLIZE) &&
-		       drgn_program_find_symbol_by_address_internal(drgn_object_program(obj),
-								    uvalue,
-								    NULL,
-								    &sym));
-	if (have_symbol && dereference && !c_string &&
+	if ((flags & DRGN_FORMAT_OBJECT_SYMBOLIZE) &&
+	    (err = drgn_program_find_symbol_by_address_internal(drgn_object_program(obj),
+								uvalue, &sym)))
+		return err;
+	if (sym && dereference && !c_string &&
 	    !string_builder_appendc(sb, '('))
 		return &drgn_enomem;
 	value_start = sb->len;
-	if (have_symbol &&
-	     !string_builder_appendf(sb, "%s+0x%" PRIx64 " = ", sym.name,
-				     uvalue - sym.address))
+	if (sym &&
+	     !string_builder_appendf(sb, "%s+0x%" PRIx64 " = ", sym->name,
+				     uvalue - sym->address))
 		return &drgn_enomem;
 
 	if (!string_builder_appendf(sb, "0x%" PRIx64, uvalue))
@@ -1307,7 +1305,7 @@ c_format_pointer_object(const struct drgn_object *obj,
 		return NULL;
 	value_end = sb->len;
 
-	if ((have_symbol && dereference && !c_string &&
+	if ((sym && dereference && !c_string &&
 	     !string_builder_appendc(sb, ')')) ||
 	    !string_builder_append(sb, " = "))
 		return &drgn_enomem;

libdrgn/program.c
@@ -2060,25 +2060,22 @@ drgn_program_find_symbol_by_address(struct drgn_program *prog, uint64_t address,
 	return err;
 }
 
-bool drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
-						  uint64_t address,
-						  Dwfl_Module *module,
-						  struct drgn_symbol *ret)
+struct drgn_error *
+drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
+					     uint64_t address,
+					     struct drgn_symbol **ret)
 {
-	if (!module) {
-		module = dwfl_addrmodule(prog->dbinfo.dwfl, address);
-		if (!module)
-			return false;
-	}
+	struct drgn_symbol_result_builder builder;
+	enum drgn_find_symbol_flags flags = DRGN_FIND_SYMBOL_ADDR | DRGN_FIND_SYMBOL_ONE;
 
-	GElf_Off offset;
-	GElf_Sym elf_sym;
-	const char *name = dwfl_module_addrinfo(module, address, &offset,
-						&elf_sym, NULL, NULL, NULL);
-	if (!name)
-		return false;
-	drgn_symbol_from_elf(name, address - offset, &elf_sym, ret);
-	return true;
+	drgn_symbol_result_builder_init(&builder, true);
+	struct drgn_error *err = drgn_program_symbols_search(prog, NULL, address,
+							     flags, &builder);
+	if (err)
+		drgn_symbol_result_builder_abort(&builder);
+	else
+		*ret = drgn_symbol_result_builder_single(&builder);
+	return err;
 }
 
 LIBDRGN_PUBLIC struct drgn_error *

libdrgn/program.h
@@ -366,18 +366,17 @@ struct drgn_error *drgn_program_cache_prstatus_entry(struct drgn_program *prog,
 						     uint32_t *ret);
 
 /*
- * Like @ref drgn_program_find_symbol_by_address(), but @p ret is already
- * allocated, we may already know the module, and doesn't return a @ref
- * drgn_error.
+ * Like @ref drgn_program_find_symbol_by_address(), but returns @c NULL rather
+ * than a lookup error if the symbol was not found.
  *
- * @param[in] module Module containing the address. May be @c NULL, in which
- * case this will look it up.
- * @return Whether the symbol was found.
+ * @param[in] address Address to search for.
+ * @param [out] ret The symbol found by the lookup (if found)
+ * @return @c NULL unless an error (unrelated to a lookup error) was encountered
  */
-bool drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
-						  uint64_t address,
-						  Dwfl_Module *module,
-						  struct drgn_symbol *ret);
+struct drgn_error *
+drgn_program_find_symbol_by_address_internal(struct drgn_program *prog,
+					     uint64_t address,
+					     struct drgn_symbol **ret);
 
 /*
  * Implementation of the Symbol finder API, based on ELF symbols

libdrgn/symbol.h
@@ -6,6 +6,7 @@
 
 #include <gelf.h>
 
+#include "cleanup.h"
 #include "drgn.h"
 #include "vector.h"
 
@@ -33,6 +34,12 @@ struct drgn_symbol_result_builder {
 		struct drgn_symbol *single;
 	};
 };
+
+#define _cleanup_symbol_ _cleanup_(drgn_symbol_cleanup)
+static inline void drgn_symbol_cleanup(struct drgn_symbol **p)
+{
+	drgn_symbol_destroy(*p);
+}
 
 /** Initialize a @ref drgn_symbol from an ELF symbol. */
 void drgn_symbol_from_elf(const char *name, uint64_t address,

commit 332e57c537ac7e84ba0a3d6ff1b176ace3be4e88

libdrgn: stack_trace: fix free of uninitialized pointer
We're not initializing sym in drgn_format_stack_trace(), so if
drgn_program_find_symbol_by_address_internal() fails, we pass an
uninitialized pointer to free().

Fixes: 37024146eb75 ("libdrgn: Use Symbol Finder API in find_symbol_by_address_internal()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/stack_trace.c
@@ -194,7 +194,7 @@ drgn_format_stack_frame(struct drgn_stack_trace *trace, size_t frame, char **ret
 		if (!string_builder_appendf(&str, "%#" PRIx64, pc.value))
 			return &drgn_enomem;
 
-		_cleanup_symbol_ struct drgn_symbol *sym;
+		_cleanup_symbol_ struct drgn_symbol *sym = NULL;
 		err = drgn_program_find_symbol_by_address_internal(trace->prog,
 								   pc.value - !regs->interrupted,
 								   &sym);