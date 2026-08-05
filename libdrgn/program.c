osandov/drgn

commit 0f7ad0ed26d4b939c5260420b83b0f1cb4901846

libdrgn: stack_trace: support unwinding stack from core dump
vmcores include a NT_PRSTATUS note for each CPU containing the PID of
the task running on that CPU at the time of the crash and its registers.
We can use that to unwind the stack of the crashed tasks.

libdrgn/program.c
@@ -28,6 +28,9 @@
 #include "type_index.h"
 #include "vector.h"
 
+DEFINE_HASH_TABLE_FUNCTIONS(drgn_prstatus_map, hash_pair_int_type,
+			    hash_table_scalar_eq)
+
 static Elf_Type note_header_type(GElf_Phdr *phdr)
 {
 	if (phdr->p_align == 8)
@@ -66,12 +69,15 @@ void drgn_program_init(struct drgn_program *prog,
 	drgn_type_index_init(&prog->tindex);
 	drgn_object_index_init(&prog->oindex);
 	prog->core_fd = -1;
+	drgn_prstatus_map_init(&prog->prstatus_cache);
 	if (platform)
 		drgn_program_set_platform(prog, platform);
 }
 
 void drgn_program_deinit(struct drgn_program *prog)
 {
+	drgn_prstatus_map_deinit(&prog->prstatus_cache);
+
 	drgn_object_index_deinit(&prog->oindex);
 	drgn_type_index_deinit(&prog->tindex);
 	drgn_memory_reader_deinit(&prog->reader);
@@ -626,6 +632,92 @@ drgn_program_load_debug_info(struct drgn_program *prog, const char **paths,
 			      drgn_set_platform_from_dwarf, prog, 0);
 	}
 	return err;
+}
+
+static struct drgn_error *drgn_program_cache_prstatus(struct drgn_program *prog)
+{
+	size_t phnum, i;
+	size_t pr_pid_offset;
+	bool bswap;
+
+	pr_pid_offset = drgn_program_is_64_bit(prog) ? 32 : 24;
+	bswap = (drgn_program_is_little_endian(prog) !=
+		 (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__));
+
+	if (elf_getphdrnum(prog->core, &phnum) != 0)
+		return drgn_error_libelf();
+	for (i = 0; i < phnum; i++) {
+		GElf_Phdr phdr_mem, *phdr;
+		Elf_Data *data;
+		size_t offset;
+		GElf_Nhdr nhdr;
+		size_t name_offset, desc_offset;
+
+		phdr = gelf_getphdr(prog->core, i, &phdr_mem);
+		if (!phdr)
+			return drgn_error_libelf();
+		if (phdr->p_type != PT_NOTE)
+			continue;
+
+		data = elf_getdata_rawchunk(prog->core, phdr->p_offset,
+					    phdr->p_filesz,
+					    note_header_type(phdr));
+		if (!data)
+			return drgn_error_libelf();
+
+		offset = 0;
+		while (offset < data->d_size &&
+		       (offset = gelf_getnote(data, offset, &nhdr, &name_offset,
+					      &desc_offset))) {
+			const char *name;
+			uint32_t pr_pid;
+			struct drgn_prstatus_map_entry entry;
+
+			name = (char *)data->d_buf + name_offset;
+			if (strncmp(name, "CORE", nhdr.n_namesz) != 0 ||
+			    nhdr.n_type != NT_PRSTATUS ||
+			    nhdr.n_descsz < pr_pid_offset + sizeof(pr_pid))
+				continue;
+			memcpy(&pr_pid,
+			       (char *)data->d_buf + desc_offset + pr_pid_offset,
+			       sizeof(pr_pid));
+			if (bswap)
+				pr_pid = bswap_32(pr_pid);
+			if (!pr_pid)
+				continue;
+
+			entry.key = pr_pid;
+			entry.value.str = (char *)data->d_buf + desc_offset;
+			entry.value.len = nhdr.n_descsz;
+			if (drgn_prstatus_map_insert(&prog->prstatus_cache,
+						     &entry, NULL) == -1)
+				return &drgn_enomem;
+		}
+	}
+	return NULL;
+}
+
+struct drgn_error *drgn_program_find_prstatus(struct drgn_program *prog,
+					      uint32_t tid, struct string *ret)
+{
+	struct drgn_error *err;
+	struct drgn_prstatus_map_iterator it;
+
+	if (!prog->prstatus_cached) {
+		err = drgn_program_cache_prstatus(prog);
+		if (err)
+			return err;
+		prog->prstatus_cached = true;
+	}
+
+	it = drgn_prstatus_map_search(&prog->prstatus_cache, &tid);
+	if (!it.entry) {
+		ret->str = NULL;
+		ret->len = 0;
+		return NULL;
+	}
+	*ret = it.entry->value;
+	return NULL;
 }
 
 struct drgn_error *drgn_program_init_core_dump(struct drgn_program *prog,

libdrgn/arch_x86_64.c.in
@@ -103,6 +103,26 @@ bnd2
 bnd3
 %%
 
+static const struct drgn_frame_register frame_registers_x86_64[] = {
+	{ DRGN_REGISTER_X86_64_rax, 8, 192, },
+	{ DRGN_REGISTER_X86_64_rdx, 8, 208, },
+	{ DRGN_REGISTER_X86_64_rcx, 8, 200, },
+	{ DRGN_REGISTER_X86_64_rbx, 8, 152, },
+	{ DRGN_REGISTER_X86_64_rsi, 8, 216, },
+	{ DRGN_REGISTER_X86_64_rdi, 8, 224, },
+	{ DRGN_REGISTER_X86_64_rbp, 8, 144, },
+	{ DRGN_REGISTER_X86_64_rsp, 8, 264, },
+	{ DRGN_REGISTER_X86_64_r8, 8, 184, },
+	{ DRGN_REGISTER_X86_64_r9, 8, 176, },
+	{ DRGN_REGISTER_X86_64_r10, 8, 168, },
+	{ DRGN_REGISTER_X86_64_r11, 8, 160, },
+	{ DRGN_REGISTER_X86_64_r12, 8, 136, },
+	{ DRGN_REGISTER_X86_64_r13, 8, 128, },
+	{ DRGN_REGISTER_X86_64_r14, 8, 120, },
+	{ DRGN_REGISTER_X86_64_r15, 8, 112, },
+	{ DRGN_REGISTER_X86_64_rip, 8, 240, },
+};
+
 static inline struct drgn_error *read_register(struct drgn_object *reg_obj,
 					       struct drgn_object *frame_obj,
 					       const char *name,
@@ -123,7 +143,7 @@ static inline struct drgn_error *read_register(struct drgn_object *reg_obj,
 
 static struct drgn_error *
 linux_kernel_set_initial_registers_x86_64(Dwfl_Thread *thread,
-					  struct drgn_object *task_obj)
+					  const struct drgn_object *task_obj)
 {
 	struct drgn_error *err;
 	struct drgn_program *prog = task_obj->prog;
@@ -204,5 +224,7 @@ const struct drgn_architecture_info arch_info_x86_64 = {
 	ARCHITECTURE_INFO,
 	.default_flags = (DRGN_PLATFORM_IS_64_BIT |
 			  DRGN_PLATFORM_IS_LITTLE_ENDIAN),
+	.frame_registers = frame_registers_x86_64,
+	.num_frame_registers = ARRAY_SIZE(frame_registers_x86_64),
 	.linux_kernel_set_initial_registers = linux_kernel_set_initial_registers_x86_64,
 };

libdrgn/platform.h
@@ -13,6 +13,13 @@ struct drgn_register {
 	enum drgn_register_number number;
 };
 
+/* Register in NT_PRSTATUS note used for stack unwinding. */
+struct drgn_frame_register {
+	enum drgn_register_number number;
+	size_t size;
+	size_t prstatus_offset;
+};
+
 struct drgn_architecture_info {
 	const char *name;
 	enum drgn_architecture arch;
@@ -20,8 +27,10 @@ struct drgn_architecture_info {
 	const struct drgn_register *registers;
 	size_t num_registers;
 	const struct drgn_register *(*register_by_name)(const char *name);
+	const struct drgn_frame_register *frame_registers;
+	size_t num_frame_registers;
 	struct drgn_error *(*linux_kernel_set_initial_registers)(Dwfl_Thread *,
-								 struct drgn_object *);
+								 const struct drgn_object *);
 };
 
 static inline const struct drgn_register *

libdrgn/program.h
@@ -17,6 +17,7 @@
 #include <libkdumpfile/kdumpfile.h>
 #endif
 
+#include "hash_table.h"
 #include "memory_reader.h"
 #include "object_index.h"
 #include "platform.h"
@@ -48,6 +49,8 @@ struct vmcoreinfo {
 	uint64_t kaslr_offset;
 };
 
+DEFINE_HASH_MAP_TYPE(drgn_prstatus_map, uint32_t, struct string)
+
 struct drgn_dwarf_info_cache;
 struct drgn_dwarf_index;
 
@@ -78,6 +81,7 @@ struct drgn_program {
 	  */
 	pid_t pid;
 	struct drgn_dwarf_info_cache *_dicache;
+	struct drgn_prstatus_map prstatus_cache;
 	/* See @ref drgn_object_stack_trace_next_thread(). */
 	const struct drgn_object *stack_trace_obj;
 	/* See @ref drgn_object_stack_trace(). */
@@ -87,6 +91,7 @@ struct drgn_program {
 	bool has_platform;
 	bool added_vmcoreinfo_object_finder;
 	bool attached_dwfl_state;
+	bool prstatus_cached;
 };
 
 /** Initialize a @ref drgn_program. */
@@ -135,17 +140,27 @@ static inline bool drgn_program_is_64_bit(struct drgn_program *prog)
 
 struct drgn_error *drgn_program_get_dwfl(struct drgn_program *prog, Dwfl **ret);
 
-/* TODO: comment this. */
+/**
+ * Find the @c NT_PRSTATUS note for the given thread ID.
+ *
+ * This assumes that <tt>prog->core</tt> is not @c NULL.
+ *
+ * @param[out] ret Returned note data. If not found, <tt>ret->str</tt> is set to
+ * @c NULL and <tt>ret->len</tt> is set to zero.
+ */
+struct drgn_error *drgn_program_find_prstatus(struct drgn_program *prog,
+					      uint32_t tid, struct string *ret);
+
+/* Like @ref drgn_program_find_symbol(), but the module is known. */
 struct drgn_error *drgn_program_find_symbol_in_module(struct drgn_program *prog,
 						      Dwfl_Module *module,
 						      uint64_t address,
 						      struct drgn_symbol **ret);
 
 /*
- * TODO: update this.
- * Like @ref drgn_program_find_symbol(), but the module is known, @p ret is
- * already allocated, and returns @ref drgn_not_found instead of a more
- * informative message.
+ * Like @ref drgn_program_find_symbol_in_module(), but @p ret is already
+ * allocated, and returns @ref drgn_not_found instead of a more informative
+ * message.
  */
 struct drgn_error *drgn_program_find_symbol_internal(struct drgn_program *prog,
 						     Dwfl_Module *module,

libdrgn/stack_trace.c
@@ -1,6 +1,7 @@
 // Copyright 2019 - Omar Sandoval
 // SPDX-License-Identifier: GPL-3.0+
 
+#include <byteswap.h>
 #include <dwarf.h>
 #include <elfutils/libdwfl.h>
 #include <endian.h>
@@ -9,6 +10,7 @@
 
 #include "internal.h"
 #include "program.h"
+#include "read.h"
 #include "string_builder.h"
 #include "symbol.h"
 
@@ -180,8 +182,8 @@ err:
 }
 
 /*
- * For drgn_object_stack_trace(), we only care about the thread
- * prog->stack_trace_obj. We return it with an arbitrary TID.
+ * We only care about the specific thread that we're unwinding, so we return it
+ * with an arbitrary TID.
  */
 #define STACK_TRACE_OBJ_TID 1
 static pid_t drgn_object_stack_trace_next_thread(Dwfl *dwfl, void *dwfl_arg,
@@ -191,19 +193,111 @@ static pid_t drgn_object_stack_trace_next_thread(Dwfl *dwfl, void *dwfl_arg,
 
 	if (*thread_argp || !prog->stack_trace_obj)
 		return 0;
-	*thread_argp = (void *)prog->stack_trace_obj;
+	*thread_argp = prog;
 	return STACK_TRACE_OBJ_TID;
+}
+
+static struct drgn_error *
+drgn_get_task_pid(const struct drgn_object *task, uint32_t *ret)
+{
+	struct drgn_error *err;
+	struct drgn_object pid;
+	int64_t value;
+
+	drgn_object_init(&pid, task->prog);
+	err = drgn_object_member_dereference(&pid, task, "pid");
+	if (err)
+		goto out;
+	err = drgn_object_read_signed(&pid, &value);
+	if (err)
+		goto out;
+	*ret = value;
+out:
+	drgn_object_deinit(&pid);
+	return err;
+}
+
+static struct drgn_error *
+drgn_prstatus_set_initial_registers(Dwfl_Thread *thread,
+				    struct drgn_platform *platform,
+				    struct string *prstatus)
+{
+	bool bswap = (!!(platform->flags & DRGN_PLATFORM_IS_LITTLE_ENDIAN) !=
+		      (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__));
+	size_t i;
+
+	if (!platform->arch->num_frame_registers) {
+		return drgn_error_format(DRGN_ERROR_INVALID_ARGUMENT,
+					 "core dump stack unwinding is not supported for %s architecture",
+					 platform->arch->name);
+	}
+
+	for (i = 0; i < platform->arch->num_frame_registers; i++) {
+		const struct drgn_frame_register *reg;
+		const char *p;
+		Dwarf_Word word;
+
+		reg = &platform->arch->frame_registers[i];
+		if (prstatus->len < reg->prstatus_offset + reg->size) {
+			return drgn_error_create(DRGN_ERROR_OTHER,
+						 "NT_PRSTATUS is truncated");
+		}
+		p = prstatus->str + reg->prstatus_offset;
+		switch (reg->size) {
+		case 4: {
+			uint32_t tmp;
+
+			memcpy(&tmp, p, sizeof(tmp));
+			if (bswap)
+				tmp = bswap_32(tmp);
+			word = tmp;
+			break;
+		}
+		case 8: {
+			uint64_t tmp;
+
+			memcpy(&tmp, p, sizeof(tmp));
+			if (bswap)
+				tmp = bswap_64(tmp);
+			word = tmp;
+			break;
+		}
+		default:
+			DRGN_UNREACHABLE();
+		}
+		if (!dwfl_thread_state_registers(thread, reg->number, 1, &word))
+			return drgn_error_libdwfl();
+	}
+	return NULL;
 }
 
 static bool drgn_linux_kernel_set_initial_registers(Dwfl_Thread *thread,
 						    void *thread_arg)
 {
 	struct drgn_error *err;
-	struct drgn_object *task_obj = thread_arg;
-	struct drgn_program *prog = task_obj->prog;
+	struct drgn_program *prog = thread_arg;
+	const struct drgn_object *task_obj = prog->stack_trace_obj;
 
+	if (prog->core) {
+		uint32_t tid;
+		struct string prstatus;
+
+		err = drgn_get_task_pid(task_obj, &tid);
+		if (err)
+			goto out;
+		err = drgn_program_find_prstatus(prog, tid, &prstatus);
+		if (err)
+			goto out;
+		if (prstatus.str) {
+			err = drgn_prstatus_set_initial_registers(thread,
+								  &prog->platform,
+								  &prstatus);
+			goto out;
+		}
+	}
 	err = prog->platform.arch->linux_kernel_set_initial_registers(thread,
 								      task_obj);
+out:
 	if (err) {
 		drgn_error_destroy(prog->stack_trace_err);
 		prog->stack_trace_err = err;

commit d530e0c2af61b82f7bf07041319c9b79c0028373

libdrgn: program: match CORE note name exactly
We're unintentionally matching any note whose name is a prefix of "CORE"
("C", "CO", "COR") in drgn_program_cache_core_dump_threads(). Use the
same exact name comparison that we do everywhere else.

Fixes: 0f7ad0ed26d4 ("libdrgn: stack_trace: support unwinding stack from core dump")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/program.c
@@ -1174,7 +1174,8 @@ drgn_program_cache_core_dump_threads(struct drgn_program *prog)
 			const char *name;
 
 			name = (char *)data->d_buf + name_offset;
-			if (strncmp(name, "CORE", nhdr.n_namesz) != 0)
+			if (nhdr.n_namesz != sizeof("CORE")
+			    || memcmp(name, "CORE", sizeof("CORE")) != 0)
 				continue;
 
 			if (nhdr.n_type == NT_PRPSINFO && !prpsinfo_fname) {