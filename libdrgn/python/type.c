0a49453d36d3e550e466ce5a757ab383565f0ea0
Omar Sandoval <osandov@osandov.com>
2026-08-05 23:10:41 -0700

    libdrgn: python: add missing error check in byteorder_converter()
    Fixes: 75c3679147a5 ("Rewrite drgn core in C")
    Signed-off-by: Omar Sandoval <osandov@osandov.com>

--- libdrgn/python/type.c
+++ libdrgn/python/type.c
@@ -1374,6 +1374,8 @@ static int byteorder_converter(PyObject *o, void *p)
 
 	if (PyUnicode_Check(o)) {
 		const char *s = PyUnicode_AsUTF8(o);
+		if (!s)
+			return 0;
 		if (strcmp(s, "little") == 0) {
 			arg->value = DRGN_LITTLE_ENDIAN;
 			return 1;
