osandov/drgn

commit 4e83130008e9c28c3d775a0ab40075f2f9123720

Introduce module and debug info finder APIs
drgn currently provides limited control over how debugging information
is found. drgn has hardcoded logic for where to search for debugging
information. The most the user can do is provide a list of files for
drgn to try in addition to the default locations (with the -s CLI option
or the drgn.Program.load_debug_info() method).

The implementation is also a mess. We use libdwfl, but its data model is
slightly different from what we want, so we have to work around it or
reimplement its functionality in several places: see commits
e5874ad18a53 ("libdrgn: use libdwfl"), e6abfeac0329 ("libdrgn:
debug_info: report userspace core dump debug info ourselves"), and
1d4854a5bce7 ("libdrgn: implement optimized x86-64 ELF relocations") for
some examples. The mismatched combination of libdwfl and our own code is
difficult to maintain, and the lack of control over the whole debug info
pipeline has made it difficult to fix several longstanding issues.

The solution is a major rework removing our libdwfl dependency and
replacing it with our own model. This (huge) commit is that rework
comprising the following components:

- drgn.Module/struct drgn_module, a representation of a binary used by a
  program.
- Automatic discovery of the modules loaded in a program.
- Interfaces for manually creating and overriding modules.
- Automatic discovery of debugging information from the standard
  locations and debuginfod.
- Interfaces for custom debug info finders and for manually overriding
  debugging information.
- Tons of test cases.

A lot of care was taken to make these interfaces extremely flexible yet
cohesive. The existing interfaces are also reimplemented on top of the
new functionality to maintain backwards compatibility, with one
exception: drgn.Program.load_debug_info()/-s would previously accept
files that it didn't find loaded in the program. This turned out to be a
big footgun for users, so now this must be done explicitly (with

tests/linux_kernel/test_debug_info.py
@@ -2,49 +2,89 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import os
-from pathlib import Path
-import unittest
 
-from drgn import Program
+from drgn import Program, RelocatableModule
+from drgn.helpers.linux.module import find_module
 from tests import modifyenv
 from tests.linux_kernel import LinuxKernelTestCase, skip_unless_have_test_kmod
 
-KALLSYMS_PATH = Path("/proc/kallsyms")
+
+def iter_proc_modules():
+    try:
+        f = open("/proc/modules", "r")
+    except FileNotFoundError:
+        return
+    with f:
+        for line in f:
+            tokens = line.split()
+            yield tokens[0], int(tokens[5], 16)
 
 
-@unittest.skipUnless(
-    KALLSYMS_PATH.exists(), "kernel does not have kallsyms (CONFIG_KALLSYMS)"
-)
-@skip_unless_have_test_kmod
-class TestModuleDebugInfo(LinuxKernelTestCase):
-    # Arbitrary symbol that we can use to check that the module debug info was
-    # loaded.
-    SYMBOL = "drgn_test_function"
+class TestModule(LinuxKernelTestCase):
+    def test_loaded_modules(self):
+        expected = [("kernel", None), *iter_proc_modules()]
 
-    def setUp(self):
-        super().setUp()
-        with KALLSYMS_PATH.open() as f:
-            for line in f:
-                tokens = line.split()
-                if tokens[2] == self.SYMBOL:
-                    self.symbol_address = int(tokens[0], 16)
-                    break
+        loaded_modules = []
+        for module, _ in self.prog.loaded_modules():
+            if isinstance(module, RelocatableModule):
+                loaded_modules.append((module.name, module.address))
             else:
-                self.fail(f"{self.SYMBOL!r} symbol not found")
+                loaded_modules.append((module.name, None))
 
-    def _test_module_debug_info(self, use_sys_module):
-        old_use_sys_module = int(os.environ.get("DRGN_USE_SYS_MODULE", "1")) != 0
-        with modifyenv({"DRGN_USE_SYS_MODULE": "1" if use_sys_module else "0"}):
-            if old_use_sys_module == use_sys_module:
-                prog = self.prog
+        self.assertCountEqual(loaded_modules, expected)
+
+    @skip_unless_have_test_kmod
+    def test_find(self):
+        self.assertEqual(self.prog.main_module().name, "kernel")
+        for name, address in iter_proc_modules():
+            if name == "drgn_test":
+                self.assertEqual(
+                    self.prog.relocatable_module(name, address).name, "drgn_test"
+                )
+                break
+        else:
+            self.fail("test module not found")
+
+    @skip_unless_have_test_kmod
+    def test_find_by_obj(self):
+        for module in self.prog.modules():
+            if module.name == "drgn_test":
+                break
+        else:
+            self.fail("test module not found")
+
+        module_obj = find_module(self.prog, "drgn_test")
+        self.assertEqual(self.prog.linux_kernel_loadable_module(module_obj), module)
+        self.assertEqual(
+            self.prog.linux_kernel_loadable_module(module_obj, create=True),
+            (module, False),
+        )
+
+    def test_no_sys_module(self):
+        # Test that we get the same modules with and without using /sys/module.
+
+        def module_dict(prog):
+            return {
+                (module.name, module.address): (
+                    module.address_range,
+                    module.build_id,
+                    dict(module.section_addresses),
+                )
+                for module, _ in prog.loaded_modules()
+                if isinstance(module, RelocatableModule)
+            }
+
+        use_sys_module = int(os.environ.get("DRGN_USE_SYS_MODULE", "1")) != 0
+
+        with modifyenv({"DRGN_USE_SYS_MODULE": str(int(not use_sys_module))}):
+            prog = Program()
+            prog.set_kernel()
+
+            if use_sys_module:
+                with_sys_module = module_dict(self.prog)
+                without_sys_module = module_dict(prog)
             else:
-                prog = Program()
-                prog.set_kernel()
-                self._load_debug_info(prog)
-            self.assertEqual(prog.symbol(self.SYMBOL).address, self.symbol_address)
+                with_sys_module = module_dict(prog)
+                without_sys_module = module_dict(self.prog)
 
-    def test_module_debug_info_use_proc_and_sys(self):
-        self._test_module_debug_info(True)
-
-    def test_module_debug_info_use_core_dump(self):
-        self._test_module_debug_info(False)
+            self.assertEqual(with_sys_module, without_sys_module)

commit f96e140515067e1e56653e1480c9a4d3dc4bfd4a

libdrgn: debug_info: ignore gnu_debugaltlink file with no debug info
drgn_module_maybe_use_elf_file() assumes that a file whose build ID
matches the wanted gnu_debugaltlink is usable as a supplementary debug
file, so it unconditionally sets has_dwarf and use_debug. But nothing
checks that the file actually has debug info that drgn can use. This
leads to obtuse "no DWARF information" errors from libdw when it's too
late to act on it, or if libdw and drgn disagree, null pointer
dereferences while indexing. Reject missing debug info early instead.

Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -1750,6 +1750,13 @@ drgn_module_try_file_internal(struct drgn_module *module, const char *path,
 	// fd and elf are owned by the drgn_elf_file now.
 	fd = -1;
 	elf = NULL;
+
+	if (is_gnu_debugaltlink_file && !drgn_elf_file_has_dwarf(file)) {
+		drgn_log_debug(prog, "%s has no debug info; ignoring", path);
+		drgn_elf_file_destroy(file);
+		return NULL;
+	}
+
 	return drgn_module_maybe_use_elf_file(module, file,
 					      is_gnu_debugaltlink_file);
 }

tests/test_debug_info.py
@@ -528,6 +528,66 @@ class TestModuleTryFile(TestCase):
             self.assertEqual(module.wants_debug_file(), True)
             self.assertRaises(ValueError, module.wanted_supplementary_debug_file)
 
+    def test_gnu_debugaltlink_no_dwarf(self):
+        build_id = b"\x01\x23\x45\x67\x89\xab\xcd\xef"
+        alt_build_id = b"\xfe\xdc\xba\x98\x76\x54\x32\x10"
+
+        with tempfile.TemporaryDirectory(
+            prefix="bin-"
+        ) as bin_dir, tempfile.TemporaryDirectory(prefix="debug-") as debug_dir:
+            bin_dir = Path(bin_dir)
+            debug_dir = Path(debug_dir)
+
+            alt_path = debug_dir / "alt.debug"
+            alt_path.write_bytes(create_elf_file(ET.EXEC, build_id=alt_build_id))
+
+            binary_path = bin_dir / "binary"
+            binary_path.write_bytes(
+                create_dwarf_file(
+                    (),
+                    sections=(ALLOCATED_SECTION,),
+                    build_id=build_id,
+                    gnu_debugaltlink=(alt_path, alt_build_id),
+                )
+            )
+
+            module = self.prog.extra_module(bin_dir / "binary", create=True)
+            module.build_id = build_id
+
+            module.try_file(binary_path)
+            self.assertEqual(
+                module.debug_file_status, ModuleFileStatus.WANT_SUPPLEMENTARY
+            )
+            self.assertIsNone(module.debug_file_path)
+            self.assertIsNone(module.supplementary_debug_file_kind)
+            self.assertIsNone(module.supplementary_debug_file_path)
+            self.assertEqual(
+                module.wanted_supplementary_debug_file(),
+                (
+                    SupplementaryFileKind.GNU_DEBUGALTLINK,
+                    str(binary_path),
+                    str(alt_path),
+                    alt_build_id,
+                ),
+            )
+
+            module.try_file(alt_path)
+            self.assertEqual(
+                module.debug_file_status, ModuleFileStatus.WANT_SUPPLEMENTARY
+            )
+            self.assertIsNone(module.debug_file_path)
+            self.assertIsNone(module.supplementary_debug_file_kind)
+            self.assertIsNone(module.supplementary_debug_file_path)
+            self.assertEqual(
+                module.wanted_supplementary_debug_file(),
+                (
+                    SupplementaryFileKind.GNU_DEBUGALTLINK,
+                    str(binary_path),
+                    str(alt_path),
+                    alt_build_id,
+                ),
+            )
+
     def test_extra_module_no_address_range(self):
         module = self.prog.extra_module("/foo/bar", create=True)
         with NamedTemporaryElfFile() as f: