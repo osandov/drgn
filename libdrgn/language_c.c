osandov/drgn

commit d0ce699d6c1c0eb022c3a336c19f6dbdf76adc47

Add implicit_convert()
There are two main types of conversions in C:

- Explicit conversions, i.e., casts.
- Implicit conversions, which are applied for variable assignments,
  assigning arguments to function parameters, return values, etc. These
  have stricter rules about what types can be converted.

drgn currently only has a cast() function to do an explicit conversion.
This adds an implicit_convert() function to do an implicit conversion.
The most obvious use case is allowing helpers to type check their
parameters and perform the same conversions that happen in C (e.g,
automatically converting `void *` to the appropriate pointer type but
rejecting other pointer types):

  >>> def foo(task: Object):
  ...     task = implicit_convert("struct task_struct *", task)
  ...
  >>> foo(Object(prog, "void *", 0))
  >>> foo(Object(prog, "struct mm_struct *", 0))
  Traceback (most recent call last):
    ...
  TypeError: cannot convert 'struct mm_struct *' to incompatible type 'struct task_struct *'

I haven't measured the performance overhead of doing this yet, so I'm
not going to add this to existing helpers yet. But, I do have another
use case for type checking function calls coming up soon.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/language_c.c
@@ -3144,6 +3144,165 @@ ret2:
 	return NULL;
 }
 
+static struct drgn_error *
+c_types_compatible_impl(struct drgn_qualified_type qualified_type1,
+			struct drgn_qualified_type qualified_type2,
+			bool *ret)
+{
+	struct drgn_error *err;
+
+	// The types must have the same qualifiers.
+	if (qualified_type1.qualifiers != qualified_type2.qualifiers) {
+		*ret = false;
+		return NULL;
+	}
+
+	struct drgn_type *type1 = drgn_underlying_type(qualified_type1.type);
+	struct drgn_type *type2 = drgn_underlying_type(qualified_type2.type);
+
+	// If the type descriptors are the same, then the types are definitely
+	// compatible.
+	if (type1 == type2)
+		return NULL;
+
+	if (drgn_type_kind(type1) != drgn_type_kind(type2)) {
+		// Enum types are compatible with their compatible integer type.
+		// but not with different enum types with the same compatible
+		// integer type.
+		if (drgn_type_kind(type1) == DRGN_TYPE_ENUM) {
+			qualified_type1.type = drgn_type_type(type1).type;
+			if (qualified_type1.type) {
+				return c_types_compatible_impl(qualified_type1,
+							       qualified_type2,
+							       ret);
+			}
+		} else if (drgn_type_kind(type2) == DRGN_TYPE_ENUM) {
+			qualified_type2.type = drgn_type_type(type2).type;
+			if (qualified_type2.type) {
+				return c_types_compatible_impl(qualified_type1,
+							       qualified_type2,
+							       ret);
+			}
+		}
+		*ret = false;
+		return NULL;
+	}
+
+	SWITCH_ENUM(drgn_type_kind(type1)) {
+	case DRGN_TYPE_VOID:
+	case DRGN_TYPE_INT:
+	case DRGN_TYPE_BOOL:
+	case DRGN_TYPE_FLOAT:
+		// These types are deduplicated, so if they were compatible they
+		// would have had the same type descriptor.
+		*ret = false;
+		return NULL;
+	case DRGN_TYPE_STRUCT:
+	case DRGN_TYPE_UNION:
+	case DRGN_TYPE_CLASS: {
+		// It's expensive to check all of the members, so we do a sloppy
+		// check: if the tag and size are the same, then the types are
+		// _probably_ compatible.
+		if (drgn_type_is_complete(type1) && drgn_type_is_complete(type2)
+		    && drgn_type_size(type1) != drgn_type_size(type2)) {
+			*ret = false;
+			return NULL;
+		}
+		const char *tag1 = drgn_type_tag(type1);
+		const char *tag2 = drgn_type_tag(type2);
+		if ((!tag1 != !tag2) || (tag1 && strcmp(tag1, tag2) != 0))
+			*ret = false;
+		return NULL;
+	}
+	case DRGN_TYPE_ENUM: {
+		// We do a similar sloppy check here: if the tag and compatible
+		// type are the same, then the types are _probably_ compatible.
+		if (drgn_type_is_complete(type1) && drgn_type_is_complete(type2)
+		    && drgn_underlying_type(drgn_type_type(type1).type)
+		       != drgn_underlying_type(drgn_type_type(type2).type)) {
+			*ret = false;
+			return NULL;
+		}
+		const char *tag1 = drgn_type_tag(type1);
+		const char *tag2 = drgn_type_tag(type2);
+		if ((!tag1 != !tag2) || (tag1 && strcmp(tag1, tag2) != 0))
+			*ret = false;
+		return NULL;
+	}
+	case DRGN_TYPE_POINTER:
+		// The types are compatible iff their referenced types are
+		// compatible.
+		return c_types_compatible_impl(drgn_type_type(type1),
+					       drgn_type_type(type2), ret);
+	case DRGN_TYPE_ARRAY:
+		// The types are compatible iff their element types are
+		// compatible and, if both types are complete, their lengths are
+		// equal.
+		if (drgn_type_is_complete(type1) && drgn_type_is_complete(type2)
+		    && drgn_type_length(type1) != drgn_type_length(type2)) {
+			*ret = false;
+			return NULL;
+		}
+		return c_types_compatible_impl(drgn_type_type(type1),
+					       drgn_type_type(type2), ret);
+	case DRGN_TYPE_FUNCTION: {
+		// The types are compatible iff their return types are
+		// compatible, they have the same number of parameters, their
+		// corresponding parameter types are compatible, and neither is
+		// variadic or both are variadic.
+		//
+		// This is expensive, but there's no good shortcut like for
+		// structs and enums.
+		size_t num_parameters = drgn_type_num_parameters(type1);
+		if (num_parameters != drgn_type_num_parameters(type2)
+		    || drgn_type_is_variadic(type1)
+		       != drgn_type_is_variadic(type2)) {
+			*ret = false;
+			return NULL;
+		}
+		err = c_types_compatible_impl(drgn_type_type(type1),
+					      drgn_type_type(type2),
+					      ret);
+		if (err || !*ret)
+			return err;
+		struct drgn_type_parameter *parameters1 =
+			drgn_type_parameters(type1);
+		struct drgn_type_parameter *parameters2 =
+			drgn_type_parameters(type2);
+		for (size_t i = 0; i < num_parameters; i++) {
+			struct drgn_qualified_type parameter_type1;
+			err = drgn_parameter_type(&parameters1[i],
+						  &parameter_type1);
+			if (err)
+				return err;
+			struct drgn_qualified_type parameter_type2;
+			err = drgn_parameter_type(&parameters2[i],
+						  &parameter_type2);
+			if (err)
+				return err;
+			err = c_types_compatible_impl(parameter_type1,
+						      parameter_type2, ret);
+			if (err || !*ret)
+				return err;
+		}
+		return NULL;
+	}
+	// This is already the underlying type, so it can't be a typedef.
+	case DRGN_TYPE_TYPEDEF:
+	default:
+		UNREACHABLE();
+	}
+}
+
+static struct drgn_error *
+c_types_compatible(struct drgn_qualified_type qualified_type1,
+		   struct drgn_qualified_type qualified_type2,
+		   bool *ret)
+{
+	*ret = true;
+	return c_types_compatible_impl(qualified_type1, qualified_type2, ret);
+}
+
 static struct drgn_error *c_operand_type(const struct drgn_object *obj,
 					 struct drgn_operand_type *type_ret,
 					 bool *is_pointer_ret,
@@ -3248,6 +3407,123 @@ static struct drgn_error *c_op_cast(struct drgn_object *res,
 	if (err)
 		return err;
 	return drgn_op_cast(res, &type, obj, &obj_type);
+}
+
+static struct drgn_error *
+c_op_implicit_convert(struct drgn_object *res,
+		      struct drgn_qualified_type qualified_type,
+		      const struct drgn_object *obj)
+{
+	struct drgn_error *err;
+
+	struct drgn_object_type type;
+	err = drgn_object_type(qualified_type, 0, &type);
+	if (err)
+		return err;
+
+	if (drgn_type_kind(type.underlying_type) == DRGN_TYPE_BOOL) {
+		bool truthy;
+		err = drgn_object_bool(obj, &truthy);
+		if (err)
+			return err;
+		return drgn_object_set_unsigned_internal(res, &type, truthy);
+	}
+
+	struct drgn_operand_type obj_type;
+	err = c_operand_type(obj, &obj_type, NULL, NULL);
+	if (err)
+		return err;
+
+	SWITCH_ENUM(drgn_type_kind(type.underlying_type)) {
+	case DRGN_TYPE_INT:
+	case DRGN_TYPE_FLOAT:
+	case DRGN_TYPE_ENUM:
+		switch (drgn_type_kind(obj_type.underlying_type)) {
+		case DRGN_TYPE_INT:
+		case DRGN_TYPE_BOOL:
+		case DRGN_TYPE_FLOAT:
+		case DRGN_TYPE_ENUM:
+			break;
+		default:
+			goto incompatible_type_error;
+		}
+		break;
+	case DRGN_TYPE_STRUCT:
+	case DRGN_TYPE_UNION:
+	case DRGN_TYPE_CLASS: {
+		struct drgn_qualified_type unqualified_type1 = {
+			.type = type.underlying_type,
+		};
+		struct drgn_qualified_type unqualified_type2 = {
+			.type = obj_type.underlying_type,
+		};
+		bool compatible;
+		err = c_types_compatible(unqualified_type1, unqualified_type2,
+					 &compatible);
+		if (err)
+			return err;
+		if (!compatible)
+			goto incompatible_type_error;
+		return drgn_object_slice_internal(res, obj, &type, 0, 0);
+	}
+	case DRGN_TYPE_POINTER: {
+		if (drgn_type_kind(obj_type.underlying_type)
+		    != DRGN_TYPE_POINTER)
+			goto incompatible_type_error;
+
+		struct drgn_qualified_type referenced_type =
+			drgn_type_type(type.underlying_type);
+		referenced_type.type =
+			drgn_underlying_type(referenced_type.type);
+		struct drgn_qualified_type obj_referenced_type =
+			drgn_type_type(obj_type.underlying_type);
+		obj_referenced_type.type =
+			drgn_underlying_type(obj_referenced_type.type);
+
+		// The type pointed to by the left must have all of the
+		// qualifiers of the type pointed to by the right:
+		// (lhs.qualifiers & rhs.qualifiers) == rhs.qualifiers.
+		// We mask here and do the equality test below or in
+		// c_types_compatible().
+		referenced_type.qualifiers &= obj_referenced_type.qualifiers;
+
+		// The type pointed to by the left and the type pointed to by
+		// the right must be compatible, or at least one must be void.
+		if (drgn_type_kind(referenced_type.type) == DRGN_TYPE_VOID
+		    || drgn_type_kind(obj_referenced_type.type) == DRGN_TYPE_VOID) {
+			if (referenced_type.qualifiers
+			    != obj_referenced_type.qualifiers)
+				goto incompatible_type_error;
+		} else {
+			bool compatible;
+			err = c_types_compatible(referenced_type, obj_referenced_type,
+						 &compatible);
+			if (err)
+				return err;
+			if (!compatible)
+				goto incompatible_type_error;
+		}
+		break;
+	}
+	case DRGN_TYPE_VOID:
+	case DRGN_TYPE_ARRAY:
+	case DRGN_TYPE_FUNCTION:
+		return drgn_qualified_type_error("cannot convert to '%s'",
+						 qualified_type);
+	// We handled bool earlier.
+	case DRGN_TYPE_BOOL:
+	// This is already the underlying type, so it can't be a typedef.
+	case DRGN_TYPE_TYPEDEF:
+	default:
+		UNREACHABLE();
+	}
+
+	return drgn_op_cast(res, &type, obj, &obj_type);
+
+incompatible_type_error:
+	return drgn_2_qualified_types_error("cannot convert '%s' to incompatible type '%s'",
+					    drgn_object_qualified_type(obj),
+					    qualified_type);
 }
 
 /*
@@ -3532,6 +3808,7 @@ LIBDRGN_PUBLIC const struct drgn_language drgn_language_c = {
 	.bool_literal = c_bool_literal,
 	.float_literal = c_float_literal,
 	.op_cast = c_op_cast,
+	.op_implicit_convert = c_op_implicit_convert,
 	.op_bool = c_op_bool,
 	.op_cmp = c_op_cmp,
 	.op_add = c_op_add,
@@ -3562,6 +3839,7 @@ LIBDRGN_PUBLIC const struct drgn_language drgn_language_cpp = {
 	.bool_literal = c_bool_literal,
 	.float_literal = c_float_literal,
 	.op_cast = c_op_cast,
+	.op_implicit_convert = c_op_implicit_convert,
 	.op_bool = c_op_bool,
 	.op_cmp = c_op_cmp,
 	.op_add = c_op_add,

_drgn.pyi
@@ -1690,18 +1690,48 @@ def NULL(prog: Program, type: Union[str, Type]) -> Object:
 
 def cast(type: Union[str, Type], obj: Object) -> Object:
     """
-    Get the value of the given object casted to another type.
+    Get the value of an object explicitly casted to another type.
 
-    Objects with a scalar type (integer, boolean, enumerated, floating-point,
-    or pointer) can be casted to a different scalar type. Other objects can
-    only be casted to the same type. This always results in a value object. See
-    also :func:`drgn.reinterpret()`.
+    This uses the programming language's rules for explicit conversions, like
+    the cast operator.
 
     >>> cast("unsigned int", Object(prog, "float", 2.0))
     (unsigned int)2
+    >>> cast("void *", Object(prog, "int", 0))
+    (void *)0x0
+
+    See also :func:`implicit_convert()` for implicit conversions (which usually
+    do stricter type checking) and :func:`reinterpret()` for reinterpreting the
+    raw memory of an object.
 
     :param type: Type to cast to.
     :param obj: Object to cast.
+    :return: Casted object. This is always a value object.
+    :raises TypeError: if casting *obj* to *type* is not allowed
+    """
+    ...
+
+def implicit_convert(type: Union[str, Type], obj: Object) -> Object:
+    """
+    Get the value of an object implicitly converted to another type.
+
+    This uses the programming language's rules for implicit conversions, like
+    when assigning to a variable or passing arguments to a function call.
+
+    >>> implicit_convert("unsigned int", Object(prog, "float", 2.0))
+    (unsigned int)2
+    >>> implicit_convert("void *", Object(prog, "int", 0))
+    Traceback (most recent call last):
+      ...
+    TypeError: cannot convert 'int' to incompatible type 'void *'
+
+    See also :func:`cast()` for explicit conversions and :func:`reinterpret()`
+    for reinterpreting the raw memory of an object.
+
+    :param type: Type to convert to.
+    :param obj: Object to convert.
+    :return: Converted object. This is always a value object.
+    :raises TypeError: if converting *obj* to *type* is not allowed
     """
     ...
 
@@ -1710,15 +1740,24 @@ def reinterpret(type: Union[str, Type], obj: Object) -> Object:
     Get the representation of an object reinterpreted as another type.
 
     This reinterprets the raw memory of the object, so an object can be
-    reinterpreted as any other type. Reinterpreting a reference results in a
-    reference, and reinterpreting a value results in a value. See also
-    :func:`drgn.cast()`.
+    reinterpreted as any other type.
 
     >>> reinterpret("unsigned int", Object(prog, "float", 2.0))
     (unsigned int)1073741824
 
+    .. note::
+
+        You usually want :func:`cast()` or :func:`implicit_convert()` instead,
+        which convert the *value* of an object instead of its in-memory
+        representation.
+
     :param type: Type to reinterpret as.
     :param obj: Object to reinterpret.
+    :return: Reinterpreted object. If *obj* is a reference object, then this is
+        a reference object. If *obj* is a value object, then this is a value
+        object.
+    :raises OutOfBoundsError: if *obj* is a value object and *type* is larger
+        than *obj*
     """
     ...
 

drgn/__init__.py
@@ -84,6 +84,7 @@ from _drgn import (
     filename_matches,
     get_default_prog,
     host_platform,
+    implicit_convert,
     offsetof,
     program_from_core_dump,
     program_from_kernel,
@@ -140,6 +141,7 @@ __all__ = (
     "filename_matches",
     "get_default_prog",
     "host_platform",
+    "implicit_convert",
     "offsetof",
     "program_from_core_dump",
     "program_from_kernel",

libdrgn/drgn.h
@@ -2083,22 +2083,41 @@ struct drgn_error *drgn_format_object(const struct drgn_object *obj,
  */
 
 /**
- * Set a @ref drgn_object to the value of an object casted to a another type.
+ * Set a @ref drgn_object to the value of an object explicitly casted to a
+ * another type.
  *
- * Objects with a scalar type can be casted to a different scalar type. Other
- * objects can only be casted to the same type. @p res is always set to a value
- * object.
+ * This uses the programming language's rules for explicit conversions, like the
+ * cast operator.
  *
- * @sa drgn_object_reinterpret()
+ * @sa drgn_object_implicit_convert(), drgn_object_reinterpret()
  *
- * @param[out] res Object to set.
+ * @param[out] res Object to set. Always set to a value object.
  * @param[in] qualified_type New type.
- * @param[in] obj Object to read.
+ * @param[in] obj Object to cast.
  * @return @c NULL on success, non-@c NULL on error.
  */
 struct drgn_error *drgn_object_cast(struct drgn_object *res,
 				    struct drgn_qualified_type qualified_type,
 				    const struct drgn_object *obj);
+
+/**
+ * Set a @ref drgn_object to the value of an object implicitly converted to a
+ * another type.
+ *
+ * This uses the programming language's rules for implicit conversions, like
+ * when assigning to a variable or passing arguments to a function call.
+ *
+ * @sa drgn_object_cast(), drgn_object_reinterpret()
+ *
+ * @param[out] res Object to set. Always set to a value object.
+ * @param[in] qualified_type New type.
+ * @param[in] obj Object to convert.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_object_implicit_convert(struct drgn_object *res,
+			     struct drgn_qualified_type qualified_type,
+			     const struct drgn_object *obj);
 
 /**
  * Set a @ref drgn_object to the representation of an object reinterpreted as
@@ -2107,12 +2126,10 @@ struct drgn_error *drgn_object_cast(struct drgn_object *res,
  * This reinterprets the raw memory of the object, so an object can be
  * reinterpreted as any other type.
  *
- * If @c obj is a value, then @c res is set to a value; if @c obj is a
- * reference, then @c res is set to a reference.
+ * @sa drgn_object_cast(), drgn_object_implicit_convert()
  *
- * @sa drgn_object_cast()
- *
- * @param[out] res Object to set.
+ * @param[out] res Object to set. If @p obj is a value, set to a value. If @p
+ * obj is a reference, set to a reference.
  * @param[in] qualified_type New type.
  * @param[in] obj Object to reinterpret.
  * @return @c NULL on success, non-@c NULL on error.

libdrgn/language.h
@@ -133,6 +133,7 @@ struct drgn_language {
 	 */
 	drgn_float_literal_fn *float_literal;
 	drgn_cast_op *op_cast;
+	drgn_cast_op *op_implicit_convert;
 	drgn_bool_op *op_bool;
 	drgn_cmp_op *op_cmp;
 	drgn_binary_op *op_add;

libdrgn/object.c
@@ -564,25 +564,18 @@ drgn_object_copy(struct drgn_object *res, const struct drgn_object *obj)
 	return NULL;
 }
 
-LIBDRGN_PUBLIC struct drgn_error *
-drgn_object_slice(struct drgn_object *res, const struct drgn_object *obj,
-		  struct drgn_qualified_type qualified_type,
-		  uint64_t bit_offset, uint64_t bit_field_size)
+struct drgn_error *
+drgn_object_slice_internal(struct drgn_object *res,
+			   const struct drgn_object *obj,
+			   const struct drgn_object_type *type,
+			   uint64_t bit_offset, uint64_t bit_field_size)
 {
 	struct drgn_error *err;
-	if (drgn_object_program(res) != drgn_object_program(obj)) {
-		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
-					 "objects are from different programs");
-	}
-	struct drgn_object_type type;
-	err = drgn_object_type(qualified_type, bit_field_size, &type);
-	if (err)
-		return err;
 
 	SWITCH_ENUM(obj->kind) {
 	case DRGN_OBJECT_VALUE: {
 		uint64_t bit_end;
-		if (__builtin_add_overflow(bit_offset, type.bit_size, &bit_end)
+		if (__builtin_add_overflow(bit_offset, type->bit_size, &bit_end)
 		    || bit_end > obj->bit_size) {
 			return drgn_error_create(DRGN_ERROR_OUT_OF_BOUNDS,
 						 "out of bounds of value");
@@ -605,13 +598,13 @@ drgn_object_slice(struct drgn_object *res, const struct drgn_object *obj,
 			if (err)
 				return err;
 		}
-		return drgn_object_set_from_buffer_internal(res, &type, buf,
+		return drgn_object_set_from_buffer_internal(res, type, buf,
 							    bit_offset);
 	}
 	case DRGN_OBJECT_REFERENCE:
 		// obj->bit_offset + bit_offset can overflow, so apply the
 		// byte-aligned part of bit_offset now.
-		return drgn_object_set_reference_internal(res, &type,
+		return drgn_object_set_reference_internal(res, type,
 							  obj->address
 							  + (bit_offset / 8),
 							  obj->bit_offset
@@ -621,6 +614,24 @@ drgn_object_slice(struct drgn_object *res, const struct drgn_object *obj,
 	default:
 		UNREACHABLE();
 	}
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_object_slice(struct drgn_object *res, const struct drgn_object *obj,
+		  struct drgn_qualified_type qualified_type,
+		  uint64_t bit_offset, uint64_t bit_field_size)
+{
+	struct drgn_error *err;
+	if (drgn_object_program(res) != drgn_object_program(obj)) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "objects are from different programs");
+	}
+	struct drgn_object_type type;
+	err = drgn_object_type(qualified_type, bit_field_size, &type);
+	if (err)
+		return err;
+	return drgn_object_slice_internal(res, obj, &type, bit_offset,
+					  bit_field_size);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
@@ -1351,6 +1362,20 @@ drgn_object_cast(struct drgn_object *res,
 					 "objects are from different programs");
 	}
 	return lang->op_cast(res, qualified_type, obj);
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
+drgn_object_implicit_convert(struct drgn_object *res,
+			     struct drgn_qualified_type qualified_type,
+			     const struct drgn_object *obj)
+{
+	const struct drgn_language *lang = drgn_type_language(qualified_type.type);
+
+	if (drgn_object_program(res) != drgn_object_program(obj)) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "objects are from different programs");
+	}
+	return lang->op_implicit_convert(res, qualified_type, obj);
 }
 
 LIBDRGN_PUBLIC struct drgn_error *

libdrgn/object.h
@@ -207,6 +207,12 @@ drgn_object_set_absent_internal(struct drgn_object *res,
 	drgn_object_reinit(res, type, DRGN_OBJECT_ABSENT);
 }
 
+struct drgn_error *
+drgn_object_slice_internal(struct drgn_object *res,
+			   const struct drgn_object *obj,
+			   const struct drgn_object_type *type,
+			   uint64_t bit_offset, uint64_t bit_field_size);
+
 /**
  * Binary operator implementation.
  *

commit bf48ee1503802f0931cb038e7f099a179f025d55

libdrgn: c: limit recursion in c_types_compatible()
c_types_compatible_impl() returns early if both types are the same type
descriptor, which stops it from recursing forever on a cycle in a single
type. But two distinct types can be cyclic and structurally identical,
in which case it recurses forever and overflows the stack. Add a
recursion guard.

Fixes: d0ce699d6c1c ("Add implicit_convert()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/language_c.c
@@ -3285,6 +3285,8 @@ c_types_compatible_impl(struct drgn_qualified_type qualified_type1,
 	if (type1 == type2)
 		return NULL;
 
+	drgn_recursion_guard(1000, "maximum type depth exceeded");
+
 	if (drgn_type_kind(type1) != drgn_type_kind(type2)) {
 		// Enum types are compatible with their compatible integer type.
 		// but not with different enum types with the same compatible

tests/test_language_c.py
@@ -2636,6 +2636,20 @@ class TestImplicitConvert(MockProgramTestCase):
             Object(self.prog, type1, 0x8),
         )
 
+    def test_function_pointer_parameter_cycle(self):
+        type1 = self.prog.function_type(
+            self.prog.type("void"), (TypeParameter(lambda: type1),)
+        )
+        type2 = self.prog.function_type(
+            self.prog.type("void"), (TypeParameter(lambda: type2),)
+        )
+        self.assertRaises(
+            RecursionError,
+            implicit_convert,
+            self.prog.pointer_type(type1),
+            Object(self.prog, self.prog.pointer_type(type2), 0x8),
+        )
+
     def test_function_pointers_with_different_is_variadic(self):
         type1 = self.prog.pointer_type(
             self.prog.function_type(self.prog.type("int"), (), is_variadic=False)