osandov/drgn

commit 7e6efe6649c25a167b8ff956cfeb5cf916c909bd

Add support for looking up types in namespaces
Looking up objects in namespaces is already well-supported by `drgn`.
These changes bring the same to functionality type lookup, so that
`prog.type('struct A::B::C::MyType')` works in an analogous fashion to
`prog['A::B::C::MyVar']`.

Signed-off-by: Kevin Svetlitski <svetlitski@meta.com>

libdrgn/language_c.c
@@ -1651,6 +1651,7 @@ enum {
 	C_TOKEN_NUMBER,
 	C_TOKEN_IDENTIFIER,
 	C_TOKEN_TEMPLATE_ARGUMENTS,
+	C_TOKEN_COLON,
 };
 
 #include "c_keywords.inc"
@@ -1697,9 +1698,12 @@ struct drgn_error *drgn_c_family_lexer_func(struct drgn_lexer *lexer,
 		token->kind = C_TOKEN_DOT;
 		p++;
 		break;
+	case ':':
+		token->kind = C_TOKEN_COLON;
+		p++;
+		break;
 	case '<':
-		// This is a hack for
-		// cpp_append_template_arguments_to_identifier(). We don't want
+		// This is a hack for cpp_append_to_identifier(). We don't want
 		// to deal with actually parsing template arguments, and we
 		// don't care about "<" otherwise, so this scans a token from
 		// the "<" to its matching ">".
@@ -2095,6 +2099,7 @@ out:
 	return primitive;
 }
 
+
 // The DWARF index currently includes template arguments in indexed names. So,
 // to be able to find a type with template arguments, we have to look it up with
 // the template arguments included. This looks for a C_TOKEN_TEMPLATE_ARGUMENTS
@@ -2105,10 +2110,8 @@ out:
 // as they appear in DWARF (which can vary between compilers). In the future, it
 // might be better to properly parse and either normalize the template arguments
 // or look them up as an AST somehow.
-static struct drgn_error *
-cpp_append_template_arguments_to_identifier(struct drgn_lexer *lexer,
-					    const char *identifier,
-					    size_t *len_ret)
+static struct drgn_error *cpp_append_to_identifier(
+	struct drgn_lexer *lexer, const char *identifier, size_t *len_ret)
 {
 	struct drgn_error *err;
 
@@ -2117,11 +2120,20 @@ cpp_append_template_arguments_to_identifier(struct drgn_lexer *lexer,
 		return NULL;
 
 	struct drgn_token token;
-	err = drgn_lexer_pop(lexer, &token);
+
+	do {
+		err = drgn_lexer_pop(lexer, &token);
+	} while (!err && (token.kind == C_TOKEN_IDENTIFIER ||
+			  token.kind == C_TOKEN_COLON));
+
 	if (err)
 		return err;
-	if (token.kind != C_TOKEN_TEMPLATE_ARGUMENTS)
-		return drgn_lexer_push(lexer, &token);
+	if (token.kind != C_TOKEN_TEMPLATE_ARGUMENTS) {
+		err = drgn_lexer_push(lexer, &token);
+		if (err)
+			return err;
+	}
+
 	*len_ret = token.value + token.len - identifier;
 	return NULL;
 }
@@ -2173,13 +2185,13 @@ c_parse_specifier_qualifier_list(struct drgn_program *prog,
 							 keyword_spelling[token.kind],
 							 specifier_spelling[prev_specifier]);
 			}
-		} else if (token.kind == C_TOKEN_IDENTIFIER &&
-			   specifier == SPECIFIER_NONE && !identifier) {
+		} else if ((token.kind == C_TOKEN_IDENTIFIER ||
+			    token.kind == C_TOKEN_COLON) &&
+			    specifier == SPECIFIER_NONE && !identifier) {
 			identifier = token.value;
 			identifier_len = token.len;
-			err = cpp_append_template_arguments_to_identifier(lexer,
-									  identifier,
-									  &identifier_len);
+			err = cpp_append_to_identifier(lexer, identifier,
+						       &identifier_len);
 			if (err)
 				return err;
 		} else if (token.kind == C_TOKEN_STRUCT ||
@@ -2201,7 +2213,8 @@ c_parse_specifier_qualifier_list(struct drgn_program *prog,
 			err = drgn_lexer_pop(lexer, &token);
 			if (err)
 				return err;
-			if (token.kind != C_TOKEN_IDENTIFIER) {
+			if (!(token.kind == C_TOKEN_IDENTIFIER ||
+			      token.kind == C_TOKEN_COLON)) {
 				return drgn_error_format(DRGN_ERROR_SYNTAX,
 							 "expected identifier after '%s'",
 							 keyword_spelling[tag_token]);
@@ -2209,9 +2222,8 @@ c_parse_specifier_qualifier_list(struct drgn_program *prog,
 			}
 			identifier = token.value;
 			identifier_len = token.len;
-			err = cpp_append_template_arguments_to_identifier(lexer,
-									  identifier,
-									  &identifier_len);
+			err = cpp_append_to_identifier(lexer, identifier,
+						       &identifier_len);
 			if (err)
 				return err;
 		} else {

libdrgn/dwarf_info.c
@@ -6816,6 +6816,45 @@ drgn_type_from_dwarf_internal(struct drgn_debug_info *dbinfo,
 	return NULL;
 }
 
+static struct drgn_error *
+find_enclosing_namespace(struct drgn_namespace_dwarf_index *global_namespace,
+			 const char **name, size_t *name_len,
+			 struct drgn_namespace_dwarf_index **namespace_ret)
+{
+	*namespace_ret = global_namespace;
+	if (*name_len >= 2 && memcmp(*name, "::", 2) == 0) {
+		/* Explicit global namespace. */
+		*name_len -= 2;
+		*name += 2;
+	}
+
+	const char *template_parameters_start = memchr(*name, '<', *name_len);
+	ptrdiff_t searchable_len =
+		template_parameters_start ?
+				  template_parameters_start - *name:
+				  *name_len;
+	const char *colons;
+	while ((colons = memmem(*name, searchable_len, "::", 2))) {
+		struct drgn_dwarf_index_iterator it;
+		uint64_t ns_tag = DW_TAG_namespace;
+		struct drgn_error *err = drgn_dwarf_index_iterator_init(
+			&it, *namespace_ret, *name, colons - *name, &ns_tag, 1);
+		if (err)
+			return err;
+		struct drgn_dwarf_index_die *index_die =
+			drgn_dwarf_index_iterator_next(&it);
+		if (!index_die)
+			return &drgn_not_found;
+		*namespace_ret = index_die->namespace;
+		size_t chars_consumed = colons + 2 - *name;
+		searchable_len -= chars_consumed;
+		*name_len -= chars_consumed;
+		*name = colons + 2;
+	}
+
+	return NULL;
+}
+
 struct drgn_error *drgn_debug_info_find_type(enum drgn_type_kind kind,
 					     const char *name, size_t name_len,
 					     const char *filename, void *arg,
@@ -6850,8 +6889,13 @@ struct drgn_error *drgn_debug_info_find_type(enum drgn_type_kind kind,
 		UNREACHABLE();
 	}
 
+	struct drgn_namespace_dwarf_index *namespace;
+	err = find_enclosing_namespace(&dbinfo->dwarf.global,
+				       &name, &name_len, &namespace);
+	if (err)
+		return err;
 	struct drgn_dwarf_index_iterator it;
-	err = drgn_dwarf_index_iterator_init(&it, &dbinfo->dwarf.global, name,
+	err = drgn_dwarf_index_iterator_init(&it, namespace, name,
 					     name_len, &tag, 1);
 	if (err)
 		return err;
@@ -6886,28 +6930,11 @@ drgn_debug_info_find_object(const char *name, size_t name_len,
 	struct drgn_error *err;
 	struct drgn_debug_info *dbinfo = arg;
 
-	struct drgn_namespace_dwarf_index *ns = &dbinfo->dwarf.global;
-	if (name_len >= 2 && memcmp(name, "::", 2) == 0) {
-		/* Explicit global namespace. */
-		name_len -= 2;
-		name += 2;
-	}
-	const char *colons;
-	while ((colons = memmem(name, name_len, "::", 2))) {
-		struct drgn_dwarf_index_iterator it;
-		uint64_t ns_tag = DW_TAG_namespace;
-		err = drgn_dwarf_index_iterator_init(&it, ns, name,
-						     colons - name, &ns_tag, 1);
-		if (err)
-			return err;
-		struct drgn_dwarf_index_die *index_die =
-			drgn_dwarf_index_iterator_next(&it);
-		if (!index_die)
-			return &drgn_not_found;
-		ns = index_die->namespace;
-		name_len -= colons + 2 - name;
-		name = colons + 2;
-	}
+	struct drgn_namespace_dwarf_index *ns;
+	err = find_enclosing_namespace(&dbinfo->dwarf.global,
+				       &name, &name_len, &ns);
+	if (err)
+		return err;
 
 	uint64_t tags[3];
 	size_t num_tags = 0;

tests/test_dwarf.py
@@ -190,6 +190,7 @@ def dwarf_program(*args, segments=None, **kwds):
         f.write(compile_dwarf(*args, **kwds))
         f.flush()
         prog.load_debug_info([f.name])
+
     if segments is not None:
         add_mock_memory_segments(prog, segments)
     return prog
@@ -3810,6 +3811,258 @@ class TestTypes(TestCase):
             ),
         )
         self.assertIdentical(prog.type("struct point"), prog.type("TEST").type)
+
+    def test_namespaces(self):
+        def make_composite_die(tag: DW_TAG, name: str) -> DwarfDie:
+            return DwarfDie(
+                tag,
+                (
+                    DwarfAttrib(DW_AT.name, DW_FORM.string, name),
+                    DwarfAttrib(
+                        DW_AT.byte_size,
+                        DW_FORM.data1,
+                        4 if tag == DW_TAG.union_type else 8,
+                    ),
+                    DwarfAttrib(DW_AT.decl_file, DW_FORM.udata, "foo.c"),
+                ),
+                (
+                    DwarfDie(
+                        DW_TAG.member,
+                        (
+                            DwarfAttrib(DW_AT.name, DW_FORM.string, "x"),
+                            DwarfAttrib(
+                                DW_AT.data_member_location,
+                                DW_FORM.data1,
+                                0,
+                            ),
+                            DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                        ),
+                    ),
+                    DwarfDie(
+                        DW_TAG.member,
+                        (
+                            DwarfAttrib(DW_AT.name, DW_FORM.string, "y"),
+                            DwarfAttrib(
+                                DW_AT.data_member_location,
+                                DW_FORM.data1,
+                                0 if tag == DW_TAG.union_type else 4,
+                            ),
+                            DwarfAttrib(DW_AT.type, DW_FORM.ref4, "float_die"),
+                        ),
+                    ),
+                ),
+            )
+
+        dies = (
+            *labeled_int_die,
+            *labeled_float_die,
+            DwarfDie(
+                DW_TAG.namespace,
+                (DwarfAttrib(DW_AT.name, DW_FORM.string, "moho"),),
+                (
+                    DwarfDie(
+                        DW_TAG.namespace,
+                        (DwarfAttrib(DW_AT.name, DW_FORM.string, "eve"),),
+                        (
+                            DwarfDie(
+                                DW_TAG.namespace,
+                                (DwarfAttrib(DW_AT.name, DW_FORM.string, "kerbin"),),
+                                (
+                                    DwarfDie(
+                                        DW_TAG.typedef,
+                                        (
+                                            DwarfAttrib(
+                                                DW_AT.name,
+                                                DW_FORM.string,
+                                                "TEST_TYPEDEF",
+                                            ),
+                                            DwarfAttrib(
+                                                DW_AT.type, DW_FORM.ref4, "int_die"
+                                            ),
+                                        ),
+                                    ),
+                                    make_composite_die(
+                                        DW_TAG.structure_type, "TEST_STRUCT"
+                                    ),
+                                    make_composite_die(DW_TAG.class_type, "TEST_CLASS"),
+                                    make_composite_die(DW_TAG.union_type, "TEST_UNION"),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        prog = dwarf_program(dies, lang=DW_LANG.C_plus_plus)
+        # Language is not set automatically when there is no DIE for `main`
+        prog.language = Language.CPP
+
+        self.assertIdentical(
+            prog.type("moho::eve::kerbin::TEST_TYPEDEF"),
+            prog.typedef_type("TEST_TYPEDEF", prog.int_type("int", 4, True)),
+        )
+        self.assertIdentical(
+            prog.type("struct moho::eve::kerbin::TEST_STRUCT"),
+            prog.struct_type(
+                "TEST_STRUCT",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.float_type("float", 4), "y", 32),
+                ),
+            ),
+        )
+        self.assertIdentical(
+            prog.type("class moho::eve::kerbin::TEST_CLASS"),
+            prog.class_type(
+                "TEST_CLASS",
+                8,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.float_type("float", 4), "y", 32),
+                ),
+            ),
+        )
+        self.assertIdentical(
+            prog.type("union moho::eve::kerbin::TEST_UNION"),
+            prog.union_type(
+                "TEST_UNION",
+                4,
+                (
+                    TypeMember(prog.int_type("int", 4, True), "x", 0),
+                    TypeMember(prog.float_type("float", 4), "y", 0),
+                ),
+            ),
+        )
+
+    def test_explicit_global_namespace(self):
+        prog = dwarf_program(
+            (
+                *labeled_int_die,
+                DwarfDie(
+                    DW_TAG.typedef,
+                    (
+                        DwarfAttrib(
+                            DW_AT.name,
+                            DW_FORM.string,
+                            "TEST_TYPEDEF_GLOBAL",
+                        ),
+                        DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                    ),
+                ),
+            ),
+            lang=DW_LANG.C_plus_plus,
+        )
+        # Language is not set automatically when there is no DIE for `main`
+        prog.language = Language.CPP
+
+        self.assertIdentical(
+            prog.type("TEST_TYPEDEF_GLOBAL"), prog.type("::TEST_TYPEDEF_GLOBAL")
+        )
+
+    def test_template_in_namespace(self):
+        dies = (
+            *labeled_int_die,
+            *labeled_unsigned_int_die,
+            DwarfDie(
+                DW_TAG.namespace,
+                (DwarfAttrib(DW_AT.name, DW_FORM.string, "containers"),),
+                (
+                    DwarfLabel("typedef_die"),
+                    DwarfDie(
+                        DW_TAG.typedef,
+                        (
+                            DwarfAttrib(
+                                DW_AT.name,
+                                DW_FORM.string,
+                                "MyTypedef",
+                            ),
+                            DwarfAttrib(DW_AT.type, DW_FORM.ref4, "unsigned_int_die"),
+                        ),
+                    ),
+                    DwarfDie(
+                        DW_TAG.class_type,
+                        (
+                            DwarfAttrib(
+                                DW_AT.name,
+                                DW_FORM.string,
+                                "Pair<int, containers::MyTypedef>",
+                            ),
+                            DwarfAttrib(
+                                DW_AT.byte_size,
+                                DW_FORM.data1,
+                                8,
+                            ),
+                        ),
+                        (
+                            DwarfDie(
+                                DW_TAG.template_type_parameter,
+                                (
+                                    DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "T"),
+                                ),
+                            ),
+                            DwarfDie(
+                                DW_TAG.template_type_parameter,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.type, DW_FORM.ref4, "typedef_die"
+                                    ),
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "V"),
+                                ),
+                            ),
+                            DwarfDie(
+                                DW_TAG.member,
+                                (
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "first"),
+                                    DwarfAttrib(
+                                        DW_AT.data_member_location,
+                                        DW_FORM.data1,
+                                        0,
+                                    ),
+                                    DwarfAttrib(DW_AT.type, DW_FORM.ref4, "int_die"),
+                                ),
+                            ),
+                            DwarfDie(
+                                DW_TAG.member,
+                                (
+                                    DwarfAttrib(DW_AT.name, DW_FORM.string, "second"),
+                                    DwarfAttrib(
+                                        DW_AT.data_member_location,
+                                        DW_FORM.data1,
+                                        4,
+                                    ),
+                                    DwarfAttrib(
+                                        DW_AT.type, DW_FORM.ref4, "typedef_die"
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        prog = dwarf_program(dies, lang=DW_LANG.C_plus_plus)
+        # Language is not set automatically when there is no DIE for `main`
+        prog.language = Language.CPP
+
+        self.assertIdentical(
+            prog.type("class containers::Pair<int, containers::MyTypedef>"),
+            prog.class_type(
+                "Pair<int, containers::MyTypedef>",
+                8,
+                members=(
+                    TypeMember(prog.int_type("int", 4, True), "first", 0),
+                    TypeMember(prog.type("containers::MyTypedef"), "second", 32),
+                ),
+                template_parameters=(
+                    TypeTemplateParameter(prog.int_type("int", 4, True), "T"),
+                    TypeTemplateParameter(prog.type("containers::MyTypedef"), "V"),
+                ),
+            ),
+        )
 
 
 class TestObjects(TestCase):

commit a0c4c912ee5924974b1f682a10bb3364312441af

libdrgn: c: don't include lookahead token in C++ identifier
cpp_append_to_identifier() consumes identifier and ":" tokens, then
consumes a template argument token if there is one. If the token that
stopped the loop isn't template arguments, it is pushed back, but the
identifier length is still computed from it. Type lookups will then
fail, e.g.:

  >>> prog.type("Foo [4]")
  LookupError: could not find 'Foo ['

Track the end of the last token that we actually consumed instead.

Fixes: 7e6efe6649c2 ("Add support for looking up types in namespaces")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/language_c.c
@@ -2185,21 +2185,25 @@ static struct drgn_error *cpp_append_to_identifier(
 		return NULL;
 
 	struct drgn_token token;
-
-	do {
+	const char *end = identifier + *len_ret;
+	for (;;) {
 		err = drgn_lexer_pop(lexer, &token);
-	} while (!err && (token.kind == C_TOKEN_IDENTIFIER ||
-			  token.kind == C_TOKEN_COLON));
-
-	if (err)
-		return err;
-	if (token.kind != C_TOKEN_TEMPLATE_ARGUMENTS) {
-		err = drgn_lexer_push(lexer, &token);
 		if (err)
 			return err;
+		if (token.kind != C_TOKEN_IDENTIFIER &&
+		    token.kind != C_TOKEN_COLON &&
+		    token.kind != C_TOKEN_TEMPLATE_ARGUMENTS) {
+			err = drgn_lexer_push(lexer, &token);
+			if (err)
+				return err;
+			break;
+		}
+		end = token.value + token.len;
+		if (token.kind == C_TOKEN_TEMPLATE_ARGUMENTS)
+			break;
 	}
 
-	*len_ret = token.value + token.len - identifier;
+	*len_ret = end - identifier;
 	return NULL;
 }
 

tests/test_program.py
@@ -1026,6 +1026,21 @@ class TestTypes(MockProgramTestCase):
             ),
         )
 
+    def test_pointer_to_class(self):
+        class_point = self.prog.class_type(
+            "Point",
+            8,
+            (
+                TypeMember(self.prog.int_type("int", 4, True), "x", 0),
+                TypeMember(self.prog.int_type("int", 4, True), "y", 32),
+            ),
+        )
+        class_point_p = self.prog.pointer_type(class_point)
+        self.types.append(class_point)
+        self.prog.language = Language.CPP
+        self.assertIdentical(self.prog.type("class Point *"), class_point_p)
+        self.assertIdentical(self.prog.type("Point *"), class_point_p)
+
 
 class TestObjects(MockProgramTestCase):
     def test_constant(self):