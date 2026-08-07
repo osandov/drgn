osandov/drgn

commit d35243b354688e8ffec053a5cf55c92e38da9aa1

libdrgn: replace lazy types with lazy objects
In order to support static members, methods, default function arguments,
and value template parameters, we need to be able to store a drgn_object
in a drgn_type_member or drgn_type_parameter. These are all cases where
we want lazy evaluation, so we can replace drgn_lazy_type with a new
drgn_lazy_object which implements the same idea but for objects. Types
can still be represented with an absent object.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

tests/test_type.py
@@ -5,6 +5,7 @@ import operator
 
 from drgn import (
     Language,
+    Object,
     PrimitiveType,
     Program,
     Qualifiers,
@@ -143,7 +144,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t),
-            "prog.struct_type(tag='point', size=8, members=(TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='x', bit_offset=0), TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='y', bit_offset=32)))",
+            "prog.struct_type(tag='point', size=8, members=(TypeMember(prog.type('int'), name='x', bit_offset=0), TypeMember(prog.type('int'), name='y', bit_offset=32)))",
         )
         self.assertEqual(sizeof(t), 8)
 
@@ -216,15 +217,39 @@ class TestType(MockProgramTestCase):
             "point",
             8,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "y", 32, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "y",
+                    32,
+                ),
             ),
         )
         self.assertIdentical(
             t.members,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "y", 32, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "y",
+                    32,
+                ),
             ),
         )
 
@@ -253,7 +278,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t),
-            "prog.union_type(tag='option', size=4, members=(TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='x', bit_offset=0), TypeMember(type=prog.int_type(name='unsigned int', size=4, is_signed=False), name='y', bit_offset=0)))",
+            "prog.union_type(tag='option', size=4, members=(TypeMember(prog.type('int'), name='x', bit_offset=0), TypeMember(prog.type('unsigned int'), name='y', bit_offset=0)))",
         )
         self.assertEqual(sizeof(t), 4)
 
@@ -326,15 +351,43 @@ class TestType(MockProgramTestCase):
             "option",
             4,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("unsigned int", 4, False), "y", 0, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog,
+                        self.prog.int_type("unsigned int", 4, False),
+                        bit_field_size=4,
+                    ),
+                    "y",
+                    0,
+                ),
             ),
         )
         self.assertIdentical(
             t.members,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("unsigned int", 4, False), "y", 0, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog,
+                        self.prog.int_type("unsigned int", 4, False),
+                        bit_field_size=4,
+                    ),
+                    "y",
+                    0,
+                ),
             ),
         )
 
@@ -365,7 +418,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t),
-            "prog.class_type(tag='coord', size=12, members=(TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='x', bit_offset=0), TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='y', bit_offset=32), TypeMember(type=prog.int_type(name='int', size=4, is_signed=True), name='z', bit_offset=64)))",
+            "prog.class_type(tag='coord', size=12, members=(TypeMember(prog.type('int'), name='x', bit_offset=0), TypeMember(prog.type('int'), name='y', bit_offset=32), TypeMember(prog.type('int'), name='z', bit_offset=64)))",
         )
         self.assertEqual(sizeof(t), 12)
 
@@ -440,17 +493,53 @@ class TestType(MockProgramTestCase):
             "coord",
             12,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "y", 32, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "z", 64, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "y",
+                    32,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "z",
+                    64,
+                ),
             ),
         )
         self.assertIdentical(
             t.members,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "y", 32, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "z", 64, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "y",
+                    32,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "z",
+                    64,
+                ),
             ),
         )
 
@@ -501,8 +590,20 @@ class TestType(MockProgramTestCase):
             "small_point",
             1,
             (
-                TypeMember(self.prog.int_type("int", 4, True), "x", 0, 4),
-                TypeMember(self.prog.int_type("int", 4, True), "y", 4, 4),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "x",
+                    0,
+                ),
+                TypeMember(
+                    Object(
+                        self.prog, self.prog.int_type("int", 4, True), bit_field_size=4
+                    ),
+                    "y",
+                    4,
+                ),
             ),
         )
         self.assertEqual(offsetof(small_point_type, "x"), 0)
@@ -731,7 +832,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t),
-            "prog.function_type(type=prog.void_type(), parameters=(TypeParameter(type=prog.int_type(name='int', size=4, is_signed=True), name='n'),), is_variadic=False)",
+            "prog.function_type(type=prog.void_type(), parameters=(TypeParameter(prog.type('int'), name='n'),), is_variadic=False)",
         )
         self.assertRaises(TypeError, sizeof, t)
 
@@ -781,7 +882,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t1),
-            "prog.struct_type(tag='foo', size=8, members=(TypeMember(type=prog.pointer_type(type=prog.struct_type(tag='foo', ...)), name='next', bit_offset=0),))",
+            "prog.struct_type(tag='foo', size=8, members=(TypeMember(prog.type('struct foo *'), name='next', bit_offset=0),))",
         )
 
     def test_cycle2(self):
@@ -805,7 +906,7 @@ class TestType(MockProgramTestCase):
 
         self.assertEqual(
             repr(t1),
-            "prog.struct_type(tag='list_head', size=16, members=(TypeMember(type=prog.pointer_type(type=prog.struct_type(tag='list_head', ...)), name='next', bit_offset=0), TypeMember(type=prog.pointer_type(type=prog.struct_type(tag='list_head', ...)), name='prev', bit_offset=8)))",
+            "prog.struct_type(tag='list_head', size=16, members=(TypeMember(prog.type('struct list_head *'), name='next', bit_offset=0), TypeMember(prog.type('struct list_head *'), name='prev', bit_offset=8)))",
         )
 
     def test_bad_thunk(self):
@@ -815,7 +916,9 @@ class TestType(MockProgramTestCase):
         with self.assertRaisesRegex(Exception, "test"):
             t1.members[0].type
         t1 = self.prog.struct_type("foo", 16, (TypeMember(lambda: 0, "bar"),))
-        with self.assertRaisesRegex(TypeError, "type callable must return Type"):
+        with self.assertRaisesRegex(
+            TypeError, r"TypeMember\(\) callable must return Object or Type"
+        ):
             t1.members[0].type
 
     def test_qualifiers(self):
@@ -866,7 +969,7 @@ class TestType(MockProgramTestCase):
     def test_different_programs_compound(self):
         self.assertRaisesRegex(
             ValueError,
-            "type is from different program",
+            "object is from different program",
             self.prog.struct_type,
             None,
             4,
@@ -874,7 +977,7 @@ class TestType(MockProgramTestCase):
         )
 
     def test_different_programs_compound_callback(self):
-        with self.assertRaisesRegex(ValueError, "type is from different program"):
+        with self.assertRaisesRegex(ValueError, "objects are from different program"):
             self.prog.struct_type(
                 None, 4, (TypeMember(lambda: Program().int_type("int", 4, True)),)
             ).members[0].type
@@ -926,14 +1029,14 @@ class TestType(MockProgramTestCase):
     def test_different_programs_function_parameter(self):
         self.assertRaisesRegex(
             ValueError,
-            "type is from different program",
+            "object is from different program",
             self.prog.function_type,
             self.prog.void_type(),
             (TypeParameter(Program().int_type("int", 4, True)),),
         )
 
     def test_different_programs_function_parameter_callback(self):
-        with self.assertRaisesRegex(ValueError, "type is from different program"):
+        with self.assertRaisesRegex(ValueError, "objects are from different programs"):
             self.prog.function_type(
                 self.prog.void_type(),
                 (TypeParameter(lambda: Program().int_type("int", 4, True)),),
@@ -969,13 +1072,15 @@ class TestTypeEnumerator(MockProgramTestCase):
 class TestTypeMember(MockProgramTestCase):
     def test_init(self):
         m = TypeMember(self.prog.void_type())
+        self.assertIdentical(m.object, Object(self.prog, self.prog.void_type()))
         self.assertIdentical(m.type, self.prog.void_type())
         self.assertIsNone(m.name)
         self.assertEqual(m.bit_offset, 0)
         self.assertEqual(m.offset, 0)
         self.assertIsNone(m.bit_field_size)
 
-        m = TypeMember(self.prog.void_type(), "foo")
+        m = TypeMember(Object(self.prog, self.prog.void_type()), "foo")
+        self.assertIdentical(m.object, Object(self.prog, self.prog.void_type()))
         self.assertIdentical(m.type, self.prog.void_type())
         self.assertEqual(m.name, "foo")
         self.assertEqual(m.bit_offset, 0)
@@ -983,46 +1088,39 @@ class TestTypeMember(MockProgramTestCase):
         self.assertIsNone(m.bit_field_size)
 
         m = TypeMember(self.prog.void_type(), "foo", 8)
+        self.assertIdentical(m.object, Object(self.prog, self.prog.void_type()))
         self.assertIdentical(m.type, self.prog.void_type())
         self.assertEqual(m.name, "foo")
         self.assertEqual(m.bit_offset, 8)
         self.assertEqual(m.offset, 1)
         self.assertIsNone(m.bit_field_size)
 
-        m = TypeMember(self.prog.void_type(), "foo", 9, 7)
-        self.assertIdentical(m.type, self.prog.void_type())
-        self.assertEqual(m.name, "foo")
-        self.assertEqual(m.bit_offset, 9)
-        self.assertRaises(ValueError, getattr, m, "offset")
-        self.assertEqual(m.bit_field_size, 7)
-
         self.assertRaises(TypeError, TypeMember, None)
         self.assertRaises(TypeError, TypeMember, self.prog.void_type(), 1)
         self.assertRaises(TypeError, TypeMember, self.prog.void_type(), "foo", None)
-        self.assertRaises(TypeError, TypeMember, self.prog.void_type(), "foo", 0, "bar")
-        self.assertRaises(ValueError, TypeMember, self.prog.void_type(), "foo", 0, 0)
 
     def test_callable(self):
         m = TypeMember(self.prog.void_type)
+        self.assertIdentical(m.object, Object(self.prog, self.prog.void_type()))
         self.assertIdentical(m.type, self.prog.void_type())
 
-        m = TypeMember(lambda: self.prog.int_type("int", 4, True))
+        m = TypeMember(lambda: Object(self.prog, self.prog.int_type("int", 4, True)))
+        self.assertIdentical(
+            m.object, Object(self.prog, self.prog.int_type("int", 4, True))
+        )
         self.assertIdentical(m.type, self.prog.int_type("int", 4, True))
 
         m = TypeMember(lambda: None)
         self.assertRaises(TypeError, getattr, m, "type")
 
     def test_repr(self):
-        m = TypeMember(type=self.prog.void_type, name="foo")
+        m = TypeMember(self.prog.void_type, name="foo")
         self.assertEqual(
-            repr(m), "TypeMember(type=prog.void_type(), name='foo', bit_offset=0)"
+            repr(m), "TypeMember(prog.type('void'), name='foo', bit_offset=0)"
         )
 
-        m = TypeMember(type=self.prog.void_type, bit_field_size=4)
-        self.assertEqual(
-            repr(m),
-            "TypeMember(type=prog.void_type(), name=None, bit_offset=0, bit_field_size=4)",
-        )
+        m = TypeMember(self.prog.void_type)
+        self.assertEqual(repr(m), "TypeMember(prog.type('void'), bit_offset=0)")
 
         m = TypeMember(lambda: None)
         self.assertRaises(TypeError, repr, m)
@@ -1031,11 +1129,19 @@ class TestTypeMember(MockProgramTestCase):
 class TestTypeParameter(MockProgramTestCase):
     def test_init(self):
         p = TypeParameter(self.prog.void_type())
+        self.assertIdentical(
+            p.default_argument, Object(self.prog, self.prog.void_type())
+        )
         self.assertIdentical(p.type, self.prog.void_type())
         self.assertIsNone(p.name)
 
-        p = TypeParameter(self.prog.void_type(), "foo")
-        self.assertIdentical(p.type, self.prog.void_type())
+        p = TypeParameter(
+            Object(self.prog, self.prog.int_type("int", 4, True), 5), "foo"
+        )
+        self.assertIdentical(
+            p.default_argument, Object(self.prog, self.prog.int_type("int", 4, True), 5)
+        )
+        self.assertIdentical(p.type, self.prog.int_type("int", 4, True))
         self.assertEqual(p.name, "foo")
 
         self.assertRaises(TypeError, TypeParameter, None)
@@ -1043,20 +1149,26 @@ class TestTypeParameter(MockProgramTestCase):
 
     def test_callable(self):
         p = TypeParameter(self.prog.void_type)
+        self.assertIdentical(
+            p.default_argument, Object(self.prog, self.prog.void_type())
+        )
         self.assertIdentical(p.type, self.prog.void_type())
 
-        p = TypeParameter(lambda: self.prog.int_type("int", 4, True))
+        p = TypeParameter(lambda: Object(self.prog, self.prog.int_type("int", 4, True)))
+        self.assertIdentical(
+            p.default_argument, Object(self.prog, self.prog.int_type("int", 4, True))
+        )
         self.assertIdentical(p.type, self.prog.int_type("int", 4, True))
 
         p = TypeParameter(lambda: None)
         self.assertRaises(TypeError, getattr, p, "type")
 
     def test_repr(self):
-        p = TypeParameter(type=self.prog.void_type, name="foo")
-        self.assertEqual(repr(p), "TypeParameter(type=prog.void_type(), name='foo')")
+        p = TypeParameter(self.prog.void_type, name="foo")
+        self.assertEqual(repr(p), "TypeParameter(prog.type('void'), name='foo')")
 
-        p = TypeParameter(type=self.prog.void_type)
-        self.assertEqual(repr(p), "TypeParameter(type=prog.void_type(), name=None)")
+        p = TypeParameter(self.prog.void_type)
+        self.assertEqual(repr(p), "TypeParameter(prog.type('void'))")
 
         p = TypeParameter(lambda: None)
         self.assertRaises(TypeError, repr, p)

commit a2899389cd19be812c2544edd86e6a4f8a1364ad

libdrgn: python: fix thunk and callable reentrancy in LazyObject_get_borrowed()
There are two bugs in reentrant calls to LazyObject_get_borrowed() from
Python callables.

The first is that in the thunk case, we read self->lazy_obj again after
drgn_lazy_object_evaluate() returns. The inner call sets self->lazy_obj
to the DRGNPY_LAZY_OBJECT_EVALUATED sentinel, so when the outer call
resumes, self->lazy_obj->obj.type is NULL and drgn_object_program()
dereferences NULL. Fix that by saving the lazy object pointer before
evaluating. Also hold a reference to self->obj, since in the thunk case
it is the parent DrgnType that owns the drgn_lazy_object.

The second is that in the callable case, we call the callable through a
borrowed reference to self->obj, which the inner call replaces and drops
the last reference to. It seems like this won't crash in CPython in
practice, but it's still technically incorrect. Holding a reference to
self->obj fixes this, too.

The inner call still gets the void placeholder that
drgn_lazy_object_evaluate() initializes before running the thunk. We
don't want to guarantee that the inner call evaluates the object again,
just that it doesn't crash. Also weaken the similar
test_callable_reentrant(): the pure Python path happens to reevaluate
the object, but we don't want to guarantee that.

Fixes: d35243b35468 ("libdrgn: replace lazy types with lazy objects")
Fixes: 26ef465007e9 ("libdrgn/python: add proper type for members and parameters")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/python/type.c
@@ -890,16 +890,20 @@ static const char *PyType_name(PyTypeObject *type)
 static DrgnObject *LazyObject_get_borrowed(LazyObject *self)
 {
 	if (unlikely(self->lazy_obj != DRGNPY_LAZY_OBJECT_EVALUATED)) {
-		DrgnObject *obj;
+		// We need to keep this alive to protect against reentrant
+		// calls.
+		_cleanup_pydecref_ PyObject *obj = self->obj;
+		Py_INCREF(obj);
+		DrgnObject *res;
 		if (self->lazy_obj == DRGNPY_LAZY_OBJECT_CALLABLE) {
-			PyObject *ret = PyObject_CallObject(self->obj, NULL);
+			PyObject *ret = PyObject_CallObject(obj, NULL);
 			if (!ret)
 				return NULL;
 			if (PyObject_TypeCheck(ret, &DrgnObject_type)) {
-				obj = (DrgnObject *)ret;
+				res = (DrgnObject *)ret;
 				if (Py_TYPE(self) ==
 				    &TypeTemplateParameter_type &&
-				    obj->obj.kind == DRGN_OBJECT_ABSENT) {
+				    res->obj.kind == DRGN_OBJECT_ABSENT) {
 					Py_DECREF(ret);
 					PyErr_Format(PyExc_ValueError,
 						     "%s() callable must not return absent Object",
@@ -907,9 +911,9 @@ static DrgnObject *LazyObject_get_borrowed(LazyObject *self)
 					return NULL;
 				}
 			} else if (PyObject_TypeCheck(ret, &DrgnType_type)) {
-				obj = DrgnType_to_absent_DrgnObject((DrgnType *)ret);
+				res = DrgnType_to_absent_DrgnObject((DrgnType *)ret);
 				Py_DECREF(ret);
-				if (!obj)
+				if (!res)
 					return NULL;
 			} else {
 				Py_DECREF(ret);
@@ -919,24 +923,27 @@ static DrgnObject *LazyObject_get_borrowed(LazyObject *self)
 				return NULL;
 			}
 		} else {
+			// Evaluating the thunk may run Python code that
+			// reenters this function and replaces this.
+			union drgn_lazy_object *lazy_obj = self->lazy_obj;
 			struct drgn_error *err =
-				drgn_lazy_object_evaluate(self->lazy_obj);
+				drgn_lazy_object_evaluate(lazy_obj);
 			if (err)
 				return set_drgn_error(err);
-			obj = DrgnObject_alloc(container_of(drgn_object_program(&self->lazy_obj->obj),
+			res = DrgnObject_alloc(container_of(drgn_object_program(&lazy_obj->obj),
 							    Program, prog));
-			if (!obj)
+			if (!res)
 				return NULL;
-			err = drgn_object_copy(&obj->obj, &self->lazy_obj->obj);
+			err = drgn_object_copy(&res->obj, &lazy_obj->obj);
 			if (err) {
-				Py_DECREF(obj);
+				Py_DECREF(res);
 				return set_drgn_error(err);
 			}
 		}
-		// lazy_obj must be updated before obj to guard against
-		// reentrant calls.
+		// Deallocating self->obj may reenter this function, so we need
+		// to update lazy_obj first.
 		self->lazy_obj = DRGNPY_LAZY_OBJECT_EVALUATED;
-		Py_SETREF(self->obj, (PyObject *)obj);
+		Py_SETREF(self->obj, (PyObject *)res);
 	}
 	return (DrgnObject *)self->obj;
 }

tests/test_type.py
@@ -1230,10 +1230,28 @@ class TestTypeMember(MockProgramTestCase):
             def __call__(self2):
                 if self2.first:
                     self2.first = False
-                    return member.object
+                    member.object
                 return Object(self.prog, self.prog.int_type("int", 4, True))
 
         member = TypeMember(Callable())
+        self.assertIdentical(
+            member.object, Object(self.prog, self.prog.int_type("int", 4, True))
+        )
+
+    def test_thunk_reentrant(self):
+        # Test a callable that reenters the object evaluation through the
+        # drgn_lazy_object thunk rather than the callable directly.
+        class Callable:
+            def __init__(self2):
+                self2.first = True
+
+            def __call__(self2):
+                if self2.first:
+                    self2.first = False
+                    member.object
+                return Object(self.prog, self.prog.int_type("int", 4, True))
+
+        member = self.prog.struct_type("foo", 8, [TypeMember(Callable())]).members[0]
         self.assertIdentical(
             member.object, Object(self.prog, self.prog.int_type("int", 4, True))
         )