osandov/drgn

commit 55a3ebca6c484a4bc47a183dacb691fc74f85fb2

libdrgn: dwarf_info: support DWO split DWARF
We've addressed all of the smaller differences with GNU Debug Fission
and split DWARF 5, so now all that remains is the DWARF index.

The general approach is: in drgn_dwarf_index_read_cus(), for each CU,
ask libdw for the "sub-DIE". For skeleton CUs, this is the split CU DIE
from the .dwo file. From that Dwarf_Die, we can get the Dwarf_CU and
then the Dwarf handle. Then, we wrap that in a struct drgn_elf_file
(cached in a hash table in the struct drgn_module), which the DWARF
index can work with from there.

Additionally, a couple of places (.debug_addr parsing and stack trace
local variable lookup) need to be updated to use the correct
drgn_elf_file.

Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -25,6 +25,12 @@
 #include "program.h"
 #include "util.h"
 
+static inline Dwarf *drgn_elf_file_dwarf_key(struct drgn_elf_file * const *entry)
+{
+	return (*entry)->dwarf;
+}
+DEFINE_HASH_TABLE_FUNCTIONS(drgn_elf_file_dwarf_table, drgn_elf_file_dwarf_key,
+			    ptr_key_hash_pair, scalar_key_eq)
 DEFINE_VECTOR_FUNCTIONS(drgn_module_vector)
 
 struct drgn_module_key {
@@ -181,6 +187,12 @@ static void drgn_module_destroy(struct drgn_module *module)
 		if (module->fd != -1)
 			close(module->fd);
 		free(module->path);
+		for (struct drgn_elf_file_dwarf_table_iterator it =
+		     drgn_elf_file_dwarf_table_first(&module->split_dwarf_files);
+		     it.entry;
+		     it = drgn_elf_file_dwarf_table_next(it))
+			drgn_elf_file_destroy(*it.entry);
+		drgn_elf_file_dwarf_table_deinit(&module->split_dwarf_files);
 		if (module->debug_file != module->loaded_file)
 			drgn_elf_file_destroy(module->debug_file);
 		drgn_elf_file_destroy(module->loaded_file);
@@ -439,6 +451,7 @@ drgn_debug_info_report_module(struct drgn_debug_info_load_state *load,
 	module->path = path_key;
 	module->fd = fd;
 	module->elf = elf;
+	drgn_elf_file_dwarf_table_init(&module->split_dwarf_files);
 
 	/* path_key, fd and elf are owned by the module now. */
 
@@ -2111,6 +2124,44 @@ void drgn_debug_info_destroy(struct drgn_debug_info *dbinfo)
 	drgn_module_table_deinit(&dbinfo->modules);
 	dwfl_end(dbinfo->dwfl);
 	free(dbinfo);
+}
+
+struct drgn_elf_file *drgn_module_find_dwarf_file(struct drgn_module *module,
+						  Dwarf *dwarf)
+{
+	if (!module->debug_file)
+		return NULL;
+	if (dwarf == module->debug_file->dwarf)
+		return module->debug_file;
+	struct drgn_elf_file_dwarf_table_iterator it =
+		drgn_elf_file_dwarf_table_search(&module->split_dwarf_files,
+						 &dwarf);
+	return it.entry ? *it.entry : NULL;
+}
+
+struct drgn_error *
+drgn_module_create_split_dwarf_file(struct drgn_module *module,
+				    const char *name, Dwarf *dwarf,
+				    struct drgn_elf_file **ret)
+{
+	struct drgn_error *err;
+	err = drgn_elf_file_create(module, name, dwarf_getelf(dwarf), ret);
+	if (err)
+		return err;
+	err = drgn_elf_file_precache_sections(*ret);
+	if (err) {
+		drgn_elf_file_destroy(*ret);
+		return err;
+	}
+	(*ret)->dwarf = dwarf;
+	int r = drgn_elf_file_dwarf_table_insert(&module->split_dwarf_files,
+						 ret, NULL);
+	if (r < 0) {
+		drgn_elf_file_destroy(*ret);
+		return &drgn_enomem;
+	}
+	assert(r > 0);
+	return NULL;
 }
 
 struct drgn_error *

libdrgn/debug_info.h
@@ -49,6 +49,8 @@ enum drgn_module_state {
 	DRGN_DEBUG_INFO_MODULE_INDEXED,
 } __attribute__((__packed__));
 
+DEFINE_HASH_TABLE_TYPE(drgn_elf_file_dwarf_table, struct drgn_elf_file *)
+
 /**
  * A module reported to a @ref drgn_debug_info.
  *
@@ -85,6 +87,8 @@ struct drgn_module {
 	 * drgn_module::debug_file.
 	 */
 	uint64_t debug_file_bias;
+
+	struct drgn_elf_file_dwarf_table split_dwarf_files;
 
 	/** DWARF debugging information. */
 	struct drgn_module_dwarf_info dwarf;
@@ -254,6 +258,14 @@ drgn_debug_info_find_object(const char *name, size_t name_len,
 			    const char *filename,
 			    enum drgn_find_object_flags flags, void *arg,
 			    struct drgn_object *ret);
+
+struct drgn_elf_file *drgn_module_find_dwarf_file(struct drgn_module *module,
+						  Dwarf *dwarf);
+
+struct drgn_error *
+drgn_module_create_split_dwarf_file(struct drgn_module *module,
+				    const char *name, Dwarf *dwarf,
+				    struct drgn_elf_file **ret);
 
 /**
  * Get the Call Frame Information in a @ref drgn_module at a given program

libdrgn/dwarf_info.c
@@ -37,6 +37,7 @@ static inline int omp_get_max_threads(void)
 #include "error.h"
 #include "language.h"
 #include "lazy_object.h"
+#include "log.h"
 #include "minmax.h"
 #include "object.h"
 #include "path.h"
@@ -508,7 +509,7 @@ void drgn_dwarf_index_state_deinit(struct drgn_dwarf_index_state *state)
 
 static struct drgn_error *
 drgn_dwarf_index_cu_set_pending(struct drgn_dwarf_index_cu *cu,
-				Dwarf_Die *cudie,
+				Dwarf_Die *skeldie, Dwarf_Die *cudie,
 				Dwarf_Off abbrev_offset)
 {
 	Elf_Data *debug_abbrev = cu->file->scn_data[DRGN_SCN_DEBUG_ABBREV];
@@ -523,12 +524,15 @@ drgn_dwarf_index_cu_set_pending(struct drgn_dwarf_index_cu *cu,
 
 	Dwarf_Attribute attr_mem, *attr;
 
-	if ((attr = dwarf_attr(cudie, DW_AT_stmt_list, &attr_mem))) {
-		Elf_Data *debug_line = cu->file->scn_data[DRGN_SCN_DEBUG_LINE];
+	// For split DWARF, DW_AT_stmt_list, DW_AT_comp_dir, and .debug_line are
+	// in the skeleton file.
+	if ((attr = dwarf_attr(skeldie, DW_AT_stmt_list, &attr_mem))) {
+		struct drgn_elf_file *skelfile = cu->file->module->debug_file;
+		Elf_Data *debug_line = skelfile->scn_data[DRGN_SCN_DEBUG_LINE];
 		if (!debug_line) {
-			return drgn_elf_file_section_error(cu->file,
-							   cu->file->scns[cu->scn],
-							   cu->file->scn_data[cu->scn],
+			return drgn_elf_file_section_error(skelfile,
+							   skelfile->scns[cu->scn],
+							   skelfile->scn_data[cu->scn],
 							   (char *)attr->valp,
 							   "DW_AT_stmt_list without .debug_line section");
 		}
@@ -538,16 +542,16 @@ drgn_dwarf_index_cu_set_pending(struct drgn_dwarf_index_cu *cu,
 			return drgn_error_libdw();
 
 		if (stmt_list > debug_line->d_size) {
-			return drgn_elf_file_section_error(cu->file,
-							   cu->file->scns[cu->scn],
-							   cu->file->scn_data[cu->scn],
+			return drgn_elf_file_section_error(skelfile,
+							   skelfile->scns[cu->scn],
+							   skelfile->scn_data[cu->scn],
 							   (char *)attr->valp,
 							   "DW_AT_stmt_list is out of bounds");
 		}
 
 		cu->pending.stmt_list = (char *)debug_line->d_buf + stmt_list;
 
-		if ((attr = dwarf_attr(cudie, DW_AT_comp_dir, &attr_mem))) {
+		if ((attr = dwarf_attr(skeldie, DW_AT_comp_dir, &attr_mem))) {
 			cu->pending.comp_dir = dwarf_formstring(attr);
 			if (!cu->pending.comp_dir)
 				return drgn_error_libdw();
@@ -592,6 +596,15 @@ drgn_dwarf_index_cu_set_pending(struct drgn_dwarf_index_cu *cu,
 	return NULL;
 }
 
+static const char *drgn_dwarf_dwo_name(Dwarf_Die *die)
+{
+	Dwarf_Attribute attr_mem, *attr;
+	if ((attr = dwarf_attr(die, DW_AT_dwo_name, &attr_mem))
+	    || (attr = dwarf_attr(die, DW_AT_GNU_dwo_name, &attr_mem)))
+		return dwarf_formstring(attr);
+	return NULL;
+}
+
 static struct drgn_error *
 drgn_dwarf_index_read_cus(struct drgn_dwarf_index_state *state,
 			  struct drgn_elf_file *file,
@@ -616,48 +629,93 @@ drgn_dwarf_index_read_cus(struct drgn_dwarf_index_state *state,
 				      &version, &abbrev_offset, &address_size,
 				      &offset_size, v4_type_signaturep,
 				      NULL)) == 0) {
-		Dwarf_Die cudie;
+		Dwarf_Die skeldie, cudie;
 		if (scn == DRGN_SCN_DEBUG_TYPES) {
 			if (!dwarf_offdie_types(file->dwarf, off + header_size,
-						&cudie))
+						&skeldie))
 				return drgn_error_libdw();
 		} else {
 			if (!dwarf_offdie(file->dwarf, off + header_size,
-					  &cudie))
+					  &skeldie))
 				return drgn_error_libdw();
 		}
+		struct drgn_elf_file *cu_file = file;
+		Dwarf_Off cu_start_off = off, cu_end_off = next_off;
 		uint8_t unit_type;
 #if _ELFUTILS_PREREQ(0, 171)
-		if (dwarf_cu_info(cudie.cu, NULL, &unit_type, &cudie, NULL,
-				  NULL, NULL, NULL))
+		if (dwarf_cu_info(skeldie.cu, NULL, &unit_type, &skeldie,
+				  &cudie, NULL, NULL, NULL))
 			return drgn_error_libdw();
+
+		if (unit_type == DW_UT_skeleton && cudie.cu) {
+			Dwarf *split_dwarf = dwarf_cu_getdwarf(cudie.cu);
+			cu_file = drgn_module_find_dwarf_file(file->module,
+							      split_dwarf);
+			if (!cu_file) {
+				const char *dwo_name =
+					drgn_dwarf_dwo_name(&skeldie);
+				if (!dwo_name)
+					dwo_name = "";
+				err = drgn_module_create_split_dwarf_file(file->module,
+									  dwo_name,
+									  split_dwarf,
+									  &cu_file);
+				if (err)
+					return err;
+			}
+
+			cu_start_off = (dwarf_dieoffset(&cudie)
+					- dwarf_cuoffset(&cudie));
+			if (dwarf_next_unit(split_dwarf, cu_start_off,
+					    &cu_end_off, NULL, &version,
+					    &abbrev_offset, &address_size,
+					    &offset_size, v4_type_signaturep,
+					    NULL)
+			    || dwarf_cu_info(cudie.cu, NULL, &unit_type, NULL,
+					     NULL, NULL, NULL, NULL))
+				return drgn_error_libdw();
+		} else {
+			if (unit_type == DW_UT_skeleton
+			    && drgn_log_is_enabled(state->dbinfo->prog,
+						   DRGN_LOG_WARNING)) {
+				const char *dwo_name =
+					drgn_dwarf_dwo_name(&skeldie);
+				drgn_log_warning(state->dbinfo->prog,
+						 "%s: split DWARF file%s%s not found",
+						 file->path ?: "",
+						 dwo_name ? " " : "",
+						 dwo_name ? dwo_name : "");
+			}
+			cudie = skeldie;
+		}
 #else
 		unit_type = (scn == DRGN_SCN_DEBUG_TYPES
 			     ? DW_UT_type : DW_UT_compile);
+		cudie = skeldie;
 #endif
 
-		if (!elf_data_contains_ptr(file->scn_data[scn], cudie.addr)) {
-			return drgn_elf_file_section_error(file, NULL, NULL,
+		if (!elf_data_contains_ptr(cu_file->scn_data[scn],
+					   cudie.addr)) {
+			return drgn_elf_file_section_error(cu_file, NULL, NULL,
 							   cudie.addr,
 							   "unit DIE from unexpected section");
 		}
-		Dwarf_Off end_off = next_off;
-		if (end_off > file->scn_data[scn]->d_size)
-			end_off = file->scn_data[scn]->d_size;
+		if (cu_end_off > cu_file->scn_data[scn]->d_size)
+			cu_end_off = cu_file->scn_data[scn]->d_size;
 		const char *cu_buf =
-			(char *)file->scn_data[scn]->d_buf + off;
+			(char *)cu_file->scn_data[scn]->d_buf + cu_start_off;
 		if (version < 2 || version > 5) {
-			return drgn_elf_file_section_errorf(file,
-							    file->scns[scn],
-							    file->scn_data[scn],
+			return drgn_elf_file_section_errorf(cu_file,
+							    cu_file->scns[scn],
+							    cu_file->scn_data[scn],
 							    cu_buf,
 							    "unknown DWARF unit version %" PRIu16,
 							    version);
 		}
 		if (address_size > 8) {
-			return drgn_elf_file_section_errorf(file,
-							    file->scns[scn],
-							    file->scn_data[scn],
+			return drgn_elf_file_section_errorf(cu_file,
+							    cu_file->scns[scn],
+							    cu_file->scn_data[scn],
 							    cu_buf,
 							    "unsupported DWARF unit address size %" PRIu8,
 							    address_size);
@@ -668,9 +726,9 @@ drgn_dwarf_index_read_cus(struct drgn_dwarf_index_state *state,
 		if (!cu)
 			return &drgn_enomem;
 		*cu = (struct drgn_dwarf_index_cu){
-			.file = file,
+			.file = cu_file,
 			.buf = cu_buf,
-			.len = end_off - off,
+			.len = cu_end_off - cu_start_off,
 			.version = version,
 			.unit_type = unit_type,
 			.address_size = address_size,
@@ -678,7 +736,7 @@ drgn_dwarf_index_read_cus(struct drgn_dwarf_index_state *state,
 			.scn = scn,
 		};
 
-		err = drgn_dwarf_index_cu_set_pending(cu, &cudie,
+		err = drgn_dwarf_index_cu_set_pending(cu, &skeldie, &cudie,
 						      abbrev_offset);
 		if (err)
 			return err;
@@ -1136,7 +1194,9 @@ static struct drgn_error *read_file_name_table(struct path_hash_cache *cache,
 		return NULL;
 
 	struct drgn_elf_file_section_buffer buffer;
-	drgn_elf_file_section_buffer_init_index(&buffer, cu->file,
+	// For split DWARF, .debug_line is in the skeleton file.
+	drgn_elf_file_section_buffer_init_index(&buffer,
+						cu->file->module->debug_file,
 						DRGN_SCN_DEBUG_LINE);
 	buffer.bb.pos = stmt_list;
 
@@ -3429,6 +3489,9 @@ static struct drgn_error *drgn_dwarf_next_addrx(struct binary_buffer *bb,
 						uint64_t *ret)
 {
 	struct drgn_error *err;
+
+	// For split DWARF, .debug_addr is in the main debug file.
+	file = file->module->debug_file;
 
 	// addr_base is a cache of the address table base for the compilation
 	// unit.

libdrgn/stack_trace.c
@@ -483,10 +483,16 @@ not_found:;
 		}
 	}
 
+	const struct drgn_register_state *regs = frame->regs;
+	struct drgn_elf_file *file =
+		drgn_module_find_dwarf_file(regs->module,
+					    dwarf_cu_getdwarf(die.cu));
+	if (!file) {
+		return drgn_error_create(DRGN_ERROR_OTHER,
+					 "couldn't find file containing DIE");
+	}
 	// It doesn't make sense to use the registers if the file has a
 	// different platform than the program.
-	const struct drgn_register_state *regs = frame->regs;
-	struct drgn_elf_file *file = regs->module->debug_file;
 	if (!drgn_platforms_equal(&file->platform, &trace->prog->platform))
 		regs = NULL;
 	Dwarf_Die function_die = frame->scopes[frame->function_scope];

tests/dwarfwriter.py
@@ -31,6 +31,7 @@ class DwarfDie(NamedTuple):
 class DwarfUnit(NamedTuple):
     type: DW_UT
     die: DwarfDie
+    dwo_id: Optional[int] = None
     type_signature: Optional[int] = None
     type_offset: Optional[str] = None
 
@@ -155,6 +156,10 @@ def _compile_debug_info(units, little_endian, bits, version, use_dw_form_indirec
         if version < 5:
             buf.append(bits // 8)  # address_size
 
+        if version >= 5 and unit.type in (DW_UT.skeleton, DW_UT.split_compile):
+            buf.extend(unit.dwo_id.to_bytes(8, byteorder))  # dwo_id
+        else:
+            assert unit.dwo_id is None
         if unit.type in (DW_UT.type, DW_UT.split_type):
             buf.extend(unit.type_signature.to_bytes(8, byteorder))  # type_signature
             relocations.append((len(buf), unit.type_offset))
@@ -302,8 +307,10 @@ def dwarf_sections(
     lang=None,
     use_dw_form_indirect=False,
     compress=None,
+    split=None,
 ):
     assert compress in (None, "zlib-gnu", "zlib-gabi")
+    assert split in (None, "dwo")
 
     if isinstance(units_or_dies, (DwarfDie, DwarfUnit)):
         units_or_dies = (units_or_dies,)
@@ -332,7 +339,12 @@ def dwarf_sections(
         for unit in units
     ]
 
-    debug_line = _compile_debug_line(units, little_endian, bits, version)
+    # TODO: line number information for a split file is in the skeleton file.
+    # We don't have any test cases yet that use line number information from a
+    # split file, but when we do, we'll have to add a way to include the split
+    # file's line number information in the skeleton file.
+    if not split:
+        debug_line = _compile_debug_line(units, little_endian, bits, version)
 
     debug_info, debug_types = _compile_debug_info(
         units, little_endian, bits, version, use_dw_form_indirect
@@ -346,6 +358,8 @@ def dwarf_sections(
             compressed_data.extend(len(data).to_bytes(8, "big"))
             compressed_data.extend(zlib.compress(data))
             data = compressed_data
+        if split:
+            name += ".dwo"
         return ElfSection(
             name=name,
             sh_type=SHT.PROGBITS,
@@ -359,9 +373,10 @@ def dwarf_sections(
             ".debug_abbrev", _compile_debug_abbrev(units, use_dw_form_indirect)
         ),
         debug_section(".debug_info", debug_info),
-        debug_section(".debug_line", debug_line),
         debug_section(".debug_str", b"\0"),
     ]
+    if not split:
+        sections.append(debug_section(".debug_line", debug_line))
     if debug_types:
         sections.append(debug_section(".debug_types", debug_types))
     return sections
@@ -376,6 +391,7 @@ def compile_dwarf(
     lang=None,
     use_dw_form_indirect=False,
     compress=None,
+    split=None,
 ):
     return create_elf_file(
         ET.EXEC,
@@ -387,6 +403,7 @@ def compile_dwarf(
             lang=lang,
             use_dw_form_indirect=use_dw_form_indirect,
             compress=compress,
+            split=split,
         ),
         little_endian=little_endian,
         bits=bits,

tests/test_dwarf.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import functools
+import logging
 import operator
 import os.path
 import re
@@ -6619,3 +6620,253 @@ class TestCompressedDebugSections(TestCase):
     def test_zlib_gabi(self):
         prog = dwarf_program(wrap_test_type_dies(int_die), compress="zlib-gabi")
         self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
+
+
+class TestSplitDwarf(TestCase):
+    def test_dwo4(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "split.dwo"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.split_compile,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_id,
+                                        DW_FORM.data8,
+                                        0xDDEEAADDBBEEFFFF,
+                                    ),
+                                ),
+                                wrap_test_type_dies(int_die),
+                            ),
+                        ),
+                        split="dwo",
+                    )
+                )
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_id,
+                                        DW_FORM.data8,
+                                        0xDDEEAADDBBEEFFFF,
+                                    ),
+                                ),
+                            ),
+                        )
+                    )
+                )
+            prog.load_debug_info([f.name])
+            self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
+
+    def test_dwo4_not_found(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_id,
+                                        DW_FORM.data8,
+                                        0xDDEEAADDBBEEFFFF,
+                                    ),
+                                ),
+                            ),
+                        )
+                    )
+                )
+            with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
+                prog.load_debug_info([f.name])
+            self.assertTrue(
+                any(
+                    "split DWARF file split.dwo not found" in output
+                    for output in log.output
+                )
+            )
+
+    def test_dwo4_id_mismatch(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "split.dwo"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.split_compile,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_id,
+                                        DW_FORM.data8,
+                                        0xBBBBBBBB00000000,
+                                    ),
+                                ),
+                            ),
+                        ),
+                        split="dwo",
+                    )
+                )
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                    DwarfAttrib(
+                                        DW_AT.GNU_dwo_id,
+                                        DW_FORM.data8,
+                                        0xDDEEAADDBBEEFFFF,
+                                    ),
+                                ),
+                            ),
+                        )
+                    )
+                )
+            with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
+                prog.load_debug_info([f.name])
+            self.assertTrue(
+                any(
+                    "split DWARF file split.dwo not found" in output
+                    for output in log.output
+                )
+            )
+
+    def test_dwo5(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "split.dwo"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.split_compile,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (),
+                                wrap_test_type_dies(int_die),
+                            ),
+                            dwo_id=0xDDEEAADDBBEEFFFF,
+                        ),
+                        version=5,
+                        split="dwo",
+                    )
+                )
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                ),
+                            ),
+                            dwo_id=0xDDEEAADDBBEEFFFF,
+                        ),
+                        version=5,
+                    )
+                )
+            prog.load_debug_info([f.name])
+            self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
+
+    def test_dwo5_not_found(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                ),
+                            ),
+                            dwo_id=0xDDEEAADDBBEEFFFF,
+                        ),
+                        version=5,
+                    )
+                )
+            with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
+                prog.load_debug_info([f.name])
+            self.assertTrue(
+                any(
+                    "split DWARF file split.dwo not found" in output
+                    for output in log.output
+                )
+            )
+
+    def test_dwo5_id_mismatch(self):
+        prog = Program()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "split.dwo"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.split_compile,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (),
+                                wrap_test_type_dies(int_die),
+                            ),
+                            dwo_id=0xBBBBBBBB00000000,
+                        ),
+                        version=5,
+                        split="dwo",
+                    )
+                )
+            with open(os.path.join(temp_dir, "skeleton"), "wb") as f:
+                f.write(
+                    compile_dwarf(
+                        DwarfUnit(
+                            DW_UT.skeleton,
+                            DwarfDie(
+                                DW_TAG.compile_unit,
+                                (
+                                    DwarfAttrib(
+                                        DW_AT.dwo_name, DW_FORM.string, "split.dwo"
+                                    ),
+                                ),
+                            ),
+                            dwo_id=0xDDEEAADDBBEEFFFF,
+                        ),
+                        version=5,
+                    )
+                )
+            with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
+                prog.load_debug_info([f.name])
+            self.assertTrue(
+                any(
+                    "split DWARF file split.dwo not found" in output
+                    for output in log.output
+                )
+            )

commit a3f1a3691f95e8089caf0ce964b5172a49c88b86

libdrgn: debug_info: error if we can't find debug sections in split DWARF file
We try to mirror libdw's logic for finding debug sections as closely as
possible, but if we disagree and fail to find .debug_abbrev, we can
dereference a null pointer while indexing. Add a sanity check.

Fixes: 55a3ebca6c48 ("libdrgn: dwarf_info: support DWO split DWARF")
Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/debug_info.c
@@ -5761,6 +5761,14 @@ drgn_module_create_split_dwarf_file(struct drgn_module *module,
 				   ret);
 	if (err)
 		return err;
+
+	if (!drgn_elf_file_has_dwarf(*ret)) {
+		drgn_split_dwarf_elf_file_destroy(*ret);
+		return drgn_error_format(DRGN_ERROR_BAD_DATA,
+					 "%s: debug info not found in split DWARF file",
+					 name);
+	}
+
 	(*ret)->_dwarf = dwarf;
 	int r = drgn_elf_file_dwarf_table_insert(&module->split_dwarf_files,
 						 ret, NULL);