osandov/drgn

commit 4e83130008e9c28c3d775a0ab40075f2f9123720

Introduce module and debug info finder APIs
drgn currently provides limited control over how debugging information
is found. drgn has hardcoded logic for where to search for debugging
information. The most the user can do is provide a list of files for
drgn to try in addition to the default locations (with the -s CLI option
or the drgn.Program.load_debug_info() method).

The implementation is also a mess. We use libdwfl, but its data model is
slightly different from what we want, so we have to work around it or
reimplement its functionality in several places: see commits
e5874ad18a53 ("libdrgn: use libdwfl"), e6abfeac0329 ("libdrgn:
debug_info: report userspace core dump debug info ourselves"), and
1d4854a5bce7 ("libdrgn: implement optimized x86-64 ELF relocations") for
some examples. The mismatched combination of libdwfl and our own code is
difficult to maintain, and the lack of control over the whole debug info
pipeline has made it difficult to fix several longstanding issues.

The solution is a major rework removing our libdwfl dependency and
replacing it with our own model. This (huge) commit is that rework
comprising the following components:

- drgn.Module/struct drgn_module, a representation of a binary used by a
  program.
- Automatic discovery of the modules loaded in a program.
- Interfaces for manually creating and overriding modules.
- Automatic discovery of debugging information from the standard
  locations and debuginfod.
- Interfaces for custom debug info finders and for manually overriding
  debugging information.
- Tons of test cases.

A lot of care was taken to make these interfaces extremely flexible yet
cohesive. The existing interfaces are also reimplemented on top of the
new functionality to maintain backwards compatibility, with one
exception: drgn.Program.load_debug_info()/-s would previously accept
files that it didn't find loaded in the program. This turned out to be a
big footgun for users, so now this must be done explicitly (with

libdrgn/elf_file.h
@@ -43,6 +43,14 @@ struct drgn_module;
  */
 struct drgn_error *read_elf_section(Elf_Scn *scn, Elf_Data **ret);
 
+/**
+ * Truncate any bytes beyond the last null character in an ELF string table.
+ *
+ * This sets `data->d_size` so that any string table index less than
+ * `data->d_size` is guaranteed to be valid.
+ */
+void truncate_elf_string_data(Elf_Data *data);
+
 static inline bool elf_data_contains_ptr(Elf_Data *data, const void *ptr)
 {
 	uintptr_t bufi = (uintptr_t)data->d_buf;
@@ -55,11 +63,38 @@ struct drgn_elf_file {
 	/** Module using this file. */
 	struct drgn_module *module;
 	/** Filesystem path to this file. */
-	const char *path;
+	char *path;
+	/**
+	 * Memory image backing @ref elf.
+	 *
+	 * @c NULL if not backed by a memory image.
+	 */
+	char *image;
+	/**
+	 * File descriptor backing @ref elf.
+	 *
+	 * -1 if not backed by a file.
+	 */
+	int fd;
+	/** Whether the file is loadable. */
+	bool is_loadable;
+	/** Whether the file is relocatable. */
+	bool is_relocatable;
+	/** Whether the file still need to have relocations applied. */
+	bool needs_relocation;
+	/** Whether the file is a Linux kernel image (`vmlinux`). */
+	bool is_vmlinux;
 	/** libelf handle. */
 	Elf *elf;
-	/** libdw handle if we're using DWARF information from this file. */
-	Dwarf *dwarf;
+	/**
+	 * libdw handle.
+	 *
+	 * @c NULL if not yet created.
+	 *
+	 * Don't access this directly. Get it with @ref
+	 * drgn_elf_file_get_dwarf() instead.
+	 */
+	Dwarf *_dwarf;
 	/**
 	 * Platform of this file.
 	 *
@@ -86,16 +121,33 @@ struct drgn_elf_file {
 	Elf_Data *alt_debug_str_data;
 };
 
+/**
+ * Create a @ref drgn_elf_file.
+ *
+ * On success, this takes ownership of @p fd, @p image, and @p elf. @p path is
+ * copied.
+ */
 struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
-					const char *path, Elf *elf,
-					struct drgn_elf_file **ret);
+					const char *path, int fd, char *image,
+					Elf *elf, struct drgn_elf_file **ret);
 
 void drgn_elf_file_destroy(struct drgn_elf_file *file);
 
-struct drgn_error *drgn_elf_file_precache_sections(struct drgn_elf_file *file);
-
+/** Apply ELF relocations to the file if needed. */
 struct drgn_error *
-drgn_elf_file_cache_section(struct drgn_elf_file *file, enum drgn_section_index scn);
+drgn_elf_file_apply_relocations(struct drgn_elf_file *file);
+
+/**
+ * Read an indexed ELF section.
+ *
+ * This applies ELF relocations to the file first if needed.
+ */
+struct drgn_error *drgn_elf_file_read_section(struct drgn_elf_file *file,
+					      enum drgn_section_index scn,
+					      Elf_Data **ret);
+
+struct drgn_error *drgn_elf_file_get_dwarf(struct drgn_elf_file *file,
+					   Dwarf **ret);
 
 static inline bool
 drgn_elf_file_is_little_endian(const struct drgn_elf_file *file)
@@ -108,6 +160,12 @@ static inline bool drgn_elf_file_bswap(const struct drgn_elf_file *file)
 	return drgn_platform_bswap(&file->platform);
 }
 
+static inline bool
+drgn_elf_file_is_64_bit(const struct drgn_elf_file *file)
+{
+	return drgn_platform_is_64_bit(&file->platform);
+}
+
 static inline uint8_t
 drgn_elf_file_address_size(const struct drgn_elf_file *file)
 {
@@ -118,6 +176,12 @@ static inline uint64_t
 drgn_elf_file_address_mask(const struct drgn_elf_file *file)
 {
 	return drgn_platform_address_mask(&file->platform);
+}
+
+static inline bool drgn_elf_file_has_dwarf(const struct drgn_elf_file *file)
+{
+	return (file->scns[DRGN_SCN_DEBUG_INFO]
+		&& file->scns[DRGN_SCN_DEBUG_ABBREV]);
 }
 
 struct drgn_error *
@@ -156,6 +220,10 @@ drgn_elf_file_section_buffer_init(struct drgn_elf_file_section_buffer *buffer,
 	buffer->data = data;
 }
 
+/**
+ * Initialize a @ref binary_buffer for an indexed ELF section that has already
+ * been read.
+ */
 static inline void
 drgn_elf_file_section_buffer_init_index(struct drgn_elf_file_section_buffer *buffer,
 					struct drgn_elf_file *file,
@@ -164,6 +232,32 @@ drgn_elf_file_section_buffer_init_index(struct drgn_elf_file_section_buffer *buf
 	drgn_elf_file_section_buffer_init(buffer, file, file->scns[scn],
 					  file->scn_data[scn]);
 }
+
+/**
+ * Read an indexed ELF section (applying ELF relocations if needed) and
+ * initialize a @ref binary_buffer for it.
+ */
+static inline struct drgn_error *
+drgn_elf_file_section_buffer_read(struct drgn_elf_file_section_buffer *buffer,
+				  struct drgn_elf_file *file,
+				  enum drgn_section_index scn)
+{
+	Elf_Data *data;
+	struct drgn_error *err = drgn_elf_file_read_section(file, scn, &data);
+	if (err)
+		return err;
+	drgn_elf_file_section_buffer_init(buffer, file, file->scns[scn], data);
+	return NULL;
+}
+
+/**
+ * Return the virtual address range of an ELF file.
+ *
+ * @param[out] start_ret Minimum virtual address (inclusive).
+ * @param[out] end_ret Maximum virtual address (exclusive).
+ */
+bool drgn_elf_file_address_range(struct drgn_elf_file *file,
+				 uint64_t *start_ret, uint64_t *end_ret);
 
 /** @} */
 

tests/test_dwarf.py
@@ -202,12 +202,16 @@ labeled_unsigned_int_die = (DwarfLabel("unsigned_int_die"), unsigned_int_die)
 labeled_float_die = (DwarfLabel("float_die"), float_die)
 
 
+def add_extra_dwarf(prog, path):
+    prog.extra_module(path, create=True)[0].try_file(path, force=True)
+
+
 def dwarf_program(*args, segments=None, **kwds):
     prog = Program()
     with tempfile.NamedTemporaryFile() as f:
         f.write(compile_dwarf(*args, **kwds))
         f.flush()
-        prog.load_debug_info([f.name])
+        add_extra_dwarf(prog, f.name)
 
     if segments is not None:
         add_mock_memory_segments(prog, segments)
@@ -6909,7 +6913,7 @@ class TestSplitDwarf(TestCase):
                         )
                     )
                 )
-            prog.load_debug_info([f.name])
+            add_extra_dwarf(prog, f.name)
             self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
 
     def test_dwo4_not_found(self):
@@ -6937,7 +6941,12 @@ class TestSplitDwarf(TestCase):
                     )
                 )
             with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
-                prog.load_debug_info([f.name])
+                add_extra_dwarf(prog, f.name)
+                # Force debug info to be indexed.
+                try:
+                    prog["foo"]
+                except KeyError:
+                    pass
             self.assertTrue(
                 any(
                     "split DWARF file split.dwo not found" in output
@@ -6989,7 +6998,12 @@ class TestSplitDwarf(TestCase):
                     )
                 )
             with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
-                prog.load_debug_info([f.name])
+                add_extra_dwarf(prog, f.name)
+                # Force debug info to be indexed.
+                try:
+                    prog["foo"]
+                except KeyError:
+                    pass
             self.assertTrue(
                 any(
                     "split DWARF file split.dwo not found" in output
@@ -7034,7 +7048,7 @@ class TestSplitDwarf(TestCase):
                         version=5,
                     )
                 )
-            prog.load_debug_info([f.name])
+            add_extra_dwarf(prog, f.name)
             self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
 
     def test_dwo5_not_found(self):
@@ -7059,7 +7073,12 @@ class TestSplitDwarf(TestCase):
                     )
                 )
             with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
-                prog.load_debug_info([f.name])
+                add_extra_dwarf(prog, f.name)
+                # Force debug info to be indexed.
+                try:
+                    prog["foo"]
+                except KeyError:
+                    pass
             self.assertTrue(
                 any(
                     "split DWARF file split.dwo not found" in output
@@ -7105,7 +7124,12 @@ class TestSplitDwarf(TestCase):
                     )
                 )
             with self.assertLogs(logging.getLogger("drgn"), "WARNING") as log:
-                prog.load_debug_info([f.name])
+                add_extra_dwarf(prog, f.name)
+                # Force debug info to be indexed.
+                try:
+                    prog["foo"]
+                except KeyError:
+                    pass
             self.assertTrue(
                 any(
                     "split DWARF file split.dwo not found" in output

commit c12b6f84c1b4347dfe440f05ccca118e7a0d2360

libdrgn: elf_file: fix handling of .zdebug_str
We don't explicitly decompress .zdebug_* sections; libdw incidentally
does it for us in dwarf_begin_elf(). However, we currently call
drgn_elf_file_read_cached_section() before dwarf_begin_elf(), and that
calls truncate_elf_string_data() on .zdebug_str. This corrupts the
compressed data by removing anything after the last zero byte.

If libdw still manages to decompress it, it also doesn't maintain our
guarantee that the section is null-terminated, so a crafted file can
trigger an out-of-bounds read while indexing.

Fix it by tracking GNU-compressed sections and decompressing them
ourselves.

Fixes: 4e83130008e9 ("Introduce module and debug info finder APIs")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/elf_file.c
@@ -12,6 +12,7 @@
 #include <unistd.h>
 
 #include "array.h"
+#include "bitmap.h"
 #include "cleanup.h"
 #include "debug_info.h"
 #include "drgn_internal.h"
@@ -41,6 +42,14 @@ enum drgn_dwarf_file_type {
 	DRGN_DWARF_FILE_PLAIN,
 };
 
+static void drgn_elf_file_create_cleanup(struct drgn_elf_file **filep)
+{
+	if (*filep) {
+		free((*filep)->gnu_compressed_sections);
+		free(*filep);
+	}
+}
+
 struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
 					const char *path, int fd, char *image,
 					Elf *elf, struct drgn_elf_file **ret)
@@ -52,7 +61,8 @@ struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
 	if (!ehdr)
 		return drgn_error_libelf();
 
-	_cleanup_free_ struct drgn_elf_file *file = calloc(1, sizeof(*file));
+	_cleanup_(drgn_elf_file_create_cleanup) struct drgn_elf_file *file =
+		calloc(1, sizeof(*file));
 	if (!file)
 		return &drgn_enomem;
 
@@ -127,10 +137,24 @@ struct drgn_error *drgn_elf_file_create(struct drgn_module *module,
 			if (strstartswith(scnname, ".debug_") ||
 			    strstartswith(scnname, ".zdebug_")) {
 				const char *subname;
-				if (strstartswith(scnname, ".zdebug_"))
+				if (strstartswith(scnname, ".zdebug_")) {
 					subname = scnname + sizeof(".zdebug_") - 1;
-				else
+
+					if (!file->gnu_compressed_sections) {
+						size_t shdrnum;
+						if (elf_getshdrnum(elf,
+								   &shdrnum))
+							return drgn_error_libelf();
+						file->gnu_compressed_sections =
+							drgn_bitmap_create(shdrnum);
+						if (!file->gnu_compressed_sections)
+							return &drgn_enomem;
+					}
+					drgn_bitmap_set_bit(file->gnu_compressed_sections,
+							    elf_ndxscn(scn));
+				} else {
 					subname = scnname + sizeof(".debug_") - 1;
+				}
 				size_t len = strlen(subname);
 				if (len >= 4
 				    && strcmp(subname + len - 4, ".dwo") == 0) {
@@ -209,6 +233,7 @@ void drgn_elf_file_destroy(struct drgn_elf_file *file)
 {
 	if (file) {
 		free(file->sections_with_address);
+		free(file->gnu_compressed_sections);
 		dwarf_end(file->_dwarf);
 		elf_end(file->elf);
 		if (file->fd >= 0)
@@ -543,6 +568,14 @@ struct drgn_error *drgn_elf_file_read_section(struct drgn_elf_file *file,
 		err = drgn_elf_file_apply_relocations(file);
 		if (err)
 			return err;
+	}
+
+	if (file->gnu_compressed_sections
+	    && drgn_bitmap_test_bit(file->gnu_compressed_sections,
+				    elf_ndxscn(scn))) {
+		// Ignore errors since we can't tell whether it has already been
+		// decompressed.
+		elf_compress_gnu(scn, 0, 0);
 	}
 
 	if ((shdr->sh_flags & SHF_COMPRESSED) && elf_compress(scn, 0, 0) < 0)

libdrgn/elf_file.h
@@ -117,6 +117,8 @@ struct drgn_elf_file {
 	 * `.debug_str` section data.
 	 */
 	Elf_Data *alt_debug_str_data;
+	/** Bitmap of GNU-compressed sections, or @c NULL if there are none. */
+	unsigned long *gnu_compressed_sections;
 	/**
 	 * For relocatable files, a bitmap of which sections have their address
 	 * set.

tests/test_dwarf.py
@@ -7197,12 +7197,23 @@ class TestProgram(TestCase):
 
 
 class TestCompressedDebugSections(TestCase):
+    # int_die using DW_FORM_strp instead of DW_FORM_string to exercise
+    # .{z,}debug_str.
+    int_die = DwarfDie(
+        DW_TAG.base_type,
+        (
+            DwarfAttrib(DW_AT.byte_size, DW_FORM.data1, 4),
+            DwarfAttrib(DW_AT.encoding, DW_FORM.data1, DW_ATE.signed),
+            DwarfAttrib(DW_AT.name, DW_FORM.strp, "int"),
+        ),
+    )
+
     def test_zlib_gnu(self):
-        prog = dwarf_program(wrap_test_type_dies(int_die), compress="zlib-gnu")
+        prog = dwarf_program(wrap_test_type_dies(self.int_die), compress="zlib-gnu")
         self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
 
     def test_zlib_gabi(self):
-        prog = dwarf_program(wrap_test_type_dies(int_die), compress="zlib-gabi")
+        prog = dwarf_program(wrap_test_type_dies(self.int_die), compress="zlib-gabi")
         self.assertIdentical(prog.type("TEST").type, prog.int_type("int", 4, True))
 
 