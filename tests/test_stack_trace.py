osandov/drgn

commit 9fecb2728322f4df7eb7d367597a74b5c4879a70

Add drgn.Program.stack_trace_from_pcs()
This function is useful when you only have a list of PCs
and not the full stack trace, for example when working with
the stack depot.

Signed-off-by: Peter Collingbourne <pcc@google.com>

libdrgn/stack_trace.c
@@ -1131,6 +1131,37 @@ drgn_program_stack_trace(struct drgn_program *prog, uint32_t tid,
 }
 
 LIBDRGN_PUBLIC struct drgn_error *
+drgn_program_stack_trace_from_pcs(struct drgn_program *prog, const uint64_t *pcs,
+				  size_t pcs_size, struct drgn_stack_trace **ret)
+{
+	struct drgn_stack_trace *trace = malloc_flexible_array(
+		struct drgn_stack_trace, frames, pcs_size);
+	struct drgn_error *err;
+	size_t trace_capacity = pcs_size;
+
+	if (!trace)
+		return &drgn_enomem;
+
+	trace->prog = prog;
+	trace->num_frames = 0;
+	for (size_t i = 0; i != pcs_size; ++i) {
+		struct drgn_register_state *regs =
+			drgn_register_state_create_impl(0, 0, false);
+		drgn_register_state_set_pc(prog, regs, pcs[i]);
+
+		err = drgn_stack_trace_add_frames(&trace, &trace_capacity, regs);
+		if (err) {
+			drgn_stack_trace_destroy(trace);
+			return err;
+		}
+	}
+
+	drgn_stack_trace_shrink_to_fit(&trace, trace_capacity);
+	*ret = trace;
+	return NULL;
+}
+
+LIBDRGN_PUBLIC struct drgn_error *
 drgn_object_stack_trace(const struct drgn_object *obj,
 			struct drgn_stack_trace **ret)
 {

_drgn.pyi
@@ -271,6 +271,13 @@ class Program:
             ``struct task_struct *`` object.
         """
         ...
+    def stack_trace_from_pcs(self, pcs: Sequence[IntegerLike]) -> StackTrace:
+        """
+        Get a stack trace with the supplied list of program counters.
+
+        :param pcs: List of program counters.
+        """
+        ...
     @overload
     def type(self, name: str, filename: Optional[str] = None) -> Type:
         """

libdrgn/drgn.h
@@ -3095,6 +3095,19 @@ struct drgn_error *drgn_program_stack_trace(struct drgn_program *prog,
 					    struct drgn_stack_trace **ret);
 
 /**
+ * Get a stack trace with the supplied list of program counters.
+ *
+ * @param[out] ret Returned stack trace. On success, it should be freed with
+ * @ref drgn_stack_trace_destroy(). On error, its contents are undefined.
+ * @return @c NULL on success, non-@c NULL on error.
+ */
+struct drgn_error *
+drgn_program_stack_trace_from_pcs(struct drgn_program *prog,
+				  const uint64_t *pcs,
+				  size_t pcs_size,
+				  struct drgn_stack_trace **ret);
+
+/**
  * Get a stack trace for the thread represented by @p obj.
  *
  * @sa drgn_program_stack_trace().

libdrgn/python/program.c
@@ -864,6 +864,43 @@ static PyObject *Program_stack_trace(Program *self, PyObject *args,
 	return ret;
 }
 
+static PyObject *Program_stack_trace_from_pcs(Program *self, PyObject *args,
+					      PyObject *kwds)
+{
+	static char *keywords[] = {"pcs", NULL};
+	struct drgn_error *err;
+	PyObject *pypcs;
+	struct drgn_stack_trace *trace;
+
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "O:stack_trace_from_pcs",
+					 keywords, &pypcs))
+		return NULL;
+
+	_cleanup_pydecref_ PyObject *pypcseq = PySequence_Fast(
+		pypcs, "stack_trace_from_pcs() argument 1 must be a list");
+	if (!pypcseq)
+		return NULL;
+
+	size_t size = PySequence_Fast_GET_SIZE(pypcseq);
+	_cleanup_free_ uint64_t *pcs = malloc_array(size, sizeof(uint64_t));
+	for (uint64_t i = 0; i != size; ++i) {
+		struct index_arg pc = {};
+
+		if (!index_converter(PySequence_Fast_GET_ITEM(pypcseq, i), &pc))
+			return NULL;
+		pcs[i] = pc.uvalue;
+	}
+
+	err = drgn_program_stack_trace_from_pcs(&self->prog, pcs, size, &trace);
+	if (err)
+		return set_drgn_error(err);
+
+	PyObject *ret = StackTrace_wrap(trace);
+	if (!ret)
+		drgn_stack_trace_destroy(trace);
+	return ret;
+}
+
 static PyObject *Program_symbols(Program *self, PyObject *args)
 {
 	struct drgn_error *err;
@@ -1158,6 +1195,8 @@ static PyMethodDef Program_methods[] = {
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_variable_DOC},
 	{"stack_trace", (PyCFunction)Program_stack_trace,
 	 METH_VARARGS | METH_KEYWORDS, drgn_Program_stack_trace_DOC},
+	{"stack_trace_from_pcs", (PyCFunction)Program_stack_trace_from_pcs,
+	 METH_VARARGS | METH_KEYWORDS, drgn_Program_stack_trace_from_pcs_DOC},
 	{"symbols", (PyCFunction)Program_symbols, METH_VARARGS,
 	 drgn_Program_symbols_DOC},
 	{"symbol", (PyCFunction)Program_symbol, METH_O,

commit f83b67f901151a072393df0d26f1b213e1dde865

libdrgn: stack_trace: require architecture support for drgn_stack_trace_from_pcs()
drgn_stack_trace_from_pcs() doesn't check if the program has a platform
that supports stack traces. If the program doesn't have a platform or
the platform doesn't support stack traces, then several functions (e.g.,
drgn_stack_frame_sp(), drgn_stack_frame_register(),
drgn_stack_frame_find_object()) may dereference a null platform or
callback pointer.

Add a check.

Fixes: 9fecb2728322 ("Add drgn.Program.stack_trace_from_pcs()")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/stack_trace.c
@@ -1371,6 +1371,16 @@ LIBDRGN_PUBLIC struct drgn_error *
 drgn_program_stack_trace_from_pcs(struct drgn_program *prog, const uint64_t *pcs,
 				  size_t pcs_size, struct drgn_stack_trace **ret)
 {
+	if (!prog->has_platform) {
+		return drgn_error_create(DRGN_ERROR_INVALID_ARGUMENT,
+					 "cannot create stack trace without platform");
+	}
+	if (!prog->platform.arch->register_layout) {
+		return drgn_error_format(DRGN_ERROR_NOT_IMPLEMENTED,
+					 "stack traces are not supported on %s architecture",
+					 prog->platform.arch->name);
+	}
+
 	_cleanup_stack_trace_ struct drgn_stack_trace *trace =
 		malloc_flexible_array(struct drgn_stack_trace, frames,
 				      pcs_size);

tests/test_stack_trace.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from drgn import Object, Program
+from drgn import Architecture, Object, Platform, PlatformFlags, Program
 from tests import TestCase
 from tests.resources import get_resource
 
@@ -52,4 +52,21 @@ class TestLinuxUserspaceCoreDump(TestCase):
             TypeError,
             self.prog.stack_trace,
             Object(self.prog, self.prog.pointer_type(self.prog.struct_type(None)), 0),
+        )
+
+
+class TestStackTraceFromPcs(TestCase):
+    def test_no_platform(self):
+        self.assertRaises(ValueError, Program().stack_trace_from_pcs, [0xDEADBEEF])
+
+    def test_unknown_platform(self):
+        self.assertRaises(
+            NotImplementedError,
+            Program(
+                Platform(
+                    Architecture.UNKNOWN,
+                    PlatformFlags.IS_64_BIT | PlatformFlags.IS_LITTLE_ENDIAN,
+                )
+            ).stack_trace_from_pcs,
+            [0xDEADBEEF],
         )