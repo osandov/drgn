osandov/drgn

commit 9f56b1eeba0a4d3798b13f9aa32a565c9b62b400

libdrgn: dwarf_info: add bias to DW_OP_addr in the middle of an expression
While evaluating a DWARF location, we currently add the debug file bias
once, at the very end. This works for DW_OP_addr operations at the end
of a location description, but if there is a DW_OP_addr operation in the
middle of an expression, it ignores the bias.

Fix it by adding the bias immediately after a DW_OP_addr (or DW_OP_addrx
or DW_OP_GNU_addr_index) operation instead.

P.S. This manifested as FaultErrors when accessing the vmap_zone_size
variable on a kernel compiled with Clang 19. The reason why is really
interesting: vmap_zone_size only has two possible values, 1 and
(1 << 4) * PAGE_SIZE. It's only used in one place:

	static inline unsigned int
	addr_to_node_id(unsigned long addr)
	{
		return (addr / vmap_zone_size) % nr_vmap_nodes;
	}

To optimize that division, Clang apparently replaced vmap_zone_size with
a shift such that the division (addr / vmap_zone_size) could be replaced
with (addr >> (shift << 4)). The location description for vmap_zone_zone
is therefore:

	DW_OP_addr 0xffffffff82350194, DW_OP_deref_size 0x1,
	DW_OP_constu 0xffff, DW_OP_mul, DW_OP_lit1, DW_OP_plus,
	DW_OP_stack_value

Fixes: 35a1af7ad690 ("libdrgn: add DWARF expression evaluation")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/dwarf_info.c
@@ -3500,6 +3500,19 @@ drgn_eval_dwarf_expression(struct drgn_dwarf_expression_context *ctx,
 							   address_size,
 							   &uvalue)))
 				return err;
+addr:
+			/*
+			 * If the address is not in the module's address range,
+			 * then it's probably something special like a Linux
+			 * per-CPU variable (which isn't actually a variable
+			 * address but an offset). Don't apply the bias in that
+			 * case.
+			 */
+			if (ctx->file->module->start
+			    <= uvalue + ctx->file->module->debug_file_bias
+			    && uvalue + ctx->file->module->debug_file_bias
+			       < ctx->file->module->end)
+				uvalue += ctx->file->module->debug_file_bias;
 			PUSH(uvalue);
 			break;
 		case DW_OP_const1u:
@@ -3562,8 +3575,19 @@ drgn_eval_dwarf_expression(struct drgn_dwarf_expression_context *ctx,
 			PUSH_MASK(uvalue);
 			break;
 		case DW_OP_addrx:
-		case DW_OP_constx:
 		case DW_OP_GNU_addr_index:
+			if (!ctx->cu_die.addr) {
+				ctx->bb.pos = ctx->bb.prev;
+				return NULL;
+			}
+			if ((err = drgn_dwarf_next_addrx(&ctx->bb, ctx->file,
+							 &ctx->cu_die,
+							 address_size,
+							 &ctx->cu_addr_base,
+							 &uvalue)))
+				return err;
+			goto addr;
+		case DW_OP_constx:
 		case DW_OP_GNU_const_index:
 			if (!ctx->cu_die.addr) {
 				ctx->bb.pos = ctx->bb.prev;
@@ -4627,17 +4651,6 @@ absent:
 		drgn_object_reinit(ret, &type, DRGN_OBJECT_ABSENT);
 		err = NULL;
 	} else if (bit_offset >= 0) {
-		uint64_t biased_address =
-			address + file->module->debug_file_bias;
-		/*
-		 * If the address is not in the module's address range, then
-		 * it's probably something special like a Linux per-CPU variable
-		 * (which isn't actually a variable address but an offset).
-		 * Don't apply the bias in that case.
-		 */
-		if (file->module->start <= biased_address
-		    && biased_address < file->module->end)
-			address = biased_address;
 		err = drgn_object_set_reference_internal(ret, &type, address,
 							 bit_offset);
 	} else if (type.encoding == DRGN_OBJECT_ENCODING_BUFFER) {

commit 81756688120fcb94e25f059140d10b58dad7605c

libdrgn: dwarf_info: apply address mask after adding bias for DW_OP_addr
The elements in the DWARF expression stack are assumed to be the size of
an address. However, DW_OP_addr doesn't enforce this if it has to add a
bias. Mask it when we apply a bias.

Fixes: 9f56b1eeba0a ("libdrgn: dwarf_info: add bias to DW_OP_addr in the middle of an expression")
Signed-off-by: Omar Sandoval <osandov@osandov.com>

libdrgn/dwarf_info.c
@@ -3954,8 +3954,10 @@ addr:
 			 * case.
 			 */
 			if (drgn_module_contains_address(ctx->file->module,
-					uvalue + ctx->file->module->debug_file_bias))
-				uvalue += ctx->file->module->debug_file_bias;
+					uvalue + ctx->file->module->debug_file_bias)) {
+				uvalue = (uvalue + ctx->file->module->debug_file_bias)
+					 & address_mask;
+			}
 			PUSH(uvalue);
 			break;
 		case DW_OP_const1u: